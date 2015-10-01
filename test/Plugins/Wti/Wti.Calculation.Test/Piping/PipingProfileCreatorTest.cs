1791d59ecae6050732d7878278154dc4f3bc21f2
Joël van den Berg <joel.vandenberg@deltares.nl>
2015-10-01 08:24:38 +0000

    Made Wti.Calculation internals visible to Wti.Calculation.Test. Added tests for PipingProfileCreatorTest and PipingSurfaceLineCreatorTest Related to Issue [WTI-8]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@72 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 612f8cf03f29ebdd71f568afcc0f71543ce51468

---
+++ test/Plugins/Wti/Wti.Calculation.Test/Piping/PipingProfileCreatorTest.cs
@@ -0,0 +1,17 @@
+﻿using Deltares.WTIPiping;
+using NUnit.Framework;
+using Wti.Calculation.Piping;
+
+namespace Wti.Calculation.Test.Piping
+{
+    public class PipingProfileCreatorTest
+    {
+        [Test]
+        public void GivenAPipingProfileCreator_WhenCreatingPipingProfile_ThenAProfileWithALayerIsReturned()
+        {
+            var pipingProfileCreator = new PipingProfileCreator();
+            PipingProfile actual = pipingProfileCreator.Create();
+            Assert.That(actual.Layers, Is.Not.Empty);
+        }
+    }
+}
---
+++ test/Plugins/Wti/Wti.Calculation.Test/Piping/PipingSurfaceLineCreatorTest.cs
@@ -0,0 +1,21 @@
+﻿using Deltares.WTIPiping;
+using NUnit.Framework;
+using Wti.Calculation.Piping;
+
+namespace Wti.Calculation.Test.Piping
+{
+    public class PipingSurfaceLineCreatorTest
+    {
+        [Test]
+        public void GivenASurfaceLineCreator_WhenCreatingPipingSurfaceLine_ThenASurfaceLineWithASinglePointAtOrigin()
+        {
+            var pipingSurfaceLineCreator = new PipingSurfaceLineCreator();
+            PipingSurfaceLine actual = pipingSurfaceLineCreator.Create();
+            Assert.That(actual.Points, Is.Not.Empty);
+            Assert.That(actual.Points[0].X, Is.EqualTo(0));
+            Assert.That(actual.Points[0].Y, Is.EqualTo(0));
+            Assert.That(actual.Points[0].Z, Is.EqualTo(0));
+        }
+         
+    }
+}
