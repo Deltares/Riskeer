8702d012630646100a38f3e7f3e8ea3dd9749718
Elmar Kleijn <elmar.kleijn@deltares.nl>
2015-10-01 13:40:00 +0000

    Removed ActivityWrapper Related to Issue [WTI-5]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@84 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: adbb38022697792dd74c6ec05818dda89c2b3470

--- src/Common/DelftTools.Shell.Core/Extensions/CompositeActivityExtensions.cs
+++ src/Common/DelftTools.Shell.Core/Extensions/CompositeActivityExtensions.cs
@@ -1,32 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using DelftTools.Shell.Core.Workflow;
 
 namespace DelftTools.Shell.Core.Extensions
 {
     public static class CompositeActivityExtensions
     {
-        /// <summary>
-        /// Get activities in this composition, that according to the currently selected workflow, run simultaneous with the given target activity.
-        /// Note: Simultaneous = parallel
-        /// </summary>
-        /// <param name="compositeModel"></param>
-        /// <param name="targetActivity"></param>
-        /// <returns></returns>
-        public static IEnumerable<IActivity> GetActivitiesRunningSimultaneous(this ICompositeActivity compositeModel, IActivity targetActivity)
-        {
-            var workflow = compositeModel.CurrentWorkflow ?? compositeModel; //if compositeModel is already a workflow, this also works fine
-
-            var parentActivity = GetParentActivity(workflow, targetActivity);
-
-            //we make the assumption here that all other composite activities run parallel
-            if (parentActivity != null && !(parentActivity is SequentialActivity)) 
-            {
-                return parentActivity.Activities.Select(UnwrapActivity).Where(a => a != targetActivity);
-            }
-            return new IActivity[0];
-        }
-
         public static IEnumerable<T> GetAllActivitiesRecursive<T>(this ICompositeActivity compositeModel) where T : IActivity
         {
             if (compositeModel is T)
@@ -58,26 +36,26 @@ namespace DelftTools.Shell.Core.Extensions
         {
             var compositeWorkflow = workflow as ICompositeActivity;
             if (compositeWorkflow == null)
+            {
                 return null;
+            }
 
             foreach (var a in compositeWorkflow.Activities)
             {
-                var activity = UnwrapActivity(a);
-
-                if (activity == targetActivity)
+                if (a == targetActivity)
+                {
                     return compositeWorkflow;
+                }
 
-                var parent = GetParentActivity(activity, targetActivity);
+                var parent = GetParentActivity(a, targetActivity);
 
                 if (parent != null)
+                {
                     return parent;
+                }
             }
-            return null;
-        }
 
-        private static IActivity UnwrapActivity(IActivity activity)
-        {
-            return activity is ActivityWrapper ? ((ActivityWrapper) activity).Activity : activity;
+            return null;
         }
     }
 }
