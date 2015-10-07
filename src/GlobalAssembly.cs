d57dbb7b7ec7e4b8e9124ffb09cf2e615adb265e
Joël van den Berg <joel.vandenberg@deltares.nl>
2015-10-07 15:25:17 +0000

    Added GlobalAssembly.cs Added references from WTI plugin projects and DeltaShell.Core and DeltaShell.Gui projects. Related to Issue [WTI-39]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@125 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 5aed324bdb939627c095ddc67aea052171a6c7e0

---
+++ src/GlobalAssembly.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Deltares")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © Deltares 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
