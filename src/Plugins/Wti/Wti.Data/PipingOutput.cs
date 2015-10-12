c3260121a8933407d845c73ed37262997ecc708d
Joël van den Berg <joel.vandenberg@deltares.nl>
2015-10-12 14:49:50 +0000

    Documented PipingOutput Related to Issue [WTI-15]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@197 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 09e8fb0bfcd48cd75785eb11cd8a62ac33cef5f8

--- src/Plugins/Wti/Wti.Data/PipingOutput.cs
+++ src/Plugins/Wti/Wti.Data/PipingOutput.cs
@@ -1,7 +1,19 @@
 ﻿namespace Wti.Data
 {
+    /// <summary>
+    /// Simple class containing the results of a Piping calculation.
+    /// </summary>
     public class PipingOutput
     {
+        /// <summary>
+        /// Creates a new instance of <see cref="PipingOutput"/>. 
+        /// </summary>
+        /// <param name="upliftZValue">The calculated z-value for the uplift sub calculation.</param>
+        /// <param name="upliftFactorOfSafety">The factor of safety for the uplift sub calculation.</param>
+        /// <param name="heaveZValue">The calculated z-value for the heave sub calculation.</param>
+        /// <param name="heaveFactorOfSafety">The factor of safety for the heave sub calculation.</param>
+        /// <param name="sellmeijerZValue">The calculated z-value for the Sellmeijer sub calculation.</param>
+        /// <param name="sellmeijerFactorOfSafety">The factor of safety for the Sellmeijer sub calculation.</param>
         public PipingOutput(double upliftZValue, double upliftFactorOfSafety, double heaveZValue, double heaveFactorOfSafety, double sellmeijerZValue, double sellmeijerFactorOfSafety)
         {
             HeaveFactorOfSafety = heaveFactorOfSafety;
@@ -12,11 +24,34 @@
             SellmeijerZValue = sellmeijerZValue;
         }
 
-        public double HeaveFactorOfSafety { get; private set; }
-        public double HeaveZValue { get; private set; }
-        public double UpliftFactorOfSafety { get; private set; }
+        /// <summary>
+        /// The calculated z-value for the uplift sub calculation.
+        /// </summary>
         public double UpliftZValue { get; private set; }
-        public double SellmeijerFactorOfSafety { get; private set; }
+
+        /// <summary>
+        /// The factor of safety for the uplift sub calculation.
+        /// </summary>
+        public double UpliftFactorOfSafety { get; private set; }
+
+        /// <summary>
+        /// The calculated z-value for the heave sub calculation.
+        /// </summary>
+        public double HeaveZValue { get; private set; }
+
+        /// <summary>
+        /// The factor of safety for the heave sub calculation.
+        /// </summary>
+        public double HeaveFactorOfSafety { get; private set; }
+
+        /// <summary>
+        /// The calculated z-value for the Sellmeijer sub calculation.
+        /// </summary>
         public double SellmeijerZValue { get; private set; }
+
+        /// <summary>
+        /// The factor of safety for the Sellmeijer sub calculation.
+        /// </summary>
+        public double SellmeijerFactorOfSafety { get; private set; }
     }
 }
