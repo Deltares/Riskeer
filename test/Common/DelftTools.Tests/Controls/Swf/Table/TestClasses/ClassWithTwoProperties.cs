ca26f9cd3c2d36b58d3102b0bd4e015fc6317183
Elmar Kleijn <elmar.kleijn@deltares.nl>
2015-10-12 06:50:40 +0000

    Removed unused classes Related to Issue [WTI-70]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@166 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 2c2b0ae9bbd6449e5789c9a1031d666c090f7bf7

--- test/Common/DelftTools.Tests/Controls/Swf/Table/TestClasses/ClassWithTwoProperties.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DelftTools.Controls.Swf.Test.Table.TestClasses
-{
-    public class ClassWithTwoProperties
-    {
-        public string Property1 { get; set; }
-        public string Property2 { get; set; }
-    }
-}
