bf3ab30ae42c1bdd2ec903b78194c50d927ad57b
Joël van den Berg <joel.vandenberg@deltares.nl>
2015-10-12 06:54:20 +0000

    Implemented Default values for Piping, which match the ones in the Piping Kernel's documentation. Related to Issue [WTI-13]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@167 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 521c34b0f5ce9305b1e9143e93478ae8d0998a35

--- src/Plugins/Wti/Wti.Data/PipingData.cs
+++ src/Plugins/Wti/Wti.Data/PipingData.cs
@@ -8,6 +8,236 @@ namespace Wti.Data
         private readonly IList<IObserver> observers = new List<IObserver>();
         private PipingOutput output;
 
+        // Defaults as they have been defined in the DikesPiping Kernel's Technical Documentation of 07 Oct 15
+        private double dampingFactorExit = 1.0;
+        private double sellmeijerReductionFactor = 0.3;
+        private double waterVolumetricWeight = 9.81;
+        private double sandParticlesVolumicWeight = 16.5;
+        private double whitesDragCoefficient = 0.25;
+        private double waterKinematicViscosity = 1.33e-6;
+        private double gravity = 9.81;
+        private double meanDiameter70 = 2.08e-4;
+        private double beddingAngle = 37.0;
+
+        /// <summary>
+        /// Gets or sets the damping factor at the exit point.
+        /// </summary>
+        public double DampingFactorExit
+        {
+            get
+            {
+                return dampingFactorExit;
+            }
+            set
+            {
+                dampingFactorExit = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the reduction factor Sellmeijer.
+        /// </summary>
+        public double SellmeijerReductionFactor
+        {
+            get
+            {
+                return sellmeijerReductionFactor;
+            }
+            set
+            {
+                sellmeijerReductionFactor = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the volumetric weight of water.
+        /// [kN/m&#179;]
+        /// </summary>
+        public double WaterVolumetricWeight
+        {
+            get
+            {
+                return waterVolumetricWeight;
+            }
+            set
+            {
+                waterVolumetricWeight = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the (lowerbound) volumic weight of sand grain material of a sand layer under water.
+        /// [kN/m&#179;]
+        /// </summary>
+        public double SandParticlesVolumicWeight
+        {
+            get
+            {
+                return sandParticlesVolumicWeight;
+            }
+            set
+            {
+                sandParticlesVolumicWeight = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the White's drag coefficient.
+        /// </summary>
+        public double WhitesDragCoefficient
+        {
+            get
+            {
+                return whitesDragCoefficient;
+            }
+            set
+            {
+                whitesDragCoefficient = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the kinematic viscosity of water at 10 degrees Celsius.
+        /// [m&#178;/s]
+        /// </summary>
+        public double WaterKinematicViscosity
+        {
+            get
+            {
+                return waterKinematicViscosity;
+            }
+            set
+            {
+                waterKinematicViscosity = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the gravitational acceleration.
+        /// [m/s&#178;]
+        /// </summary>
+        public double Gravity
+        {
+            get
+            {
+                return gravity;
+            }
+            set
+            {
+                gravity = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the mean diameter of small scale tests applied to different kinds of sand, on which the formula of Sellmeijer has been fit.
+        /// [m]
+        /// </summary>
+        public double MeanDiameter70
+        {
+            get
+            {
+                return meanDiameter70;
+            }
+            set
+            {
+                meanDiameter70 = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the angle of the force balance representing the amount in which sand grains resist rolling.
+        /// [&#176;]
+        /// </summary>
+        public double BeddingAngle
+        {
+            get
+            {
+                return beddingAngle;
+            }
+            set
+            {
+                beddingAngle = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the calculation value used to account for uncertainty in the model for uplift.
+        /// </summary>
+        public double UpliftModelFactor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the outside high water level.
+        /// [m]
+        /// </summary>
+        public double AssessmentLevel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the piezometric head at the exit point.
+        /// [m]
+        /// </summary>
+        public double PiezometricHeadExit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phreatic level at the exit point.
+        /// [m]
+        /// </summary>
+        public double PhreaticLevelExit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the piezometric head in the hinterland.
+        /// [m]
+        /// </summary>
+        public double PiezometricHeadPolder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the critical exit gradient for heave.
+        /// </summary>
+        public double CriticalHeaveGradient { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total thickness of the coverage layer at the exit point.
+        /// [m]
+        /// </summary>
+        public double ThicknessCoverageLayer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the calculation value used to account for uncertainty in the model for Sellmeijer.
+        /// </summary>
+        public double SellmeijerModelFactor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the horizontal distance between entree and exit point.
+        /// [m]
+        /// </summary>
+        public double SeepageLength { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sieve size through which 70% fraction of the grains of the top part of the aquifer passes.
+        /// [m]
+        /// </summary>
+        public double Diameter70 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Darcy-speed with which water flows through the aquifer layer.
+        /// [m/s]
+        /// </summary>
+        public double DarcyPermeability { get; set; }
+
+        /// <summary>
+        /// Gets or sets the thickness of the aquifer layer.
+        /// [m]
+        /// </summary>
+        public double ThicknessAquiferLayer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the x coordinate of the exit point.
+        /// [m]
+        /// </summary>
+        public double ExitPointXCoordinate { get; set; }
+
+        /// <summary>
+        /// Gets or sets <see cref="PipingOutput"/>, which contains the results of a Piping calculation.
+        /// </summary>
         public PipingOutput Output
         {
             get
@@ -20,29 +250,6 @@ namespace Wti.Data
                 NotifyObservers();
             }
         }
-
-        public double WaterVolumetricWeight { get; set; }
-        public double UpliftModelFactor { get; set; }
-        public double AssessmentLevel { get; set; }
-        public double PiezometricHeadExit { get; set; }
-        public double DampingFactorExit { get; set; }
-        public double PhreaticLevelExit { get; set; }
-        public double PiezometricHeadPolder { get; set; }
-        public double CriticalHeaveGradient { get; set; }
-        public double ThicknessCoverageLayer { get; set; }
-        public double SellmeijerModelFactor { get; set; }
-        public double SellmeijerReductionFactor { get; set; }
-        public double SeepageLength { get; set; }
-        public double SandParticlesVolumicWeight { get; set; }
-        public double WhitesDragCoefficient { get; set; }
-        public double Diameter70 { get; set; }
-        public double DarcyPermeability { get; set; }
-        public double WaterKinematicViscosity { get; set; }
-        public double Gravity { get; set; }
-        public double ThicknessAquiferLayer { get; set; }
-        public double MeanDiameter70 { get; set; }
-        public double BeddingAngle { get; set; }
-        public double ExitPointXCoordinate { get; set; }
 
         public void Attach(IObserver observer)
         {
