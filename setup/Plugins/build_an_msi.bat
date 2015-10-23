67c18c23205ca7b52c9fc528359b700af6b83108
Elmar Kleijn <elmar.kleijn@deltares.nl>
2015-10-23 08:59:06 +0000

    * Removed redundant setup files * Cleaned up build configuration Related to Issue [WTI-70]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@426 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 5382ee6b6b5d340f9b8dc75cc51c724a5550a31a

--- setup/Plugins/build_an_msi.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p pluginName="Enter plugin name: " %=%
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %pluginName%\%pluginName%.wixproj /t:Rebuild /p:SolutionDir=..\..\..\
-PAUSE
