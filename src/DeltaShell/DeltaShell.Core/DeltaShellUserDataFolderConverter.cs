bcbdc73908a20ed864f8e107c5dc01461caaedbf
Marten Janssen <marten.janssen@deltares.nl>
2015-10-23 05:58:59 +0000

    File should not have been removed in WTi-98 Related to Issue [WTI-98]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@412 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 95f58bd47ac0a99a0e0d7c6ff7f44f324c892b87

---
+++ src/DeltaShell/DeltaShell.Core/DeltaShellUserDataFolderConverter.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using DelftTools.Utils;
+using log4net.Util;
+
+namespace DeltaShell.Core
+{
+    //special folders not in latests stable (1.2.10) version of log4net hence this for DS user dir.
+    //Any arguments are ignored and just just return c:\user\muurman\appdata\deltares\ds1.0.0.0\
+    //from http://logging.apache.org/log4net/release/release-notes.html
+    /// <summary>
+    /// PatternConverter for 'Special' Folder 
+    /// </summary>
+    public class DeltaShellUserDataFolderConverter : PatternConverter // NOTE: Class might be marked as unused, but it's actually created in DeltaShell.Core/app.config!
+    {
+        protected override void Convert(TextWriter writer, object state)
+        {
+            var settingsDirectory = SettingsHelper.GetApplicationLocalUserSettingsDirectory();
+
+            DeleteOldLogFiles(settingsDirectory);
+
+            writer.Write(settingsDirectory);
+        }
+
+        private void DeleteOldLogFiles(string settingsDirectory)
+        {
+            var daysToKeepLogFiles = 30;
+
+            // HACK: don't keep log files for tests
+            if (settingsDirectory.ToLower().Contains("tests"))
+            {
+                daysToKeepLogFiles = 0;
+            }
+
+            var logFiles = Directory.GetFiles(settingsDirectory, "*.log");
+            foreach (var logFile in logFiles)
+            {
+                if ((DateTime.Now - File.GetCreationTime(logFile)).TotalDays > daysToKeepLogFiles)
+                {
+                    File.Delete(logFile);
+                }
+            }
+        }
+    }
+}
+
+/* Example: 
+ * <file type="log4net.Util.PatternString">
+        <converter>
+          <name value="dsuserdata" />
+          <type value="DeltaShell.Core.DeltaShellUserDataFolderConverter,DeltaShell.Core" />
+        </converter>
+        <conversionPattern value="%dsuserdata\log-file.txt" />
+      </file>
+ */
