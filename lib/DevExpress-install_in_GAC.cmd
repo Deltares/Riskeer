58a0e3f5185c36d5bcbb7e913950bde3ee058569
Elmar Kleijn <elmar.kleijn@deltares.nl>
2015-10-27 07:56:48 +0000

    Removed rendundant .cmd files from lib folder Related to Issue [WTI-70]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@473 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 47c67a9f5fd4f2d74215d75c3c13d121a83bce17

--- lib/DevExpress-install_in_GAC.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-for %%f in (.\DevExpress*.dll) do "..\build\tools\gacutil.exe" -I %%f
-rem for %%f in (DevExpress*.dll) do "C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\gacutil.exe" -U %%f
--- lib/ngen_install.cmd
+++
@@ -1,6 +0,0 @@
-for %%i in (*.dll;*.exe) do C:\Windows\Microsoft.NET\Framework64\v2.0.50727\ngen.exe install %%i
-for %%i in (*.dll;*.exe) do C:\Windows\Microsoft.NET\Framework64\v4.0.30319\ngen.exe install %%i
-
-for %%i in (*.dll;*.exe) do C:\Windows\Microsoft.NET\Framework\v2.0.50727\ngen.exe install %%i
-for %%i in (*.dll;*.exe) do C:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe install %%i
-
