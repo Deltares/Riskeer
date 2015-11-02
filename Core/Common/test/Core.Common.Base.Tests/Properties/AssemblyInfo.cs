2d061a0c1aadd094d138ad71d822717ea9203b51
Marten Janssen <marten.janssen@deltares.nl>
2015-11-02 15:07:28 +0000

    - Renamed Core.Common.DelftTools.Tests to Rename Core.Common.Base.Tests Related to Issue [WTI-92]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@602 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 3a60304e11c40d17dab49efddba42d9e476f7ee2

--- Core/Common/test/Core.Common.Base.Tests/Properties/AssemblyInfo.cs
+++ Core/Common/test/Core.Common.Base.Tests/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.InteropServices;
 using NUnit.Framework;
 
-[assembly: AssemblyTitle("Core.Common.DelftTools.Tests")]
-[assembly: AssemblyProduct("Core.Common.DelftTools.Tests")]
+[assembly: AssemblyTitle("Core.Common.Base.Tests")]
+[assembly: AssemblyProduct("Core.Common.Base.Tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
