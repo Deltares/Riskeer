fb52b2e510f4c7f1e5c23f7bd06347a7979a80a8
rodriqu_dd <rodriqu_dd@example.com>
2015-11-09 12:43:42 +0000

    Removed Ringtoets manual from repository with code.
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@687 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 419ff6dadee9bb7aa939de009e8f4190bf29d63a

--- doc/manual/set_default_cover_pages.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-if  not exist "cover\cover-pages.pdf" (
-    copy cover\default-cover-pages.pdf cover\cover-pages.pdf
-    copy cover\default-chapter-ribbon.jpg cover\chapter-ribbon.jpg
-)
