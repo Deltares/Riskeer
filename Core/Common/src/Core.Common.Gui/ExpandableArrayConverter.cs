05061cff6fc2d63d6be48e570153eb7909e01298
bouvrie <bouvrie@example.com>
2015-11-11 14:29:31 +0000

    * Changed ExpandableArrayConverter to show child properties with index starting from 1 instead of 0. * Improved test coverage for ExpandableArrayConverter Related to Issue [WTI-20]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@747 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 5dddedd73719227f9924d82d626a99fa1a15cec9

--- Core/Common/src/Core.Common.Gui/ExpandableArrayConverter.cs
+++ Core/Common/src/Core.Common.Gui/ExpandableArrayConverter.cs
@@ -6,18 +6,78 @@ using Core.Common.Gui.Properties;
 namespace Core.Common.Gui
 {
     /// <summary>
-    /// <see cref="ArrayConverter"/> with modified conversion to string.
+    /// <see cref="ArrayConverter"/> with modified conversion to string and shows as array
+    /// starting with index 1 instead of 0.
     /// </summary>
     public class ExpandableArrayConverter : ArrayConverter
     {
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
         {
-            if (destinationType == typeof(string) && value is Array)
+            var array = value as Array;
+            if (destinationType == typeof(string) && array != null)
             {
-                return string.Format(Resources.ExpandableArrayConverter_ConvertTo_Aantal_0_, ((Array) value).Length);
+                return string.Format(Resources.ExpandableArrayConverter_ConvertTo_Aantal_0_, array.GetLength(0));
             }
 
             return base.ConvertTo(context, culture, value, destinationType);
         }
+
+        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+        {
+            PropertyDescriptor[] properties = null;
+            var array = value as Array;
+            if (array != null)
+            {
+                int length = array.GetLength(0);
+                properties = new PropertyDescriptor[length];
+
+                Type type = array.GetType();
+                Type elementType = type.GetElementType();
+                for (int index = 0; index < length; ++index)
+                {
+                    properties[index] = new ArrayPropertyDescriptor(type, elementType, index);
+                }
+            }
+            return new PropertyDescriptorCollection(properties);
+        }
+
+        #region Nested Type: ArrayPropertyDescriptor
+
+        /// <summary>
+        /// Array element property descriptor used by <see cref="ExpandableArrayConverter"/>.
+        /// Properties are named based on their index + 1.
+        /// </summary>
+        private class ArrayPropertyDescriptor : SimplePropertyDescriptor
+        {
+            private readonly int index;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="ArrayPropertyDescriptor"/> class.
+            /// </summary>
+            /// <param name="arrayType">Type of the array.</param>
+            /// <param name="elementType">Type of the elements in <paramref name="arrayType"/>.</param>
+            /// <param name="elementIndex">Index of the element corresponding with this property descriptor.</param>
+            public ArrayPropertyDescriptor(Type arrayType, Type elementType, int elementIndex)
+                : base(arrayType, "[" + (elementIndex + 1) + "]", elementType, null)
+            {
+                index = elementIndex;
+            }
+
+            public override object GetValue(object instance)
+            {
+                var array = (Array)instance;
+                return array.GetValue(index);
+            }
+
+            public override void SetValue(object instance, object value)
+            {
+                var array = (Array)instance;
+                array.SetValue(value, index);
+                OnValueChanged(array, EventArgs.Empty); // This line is also available in the C# .Net version of this class
+            }
+        }
+
+        #endregion
+
     }
 }
---
+++ Core/Common/test/Core.Common.Gui.Test/ExpandableArrayConverterTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+
+using NUnit.Framework;
+
+namespace Core.Common.Gui.Tests
+{
+    [TestFixture]
+    public class ExpandableArrayConverterTest
+    {
+        [Test]
+        public void DefaultConstructor_ExpectedValues()
+        {
+            // Call
+            var converter = new ExpandableArrayConverter();
+
+            // Assert
+            Assert.IsInstanceOf<ArrayConverter>(converter);
+        }
+
+        [Test]
+        public void ConvertTo_ValueIsArrayObject_ReturnExpectedText()
+        {
+            // Setup
+            var array = new[]
+            {
+                1.1,
+                2.2,
+                3.3
+            };
+            var converter = new ExpandableArrayConverter();
+
+            // Call
+            var text = (string)converter.ConvertTo(null, null, array, typeof(string));
+
+            // Assert
+            Assert.AreEqual("Aantal (3)", text);
+        }
+
+        [Test]
+        public void ConvertTo_ValueIsNotAnArrayObject_ReturnTypeToString()
+        {
+            // Setup
+            var o = new object();
+            var converter = new ExpandableArrayConverter();
+
+            // Call
+            object text = converter.ConvertTo(null, null, o, typeof(string));
+
+            // Assert
+            Assert.AreEqual(o.ToString(), text);
+        }
+
+        [Test]
+        public void GetProperties_ValueIsArrayObject_ReturnPropertyDescriptorsForArryEntries()
+        {
+            // Setup
+            var array = new[]
+            {
+                1.1,
+                2.2,
+                3.3
+            };
+            var converter = new ExpandableArrayConverter();
+
+            // Call
+            PropertyDescriptorCollection properties = converter.GetProperties(null, array, new Attribute[0]);
+
+            // Assert
+            Assert.AreEqual(array.Length, properties.Count);
+            int valueChangedCallCount = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                PropertyDescriptor propertyDescriptor = properties[i];
+
+                Assert.AreEqual(String.Format("[{0}]", i+1), propertyDescriptor.DisplayName);
+
+                Assert.AreEqual(array[i], propertyDescriptor.GetValue(array));
+
+                propertyDescriptor.AddValueChanged(array, delegate(object sender, EventArgs args)
+                {
+                    Assert.AreSame(array, sender);
+                    valueChangedCallCount++;
+                });
+                propertyDescriptor.SetValue(array, i);
+                Assert.AreEqual(i, array[i]);
+            }
+            Assert.AreEqual(array.Length, valueChangedCallCount);
+        }
+    }
+}
