02d4fb099ae6e6c228220838181eaa0f06d1d06f
Joël van den Berg <joel.vandenberg@deltares.nl>
2015-11-12 14:49:57 +0000

    Removed obsolete dutch language files. Related to Issue [WTI-6]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@777 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: b126cf72b62f8aaa1bb07f9dfbb38de2a9a18ef9

--- Core/Plugins/src/Core.Plugins.SharpMapGis/Properties/Resources.Designer.cs
+++ Core/Plugins/src/Core.Plugins.SharpMapGis/Properties/Resources.Designer.cs
@@ -107,30 +107,5 @@ namespace Core.Plugins.SharpMapGis.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
-        ///&lt;root&gt;
-        ///  &lt;!-- 
-        ///    Microsoft ResX Schema 
-        ///    
-        ///    Version 2.0
-        ///    
-        ///    The primary goals of this format is to allow a simple XML format 
-        ///    that is mostly human readable. The generation and parsing of the 
-        ///    various data types are done through the TypeConverter classes 
-        ///    associated with the data types.
-        ///    
-        ///    Example:
-        ///    
-        ///    ... ado.net/XML headers &amp; schema ...
-        ///    &lt;resheader name=&quot;resmimetype&quot;&gt;text/microsoft-resx&lt;/resheader&gt;
-        ///    &lt;resheader n [rest of string was truncated]&quot;;.
-        /// </summary>
-        public static string Resources_nl_NL {
-            get {
-                return ResourceManager.GetString("Resources_nl_NL", resourceCulture);
-            }
-        }
     }
 }
