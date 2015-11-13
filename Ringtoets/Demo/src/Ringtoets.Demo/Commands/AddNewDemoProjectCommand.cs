32b132d289373b9182dfd4abfb28a5c2e0ea5d8c
bouvrie <bouvrie@example.com>
2015-11-13 13:47:28 +0000

    * Fully implemented 'Add Demo project' button to ribbon in separate Demo tab. This will create a project that is configured to calculate piping without errors. Related to Issue [WTI-139]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@787 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: b24c8ac6403d1774c86ac0275f4f7c534471bb30

--- Ringtoets/Demo/src/Ringtoets.Demo/Commands/AddNewDemoProjectCommand.cs
+++ Ringtoets/Demo/src/Ringtoets.Demo/Commands/AddNewDemoProjectCommand.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 using Core.Common.Gui;
@@ -42,36 +42,69 @@ namespace Ringtoets.Demo.Commands
                 demoAssessmentSection.InitializePipingFailureMechanism();
             }
 
-            //var pipingFailureMechanism = demoAssessmentSection.PipingFailureMechanism;
+            var pipingFailureMechanism = demoAssessmentSection.PipingFailureMechanism;
 
-            //using (var tempPath = new TemporaryImportFile("DR6_surfacelines.csv"))
-            //{
+            using (var tempPath = new TemporaryImportFile("DR6_surfacelines.csv"))
+            {
 
-            //    var surfaceLinesImporter = new PipingSurfaceLinesCsvImporter();
-            //    surfaceLinesImporter.ImportItem(tempPath.FilePath, pipingFailureMechanism.SurfaceLines);
-            //}
+                var surfaceLinesImporter = new PipingSurfaceLinesCsvImporter();
+                surfaceLinesImporter.ImportItem(tempPath.FilePath, pipingFailureMechanism.SurfaceLines);
+            }
+
+            using (var tempPath = new TemporaryImportFile("complete.soil"))
+            {
+
+                var surfaceLinesImporter = new PipingSoilProfilesImporter();
+                surfaceLinesImporter.ImportItem(tempPath.FilePath, pipingFailureMechanism.SoilProfiles);
+            }
+
+            var calculation = pipingFailureMechanism.Calculations.First();
+            calculation.SurfaceLine = pipingFailureMechanism.SurfaceLines.First(sl => sl.Name == "PK001_0001");
+            calculation.SoilProfile = pipingFailureMechanism.SoilProfiles.First(sl => sl.Name == "AD640M00_Segment_36005_1D2");
         }
 
         public IGui Gui { get; set; }
 
+        /// <summary>
+        /// Class for creating a temporary file in the windows Temp directory based on a
+        /// file stored in Embedded Resources.
+        /// </summary>
         private class TemporaryImportFile : IDisposable
         {
             private readonly string tempTargetFolderPath;
             private readonly string fullFilePath;
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TemporaryImportFile"/> class.
+            /// </summary>
+            /// <param name="embeddedResourceFileName">Name of the file with build action 'Embedded Resource' within this project.</param>
             public TemporaryImportFile(string embeddedResourceFileName)
             {
                 tempTargetFolderPath = Path.Combine(Path.GetTempPath(), "demo_traject");
+                Directory.CreateDirectory(tempTargetFolderPath);
+
                 fullFilePath = Path.Combine(tempTargetFolderPath, embeddedResourceFileName);
+
+                var stream = GetStreamToFileInResource(embeddedResourceFileName);
+
+                var bytes = GetBinaryDataOfStream(stream);
+
+                File.WriteAllBytes(fullFilePath, bytes); 
+            }
+
+            private Stream GetStreamToFileInResource(string embeddedResourceFileName)
+            {
                 var assembly = Assembly.GetAssembly(GetType());
-                var stream = assembly.GetManifestResourceStream(GetType(), "Ringtoets.Demo.Resources."+embeddedResourceFileName);
+                string embeddedResourceName = assembly.GetManifestResourceNames().First(n => n.EndsWith(embeddedResourceFileName));
+                return assembly.GetManifestResourceStream(embeddedResourceName);
+            }
+
+            private static byte[] GetBinaryDataOfStream(Stream stream)
+            {
+                var bytes = new byte[stream.Length];
                 var reader = new BinaryReader(stream);
-                List<byte> bytes = new List<byte>();
-                while (reader.PeekChar() != -1)
-                {
-                    bytes.Add(reader.ReadByte());
-                }
-                File.WriteAllBytes(fullFilePath, bytes.ToArray()); 
+                reader.Read(bytes, 0, (int)stream.Length);
+                return bytes;
             }
 
             public string FilePath
--- Ringtoets/Demo/test/Ringtoets.Demo.Test/Commands/AddNewDemoProjectCommandTest.cs
+++ Ringtoets/Demo/test/Ringtoets.Demo.Test/Commands/AddNewDemoProjectCommandTest.cs
@@ -1,4 +1,6 @@
-﻿using Core.Common.Base;
+﻿using System.Linq;
+
+using Core.Common.Base;
 using Core.Common.Controls;
 using Core.Common.Gui;
 
@@ -7,7 +9,9 @@ using NUnit.Framework;
 using Rhino.Mocks;
 
 using Ringtoets.Demo.Commands;
+using Ringtoets.Piping.Calculation.Piping;
 using Ringtoets.Piping.Data;
+using Ringtoets.Piping.Service;
 
 namespace Ringtoets.Demo.Test.Commands
 {
@@ -57,11 +61,54 @@ namespace Ringtoets.Demo.Test.Commands
             var demoProject = (AssessmentSection) project.Items[0];
             Assert.AreEqual("Demo traject", demoProject.Name);
 
-            Assert.AreEqual(1, demoProject.PipingFailureMechanism.SoilProfiles);
-            Assert.AreEqual(1, demoProject.PipingFailureMechanism.SurfaceLines);
+            var profiles = demoProject.PipingFailureMechanism.SoilProfiles.ToArray();
+            Assert.AreEqual(26, profiles.Length);
+            var surfaceLines = demoProject.PipingFailureMechanism.SurfaceLines.ToArray();
+            Assert.AreEqual(4, surfaceLines.Length);
 
-            Assert.AreEqual(1, demoProject.PipingFailureMechanism.Calculations);
+            Assert.AreEqual(1, demoProject.PipingFailureMechanism.Calculations.Count);
+            var calculation = demoProject.PipingFailureMechanism.Calculations.First();
+            AssertCalculationAbleToCalculate(calculation);
             mocks.VerifyAll();
+        }
+
+        private void AssertCalculationAbleToCalculate(PipingData calculation)
+        {
+            Assert.AreEqual(1.0, calculation.UpliftModelFactor, 1e-3);
+            Assert.AreEqual(1.0, calculation.SellmeijerModelFactor, 1e-3);
+
+            Assert.AreEqual(10.0, calculation.WaterVolumetricWeight, 1e-3);
+            Assert.AreEqual(0.0, calculation.AssessmentLevel, 1e-3);
+            Assert.AreEqual(0.0, calculation.PiezometricHeadExit, 1e-3);
+            Assert.AreEqual(0.0, calculation.PiezometricHeadPolder, 1e-3);
+            Assert.AreEqual(0.3, calculation.SellmeijerReductionFactor, 1e-3);
+            Assert.AreEqual(16.5, calculation.SandParticlesVolumicWeight, 1e-3);
+            Assert.AreEqual(0.25, calculation.WhitesDragCoefficient, 1e-3);
+            Assert.AreEqual(1.33e-6, calculation.WaterKinematicViscosity, 1e-3);
+            Assert.AreEqual(9.81, calculation.Gravity, 1e-3);
+            Assert.AreEqual(0.000208, calculation.MeanDiameter70, 1e-3);
+            Assert.AreEqual(37, calculation.BeddingAngle, 1e-3);
+
+            Assert.AreEqual("PK001_0001", calculation.SurfaceLine.Name);
+            Assert.AreEqual("AD640M00_Segment_36005_1D2", calculation.SoilProfile.Name);
+
+            Assert.AreEqual(3.666, PipingSemiProbabilisticDesignValueFactory.GetDampingFactorExit(calculation).GetDesignValue(), 1e-3);
+            Assert.AreEqual(-1.645, PipingSemiProbabilisticDesignValueFactory.GetPhreaticLevelExit(calculation).GetDesignValue(), 1e-3);
+            Assert.AreEqual(0.011, PipingSemiProbabilisticDesignValueFactory.GetThicknessCoverageLayer(calculation).GetDesignValue(), 1e-3);
+            Assert.AreEqual(0.011, PipingSemiProbabilisticDesignValueFactory.GetSeepageLength(calculation).GetDesignValue(), 1e-3);
+            Assert.AreEqual(0.011, PipingSemiProbabilisticDesignValueFactory.GetDiameter70(calculation).GetDesignValue(), 1e-3);
+            Assert.AreEqual(2.345, PipingSemiProbabilisticDesignValueFactory.GetDarcyPermeability(calculation).GetDesignValue(), 1e-3);
+            Assert.AreEqual(2.345, PipingSemiProbabilisticDesignValueFactory.GetThicknessAquiferLayer(calculation).GetDesignValue(), 1e-3);
+
+            Assert.IsTrue(PipingCalculationService.Validate(calculation));
+            PipingCalculationService.Calculate(calculation);
+            Assert.IsTrue(calculation.HasOutput);
+            Assert.AreEqual(0.0021, calculation.Output.HeaveFactorOfSafety, 1e-3);
+            Assert.AreEqual(-143.3235, calculation.Output.HeaveZValue, 1e-3);
+            Assert.AreEqual(4.4072, calculation.Output.UpliftFactorOfSafety, 1e-3);
+            Assert.AreEqual(5.6044, calculation.Output.UpliftZValue, 1e-3);
+            Assert.AreEqual(0.0016, calculation.Output.SellmeijerFactorOfSafety, 1e-3);
+            Assert.AreEqual(-1.6387, calculation.Output.SellmeijerZValue, 1e-3);
         }
     }
 }
