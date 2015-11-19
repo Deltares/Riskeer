6413227c0e793756474d0e3fdb04541ff18b4efd
Elmar Kleijn <elmar.kleijn@deltares.nl>
2015-11-19 21:32:29 +0000

    Removed Entity from Url Related to Issue [WTI-81]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@883 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 6f7e118299d9ac67ca67b96a1134db80fe0114c0

--- Core/Common/src/Core.Common.Utils/Url.cs
+++ Core/Common/src/Core.Common.Utils/Url.cs
@@ -1,70 +1,18 @@
-using System;
-using System.Xml;
-using System.Xml.Schema;
-using System.Xml.Serialization;
-using Core.Common.Utils.Aop;
-
 namespace Core.Common.Utils
 {
     /// <summary>
-    /// Web link, url.
+    /// Web link (url).
     /// </summary>
-    [Entity(FireOnCollectionChange = false)]
-    public class Url : ICloneable, IXmlSerializable
+    public class Url
     {
-        private string path;
-
-        private string name;
-
         public Url(string name, string path)
         {
-            this.name = name;
-            this.path = path;
+            Name = name;
+            Path = path;
         }
 
-        public virtual string Path
-        {
-            get
-            {
-                return path;
-            }
-            set
-            {
-                path = value;
-            }
-        }
+        public string Path { get; set; }
 
-        public virtual string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                name = value;
-            }
-        }
-
-        public object Clone()
-        {
-            return new Url(name, path);
-        }
-
-        public XmlSchema GetSchema()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ReadXml(XmlReader reader)
-        {
-            name = reader.GetAttribute("name");
-            path = reader.GetAttribute("path");
-        }
-
-        public void WriteXml(XmlWriter writer)
-        {
-            throw new NotImplementedException();
-        }
+        public string Name { get; set; }
     }
 }
--- Core/Common/test/Core.Common.Base.Tests/Shell/Core/UrlTest.cs
+++ Core/Common/test/Core.Common.Base.Tests/Shell/Core/UrlTest.cs
@@ -9,19 +9,10 @@ namespace Core.Common.Base.Tests.Shell.Core
         [Test]
         public void Initialization()
         {
-            Url url = new Url("Deltares", "http://www.deltares.com");
+            var url = new Url("Deltares", "http://www.deltares.com");
 
             Assert.AreEqual("Deltares", url.Name);
             Assert.AreEqual("http://www.deltares.com", url.Path);
-        }
-
-        [Test]
-        public void Cloning()
-        {
-            Url url = new Url("Deltares", "http://www.deltares.com");
-            Url urlClone = (Url) url.Clone();
-            Assert.AreEqual(urlClone.Name, url.Name);
-            Assert.AreEqual(urlClone.Path, url.Path);
         }
     }
 }
