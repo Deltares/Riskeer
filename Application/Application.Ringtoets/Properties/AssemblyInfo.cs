5a2f411b10f2f7d2ab2f5056c9437a11b52a2543
Joël van den Berg <joel.vandenberg@deltares.nl>
2015-11-19 13:04:54 +0000

    Added Dutch translation for avalon dock. Added GlobalAssembly.cs.svn to the Core.Common.Version project. Added NeutralResourceLanguage to GlobalAssembly.cs.svn Related to Issue [WTI-6]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@880 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: a43f506b1e697dfa213bb576eff433582fcc08ca

--- Application/Application.Ringtoets/Properties/AssemblyInfo.cs
+++ Application/Application.Ringtoets/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
 using log4net.Config;
 
@@ -7,4 +6,3 @@ using log4net.Config;
 [assembly: AssemblyTitle("Application.Ringtoets")]
 [assembly: AssemblyProduct("Application.Ringtoets")]
 [assembly: Guid("a4036982-77b5-4a89-84be-0c05301f28a6")]
-[assembly: NeutralResourcesLanguage("en-US")]
