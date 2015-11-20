bc5c669eb6bf927e997d5b88b5c9eddd1ba87f00
Marten Janssen <marten.janssen@deltares.nl>
2015-11-20 09:02:32 +0000

    Moved log4net to NUGet Related to Issue [WTI-100]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@888 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: efc1ee9dc887a144abb0c543a7744ff8387ec427

--- Core/Common/src/Core.Common.Base/RingtoetsApplication.cs
+++ Core/Common/src/Core.Common.Base/RingtoetsApplication.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
