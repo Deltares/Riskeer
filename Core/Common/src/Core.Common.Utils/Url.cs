e6c483b5a4f0d45b431602d7645765a51d37a47d
Elmar Kleijn <elmar.kleijn@deltares.nl>
2015-11-23 06:44:28 +0000

    Final review related changes => Summaries Related to Issue [WTI-81]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@914 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 71ecf667738e5d6ff29322b9098f3da178dadec4

--- Core/Common/src/Core.Common.Utils/Url.cs
+++ Core/Common/src/Core.Common.Utils/Url.cs
@@ -5,14 +5,25 @@ namespace Core.Common.Utils
     /// </summary>
     public class Url
     {
+        /// <summary>
+        /// Creates a url.
+        /// </summary>
+        /// <param name="name">The name of the url.</param>
+        /// <param name="path">The path of the url.</param>
         public Url(string name, string path)
         {
             Name = name;
             Path = path;
         }
 
+        /// <summary>
+        /// Gets or sets the path of the url.
+        /// </summary>
         public string Path { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the url.
+        /// </summary>
         public string Name { get; set; }
     }
 }
