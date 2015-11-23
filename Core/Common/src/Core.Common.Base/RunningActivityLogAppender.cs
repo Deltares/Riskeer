6c344f172673b9ff70d72de31b6cde325499a555
Elmar Kleijn <elmar.kleijn@deltares.nl>
2015-11-23 15:26:11 +0000

    Review related change => Paired with hryb Related to Issue [WTI-67]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@923 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 0f81471d80c8f2360a7c971b5870652acf326f82

--- Core/Common/src/Core.Common.Base/RunningActivityLogAppender.cs
+++ Core/Common/src/Core.Common.Base/RunningActivityLogAppender.cs
@@ -73,25 +73,6 @@ namespace Core.Common.Base
             }
         }
 
-        /// <summary>
-        /// Subclasses of <see cref="T:log4net.Appender.AppenderSkeleton"/> should implement this method 
-        ///             to perform actual logging.
-        /// </summary>
-        /// <param name="loggingEvent">The event to append.</param>
-        /// <remarks>
-        /// <para>
-        /// A subclass must implement this method to perform
-        ///             logging of the <paramref name="loggingEvent"/>.
-        /// </para>
-        /// <para>
-        /// This method will be called by <see cref="M:DoAppend(LoggingEvent)"/>
-        ///             if all the conditions listed for that method are met.
-        /// </para>
-        /// <para>
-        /// To restrict the logging of events in the appender
-        ///             override the <see cref="M:PreAppendCheck()"/> method.
-        /// </para>
-        /// </remarks>
         protected override void Append(LoggingEvent loggingEvent)
         {
             foreach (var act in GetRunningActivitiesThreadSafe())
