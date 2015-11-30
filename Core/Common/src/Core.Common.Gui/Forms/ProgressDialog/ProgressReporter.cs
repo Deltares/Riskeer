f7b33553260dc7233194b45e1fd378cd806cfcf7
Elmar Kleijn <elmar.kleijn@deltares.nl>
2015-11-30 10:35:07 +0000

    Updated namespace of ProgressReporter Related to Issue [WTI-182]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1027 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: a7be669deb7610db3bb14e2328056fa3c1c3cd25

--- Core/Common/src/Core.Common.Gui/Forms/ProgressDialog/ProgressReporter.cs
+++ Core/Common/src/Core.Common.Gui/Forms/ProgressDialog/ProgressReporter.cs
@@ -2,46 +2,49 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-/// <summary>
-/// A class used by <see cref="Task"/> to report progress or completion updates back to the UI.
-/// </summary>
-public class ProgressReporter
+namespace Core.Common.Gui.Forms.ProgressDialog
 {
     /// <summary>
-    /// The underlying scheduler for the UI's synchronization context.
+    /// A class used by <see cref="Task"/> to report progress or completion updates back to the UI.
     /// </summary>
-    private readonly TaskScheduler scheduler;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ProgressReporter"/> class.
-    /// This should be run on a UI thread.
-    /// </summary>
-    public ProgressReporter()
+    public class ProgressReporter
     {
-        scheduler = TaskScheduler.FromCurrentSynchronizationContext();
-    }
+        /// <summary>
+        /// The underlying scheduler for the UI's synchronization context.
+        /// </summary>
+        private readonly TaskScheduler scheduler;
 
-    /// <summary>
-    /// Reports the progress to the UI thread, and waits for the UI thread to process
-    /// the update before returning. This method should be called from the task.
-    /// </summary>
-    /// <param name="action">The action to perform in the context of the UI thread.</param>
-    public void ReportProgress(Action action)
-    {
-        var task = Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.None, scheduler);
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgressReporter"/> class.
+        /// This should be run on a UI thread.
+        /// </summary>
+        public ProgressReporter()
+        {
+            scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        }
 
-        task.Wait();
-    }
+        /// <summary>
+        /// Reports the progress to the UI thread, and waits for the UI thread to process
+        /// the update before returning. This method should be called from the task.
+        /// </summary>
+        /// <param name="action">The action to perform in the context of the UI thread.</param>
+        public void ReportProgress(Action action)
+        {
+            var task = Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.None, scheduler);
 
-    /// <summary>
-    /// Registers a UI thread handler for when the specified task finishes execution,
-    /// whether it finishes with success, failure, or cancellation.
-    /// </summary>
-    /// <param name="task">The task to monitor for completion.</param>
-    /// <param name="action">The action to take when the task has completed, in the context of the UI thread.</param>
-    /// <returns>The continuation created to handle completion. This is normally ignored.</returns>
-    public void RegisterContinuation(Task task, Action action)
-    {
-        task.ContinueWith(_ => action(), CancellationToken.None, TaskContinuationOptions.None, scheduler);
+            task.Wait();
+        }
+
+        /// <summary>
+        /// Registers a UI thread handler for when the specified task finishes execution,
+        /// whether it finishes with success, failure, or cancellation.
+        /// </summary>
+        /// <param name="task">The task to monitor for completion.</param>
+        /// <param name="action">The action to take when the task has completed, in the context of the UI thread.</param>
+        /// <returns>The continuation created to handle completion. This is normally ignored.</returns>
+        public void RegisterContinuation(Task task, Action action)
+        {
+            task.ContinueWith(_ => action(), CancellationToken.None, TaskContinuationOptions.None, scheduler);
+        }
     }
 }
