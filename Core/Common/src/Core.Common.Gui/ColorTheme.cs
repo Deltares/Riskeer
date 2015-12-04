895c40e163ee005220d561851df27234e387b2f2
Joël van den Berg <joel.vandenberg@deltares.nl>
2015-12-04 08:02:27 +0000

    Additional doc. Related to Issue [WTI-6]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1149 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: d16847237afff82b55e1e524703dd0e7764c46da

--- Core/Common/src/Core.Common.Gui/ColorTheme.cs
+++ Core/Common/src/Core.Common.Gui/ColorTheme.cs
@@ -3,6 +3,9 @@ using Core.Common.Gui.Properties;
 
 namespace Core.Common.Gui
 {
+    /// <summary>
+    /// Enumeration for the all the possible color themes in the application.
+    /// </summary>
     public enum ColorTheme
     {
         Dark,
@@ -13,8 +16,17 @@ namespace Core.Common.Gui
         Generic
     }
 
+    /// <summary>
+    /// Extension methods for the <see cref="ColorTheme"/> class
+    /// </summary>
     public static class ColorThemeExtensions
     {
+        /// <summary>
+        /// Gets the localized string from the <see cref="Resources"/> based on the 
+        /// name of the <see cref="ColorTheme"/>.
+        /// </summary>
+        /// <param name="theme">The <see cref="ColorTheme"/> for which to get the localized name.</param>
+        /// <returns>The localized name from the <see cref="Resources"/>.</returns>
         public static string Localized(this ColorTheme theme)
         {
             return Resources.ResourceManager.GetString(Enum.GetName(typeof(ColorTheme), theme));
--- Core/Common/src/Core.Common.Gui/Forms/OptionsDialog/ColorThemeItem.cs
+++ Core/Common/src/Core.Common.Gui/Forms/OptionsDialog/ColorThemeItem.cs
@@ -5,7 +5,14 @@ namespace Core.Common.Gui.Forms.OptionsDialog
     /// </summary>
     public class ColorThemeItem
     {
+        /// <summary>
+        /// Gets or sets the <see cref="ColorTheme"/> for this item.
+        /// </summary>
         public ColorTheme Theme { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name to display for the <see cref="ColorTheme"/>.
+        /// </summary>
         public string DisplayName { get; set; }
     }
 }
