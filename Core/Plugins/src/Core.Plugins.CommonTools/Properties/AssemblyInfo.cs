257d05c151e5c8e1f661870752d8a3783633924b
Joël van den Berg <joel.vandenberg@deltares.nl>
2015-12-15 15:39:39 +0000

    Removed empty Core.Plugins.Commontools.Project. Related to Issue [WTI-188]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1327 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 0562bc19f476e029635a97f6eb9528d4fe1b3384

--- Core/Plugins/src/Core.Plugins.CommonTools/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Core.Plugins.CommonTools")]
-[assembly: AssemblyProduct("Core.Plugins.CommonTools")]
-[assembly: Guid("79b65ba0-d711-4212-b21b-c2b10e27adfc")]
