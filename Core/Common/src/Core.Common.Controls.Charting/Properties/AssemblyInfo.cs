78b769cdeacaa9a56939825e527e7fe71c3a0281
Marten Janssen <marten.janssen@deltares.nl>
2015-12-16 14:05:01 +0000

    In preparation of cleaning Core.Common.Controls.swf, six new projects were created Related to Issue [WTI-184]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1368 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 99d8e88ca7c7315803248fd47abe56234801e44a

---
+++ Core/Common/src/Core.Common.Controls.Charting/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Core.Common.Controls.Charting")]
+[assembly: AssemblyProduct("Core.Common.Controls.Charting")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60b03219-83ac-4f93-9dd8-ffca94bf410b")]
---
+++ Core/Common/src/Core.Common.Controls.Table/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Core.Common.Controls.Table")]
+[assembly: AssemblyProduct("Core.Common.Controls.Table")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d642299f-9404-40f2-9fb4-6c9ead2a5906")]
