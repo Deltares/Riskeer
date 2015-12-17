c6607041d1570d4b500820eb4a967ee94b88ec86
Marten Janssen <marten.janssen@deltares.nl>
2015-12-17 13:31:49 +0000

    Removed link to global assembly (paired with Elmar) Related to Issue [WTI-184]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1408 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 313b3bd7ce4eb646cf69e46d084b3200beaf1787

--- Core/GIS/src/Core.GIS.GeoAPI.Extensions/Properties/AssemblyInfo.cs
+++ Core/GIS/src/Core.GIS.GeoAPI.Extensions/Properties/AssemblyInfo.cs
@@ -3,4 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GeoAPI.Enumerable")]
 [assembly: AssemblyProduct("GeoAPI.Enumerable")]
+[assembly: ComVisible(false)]
 [assembly: Guid("c2e47fc1-be92-4023-9c5a-9478c6357b5c")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
