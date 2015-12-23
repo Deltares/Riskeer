9ce8db4927361bcdb1dcab5bde9ca2f5b5f57913
Marten Janssen <marten.janssen@deltares.nl>
2015-12-23 09:24:21 +0000

    Removed UML folders and remaining UML projects Related to Issue [WTI-278]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1459 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 89120b91080d5ee26b5fa0679914af41f2a606a8

--- Core/Common/uml/Core.Common.Controls.Swf.UML/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Core.Common.Controls.Swf.UML")]
-[assembly: AssemblyProduct("Core.Common.Controls.Swf.UML")]
-[assembly: Guid("5882e286-9327-4574-80ff-14f3ae90be26")]
