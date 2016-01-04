3d4b98ca16e7a608e4db4ba639c028ce81bd440f
Joël van den Berg <joel.vandenberg@deltares.nl>
2016-01-04 09:05:17 +0000

    Initial setup of the oxyplot projects. Related to Issue [WTI-266]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1464 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 782c85638aeef5bd7f109219357499ec0c45ec1a

---
+++ Core/Components/src/Core.Components.OxyPlot/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Core.Components.OxyPlot")]
+[assembly: AssemblyProduct("Core.Components.OxyPlot")]
+[assembly: Guid("05a1a16f-6f4f-4ebb-9540-b09a94bfded0")]
---
+++ Core/Plugins/src/Core.Plugins.OxyPlot/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Core.Plugins.OxyPlot")]
+[assembly: AssemblyProduct("Core.Plugins.OxyPlot")]
+[assembly: Guid("008e702b-a124-422e-9fb5-cd65fc85e49a")]
