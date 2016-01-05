307d9f9ef20f6d38919cec957280212806a8e123
Joël van den Berg <joel.vandenberg@deltares.nl>
2016-01-05 15:24:57 +0000

    Fixed issues with configuration. Related to Issue [WTI-266]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1478 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: a317121698fec1c564e4223b5b10ae56d17f69cd

--- Core/Components/test/Core.Components.OxyPlot.Test/Properties/AssemblyInfo.cs
+++ Core/Components/test/Core.Components.OxyPlot.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Core.Components.OxyPlot")]
+[assembly: AssemblyTitle("Core.Components.OxyPlot.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Core.Components.OxyPlot")]
+[assembly: AssemblyProduct("Core.Components.OxyPlot.Test")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
