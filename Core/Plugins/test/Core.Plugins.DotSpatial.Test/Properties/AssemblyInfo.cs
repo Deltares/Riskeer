10b2b57c73ee15752a3556c52c364deb289fe4be
Robert Tangerman <robert.tangerman@deltares.nl>
2016-01-13 14:25:31 +0000

    Added DotSpatial projects in - Core.Components - Core.Components.Test - Core.Plugins - Core.Plugins.Test Related to Issue [WTI-240]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1557 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 2f2f83b1119e03e4f306e15a6852107d311fe1f7

---
+++ Core/Plugins/test/Core.Plugins.DotSpatial.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Core.Common.NewProjectExample")]
+[assembly: AssemblyProduct("Core.Common.NewProjectExample")]
+[assembly: Guid("844dca4c-6779-4474-8f12-efbee9907c2c")]
