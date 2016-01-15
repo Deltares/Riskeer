fc13ee43a29e33da4fdbc6032145c4f519f97993
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-01-15 09:17:43 +0000

    Extended TreeNodeInfo Related to Issue [WTI-184]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1590 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 699e5d35621323f7f3c2be6a97d329e1d2189587

--- Core/Common/src/Core.Common.Controls.TreeView/TreeNodeInfo.cs
+++ Core/Common/src/Core.Common.Controls.TreeView/TreeNodeInfo.cs
@@ -32,6 +32,32 @@ namespace Core.Common.Controls.TreeView
         /// The <c>object</c> parameter holds the wrapped data of the tree node.
         /// </summary>
         public Func<object, Image> Image { get; set; }
+
+        /// <summary>
+        /// Gets or sets a function for obtaining child node objects.
+        /// The <c>object</c> parameter holds the wrapped data of the tree node.
+        /// </summary>
+        public Func<object, object[]> ChildNodeObjects { get; set; }
+
+        /// <summary>
+        /// Gets or sets a function for checking whether or not the tree node can be renamed.
+        /// The <c>object</c> parameter holds the wrapped data of the tree node.
+        /// </summary>
+        public Func<object, bool> CanRename { get; set; }
+
+        /// <summary>
+        /// Gets or sets an action for obtaining the logic to perform after renaming the tree node.
+        /// The <c>object</c> parameter holds the wrapped data of the tree node.
+        /// The <c>string</c> parameter holds the new name of the tree node.
+        /// </summary>
+        public Action<object, string> OnNodeRenamed { get; set; }
+
+        /// <summary>
+        /// Gets or sets an action for obtaining the logic to perform after checking or unchecking the tree node.
+        /// The <c>object</c> parameter holds the wrapped data of the tree node.
+        /// The <c>bool</c> parameter holds the new checked state of the tree node.
+        /// </summary>
+        public Action<object, bool> OnNodeChecked { get; set; }
     }
 
     /// <summary>
@@ -72,6 +98,32 @@ namespace Core.Common.Controls.TreeView
         public Func<TData, Image> Image { get; set; }
 
         /// <summary>
+        /// Gets or sets a function for obtaining child node objects.
+        /// The <typeparamref name="TData"/> parameter holds the wrapped data of the tree node.
+        /// </summary>
+        public Func<TData, object[]> ChildNodeObjects { get; set; }
+
+        /// <summary>
+        /// Gets or sets a function for checking whether or not the tree node can be renamed.
+        /// The <typeparamref name="TData"/> parameter holds the wrapped data of the tree node.
+        /// </summary>
+        public Func<TData, bool> CanRename { get; set; }
+
+        /// <summary>
+        /// Gets or sets an action for obtaining the logic to perform after renaming the tree node.
+        /// The <typeparamref name="TData"/> parameter holds the wrapped data of the tree node.
+        /// The <c>string</c> parameter holds the new name of the tree node.
+        /// </summary>
+        public Action<TData, string> OnNodeRenamed { get; set; }
+
+        /// <summary>
+        /// Gets or sets an action for obtaining the logic to perform after checking or unchecking the tree node.
+        /// The <typeparamref name="TData"/> parameter holds the wrapped data of the tree node.
+        /// The <c>bool</c> parameter holds the new checked state of the tree node.
+        /// </summary>
+        public Action<TData, bool> OnNodeChecked { get; set; }
+
+        /// <summary>
         /// This operator converts a <see cref="TreeNodeInfo{TData}"/> into a <see cref="TreeNodeInfo"/>.
         /// </summary>
         /// <param name="treeNodeInfo">The <see cref="TreeNodeInfo{TData}"/> to convert.</param>
@@ -82,7 +134,11 @@ namespace Core.Common.Controls.TreeView
             {
                 TagType = treeNodeInfo.TagType,
                 Text = tag => treeNodeInfo.Text((TData) tag),
-                Image = tag => treeNodeInfo.Image((TData) tag)
+                Image = tag => treeNodeInfo.Image((TData) tag),
+                ChildNodeObjects = tag => treeNodeInfo.ChildNodeObjects((TData) tag),
+                CanRename = tag => treeNodeInfo.CanRename((TData) tag),
+                OnNodeRenamed = (tag, name) => treeNodeInfo.OnNodeRenamed((TData) tag, name),
+                OnNodeChecked = (tag, checkedState) => treeNodeInfo.OnNodeChecked((TData) tag, checkedState)
             };
         }
     }
