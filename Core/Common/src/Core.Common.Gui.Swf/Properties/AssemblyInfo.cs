232a51f976c9b38d71ed9eb959bcf4004cbb4ded
bouvrie <bouvrie@example.com>
2016-01-19 10:49:05 +0000

    * Deleted Core.Common.Gui.Swf as nothing is being used any more. Related to Issue [WTI-186]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1629 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: b900400a1721316b6bfb58f3ca0d2f953cdf6907

--- Core/Common/src/Core.Common.Gui.Swf/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Core.Common.Gui.Swf")]
-[assembly: AssemblyProduct("Core.Common.Gui.Swf")]
-[assembly: Guid("e979f2a9-47d5-43c1-b003-829b4e2a801e")]
