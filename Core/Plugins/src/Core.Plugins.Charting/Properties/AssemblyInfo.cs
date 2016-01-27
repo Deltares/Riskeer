ae08f30060762e2364ec493d298fbb9550b7ca3f
Joël van den Berg <joel.vandenberg@deltares.nl>
2016-01-27 15:08:47 +0000

    Removed Core.Plugins.Charting and Core.Plugins.Charting.Test. Related to Issue [WTI-277]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1808 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 70d2562a75db6b2dff4bebd5da956fedaa35588f

--- Core/Plugins/src/Core.Plugins.Charting/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Core.Plugins.Charting")]
-[assembly: AssemblyProduct("Core.Plugins.Charting")]
-[assembly: Guid("96c45564-73c2-4a29-953c-2c407ee57a43")]
