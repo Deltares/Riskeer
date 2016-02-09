4512af7782ee31b36941bb280b54d9da2953dd71
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-02-09 10:50:45 +0000

    Removed redundant white spaces from source header file
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2000 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 5385e547d35c5b2b44dd4f890b5e92439e73ba0e

--- Ringtoets/Piping/src/Ringtoets.Piping.Calculation/PipingCalculatorResult.cs
+++ Ringtoets/Piping/src/Ringtoets.Piping.Calculation/PipingCalculatorResult.cs
@@ -9,14 +9,14 @@
 // 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 //
-// All names, logos, and references to "Deltares" are registered trademarks of 
-// Stichting Deltares and remain full property of Stichting Deltares at all times. 
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
 namespace Ringtoets.Piping.Calculation
