4d67bb58184d12acb4ea8aedbc1da448ed54bcae
Marten Janssen <marten.janssen@deltares.nl>
2016-02-09 10:21:19 +0000

    # Renamed application from *Application.Ringtoets* to *Ringtoets*. This is visible in the registry and "Open with" dialog # Set icon for Ringtoets.exe. This icon is visible when browsing in the installed bin folder and "Open with" dialog Related to Issue [WTI-316]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@1999 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 84011efe522a1fb70322915296123c3e86cca2f7

--- Application/Ringtoets/src/Application.Ringtoets/Properties/AssemblyInfo.cs
+++ Application/Ringtoets/src/Application.Ringtoets/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 using log4net.Config;
 
 [assembly: XmlConfigurator(Watch = true)]
-[assembly: AssemblyTitle("Application.Ringtoets")]
-[assembly: AssemblyProduct("Application.Ringtoets")]
+[assembly: AssemblyTitle("Ringtoets")]
+[assembly: AssemblyProduct("Ringtoets")]
 [assembly: Guid("a4036982-77b5-4a89-84be-0c05301f28a6")]
