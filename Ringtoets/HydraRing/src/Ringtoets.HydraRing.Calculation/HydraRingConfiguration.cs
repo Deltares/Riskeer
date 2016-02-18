50ec6030af058eee675d7d9527ddf1e5ed57f3da
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-02-18 13:12:03 +0000

    Introduced HydraRingConfiguration Related to Issue [WTI-324]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2124 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: c8551ab3aa064452e9e8d18494dfa6d2256ea71c
    Former-commit-id: a6ecdab6325442834f33cfb7febd958078b72c8d

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.Calculation/HydraRingConfiguration.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace Ringtoets.HydraRing.Calculation
+{
+    /// <summary>
+    /// Container for all configurations that are necessary for performing a Hydra-Ring calculation.
+    /// </summary>
+    public class HydraRingConfiguration
+    {
+        private readonly Dictionary<string, List<OrderedDictionary>> configurationDictionary = new Dictionary<string, List<OrderedDictionary>>();
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="HydraRingConfiguration"/> class.
+        /// </summary>
+        public HydraRingConfiguration()
+        {
+            InitializeHydraulicModels();
+        }
+
+        /// <summary>
+        /// Generates a database creation script that can be used to perform a Hydra-Ring calculation.
+        /// </summary>
+        /// <returns>The database creation script.</returns>
+        public string GenerateDataBaseCreationScript()
+        {
+            var lines = new List<string>();
+
+            foreach (var tableName in configurationDictionary.Keys)
+            {
+                lines.Add("DELETE FROM [" + tableName + "];");
+
+                if (configurationDictionary[tableName].Count <= 0)
+                {
+                    continue;
+                }
+
+                foreach (var orderedDictionary in configurationDictionary[tableName])
+                {
+                    var valueStrings = new List<string>();
+
+                    foreach (var val in orderedDictionary.Values)
+                    {
+                        if (val == null)
+                        {
+                            valueStrings.Add("NULL");
+                            continue;
+                        }
+
+                        if (val is string)
+                        {
+                            valueStrings.Add("'" + val + "'");
+                            continue;
+                        }
+
+                        if (val is double)
+                        {
+                            valueStrings.Add(((double) val).ToString(CultureInfo.InvariantCulture));
+                            continue;
+                        }
+
+                        valueStrings.Add(val.ToString());
+                    }
+
+                    var valuesString = string.Join(", ", valueStrings);
+
+                    lines.Add("INSERT INTO [" + tableName + "] VALUES (" + valuesString + ");");
+                }
+
+                lines.Add("");
+            }
+
+            return string.Join(Environment.NewLine, lines);
+        }
+
+        private void InitializeHydraulicModels()
+        {
+            configurationDictionary["HydraulicModels"] = new List<OrderedDictionary>
+            {
+                new OrderedDictionary
+                {
+                    {
+                        "TimeIntegrationSchemeID", null // Model property: HydraulicModelsTimeIntegrationSchemeId
+                    },
+                    {
+                        "UncertaintiesID", null // Model property: HydraulicModelsUncertaintiesId
+                    },
+                    {
+                        "DataSetName", "WTI 2017"
+                    }
+                }
+            };
+        }
+    }
+}
