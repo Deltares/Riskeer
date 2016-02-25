02e4e3830d745db66a35986a2ffdfb9e484ad48a
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-02-25 13:42:54 +0000

    Introduced HydraRingVariable Related to Issue [WTI-324]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2276 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 758844acfbc37d20e600ce32c5a82a686c376bc5
    Former-commit-id: a18477107e2c32e6131ffae9ed0edba2e01225f4

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.Calculation/Data/HydraRingVariable.cs
@@ -0,0 +1,167 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Ringtoets.HydraRing.Calculation.Types;
+
+namespace Ringtoets.HydraRing.Calculation.Data
+{
+    /// <summary>
+    /// Container for Hydra-Ring variable related data.
+    /// </summary>
+    internal abstract class HydraRingVariable
+    {
+        private readonly int variableId;
+        private readonly HydraRingDistributionType distributionType;
+        private readonly double value;
+        private readonly HydraRingDeviationType deviationType;
+        private readonly double mean;
+        private readonly double variability;
+        private readonly double shift;
+        private readonly double correlationLength;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="HydraRingVariable"/> class.
+        /// </summary>
+        /// <param name="variableId">The Hydra-Ring id corresponding to the variable that is considered.</param>
+        /// <param name="distributionType">The probability distribution of the variable.</param>
+        /// <param name="value">The value in case the variable is deterministic.</param>
+        /// <param name="deviationType">The deviation type in case the variable is random.</param>
+        /// <param name="mean">The mean value in case the variable is random.</param>
+        /// <param name="variability">The variability in case the variable is random.</param>
+        /// <param name="shift">The shift in case the variable is random.</param>
+        /// <param name="correlationLength">The correlation length.</param>
+        protected HydraRingVariable(int variableId, HydraRingDistributionType distributionType, double value, HydraRingDeviationType deviationType, double mean, double variability, double shift, double correlationLength)
+        {
+            this.variableId = variableId;
+            this.distributionType = distributionType;
+            this.value = value;
+            this.deviationType = deviationType;
+            this.mean = mean;
+            this.variability = variability;
+            this.shift = shift;
+            this.correlationLength = correlationLength;
+        }
+
+        /// <summary>
+        /// Gets the Hydra-Ring id corresponding to the variable that is considered.
+        /// </summary>
+        public int VariableId
+        {
+            get
+            {
+                return variableId;
+            }
+        }
+
+        /// <summary>
+        /// Gets the probability distribution of the variable.
+        /// </summary>
+        public HydraRingDistributionType DistributionType
+        {
+            get
+            {
+                return distributionType;
+            }
+        }
+
+        /// <summary>
+        /// Gets the value in case the variable is deterministic.
+        /// </summary>
+        /// <remarks>
+        /// This property is only relevant when <see cref="DistributionType"/> equals <see cref="HydraRingDistributionType.Deterministic"/>.
+        /// </remarks>
+        public double Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the deviation type in case the variable is random.
+        /// </summary>
+        /// <remarks>
+        /// This property is only relevant when <see cref="DistributionType"/> is not equal to <see cref="HydraRingDistributionType.Deterministic"/>.
+        /// </remarks>
+        public HydraRingDeviationType DeviationType
+        {
+            get
+            {
+                return deviationType;
+            }
+        }
+
+        /// <summary>
+        /// Gets the mean value in case the variable is random.
+        /// </summary>
+        /// <remarks>
+        /// This property is only relevant when <see cref="DistributionType"/> is not equal to <see cref="HydraRingDistributionType.Deterministic"/>.
+        /// </remarks>
+        public double Mean
+        {
+            get
+            {
+                return mean;
+            }
+        }
+
+        /// <summary>
+        /// Gets the variablity in case the variable is random.
+        /// </summary>
+        /// <remarks>
+        /// The value represents a standard deviation in case the <see cref="DeviationType"/> equals <see cref="HydraRingDeviationType.Standard"/>.
+        /// The value represents a variation coefficient in case the <see cref="DeviationType"/> equals <see cref="HydraRingDeviationType.Variation"/>.
+        /// </remarks>
+        public double Variability
+        {
+            get
+            {
+                return variability;
+            }
+        }
+
+        /// <summary>
+        /// Gets the shift in case the variable is random.
+        /// </summary>
+        /// <remarks>
+        /// This property is only relevant when <see cref="DistributionType"/> equals <see cref="HydraRingDistributionType.LogNormal"/>.
+        /// </remarks>
+        public double Shift
+        {
+            get
+            {
+                return shift;
+            }
+        }
+
+        /// <summary>
+        /// Gets the correlation length.
+        /// </summary>
+        public double CorrelationLength
+        {
+            get
+            {
+                return correlationLength;
+            }
+        }
+    }
+}
---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.Calculation/Types/HydraRingDeviationType.cs
@@ -0,0 +1,14 @@
+﻿namespace Ringtoets.HydraRing.Calculation.Types
+{
+    /// <summary>
+    /// Enumeration that defines the deviation types supported by Hydra-Ring.
+    /// </summary>
+    /// <remarks>
+    /// The integer values correspond to deviation ids defined within Hydra-Ring.
+    /// </remarks>
+    public enum HydraRingDeviationType
+    {
+        Standard = 0,
+        Variation = 1
+    }
+}
