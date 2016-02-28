8693789d18f77ae54b04670ed135a10ea4ad1a8d
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-02-28 19:39:16 +0000

    Added integration test for HydraRingConfiguration with AssessmentLevelCalculationData Related to Issue [WTI-324]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2318 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 9e510cddc4de685f52c10c820c9f97f7bcfa1c45
    Former-commit-id: 28de1d062d5ea7f826d55188c4ead66f564acda5

---
+++ Ringtoets/HydraRing/test/Ringtoets.HydraRing.Calculation.Test/HydraRingConfigurationIntegrationTest.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+using Ringtoets.HydraRing.Calculation.Common;
+using Ringtoets.HydraRing.Calculation.Data;
+using Ringtoets.HydraRing.Calculation.Types;
+
+namespace Ringtoets.HydraRing.Calculation.Test
+{
+    [TestFixture]
+    public class HydraRingConfigurationIntegrationTest
+    {
+        [Test]
+        public void GenerateDataBaseCreationScript_HydraRingConfigurationWithAssessmentLevelCalculation_ReturnsExpectedCreationScript()
+        {
+            var hydraRingConfiguration = new HydraRingConfiguration(HydraRingTimeIntegrationSchemeType.FBC, HydraRingUncertaintiesType.All);
+
+            hydraRingConfiguration.AddHydraRingCalculation(new AssessmentLevelCalculationData(700004, 3.29053));
+
+            var expectedCreationScript = "DELETE FROM [HydraulicModels];" + Environment.NewLine +
+                                         "INSERT INTO [HydraulicModels] VALUES (1, 1, 'WTI 2017');" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [Sections];" + Environment.NewLine +
+                                         "INSERT INTO [Sections] VALUES (999, 1, 1, 'HydraRingLocation', 'HydraRingLocation', NULL, NULL, NULL, NULL, 700004, 700004, 100, NULL, NULL);" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [DesignTables];" + Environment.NewLine +
+                                         "INSERT INTO [DesignTables] VALUES (999, 1, NULL, NULL, 2, 26, NULL, NULL, NULL, NULL, 0, 50, 3.29053);" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [Numerics];" + Environment.NewLine +
+                                         "INSERT INTO [Numerics] VALUES (999, 1, NULL, NULL, 1, 1, 4, 50, 0.15, 0.01, 0.01, 0.01, 2, 1, 10000, 20000, 0.1, -6, 6, 25);" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [VariableDatas];" + Environment.NewLine +
+                                         "INSERT INTO [VariableDatas] VALUES (999, 1, NULL, NULL, 26, 0, 0, NULL, NULL, NULL, NULL, 0, NULL, 300);" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [CalculationProfiles];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [SectionFaultTreeModels];" + Environment.NewLine +
+                                         "INSERT INTO [SectionFaultTreeModels] VALUES (999, 1, NULL, NULL, 1);" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [SectionSubMechanismModels];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [Fetches];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [AreaPoints];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [PresentationSections];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [Profiles];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [ForelandModels];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [Forelands];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [ProbabilityAlternatives];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [SetUpHeights];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [CalcWindDirections];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [Swells];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [WaveReductions];" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [Areas];" + Environment.NewLine +
+                                         "INSERT INTO [Areas] VALUES (1, '1', 'Nederland');" + Environment.NewLine +
+                                         Environment.NewLine +
+                                         "DELETE FROM [Projects];" + Environment.NewLine +
+                                         "INSERT INTO [Projects] VALUES (1, 'WTI 2017', 'Ringtoets calculation');" + Environment.NewLine;
+
+            var creationScript = hydraRingConfiguration.GenerateDataBaseCreationScript();
+
+            Assert.AreEqual(expectedCreationScript, creationScript);
+        }
+    }
+}
