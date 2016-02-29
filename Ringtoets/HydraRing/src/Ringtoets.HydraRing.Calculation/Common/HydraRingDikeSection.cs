a86ee48c248b3095d7db43023c64780534dd2e59
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-02-29 07:29:25 +0000

    Added HydraRingDikeSection Related to Issue [WTI-324]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2320 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 757d8f3c9df51a09d790edf99e13ec01103cf5da
    Former-commit-id: ce08db2e54795431fca8ce5416819991f35e58c1

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.Calculation/Common/HydraRingDikeSection.cs
@@ -0,0 +1,161 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Ringtoets.HydraRing.Calculation.Common
+{
+    /// <summary>
+    /// Container for Hydra-Ring dike (cross) section data.
+    /// </summary>
+    public class HydraRingDikeSection
+    {
+        private readonly int sectionId;
+        private readonly string sectionName;
+        private readonly double sectionBeginCoordinate;
+        private readonly double sectionEndCoordinate;
+        private readonly double sectionLength;
+        private readonly double crossSectionXCoordinate;
+        private readonly double crossSectionYCoordinate;
+        private readonly double crossSectionNormal;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="HydraRingDikeSection"/> class.
+        /// </summary>
+        /// <param name="sectionId">The id of the dike section.</param>
+        /// <param name="sectionName">The name of the dike section.</param>
+        /// <param name="sectionBeginCoordinate">The begin coordinate of the dike section.</param>
+        /// <param name="sectionEndCoordinate">The end coordinate of the dike section.</param>
+        /// <param name="sectionLength">The length of the dike section.</param>
+        /// <param name="crossSectionXCoordinate">The x coordinate of the dike cross section.</param>
+        /// <param name="crossSectionYCoordinate">the y coordinate of the dike cross section.</param>
+        /// <param name="crossSectionNormal">The normal of the dike cross section.</param>
+        public HydraRingDikeSection(int sectionId, string sectionName, double sectionBeginCoordinate, double sectionEndCoordinate, double sectionLength, double crossSectionXCoordinate, double crossSectionYCoordinate, double crossSectionNormal)
+        {
+            this.sectionId = sectionId;
+            this.sectionName = sectionName;
+            this.sectionBeginCoordinate = sectionBeginCoordinate;
+            this.sectionEndCoordinate = sectionEndCoordinate;
+            this.sectionLength = sectionLength;
+            this.crossSectionXCoordinate = crossSectionXCoordinate;
+            this.crossSectionYCoordinate = crossSectionYCoordinate;
+            this.crossSectionNormal = crossSectionNormal;
+        }
+
+        /// <summary>
+        /// Gets the id of the dike section.
+        /// </summary>
+        public int SectionId
+        {
+            get
+            {
+                return sectionId;
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the dike section.
+        /// </summary>
+        /// <remarks>
+        /// This name is used in the input file and the output file of Hydra-Ring calculations.
+        /// </remarks>
+        public string SectionName
+        {
+            get
+            {
+                return sectionName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the begin coordinate of the dike section.
+        /// </summary>
+        /// <remarks>
+        /// If only a dike cross section is considered, this property should be set equal to <see cref="CrossSectionXCoordinate"/>.
+        /// </remarks>
+        public double SectionBeginCoordinate
+        {
+            get
+            {
+                return sectionBeginCoordinate;
+            }
+        }
+
+        /// <summary>
+        /// Gets the end coordinate of the dike section.
+        /// </summary>
+        /// <remarks>
+        /// If only a dike cross section is considered, this property should be set equal to <see cref="CrossSectionYCoordinate"/>.
+        /// </remarks>
+        public double SectionEndCoordinate
+        {
+            get
+            {
+                return sectionEndCoordinate;
+            }
+        }
+
+        /// <summary>
+        /// Gets the length of the dike section.
+        /// </summary>
+        public double SectionLength
+        {
+            get
+            {
+                return sectionLength;
+            }
+        }
+
+        /// <summary>
+        /// Gets the x coordinate of the dike cross section.
+        /// </summary>
+        public double CrossSectionXCoordinate
+        {
+            get
+            {
+                return crossSectionXCoordinate;
+            }
+        }
+
+        /// <summary>
+        /// Gets the y coordinate of the dike cross section.
+        /// </summary>
+        public double CrossSectionYCoordinate
+        {
+            get
+            {
+                return crossSectionYCoordinate;
+            }
+        }
+
+        /// <summary>
+        /// Gets the normal of the dike cross section.
+        /// </summary>
+        /// <remarks>
+        /// The normal corresponds to the angle of the straight line perpendicular to the dike stretch at the location of the dike cross section (with respect to the north).
+        /// </remarks>
+        public double CrossSectionNormal
+        {
+            get
+            {
+                return crossSectionNormal;
+            }
+        }
+    }
+}
---
+++ Ringtoets/HydraRing/test/Ringtoets.HydraRing.Calculation.Test/Common/HydraRingDikeSectionTest.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+using Ringtoets.HydraRing.Calculation.Common;
+
+namespace Ringtoets.HydraRing.Calculation.Test.Common
+{
+    [TestFixture]
+    public class HydraRingDikeSectionTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var hydraRingDikeSection = new HydraRingDikeSection(1, "Name", 2.2, 3.3, 4.4, 5.5, 6.6, 7.7); 
+
+            // Assert
+            Assert.AreEqual(1, hydraRingDikeSection.SectionId);
+            Assert.AreEqual("Name", hydraRingDikeSection.SectionName);
+            Assert.AreEqual(2.2, hydraRingDikeSection.SectionBeginCoordinate);
+            Assert.AreEqual(3.3, hydraRingDikeSection.SectionEndCoordinate);
+            Assert.AreEqual(4.4, hydraRingDikeSection.SectionLength);
+            Assert.AreEqual(5.5, hydraRingDikeSection.CrossSectionXCoordinate);
+            Assert.AreEqual(6.6, hydraRingDikeSection.CrossSectionYCoordinate);
+            Assert.AreEqual(7.7, hydraRingDikeSection.CrossSectionNormal);
+        }
+    }
+}
