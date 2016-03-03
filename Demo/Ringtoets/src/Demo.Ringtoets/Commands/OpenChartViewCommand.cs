05da43c9c7424ffd7e5c17919884d9946cd07d5d
rodriqu_dd <rodriqu_dd@example.com>
2016-03-03 12:30:58 +0000

    Added more realistic demo map and demo chart. Related to Issue [WTI-245]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2405 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 91d72d0bb170bbec605369fc46292632984d9cdd
    Former-commit-id: 8c6b70dbfbb027005acbb40b78a81786cf829de5

--- Demo/Ringtoets/src/Demo.Ringtoets/Commands/OpenChartViewCommand.cs
+++ Demo/Ringtoets/src/Demo.Ringtoets/Commands/OpenChartViewCommand.cs
@@ -41,35 +41,176 @@ namespace Demo.Ringtoets.Commands
 
         public void Execute(params object[] arguments)
         {
-            var line = new LineData(new Collection<Tuple<double, double>>
+            var line1 = new LineData(new Collection<Tuple<double, double>>
             {
-                Tuple.Create(0.0, 1.1),    
-                Tuple.Create(1.0, 2.1),
-                Tuple.Create(1.6, 1.6)    
+                Tuple.Create(0.0,0.500),
+                Tuple.Create(0.1,0.723),
+                Tuple.Create(0.2,0.892),
+                Tuple.Create(0.3,1.013),
+                Tuple.Create(0.4,1.092),
+                Tuple.Create(0.5,1.135),
+                Tuple.Create(0.6,1.148),
+                Tuple.Create(0.7,1.137),
+                Tuple.Create(0.8,1.108),
+                Tuple.Create(0.9,1.067),
+                Tuple.Create(1.0,1.020),
+                Tuple.Create(1.1,0.973),
+                Tuple.Create(1.2,0.932),
+                Tuple.Create(1.3,0.903),
+                Tuple.Create(1.4,0.892),
+                Tuple.Create(1.5,0.905),
+                Tuple.Create(1.6,0.948),
+                Tuple.Create(1.7,1.027),
+                Tuple.Create(1.8,1.148),
+                Tuple.Create(1.9,1.317),
+                Tuple.Create(2.0,1.540),
+                Tuple.Create(2.1,1.823)
             });
-            var area = new AreaData(new Collection<Tuple<double, double>>
+
+            var line2 = new LineData(new Collection<Tuple<double, double>>
             {
-                Tuple.Create(0.0, 1.1),    
-                Tuple.Create(1.0, 2.1),
-                Tuple.Create(1.6, 1.6),
-                Tuple.Create(1.6, 0.5),
-                Tuple.Create(0.0, 0.5),
-                Tuple.Create(0.0, 1.1)
+                Tuple.Create(0.0,0.800),
+                Tuple.Create(0.1,1.009),
+                Tuple.Create(0.2,1.162),
+                Tuple.Create(0.3,1.267),
+                Tuple.Create(0.4,1.328),
+                Tuple.Create(0.5,1.351),
+                Tuple.Create(0.6,1.340),
+                Tuple.Create(0.7,1.302),
+                Tuple.Create(0.8,1.242),
+                Tuple.Create(0.9,1.165),
+                Tuple.Create(1.0,1.076),
+                Tuple.Create(1.1,0.982),
+                Tuple.Create(1.2,0.886),
+                Tuple.Create(1.3,0.796),
+                Tuple.Create(1.4,0.716),
+                Tuple.Create(1.5,0.652),
+                Tuple.Create(1.6,0.608),
+                Tuple.Create(1.7,0.591),
+                Tuple.Create(1.8,0.606),
+                Tuple.Create(1.9,0.658),
+                Tuple.Create(2.0,0.752),
+                Tuple.Create(2.1,0.895)
             });
-            var clearArea = new AreaData(new Collection<Tuple<double, double>>
+
+
+
+
+            var area1 = new AreaData(new Collection<Tuple<double, double>>
             {
-                Tuple.Create(0.5, 0.5),    
-                Tuple.Create(0.5, 1.0),
-                Tuple.Create(1.0, 1.0),
-                Tuple.Create(0.5, 0.5)
+                Tuple.Create(0.0,0.500),
+                Tuple.Create(0.1,0.723),
+                Tuple.Create(0.2,0.892),
+                Tuple.Create(0.3,1.013),
+                Tuple.Create(0.4,1.092),
+                Tuple.Create(0.5,1.135),
+                Tuple.Create(0.6,1.148),
+                Tuple.Create(0.7,1.137),
+                Tuple.Create(0.8,1.108),
+                Tuple.Create(0.9,1.067),
+                Tuple.Create(1.0,1.020),
+                Tuple.Create(1.1,0.973),
+                Tuple.Create(1.2,0.932),
+                Tuple.Create(1.3,0.903),
+                Tuple.Create(1.4,0.892),
+                Tuple.Create(1.5,0.905),
+                Tuple.Create(1.5,0.905-0.5),
+                Tuple.Create(1.4,0.892-0.5),
+                Tuple.Create(1.3,0.903-0.5),
+                Tuple.Create(1.2,0.932-0.5),
+                Tuple.Create(1.1,0.973-0.5),
+                Tuple.Create(1.0,1.020-0.5),
+                Tuple.Create(0.9,1.067-0.5),
+                Tuple.Create(0.8,1.108-0.51),
+                Tuple.Create(0.7,1.137-0.52),
+                Tuple.Create(0.6,1.148-0.53),
+                Tuple.Create(0.5,1.135-0.52),
+                Tuple.Create(0.4,1.092-0.51),
+                Tuple.Create(0.3,1.013-0.5),
+                Tuple.Create(0.2,0.892-0.5),
+                Tuple.Create(0.1,0.723-0.5),
+                Tuple.Create(0.0,0.000),
+                Tuple.Create(0.0,0.500)
             });
-            var points = new PointData(new Collection<Tuple<double, double>>
+            var area2 = new AreaData(new Collection<Tuple<double, double>>
             {
-                Tuple.Create(0.0, 1.1),    
-                Tuple.Create(0.5, 1.6),  
-                Tuple.Create(1.0, 2.1)
+
+                Tuple.Create(0.1,0.723-0.5),
+                Tuple.Create(0.2,0.892-0.5),
+                Tuple.Create(0.3,1.013-0.49),
+                Tuple.Create(0.4,1.092-0.48),
+                Tuple.Create(0.5,1.135-0.47),
+                Tuple.Create(0.6,1.148-0.46),
+                Tuple.Create(0.7,1.137-0.47),
+                Tuple.Create(0.8,1.108-0.48),
+                Tuple.Create(0.9,1.067-0.49),
+                Tuple.Create(1.0,1.020-0.50),
+                Tuple.Create(1.1,0.973-0.5),
+                Tuple.Create(1.2,0.932-0.5),
+                Tuple.Create(1.3,0.903-0.5),
+                Tuple.Create(1.4,0.892-0.5),
+                Tuple.Create(1.5,0.905-0.5),
+                Tuple.Create(1.5,0.905),
+                Tuple.Create(1.6,0.948),
+                Tuple.Create(1.7,1.027),
+                Tuple.Create(1.8,1.148),
+                Tuple.Create(1.8,0.606),
+                Tuple.Create(1.9,0.658),
+                Tuple.Create(2.0,0.752),
+                Tuple.Create(2.0,0.350),
+                Tuple.Create(1.5,0.905-0.7),
+                Tuple.Create(1.4,0.892-0.7),
+                Tuple.Create(1.3,0.903-0.7),
+                Tuple.Create(1.2,0.932-0.7),
+                Tuple.Create(1.1,0.973-0.7),
+                Tuple.Create(1.0,1.020-0.7),
+                Tuple.Create(0.9,1.067-0.7),
+                Tuple.Create(0.8,1.108-0.7),
+                Tuple.Create(0.7,1.137-0.7),
+                Tuple.Create(0.6,1.148-0.7),
+                Tuple.Create(0.5,1.135-0.7),
+                Tuple.Create(0.4,1.092-0.7),
+                Tuple.Create(0.3,1.013-0.7),
+                Tuple.Create(0.2,0.892-0.7),
+                Tuple.Create(0.1,0.723-0.7),
+                Tuple.Create(0.1,0.723-0.5)
+
+                
+                
+                
             });
-            documentViewController.DocumentViewsResolver.OpenViewForData(new ChartDataCollection(new List<ChartData> { area, clearArea, line, points }));
+            var points1 = new PointData(new Collection<Tuple<double, double>>
+            {
+                Tuple.Create(0.2,0.892+0.04),
+                Tuple.Create(0.3,1.013+0.02),
+                Tuple.Create(0.4,1.092),
+                Tuple.Create(0.5,1.135-0.02),
+                Tuple.Create(0.6,1.148+0.01),
+                Tuple.Create(1.4,0.892-0.02),
+                Tuple.Create(1.5,0.905+0.01),
+                Tuple.Create(1.8,1.148+0.02)
+            });
+
+            var points2 = new PointData(new Collection<Tuple<double, double>>
+            {
+                Tuple.Create(0.0,0.800 + 0.01),
+                Tuple.Create(0.1,1.009 + 0.02),
+                Tuple.Create(0.2,1.162 + 0.03),
+                Tuple.Create(0.2,1.162 + 0.05),
+                Tuple.Create(0.2,1.162 - 0.03),
+                Tuple.Create(0.2,1.162 - 0.01),
+                Tuple.Create(0.3,1.267),
+                Tuple.Create(0.4,1.328 - 0.01),
+                Tuple.Create(0.53,1.351),
+                Tuple.Create(0.69,1.340),
+                Tuple.Create(0.73,1.302),
+                Tuple.Create(1.4,0.716 - 0.02),
+                Tuple.Create(1.4,0.716 + 0.02),
+                Tuple.Create(1.7,0.591),
+                Tuple.Create(1.8,0.606)
+            });
+
+            documentViewController.DocumentViewsResolver.OpenViewForData(new ChartDataCollection(new List<ChartData> { area1, area2, line1, line2, points1, points2 }));
         }
     }
 }
