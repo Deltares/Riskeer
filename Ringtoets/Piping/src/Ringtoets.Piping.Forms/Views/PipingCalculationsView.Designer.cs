043bdec744a139422a51eadc15998d6812c03178
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-03-17 07:49:30 +0000

    Added controls to PipingCalculationsView Related to Issue [WTI-419]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2584 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: ed8f1bc8b639006a667d92c91999b177efa94456
    Former-commit-id: e014f07d4e479c87f611430449a3d48f813eef59

--- Ringtoets/Piping/src/Ringtoets.Piping.Forms/Views/PipingCalculationsView.Designer.cs
+++ Ringtoets/Piping/src/Ringtoets.Piping.Forms/Views/PipingCalculationsView.Designer.cs
@@ -28,18 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanelUserControl = new System.Windows.Forms.TableLayoutPanel();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanelListBox = new System.Windows.Forms.TableLayoutPanel();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.labelFailureMechanismSections = new System.Windows.Forms.Label();
+            this.tableLayoutPanelDataGrid = new System.Windows.Forms.TableLayoutPanel();
+            this.labelCalculations = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonCreateCalculations = new System.Windows.Forms.Button();
+            this.buttonNavigateToSelectedCalculation = new System.Windows.Forms.Button();
+            this.tableLayoutPanelUserControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.tableLayoutPanelListBox.SuspendLayout();
+            this.tableLayoutPanelDataGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanelUserControl
+            // 
+            this.tableLayoutPanelUserControl.ColumnCount = 1;
+            this.tableLayoutPanelUserControl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelUserControl.Controls.Add(this.splitContainer, 0, 0);
+            this.tableLayoutPanelUserControl.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tableLayoutPanelUserControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelUserControl.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelUserControl.MinimumSize = new System.Drawing.Size(500, 300);
+            this.tableLayoutPanelUserControl.Name = "tableLayoutPanelUserControl";
+            this.tableLayoutPanelUserControl.RowCount = 2;
+            this.tableLayoutPanelUserControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelUserControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanelUserControl.Size = new System.Drawing.Size(762, 560);
+            this.tableLayoutPanelUserControl.TabIndex = 0;
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.tableLayoutPanelListBox);
+            this.splitContainer.Panel1MinSize = 200;
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.tableLayoutPanelDataGrid);
+            this.splitContainer.Panel2MinSize = 300;
+            this.splitContainer.Size = new System.Drawing.Size(756, 522);
+            this.splitContainer.SplitterDistance = 209;
+            this.splitContainer.TabIndex = 0;
+            this.splitContainer.TabStop = false;
+            // 
+            // tableLayoutPanelListBox
+            // 
+            this.tableLayoutPanelListBox.ColumnCount = 1;
+            this.tableLayoutPanelListBox.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelListBox.Controls.Add(this.listBox, 0, 1);
+            this.tableLayoutPanelListBox.Controls.Add(this.labelFailureMechanismSections, 0, 0);
+            this.tableLayoutPanelListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelListBox.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelListBox.Name = "tableLayoutPanelListBox";
+            this.tableLayoutPanelListBox.RowCount = 2;
+            this.tableLayoutPanelListBox.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelListBox.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelListBox.Size = new System.Drawing.Size(209, 522);
+            this.tableLayoutPanelListBox.TabIndex = 0;
+            // 
+            // listBox
+            // 
+            this.listBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.IntegralHeight = false;
+            this.listBox.Location = new System.Drawing.Point(3, 20);
+            this.listBox.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(203, 502);
+            this.listBox.TabIndex = 1;
+            // 
+            // labelFailureMechanismSections
+            // 
+            this.labelFailureMechanismSections.AutoSize = true;
+            this.labelFailureMechanismSections.Location = new System.Drawing.Point(3, 0);
+            this.labelFailureMechanismSections.Name = "labelFailureMechanismSections";
+            this.labelFailureMechanismSections.Size = new System.Drawing.Size(44, 13);
+            this.labelFailureMechanismSections.TabIndex = 0;
+            this.labelFailureMechanismSections.Text = "Vakken";
+            // 
+            // tableLayoutPanelDataGrid
+            // 
+            this.tableLayoutPanelDataGrid.ColumnCount = 1;
+            this.tableLayoutPanelDataGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelDataGrid.Controls.Add(this.labelCalculations, 0, 0);
+            this.tableLayoutPanelDataGrid.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanelDataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelDataGrid.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelDataGrid.Name = "tableLayoutPanelDataGrid";
+            this.tableLayoutPanelDataGrid.RowCount = 2;
+            this.tableLayoutPanelDataGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelDataGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelDataGrid.Size = new System.Drawing.Size(543, 522);
+            this.tableLayoutPanelDataGrid.TabIndex = 1;
+            // 
+            // labelCalculations
+            // 
+            this.labelCalculations.AutoSize = true;
+            this.labelCalculations.Location = new System.Drawing.Point(3, 0);
+            this.labelCalculations.Name = "labelCalculations";
+            this.labelCalculations.Size = new System.Drawing.Size(182, 13);
+            this.labelCalculations.TabIndex = 0;
+            this.labelCalculations.Text = "Berekeningen voor geselecteerd vak";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 20);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(537, 502);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonCreateCalculations, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonNavigateToSelectedCalculation, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 531);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(756, 26);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonCreateCalculations
+            // 
+            this.buttonCreateCalculations.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonCreateCalculations.Location = new System.Drawing.Point(3, 3);
+            this.buttonCreateCalculations.Name = "buttonCreateCalculations";
+            this.buttonCreateCalculations.Size = new System.Drawing.Size(137, 20);
+            this.buttonCreateCalculations.TabIndex = 0;
+            this.buttonCreateCalculations.Text = "Berekeningen aanmaken";
+            this.buttonCreateCalculations.UseVisualStyleBackColor = true;
+            // 
+            // buttonNavigateToSelectedCalculation
+            // 
+            this.buttonNavigateToSelectedCalculation.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonNavigateToSelectedCalculation.Location = new System.Drawing.Point(572, 3);
+            this.buttonNavigateToSelectedCalculation.Name = "buttonNavigateToSelectedCalculation";
+            this.buttonNavigateToSelectedCalculation.Size = new System.Drawing.Size(181, 20);
+            this.buttonNavigateToSelectedCalculation.TabIndex = 1;
+            this.buttonNavigateToSelectedCalculation.Text = "Ga naar geselecteerde berekening";
+            this.buttonNavigateToSelectedCalculation.UseVisualStyleBackColor = true;
             // 
             // PipingCalculationsView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoScrollMinSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.tableLayoutPanelUserControl);
             this.Name = "PipingCalculationsView";
-            this.Size = new System.Drawing.Size(491, 351);
+            this.Size = new System.Drawing.Size(762, 560);
+            this.tableLayoutPanelUserControl.ResumeLayout(false);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.tableLayoutPanelListBox.ResumeLayout(false);
+            this.tableLayoutPanelListBox.PerformLayout();
+            this.tableLayoutPanelDataGrid.ResumeLayout(false);
+            this.tableLayoutPanelDataGrid.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelUserControl;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelListBox;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label labelFailureMechanismSections;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelDataGrid;
+        private System.Windows.Forms.Label labelCalculations;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonCreateCalculations;
+        private System.Windows.Forms.Button buttonNavigateToSelectedCalculation;
     }
 }
