f9f9c0b7a1648fa87a6b423140e09d29c4b26a9d
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-03-31 14:49:14 +0000

    Improved EmbeddedResourceFileWriter => Comments => Added tests Related to Issue [WTI-419]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2790 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 02ff55dcb2f98c5cdf56b33bf01dccb6baab561a
    Former-commit-id: 70141fb436e3a2f5e1169a7862709c1946a78502

--- Core/Common/src/Core.Common.Utils/IO/EmbeddedResourceFileWriter.cs
+++ Core/Common/src/Core.Common.Utils/IO/EmbeddedResourceFileWriter.cs
@@ -40,6 +40,7 @@ namespace Core.Common.Utils.IO
         /// <param name="assembly">The assembly that embeds the resources to write.</param>
         /// <param name="removeFilesOnDispose">Whether or not the files should be removed after disposing the created <see cref="EmbeddedResourceFileWriter"/> instance.</param>
         /// <param name="embeddedResourceFileNames">The names of the Embedded Resource files to (temporary) write to the Windows Temp directory.</param>
+        /// <exception cref="ArgumentException">An embedded resource file in <paramref name="embeddedResourceFileNames"/> cannot be found in <paramref name="assembly"/>.</exception>
         public EmbeddedResourceFileWriter(Assembly assembly, bool removeFilesOnDispose, params string[] embeddedResourceFileNames)
         {
             this.removeFilesOnDispose = removeFilesOnDispose;
@@ -55,7 +56,7 @@ namespace Core.Common.Utils.IO
         }
 
         /// <summary>
-        /// Gets the target folder path.
+        /// Gets the (automatically generated) target folder path.
         /// </summary>
         public string TargetFolderPath
         {
---
+++ Core/Common/test/Core.Common.Utils.Test/IO/EmbeddedResourceFileWriterTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using Core.Common.Utils.IO;
+using NUnit.Framework;
+
+namespace Core.Common.Utils.Test.IO
+{
+    [TestFixture]
+    public class EmbeddedResourceFileWriterTest
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        public void EmbeddedResourceFileWriter_ValidEmbeddedResources_FilesCorrectlyWritten(bool removeFilesOnDispose)
+        {
+            // Call
+            using (var embeddedResourceFileWriter = new EmbeddedResourceFileWriter(GetType().Assembly, removeFilesOnDispose, "EmbeddedResource1.txt", "EmbeddedResource2.txt"))
+            {
+                // Assert
+                Assert.IsTrue(File.Exists(Path.Combine(embeddedResourceFileWriter.TargetFolderPath, "EmbeddedResource1.txt")));
+                Assert.IsTrue(File.Exists(Path.Combine(embeddedResourceFileWriter.TargetFolderPath, "EmbeddedResource2.txt")));
+            }
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void EmbeddedResourceFileWriter_ValidEmbeddedResources_FilesPreservedAccordingToFlagRemoveFilesOnDispose(bool removeFilesOnDispose)
+        {
+            // Setup
+            string targetFolderPath;
+
+            // Call
+            using (var embeddedResourceFileWriter = new EmbeddedResourceFileWriter(GetType().Assembly, removeFilesOnDispose, "EmbeddedResource1.txt", "EmbeddedResource2.txt"))
+            {
+                targetFolderPath = embeddedResourceFileWriter.TargetFolderPath;
+            }
+
+            // Assert
+            Assert.AreEqual(!removeFilesOnDispose, File.Exists(Path.Combine(targetFolderPath, "EmbeddedResource1.txt")));
+            Assert.AreEqual(!removeFilesOnDispose, File.Exists(Path.Combine(targetFolderPath, "EmbeddedResource2.txt")));
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void EmbeddedResourceFileWriter_ResourceNotEmbedded_ArgumentExceptionThrown(bool removeFilesOnDispose)
+        {
+            // Setup & Call
+            TestDelegate test = () => new EmbeddedResourceFileWriter(GetType().Assembly, removeFilesOnDispose, "NonEmbeddedResource.txt");
+
+            // Assert
+            var exception = Assert.Throws<ArgumentException>(test);
+            Assert.IsInstanceOf<InvalidOperationException>(exception.InnerException);
+            Assert.IsTrue(exception.Message.Contains("Cannot find embedded resource file 'NonEmbeddedResource.txt'"));
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void EmbeddedResourceFileWriter_NonExistingResource_ArgumentExceptionThrown(bool removeFilesOnDispose)
+        {
+            // Setup & Call
+            TestDelegate test = () => new EmbeddedResourceFileWriter(GetType().Assembly, removeFilesOnDispose, "NonExistingResource.txt");
+
+            // Assert
+            var exception = Assert.Throws<ArgumentException>(test);
+            Assert.IsInstanceOf<InvalidOperationException>(exception.InnerException);
+            Assert.IsTrue(exception.Message.Contains("Cannot find embedded resource file 'NonExistingResource.txt'"));
+        }
+    }
+}
