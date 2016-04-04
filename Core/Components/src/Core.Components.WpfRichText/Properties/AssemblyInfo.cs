87078a6e2cde2c9a94644739f0cf998c60c5da75
Robert Tangerman <robert.tangerman@deltares.nl>
2016-04-04 10:59:58 +0000

    Added new project to encapsulate the Wpf RichText Editor. Related to Issue [WTI-431]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2818 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 75abc925b5a6b3653477c10752b5b29ed25c4d5d
    Former-commit-id: 3a288ff4e96dbf7dc84187e5ed8e46362558da38

---
+++ Core/Components/src/Core.Components.WpfRichText/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Core.Components.WpfRichText")]
+[assembly: AssemblyProduct("Core.Components.WpfRichText")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93D1C768-62E9-4F35-B098-689077ADCFA8")]
