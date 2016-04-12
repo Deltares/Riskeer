b2a56be4d42df059d8aaaef53cbdce67b48fd9c3
Robert Tangerman <robert.tangerman@deltares.nl>
2016-04-12 09:28:34 +0000

    Implemented derived class of PipingCalculation for scenarios. Related to Issue [WTI-421]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2903 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 7521fefea19d32c3ed0353be2ca0229d82abf224
    Former-commit-id: d85745bf85e89f2444559d85d9bb07fd02406afc

---
+++ Ringtoets/Piping/src/Ringtoets.Piping.Data/PipingCalculationScenario.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+
+namespace Ringtoets.Piping.Data
+{
+    /// <summary>
+    /// This class holds the information for a calculation scenario.
+    /// </summary>
+    public class PipingCalculationScenario : PipingCalculation
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="PipingCalculationScenario"/> with default values set for some of the parameters.
+        /// </summary>
+        /// <param name="generalInputParameters">General piping calculation parameters that
+        /// are the same across all piping calculations.</param>
+        /// <param name="semiProbabilisticInputParameters">General semi-probabilistic parameters that 
+        /// are used in a semi-probabilistic piping assessment.</param>
+        /// <exception cref="ArgumentNullException">When <paramref name="generalInputParameters"/> or 
+        /// <paramref name="semiProbabilisticInputParameters"/> is <c>null</c>.</exception>
+        public PipingCalculationScenario(GeneralPipingInput generalInputParameters, SemiProbabilisticPipingInput semiProbabilisticInputParameters)
+            : base(generalInputParameters, semiProbabilisticInputParameters)
+        {
+            IsRelevant = true;
+        }
+
+        /// <summary>
+        /// Gets  or sets whether this scenario is relevant or not.
+        /// </summary>
+        public bool IsRelevant { get; set; }
+
+        /// <summary>
+        /// Gets the contribution of the scenario.
+        /// </summary>
+        public double Contribution { get; set; }
+    }
+}
---
+++ Ringtoets/Piping/test/Ringtoets.Piping.Data.Test/PipingCalculationScenarioTest.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+
+namespace Ringtoets.Piping.Data.Test
+{
+    [TestFixture]
+    public class PipingCalculationScenarioTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var generalInputParameters = new GeneralPipingInput();
+            var semiProbabilisticInputParameters = new SemiProbabilisticPipingInput();
+
+            // Call
+            var scenario = new PipingCalculationScenario(generalInputParameters, semiProbabilisticInputParameters);
+
+            // Assert
+            Assert.IsInstanceOf<PipingCalculation>(scenario);
+            Assert.AreSame(semiProbabilisticInputParameters, scenario.SemiProbabilisticParameters);
+            Assert.IsTrue(scenario.IsRelevant);
+            Assert.AreEqual(0, scenario.Contribution);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsRelevant_Always_ReturnsSetValue(bool isRelevant)
+        {
+            // Setup
+            var generalInputParameters = new GeneralPipingInput();
+            var semiProbabilisticInputParameters = new SemiProbabilisticPipingInput();
+
+            var scenario = new PipingCalculationScenario(generalInputParameters, semiProbabilisticInputParameters);
+
+            // Call
+            scenario.IsRelevant = isRelevant;
+
+            // Assert
+            Assert.AreEqual(isRelevant, scenario.IsRelevant);
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(15.0)]
+        public void Contribution_Always_ReturnsSetValue(double newValue)
+        {
+            // Setup
+            var generalInputParameters = new GeneralPipingInput();
+            var semiProbabilisticInputParameters = new SemiProbabilisticPipingInput();
+
+            var scenario = new PipingCalculationScenario(generalInputParameters, semiProbabilisticInputParameters);
+
+            // Call
+            scenario.Contribution = newValue;
+
+            // Assert
+            Assert.AreEqual(newValue, scenario.Contribution);
+        }
+    }
+}
