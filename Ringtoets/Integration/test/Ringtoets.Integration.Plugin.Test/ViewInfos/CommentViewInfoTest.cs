4c21758818f3c1f159de75fec5a3721fe5b50b6a
Marten Janssen <marten.janssen@deltares.nl>
2016-04-12 06:27:21 +0000

    small review changes Related to Issue [WTI-435]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2900 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 11991b4c31232b0319cf3a6f6f706773d53baf52
    Former-commit-id: 68aba288aa11f1422a0a2cc76ff2b1e3465f15d4

--- Ringtoets/Integration/test/Ringtoets.Integration.Plugin.Test/ViewInfos/CommentViewInfoTest.cs
+++ Ringtoets/Integration/test/Ringtoets.Integration.Plugin.Test/ViewInfos/CommentViewInfoTest.cs
@@ -1,4 +1,25 @@
-﻿using System.Linq;
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Linq;
 using Core.Common.Gui.Plugin;
 using Core.Common.TestUtil;
 using NUnit.Framework;
@@ -282,7 +303,7 @@ namespace Ringtoets.Integration.Plugin.Test.ViewInfos
             var viewDataCalculation = mocks.Stub<ICalculationItem>();
 
             var deletedCalculation = mocks.Stub<ICalculationItem>();
-            
+
             var deletedfailureMechanism = mocks.Stub<IFailureMechanism>();
             deletedfailureMechanism.Stub(fm => fm.CalculationItems).Return(new[]
             {
@@ -322,10 +343,10 @@ namespace Ringtoets.Integration.Plugin.Test.ViewInfos
             var failureMechanism = new PipingFailureMechanism();
 
             var deletedCalculationContext = new PipingCalculationContext(viewDataCalculation,
-                                                                  Enumerable.Empty<RingtoetsPipingSurfaceLine>(),
-                                                                  Enumerable.Empty<StochasticSoilModel>(),
-                                                                  failureMechanism,
-                                                                  assessmentSectionMock);
+                                                                         Enumerable.Empty<RingtoetsPipingSurfaceLine>(),
+                                                                         Enumerable.Empty<StochasticSoilModel>(),
+                                                                         failureMechanism,
+                                                                         assessmentSectionMock);
 
             using (var view = new CommentView
             {
@@ -338,7 +359,7 @@ namespace Ringtoets.Integration.Plugin.Test.ViewInfos
                 // Assert
                 Assert.IsTrue(closeForData);
             }
-            
+
             mocks.VerifyAll();
         }
 
