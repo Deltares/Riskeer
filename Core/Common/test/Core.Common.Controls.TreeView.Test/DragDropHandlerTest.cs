c8813d252befec1494a76e44e04b64985b34d7fc
Joël van den Berg <joel.vandenberg@deltares.nl>
2016-04-18 12:56:20 +0000

    Added test in DragDropHandlerTest for dragging a tree node. Related to Issue [WTI-481]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2939 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: c9bc6b93169257659a0cde7ecccbecbe32c48ea4
    Former-commit-id: 18bbd195c189e286ea28edd6151f13408738fd81

---
+++ Core/Common/test/Core.Common.Controls.TreeView.Test/DragDropHandlerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+using NUnit.Framework;
+
+namespace Core.Common.Controls.TreeView.Test
+{
+    [TestFixture]
+    public class DragDropHandlerTest
+    {
+        [Test]
+        public void HandleItemDrag_WithItemInTree_SelectsItem()
+        {
+            // Setup
+            DragDropHandler ddh = new DragDropHandler();
+            Func<object, TreeNodeInfo> action = o => new TreeNodeInfo();
+            var treeNode = new TreeNode();
+            ItemDragEventArgs dragEvent = new ItemDragEventArgs(MouseButtons.Left, treeNode);
+            System.Windows.Forms.TreeView treeView = new System.Windows.Forms.TreeView();
+
+            treeView.Nodes.Add(treeNode);
+            treeView.SelectedNode = null;
+
+            // Call
+            ddh.HandleItemDrag(treeView, dragEvent, action);
+
+            // Assert
+            Assert.AreSame(treeNode, treeView.SelectedNode);
+        }
+
+        [Test]
+        public void HandleItemDrag_WithItemNotInTree_SelectsNull()
+        {
+            // Setup
+            DragDropHandler ddh = new DragDropHandler();
+            Func<object, TreeNodeInfo> action = o => new TreeNodeInfo();
+            var draggingNode = new TreeNode();
+            var treeNode = new TreeNode();
+            ItemDragEventArgs dragEvent = new ItemDragEventArgs(MouseButtons.Left, draggingNode);
+            System.Windows.Forms.TreeView treeView = new System.Windows.Forms.TreeView();
+
+            treeView.Nodes.Add(treeNode);
+            treeView.SelectedNode = treeNode;
+
+            // Call
+            ddh.HandleItemDrag(treeView, dragEvent, action);
+
+            // Assert
+            Assert.IsNull(treeView.SelectedNode);
+        }
+    }
+}
