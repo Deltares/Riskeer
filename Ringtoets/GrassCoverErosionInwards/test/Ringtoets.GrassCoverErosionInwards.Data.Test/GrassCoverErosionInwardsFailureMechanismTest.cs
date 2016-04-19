d2b3e26fb42063a071bf4fc65b6198ef2369db30
Marten Janssen <marten.janssen@deltares.nl>
2016-04-19 09:27:09 +0000

    Overtopping should have been GrassCoverErosionInwards Related to Issue [WTI-456]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@2946 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: cc3c75f533b809e17bf84af9b815b611c1467261
    Former-commit-id: 875aa67e7f575a019b66fb3d3e97fb0a91429c46

---
+++ Ringtoets/GrassCoverErosionInwards/test/Ringtoets.GrassCoverErosionInwards.Data.Test/GrassCoverErosionInwardsFailureMechanismTest.cs
@@ -0,0 +1,142 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Base;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.GrassCoverErosionInwards.Data.Properties;
+
+namespace Ringtoets.GrassCoverErosionInwards.Data.Test
+{
+    [TestFixture]
+    public class GrassCoverErosionInwardsFailureMechanismTest
+    {
+        private MockRepository mockRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockRepository = new MockRepository();
+        }
+
+        [Test]
+        public void DefaultConstructor_ExpectedValues()
+        {
+            // call
+            var grassCoverErosionInwardsFailureMechanism = new GrassCoverErosionInwardsFailureMechanism();
+
+            // assert
+            Assert.IsInstanceOf<FailureMechanismBase>(grassCoverErosionInwardsFailureMechanism);
+            Assert.AreEqual(Resources.GrassCoverErosionInwardsFailureMechanism_DisplayName, grassCoverErosionInwardsFailureMechanism.Name);
+        }
+
+        [Test]
+        public void Notify_SingleListenerAttached_ListenerIsNotified()
+        {
+            // Setup
+            var observer = mockRepository.StrictMock<IObserver>();
+            observer.Expect(o => o.UpdateObserver());
+            mockRepository.ReplayAll();
+
+            var grassCoverErosionInwardsFailureMechanism = new GrassCoverErosionInwardsFailureMechanism();
+
+            grassCoverErosionInwardsFailureMechanism.Attach(observer);
+
+            // Call & Assert
+            grassCoverErosionInwardsFailureMechanism.NotifyObservers();
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void Notify_SingleListenerAttachedAndDeattached_ListenerIsNotNotified()
+        {
+            // Setup
+            var observer = mockRepository.StrictMock<IObserver>();
+            observer.Expect(o => o.UpdateObserver()).Repeat.Never();
+            mockRepository.ReplayAll();
+
+            var grassCoverErosionInwardsFailureMechanism = new GrassCoverErosionInwardsFailureMechanism();
+
+            grassCoverErosionInwardsFailureMechanism.Attach(observer);
+            grassCoverErosionInwardsFailureMechanism.Detach(observer);
+
+            // Call & Assert
+            grassCoverErosionInwardsFailureMechanism.NotifyObservers();
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void Notify_TwoListenersAttached_BothAreNotified()
+        {
+            // Setup
+            var observerA = mockRepository.StrictMock<IObserver>();
+            observerA.Expect(o => o.UpdateObserver());
+
+            var observerB = mockRepository.StrictMock<IObserver>();
+            observerB.Expect(o => o.UpdateObserver());
+            mockRepository.ReplayAll();
+
+            var grassCoverErosionInwardsFailureMechanism = new GrassCoverErosionInwardsFailureMechanism();
+
+            grassCoverErosionInwardsFailureMechanism.Attach(observerA);
+            grassCoverErosionInwardsFailureMechanism.Attach(observerB);
+
+            // Call & Assert
+            grassCoverErosionInwardsFailureMechanism.NotifyObservers();
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void Notify_TwoListenersAttachedOneDetached_InvokedOnce()
+        {
+            // Setup
+            var observerA = mockRepository.StrictMock<IObserver>();
+            observerA.Expect(o => o.UpdateObserver()).Repeat.Never();
+
+            var observerB = mockRepository.StrictMock<IObserver>();
+            observerB.Expect(o => o.UpdateObserver());
+            mockRepository.ReplayAll();
+
+            var grassCoverErosionInwardsFailureMechanism = new GrassCoverErosionInwardsFailureMechanism();
+
+            grassCoverErosionInwardsFailureMechanism.Attach(observerA);
+            grassCoverErosionInwardsFailureMechanism.Attach(observerB);
+            grassCoverErosionInwardsFailureMechanism.Detach(observerA);
+
+            // Call & Assert
+            grassCoverErosionInwardsFailureMechanism.NotifyObservers();
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void Detach_DetachNonAttachedObserver_ThrowsNoException()
+        {
+            // Setup
+            var observer = mockRepository.StrictMock<IObserver>();
+
+            var grassCoverErosionInwardsFailureMechanism = new GrassCoverErosionInwardsFailureMechanism();
+
+            // Call & Assert
+            grassCoverErosionInwardsFailureMechanism.Detach(observer);
+        }
+    }
+}
---
+++ Ringtoets/GrassCoverErosionInwards/test/Ringtoets.GrassCoverErosionInwards.Data.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Ringtoets.GrassCoverErosionInwards.Data.Test")]
+[assembly: AssemblyProduct("Ringtoets.GrassCoverErosionInwards.Data.Test")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d0331be-7bae-43bb-bde0-4558b164b176")]
