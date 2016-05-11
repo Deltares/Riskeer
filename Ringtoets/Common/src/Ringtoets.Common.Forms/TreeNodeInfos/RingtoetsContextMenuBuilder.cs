e13aad684439fe0ce56760077b13a7c5b03674de
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-05-11 13:25:43 +0000

    Implemented first version of RingtoetsContextMenuBuilder Related to Issue [WTI-460]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@3162 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 716dfba9690f726d113d3b8c6e9d7bbb3ed6930d
    Former-commit-id: a6d1f8092372eb8b772ddf4baf4ebc79bf724884

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/TreeNodeInfos/RingtoetsContextMenuBuilder.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Windows.Forms;
+using Core.Common.Gui.ContextMenu;
+
+namespace Ringtoets.Common.Forms.TreeNodeInfos
+{
+    /// <summary>
+    /// Decorator for <see cref="ContextMenuBuilder"/>.
+    /// </summary>
+    public class RingtoetsContextMenuBuilder : IContextMenuBuilder
+    {
+        private readonly IContextMenuBuilder contextMenuBuilder;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="RingtoetsContextMenuBuilder"/>.
+        /// </summary>
+        /// <param name="contextMenuBuilder">The context menu builder to decorate.</param>
+        public RingtoetsContextMenuBuilder(IContextMenuBuilder contextMenuBuilder)
+        {
+            this.contextMenuBuilder = contextMenuBuilder;
+        }
+
+        public IContextMenuBuilder AddRenameItem()
+        {
+            return contextMenuBuilder.AddRenameItem();
+        }
+
+        public IContextMenuBuilder AddDeleteItem()
+        {
+            return contextMenuBuilder.AddDeleteItem();
+        }
+
+        public IContextMenuBuilder AddExpandAllItem()
+        {
+            return contextMenuBuilder.AddExpandAllItem();
+        }
+
+        public IContextMenuBuilder AddCollapseAllItem()
+        {
+            return contextMenuBuilder.AddCollapseAllItem();
+        }
+
+        public IContextMenuBuilder AddOpenItem()
+        {
+            return contextMenuBuilder.AddOpenItem();
+        }
+
+        public IContextMenuBuilder AddExportItem()
+        {
+            return contextMenuBuilder.AddExportItem();
+        }
+
+        public IContextMenuBuilder AddImportItem()
+        {
+            return contextMenuBuilder.AddImportItem();
+        }
+
+        public IContextMenuBuilder AddPropertiesItem()
+        {
+            return contextMenuBuilder.AddPropertiesItem();
+        }
+
+        public IContextMenuBuilder AddSeparator()
+        {
+            return contextMenuBuilder.AddSeparator();
+        }
+
+        public IContextMenuBuilder AddCustomItem(StrictContextMenuItem item)
+        {
+            return contextMenuBuilder.AddCustomItem(item);
+        }
+
+        public ContextMenuStrip Build()
+        {
+            return contextMenuBuilder.Build();
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.Test/TreeNodeInfos/RingtoetsContextMenuBuilderTest.cs
@@ -0,0 +1,253 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Gui.ContextMenu;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Forms.TreeNodeInfos;
+
+namespace Ringtoets.Common.Forms.Test.TreeNodeInfos
+{
+    [TestFixture]
+    public class RingtoetsContextMenuBuilderTest
+    {
+        [Test]
+        public void AddRenameItem_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddRenameItem());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddRenameItem();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void AddDeleteItem_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddDeleteItem());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddDeleteItem();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void AddExpandAllItem_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddExpandAllItem());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddExpandAllItem();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void AddCollapseAllItem_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddCollapseAllItem());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddCollapseAllItem();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void AddOpenItem_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddOpenItem());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddOpenItem();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void AddExportItem_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddExportItem());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddExportItem();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void AddImportItem_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddImportItem());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddImportItem();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void AddPropertiesItem_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddPropertiesItem());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddPropertiesItem();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void AddSeparator_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddSeparator());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddSeparator();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void AddCustomItem_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuItem = mocks.StrictMock<StrictContextMenuItem>();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.AddCustomItem(contextMenuItem));
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.AddCustomItem(contextMenuItem);
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Build_ContextMenuBuilder_CorrectlyDecorated()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var contextMenuBuilder = mocks.StrictMock<IContextMenuBuilder>();
+
+            contextMenuBuilder.Expect(cmb => cmb.Build());
+
+            mocks.ReplayAll();
+
+            var ringtoetsContextMenuBuilder = new RingtoetsContextMenuBuilder(contextMenuBuilder);
+
+            // Call
+            ringtoetsContextMenuBuilder.Build();
+
+            // Assert
+            mocks.VerifyAll();
+        }
+    }
+}
