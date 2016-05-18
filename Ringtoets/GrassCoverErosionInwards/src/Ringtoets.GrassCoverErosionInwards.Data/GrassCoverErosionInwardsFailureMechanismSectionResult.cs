3022ccb9ae4349c4b9b67c4fb35ab4c48bce21b2
Joël van den Berg <joel.vandenberg@deltares.nl>
2016-05-18 15:12:17 +0000

    Added missing file. Related to Issue [WTI-513]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@3229 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: c8210e63865d9500dad991ae953f00f21bf8ced7
    Former-commit-id: d51060bba088db2e4fb1ab8007914ccff7fd629f

---
+++ Ringtoets/GrassCoverErosionInwards/src/Ringtoets.GrassCoverErosionInwards.Data/GrassCoverErosionInwardsFailureMechanismSectionResult.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Base.Data;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.GrassCoverErosionInwards.Data
+{
+    public class GrassCoverErosionInwardsFailureMechanismSectionResult : FailureMechanismSectionResult
+    {
+        public GrassCoverErosionInwardsFailureMechanismSectionResult(FailureMechanismSection section) : base(section)
+        { }
+
+        /// <summary>
+        /// Gets or sets the state of the assessment layer one.
+        /// </summary>
+        public bool AssessmentLayerOne { get; set; }
+
+        /// <summary>
+        /// Gets the value of assessment layer two a.
+        /// </summary>
+        public RoundedDouble AssessmentLayerTwoA
+        {
+            get
+            {
+                return (RoundedDouble)0.5;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the value of assessment layer two b.
+        /// </summary>
+        public RoundedDouble AssessmentLayerTwoB { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of assessment layer three.
+        /// </summary>
+        public RoundedDouble AssessmentLayerThree { get; set; }
+    }
+}
