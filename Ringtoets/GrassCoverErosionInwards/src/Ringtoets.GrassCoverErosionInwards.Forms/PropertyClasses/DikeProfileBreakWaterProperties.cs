431aeae1b832351f8cef9668959794ed13e1d365
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-06-16 12:51:17 +0000

    * Introduced DikeProfileBreakWaterProperties Related to Issue [WTI-640]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@3667 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 354c6b1a72723b6137ef228fa42d6926bf132c55
    Former-commit-id: fd789efd83b355d3efeb6eaa28716957803a0261

---
+++ Ringtoets/GrassCoverErosionInwards/src/Ringtoets.GrassCoverErosionInwards.Forms/PropertyClasses/DikeProfileBreakWaterProperties.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.ComponentModel;
+using Core.Common.Base.Data;
+using Core.Common.Gui.Attributes;
+using Core.Common.Gui.PropertyBag;
+using Core.Common.Utils.Attributes;
+using Ringtoets.GrassCoverErosionInwards.Data;
+using Ringtoets.GrassCoverErosionInwards.Forms.Properties;
+using Ringtoets.GrassCoverErosionInwards.Forms.TypeConverters;
+
+namespace Ringtoets.GrassCoverErosionInwards.Forms.PropertyClasses
+{
+    /// <summary>
+    /// ViewModel of break water of <see cref="DikeProfile"/> for properties panel.
+    /// </summary>
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class DikeProfileBreakWaterProperties : ObjectProperties<DikeProfile>
+    {
+        [PropertyOrder(1)]
+        [ResourcesDisplayName(typeof(Resources), "BreakWater_UseBreakWater_DisplayName")]
+        [ResourcesDescription(typeof(Resources), "BreakWater_UseBreakWater_Description")]
+        public bool HasBreakWater
+        {
+            get
+            {
+                return data.HasBreakWater;
+            }
+        }
+
+        [PropertyOrder(2)]
+        [ResourcesDisplayName(typeof(Resources), "BreakWaterType_DisplayName")]
+        [ResourcesDescription(typeof(Resources), "BreakWaterType_Description")]
+        [TypeConverter(typeof(EnumTypeConverter))]
+        public BreakWaterType BreakWaterType
+        {
+            get
+            {
+                return data.BreakWater.Type;
+            }
+        }
+
+        [PropertyOrder(3)]
+        [ResourcesDisplayName(typeof(Resources), "BreakWaterHeight_DisplayName")]
+        [ResourcesDescription(typeof(Resources), "BreakWaterHeight_Description")]
+        public RoundedDouble BreakWaterHeight
+        {
+            get
+            {
+                return data.BreakWater.Height;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ Ringtoets/GrassCoverErosionInwards/test/Ringtoets.GrassCoverErosionInwards.Forms.Test/PropertyClasses/DikeProfileBreakWaterPropertiesTest.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.ComponentModel;
+using Core.Common.Base.Geometry;
+using Core.Common.Gui.PropertyBag;
+using NUnit.Framework;
+using Ringtoets.GrassCoverErosionInwards.Data;
+using Ringtoets.GrassCoverErosionInwards.Forms.PropertyClasses;
+
+namespace Ringtoets.GrassCoverErosionInwards.Forms.Test.PropertyClasses
+{
+    [TestFixture]
+    public class DikeProfileBreakWaterPropertiesTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup & Call
+            var properties = new DikeProfileBreakWaterProperties();
+
+            // Assert
+            Assert.IsInstanceOf<ObjectProperties<DikeProfile>>(properties);
+            Assert.IsNull(properties.Data);
+            Assert.AreEqual(string.Empty, properties.ToString());
+        }
+
+        [Test]
+        public void Data_SetDikeProfileInstanceWithData_ReturnCorrectPropertyValues()
+        {
+            // Setup
+            var dikeProfile = new DikeProfile(new Point2D(0, 0))
+            {
+                BreakWater = new BreakWater(BreakWaterType.Wall, 10.0)
+            };
+
+            var properties = new DikeProfileBreakWaterProperties();
+
+            // Call
+            properties.Data = dikeProfile;
+
+            // Assert
+            Assert.IsTrue(properties.HasBreakWater);
+            Assert.AreEqual(BreakWaterType.Wall, properties.BreakWaterType);
+            Assert.AreEqual(10.0, properties.BreakWaterHeight.Value);
+        }
+
+        [Test]
+        public void PropertyAttributes_ReturnExpectedValues()
+        {
+            // Setup
+            var dikeProfile = new DikeProfile(new Point2D(0, 0));
+
+            // Call
+            var properties = new DikeProfileBreakWaterProperties
+            {
+                Data = dikeProfile
+            };
+
+            // Assert
+            TypeConverter classTypeConverter = TypeDescriptor.GetConverter(properties, true);
+            Assert.IsInstanceOf<ExpandableObjectConverter>(classTypeConverter);
+
+            var dynamicPropertyBag = new DynamicPropertyBag(properties);
+            PropertyDescriptorCollection dynamicProperties = dynamicPropertyBag.GetProperties();
+            Assert.AreEqual(4, dynamicProperties.Count);
+
+            PropertyDescriptor useBreakWaterProperty = dynamicProperties[0];
+            Assert.IsNotNull(useBreakWaterProperty);
+            Assert.IsTrue(useBreakWaterProperty.IsReadOnly);
+            Assert.AreEqual("Aanwezig", useBreakWaterProperty.DisplayName);
+            Assert.AreEqual("Is er een dam aanwezig?", useBreakWaterProperty.Description);
+
+            PropertyDescriptor breakWaterTypeProperty = dynamicProperties[1];
+            Assert.IsNotNull(breakWaterTypeProperty);
+            Assert.IsTrue(breakWaterTypeProperty.IsReadOnly);
+            Assert.AreEqual("Type", breakWaterTypeProperty.DisplayName);
+            Assert.AreEqual("Het type van de dam.", breakWaterTypeProperty.Description);
+
+            PropertyDescriptor breakWaterHeightProperty = dynamicProperties[2];
+            Assert.IsNotNull(breakWaterHeightProperty);
+            Assert.IsTrue(breakWaterHeightProperty.IsReadOnly);
+            Assert.AreEqual("Hoogte [m+NAP]", breakWaterHeightProperty.DisplayName);
+            Assert.AreEqual("De hoogte van de dam [m+NAP].", breakWaterHeightProperty.Description);
+        }
+    }
+}
