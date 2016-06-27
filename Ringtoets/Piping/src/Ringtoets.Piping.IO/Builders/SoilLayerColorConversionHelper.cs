8129f84fe18c49d8e7cbafcb16e8acf5dd08130b
Joël van den Berg <joel.vandenberg@deltares.nl>
2016-06-27 13:48:20 +0000

    Added license headers Related to Issue [WTI-443]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@3832 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 84937285b5360b7f657a0bacdeeb9af9e6c63f22
    Former-commit-id: eedcf56bd38a405b0cdd66fb1d7ed5b956b310b7

--- Ringtoets/Piping/src/Ringtoets.Piping.IO/Builders/SoilLayerColorConversionHelper.cs
+++ Ringtoets/Piping/src/Ringtoets.Piping.IO/Builders/SoilLayerColorConversionHelper.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
 using System.Drawing;
 
 namespace Ringtoets.Piping.IO.Builders
@@ -16,11 +37,7 @@ namespace Ringtoets.Piping.IO.Builders
         /// <returns>A <see cref="Color"/> instance based on the <paramref name="colorValue"/>.</returns>
         public static Color ColorFromNullableDouble(double? colorValue)
         {
-            if (colorValue == null)
-            {
-                return Color.Empty;
-            }
-            return Color.FromArgb(Convert.ToInt32(colorValue));
+            return colorValue == null ? Color.Empty : Color.FromArgb(Convert.ToInt32(colorValue));
         }
     }
 }
