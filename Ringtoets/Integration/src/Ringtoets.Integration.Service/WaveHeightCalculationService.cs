7eb4647c7751883ebba18fcc71c72825636906b0
Marten Janssen <marten.janssen@deltares.nl>
2016-08-04 13:12:58 +0000

    - Added calculation service for wave height calculation Related to Issue [WTI-502]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@4497 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 1b93efb584bac1f9371ac3f1595c7ae38be37679
    Former-commit-id: 64ac67ec1aa3b1de4c9ca7516478a259619ebc7c

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Service/WaveHeightCalculationService.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.IO;
+using log4net;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Service;
+using Ringtoets.HydraRing.Calculation.Data;
+using Ringtoets.HydraRing.Calculation.Data.Input.Hydraulics;
+using Ringtoets.HydraRing.Calculation.Data.Output;
+using Ringtoets.HydraRing.Calculation.Parsers;
+using Ringtoets.HydraRing.Calculation.Services;
+using Ringtoets.HydraRing.Data;
+using Ringtoets.HydraRing.IO;
+using Ringtoets.Integration.Service.Properties;
+using RingtoetsCommonServiceResources = Ringtoets.Common.Service.Properties.Resources;
+using RingtoetsCommonFormsResources = Ringtoets.Common.Forms.Properties.Resources;
+
+namespace Ringtoets.Integration.Service
+{
+    /// <summary>
+    /// Service that provides methods for performing Hydra-Ring calculations for marginal wave statistics.
+    /// </summary>
+    public class WaveHeightCalculationService
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(WaveHeightCalculationService));
+
+        /// <summary>
+        /// Performs validation over the values on the given <paramref name="hydraulicBoundaryDatabase"/>. Error information is logged during
+        /// the execution of the operation.
+        /// </summary>
+        /// <param name="hydraulicBoundaryDatabase">The <see cref="HydraulicBoundaryDatabase"/> for which to validate the values.</param>
+        /// <param name="hydraulicBoundaryLocation">The <see cref="HydraulicBoundaryLocation"/> for which to validate the values.</param>
+        /// <returns><c>False</c> if <paramref name="hydraulicBoundaryDatabase"/> contains validation errors; <c>True</c> otherwise.</returns>
+        internal static bool Validate(HydraulicBoundaryDatabase hydraulicBoundaryDatabase, HydraulicBoundaryLocation hydraulicBoundaryLocation)
+        {
+            CalculationServiceHelper.LogValidationBeginTime(hydraulicBoundaryLocation.Name);
+
+            var validationProblem = HydraulicDatabaseHelper.ValidatePathForCalculation(hydraulicBoundaryDatabase.FilePath);
+            var hasErrors = string.IsNullOrEmpty(validationProblem);
+
+            if (!hasErrors)
+            {
+                CalculationServiceHelper.LogMessagesAsError(RingtoetsCommonFormsResources.Hydraulic_boundary_database_connection_failed_0_,
+                                                            validationProblem);
+            }
+
+            CalculationServiceHelper.LogValidationEndTime(hydraulicBoundaryLocation.Name);
+
+            return hasErrors;
+        }
+
+        internal static TargetProbabilityCalculationOutput Calculate(IAssessmentSection assessmentSection, HydraulicBoundaryDatabase hydraulicBoundaryDatabase,
+                                                                     HydraulicBoundaryLocation hydraulicBoundaryLocation, string ringId)
+        {
+            var hlcdDirectory = Path.GetDirectoryName(hydraulicBoundaryDatabase.FilePath);
+            var input = CreateInput(assessmentSection, hydraulicBoundaryLocation);
+            var targetProbabilityCalculationParser = new TargetProbabilityCalculationParser();
+
+            CalculationServiceHelper.PerformCalculation(
+                hydraulicBoundaryLocation.Name,
+                () =>
+                {
+                    HydraRingCalculationService.PerformCalculation(
+                        hlcdDirectory,
+                        ringId,
+                        HydraRingTimeIntegrationSchemeType.FerryBorgesCastanheta,
+                        HydraRingUncertaintiesType.All,
+                        input,
+                        new[]
+                        {
+                            targetProbabilityCalculationParser
+                        });
+
+                    VerifyOutput(targetProbabilityCalculationParser.Output, hydraulicBoundaryLocation.Name);
+                });
+
+            return targetProbabilityCalculationParser.Output;
+        }
+
+        private static void VerifyOutput(TargetProbabilityCalculationOutput output, string name)
+        {
+            if (output == null)
+            {
+                log.ErrorFormat(Resources.WaveHeightCalculationService_Calculate_Error_in_wave_height_0_calculation, name);
+            }
+        }
+
+        private static WaveHeightCalculationInput CreateInput(IAssessmentSection assessmentSection, HydraulicBoundaryLocation hydraulicBoundaryLocation)
+        {
+            return new WaveHeightCalculationInput(1, hydraulicBoundaryLocation.Id, assessmentSection.FailureMechanismContribution.Norm);
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Service.Test/WaveHeightCalculationServiceTest.cs
@@ -0,0 +1,157 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.IO;
+using System.Linq;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.HydraRing.Calculation.Data.Output;
+using Ringtoets.HydraRing.Data;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.Plugin.FileImporters;
+
+namespace Ringtoets.Integration.Service.Test
+{
+    public class WaveHeightCalculationServiceTest
+    {
+        private readonly string testDataPath = TestHelper.GetTestDataPath(TestDataPath.Ringtoets.Integration.Service, "HydraRingCalculation");
+
+        [Test]
+        public void Validate_ValidHydraulicBoundaryDatabase_ReturnsTrue()
+        {
+            // Setup
+            var assessmentSection = new AssessmentSection(AssessmentSectionComposition.Dike);
+            ImportHydraulicBoundaryDatabase(assessmentSection);
+
+            bool valid = false;
+            var hydraulicBoundaryLocation = new HydraulicBoundaryLocation(1, "test", 1, 1);
+
+            // Call
+            Action call = () => valid = WaveHeightCalculationService.Validate(assessmentSection.HydraulicBoundaryDatabase, hydraulicBoundaryLocation);
+
+            // Assert
+            TestHelper.AssertLogMessages(call, messages =>
+            {
+                var msgs = messages.ToArray();
+                Assert.AreEqual(2, msgs.Length);
+                StringAssert.StartsWith(string.Format("Validatie van '{0}' gestart om: ", hydraulicBoundaryLocation.Name), msgs[0]);
+                StringAssert.StartsWith(string.Format("Validatie van '{0}' beëindigd om: ", hydraulicBoundaryLocation.Name), msgs[1]);
+            });
+            Assert.IsTrue(valid);
+        }
+
+        [Test]
+        public void Validate_InvalidHydraulicBoundaryDatabase_LogsErrorAndReturnsFalse()
+        {
+            // Setup
+            var hydraulicBoundaryDatabase = new HydraulicBoundaryDatabase
+            {
+                FilePath = Path.Combine(testDataPath, "notexisting.sqlite")
+            };
+
+            bool valid = false;
+            var hydraulicBoundaryLocation = new HydraulicBoundaryLocation(1, "test", 1, 1);
+
+            // Call
+            Action call = () => valid = WaveHeightCalculationService.Validate(hydraulicBoundaryDatabase, hydraulicBoundaryLocation);
+
+            // Assert
+            TestHelper.AssertLogMessages(call, messages =>
+            {
+                var msgs = messages.ToArray();
+                Assert.AreEqual(3, msgs.Length);
+                StringAssert.StartsWith(string.Format("Validatie van '{0}' gestart om: ", hydraulicBoundaryLocation.Name), msgs[0]);
+                StringAssert.StartsWith("Herstellen van de verbinding met de hydraulische randvoorwaardendatabase is mislukt. Fout bij het lezen van bestand", msgs[1]);
+                StringAssert.StartsWith(string.Format("Validatie van '{0}' beëindigd om: ", hydraulicBoundaryLocation.Name), msgs[2]);
+            });
+            Assert.IsFalse(valid);
+        }
+
+        [Test]
+        public void CalculateWaveHeight_ValidHydraulicBoundaryDatabaseAndLocation_LogStartAndEndAndReturnOutput()
+        {
+            // Setup
+            var assessmentSection = new AssessmentSection(AssessmentSectionComposition.Dike);
+            ImportHydraulicBoundaryDatabase(assessmentSection);
+
+            var hydraulicBoundaryLocation = assessmentSection.HydraulicBoundaryDatabase.Locations.First(hl => hl.Id == 1300001);
+
+            TargetProbabilityCalculationOutput output = null;
+
+            // Call
+            Action call = () => output = WaveHeightCalculationService.Calculate(assessmentSection,
+                                                                                      assessmentSection.HydraulicBoundaryDatabase,
+                                                                                      hydraulicBoundaryLocation,
+                                                                                      assessmentSection.Name);
+
+            // Assert
+            TestHelper.AssertLogMessages(call, messages =>
+            {
+                var msgs = messages.ToArray();
+                Assert.AreEqual(2, msgs.Length);
+                StringAssert.StartsWith(string.Format("Berekening van '{0}' gestart om: ", hydraulicBoundaryLocation.Name), msgs[0]);
+                StringAssert.StartsWith(string.Format("Berekening van '{0}' beëindigd om: ", hydraulicBoundaryLocation.Name), msgs[1]);
+            });
+            Assert.IsNotNull(output);
+        }
+
+        [Test]
+        public void CalculateWaveHeight_ValidHydraulicBoundaryDatabaseInvalidHydraulicBoundaryLocation_LogStartAndEndAndErrorMessageAndReturnNull()
+        {
+            // Setup
+            var assessmentSection = new AssessmentSection(AssessmentSectionComposition.Dike);
+            ImportHydraulicBoundaryDatabase(assessmentSection);
+
+            var hydraulicBoundaryLocation = new HydraulicBoundaryLocation(1, "test", 1, 1);
+
+            TargetProbabilityCalculationOutput output = null;
+
+            // Call
+            Action call = () => output = WaveHeightCalculationService.Calculate(assessmentSection,
+                                                                                      assessmentSection.HydraulicBoundaryDatabase,
+                                                                                      hydraulicBoundaryLocation,
+                                                                                      assessmentSection.Name);
+
+            // Assert
+            TestHelper.AssertLogMessages(call, messages =>
+            {
+                var msgs = messages.ToArray();
+                Assert.AreEqual(3, msgs.Length);
+                StringAssert.StartsWith(string.Format("Berekening van '{0}' gestart om: ", hydraulicBoundaryLocation.Name), msgs[0]);
+                StringAssert.StartsWith(string.Format("Er is een fout opgetreden tijdens de golfhoogte berekening '{0}': inspecteer het logbestand.", hydraulicBoundaryLocation.Name), msgs[1]);
+                StringAssert.StartsWith(string.Format("Berekening van '{0}' beëindigd om: ", hydraulicBoundaryLocation.Name), msgs[2]);
+            });
+            Assert.IsNull(output);
+        }
+
+        private void ImportHydraulicBoundaryDatabase(AssessmentSection assessmentSection)
+        {
+            string validFilePath = Path.Combine(testDataPath, "HRD dutch coast south.sqlite");
+
+            using (var importer = new HydraulicBoundaryDatabaseImporter())
+            {
+                importer.Import(assessmentSection, validFilePath);
+            }
+        } 
+    }
+}
