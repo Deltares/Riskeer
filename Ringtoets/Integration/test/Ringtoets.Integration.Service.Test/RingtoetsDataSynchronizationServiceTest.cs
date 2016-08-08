d3aad49804a55bfc90bf245cad332370777226ea
Robert Tangerman <robert.tangerman@deltares.nl>
2016-08-08 10:58:26 +0000

    Implemented new RingtoetsDataSynchronizationService which clears the HydraulicBoundaryLocation output. Related to Issue [WTI-441]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@4548 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 89c21696cc43ac7e648ee5c7004f9abacbda05aa
    Former-commit-id: 4bf53d74674fbc20824887a0b5b90989e0e769cb

---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Service.Test/RingtoetsDataSynchronizationServiceTest.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.HydraRing.Data;
+using Ringtoets.Integration.Data;
+
+namespace Ringtoets.Integration.Service.Test
+{
+    [TestFixture]
+    public class RingtoetsDataSynchronizationServiceTest
+    {
+        [Test]
+        public void ClearAssessmentSectionData_WithoutAssessmentSection_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate test = () => RingtoetsDataSynchronizationService.ClearAssessmentSectionData(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(test);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
+        public void ClearAssessmentSectionData_WithAssessmentSection_ClearsAllData()
+        {
+            // Setup
+            AssessmentSection assessmentSection = new AssessmentSection(AssessmentSectionComposition.Dike)
+            {
+                HydraulicBoundaryDatabase = new HydraulicBoundaryDatabase()
+            };
+
+            assessmentSection.HydraulicBoundaryDatabase.Locations.Add(new HydraulicBoundaryLocation(1, "test", 0.0, 0.0)
+            {
+                WaveHeight = 3.0,
+                DesignWaterLevel = 4.2
+            });
+
+            // Call
+            RingtoetsDataSynchronizationService.ClearAssessmentSectionData(assessmentSection);
+
+            // Assert
+            HydraulicBoundaryLocation location = assessmentSection.HydraulicBoundaryDatabase.Locations[0];
+            Assert.IsNaN(location.WaveHeight);
+            Assert.IsNaN(location.DesignWaterLevel);
+        }
+    }
+}
