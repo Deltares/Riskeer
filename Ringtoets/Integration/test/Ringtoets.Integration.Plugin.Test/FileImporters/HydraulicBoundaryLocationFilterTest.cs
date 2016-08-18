3d53c0f0b1a88c65b24a20f0add0ead3d796f0ca
bouvrie <bouvrie@example.com>
2016-08-18 14:53:11 +0000

    Merged revision(s) 4827 from branches/Release/16_1_1: * Introduced HydraulicBoundaryLocationFilter ** HydraulicBoundaryDatabaseImporter now uses this filter to determine if certain locations should be ignored. Related to Issue [WTI-785]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@4831 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 27a0cdd7200dcadd51255352aba5e268a62ce7f3
    Former-commit-id: 4749959c2f6b78cda35835ef79bf42e2fdc10df5

---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Plugin.Test/FileImporters/HydraulicBoundaryLocationFilterTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using Ringtoets.Integration.Plugin.FileImporters;
+
+namespace Ringtoets.Integration.Plugin.Test.FileImporters
+{
+    [TestFixture]
+    public class HydraulicBoundaryLocationFilterTest
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(long.MinValue)]
+        [TestCase(303192)]
+        [TestCase(303194)]
+        [TestCase(603074)]
+        [TestCase(603076)]
+        [TestCase(long.MaxValue)]
+        public void ShouldInclude_NameNotInFilterSet_ReturnsTrue(long id)
+        {
+            // Setup
+            var filter = new HydraulicBoundaryLocationFilter();
+
+            // Call
+            bool shouldBeIncluded = filter.ShouldInclude(id);
+
+            // Assert
+            Assert.IsTrue(shouldBeIncluded);
+        }
+
+        [Test]
+        [TestCase(303193)]
+        [TestCase(603075)]
+        public void ShouldInclude_NameInFilterSet_ReturnsFalse(long id)
+        {
+            // Setup
+            var filter = new HydraulicBoundaryLocationFilter();
+
+            // Call
+            bool shouldBeIncluded = filter.ShouldInclude(id);
+
+            // Assert
+            Assert.IsFalse(shouldBeIncluded);
+        }
+    }
+}
