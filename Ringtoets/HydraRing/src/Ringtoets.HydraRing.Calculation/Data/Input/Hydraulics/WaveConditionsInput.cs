4142f56ada4f19e2fc3a0e828042bec111c1cecd
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-08-23 14:29:44 +0000

    Introduced WaveConditionsInput Related to Issue [WTI-725]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@4920 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 7297d892f56e444e9b0ce71bd0df6ff84fd7152a
    Former-commit-id: 97ada279117d7666980ea9c21c87526368e04cbe

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.Calculation/Data/Input/Hydraulics/WaveConditionsInput.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Ringtoets.HydraRing.Calculation.Data.Input.Hydraulics
+{
+    /// <summary>
+    /// Container of all data necessary for performing a wave conditions calculation (Q-variant) via Hydra-Ring.
+    /// </summary>
+    public class WaveConditionsInput : HydraRingCalculationInput
+    {
+        private readonly HydraRingSection section;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="WaveConditionsInput"/> class.
+        /// </summary>
+        /// <param name="sectionId">The id of the section to use during the calculation.</param>
+        /// <param name="hydraulicBoundaryLocationId">The id of the hydraulic station to use during the calculation.</param>
+        public WaveConditionsInput(int sectionId, long hydraulicBoundaryLocationId) : base(hydraulicBoundaryLocationId)
+        {
+            section = new HydraRingSection(sectionId, double.NaN, double.NaN);
+        }
+
+        public override HydraRingFailureMechanismType FailureMechanismType
+        {
+            get
+            {
+                return HydraRingFailureMechanismType.QVariant;
+            }
+        }
+
+        public override int CalculationTypeId
+        {
+            get
+            {
+                return 6;
+            }
+        }
+
+        public override int VariableId
+        {
+            get
+            {
+                return 114;
+            }
+        }
+
+        public override HydraRingSection Section
+        {
+            get
+            {
+                return section;
+            }
+        }
+    }
+}
---
+++ Ringtoets/HydraRing/test/Ringtoets.HydraRing.Calculation.Test/Data/Input/Hydraulics/WaveConditionsInputTest.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+using Ringtoets.HydraRing.Calculation.Data;
+using Ringtoets.HydraRing.Calculation.Data.Input;
+using Ringtoets.HydraRing.Calculation.Data.Input.Hydraulics;
+
+namespace Ringtoets.HydraRing.Calculation.Test.Data.Input.Hydraulics
+{
+    [TestFixture]
+    public class WaveConditionsInputTest
+    {
+        [Test]
+        public void Constructor_Always_ExpectedValues()
+        {
+            // Setup
+            const int sectionId = 2;
+            const int hydraulicBoundaryLocationId = 1000;
+
+            // Call
+            var waveConditionsInput = new WaveConditionsInput(sectionId, hydraulicBoundaryLocationId);
+
+            // Assert
+            const int expectedCalculationTypeId = 6;
+            const int expectedVariableId = 114;
+            Assert.IsInstanceOf<HydraRingCalculationInput>(waveConditionsInput);
+            Assert.AreEqual(HydraRingFailureMechanismType.QVariant, waveConditionsInput.FailureMechanismType);
+            Assert.AreEqual(expectedCalculationTypeId, waveConditionsInput.CalculationTypeId);
+            Assert.AreEqual(expectedVariableId, waveConditionsInput.VariableId);
+            Assert.AreEqual(hydraulicBoundaryLocationId, waveConditionsInput.HydraulicBoundaryLocationId);
+            Assert.IsNotNull(waveConditionsInput.Section);
+            Assert.AreEqual(sectionId, waveConditionsInput.Section.SectionId);
+        }
+    }
+}
