48e1ce89dde79fee454de1157cf61b1c5525506a
Robert Tangerman <robert.tangerman@deltares.nl>
2016-08-25 08:01:36 +0000

    Implemented input class for wave conditions. Related to Issue [WTI-723]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@4982 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: fd2c642d429be2bd574ab68b49254c06ccbbaeae
    Former-commit-id: cdd945081aa0b7324f9643f8aee551956b87f6a4

---
+++ Ringtoets/Revetment/src/Ringtoets.Revetment.Data/WaveConditionsInput.cs
@@ -0,0 +1,117 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Linq;
+using Core.Common.Base.Data;
+using Core.Common.Base.Geometry;
+using Ringtoets.Common.Data.DikeProfiles;
+using Ringtoets.HydraRing.Data;
+
+namespace Ringtoets.Revetment.Data
+{
+    /// <summary>
+    /// Class that holds all wave conditions calculation specific input paramaters.
+    /// </summary>
+    public class WaveConditionsInput
+    {
+        private DikeProfile dikeProfile;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="WaveConditionsInput"/>.
+        /// </summary>
+        public WaveConditionsInput()
+        {
+            UpdateProfileParameters();
+        }
+
+        /// <summary>
+        /// Gets or set the dike profile.
+        /// </summary>
+        public DikeProfile DikeProfile
+        {
+            get
+            {
+                return dikeProfile;
+            }
+            set
+            {
+                dikeProfile = value;
+                UpdateProfileParameters();
+            }
+        }
+
+        /// <summary>
+        /// Gets or set the hydraulic boundary location from which to use the assessment level.
+        /// </summary>
+        public HydraulicBoundaryLocation HydraulicBoundaryLocation { get; set; }
+
+        /// <summary>
+        /// Gets or sets if <see cref="BreakWater"/> needs to be taken into account.
+        /// </summary>
+        public bool UseBreakWater { get; set; }
+
+        /// <summary>
+        /// Gets the <see cref="BreakWater"/>.
+        /// </summary>
+        public BreakWater BreakWater { get; private set; }
+
+        /// <summary>
+        /// Gets or sets if the <see cref="ForeshoreGeometry"/> needs to be taken into account.
+        /// </summary>
+        public bool UseForeshore { get; set; }
+
+        /// <summary>
+        /// Gets the geometry of the foreshore.
+        /// </summary>
+        public RoundedPoint2DCollection ForeshoreGeometry
+        {
+            get
+            {
+                return dikeProfile != null
+                           ? dikeProfile.ForeshoreGeometry
+                           : new RoundedPoint2DCollection(2, Enumerable.Empty<Point2D>());
+            }
+        }
+
+        private void UpdateProfileParameters()
+        {
+            if (dikeProfile == null)
+            {
+                UseForeshore = false;
+                UseBreakWater = false;
+                BreakWater = GetDefaultBreakWater();
+            }
+            else
+            {
+                UseForeshore = dikeProfile.ForeshoreGeometry.Count() > 1;
+                UseBreakWater = dikeProfile.HasBreakWater;
+                BreakWater = dikeProfile.HasBreakWater
+                                 ? new BreakWater(dikeProfile.BreakWater.Type, dikeProfile.BreakWater.Height)
+                                 : GetDefaultBreakWater();
+            }
+        }
+
+        private static BreakWater GetDefaultBreakWater()
+        {
+            return new BreakWater(BreakWaterType.Dam, 0.0);
+        }
+    }
+}
---
+++ Ringtoets/Revetment/test/Ringtoets.Revetment.Data.Test/WaveConditionsInputTest.cs
@@ -0,0 +1,158 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+using Core.Common.Base.Data;
+using Core.Common.Base.Geometry;
+using NUnit.Framework;
+using Ringtoets.Common.Data.DikeProfiles;
+using Ringtoets.HydraRing.Data;
+
+namespace Ringtoets.Revetment.Data.Test
+{
+    [TestFixture]
+    public class WaveConditionsInputTest
+    {
+        [Test]
+		public void Constructor_ExpectedValues()
+		{
+			// Call
+			var input = new WaveConditionsInput();
+					
+			// Assert
+            Assert.IsNull(input.HydraulicBoundaryLocation);
+			Assert.IsNull(input.DikeProfile);
+            Assert.IsFalse(input.UseBreakWater);
+            Assert.AreEqual(BreakWaterType.Dam, input.BreakWater.Type);
+            Assert.AreEqual(new RoundedDouble(2), input.BreakWater.Height);
+            Assert.IsFalse(input.UseForeshore);
+            CollectionAssert.IsEmpty(input.ForeshoreGeometry);
+		}
+
+        [Test]
+        [Combinatorial]
+        public void DikeProfile_SetNewValue_InputSyncedAccordingly(
+            [Values(true, false)] bool withBreakWater,
+            [Values(true, false)] bool withValidForeshore)
+        {
+            // Setup
+            var input = new WaveConditionsInput();
+            BreakWaterType originalBreakWaterType = input.BreakWater.Type;
+            RoundedDouble originalBreakWaterHeight = input.BreakWater.Height;
+            HydraulicBoundaryLocation originalHydraulicBoundaryLocation = input.HydraulicBoundaryLocation;
+
+            var foreShoreGeometry = new List<Point2D>
+            {
+                new Point2D(2.2, 3.3)
+            };
+
+            if (withValidForeshore)
+            {
+                foreShoreGeometry.Add(new Point2D(4.4, 5.5));
+            }
+
+            BreakWater breakWater = null;
+            if (withBreakWater)
+            {
+                var nonDefaultBreakWaterType = BreakWaterType.Wall;
+                var nonDefaultBreakWaterHeight = 5.5;
+
+                // Precondition
+                Assert.AreNotEqual(nonDefaultBreakWaterType, input.BreakWater.Type);
+                Assert.AreNotEqual(nonDefaultBreakWaterHeight, input.BreakWater.Height);
+
+                breakWater = new BreakWater(nonDefaultBreakWaterType, nonDefaultBreakWaterHeight);
+            }
+
+            var dikeProfile = new DikeProfile(new Point2D(0, 0),
+                                              new[]
+                                              {
+                                                  new RoughnessPoint(new Point2D(6.6, 7.7), 0.8)
+                                              }, foreShoreGeometry.ToArray(), breakWater,
+                                              new DikeProfile.ConstructionProperties
+                                              {
+                                                  Orientation = 1.1,
+                                                  DikeHeight = 4.4
+                                              });
+
+            // Call
+            input.DikeProfile = dikeProfile;
+
+            // Assert
+            Assert.AreSame(dikeProfile, input.DikeProfile);
+            Assert.AreEqual(withBreakWater, input.UseBreakWater);
+            Assert.AreEqual(withBreakWater ? dikeProfile.BreakWater.Type : originalBreakWaterType, input.BreakWater.Type);
+            Assert.AreEqual(withBreakWater ? dikeProfile.BreakWater.Height : originalBreakWaterHeight, input.BreakWater.Height);
+            Assert.AreEqual(withValidForeshore, input.UseForeshore);
+            CollectionAssert.AreEqual(dikeProfile.ForeshoreGeometry, input.ForeshoreGeometry);
+            Assert.AreEqual(originalHydraulicBoundaryLocation, input.HydraulicBoundaryLocation);
+        }
+
+        [Test]
+        [Combinatorial]
+        public void DikeProfile_SetNullValue_InputSyncedToDefaults()
+        {
+            // Setup
+            var input = new WaveConditionsInput();
+            BreakWaterType originalBreakWaterType = input.BreakWater.Type;
+            RoundedDouble originalBreakWaterHeight = input.BreakWater.Height;
+            HydraulicBoundaryLocation originalHydraulicBoundaryLocation = input.HydraulicBoundaryLocation;
+
+            var dikeProfile = new DikeProfile(new Point2D(0, 0),
+                                              new[]
+                                              {
+                                                  new RoughnessPoint(new Point2D(7.7, 8.8), 0.6)
+                                              }, new[]
+                                              {
+                                                  new Point2D(3.3, 4.4),
+                                                  new Point2D(5.5, 6.6)
+                                              },
+                                              new BreakWater(BreakWaterType.Caisson, 2.2),
+                                              new DikeProfile.ConstructionProperties
+                                              {
+                                                  Orientation = 1.1,
+                                                  DikeHeight = 9.9
+                                              });
+
+            input.DikeProfile = dikeProfile;
+
+            // Precondition
+            Assert.AreSame(dikeProfile, input.DikeProfile);
+            Assert.IsTrue(input.UseBreakWater);
+            Assert.AreNotEqual(originalBreakWaterType, input.BreakWater.Type);
+            Assert.AreNotEqual(originalBreakWaterHeight, input.BreakWater.Height);
+            Assert.IsTrue(input.UseForeshore);
+            CollectionAssert.IsNotEmpty(input.ForeshoreGeometry);
+            Assert.AreEqual(originalHydraulicBoundaryLocation, input.HydraulicBoundaryLocation);
+
+            // Call
+            input.DikeProfile = null;
+
+            // Assert
+            Assert.IsFalse(input.UseBreakWater);
+            Assert.AreEqual(originalBreakWaterType, input.BreakWater.Type);
+            Assert.AreEqual(originalBreakWaterHeight, input.BreakWater.Height);
+            Assert.IsFalse(input.UseForeshore);
+            CollectionAssert.IsEmpty(input.ForeshoreGeometry);
+            Assert.AreEqual(originalHydraulicBoundaryLocation, input.HydraulicBoundaryLocation);
+        }
+    }
+}
