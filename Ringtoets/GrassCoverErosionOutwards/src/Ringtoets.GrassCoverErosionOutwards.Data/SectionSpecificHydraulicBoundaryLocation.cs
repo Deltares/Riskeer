75b6f60a2d13f7a3a57c17356513f2c66ee3a759
Marten Janssen <marten.janssen@deltares.nl>
2016-08-26 08:47:56 +0000

    Added SectionSpecificHydraulicBoundaryLocation for GCEO Related to Issue [WTI-708]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5000 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: cd5d6436991075d026f8e49a7ed21d7d7300366b
    Former-commit-id: e26a617aa5c907e31eb23b88da3d99a0cbc8680f

---
+++ Ringtoets/GrassCoverErosionOutwards/src/Ringtoets.GrassCoverErosionOutwards.Data/SectionSpecificHydraulicBoundaryLocation.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.Data;
+using Ringtoets.HydraRing.Data;
+
+namespace Ringtoets.GrassCoverErosionOutwards.Data
+{
+    /// <summary>
+    /// Section specific hydraulic boundary location.
+    /// </summary>
+    public class SectionSpecificHydraulicBoundaryLocation
+    {
+        private RoundedDouble sectionSpecificWaterLevel;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="SectionSpecificHydraulicBoundaryLocation"/>.
+        /// </summary>
+        /// <param name="hydraulicBoundaryLocation">The <see cref="Ringtoets.HydraRing.Data.HydraulicBoundaryLocation"/> 
+        /// this specific water level applies to.</param>
+        public SectionSpecificHydraulicBoundaryLocation(HydraulicBoundaryLocation hydraulicBoundaryLocation)
+        {
+            if (hydraulicBoundaryLocation == null)
+            {
+                throw new ArgumentNullException("hydraulicBoundaryLocation");
+            }
+            HydraulicBoundaryLocation = hydraulicBoundaryLocation;
+            sectionSpecificWaterLevel = new RoundedDouble(2, double.NaN);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Ringtoets.HydraRing.Data.HydraulicBoundaryLocation"/>.
+        /// </summary>
+        public HydraulicBoundaryLocation HydraulicBoundaryLocation { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the section specific water level of <see cref="Ringtoets.HydraRing.Data.HydraulicBoundaryLocation"/>.
+        /// </summary>
+        public RoundedDouble SectionSpecificWaterLevel
+        {
+            get
+            {
+                return sectionSpecificWaterLevel;
+            }
+            set
+            {
+                sectionSpecificWaterLevel = value.ToPrecision(sectionSpecificWaterLevel.NumberOfDecimalPlaces);
+            }
+        }
+    }
+}
---
+++ Ringtoets/GrassCoverErosionOutwards/test/Ringtoets.GrassCoverErosionOutwards.Data.Test/SectionSpecificHydraulicBoundaryLocationTest.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.Data;
+using NUnit.Framework;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.HydraRing.Data;
+
+namespace Ringtoets.GrassCoverErosionOutwards.Data.Test
+{
+    [TestFixture]
+    public class SectionSpecificHydraulicBoundaryLocationTest
+    {
+        [Test]
+        public void Constructor_NullHydraulicBoundaryLocation_ThrowsArgumentNullException()
+        {
+            // Setup
+
+            // Call
+            TestDelegate test = () => new SectionSpecificHydraulicBoundaryLocation(null);
+
+            // Assert
+            var paramName = Assert.Throws<ArgumentNullException>(test).ParamName;
+            Assert.AreEqual("hydraulicBoundaryLocation", paramName);
+        }
+
+        [Test]
+        public void Constructor_ValidArguments_PropertiesAsExpected()
+        {
+            // Setup
+            var hydraulicBoundaryLocation = new HydraulicBoundaryLocation(1, "name", 0.0, 0.0);
+
+            // Call
+            var specificHydraulicBoundaryLocation = new SectionSpecificHydraulicBoundaryLocation(hydraulicBoundaryLocation);
+
+            // Assert
+            Assert.AreSame(hydraulicBoundaryLocation, specificHydraulicBoundaryLocation.HydraulicBoundaryLocation);
+            Assert.IsNaN(specificHydraulicBoundaryLocation.SectionSpecificWaterLevel);
+            Assert.AreEqual(2, specificHydraulicBoundaryLocation.SectionSpecificWaterLevel.NumberOfDecimalPlaces);
+        }
+
+        [Test]
+        public void SetProperties_ValidParameters_ReturnsExpectedValues()
+        {
+            // Setup
+            var hydraulicBoundaryLocation = new HydraulicBoundaryLocation(1, "name", 0.0, 0.0);
+            var specificHydraulicBoundaryLocation = new SectionSpecificHydraulicBoundaryLocation(hydraulicBoundaryLocation);
+
+            // Call
+            var sectionSpecificWaterLevel = (RoundedDouble) 1.23456;
+            specificHydraulicBoundaryLocation.SectionSpecificWaterLevel = sectionSpecificWaterLevel;
+
+            // Assert
+            Assert.AreEqual(sectionSpecificWaterLevel, specificHydraulicBoundaryLocation.SectionSpecificWaterLevel, 
+                specificHydraulicBoundaryLocation.SectionSpecificWaterLevel.GetAccuracy());
+        }
+    }
+}
