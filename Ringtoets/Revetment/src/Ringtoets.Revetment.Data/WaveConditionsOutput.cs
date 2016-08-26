8248ba08dc31f223fcc30cf4626d8721e6e02f7a
Robert Tangerman <robert.tangerman@deltares.nl>
2016-08-26 11:50:25 +0000

    Introduced output class for wave conditions calculations. Related to Issue [WTI-723]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5015 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 31d7cbd7ad0f99d8c8331154ca003160864c3974
    Former-commit-id: fd55f4da5e3730a4ee9ab3897be2ef6ced3b0b0a

---
+++ Ringtoets/Revetment/src/Ringtoets.Revetment.Data/WaveConditionsOutput.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Base;
+using Ringtoets.Common.Data.Calculation;
+
+namespace Ringtoets.Revetment.Data
+{
+    /// <summary>
+    /// The result of the wave conditions assessment.
+    /// </summary>
+    public class WaveConditionsOutput : Observable, ICalculationOutput
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="WaveConditionsOutput"/>.
+        /// </summary>
+        /// <param name="waterLevel">The water level which has been calculated.</param>
+        /// <param name="waveHeight">The calculated wave height.</param>
+        /// <param name="wavePeriod">The calculated wave period.</param>
+        /// <param name="waveAngle">The calculated wave angle.</param>
+        public WaveConditionsOutput(double waterLevel, double waveHeight, double wavePeriod, double waveAngle)
+        {
+            WaterLevel = waterLevel;
+            WaveHeight = waveHeight;
+            WavePeriod = wavePeriod;
+            WaveAngle = waveAngle;
+        }
+
+        /// <summary>
+        /// Gets the water level which has been calculated.
+        /// </summary>
+        public double WaterLevel { get; private set; }
+
+        /// <summary>
+        /// Gets the calculated wave height.
+        /// </summary>
+        public double WaveHeight { get; private set; }
+
+        /// <summary>
+        /// Gets the calculated wave period.
+        /// </summary>
+        public double WavePeriod { get; private set; }
+
+        /// <summary>
+        /// Gets the calculated wave angle.
+        /// </summary>
+        public double WaveAngle { get; private set; }
+    }
+}
