2876f9193f8ea4f2bf130601553d4f8eace0580c
Robert Tangerman <robert.tangerman@deltares.nl>
2016-09-01 15:11:42 +0000

    - Implemented calculation object and output object for grass cover erosion outwards.
    Related to Issue [WTI-811]
    
    
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5168 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 9781520b514183423ef95ba40d541fc29b546140
    Former-commit-id: dbaf6064ce79ee9acb49c94e7d2048fe2f6e1c30

---
+++ Ringtoets/GrassCoverErosionOutwards/src/Ringtoets.GrassCoverErosionOutwards.Data/GrassCoverErosionOutwardsWaveConditionsCalculation.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Base;
+using Ringtoets.Common.Data.Calculation;
+using Ringtoets.GrassCoverErosionOutwards.Data.Properties;
+using Ringtoets.Revetment.Data;
+
+namespace Ringtoets.GrassCoverErosionOutwards.Data
+{
+    /// <summary>
+    /// Class holding information about a wave conditions calculation for the <see cref="GrassCoverErosionOutwardsFailureMechanism"/>.
+    /// </summary>
+    public class GrassCoverErosionOutwardsWaveConditionsCalculation : Observable, ICalculation
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="GrassCoverErosionOutwardsWaveConditionsCalculation"/>.
+        /// </summary>
+        public GrassCoverErosionOutwardsWaveConditionsCalculation()
+        {
+            Name = Resources.GrassCoverErosionOutwardsWaveConditionsCalculation_DefaultName;
+            InputParameters = new WaveConditionsInput();
+        }
+
+        /// <summary>
+        /// Gets the input parameters to perform a wave conditions calculation with.
+        /// </summary>
+        public WaveConditionsInput InputParameters { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the output which contains the results of a wave conditions calculation.
+        /// </summary>
+        public GrassCoverErosionOutwardsWaveConditionsOutput Output { get; set; }
+
+        public string Name { get; set; }
+
+        public string Comments { get; set; }
+
+        public bool HasOutput
+        {
+            get
+            {
+                return Output != null;
+            }
+        }
+
+        public void ClearOutput()
+        {
+            Output = null;
+        }
+
+        public ICalculationInput GetObservableInput()
+        {
+            return InputParameters;
+        }
+
+        public ICalculationOutput GetObservableOutput()
+        {
+            return Output;
+        }
+    }
+}
