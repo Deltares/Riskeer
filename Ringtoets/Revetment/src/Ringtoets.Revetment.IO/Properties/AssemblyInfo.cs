d87124c5da4427f504d613ce58e017ec0987f9e8
bos_en <bos_en@example.com>
2016-09-01 12:01:59 +0000

    Added project Ringtoets.Revetment.IO to house shared WaveConditionsWriter. Related to Issue [WTI-728]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5157 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 18dac5a68e9c2ce08b31a5cb6a4830b5a4078984
    Former-commit-id: 6f33cc729301f9b674c3876cc4a544b6961ef509

---
+++ Ringtoets/Revetment/src/Ringtoets.Revetment.IO/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ringtoets.Revetment.IO")]
+[assembly: AssemblyProduct("Ringtoets.Revetment.IO")]
+[assembly: Guid("c344f779-1341-47eb-acf6-e87560c57132")]
---
+++ Ringtoets/Revetment/test/Ringtoets.Revetment.IO.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ringtoets.Revetment.IO.Test")]
+[assembly: AssemblyProduct("Ringtoets.Revetment.IO.Test")]
+[assembly: Guid("b58beeba-17a4-47b6-a131-a3f53b1c717d")]
