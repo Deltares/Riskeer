85e95a2b6e8fd7a873ba166ddfba608f2466a40c
Robert Tangerman <robert.tangerman@deltares.nl>
2016-09-05 11:38:34 +0000

    Added new  activity for performing HydraRing wave conditions calculations for grass cover erosion outwards. Related to Issue [WTI-812]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5207 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 16f2ab725ce77a673d749fae9d2ae7c54a64502b
    Former-commit-id: fd029fac5d92e5005100cf4f89da928ea31af1a1

---
+++ Ringtoets/GrassCoverErosionOutwards/src/Ringtoets.GrassCoverErosionOutwards.Service/GrassCoverErosionOutwardsWaveConditionsCalculationActivity.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Core.Common.Base.Service;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.GrassCoverErosionOutwards.Data;
+using Ringtoets.HydraRing.Calculation.Activities;
+using Ringtoets.Revetment.Data;
+
+namespace Ringtoets.GrassCoverErosionOutwards.Service
+{
+    /// <summary>
+    /// <see cref="Activity"/> for running a grass cover erosion outwards wave conditions calculation.
+    /// </summary>
+    public class GrassCoverErosionOutwardsWaveConditionsCalculationActivity : HydraRingActivity<List<WaveConditionsOutput>>
+    {
+        private readonly GrassCoverErosionOutwardsWaveConditionsCalculation calculation;
+        private readonly string hlcdDirectory;
+        private readonly GrassCoverErosionOutwardsFailureMechanism failureMechanism;
+        private readonly IAssessmentSection assessmentSection;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="GrassCoverErosionOutwardsWaveConditionsCalculationActivity"/>.
+        /// </summary>
+        /// <param name="calculation">The grass cover erosion outwards wave conditions data used for the calculation.</param>
+        /// <param name="hlcdDirectory">The directory of the HLCD file that should be used for performing the calculation.</param>
+        /// <param name="failureMechanism">The failure mechanism the calculation belongs to.</param>
+        /// <param name="assessmentSection">The assessment section the calculation belongs to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any input argument is <c>null</c>.</exception>
+        public GrassCoverErosionOutwardsWaveConditionsCalculationActivity(GrassCoverErosionOutwardsWaveConditionsCalculation calculation,
+                                                                          string hlcdDirectory,
+                                                                          GrassCoverErosionOutwardsFailureMechanism failureMechanism,
+                                                                          IAssessmentSection assessmentSection)
+        {
+            if (calculation == null)
+            {
+                throw new ArgumentNullException("calculation");
+            }
+            if (hlcdDirectory == null)
+            {
+                throw new ArgumentNullException("hlcdDirectory");
+            }
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException("failureMechanism");
+            }
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException("assessmentSection");
+            }
+
+            this.calculation = calculation;
+            this.hlcdDirectory = hlcdDirectory;
+            this.failureMechanism = failureMechanism;
+            this.assessmentSection = assessmentSection;
+
+            Name = calculation.Name;
+        }
+
+        protected override void OnRun()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnFinish()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
