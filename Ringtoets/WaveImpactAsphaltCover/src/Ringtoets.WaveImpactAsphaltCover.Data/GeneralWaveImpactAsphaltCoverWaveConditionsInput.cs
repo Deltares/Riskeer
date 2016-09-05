dba066791390470e63d15dd1dcef3ec4d4587c03
Robert Tangerman <robert.tangerman@deltares.nl>
2016-09-05 07:32:31 +0000

    Added general wave conditions input for wave impact asphalt cover. Related to Issue [WTI-818]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5189 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 2d6360e23e357c2c5fa29770b6736d2bd4d70b0b
    Former-commit-id: ec1291f969b789e21e7c2832f61fe25d70083deb

---
+++ Ringtoets/WaveImpactAsphaltCover/src/Ringtoets.WaveImpactAsphaltCover.Data/GeneralWaveImpactAsphaltCoverWaveConditionsInput.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Ringtoets.WaveImpactAsphaltCover.Data
+{
+    /// <summary>
+    /// Class that holds all the static wave impact asphalt cover wave conditions input parameters.
+    /// </summary>
+    public class GeneralWaveImpactAsphaltCoverWaveConditionsInput
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="GeneralWaveImpactAsphaltCoverWaveConditionsInput"/>.
+        /// </summary>
+        public GeneralWaveImpactAsphaltCoverWaveConditionsInput()
+        {
+            A = 1.0;
+            B = 0.0;
+            C = 0.0;
+        }
+
+        /// <summary>
+        /// Gets the 'a' parameter used in wave conditions calculations for blocks.
+        /// </summary>
+        public double A { get; private set; }
+
+        /// <summary>
+        /// Gets the 'b' parameter used in wave conditions calculations for blocks.
+        /// </summary>
+        public double B { get; private set; }
+
+        /// <summary>
+        /// Gets the 'c' parameter used in wave conditions calculations for blocks.
+        /// </summary>
+        public double C { get; private set; }
+    }
+}
