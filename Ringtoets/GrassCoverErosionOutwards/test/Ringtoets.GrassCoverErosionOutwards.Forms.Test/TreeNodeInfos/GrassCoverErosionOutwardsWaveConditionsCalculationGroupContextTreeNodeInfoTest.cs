cd26e3c16a36142a5c223d498d78f17721c917fc
Robert Tangerman <robert.tangerman@deltares.nl>
2016-09-15 08:05:57 +0000

    Add tree node info test file to svn. Related to Issue [WTI-813]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5365 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 2e7da3f404a390ab3ff6a07138f380e6332695d1
    Former-commit-id: da167201536edce02d4059b50e3364ea5409a8b1

---
+++ Ringtoets/GrassCoverErosionOutwards/test/Ringtoets.GrassCoverErosionOutwards.Forms.Test/TreeNodeInfos/GrassCoverErosionOutwardsWaveConditionsCalculationGroupContextTreeNodeInfoTest.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Drawing;
+using System.Linq;
+using Core.Common.Controls.TreeView;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.GrassCoverErosionOutwards.Data;
+using Ringtoets.GrassCoverErosionOutwards.Forms.PresentationObjects;
+using Ringtoets.GrassCoverErosionOutwards.Plugin;
+using RingtoetsCommonFormsResources = Ringtoets.Common.Forms.Properties.Resources;
+
+namespace Ringtoets.GrassCoverErosionOutwards.Forms.Test.TreeNodeInfos
+{
+    [TestFixture]
+    public class GrassCoverErosionOutwardsWaveConditionsCalculationGroupContextTreeNodeInfoTest
+    {
+        private MockRepository mocks;
+        private GrassCoverErosionOutwardsPlugin plugin;
+        private TreeNodeInfo info;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mocks = new MockRepository();
+            plugin = new GrassCoverErosionOutwardsPlugin();
+            info = plugin.GetTreeNodeInfos().First(tni => tni.TagType == typeof(GrassCoverErosionOutwardsWaveConditionsCalculationGroupContext));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            plugin.Dispose();
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Initialized_Always_ExpectedPropertiesSet()
+        {
+            // Setup
+            mocks.ReplayAll();
+
+            // Assert
+            Assert.AreEqual(typeof(GrassCoverErosionOutwardsWaveConditionsCalculationGroupContext), info.TagType);
+
+            Assert.IsNull(info.ForeColor);
+            Assert.IsNull(info.CanCheck);
+            Assert.IsNull(info.IsChecked);
+            Assert.IsNull(info.OnNodeChecked);
+        }
+
+        [Test]
+        public void Text_Always_ReturnGroupName()
+        {
+            // Setup
+            mocks.ReplayAll();
+
+            var failureMechanism = new GrassCoverErosionOutwardsFailureMechanism();
+            var context = new GrassCoverErosionOutwardsWaveConditionsCalculationGroupContext(failureMechanism.WaveConditionsCalculationGroup,
+                                                                                             failureMechanism);
+
+            // Call
+            string text = info.Text(context);
+
+            // Assert
+            Assert.AreEqual("Berekeningen", text);
+        }
+
+        [Test]
+        public void Image_Always_ReturnCalculationGroupIcon()
+        {
+            // Setup
+            var failureMechanism = new GrassCoverErosionOutwardsFailureMechanism();
+            var context = new GrassCoverErosionOutwardsWaveConditionsCalculationGroupContext(failureMechanism.WaveConditionsCalculationGroup,
+                                                                                             failureMechanism);
+
+            // Call
+            Image icon = info.Image(context);
+
+            // Assert
+            TestHelper.AssertImagesAreEqual(RingtoetsCommonFormsResources.GeneralFolderIcon, icon);
+        }
+    }
+}
