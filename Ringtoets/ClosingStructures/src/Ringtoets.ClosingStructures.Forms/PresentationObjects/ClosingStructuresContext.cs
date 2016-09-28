f02ecc2ee4949596335f3c9792dd77d4120a14bd
Dennis Tang <dennis.tang@deltares.nl>
2016-09-28 06:34:12 +0000

    - Added CalculationGroupContext for the ClosingStructures - Added ClosingStructuresContext - Added relevant tests
    Related to Issue [WTI-548]
    
    
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5578 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: ef0276794436a513fd12ce83fa707bba6098ac5e
    Former-commit-id: d45e1f0a153467fb1535420f8c7586d8612285c8

---
+++ Ringtoets/ClosingStructures/src/Ringtoets.ClosingStructures.Forms/PresentationObjects/ClosingStructuresContext.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base;
+using Core.Common.Controls.PresentationObjects;
+using Ringtoets.ClosingStructures.Data;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.HydraRing.Data;
+using RingtoetsCommonFormsResources = Ringtoets.Common.Forms.Properties.Resources;
+
+namespace Ringtoets.ClosingStructures.Forms.PresentationObjects
+{
+    /// <summary>
+    /// Presentation object representing all required closing structures input knowledge to configure and create
+    /// related objects. It will delegate observable behaviour to the wrapped data object.
+    /// </summary>
+    public abstract class ClosingStructuresContext<T> : ObservableWrappedObjectContextBase<T> where T:IObservable
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ClosingStructuresContext{T}"/>
+        /// </summary>
+        /// <param name="wrappedData">The concrete data instance wrapped by the context object</param>
+        /// <param name="failureMechanism">The failure mechanism which the context belongs to</param>
+        /// <param name="assessmentSection">The assessment section which the context belongs to</param>
+        /// <exception cref="ArgumentNullException">When any of the input parameters are <c>null</c></exception>
+        protected ClosingStructuresContext(T wrappedData,
+                                        ClosingStructuresFailureMechanism failureMechanism,
+                                        IAssessmentSection assessmentSection)
+            : base(wrappedData)
+        {
+            if (failureMechanism == null)
+            {
+                string message = string.Format(RingtoetsCommonFormsResources.AssertInputsAreNotNull_DataDescription_0_cannot_be_null,
+                                               "Het sluitings kunstwerk toetsspoor");
+                throw new ArgumentNullException("failureMechanism", message);
+            }
+
+            if (assessmentSection == null)
+            {
+                string message = string.Format(RingtoetsCommonFormsResources.AssertInputsAreNotNull_DataDescription_0_cannot_be_null,
+                                            RingtoetsCommonFormsResources.FailureMechanismContext_DataDescription_AssessmentSection);
+
+                throw new ArgumentNullException("assessmentSection", message);
+            }
+
+            FailureMechanism = failureMechanism;
+            AssessmentSection = assessmentSection;
+        }
+
+        /// <summary>
+        /// Gets the assessment section belonging to the context
+        /// </summary>
+        public IAssessmentSection AssessmentSection { get; private set; }
+
+        /// <summary>
+        /// Gets the failure mechanism belonging to the context
+        /// </summary>
+        public ClosingStructuresFailureMechanism FailureMechanism { get; private set; }
+    }
+}
---
+++ Ringtoets/ClosingStructures/test/Ringtoets.ClosingStructures.Forms.Test/PresentationObjects/ClosingStructuresContextTest.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using Core.Common.Base;
+using Core.Common.Controls.PresentationObjects;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.ClosingStructures.Data;
+using Ringtoets.ClosingStructures.Forms.PresentationObjects;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.HydraRing.Data;
+
+namespace Ringtoets.ClosingStructures.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class ClosingStructuresContextTest
+    {
+        [Test]
+        public void ParameteredConstructor_ExpectedValues()
+        {
+            // Setup
+            var mockRepository = new MockRepository();
+            var assessmentSectionMock = mockRepository.StrictMock<IAssessmentSection>();
+            mockRepository.ReplayAll();
+
+            var target = new ObservableObject();
+            var failureMechanism = new ClosingStructuresFailureMechanism();
+
+            // Call
+            var context = new SimpleClosingStructuresContext<ObservableObject>(target, failureMechanism, assessmentSectionMock);
+
+            // Assert
+            Assert.IsInstanceOf<ObservableWrappedObjectContextBase<ObservableObject>>(context);
+            Assert.AreSame(target, context.WrappedData);
+            Assert.AreSame(assessmentSectionMock, context.AssessmentSection);
+            Assert.AreSame(failureMechanism, context.FailureMechanism);
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void ParameteredConstructor_FailureMechanismIsNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mockRepository = new MockRepository();
+            var assessmentSectionMock = mockRepository.StrictMock<IAssessmentSection>();
+            mockRepository.ReplayAll();
+
+            var observableObject = new ObservableObject();
+
+            // Call
+            TestDelegate call = () => new SimpleClosingStructuresContext<ObservableObject>(observableObject, null, assessmentSectionMock);
+
+            // Assert
+            const string expectedMessage = "Het sluitings kunstwerk toetsspoor mag niet 'null' zijn.";
+            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<ArgumentNullException>(call, expectedMessage);
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void ParameteredConstructor_AssessmentSectionIsNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var observableObject = new ObservableObject();
+            var failureMechanism = new ClosingStructuresFailureMechanism();
+
+            // Call
+            TestDelegate call = () => new SimpleClosingStructuresContext<ObservableObject>(observableObject, failureMechanism, null);
+
+            // Assert
+            const string expectedMessage = "Het traject mag niet 'null' zijn.";
+            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<ArgumentNullException>(call, expectedMessage);
+        }
+
+        private class ObservableObject : Observable { }
+
+        private class SimpleClosingStructuresContext<T> : ClosingStructuresContext<T> where T : IObservable
+        {
+            public SimpleClosingStructuresContext(T target, ClosingStructuresFailureMechanism failureMechanism, IAssessmentSection assessmentSection)
+                : base(target, failureMechanism, assessmentSection) { }
+        }
+    }
+}
