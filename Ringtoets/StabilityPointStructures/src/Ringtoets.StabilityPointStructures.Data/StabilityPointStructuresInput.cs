e030b3edd862ddab56d40c727e1541efe3ec63be
Dennis Tang <dennis.tang@deltares.nl>
2016-10-03 15:23:21 +0000

    Initial commit for the input class: - Added hydraulic loads and data input properties - Added orientation input property
    Related to Issue [WTI-567]
    
    
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5698 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: d8a9759bde51083114ef178335af266411c785ee
    Former-commit-id: f4f92e7ecdd269f8c36660c64b73c1ce1f2d400e

--- Ringtoets/StabilityPointStructures/src/Ringtoets.StabilityPointStructures.Data/StabilityPointStructuresInput.cs
+++ Ringtoets/StabilityPointStructures/src/Ringtoets.StabilityPointStructures.Data/StabilityPointStructuresInput.cs
@@ -19,8 +19,13 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
+using System.Linq;
 using Core.Common.Base;
+using Core.Common.Base.Data;
+using Core.Common.Base.Geometry;
 using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Common.Data.DikeProfiles;
+using Ringtoets.Common.Data.Probabilistics;
 using Ringtoets.HydraRing.Data;
 
 namespace Ringtoets.StabilityPointStructures.Data
@@ -30,14 +35,243 @@ namespace Ringtoets.StabilityPointStructures.Data
     /// </summary>
     public class StabilityPointStructuresInput : Observable, ICalculationInput
     {
+        private ForeshoreProfile foreshoreProfile;
+        private RoundedDouble structureNormalOrientation;
+        private RoundedDouble volumicWeightWater;
+        private readonly NormalDistribution insideWaterLevelFailureConstruction;
+        private readonly NormalDistribution insideWaterLevel;
+        private readonly LogNormalDistribution stormDuration;
+
         /// <summary>
         /// Creates a new instance of <see cref="StabilityPointStructuresInput"/>.
         /// </summary>
-        public StabilityPointStructuresInput() {}
+        public StabilityPointStructuresInput()
+        {
+            volumicWeightWater = new RoundedDouble(2, 9.81);
+            structureNormalOrientation = new RoundedDouble(2);
+
+            insideWaterLevelFailureConstruction = new NormalDistribution(2)
+            {
+                Mean = (RoundedDouble) double.NaN,
+                StandardDeviation = (RoundedDouble) 0.1
+            };
+
+            insideWaterLevel = new NormalDistribution(2)
+            {
+                Mean = (RoundedDouble) double.NaN,
+                StandardDeviation = (RoundedDouble) 0.1
+            };
+
+            stormDuration = new LogNormalDistribution(2)
+            {
+                Mean = (RoundedDouble) 7.5,
+            };
+            stormDuration.SetStandardDeviationFromVariationCoefficient(0.25);
+
+            UpdateForeshoreProperties();
+        }
+
+        #region Structure properties
+
+        /// <summary>
+        /// Gets or sets the stability point structure.
+        /// </summary>
+        public StabilityPointStructure StabilityPointStructure { get; set; }
+
+        #endregion
+
+        #region Hydraulic data and loads
 
         /// <summary>
         /// Gets or sets the hydraulic boundary location from which to use the assessment level.
         /// </summary>
         public HydraulicBoundaryLocation HydraulicBoundaryLocation { get; set; }
+
+        #region Foreshore profile
+
+        /// <summary>
+        /// Gets or sets the foreshore profile.
+        /// </summary>
+        public ForeshoreProfile ForeshoreProfile
+        {
+            get
+            {
+                return foreshoreProfile;
+            }
+            set
+            {
+                foreshoreProfile = value;
+                UpdateForeshoreProperties();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets whether the <see cref="BreakWater"/> needs to be taken into account.
+        /// </summary>
+        public bool UseBreakWater { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the <see cref="ForeshoreProfile"/> needs to be taken into account.
+        /// </summary>
+        public bool UseForeshore { get; set; }
+
+        /// <summary>
+        /// Gets the geometry of the foreshore.
+        /// </summary>
+        public RoundedPoint2DCollection ForeshoreGeometry
+        {
+            get
+            {
+                return foreshoreProfile != null
+                           ? foreshoreProfile.Geometry
+                           : new RoundedPoint2DCollection(2, Enumerable.Empty<Point2D>());
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="BreakWater"/>.
+        /// </summary>
+        public BreakWater BreakWater { get; private set; }
+
+        private void UpdateForeshoreProperties()
+        {
+            if (foreshoreProfile == null)
+            {
+                UseForeshore = false;
+                UseBreakWater = false;
+                BreakWater = GetDefaultBreakWaterProperties();
+            }
+            else
+            {
+                UseForeshore = foreshoreProfile.Geometry.Count() > 1;
+                UseBreakWater = foreshoreProfile.HasBreakWater;
+                BreakWater = foreshoreProfile.HasBreakWater ?
+                                 new BreakWater(foreshoreProfile.BreakWater.Type, foreshoreProfile.BreakWater.Height) :
+                                 GetDefaultBreakWaterProperties();
+            }
+        }
+
+        private BreakWater GetDefaultBreakWaterProperties()
+        {
+            return new BreakWater(BreakWaterType.Dam, 0.0);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets or sets the volumic weight of water.
+        /// </summary>
+        public RoundedDouble VolumicWeightWater
+        {
+            get
+            {
+                return volumicWeightWater;
+            }
+            set
+            {
+                volumicWeightWater = value.ToPrecision(volumicWeightWater.NumberOfDecimalPlaces);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the inside water level failure construction.
+        /// </summary>
+        public NormalDistribution InsideWaterLevelFailureConstruction
+        {
+            get
+            {
+                return insideWaterLevelFailureConstruction;
+            }
+            set
+            {
+                insideWaterLevelFailureConstruction.Mean = value.Mean;
+                insideWaterLevelFailureConstruction.StandardDeviation = value.StandardDeviation;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the inside water level.
+        /// </summary>
+        public NormalDistribution InsideWaterLevel
+        {
+            get
+            {
+                return insideWaterLevel;
+            }
+            set
+            {
+                insideWaterLevel.Mean = value.Mean;
+                insideWaterLevel.StandardDeviation = value.StandardDeviation;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the storm duration.
+        /// </summary>
+        /// <remarks>Only sets the mean.</remarks>
+        public LogNormalDistribution StormDuration
+        {
+            get
+            {
+                return stormDuration;
+            }
+            set
+            {
+                stormDuration.Mean = value.Mean;
+            }
+        }
+
+        #endregion
+
+        #region Model Inputs
+
+        #endregion
+
+        #region Schematization
+
+        #region Orientation
+
+        /// <summary>
+        /// Gets or sets the orientation of the normal of the structure.
+        /// </summary>
+        /// <remarks><list type="bullet">
+        /// <item>When the value is smaller than 0, it will be set to 0.</item>
+        /// <item>When the value is larger than 360, it will be set to 360.</item>
+        /// </list></remarks>
+        public RoundedDouble StructureNormalOrientation
+        {
+            get
+            {
+                return structureNormalOrientation;
+            }
+            set
+            {
+                RoundedDouble newOrientationValue = value.ToPrecision(structureNormalOrientation.NumberOfDecimalPlaces);
+                newOrientationValue = ValidateStructureNormalOrientationInRange(newOrientationValue);
+
+                structureNormalOrientation = newOrientationValue;
+            }
+        }
+
+        private RoundedDouble ValidateStructureNormalOrientationInRange(RoundedDouble newOrientationValue)
+        {
+            const double upperBoundaryRange = 360;
+            const double lowerBoundaryRange = 0.0;
+
+            if (newOrientationValue > upperBoundaryRange)
+            {
+                newOrientationValue = new RoundedDouble(2, upperBoundaryRange);
+            }
+            else if (newOrientationValue < lowerBoundaryRange)
+            {
+                newOrientationValue = new RoundedDouble(2, lowerBoundaryRange);
+            }
+
+            return newOrientationValue;
+        }
+
+        #endregion
+
+        #endregion
     }
 }
--- Ringtoets/StabilityPointStructures/test/Ringtoets.StabilityPointStructures.Data.Test/StabilityPointStructuresInputTest.cs
+++ Ringtoets/StabilityPointStructures/test/Ringtoets.StabilityPointStructures.Data.Test/StabilityPointStructuresInputTest.cs
@@ -19,9 +19,17 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
+using System;
+using System.Collections.Generic;
 using Core.Common.Base;
+using Core.Common.Base.Data;
+using Core.Common.Base.Geometry;
 using NUnit.Framework;
 using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Common.Data.DikeProfiles;
+using Ringtoets.Common.Data.Probabilistics;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.HydraRing.Data;
 
 namespace Ringtoets.StabilityPointStructures.Data.Test
 {
@@ -38,6 +46,280 @@ namespace Ringtoets.StabilityPointStructures.Data.Test
             Assert.IsInstanceOf<Observable>(input);
             Assert.IsInstanceOf<ICalculationInput>(input);
             Assert.IsNull(input.HydraulicBoundaryLocation);
+
+            Assert.IsNull(input.StabilityPointStructure);
+
+            Assert.IsNull(input.ForeshoreProfile);
+            Assert.IsFalse(input.UseBreakWater);
+            Assert.AreEqual(BreakWaterType.Dam, input.BreakWater.Type);
+            Assert.AreEqual(new RoundedDouble(2), input.BreakWater.Height);
+            Assert.IsFalse(input.UseForeshore);
+            CollectionAssert.IsEmpty(input.ForeshoreGeometry);
+
+            Assert.AreEqual(2, input.VolumicWeightWater.NumberOfDecimalPlaces);
+            AssertEqualValues(9.81, input.VolumicWeightWater);
+
+            Assert.IsInstanceOf<NormalDistribution>(input.InsideWaterLevelFailureConstruction);
+            AssertEqualValues(0.1, input.InsideWaterLevelFailureConstruction.StandardDeviation);
+
+            Assert.IsInstanceOf<NormalDistribution>(input.InsideWaterLevel);
+            AssertEqualValues(0.1, input.InsideWaterLevel.StandardDeviation);
+
+            Assert.IsInstanceOf<LogNormalDistribution>(input.StormDuration);
+            AssertEqualValues(7.5, input.StormDuration.Mean);
+            AssertEqualValues(0.25, input.StormDuration.GetVariationCoefficient());
         }
+
+        #region Hydraulic loads and data
+
+        [Test]
+        public void Properties_HydraulicBoundaryLocation_ExpectedValues()
+        {
+            // Setup
+            var input = new StabilityPointStructuresInput();
+            var location = new HydraulicBoundaryLocation(0, "test", 0, 0);
+
+            // Call
+            input.HydraulicBoundaryLocation = location;
+
+            // Assert
+            Assert.AreEqual(location, input.HydraulicBoundaryLocation);
+        }
+
+        [Test]
+        [Combinatorial]
+        public void ForeshoreProfile_SetNewValue_InputSyncedAccordingly(
+            [Values(true, false)] bool withBreakWater,
+            [Values(true, false)] bool withValidForeshore)
+        {
+            // Setup
+            var input = new StabilityPointStructuresInput();
+            BreakWaterType originalBreakWaterType = input.BreakWater.Type;
+            RoundedDouble originalBreakWaterHeight = input.BreakWater.Height;
+            HydraulicBoundaryLocation originalHydraulicBoundaryLocation = input.HydraulicBoundaryLocation;
+
+            var foreshoreGeometry = new List<Point2D>
+            {
+                new Point2D(2.2, 3.3)
+            };
+
+            if (withValidForeshore)
+            {
+                foreshoreGeometry.Add(new Point2D(4.4, 5.5));
+            }
+
+            BreakWater breakWater = null;
+            if (withBreakWater)
+            {
+                var nonDefaultBreakWaterType = BreakWaterType.Wall;
+                var nonDefaultBreakWaterHeight = 5.5;
+
+                // Precondition
+                Assert.AreNotEqual(nonDefaultBreakWaterType, input.BreakWater.Type);
+                Assert.AreNotEqual(nonDefaultBreakWaterHeight, input.BreakWater.Height);
+
+                breakWater = new BreakWater(nonDefaultBreakWaterType, nonDefaultBreakWaterHeight);
+            }
+
+            double orientation = 96;
+            var foreshoreProfile = new ForeshoreProfile(new Point2D(0, 0),
+                                                        foreshoreGeometry.ToArray(),
+                                                        breakWater,
+                                                        new ForeshoreProfile.ConstructionProperties
+                                                        {
+                                                            Orientation = orientation
+                                                        });
+
+            // Call
+            input.ForeshoreProfile = foreshoreProfile;
+
+            // Assert
+            Assert.AreSame(foreshoreProfile, input.ForeshoreProfile);
+            Assert.AreEqual(withBreakWater, input.UseBreakWater);
+            Assert.AreEqual(withBreakWater ? foreshoreProfile.BreakWater.Type : originalBreakWaterType, input.BreakWater.Type);
+            Assert.AreEqual(withBreakWater ? foreshoreProfile.BreakWater.Height : originalBreakWaterHeight, input.BreakWater.Height);
+            Assert.AreEqual(withValidForeshore, input.UseForeshore);
+            CollectionAssert.AreEqual(foreshoreProfile.Geometry, input.ForeshoreGeometry);
+            Assert.AreEqual(originalHydraulicBoundaryLocation, input.HydraulicBoundaryLocation);
+        }
+
+        [Test]
+        public void Foreshore_SetNullValue_InputSyncedToDefaults()
+        {
+            // Setup
+            var input = new StabilityPointStructuresInput();
+            BreakWaterType originalBreakWaterType = input.BreakWater.Type;
+            RoundedDouble originalBreakWaterHeight = input.BreakWater.Height;
+            HydraulicBoundaryLocation originalHydraulicBoundaryLocation = input.HydraulicBoundaryLocation;
+
+            var foreshoreProfile = new ForeshoreProfile(new Point2D(0, 0),
+                                                        new[]
+                                                        {
+                                                            new Point2D(3.3, 4.4),
+                                                            new Point2D(5.5, 6.6)
+                                                        },
+                                                        new BreakWater(BreakWaterType.Caisson, 2.2),
+                                                        new ForeshoreProfile.ConstructionProperties
+                                                        {
+                                                            Orientation = 96
+                                                        });
+
+            input.ForeshoreProfile = foreshoreProfile;
+
+            // Precondition
+            Assert.AreSame(foreshoreProfile, input.ForeshoreProfile);
+            Assert.IsTrue(input.UseBreakWater);
+            Assert.AreNotEqual(originalBreakWaterType, input.BreakWater.Type);
+            Assert.AreNotEqual(originalBreakWaterHeight, input.BreakWater.Height);
+            Assert.IsTrue(input.UseForeshore);
+            CollectionAssert.IsNotEmpty(input.ForeshoreGeometry);
+            Assert.AreEqual(originalHydraulicBoundaryLocation, input.HydraulicBoundaryLocation);
+
+            // Call
+            input.ForeshoreProfile = null;
+
+            // Assert
+            Assert.IsFalse(input.UseBreakWater);
+            Assert.AreEqual(originalBreakWaterType, input.BreakWater.Type);
+            Assert.AreEqual(originalBreakWaterHeight, input.BreakWater.Height);
+            Assert.IsFalse(input.UseForeshore);
+            CollectionAssert.IsEmpty(input.ForeshoreGeometry);
+            Assert.AreEqual(originalHydraulicBoundaryLocation, input.HydraulicBoundaryLocation);
+        }
+
+        [Test]
+        public void Properties_VolumicWeightWater_ExpectedValues()
+        {
+            // Setup
+            var input = new StabilityPointStructuresInput();
+            var random = new Random(22);
+
+            RoundedDouble volumicWeightWater = new RoundedDouble(5, random.NextDouble());
+
+            // Call
+            input.VolumicWeightWater = volumicWeightWater;
+
+            // Assert
+            Assert.AreEqual(2, input.VolumicWeightWater.NumberOfDecimalPlaces);
+            AssertEqualValues(volumicWeightWater, input.VolumicWeightWater);
+        }
+
+        [Test]
+        public void Properties_InsideWaterLevelFailureConstruction_ExpectedValues()
+        {
+            // Setup
+            var input = new StabilityPointStructuresInput();
+            NormalDistribution insideWaterLevelFailureConstruction = GenerateNormalDistribution();
+
+            //Call
+            input.InsideWaterLevelFailureConstruction = insideWaterLevelFailureConstruction;
+
+            //Assert
+            Assert.AreEqual(insideWaterLevelFailureConstruction.Mean, input.InsideWaterLevelFailureConstruction.Mean);
+            Assert.AreEqual(insideWaterLevelFailureConstruction.StandardDeviation, input.InsideWaterLevelFailureConstruction.StandardDeviation);
+        }
+
+        [Test]
+        public void Properties_InsideWaterLevel_ExpectedValues()
+        {
+            // Setup
+            var input = new StabilityPointStructuresInput();
+            NormalDistribution insideWaterLevel = GenerateNormalDistribution();
+
+            //Call
+            input.InsideWaterLevel = insideWaterLevel;
+
+            //Assert
+            Assert.AreEqual(insideWaterLevel.Mean, input.InsideWaterLevel.Mean);
+            Assert.AreEqual(insideWaterLevel.StandardDeviation, input.InsideWaterLevel.StandardDeviation);
+        }
+
+        [Test]
+        public void Properties_StormDuration_ExpectedValues()
+        {
+            // Setup
+            var input = new StabilityPointStructuresInput();
+            LogNormalDistribution stormDuration = GenerateLogNormalDistribution();
+
+            RoundedDouble initialStd = input.StormDuration.StandardDeviation;
+
+            //Call
+            input.StormDuration = stormDuration;
+
+            //Assert
+            Assert.AreEqual(stormDuration.Mean, input.StormDuration.Mean);
+            AssertEqualValues(initialStd, input.StormDuration.StandardDeviation);
+        }
+
+        #endregion
+
+        #region Schematization
+
+        [Test]
+        [TestCase(360.004)]
+        [TestCase(300)]
+        [TestCase(0)]
+        [TestCase(-0.004)]
+        public void Properties_StructureNormalOrientationValidValues_NewValueSet(double orientation)
+        {
+            // Setup
+            var input = new StabilityPointStructuresInput();
+
+            // Call
+            input.StructureNormalOrientation = (RoundedDouble) orientation;
+
+            // Assert
+            Assert.AreEqual(2, input.StructureNormalOrientation.NumberOfDecimalPlaces);
+            AssertEqualValues(orientation, input.StructureNormalOrientation);
+        }
+
+        [Test]
+        [TestCase(400, 360)]
+        [TestCase(360.05, 360)]
+        [TestCase(-0.005, 0)]
+        [TestCase(-23, 0)]
+        public void Properties_StructureNormalOrientationInValidValues_ValueRoundedToValidValue(double invalidValue, double validValue)
+        {
+            // Setup
+            var input = new StabilityPointStructuresInput();
+
+            // Call
+            input.StructureNormalOrientation = (RoundedDouble) invalidValue;
+
+            // Assert
+            Assert.AreEqual(2, input.StructureNormalOrientation.NumberOfDecimalPlaces);
+            AssertEqualValues(validValue, input.StructureNormalOrientation);
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private void AssertEqualValues(double expectedValue, RoundedDouble actualValue)
+        {
+            Assert.AreEqual(expectedValue, actualValue, actualValue.GetAccuracy());
+        }
+
+        private static LogNormalDistribution GenerateLogNormalDistribution()
+        {
+            var random = new Random(22);
+            return new LogNormalDistribution(2)
+            {
+                Mean = (RoundedDouble) (0.01 + random.NextDouble()),
+                StandardDeviation = (RoundedDouble) random.NextDouble()
+            };
+        }
+
+        private static NormalDistribution GenerateNormalDistribution()
+        {
+            var random = new Random(22);
+            return new NormalDistribution(2)
+            {
+                Mean = (RoundedDouble) (0.01 + random.NextDouble()),
+                StandardDeviation = (RoundedDouble) random.NextDouble()
+            };
+        }
+
+        #endregion
     }
 }
