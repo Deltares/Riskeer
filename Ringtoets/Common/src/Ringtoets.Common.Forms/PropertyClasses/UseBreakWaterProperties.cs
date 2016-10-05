443fcdbfa5d49c306f76bb12f054c6e6bf859e18
Marten Janssen <marten.janssen@deltares.nl>
2016-10-05 09:49:06 +0000

    - Added interfaces for UseBreakWater and UseForeshore properties Related to Issue [WTI-584]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@5747 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 7ad39ecd7315a81e042361c4abff2adaef2acef3
    Former-commit-id: 88d1a2403851cacc0d4cb583c9c176ac343545f0

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/PropertyClasses/UseBreakWaterProperties.cs
@@ -0,0 +1,131 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using Core.Common.Base.Data;
+using Core.Common.Gui.Attributes;
+using Core.Common.Utils;
+using Core.Common.Utils.Attributes;
+using Core.Common.Utils.Reflection;
+using Ringtoets.Common.Data.DikeProfiles;
+using Ringtoets.Common.Forms.Properties;
+
+namespace Ringtoets.Common.Forms.PropertyClasses
+{
+    /// <summary>
+    /// ViewModel of <see cref="IUseBreakWater"/>.
+    /// </summary>
+    public class UseBreakWaterProperties
+    {
+        private const int useBreakWaterPropertyIndex = 1;
+        private const int breakWaterTypePropertyIndex = 2;
+        private const int breakWaterHeightPropertyIndex = 3;
+        private readonly IUseBreakWater data;
+        private readonly Func<bool> useBreakWaterEnabled;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="UseBreakWaterProperties"/>.
+        /// </summary>
+        /// <param name="useBreakWaterData">The data to use for the properties.</param>
+        /// <param name="useBreakWaterEnabled">Function to check if <see cref="UseBreakWater"/> should be read-only or not.</param>
+        /// <exception cref="ArgumentNullException">Thrown if any of the input parameters is <c>null</c>.</exception>
+        public UseBreakWaterProperties(IUseBreakWater useBreakWaterData, Func<bool> useBreakWaterEnabled)
+        {
+            if (useBreakWaterData == null)
+            {
+                throw new ArgumentNullException("useBreakWaterData");
+            }
+            if (useBreakWaterEnabled == null)
+            {
+                throw new ArgumentNullException("useBreakWaterEnabled");
+            }
+            data = useBreakWaterData;
+            this.useBreakWaterEnabled = useBreakWaterEnabled;
+        }
+
+        [DynamicReadOnly]
+        [PropertyOrder(useBreakWaterPropertyIndex)]
+        [ResourcesDisplayName(typeof(Resources), "BreakWater_UseBreakWater_DisplayName")]
+        [ResourcesDescription(typeof(Resources), "BreakWater_UseBreakWater_Description")]
+        public bool UseBreakWater
+        {
+            get
+            {
+                return data.UseBreakWater;
+            }
+            set
+            {
+                data.UseBreakWater = value;
+                data.NotifyObservers();
+            }
+        }
+
+        [DynamicReadOnly]
+        [PropertyOrder(breakWaterTypePropertyIndex)]
+        [ResourcesDisplayName(typeof(Resources), "BreakWaterType_DisplayName")]
+        [ResourcesDescription(typeof(Resources), "BreakWaterType_Description")]
+        [TypeConverter(typeof(EnumTypeConverter))]
+        public BreakWaterType BreakWaterType
+        {
+            get
+            {
+                return data.BreakWater == null ? BreakWaterType.Dam : data.BreakWater.Type;
+            }
+            set
+            {
+                data.BreakWater.Type = value;
+                data.NotifyObservers();
+            }
+        }
+
+        [DynamicReadOnly]
+        [PropertyOrder(breakWaterHeightPropertyIndex)]
+        [ResourcesDisplayName(typeof(Resources), "BreakWaterHeight_DisplayName")]
+        [ResourcesDescription(typeof(Resources), "BreakWaterHeight_Description")]
+        public RoundedDouble BreakWaterHeight
+        {
+            get
+            {
+                return data.BreakWater == null ? (RoundedDouble) double.NaN
+                           : data.BreakWater.Height;
+            }
+            set
+            {
+                data.BreakWater.Height = value;
+                data.NotifyObservers();
+            }
+        }
+
+        [DynamicReadOnlyValidationMethod]
+        public bool DynamicReadOnlyValidationMethod(string propertyName)
+        {
+            return !useBreakWaterEnabled()
+                   || !propertyName.Equals(TypeUtils.GetMemberName<UseBreakWaterProperties>(i => i.UseBreakWater))
+                   && !UseBreakWater;
+        }
+
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/PropertyClasses/UseForeshoreProperties.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using System.Linq;
+using Core.Common.Base.Geometry;
+using Core.Common.Gui.Attributes;
+using Core.Common.Gui.Converters;
+using Core.Common.Utils.Attributes;
+using Ringtoets.Common.Data.DikeProfiles;
+using Ringtoets.Common.Forms.Properties;
+
+namespace Ringtoets.Common.Forms.PropertyClasses
+{
+    /// <summary>
+    /// ViewModel of <see cref="IUseForeshore"/>.
+    /// </summary>
+    public class UseForeshoreProperties
+    {
+        private const int useForeshorePropertyIndex = 1;
+        private const int coordinatesPropertyIndex = 2;
+        private readonly IUseForeshore data;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="UseForeshoreProperties"/>.
+        /// </summary>
+        /// <param name="useForshoreData">The data to use for the properties.</param>
+        /// <exception cref="ArgumentNullException">Thrown if any of <paramref name="useForshoreData"/> is <c>null</c>.</exception>
+        public UseForeshoreProperties(IUseForeshore useForshoreData)
+        {
+            if (useForshoreData == null)
+            {
+                throw new ArgumentNullException("useForshoreData");
+            }
+            data = useForshoreData;
+        }
+
+        [DynamicReadOnly]
+        [PropertyOrder(useForeshorePropertyIndex)]
+        [ResourcesDisplayName(typeof(Resources), "Foreshore_UseForeshore_DisplayName")]
+        [ResourcesDescription(typeof(Resources), "Foreshore_UseForeshore_Description")]
+        public bool UseForeshore
+        {
+            get
+            {
+                return data.UseForeshore;
+            }
+            set
+            {
+                data.UseForeshore = value;
+                data.NotifyObservers();
+            }
+        }
+
+        [PropertyOrder(coordinatesPropertyIndex)]
+        [TypeConverter(typeof(ExpandableReadOnlyArrayConverter))]
+        [ResourcesDisplayName(typeof(Resources), "Geometry_Coordinates_DisplayName")]
+        [ResourcesDescription(typeof(Resources), "Geometry_Coordinates_Description")]
+        public Point2D[] Coordinates
+        {
+            get
+            {
+                return data.ForeshoreGeometry == null ? new Point2D[0] : data.ForeshoreGeometry.ToArray();
+            }
+        }
+
+        [DynamicReadOnlyValidationMethod]
+        public bool DynamicReadOnlyValidationMethod(string propertyName)
+        {
+            return data.ForeshoreGeometry == null || data.ForeshoreGeometry.Count() < 2;
+        }
+
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+    }
+}
