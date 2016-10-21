7d7217b5823e5008579462c990e259e2bb5b4018
Dennis Tang <dennis.tang@deltares.nl>
2016-10-21 07:40:34 +0000

    Connecting Hydraring: - Added interface and the calculator Related to Issue [WTI-554]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@6053 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 42cdf388f8d9e2f094fdf623901377ae7e00314c
    Former-commit-id: 643c352c0667af6e17afc4301381db2b227ff5b8

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.Calculation/Calculator/StructuresClosureCalculator.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Ringtoets.HydraRing.Calculation.Data;
+using Ringtoets.HydraRing.Calculation.Data.Input.Structures;
+using Ringtoets.HydraRing.Calculation.Parsers;
+
+namespace Ringtoets.HydraRing.Calculation.Calculator
+{
+    /// <summary>
+    /// Calculator for calculating probability of failure by overtopping or overflow and the
+    /// associated wave height. This is used in a closure structures assessment.
+    /// </summary>
+    internal class StructuresClosureCalculator : HydraRingCalculatorBase, IStructuresClosureCalculator
+    {
+        private readonly ExceedanceProbabilityCalculationParser exceedanceProbabilityCalculationParser;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="StructuresClosureCalculator"/>.
+        /// </summary>
+        /// <param name="hlcdDirectory">The directory in which the hydraulic boundary database can be found.</param>
+        /// <param name="ringId">The id of the traject which is used in the calculation.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="hlcdDirectory"/> is <c>null</c>.</exception>
+        public StructuresClosureCalculator(string hlcdDirectory, string ringId) 
+            : base(hlcdDirectory, ringId)
+        {
+            exceedanceProbabilityCalculationParser = new ExceedanceProbabilityCalculationParser();
+            ExceedanceProbabilityBeta = double.NaN;
+        }
+
+        public double ExceedanceProbabilityBeta { get; private set; }
+
+        public void Calculate(StructuresClosureCalculationInput input)
+        {
+            Calculate(HydraRingUncertaintiesType.All, input);
+        }
+
+        protected override IEnumerable<IHydraRingFileParser> GetParsers()
+        {
+            yield return exceedanceProbabilityCalculationParser;
+        }
+
+        protected override void SetOutputs()
+        {
+            if (exceedanceProbabilityCalculationParser.Output != null)
+            {
+                ExceedanceProbabilityBeta = exceedanceProbabilityCalculationParser.Output.Beta;
+            }
+        }
+    }
+}
