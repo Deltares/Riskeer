29bdf364fd59627ccc39e9a302a40ea5f2d4da97
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-10-24 18:38:22 +0000

    Added first version of StructuresInputBaseProperties Related to Issue [WTI-556]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@6100 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 51ca0f152d80be43281137e3c8fd91e6ab01711f
    Former-commit-id: 4538dea04e6eb99afeae5c1bab8e6d44773855d3

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/PropertyClasses/StructuresInputBaseProperties.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Core.Common.Gui.PropertyBag;
+using Ringtoets.Common.Data;
+using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Common.Data.DikeProfiles;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.Structures;
+using Ringtoets.Common.Forms.PresentationObjects;
+using Ringtoets.Common.Forms.UITypeEditors;
+using Ringtoets.HydraRing.Data;
+
+namespace Ringtoets.Common.Forms.PropertyClasses
+{
+    /// <summary>
+    /// ViewModel of structure calculation input for properties panel.
+    /// </summary>
+    /// <typeparam name="TStructure">The type of structures at stake.</typeparam>
+    /// <typeparam name="TStructureInput">The type of structures calculation input.</typeparam>
+    /// <typeparam name ="TCalculation">The type of the calculation containing the structures calculation input.</typeparam>
+    /// <typeparam name="TFailureMechanism">The type of the failure mechanism.</typeparam>
+    public abstract class StructuresInputBaseProperties<TStructure, TStructureInput, TCalculation, TFailureMechanism> :
+        ObjectProperties<InputContextBase<TStructureInput, TCalculation, TFailureMechanism>>,
+        IHasHydraulicBoundaryLocationProperty,
+        IHasStructureProperty<TStructure>,
+        IHasForeshoreProfileProperty
+        where TStructure : StructureBase
+        where TStructureInput : StructuresInputBase<TStructure>
+        where TCalculation : ICalculation
+        where TFailureMechanism : IFailureMechanism
+    {
+        public ForeshoreProfile ForeshoreProfile { get; private set; }
+
+        public HydraulicBoundaryLocation HydraulicBoundaryLocation { get; private set; }
+
+        public TStructure Structure { get; private set; }
+
+        public IEnumerable<ForeshoreProfile> GetAvailableForeshoreProfiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<HydraulicBoundaryLocation> GetAvailableHydraulicBoundaryLocations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TStructure> GetAvailableStructures()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
