ccda7c4eae626e537cf8497367664bef0095c19f
Robert Tangerman <robert.tangerman@deltares.nl>
2016-11-14 12:49:37 +0000

    Added HydraRingCalculationException Related to Issue [WTI-928]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@6598 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 1db6f9603bedf66d7a13a8da2f228ce786a4ee2f
    Former-commit-id: 174760fb1c8da01ae2a65c7c8c643309f9a16dd9

---
+++ Ringtoets/HydraRing/test/Ringtoets.HydraRing.Calculation.Test/Exceptions/HydraRingCalculationExceptionTest.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.HydraRing.Calculation.Exceptions;
+
+namespace Ringtoets.HydraRing.Calculation.Test.Exceptions
+{
+    [TestFixture]
+    public class HydraRingCalculationExceptionTest
+    {
+        [Test]
+        public void DefaultConstructor_InnerExceptionNullAndMessageDefault()
+        {
+            // Setup
+            string expectedMessage = string.Format("Exception of type '{0}' was thrown.", typeof(HydraRingCalculationException).FullName);
+
+            // Call
+            HydraRingCalculationException exception = new HydraRingCalculationException();
+
+            // Assert
+            Assert.IsInstanceOf<Exception>(exception);
+            Assert.IsNull(exception.InnerException);
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void Constructor_WithCustomMessage_InnerExceptionNullAndMessageSetToCustom()
+        {
+            // Setup
+            const string expectedMessage = "Some exception message";
+
+            // Call
+            HydraRingCalculationException exception = new HydraRingCalculationException(expectedMessage);
+
+            // Assert
+            Assert.IsNull(exception.InnerException);
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void Constructor_WithCustomMessageAndInnerException_InnerExceptionSetAndMessageSetToCustom()
+        {
+            // Setup
+            const string expectedMessage = "Some exception message";
+            Exception expectedInnerException = new Exception();
+
+            // Call
+            HydraRingCalculationException exception = new HydraRingCalculationException(expectedMessage, expectedInnerException);
+
+            // Assert
+            Assert.AreSame(expectedInnerException, exception.InnerException);
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void Constructor_SerializationRoundTrip_ExceptionProperlyInitialized()
+        {
+            // Setup
+            var originalInnerException = new Exception("inner");
+            var originalException = new HydraRingCalculationException("outer", originalInnerException);
+
+            // Precondition
+            Assert.IsNotNull(originalException.InnerException);
+            Assert.IsNull(originalException.InnerException.InnerException);
+
+            // Call
+            HydraRingCalculationException persistedException = SerializationTestHelper.SerializeAndDeserializeException(originalException);
+
+            // Assert
+            Assert.AreEqual(originalException.Message, persistedException.Message);
+            Assert.IsNotNull(persistedException.InnerException);
+            Assert.AreEqual(originalException.InnerException.GetType(), persistedException.InnerException.GetType());
+            Assert.AreEqual(originalException.InnerException.Message, persistedException.InnerException.Message);
+            Assert.IsNull(persistedException.InnerException.InnerException);
+        }
+    }
+}
