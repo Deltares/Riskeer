1de81bf464ddef0b379aa3b3e545e4937e964a71
Elmar Kleijn <elmar.kleijn@deltares.nl>
2016-11-28 16:48:40 +0000

    Integrated new methods in MapFeatureLayerFactory Related to Issue [WTI-1018]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@6910 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 5a37e8eee11b06d82413e993c28da22f54ed0605
    Former-commit-id: feee8d4a81cc92578b6fc225556013c0a259f231

--- Core/Components/src/Core.Components.DotSpatial.Forms/MapControl.cs
+++ Core/Components/src/Core.Components.DotSpatial.Forms/MapControl.cs
@@ -31,7 +31,6 @@ using Core.Components.Gis.Features;
 using Core.Components.Gis.Forms;
 using DotSpatial.Controls;
 using DotSpatial.Data;
-using DotSpatial.Symbology;
 using DotSpatial.Topology;
 
 namespace Core.Components.DotSpatial.Forms
@@ -272,25 +271,13 @@ namespace Core.Components.DotSpatial.Forms
 
             drawnMapData.Observer = new Observer(() =>
             {
-                var newMapFeatureLayer = MapFeatureLayerFactory.Create(drawnMapData.FeatureBasedMapData);
+                MapFeatureLayerFactory.ConvertLayerProperties(drawnMapData.FeatureBasedMapData, drawnMapData.MapFeatureLayer);
 
-                if (ReferenceEquals(drawnMapData.FeatureBasedMapData.Features, drawnMapData.Features))
+                if (!ReferenceEquals(drawnMapData.FeatureBasedMapData.Features, drawnMapData.Features))
                 {
-                    // Only update some properties when the array of drawn features is still the same
-                    drawnMapData.MapFeatureLayer.IsVisible = newMapFeatureLayer.IsVisible;
-                    ((FeatureLayer) drawnMapData.MapFeatureLayer).Name = ((FeatureLayer) drawnMapData.MapFeatureLayer).Name;
-                    drawnMapData.MapFeatureLayer.ShowLabels = newMapFeatureLayer.ShowLabels;
-                    drawnMapData.MapFeatureLayer.LabelLayer = newMapFeatureLayer.LabelLayer;
-                    drawnMapData.MapFeatureLayer.Symbolizer = newMapFeatureLayer.Symbolizer;
-                }
-                else
-                {
-                    // Otherwise redraw the feature layer and update the drawn map data lookup
-                    var replaceIndex = map.Layers.IndexOf(drawnMapData.MapFeatureLayer);
-                    map.Layers.RemoveAt(replaceIndex);
-                    map.Layers.Insert(replaceIndex, newMapFeatureLayer);
+                    MapFeatureLayerFactory.ConvertLayerFeatures(drawnMapData.FeatureBasedMapData, drawnMapData.MapFeatureLayer);
+
                     drawnMapData.Features = drawnMapData.FeatureBasedMapData.Features;
-                    drawnMapData.MapFeatureLayer = newMapFeatureLayer;
                 }
             })
             {
