3585aaaee85811301a4b53d06f3cab9fa8bf4957
Dennis Tang <dennis.tang@deltares.nl>
2016-11-30 12:05:46 +0000

    Added TestUtil project for Revetment to define a TestWaveConditionsOutput class at a centralized location Related to Issue [WTI-897]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@6928 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: ca56e34d983aa5168531feedf60422a94ef367ec
    Former-commit-id: 22e177da92ea7be192b1b65b6545adb5e0b0ee58

---
+++ Ringtoets/Revetment/test/Ringtoets.Revetment.Data.TestUtil.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ringtoets.Revetment.TestUtil.Test")]
+[assembly: AssemblyProduct("Ringtoets.Revetment.TestUtil.Test")]
+[assembly: Guid("1708af4a-ab19-46d1-a66b-fbe89de78217")]
---
+++ Ringtoets/Revetment/test/Ringtoets.Revetment.Data.TestUtil/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ringtoets.Revetment.TestUtil")]
+[assembly: AssemblyProduct("Ringtoets.Revetment.TestUtil")]
+[assembly: Guid("864dbb59-6281-4bc6-baf5-e5fb6c136eae")]
