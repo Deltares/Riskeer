649e89c7bbd4e79a738c90513f9d51d387f019a2
Robert Tangerman <robert.tangerman@deltares.nl>
2016-12-09 12:20:52 +0000

    Introduced RandomHydraRingVariable. Related to Issue [WTI-1007]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@7186 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 26fc88c49ffbcf1de73f0b64556ffd6778276e97
    Former-commit-id: 48a523508fe38fd217ab0ab6d4a007ed2fd6674a

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.Calculation/Data/Variables/RandomHydraRingVariable.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Ringtoets.HydraRing.Calculation.Data.Variables
+{
+    /// <summary>
+    /// Class for random Hydra-Ring variable related data.
+    /// </summary>
+    public abstract class RandomHydraRingVariable : HydraRingVariable
+    {
+        private readonly HydraRingDistributionType distributionType;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RandomHydraRingVariable"/>
+        /// </summary>
+        /// <param name="variableId">The Hydra-Ring id corresponding to the variable that is considered.</param>
+        /// <param name="deviationType">The deviation type of the variable.</param>
+        /// <param name="distributionType">The distribution type of the variable.</param>
+        protected RandomHydraRingVariable(int variableId, HydraRingDeviationType deviationType, HydraRingDistributionType distributionType) 
+            : base(variableId, deviationType)
+        {
+            this.distributionType = distributionType;
+        }
+
+        public override HydraRingDistributionType DistributionType
+        {
+            get
+            {
+                return distributionType;
+            }
+        }
+    }
+}
---
+++ Ringtoets/HydraRing/test/Ringtoets.HydraRing.Calculation.Test/Data/Variables/RandomHydraRingVariableTest.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+using Ringtoets.HydraRing.Calculation.Data;
+using Ringtoets.HydraRing.Calculation.Data.Variables;
+
+namespace Ringtoets.HydraRing.Calculation.Test.Data.Variables
+{
+    [TestFixture]
+    public class RandomHydraRingVariableTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var variableId = 1;
+            var deviationType = HydraRingDeviationType.Variation;
+            var distributionType = HydraRingDistributionType.LogNormal;
+
+            // Call
+            var variable = new TestRandomHydraRingVariable(variableId, deviationType, distributionType);
+
+            // Assert
+            Assert.IsInstanceOf<HydraRingVariable>(variable);
+            Assert.AreEqual(distributionType, variable.DistributionType);
+        }
+
+        private class TestRandomHydraRingVariable : RandomHydraRingVariable
+        {
+            public TestRandomHydraRingVariable(int variableId, HydraRingDeviationType deviationType, HydraRingDistributionType distributionType) 
+                : base(variableId, deviationType, distributionType) {}
+        }
+    }
+}
