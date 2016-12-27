0f6942caadb447b170627a385ebd13cc1b6aac20
Robert Tangerman <robert.tangerman@deltares.nl>
2016-12-27 15:00:07 +0000

    - Added dune locations embedded resources. - Implemented reader for dune locations. Related to Issue [WTI-775]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@7453 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: ad9a9e4023ec4663e7d4a52fceef13a98a39044e
    Former-commit-id: 54281820cfa1df3596908d8c71e460066bf93063

---
+++ Ringtoets/DuneErosion/src/Ringtoets.DuneErosion.IO/DuneLocationsReader.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Core.Common.Base.Geometry;
+using Core.Common.Utils.IO;
+using Core.Components.Gis.Data;
+using Core.Components.Gis.IO.Readers;
+using Ringtoets.DuneErosion.Data;
+
+namespace Ringtoets.DuneErosion.IO
+{
+    /// <summary>
+    /// Shapefile reader that reads a <see cref="DuneLocation"/> based on the line feature in the file.
+    /// </summary>
+    public class DuneLocationsReader
+    {
+        private const string nameKey = "Naam";
+        private const string coastalAreaIdKey = "KV";
+        private const string offsetKey = "RSP";
+        private const string orientationKey = "Richting";
+        private const string d50Key = "Dreken";
+
+        /// <summary>
+        /// Reads an <see cref="IEnumerable{T}"/> of <see cref="DuneLocation"/> from an embedded shape file containing points.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="DuneLocation"/>.</returns>
+        public IEnumerable<DuneLocation> ReadDuneLocations()
+        {
+            using (var embeddedResourceFileWriter = new EmbeddedResourceFileWriter(typeof(DuneLocationsReader).Assembly,
+                                                                                   true,
+                                                                                   "RSPstelsel.shp",
+                                                                                   "RSPstelsel.dbf",
+                                                                                   "RSPstelsel.cpg",
+                                                                                   "RSPstelsel.sbn",
+                                                                                   "RSPstelsel.sbx",
+                                                                                   "RSPstelsel.shx"))
+            {
+                string filePath = Path.Combine(embeddedResourceFileWriter.TargetFolderPath, "RSPstelsel.shp");
+
+                using (var pointShapeReader = new PointShapeFileReader(filePath))
+                {
+                    var locationsData = pointShapeReader.ReadShapeFile();
+                    return CreateDuneLocations(locationsData);
+                }
+            }
+        }
+
+        private IEnumerable<DuneLocation> CreateDuneLocations(FeatureBasedMapData locationsData)
+        {
+            foreach (var locationData in locationsData.Features)
+            {
+                Point2D location = locationData.MapGeometries.First().PointCollections.First().First();
+
+                double xCoordinate = location.X;
+                double yCoordinate = location.Y;
+
+                string name = locationData.MetaData[nameKey].ToString();
+                int coastalAreaId = Convert.ToInt32(locationData.MetaData[coastalAreaIdKey]);
+                double offset = Convert.ToDouble(locationData.MetaData[offsetKey]);
+                double orientation = Convert.ToDouble(locationData.MetaData[orientationKey]);
+                double d50 = Convert.ToDouble(locationData.MetaData[d50Key]);
+
+                yield return  new DuneLocation(name, xCoordinate, yCoordinate, coastalAreaId, offset, orientation, d50);
+            }
+        }
+    }
+}
---
+++ Ringtoets/DuneErosion/test/Ringtoets.DuneErosion.IO.Test/DuneLocationsReaderTest.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Ringtoets.DuneErosion.Data;
+
+namespace Ringtoets.DuneErosion.IO.Test
+{
+    [TestFixture]
+    public class DuneLocationsReaderTest
+    {
+        [Test]
+        public void ReadDuneLocations_Always_ReadsEmbeddedDuneLocations()
+        {
+            // Setup
+            var reader = new DuneLocationsReader();
+
+            // Call
+            IEnumerable<DuneLocation> locations = reader.ReadDuneLocations();
+
+            // Assert
+            Assert.AreEqual(3431, locations.Count());
+            foreach (DuneLocation duneLocation in locations)
+            {
+                Assert.IsNotNullOrEmpty(duneLocation.Name);
+                Assert.IsNotNull(duneLocation.Location);
+                Assert.IsNotNull(duneLocation.Location.X);
+                Assert.IsNotNull(duneLocation.Location.Y);
+                Assert.IsNotNull(duneLocation.CoastalAreaId);
+                Assert.IsNotNull(duneLocation.Offset);
+                Assert.IsNotNull(duneLocation.Orientation);
+                Assert.IsNotNull(duneLocation.D50);
+            }
+        }
+    }
+}
