9797c9d215a710475c415fb3864e5c3a0d420ae0
Dennis Tang <dennis.tang@deltares.nl>
2017-01-02 09:05:03 +0000

    Introduced PropertyInfo for DuneLocations: - Added test and class - Added a test asserting values for the display values of the CalculationConvergence to prevent from explicitly asserting the values in the property class test. Related to Issue [WTI-778]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@7540 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: a0e080dfed98bdf7671b40ada479ed4a972565b6
    Former-commit-id: 1be672952625f08eb7791f3b8054d2e281bee242

---
+++ Ringtoets/Common/test/Ringtoets.Common.Data.Test/Hydraulics/CalculationConvergenceTest.cs
@@ -0,0 +1,27 @@
+﻿using Core.Common.Utils.Attributes;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Hydraulics;
+
+namespace Ringtoets.Common.Data.Test.Hydraulics
+{
+    [TestFixture]
+    public class CalculationConvergenceTest
+    {
+        [Test]
+        public void DisplayName_Always_ReturnExpectedValues()
+        {
+            // Assert
+            Assert.AreEqual("", GetDisplayName(CalculationConvergence.NotCalculated));
+            Assert.AreEqual("Nee", GetDisplayName(CalculationConvergence.CalculatedNotConverged));
+            Assert.AreEqual("Ja", GetDisplayName(CalculationConvergence.CalculatedConverged));
+        }
+
+        private string GetDisplayName(CalculationConvergence value)
+        {
+            var type = typeof(CalculationConvergence);
+            var memInfo = type.GetMember(value.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(ResourcesDisplayNameAttribute), false);
+            return ((ResourcesDisplayNameAttribute) attributes[0]).DisplayName;
+        }
+    }
+}
