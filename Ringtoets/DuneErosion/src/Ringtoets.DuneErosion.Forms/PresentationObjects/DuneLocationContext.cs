eba51640db5903b4dfc12625dba12a8f671b9442
Robert Tangerman <robert.tangerman@deltares.nl>
2017-01-11 09:27:04 +0000

    Introduced context for dune location. Related to Issue [WTI-1044]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@7725 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 2ff6dbf8212d75c551a6803fc38d4b47eeadfef2
    Former-commit-id: c20e60c95cea49015a1fea25dc66b10c4a9ffbc1

---
+++ Ringtoets/DuneErosion/src/Ringtoets.DuneErosion.Forms/PresentationObjects/DuneLocationContext.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base;
+using Core.Common.Controls.PresentationObjects;
+using Ringtoets.DuneErosion.Data;
+
+namespace Ringtoets.DuneErosion.Forms.PresentationObjects
+{
+    /// <summary>
+    /// Presentation object for all data required to configure an instance of a <see cref="DuneLocation"/>
+    /// </summary>
+    public class DuneLocationContext : ObservableWrappedObjectContextBase<ObservableList<DuneLocation>>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="DuneLocationContext"/>.
+        /// </summary>
+        /// <param name="wrappedList">The <see cref="ObservableList{T}"/> which the <see cref="DuneLocationContext"/>
+        /// belongs to.</param>
+        /// <param name="duneLocation">The <see cref="DuneLocation"/> which the <see cref="DuneLocationContext"/>
+        /// belongs to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public DuneLocationContext(ObservableList<DuneLocation> wrappedList, DuneLocation duneLocation)
+            : base(wrappedList)
+        {
+            if (duneLocation == null)
+            {
+                throw new ArgumentNullException(nameof(duneLocation));
+            }
+
+            DuneLocation = duneLocation;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Data.DuneLocation"/>.
+        /// </summary>
+        public DuneLocation DuneLocation { get; }
+
+        public override bool Equals(WrappedObjectContextBase<ObservableList<DuneLocation>> other)
+        {
+            return base.Equals(other) && ReferenceEquals(((DuneLocationContext)other).DuneLocation, DuneLocation);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() ^ DuneLocation.GetHashCode();
+        }
+    }
+}
---
+++ Ringtoets/DuneErosion/test/Ringtoets.DuneErosion.Forms.Test/PresentationObjects/DuneLocationContextTest.cs
@@ -0,0 +1,173 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base;
+using Core.Common.Controls.PresentationObjects;
+using NUnit.Framework;
+using Ringtoets.DuneErosion.Data;
+using Ringtoets.DuneErosion.Data.TestUtil;
+using Ringtoets.DuneErosion.Forms.PresentationObjects;
+
+namespace Ringtoets.DuneErosion.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class DuneLocationContextTest
+    {
+        [Test]
+        public void Constructor_DuneLocationNull_ThrowArgumentNullException()
+        {
+            // Setup
+            var locations = new ObservableList<DuneLocation>();
+
+            // Call
+            TestDelegate test = () => new DuneLocationContext(locations, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(test);
+            Assert.AreEqual("duneLocation", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var locations = new ObservableList<DuneLocation>();
+            var location = new TestDuneLocation();
+
+            // Call
+            var context = new DuneLocationContext(locations, location);
+
+            // Assert
+            Assert.IsInstanceOf<ObservableWrappedObjectContextBase<ObservableList<DuneLocation>>>(context);
+            Assert.AreSame(locations, context.WrappedData);
+            Assert.AreSame(location, context.DuneLocation);
+        }
+
+        [Test]
+        public void Equals_ToNull_ReturnFalse()
+        {
+            // Setup
+            var duneLocations = new ObservableList<DuneLocation>();
+            var duneLocation = new TestDuneLocation();
+
+            var context = new DuneLocationContext(duneLocations, duneLocation);
+
+            // Call
+            var isEqual = context.Equals(null);
+
+            // Assert
+            Assert.IsFalse(isEqual);
+        }
+
+        [Test]
+        public void Equals_ToItself_ReturnTrue()
+        {
+            // Setup
+            var duneLocations = new ObservableList<DuneLocation>();
+            var duneLocation = new TestDuneLocation();
+
+            var context = new DuneLocationContext(duneLocations, duneLocation);
+
+            // Call
+            var isEqual = context.Equals(context);
+
+            // Assert
+            Assert.IsTrue(isEqual);
+        }
+
+        [Test]
+        public void Equals_ToOtherWithDifferentLocationListAndSameLocation_ReturnFalse()
+        {
+            // Setup
+            var duneLocations1 = new ObservableList<DuneLocation>();
+            var duneLocations2 = new ObservableList<DuneLocation>();
+            var duneLocation = new TestDuneLocation();
+            var context1 = new DuneLocationContext(duneLocations1, duneLocation);
+            var context2 = new DuneLocationContext(duneLocations2, duneLocation);
+
+            // Call
+            var isEqual1 = context1.Equals(context2);
+            var isEqual2 = context2.Equals(context1);
+
+            // Assert
+            Assert.IsFalse(isEqual1);
+            Assert.IsFalse(isEqual2);
+        }
+
+        [Test]
+        public void Equals_ToOtherWithSameLocationListAndDifferentLocation_ReturnFalse()
+        {
+            // Setup
+            var duneLocations = new ObservableList<DuneLocation>();
+            var duneLocation1 = new TestDuneLocation();
+            var duneLocation2 = new TestDuneLocation();
+            var context1 = new DuneLocationContext(duneLocations, duneLocation1);
+            var context2 = new DuneLocationContext(duneLocations, duneLocation2);
+
+            // Call
+            var isEqual1 = context1.Equals(context2);
+            var isEqual2 = context2.Equals(context1);
+
+            // Assert
+            Assert.IsFalse(isEqual1);
+            Assert.IsFalse(isEqual2);
+        }
+
+        [Test]
+        public void Equals_ToOtherWithSameLocationListAndSameLocation_ReturnTrue()
+        {
+            // Setup
+            var duneLocations = new ObservableList<DuneLocation>();
+            var duneLocation = new TestDuneLocation();
+            var context1 = new DuneLocationContext(duneLocations, duneLocation);
+            var context2 = new DuneLocationContext(duneLocations, duneLocation);
+
+            // Call
+            var isEqual1 = context1.Equals(context2);
+            var isEqual2 = context2.Equals(context1);
+
+            // Assert
+            Assert.IsTrue(isEqual1);
+            Assert.IsTrue(isEqual2);
+        }
+
+        [Test]
+        public void GetHashCode_EqualObjects_ReturnSameHashCode()
+        {
+            // Setup
+            var duneLocations = new ObservableList<DuneLocation>();
+            var duneLocation = new TestDuneLocation();
+            var context1 = new DuneLocationContext(duneLocations, duneLocation);
+            var context2 = new DuneLocationContext(duneLocations, duneLocation);
+
+            // Precondition
+            Assert.AreEqual(context1, context2);
+
+            // Call
+            var hashCode1 = context1.GetHashCode();
+            var hashCode2 = context2.GetHashCode();
+
+            // Assert
+            Assert.AreEqual(hashCode1, hashCode2);
+        }
+    }
+}
