83fbfb9ccbeaef0916d3485f89d48fdae9b8eb1d
Marten Janssen <marten.janssen@deltares.nl>
2017-01-19 13:32:44 +0000

    Added MigrationScript and tests Related to Issue [WTI-1060]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@7843 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 65015386453e4eb1ec8fda058944afe0d8bf9163
    Former-commit-id: 25ae7c77687b108e6ab5702a71bc107c9bed76dc

---
+++ Migration/Scripts/src/Migration.Scripts.Data/MigrationScript.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Data.SQLite;
+using System.IO;
+using Migration.Scripts.Data.Exceptions;
+
+namespace Migration.Scripts.Data
+{
+    /// <summary>
+    /// Class that provides methods for migration a <see cref="VersionedFile"/>.
+    /// </summary>
+    public class MigrationScript
+    {
+        private readonly CreateScript createScript;
+        private readonly UpgradeScript upgradeScript;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="MigrationScript"/>.
+        /// </summary>
+        /// <param name="createScript">The create script to use for migrating.</param>
+        /// <param name="upgradeScript">The upgrade script to use for migrating.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any of the input parameters is <c>null</c>.</exception>
+        public MigrationScript(CreateScript createScript, UpgradeScript upgradeScript)
+        {
+            if (createScript == null)
+            {
+                throw new ArgumentNullException(nameof(createScript));
+            }
+            if (upgradeScript == null)
+            {
+                throw new ArgumentNullException(nameof(upgradeScript));
+            }
+            this.createScript = createScript;
+            this.upgradeScript = upgradeScript;
+        }
+
+        /// <summary>
+        /// Gets the supported version.
+        /// </summary>
+        /// <returns>The supported version.</returns>
+        public string SupportedVersion()
+        {
+            return upgradeScript.FromVersion();
+        }
+
+        /// <summary>
+        /// Gets the target version.
+        /// </summary>
+        /// <returns>The target version.</returns>
+        public string TargetVersion()
+        {
+            return upgradeScript.ToVersion();
+        }
+
+        /// <summary>
+        /// Uses <paramref name="sourceVersionedFile"/> to upgrade to a new <see cref="VersionedFile"/>.
+        /// </summary>
+        /// <param name="sourceVersionedFile"></param>
+        /// <returns>The upgraded <see cref="VersionedFile"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="sourceVersionedFile"/> is <c>null</c>.</exception>
+        /// <exception cref="CriticalDatabaseMigrationException">Thrown when migration failed.</exception>
+        public VersionedFile Upgrade(VersionedFile sourceVersionedFile)
+        {
+            if (sourceVersionedFile == null)
+            {
+                throw new ArgumentNullException(nameof(sourceVersionedFile));
+            }
+            var newLocation = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+            try
+            {
+                VersionedFile newVersionedFile = createScript.CreateEmptyVersionedFile(newLocation);
+                upgradeScript.Upgrade(sourceVersionedFile, newVersionedFile);
+                return newVersionedFile;
+            }
+            catch (SQLiteException exception)
+            {
+                throw new CriticalDatabaseMigrationException("Migratie is niet gelukt.", exception);
+            }
+        }
+    }
+}
---
+++ Migration/Scripts/test/Migration.Scripts.Data.Test/MigrationScriptTest.cs
@@ -0,0 +1,131 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Data.SQLite;
+using System.IO;
+using Core.Common.TestUtil;
+using Migration.Scripts.Data.Exceptions;
+using NUnit.Framework;
+
+namespace Migration.Scripts.Data.Test
+{
+    [TestFixture]
+    public class MigrationScriptTest
+    {
+        [Test]
+        public void Constructor_CreateScriptNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var upgradeScript = new UpgradeScript("1", "2", ";");
+
+            // Call
+            TestDelegate call = () => new MigrationScript(null, upgradeScript);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("createScript", paramName);
+        }
+
+        [Test]
+        public void Constructor_UpgradeScriptNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var createScript = new CreateScript("1", ";");
+
+            // Call
+            TestDelegate call = () => new MigrationScript(createScript, null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("upgradeScript", paramName);
+        }
+
+        [Test]
+        public void Constructor_ValidParameters_ExpectedProperties()
+        {
+            // Setup
+            var createScript = new CreateScript("2", ";");
+            var upgradeScript = new UpgradeScript("1", "2", ";");
+
+            // Call
+            var migrationScript = new MigrationScript(createScript, upgradeScript);
+
+            // Assert
+            Assert.AreEqual(upgradeScript.FromVersion(), migrationScript.SupportedVersion());
+            Assert.AreEqual(upgradeScript.ToVersion(), migrationScript.TargetVersion());
+        }
+
+        [Test]
+        public void Upgrade_VersionedFileNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var createScript = new CreateScript("2", ";");
+            var upgradeScript = new UpgradeScript("1", "2", ";");
+            var migrationScript = new MigrationScript(createScript, upgradeScript);
+
+            // Call
+            TestDelegate call = () => migrationScript.Upgrade(null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("sourceVersionedFile", paramName);
+        }
+
+        [Test]
+        public void Upgrade_UpgradeFails_ThrowsCriticalDatabaseMigrationException()
+        {
+            // Setup
+            var createScript = new CreateScript("2", "THIS WILL FAIL");
+            var upgradeScript = new UpgradeScript("1", "2", ";");
+            var migrationScript = new MigrationScript(createScript, upgradeScript);
+            var versionedFile = new VersionedFile("c:\\file.ext");
+
+            // Call
+            TestDelegate call = () => migrationScript.Upgrade(versionedFile);
+
+            // Assert
+            CriticalDatabaseMigrationException exception = Assert.Throws<CriticalDatabaseMigrationException>(call);
+            Assert.AreEqual("Migratie is niet gelukt.", exception.Message);
+            Assert.IsInstanceOf<SQLiteException>(exception.InnerException);
+        }
+
+        [Test]
+        public void Upgrade_ValidParameters_ExpectedProperties()
+        {
+            // Setup
+            var createScript = new CreateScript("2", ";");
+            var upgradeScript = new UpgradeScript("1", "2", ";");
+            var migrationScript = new MigrationScript(createScript, upgradeScript);
+            var versionedFile = new VersionedFile("c:\\file.ext");
+
+            // Call
+            VersionedFile upgradedFile = migrationScript.Upgrade(versionedFile);
+
+            // Assert
+            Assert.IsNotNull(upgradedFile);
+            using (new FileDisposeHelper(upgradedFile.Location))
+            {
+                Assert.IsTrue(File.Exists(upgradedFile.Location));
+            }
+        }
+    }
+}
