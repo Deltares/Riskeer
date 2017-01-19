818656bda5265c4053d1d7a3bec2d0259080e2e0
Marten Janssen <marten.janssen@deltares.nl>
2017-01-19 13:28:33 +0000

    Added CreateScript and tests Related to Issue [WTI-1060]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@7841 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 9ccbcf2cf74252fd1b860fec9136f88be0f262fd
    Former-commit-id: c085291180d42080bfdbeb05e773a163f23523e6

---
+++ Migration/Scripts/src/Migration.Scripts.Data/CreateScript.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Data.SQLite;
+using Core.Common.Utils;
+
+namespace Migration.Scripts.Data
+{
+    /// <summary>
+    /// Class that provides methods for the creating a <see cref="VersionedFile"/> for a specific version.
+    /// </summary>
+    public class CreateScript
+    {
+        private readonly string createQuery;
+        private readonly string version;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="CreateScript"/> class.
+        /// </summary>
+        /// <param name="version">The version <paramref name="query"/> was designed for.</param>
+        /// <param name="query">The SQL query that belongs to <paramref name="version"/>.</param>
+        /// <exception cref="ArgumentException">Thrown when:
+        /// <list type="bullet">
+        /// <item><paramref name="version"/> is empty or <c>null</c>,</item>
+        /// <item><paramref name="query"/> is empty, <c>null</c>, or consist out of only whitespace characters.</item>
+        /// </list></exception>
+        public CreateScript(string version, string query)
+        {
+            if (string.IsNullOrEmpty(version))
+            {
+                throw new ArgumentException(@"Version must have a value.", nameof(version));
+            }
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                throw new ArgumentException(@"Query must have a value.", nameof(query));
+            }
+            this.version = version;
+            createQuery = query;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="VersionedFile"/> at <paramref name="location"/>.
+        /// </summary>
+        /// <param name="location">The location to store the <see cref="VersionedFile"/>.</param>
+        /// <returns>A new <see cref="VersionedFile"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="location"/>:
+        /// <list type="bullet">
+        /// <item>is empty or <c>null</c>,</item>
+        /// <item>consists out of only whitespace characters,</item>
+        /// <item>contains an invalid character,</item>
+        /// <item>ends with a directory or path separator (empty file name),</item>
+        /// <item>is not writable.</item>
+        /// </list></exception>
+        /// <exception cref="SQLiteException">Thrown when executing query failed.</exception>
+        /// 
+        public VersionedFile CreateEmptyVersionedFile(string location)
+        {
+            FileUtils.ValidateFilePathIsWritable(location);
+
+            using (var databaseFile = new RingtoetsDatabaseFile(location))
+            {
+                databaseFile.OpenDatabaseConnection();
+                databaseFile.CreateStructure(createQuery);
+            }
+
+            return new VersionedFile(location);
+        }
+
+        /// <summary>
+        /// Gets the version <see cref="CreateScript"/> was created for.
+        /// </summary>
+        /// <returns>The version.</returns>
+        public string Version()
+        {
+            return version;
+        }
+    }
+}
---
+++ Migration/Scripts/test/Migration.Scripts.Data.Test/CreateScriptTest.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.IO;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+
+namespace Migration.Scripts.Data.Test
+{
+    [TestFixture]
+    public class CreateScriptTest
+    {
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        public void Constructor_InvalidVersion_ThrowsArgumentException(string version)
+        {
+            // Setup
+            const string query = "Valid query";
+
+            // Call
+            TestDelegate call = () => new CreateScript(version, query);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentException>(call).ParamName;
+            Assert.AreEqual("version", paramName);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase(null)]
+        public void Constructor_InvalidQuery_ThrowsArgumentException(string query)
+        {
+            // Setup
+            const string version = "Valid version";
+
+            // Call
+            TestDelegate call = () => new CreateScript(version, query);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentException>(call).ParamName;
+            Assert.AreEqual("query", paramName);
+        }
+
+        [Test]
+        public void Constructor_ValidParameters_ExpectedValues()
+        {
+            // Setup
+            const string query = "Valid query";
+            const string version = "Valid version";
+
+            // Call
+            var createScript = new CreateScript(version, query);
+
+            // Assert
+            Assert.AreEqual(version, createScript.Version());
+        }
+
+        [Test]
+        public void CreateEmptyVersionedFile_FileDoesNotExist_ReturnsVersionedFile()
+        {
+            // Setup
+            const string query = ";";
+            const string version = "Valid version";
+
+            string targetFilename = Path.GetRandomFileName();
+            string filePath = TestHelper.GetTestDataPath(TestDataPath.Migration.Core.Storage, targetFilename);
+            var createScript = new CreateScript(version, query);
+
+            // Call
+            VersionedFile versionedFile = createScript.CreateEmptyVersionedFile(filePath);
+
+            // Assert
+            Assert.IsTrue(File.Exists(versionedFile.Location));
+            using (new FileDisposeHelper(filePath)) {}
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase(null)]
+        public void CreateEmptyVersionedFile_InvalidPath_ThrowsArgumentException(string filePath)
+        {
+            // Setup
+            const string query = ";";
+            const string version = "Valid version";
+            var createScript = new CreateScript(version, query);
+
+            // Call
+            TestDelegate call = () => createScript.CreateEmptyVersionedFile(filePath);
+
+            // Assert
+            string message = Assert.Throws<ArgumentException>(call).Message;
+            Assert.AreEqual($"Fout bij het lezen van bestand '{filePath}': bestandspad mag niet leeg of ongedefinieerd zijn.", message);
+        }
+
+        [Test]
+        public void CreateEmptyVersionedFile_FileExistsButNotWritable_ThrowsArgumentException()
+        {
+            // Setup
+            const string query = ";";
+            const string version = "Valid version";
+
+            string filename = Path.GetRandomFileName();
+            string filePath = TestHelper.GetTestDataPath(TestDataPath.Migration.Core.Storage, filename);
+            var createScript = new CreateScript(version, query);
+
+            using (new FileDisposeHelper(filePath))
+            {
+                FileAttributes attributes = File.GetAttributes(filePath);
+                File.SetAttributes(filePath, attributes | FileAttributes.ReadOnly);
+
+                // Call
+                TestDelegate call = () => createScript.CreateEmptyVersionedFile(filePath);
+
+                // Assert
+                ArgumentException exception = Assert.Throws<ArgumentException>(call);
+                Assert.AreEqual("path", exception.ParamName);
+                File.SetAttributes(filePath, attributes);
+            }
+        }
+    }
+}
