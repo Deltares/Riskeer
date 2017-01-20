0bf0b88b0ee95f604d7ef5242670371d0da71c38
Dennis Tang <dennis.tang@deltares.nl>
2017-01-20 10:45:20 +0000

    Added a property class for the BackgroundMapDataContext with specified properties
    Related to Issue [WTI-1086]
    
    
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@7861 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 255d4f29819db3c3478a8e92339b9be5bf7567bb
    Former-commit-id: 865bb75aa434ddbe0f9cd5f49f945a2f870cf250

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/PropertyClasses/BackgroundMapDataContextProperties.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Base.Data;
+using Core.Common.Gui.PropertyBag;
+using Core.Common.Utils.Attributes;
+using Ringtoets.Integration.Forms.PresentationObjects;
+using Ringtoets.Integration.Forms.Properties;
+using RingtoetsCommonForms = Ringtoets.Common.Forms.Properties.Resources;
+
+namespace Ringtoets.Integration.Forms.PropertyClasses
+{
+    /// <summary>
+    /// ViewModel of the <see cref="BackgroundMapDataContext"/> for properties panel.
+    /// </summary>
+    public class BackgroundMapDataContextProperties : ObjectProperties<BackgroundMapDataContext>
+    {
+        [ResourcesCategory(typeof(RingtoetsCommonForms), nameof(RingtoetsCommonForms.Categories_General))]
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.BackgroundMapDataContextProperties_Name_DisplayName))]
+        [ResourcesDescription(typeof(Resources), nameof(Resources.BackgroundMapDataContextProperties_Name_Description))]
+        public string Name
+        {
+            get
+            {
+                return data.WrappedData.Name;
+            }
+        }
+
+        [ResourcesCategory(typeof(RingtoetsCommonForms), nameof(RingtoetsCommonForms.Categories_General))]
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.BackgroundMapDataContextProperties_Url_DisplayName))]
+        [ResourcesDescription(typeof(Resources), nameof(Resources.BackgroundMapDataContextProperties_Url_Description))]
+        public string Url
+        {
+            get
+            {
+                return data.WrappedData.SourceCapabilitiesUrl;
+            }
+        }
+
+        [ResourcesCategory(typeof(RingtoetsCommonForms), nameof(RingtoetsCommonForms.Categories_General))]
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.BackgroundMapDataContextProperties_Transparency_DisplayName))]
+        [ResourcesDescription(typeof(Resources), nameof(Resources.BackgroundMapDataContextProperties_Transparency_Description))]
+        public RoundedDouble Transparency
+        {
+            get
+            {
+                return data.WrappedData.Transparency;
+            }
+            set
+            {
+                data.WrappedData.Transparency = value.ToPrecision(data.WrappedData.Transparency.NumberOfDecimalPlaces);
+                data.WrappedData.NotifyObservers();
+            }
+        }
+
+        [ResourcesCategory(typeof(RingtoetsCommonForms), nameof(RingtoetsCommonForms.Categories_General))]
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.BackgroundMapDataContextProperties_IsVisible_DisplayName))]
+        [ResourcesDescription(typeof(Resources), nameof(Resources.BackgroundMapDataContextProperties_IsVisible_Description))]
+        public bool IsVisible
+        {
+            get
+            {
+                return data.WrappedData.IsVisible;
+            }
+            set
+            {
+                data.WrappedData.IsVisible = value;
+                data.WrappedData.NotifyObservers();
+            }
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/PropertyClasses/BackgroundMapDataContextPropertiesTest.cs
@@ -0,0 +1,147 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using Core.Common.Base;
+using Core.Common.Base.Data;
+using Core.Common.Gui.PropertyBag;
+using Core.Common.TestUtil;
+using Core.Components.Gis.Data;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Integration.Forms.PresentationObjects;
+using Ringtoets.Integration.Forms.PropertyClasses;
+
+namespace Ringtoets.Integration.Forms.Test.PropertyClasses
+{
+    [TestFixture]
+    public class BackgroundMapDataContextPropertiesTest
+    {
+        private const int requiredNamePropertyIndex = 0;
+        private const int requiredUrlPropertyIndex = 1;
+        private const int requiredTransparencyPropertyIndex = 2;
+        private const int requiredVisibilityPropertyIndex = 3;
+
+        [Test]
+        public void GetProperties_ValidData_ReturnsExpectedValues()
+        {
+            // Setup
+            WmtsMapData mapData = WmtsMapData.CreateDefaultPdokMapData();
+            var mapDataContext = new BackgroundMapDataContext(mapData);
+
+            // Call
+            var properties = new BackgroundMapDataContextProperties
+            {
+                Data = mapDataContext
+            };
+
+            // Assert
+            Assert.IsInstanceOf<ObjectProperties<BackgroundMapDataContext>>(properties);
+
+            Assert.AreSame(mapDataContext, properties.Data);
+
+            Assert.AreEqual(mapData.Name, properties.Name);
+            Assert.AreEqual(mapData.SourceCapabilitiesUrl, properties.Url);
+
+            Assert.AreEqual(2, properties.Transparency.NumberOfDecimalPlaces);
+            Assert.AreEqual(mapData.Transparency, properties.Transparency);
+            Assert.AreEqual(mapData.IsVisible, properties.IsVisible);
+        }
+
+        [Test]
+        public void SetProperties_IndividualProperties_UpdateDataAndNotifyObservers()
+        {
+            // Setup
+            var mockRepository = new MockRepository();
+            var observer = mockRepository.StrictMock<IObserver>();
+            const int numberOfChangedProperties = 2;
+            observer.Expect(o => o.UpdateObserver()).Repeat.Times(numberOfChangedProperties);
+            mockRepository.ReplayAll();
+
+            var random = new Random(21);
+            var newTransparency = (RoundedDouble)random.NextDouble();
+            var newVisibility = random.NextBoolean();
+
+            WmtsMapData mapData = WmtsMapData.CreateDefaultPdokMapData();
+            mapData.Attach(observer);
+
+            var properties = new BackgroundMapDataContextProperties
+            {
+                Data = new BackgroundMapDataContext(mapData)
+            };
+
+            // Call
+            properties.IsVisible = newVisibility;
+            properties.Transparency = newTransparency;
+
+            // Assert
+            Assert.AreEqual(newTransparency, properties.Transparency, properties.Transparency.GetAccuracy());
+            Assert.AreEqual(newVisibility, properties.IsVisible);
+
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void Constructor_Always_PropertiesHaveExpectedAttributesValues()
+        {
+            // Setup
+            WmtsMapData mapData = WmtsMapData.CreateDefaultPdokMapData();
+
+            // Call
+            var properties = new BackgroundMapDataContextProperties
+            {
+                Data = new BackgroundMapDataContext(mapData)
+            };
+
+            // Assert
+            PropertyDescriptorCollection dynamicProperties = PropertiesTestHelper.GetAllVisiblePropertyDescriptors(properties);
+            Assert.AreEqual(4, dynamicProperties.Count);
+
+            PropertyDescriptor nameProperty = dynamicProperties[requiredNamePropertyIndex];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(nameProperty,
+                                                                            "Algemeen",
+                                                                            "Naam van de kaartlaag",
+                                                                            "Naam van de achtergrond kaartlaag.",
+                                                                            true);
+
+            PropertyDescriptor urlProperty = dynamicProperties[requiredUrlPropertyIndex];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(urlProperty,
+                                                                            "Algemeen",
+                                                                            "URL naar de wms / wmts service",
+                                                                            "Volledige URL naar de wms of wmts service die als achtergrond kaartlaag gebruikt wordt.",
+                                                                            true);
+
+            PropertyDescriptor transparencyPropertyIndex = dynamicProperties[requiredTransparencyPropertyIndex];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(transparencyPropertyIndex,
+                                                                            "Algemeen",
+                                                                            "Transparantie",
+                                                                            "Transparantie waarmee de achtergrondlaag wordt weergegeven.");
+
+            PropertyDescriptor visibilityProperty = dynamicProperties[requiredVisibilityPropertyIndex];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(visibilityProperty,
+                                                                            "Algemeen",
+                                                                            "Achtergrondlaag tonen",
+                                                                            "Geeft aan of de geselecteerde achtergrondlaag in alle kaarten wordt weergegeven.");
+        }
+    }
+}
