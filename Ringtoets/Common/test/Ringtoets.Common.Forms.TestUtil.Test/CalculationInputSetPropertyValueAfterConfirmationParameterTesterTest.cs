b40918ecce110abdd8be9aef9ee156153bd412c3
Robert Tangerman <robert.tangerman@deltares.nl>
2017-01-24 13:39:48 +0000

    Added tester for calculation input property changes. Related to Issue [WTI-1053]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@7930 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 6c3110172fb98777fa4d882107cede2e4d55bca3
    Former-commit-id: 0d52a523fd19b6cb5d17d0914f83843fd5353baf

---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.TestUtil.Test/CalculationInputSetPropertyValueAfterConfirmationParameterTesterTest.cs
@@ -0,0 +1,166 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using Core.Common.Base;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Common.Forms.PropertyClasses;
+
+namespace Ringtoets.Common.Forms.TestUtil.Test
+{
+    [TestFixture]
+    public class CalculationInputSetPropertyValueAfterConfirmationParameterTesterTest
+    {
+        [Test]
+        public void Constructructed_Always_PropertiesSet()
+        {
+            // Setup
+            const double value = 3.0;
+            var testCalculationInput = new TestCalculationInput();
+            var testCalculation = new TestCalculation();
+            var returnedAffectedObjects = Enumerable.Empty<IObservable>();
+
+            // Call
+            var tester = new CalculationInputSetPropertyValueAfterConfirmationParameterTester<ICalculationInput, ICalculation, double>(
+                testCalculationInput, testCalculation, value, returnedAffectedObjects);
+
+            // Assert
+            Assert.IsInstanceOf<ICalculationInputPropertyChangeHandler<ICalculationInput, ICalculation>>(tester);
+            Assert.AreSame(testCalculationInput, tester.ExpectedCalculationInput);
+            Assert.AreSame(testCalculation, tester.ExpectedCalculation);
+            Assert.AreEqual(value, tester.ExpectedValue);
+            Assert.AreSame(returnedAffectedObjects, tester.ReturnedAffectedObjects);
+            Assert.IsFalse(tester.Called);
+        }
+
+        [Test]
+        public void SetPropertyValueAfterConfirmation_CalculationInputPassedNotSame_ThrowsAssertionException()
+        {
+            // Setup
+            const double value = 3.0;
+            var testCalculationInput = new TestCalculationInput();
+            var passedCalculationInput = new TestCalculationInput();
+            var testCalculation = new TestCalculation();
+            var returnedAffectedObjects = Enumerable.Empty<IObservable>();
+
+            var tester = new CalculationInputSetPropertyValueAfterConfirmationParameterTester<ICalculationInput, ICalculation, double>(
+                testCalculationInput, testCalculation, value, returnedAffectedObjects);
+
+            // Call
+            TestDelegate test = () => tester.SetPropertyValueAfterConfirmation(passedCalculationInput, testCalculation, value, (input, v) => { });
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+            Assert.IsTrue(tester.Called);
+        }
+
+        [Test]
+        public void SetPropertyValueAfterConfirmation_CalculationPassedNotSame_ThrowsAssertionException()
+        {
+            // Setup
+            const double value = 3.0;
+            var testCalculationInput = new TestCalculationInput();
+            var testCalculation = new TestCalculation();
+            var passedCalculation = new TestCalculation();
+            var returnedAffectedObjects = Enumerable.Empty<IObservable>();
+
+            var tester = new CalculationInputSetPropertyValueAfterConfirmationParameterTester<ICalculationInput, ICalculation, double>(
+                testCalculationInput, testCalculation, value, returnedAffectedObjects);
+
+            // Call
+            TestDelegate test = () => tester.SetPropertyValueAfterConfirmation(testCalculationInput, passedCalculation, value, (input, v) => { });
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+            Assert.IsTrue(tester.Called);
+        }
+
+        [Test]
+        public void SetPropertyValueAfterConfirmation_ValuePassedNotEqual_ThrowsAssertionException()
+        {
+            // Setup
+            const double expectedValue = 3.0;
+            const double passedValue = 2.0;
+            var testCalculationInput = new TestCalculationInput();
+            var testCalculation = new TestCalculation();
+            var returnedAffectedObjects = Enumerable.Empty<IObservable>();
+
+            var tester = new CalculationInputSetPropertyValueAfterConfirmationParameterTester<ICalculationInput, ICalculation, double>(
+                testCalculationInput, testCalculation, expectedValue, returnedAffectedObjects);
+
+            // Call
+            TestDelegate test = () => tester.SetPropertyValueAfterConfirmation(testCalculationInput, testCalculation, passedValue, (input, v) => { });
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+            Assert.IsTrue(tester.Called);
+        }
+
+        [Test]
+        public void SetPropertyValueAfterConfirmation_ParametersAreSameAndEqual_SetValueCalledReturnsGivenAffectedObjects()
+        {
+            // Setup
+            const double value = 3.0;
+
+            var returnedAffectedObjects = Enumerable.Empty<IObservable>();
+            var testCalculationInput = new TestCalculationInput();
+            var testCalculation = new TestCalculation();
+            var called = 0;
+
+            var tester = new CalculationInputSetPropertyValueAfterConfirmationParameterTester<ICalculationInput, ICalculation, double>(
+                testCalculationInput, testCalculation, value, returnedAffectedObjects);
+
+            // Call
+            var affectedObjects = tester.SetPropertyValueAfterConfirmation(testCalculationInput, testCalculation, value, (m, v) => called++);
+
+            // Assert
+            Assert.AreEqual(1, called);
+            Assert.AreSame(returnedAffectedObjects, affectedObjects);
+            Assert.IsTrue(tester.Called);
+        }
+
+        [Test]
+        public void SetPropertyValueAfterConfirmation_SetValueThrowsException_BubblesException()
+        {
+            // Setup
+            const double value = 3.0;
+
+            var returnedAffectedObjects = Enumerable.Empty<IObservable>();
+            var testCalculationInput = new TestCalculationInput();
+            var testCalculation = new TestCalculation();
+
+             var tester = new CalculationInputSetPropertyValueAfterConfirmationParameterTester<ICalculationInput, ICalculation, double>(
+                testCalculationInput, testCalculation, value, returnedAffectedObjects);
+
+            var expectedException = new Exception();
+
+            // Call
+            TestDelegate test = () => tester.SetPropertyValueAfterConfirmation(testCalculationInput, testCalculation, value, (input, v) => { throw expectedException; });
+
+            // Assert
+            var exception = Assert.Throws<Exception>(test);
+            Assert.AreSame(expectedException, exception);
+            Assert.IsTrue(tester.Called);
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.TestUtil/CalculationInputSetPropertyValueAfterConfirmationParameterTester.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Core.Common.Base;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Common.Forms.PropertyClasses;
+
+namespace Ringtoets.Common.Forms.TestUtil
+{
+    /// <summary>
+    /// This class can be used in tests to verify that the correct arguments 
+    /// are passed to the <see cref="ICalculationInputPropertyChangeHandler{TCalculationInput,TCalculation}.SetPropertyValueAfterConfirmation{TValue}"/> method.
+    /// </summary>
+    /// <typeparam name="TCalculationInput">The type of the calculation input that is expected to be passed to the method.</typeparam>
+    /// <typeparam name="TCalculation">The type of  the calculation that is expected to be passed to the method.</typeparam>
+    /// <typeparam name="TExpectedValue">The type of the value that is epected to be passed to the method.</typeparam>    
+    public class CalculationInputSetPropertyValueAfterConfirmationParameterTester<TCalculationInput, TCalculation, TExpectedValue>
+        : ICalculationInputPropertyChangeHandler<TCalculationInput, TCalculation>
+        where TCalculationInput : ICalculationInput
+        where TCalculation : ICalculation
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="CalculationInputSetPropertyValueAfterConfirmationParameterTester{TCalculationInput,TCalculation,TExpectedValue}"/>.
+        /// </summary>
+        /// <param name="expectedCalculationInput">The calculation input that is expected to be passed to the <see cref="SetPropertyValueAfterConfirmation{TValue}"/>.</param>
+        /// <param name="expectedCalculation">The calculation that is expected to be passed to the <see cref="SetPropertyValueAfterConfirmation{TValue}"/>. </param>
+        /// <param name="expectedValue">The value that is expected to be passed to the <see cref="SetPropertyValueAfterConfirmation{TValue}"/>.</param>
+        /// <param name="returnedAffectedObjects">The affected object that are returned by <see cref="SetPropertyValueAfterConfirmation{TValue}"/>.</param>
+        public CalculationInputSetPropertyValueAfterConfirmationParameterTester(TCalculationInput expectedCalculationInput,
+            TCalculation expectedCalculation,
+                                                                                TExpectedValue expectedValue,
+                                                                                IEnumerable<IObservable> returnedAffectedObjects)
+        {
+            ExpectedCalculationInput = expectedCalculationInput;
+            ExpectedCalculation = expectedCalculation;
+            ExpectedValue = expectedValue;
+            ReturnedAffectedObjects = returnedAffectedObjects;
+        }
+
+        /// <summary>
+        /// Gets a value representing whether <see cref="SetPropertyValueAfterConfirmation{TValue}"/> was called.
+        /// </summary>
+        public bool Called { get; private set; }
+
+        /// <summary>
+        /// Gets the calculation input that is expected to be passed to the <see cref="SetPropertyValueAfterConfirmation{TValue}"/>.
+        /// </summary>
+        public TCalculationInput ExpectedCalculationInput { get; }
+
+        /// <summary>
+        /// Gets the calculation that is expected to be passed to the <see cref="SetPropertyValueAfterConfirmation{TValue}"/>.
+        /// </summary>
+        public TCalculation ExpectedCalculation { get; }
+
+        /// <summary>
+        /// Gets the value that is expected to be passed to the <see cref="SetPropertyValueAfterConfirmation{TValue}"/>.
+        /// </summary>
+        public TExpectedValue ExpectedValue { get; }
+
+        /// <summary>
+        /// Gets the affected object that are returned by <see cref="SetPropertyValueAfterConfirmation{TValue}"/>.
+        /// </summary>
+        public IEnumerable<IObservable> ReturnedAffectedObjects { get; }
+
+        public IEnumerable<IObservable> SetPropertyValueAfterConfirmation<TValue>(
+            TCalculationInput calculationInput,
+            TCalculation calculation,
+            TValue value,
+            SetCalculationInputPropertyValueDelegate<TCalculationInput, TValue> setValue)
+        {
+            Called = true;
+            Assert.AreSame(ExpectedCalculationInput, calculationInput);
+            Assert.AreSame(ExpectedCalculation, calculation);
+            Assert.AreEqual(ExpectedValue, value);
+            setValue(calculationInput, value);
+            return ReturnedAffectedObjects;
+        }
+    }
+}
