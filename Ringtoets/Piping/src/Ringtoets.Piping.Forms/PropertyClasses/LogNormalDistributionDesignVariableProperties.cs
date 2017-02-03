b7ddde12390bd07ae5bfaad1f96a1b9e5d4196f0
Robert Tangerman <robert.tangerman@deltares.nl>
2017-02-03 15:20:28 +0000

    Introduced LogNormalDistributionDesignVariableProperties. Related to Issue [WTI-1053]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@8073 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 2e4c83180e866ee1233244007973cab3055b6ae9
    Former-commit-id: da2bf317960c1c64ebc73d6fb9e78116f2e5b8b8

---
+++ Ringtoets/Piping/src/Ringtoets.Piping.Forms/PropertyClasses/LogNormalDistributionDesignVariableProperties.cs
@@ -0,0 +1,125 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.Data;
+using Core.Common.Utils.Attributes;
+using Ringtoets.Common.Data.Probabilistics;
+using Ringtoets.Common.Forms.PropertyClasses;
+using Ringtoets.Piping.Data;
+using Ringtoets.Piping.Forms.Properties;
+using RingtoetsCommonFormsResources = Ringtoets.Common.Forms.Properties.Resources;
+
+namespace Ringtoets.Piping.Forms.PropertyClasses
+{
+    /// <summary>
+    /// ViewModel of <see cref="DesignVariable{TDistributionType}"/> of <see cref="LogNormalDistribution"/> for properties panel.
+    /// </summary>
+    public class LogNormalDistributionDesignVariableProperties
+        : PipingDistributionPropertiesBase<LogNormalDistribution, PipingInput, PipingCalculationScenario>
+    {
+        private readonly DesignVariable<LogNormalDistribution> designVariable;
+
+        /// <summary>
+        /// Creates a new <see cref="LogNormalDistributionDesignVariableProperties"/>.
+        /// </summary>
+        /// <param name="propertiesReadOnly">Indicates which properties, if any, should be marked as read-only.</param>
+        /// <param name="designVariable">The <see cref="DesignVariable{T}"/> to create the properties for.</param>
+        /// <param name="calculation">The calculation the <paramref name="designVariable"/> belgons to.</param>
+        /// <param name="calculationInput">The calculation input the <paramref name="designVariable"/> belongs to.</param>
+        /// <param name="handler">The handler responsible for handling effects of a property change.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="designVariable"/> is <c>null</c>
+        /// or when any number of properties in this class is editable and any other parameter is <c>null</c>.</exception>
+        public LogNormalDistributionDesignVariableProperties(DistributionPropertiesReadOnly propertiesReadOnly,
+                                                             DesignVariable<LogNormalDistribution> designVariable,
+                                                             PipingCalculationScenario calculation,
+                                                             PipingInput calculationInput,
+                                                             ICalculationInputPropertyChangeHandler<PipingInput, PipingCalculationScenario> handler)
+            : base(propertiesReadOnly,
+                   GetDistribution(designVariable),
+                   calculation,
+                   calculationInput,
+                   handler)
+        {
+            this.designVariable = designVariable;
+        }
+
+        public override string DistributionType { get; } = RingtoetsCommonFormsResources.DistributionType_LogNormal;
+
+        [ResourcesDescription(typeof(RingtoetsCommonFormsResources), nameof(RingtoetsCommonFormsResources.LogNormalDistribution_Mean_Description))]
+        public override RoundedDouble Mean
+        {
+            get
+            {
+                return base.Mean;
+            }
+            set
+            {
+                base.Mean = value;
+            }
+        }
+
+        [ResourcesDescription(typeof(RingtoetsCommonFormsResources), nameof(RingtoetsCommonFormsResources.LogNormalDistribution_StandardDeviation_Description))]
+        public override RoundedDouble StandardDeviation
+        {
+            get
+            {
+                return base.StandardDeviation;
+            }
+            set
+            {
+                base.StandardDeviation = value;
+            }
+        }
+
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.DesignVariableTypeConverter_DesignValue_DisplayName))]
+        [ResourcesDescription(typeof(Resources), nameof(Resources.DesignVariableTypeConverter_DesignValue_Description))]
+        public RoundedDouble DesignValue
+        {
+            get
+            {
+                return designVariable.GetDesignValue();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{DesignValue} ({RingtoetsCommonFormsResources.NormalDistribution_Mean_DisplayName} = {Mean}, " +
+                   $"{RingtoetsCommonFormsResources.NormalDistribution_StandardDeviation_DisplayName} = {StandardDeviation})";
+        }
+
+        /// <summary>
+        /// Gets the <see cref="LogNormalDistribution"/> of the <see cref="DesignVariable{T}"/>.
+        /// </summary>
+        /// <param name="logNormalDesignVariable">The design variable to get the distribution from.</param>
+        /// <returns>The distribution of the design variable.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="logNormalDesignVariable"/>
+        /// is <c>null</c>.</exception>
+        private static LogNormalDistribution GetDistribution(DesignVariable<LogNormalDistribution> logNormalDesignVariable)
+        {
+            if (logNormalDesignVariable == null)
+            {
+                throw new ArgumentNullException(nameof(logNormalDesignVariable));
+            }
+            return logNormalDesignVariable.Distribution;
+        }
+    }
+}
