3f73936da98ec936963df7f4487cb152371a40f5
Marten Janssen <marten.janssen@deltares.nl>
2017-02-07 09:11:29 +0000

    Created WmtsCapability that holds information about a WMTS capability Created WmtsCapabilityRow that will be used to display the Wmts capability in a DataGridView Related to Issue [WTI-1091]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@8117 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: dcc75c032cedc38bcf0097c8e3c619a7e5e3f58d
    Former-commit-id: 17745d87105387dc9fa3bb4bb2bf476acf1fb296

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Views/WmtsCapabilityRow.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+
+namespace Ringtoets.Integration.Forms.Views
+{
+    /// <summary>
+    /// Class for displaying a <see cref="WmtsCapability"/> as a row in a grid view.
+    /// </summary>
+    public class WmtsCapabilityRow
+    {
+        private readonly WmtsCapability wmtsCapability;
+
+        /// <summary>
+        /// Creates new instance of <see cref="WmtsCapabilityRow"/>.
+        /// </summary>
+        /// <param name="wmtsCapability">The <see cref="WmtsCapability"/> to wrap
+        /// so that it can be displayed as a row.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="wmtsCapability"/> is <c>null</c>.</exception>
+        public WmtsCapabilityRow(WmtsCapability wmtsCapability)
+        {
+            if (wmtsCapability == null)
+            {
+                throw new ArgumentNullException(nameof(wmtsCapability));
+            }
+            this.wmtsCapability = wmtsCapability;
+        }
+
+        /// <summary>
+        /// Gets the id of the <see cref="WmtsCapability"/>.
+        /// </summary>
+        public string Id
+        {
+            get
+            {
+                return wmtsCapability.Id;
+            }
+        }
+
+        /// <summary>
+        /// Gets the image format of the <see cref="WmtsCapability"/>.
+        /// </summary>
+        public string Format
+        {
+            get
+            {
+                return wmtsCapability.Format;
+            }
+        }
+
+        /// <summary>
+        /// Gets the title of the <see cref="WmtsCapability"/>.
+        /// </summary>
+        public string Title
+        {
+            get
+            {
+                return wmtsCapability.Title;
+            }
+        }
+
+        /// <summary>
+        /// Gets the coordinate system of the <see cref="WmtsCapability"/>.
+        /// </summary>
+        public string CoordinateSystem
+        {
+            get
+            {
+                return wmtsCapability.CoordinateSystem;
+            }
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/Views/WmtsCapabilityRowTest.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+using Ringtoets.Integration.Forms.Views;
+
+namespace Ringtoets.Integration.Forms.Test.Views
+{
+    [TestFixture]
+    public class WmtsCapabilityRowTest
+    {
+        [Test]
+        public void Constructor_WmtsCapabilityNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new WmtsCapabilityRow(null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("wmtsCapability", paramName);
+        }
+
+        [Test]
+        public void Constructor_ValidWmtsCapability_ExpectedProperties()
+        {
+            // Setup
+            const string id = "laag1(abc)";
+            const string format = "image/png";
+            const string title = "Eerste kaartlaag";
+            const string coordinateSystem = "Coördinatenstelsel";
+            var wmtsCapability = new WmtsCapability(id, format, title, coordinateSystem);
+
+            // Call
+            var row = new WmtsCapabilityRow(wmtsCapability);
+
+            // Assert
+            Assert.AreEqual(id, row.Id);
+            Assert.AreEqual(format, row.Format);
+            Assert.AreEqual(title, row.Title);
+            Assert.AreEqual(coordinateSystem, row.CoordinateSystem);
+        }
+    }
+}
