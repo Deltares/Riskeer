36b1ab8fe4c395ade52f057cb2ce07960d04098c
Robert Tangerman <robert.tangerman@deltares.nl>
2017-02-16 10:42:18 +0000

    Implemented first part of the PipingConfigurationExporter Related to Issue [WTI-1134]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@8268 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 9264e48bdd41609b4745fa27e30e2062875af6ef
    Former-commit-id: 926294310b46a0ea9e5895f240525617a200b828

---
+++ Ringtoets/Piping/src/Ringtoets.Piping.IO/Exporters/PipingConfigurationExporter.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.IO;
+using Core.Common.Utils;
+using Ringtoets.Common.Data.Calculation;
+
+namespace Ringtoets.Piping.IO.Exporters
+{
+    /// <summary>
+    /// Exports a piping configuration and stores it as a xml file.
+    /// </summary>
+    public class PipingConfigurationExporter : IFileExporter
+    {
+        private readonly CalculationGroup calculationGroup;
+        private readonly string filePath;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="PipingConfigurationExporter"/>.
+        /// </summary>
+        /// <param name="calculationGroup">The calculation group to export.</param>
+        /// <param name="filePath">The path of the file to export to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="calculationGroup"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="filePath"/> is invalid.</exception>
+        public PipingConfigurationExporter(CalculationGroup calculationGroup, string filePath)
+        {
+            if (calculationGroup == null)
+            {
+                throw new ArgumentNullException(nameof(calculationGroup));
+            }
+
+            IOUtils.ValidateFilePath(filePath);
+
+            this.calculationGroup = calculationGroup;
+            this.filePath = filePath;
+        }
+
+        public bool Export()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Ringtoets/Piping/test/Ringtoets.Piping.IO.Test/Exporters/PipingConfigurationExporterTest.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.IO;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Piping.IO.Exporters;
+
+namespace Ringtoets.Piping.IO.Test.Exporters
+{
+    [TestFixture]
+    public class PipingConfigurationExporterTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var exporter = new PipingConfigurationExporter(new CalculationGroup(), "test.xml");
+
+            // Assert
+            Assert.IsInstanceOf<IFileExporter>(exporter);
+        }
+
+        [Test]
+        public void Constructor_CalculationGroupNull_ThrowArgumentNullException()
+        {
+            // Call
+            TestDelegate test = () => new PipingConfigurationExporter(null, "test.xml");
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(test);
+            Assert.AreEqual("calculationGroup", exception.ParamName);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase("c:\\>")]
+        public void Constructor_FilePathInvalid_ThrowArgumentException(string filePath)
+        {
+            // Call
+            TestDelegate test = () => new PipingConfigurationExporter(new CalculationGroup(), filePath);
+
+            // Assert
+            Assert.Throws<ArgumentException>(test);
+        }
+    }
+}
