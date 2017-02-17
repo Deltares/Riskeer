18123986eb930726584d136a4f222c2bfde9ae25
Marten Janssen <marten.janssen@deltares.nl>
2017-02-17 15:06:07 +0000

    Introduced DirectoryDisposeHelper Related to Issue [WTI-1091]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@8296 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: d89a7dd1e9fea5c031bac8cb2af2a62c49238721
    Former-commit-id: aa6617a6a6a9e5d3aa2b21451aaf0eacd4ad49c9

---
+++ Core/Common/test/Core.Common.TestUtil.Test/DirectoryDisposeHelperTest.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Core.Common.TestUtil.Test
+{
+    [TestFixture]
+    public class DirectoryDisposeHelperTest
+    {
+        private static readonly TestDataPath testPath = TestDataPath.Core.Common.TestUtils;
+
+        [Test]
+        public void Constructor_NotExistingFolder_CreatesFolder()
+        {
+            // Setup
+            string folderPath = TestHelper.GetTestDataPath(testPath, Path.GetRandomFileName());
+            var folderExists = false;
+
+            // Precondition
+            Assert.IsFalse(Directory.Exists(folderPath), $"Precondition failed: Folder '{folderPath}' should not exist");
+
+            // Call
+            using (new DirectoryDisposeHelper(folderPath))
+            {
+                folderExists = Directory.Exists(folderPath);
+            }
+
+            // Assert
+            Assert.IsTrue(folderExists);
+            if (Directory.Exists(folderPath))
+            {
+                Directory.Delete(folderPath, true);
+                Assert.Fail($"Folder path '{folderPath}' was not removed.");
+            }
+        }
+
+        [Test]
+        public void Constructor_ExistingFolder_DoesNotThrowException()
+        {
+            // Setup
+            string folderPath = TestHelper.GetTestDataPath(testPath, Path.GetRandomFileName());
+            DirectoryDisposeHelper disposeHelper = null;
+
+            try
+            {
+                Directory.CreateDirectory(folderPath);
+
+                // Call
+                TestDelegate test = () => disposeHelper = new DirectoryDisposeHelper(folderPath);
+
+                // Assert
+                Assert.DoesNotThrow(test);
+                disposeHelper.Dispose();
+            }
+            catch (Exception exception)
+            {
+                Assert.Fail(exception.Message);
+            }
+            finally
+            {
+                if (Directory.Exists(folderPath))
+                {
+                    Directory.Delete(folderPath, true);
+                    Assert.Fail($"Folder path '{folderPath}' was not removed.");
+                }
+            }
+        }
+
+        [Test]
+        [TestCase("/fo:der/")]
+        [TestCase("f*lder")]
+        public void Constructor_InvalidFolderPath_ThrowsArgumentException(string folderPath)
+        {
+            // Call
+            TestDelegate test = () => new DirectoryDisposeHelper(folderPath);
+
+            // Assert
+            Assert.Throws<ArgumentException>(test);
+        }
+
+        [Test]
+        public void Dispose_AlreadyDisposed_DoesNotThrowException()
+        {
+            // Setup
+            string folderPath = TestHelper.GetTestDataPath(testPath, Path.GetRandomFileName());
+
+            // Call
+            TestDelegate test = () =>
+            {
+                using (var directoryDisposeHelper = new DirectoryDisposeHelper(folderPath))
+                {
+                    directoryDisposeHelper.Dispose();
+                }
+            };
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+
+        [Test]
+        public void Dispose_FolderAlreadyRemoved_DoesNotThrowException()
+        {
+            // Setup
+            string folderPath = TestHelper.GetTestDataPath(testPath, Path.GetRandomFileName());
+
+            // Call
+            TestDelegate test = () =>
+            {
+                using (new DirectoryDisposeHelper(folderPath))
+                {
+                    Directory.Delete(folderPath, true);
+                }
+            };
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+    }
+}
---
+++ Core/Common/test/Core.Common.TestUtil/DirectoryDisposeHelper.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.IO;
+
+namespace Core.Common.TestUtil
+{
+    /// <summary>
+    /// This class can be used to set temporary folders while testing. 
+    /// Disposing an instance of this class will delete the folder(s).
+    /// </summary>
+    /// <example>
+    /// The following is an example for how to use this class:
+    /// <code>
+    /// using(new DirectoryDisposeHelper("path")) {
+    ///     // Perform tests with folders
+    /// }
+    /// </code>
+    /// </example>
+    public class DirectoryDisposeHelper : IDisposable
+    {
+        private readonly string folderPath;
+        private bool disposed;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="DirectoryDisposeHelper"/>.
+        /// </summary>
+        /// <param name="folderPath">Paths that will be created, if the path is valid.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="folderPath"/> could 
+        /// not be created by the system.</exception>
+        public DirectoryDisposeHelper(string folderPath)
+        {
+            this.folderPath = folderPath;
+            CreatePath();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            try
+            {
+                Directory.Delete(folderPath, true);
+            }
+            catch
+            {
+                // ignored
+            }
+
+            disposed = true;
+        }
+
+        /// <summary>
+        /// Creates the temporary folder path.
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown when the file could not be created by the system.</exception>
+        private void CreatePath()
+        {
+            try
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            catch (Exception e) when (e is DirectoryNotFoundException || e is IOException || e is NotSupportedException || e is UnauthorizedAccessException)
+            {
+                throw new ArgumentException(e.Message, e);
+            }
+        }
+    }
+}
