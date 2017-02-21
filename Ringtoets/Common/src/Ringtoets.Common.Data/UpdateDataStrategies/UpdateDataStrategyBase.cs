92d8502b4ffd316c6387a0b1378e4d097c43c58d
Dennis Tang <dennis.tang@deltares.nl>
2017-02-21 12:27:15 +0000

    Incorporated comments review 1468: - Refactored the UpdateDataStrategies by introducing a template class UpDataStrategyBase:
    Related to Issue [WTI-1075]
    
    
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@8358 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: d012126ecc4261bcb460eaa1c5fba7ed5bac1a9b
    Former-commit-id: d9faa4ce1d1f641bb915a6c1dba082f9f0c79aba

---
+++ Ringtoets/Common/src/Ringtoets.Common.Data/UpdateDataStrategies/UpdateDataStrategyBase.cs
@@ -0,0 +1,174 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base;
+using Core.Common.Utils;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Common.Data.UpdateDataStrategies
+{
+    /// <summary>
+    /// Strategy for updating the current collection and dependent data with imported data:
+    /// <list type="bullet">
+    /// <item>Adds imported items that are not part of the current collection.</item>
+    /// <item>Removes items that are part of the current collection, but are not part of the imported item collection.</item>
+    /// <item>Updates the items that are part of the current collection and are part of the imported item collection.</item>
+    /// </list>
+    /// </summary>
+    /// <typeparam name="TTargetData">The target data type.</typeparam>
+    /// <typeparam name="TFeature">The feature of the target data that should be validated on for the uniqueness of elements.</typeparam>
+    /// <typeparam name="TFailureMechanism">The failure mechanism in which the target collection should be updated.</typeparam>
+    public abstract class UpdateDataStrategyBase<TTargetData, TFeature, TFailureMechanism>
+        where TTargetData : class
+        where TFeature : class
+        where TFailureMechanism : IFailureMechanism
+    {
+        protected TFailureMechanism failureMechanism;
+        private readonly IEqualityComparer<TTargetData> equalityComparer;
+
+        /// <summary>
+        /// Instantiates a <see cref="UpdateDataStrategyBase{TObject,TFeature,TFailureMechanism}"/> object.
+        /// </summary>
+        /// <param name="failureMechanism">The failure mechanism which needs to be updated.</param>
+        /// <param name="equalityComparer">The comparer which should be used to determine when two objects are equal</param>
+        /// <exception cref="ArgumentNullException">Thrown when any input parameter is <c>null</c>.</exception>
+        protected UpdateDataStrategyBase(TFailureMechanism failureMechanism, IEqualityComparer<TTargetData> equalityComparer)
+        {
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+            if (equalityComparer == null)
+            {
+                throw new ArgumentNullException(nameof(equalityComparer));
+            }
+
+            this.equalityComparer = equalityComparer;
+            this.failureMechanism = failureMechanism;
+        }
+
+        /// <summary>
+        /// Updates the (dependent) objects with new data from the imported data.
+        /// </summary>
+        /// <param name="objectsToUpdate">Objects that need to be updated.</param>
+        /// <param name="importedDataCollection">The data that was imported.</param>
+        /// <returns>An <see cref="IEnumerable{IObservable}"/> with affected objects.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when duplicate items are found.</exception>
+        protected abstract IEnumerable<IObservable> UpdateData(IEnumerable<TTargetData> objectsToUpdate,
+                                                               IEnumerable<TTargetData> importedDataCollection);
+
+        /// <summary>
+        /// Removes the objects and their dependent data.
+        /// </summary>
+        /// <param name="removedObjects">The objects that are removed.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> with affected objects.</returns>
+        protected abstract IEnumerable<IObservable> RemoveData(IEnumerable<TTargetData> removedObjects);
+
+        /// <summary>
+        /// Updates the items and their associated data within the target collection with the data contained 
+        /// in the imported data collection.
+        /// </summary>
+        /// <param name="targetDataCollection">The target collection that needs to be updated.</param>
+        /// <param name="importedDataCollection">The imported data collection that is used to update
+        /// the <paramref name="targetDataCollection"/>.</param>
+        /// <param name="sourceFilePath">The source file path.</param>
+        /// <returns>A <see cref="IEnumerable{T}"/> of affected objects.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the input parameters are <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when duplicate items are being added to the 
+        /// <paramref name="targetDataCollection"/>.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when duplicate items are found during the 
+        /// update of the items to be updatd in the <paramref name="targetDataCollection"/>.</exception>
+        protected IEnumerable<IObservable> UpdateTargetCollectionData(ObservableUniqueItemCollectionWithSourcePath<TTargetData, TFeature> targetDataCollection,
+                                                                      IEnumerable<TTargetData> importedDataCollection,
+                                                                      string sourceFilePath)
+        {
+            if (targetDataCollection == null)
+            {
+                throw new ArgumentNullException(nameof(targetDataCollection));
+            }
+            if (importedDataCollection == null)
+            {
+                throw new ArgumentNullException(nameof(importedDataCollection));
+            }
+            if (sourceFilePath == null)
+            {
+                throw new ArgumentNullException(nameof(sourceFilePath));
+            }
+
+            return ModifyDataCollection(targetDataCollection, importedDataCollection, sourceFilePath);
+        }
+
+        /// <summary>
+        /// Identifies which models were changed, removed and added to the target collection 
+        /// when compared with the imported data and performs the necessary operations for 
+        /// the dependent data of the affected elements. 
+        /// </summary>
+        /// <param name="targetDataCollection">The target data collection which needs to be updated.</param>
+        /// <param name="importedDataCollection">The imported data collection which is used to update 
+        /// the <paramref name="targetDataCollection"/> </param>
+        /// <param name="sourceFilePath">The source file path.</param>
+        /// <returns>A <see cref="IEnumerable{T}"/> with affected objects.</returns>
+        /// <exception cref="ArgumentException">Thrown when duplicate items are being added to the 
+        /// <paramref name="targetDataCollection"/>.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when duplicate items are found during the 
+        /// update of the items to be updatd in the <paramref name="targetDataCollection"/>.</exception>
+        private IEnumerable<IObservable> ModifyDataCollection(ObservableUniqueItemCollectionWithSourcePath<TTargetData, TFeature> targetDataCollection,
+                                                          IEnumerable<TTargetData> importedDataCollection,
+                                                          string sourceFilePath)
+        {
+            TTargetData[] importedObjects = importedDataCollection.ToArray();
+            TTargetData[] addedObjects = GetAddedObjects(targetDataCollection, importedObjects).ToArray();
+            TTargetData[] removedObjects = GetRemovedObjects(targetDataCollection, importedObjects).ToArray();
+            TTargetData[] updatedObjects = GetUpdatedObjects(targetDataCollection, importedObjects).ToArray();
+
+            var affectedObjects = new List<IObservable>();
+            if (addedObjects.Any())
+            {
+                affectedObjects.Add(targetDataCollection);
+            }
+            affectedObjects.AddRange(UpdateData(targetDataCollection, importedObjects));
+            affectedObjects.AddRange(RemoveData(removedObjects));
+            
+            targetDataCollection.Clear();
+            targetDataCollection.AddRange(addedObjects.Union(updatedObjects), sourceFilePath);
+
+            return affectedObjects.Distinct(new ReferenceEqualityComparer<IObservable>());
+        }
+
+        private IEnumerable<TTargetData> GetRemovedObjects(IEnumerable<TTargetData> existingCollection, IEnumerable<TTargetData> importedDataOjects)
+        {
+            return existingCollection.Except(importedDataOjects, equalityComparer);
+        }
+
+        private IEnumerable<TTargetData> GetUpdatedObjects(IEnumerable<TTargetData> existingCollection, IEnumerable<TTargetData> importedDataObjects)
+        {
+            return existingCollection.Intersect(importedDataObjects, equalityComparer);
+        }
+
+        private IEnumerable<TTargetData> GetAddedObjects(IEnumerable<TTargetData> existingCollection, IEnumerable<TTargetData> importedDataObjects)
+        {
+            return importedDataObjects.Where(source => !existingCollection.Contains(source, equalityComparer));
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Data.Test/UpdateDataStrategies/UpdateDataStrategyBaseTest.cs
@@ -0,0 +1,266 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base;
+using NUnit.Framework;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Common.Data.UpdateDataStrategies;
+
+namespace Ringtoets.Common.Data.Test.UpdateDataStrategies
+{
+    [TestFixture]
+    public class UpdateDataStrategyBaseTest
+    {
+        private readonly Func<TestItem, string> getUniqueFeature = item => item.Name;
+        private const string typeDescriptor = "TestItem";
+        private const string featureDescription = "naam";
+
+        [Test]
+        public void DefaultConstructor_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new ConcreteUpdateDataStrategy(null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("failureMechanism", paramName);
+        }
+
+        [Test]
+        public void DefaultConstructor_EqualityComparerNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new ConcreteUpdateDataStrategy(new TestFailureMechanism(), null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("equalityComparer", paramName);
+        }
+
+        [Test]
+        public void DefaultConstructor_FailureMechanismNotNull_DoesNotThrowException()
+        {
+            // Call
+            TestDelegate call = () => new ConcreteUpdateDataStrategy(new TestFailureMechanism());
+
+            // Assert
+            Assert.DoesNotThrow(call);
+        }
+
+        [Test]
+        public void UpdateTargetCollectionData_TargetCollectionNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var strategy = new ConcreteUpdateDataStrategy(new TestFailureMechanism());
+
+            // Call
+            TestDelegate call = () => strategy.ConcreteUpdateData(null, Enumerable.Empty<TestItem>(), string.Empty);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("targetDataCollection", paramName);
+        }
+
+        [Test]
+        public void UpdateTargetCollectionData_ImportedDataCollectionNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var strategy = new ConcreteUpdateDataStrategy(new TestFailureMechanism());
+            var collection = new ObservableUniqueItemCollectionWithSourcePath<TestItem, string>(
+                getUniqueFeature, typeDescriptor, featureDescription);
+
+            // Call
+            TestDelegate call = () => strategy.ConcreteUpdateData(collection, null, string.Empty);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("importedDataCollection", paramName);
+        }
+
+        [Test]
+        public void UpdateTargetCollectionData_SourceFilePathNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var strategy = new ConcreteUpdateDataStrategy(new TestFailureMechanism());
+            var collection = new ObservableUniqueItemCollectionWithSourcePath<TestItem, string>(
+                getUniqueFeature, typeDescriptor, featureDescription);
+
+            // Call
+            TestDelegate call = () => strategy.ConcreteUpdateData(collection, Enumerable.Empty<TestItem>(), null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("sourceFilePath", paramName);
+        }
+
+        [Test]
+        public void UpdateTargetCollectionData_WithCollectionAndImportedDataEmpty_ClearsTargetCollection()
+        {
+            // Setup
+            var collection = new ObservableUniqueItemCollectionWithSourcePath<TestItem, string>(
+                getUniqueFeature, typeDescriptor, featureDescription);
+            const string filePath = "path";
+            collection.AddRange(new[]
+            {
+                new TestItem("Name A"),
+                new TestItem("Name B")
+            }, filePath);
+
+            var strategy = new ConcreteUpdateDataStrategy(new TestFailureMechanism());
+
+            // Call
+            strategy.ConcreteUpdateData(collection, Enumerable.Empty<TestItem>(), filePath);
+
+            // Assert
+            CollectionAssert.IsEmpty(collection);
+            Assert.AreEqual(filePath, collection.SourcePath);
+        }
+
+        [Test]
+        public void UpdateTargetCollectionData_Call_CallsFunctions()
+        {
+            // Setup
+            var collection = new ObservableUniqueItemCollectionWithSourcePath<TestItem, string>(
+               getUniqueFeature, typeDescriptor, featureDescription);
+
+            var strategy = new ConcreteUpdateDataStrategy(new TestFailureMechanism());
+
+            // Call
+            strategy.ConcreteUpdateData(collection, Enumerable.Empty<TestItem>(), "path");
+
+            // Assert
+            Assert.IsTrue(strategy.IsUpdateDataCalled);
+            Assert.IsTrue(strategy.IsRemoveDataCalled);
+        }
+
+        [Test]
+        public void UpdateTargetCollectionData_WithEmptyCollectionAndImportedDataCollectionNotEmpty_AddsNewItems()
+        {
+            // Setup
+            var collection = new ObservableUniqueItemCollectionWithSourcePath<TestItem, string>(
+                getUniqueFeature, typeDescriptor, featureDescription);
+            var importedCollection = new[]
+            {
+                new TestItem("Name A"),
+                new TestItem("Name B")
+            };
+
+            var strategy = new ConcreteUpdateDataStrategy(new TestFailureMechanism());
+
+            // Call
+            IEnumerable<IObservable> affectedObjects = strategy.ConcreteUpdateData(collection, importedCollection, "path");
+
+            // Assert
+            CollectionAssert.AreEqual(importedCollection, collection);
+            CollectionAssert.Contains(affectedObjects, collection);
+            Assert.AreEqual("path", collection.SourcePath);
+        }
+
+        [Test]
+        public void UpdateTargetCollectionData_ImportedDataContainsDuplicateData_ThrowsArgumentException()
+        {
+            // Setup
+            var collection = new ObservableUniqueItemCollectionWithSourcePath<TestItem, string>(
+                getUniqueFeature, typeDescriptor, featureDescription);
+
+            const string duplicateName = "Duplicate Name";
+            var importedCollection = new[]
+            {
+                new TestItem(duplicateName),
+                new TestItem(duplicateName)
+            };
+
+            var strategy = new ConcreteUpdateDataStrategy(new TestFailureMechanism());
+
+            // Call
+            TestDelegate call = () => strategy.ConcreteUpdateData(collection, importedCollection, "path");
+
+            // Assert
+            var exception = Assert.Throws<ArgumentException>(call);
+            string message = $"{typeDescriptor} moeten een unieke {featureDescription} hebben. Gevonden dubbele elementen: {duplicateName}.";
+            Assert.AreEqual(message, exception.Message);
+
+            CollectionAssert.IsEmpty(collection);
+        }
+        
+        private class ConcreteUpdateDataStrategy : UpdateDataStrategyBase<TestItem, string, TestFailureMechanism>
+        {
+            public bool IsUpdateDataCalled { get; private set; }
+            public bool IsRemoveDataCalled { get; private set; }
+
+            public ConcreteUpdateDataStrategy(TestFailureMechanism failureMechanism, IEqualityComparer<TestItem> comparer)
+                : base(failureMechanism, comparer) {}
+
+            public ConcreteUpdateDataStrategy(TestFailureMechanism failureMechanism)
+                : base(failureMechanism, new NameComparer()) {}
+
+            public IEnumerable<IObservable> ConcreteUpdateData(ObservableUniqueItemCollectionWithSourcePath<TestItem, string> targetCollection,
+                                                               IEnumerable<TestItem> importedDataCollection,
+                                                               string sourceFilePath)
+            {
+                return UpdateTargetCollectionData(targetCollection, importedDataCollection, sourceFilePath);
+            }
+
+            protected override IEnumerable<IObservable> UpdateData(IEnumerable<TestItem> objectsToUpdate, IEnumerable<TestItem> importedDataCollection)
+            {
+                IsUpdateDataCalled = true;
+                return Enumerable.Empty<IObservable>();
+            }
+
+            protected override IEnumerable<IObservable> RemoveData(IEnumerable<TestItem> removedObjects)
+            {
+                IsRemoveDataCalled = true;
+                return Enumerable.Empty<IObservable>();
+            }
+
+            private class NameComparer : IEqualityComparer<TestItem>
+            {
+                public bool Equals(TestItem x, TestItem y)
+                {
+                    return x.Name == y.Name;
+                }
+
+                public int GetHashCode(TestItem obj)
+                {
+                    return obj.Name.GetHashCode();
+                }
+            }
+        }
+
+        private class TestItem
+        {
+            public string Name { get; }
+
+            public TestItem(string name)
+            {
+                Name = name;
+            }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+    }
+}
