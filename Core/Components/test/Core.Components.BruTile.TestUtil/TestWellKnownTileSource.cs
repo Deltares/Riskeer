6e700ec6ce8242c0c41a3c1028729995981ea44b
Marten Janssen <marten.janssen@deltares.nl>
2017-03-10 10:38:52 +0000

    Reordered MapControl Related to Issue [WTI-1143]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@8715 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 292d27c962f09f5d1a3e671ee4a087cf18854fc8
    Former-commit-id: ada6aebfb37ca8b72ece88f824759067428ef53b

---
+++ Core/Components/test/Core.Components.BruTile.TestUtil/TestWellKnownTileSource.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using BruTile;
+using BruTile.Predefined;
+using BruTile.Web;
+using Core.Components.BruTile.TestUtil.Properties;
+using Core.Components.Gis.Data;
+
+namespace Core.Components.BruTile.TestUtil
+{
+    /// <summary>
+    /// Defines an <see cref="ITileSource"/> suitable to most unit test cases related to
+    /// dealing with <see cref="WellKnownTileSourceMapData"/>.
+    /// </summary>
+    public class TestWellKnownTileSource : HttpTileSource
+    {
+        private static byte[] pngTileDataStub;
+
+        /// <summary>
+        /// Create a new instance of <see cref="TestWellKnownTileSource"/> suitable to work for
+        /// a given <see cref="WellKnownTileSourceMapData"/>.
+        /// </summary>
+        /// <param name="mapData">The map data to work with.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="mapData"/> is <c>null</c>.</exception>
+        public TestWellKnownTileSource(WellKnownTileSourceMapData mapData)
+            : base(CreateTileSchema(mapData), new WellKnownRequestStub(), "Stub schema", null, GetStubTile) {}
+
+        /// <summary>
+        /// Create a new tile schema.
+        /// </summary>
+        /// <param name="mapData">The map data to work with.</param>
+        /// <returns>The new tile schema.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="mapData"/> is <c>null</c>.</exception>
+        private static GlobalSphericalMercator CreateTileSchema(WellKnownTileSourceMapData mapData)
+        {
+            if (mapData == null)
+            {
+                throw new ArgumentNullException(nameof(mapData));
+            }
+            return new GlobalSphericalMercator(YAxis.TMS, 0, 19, mapData.Name);
+        }
+
+        private static byte[] GetStubTile(Uri url)
+        {
+            if (pngTileDataStub == null)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    Resources.stubTile.Save(stream, ImageFormat.Png);
+                    pngTileDataStub = stream.ToArray();
+                }
+            }
+            return pngTileDataStub;
+        }
+
+        private class WellKnownRequestStub : IRequest
+        {
+            public Uri GetUri(TileInfo info)
+            {
+                return new Uri(@"https://www.stub.org/tiles/someTile.png");
+            }
+        }
+    }
+}
