63f628ad3e252277b290dd3f08ad8a5a6c8b70e8
Elmar Kleijn <elmar.kleijn@deltares.nl>
2017-03-10 08:54:46 +0000

    Introduced CombinedXmlSchemaDefinition Related to Issue [WTI-1157]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@8706 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 6b98eb95a67d71ff662fdc2dd72d1572b64207b0
    Former-commit-id: c3b40339ff3e1ca37c81e435219fee129354bbf7

---
+++ Ringtoets/Common/src/Ringtoets.Common.IO/Readers/CombinedXmlSchemaDefinition.cs
@@ -0,0 +1,184 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Ringtoets.Common.IO.Readers
+{
+    /// <summary>
+    /// Container for one or more related/nested schema definitions, representing a combined
+    /// XML Schema Definition (XSD).
+    /// </summary>
+    public class CombinedXmlSchemaDefinition
+    {
+        private readonly XmlSchemaSet xmlSchemaSet;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="CombinedXmlSchemaDefinition"/>.
+        /// </summary>
+        /// <param name="mainSchemaDefinition">A string representing the main schema definition.</param>
+        /// <param name="nestedSchemaDefinitions">A <see cref="IDictionary{TKey,TValue}"/> containing
+        /// one or more nested schema definitions; the keys should represent unique file names by which
+        /// the schema definitions can be referenced from <paramref name="mainSchemaDefinition"/>, the
+        /// values should represent the corresponding schema definition strings.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="nestedSchemaDefinitions"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when:
+        /// <list type="bullet">
+        /// <item><paramref name="mainSchemaDefinition"/> is invalid.</item>
+        /// <item><paramref name="nestedSchemaDefinitions"/> contains invalid schema definition values.</item>
+        /// <item><paramref name="mainSchemaDefinition"/>, all together with its referenced
+        /// <paramref name="nestedSchemaDefinitions"/>, contains an invalid schema definition.</item>
+        /// <item><paramref name="nestedSchemaDefinitions"/> contains schema definitions that are not
+        /// referenced by <see cref="mainSchemaDefinition"/>.</item>
+        /// </list>
+        /// </exception>
+        public CombinedXmlSchemaDefinition(string mainSchemaDefinition, IDictionary<string, string> nestedSchemaDefinitions)
+        {
+            CheckSchemaDefinitions(mainSchemaDefinition, nestedSchemaDefinitions);
+
+            var nestedSchemaDefinitionsResolver = new NestedSchemaDefinitionsResolver(nestedSchemaDefinitions);
+            xmlSchemaSet = new XmlSchemaSet
+            {
+                XmlResolver = nestedSchemaDefinitionsResolver
+            };
+
+            CompileSchemaSet(mainSchemaDefinition);
+
+            if (!nestedSchemaDefinitionsResolver.AllNestedSchemaDefinitionsReferenced)
+            {
+                throw new ArgumentException($"'{nameof(nestedSchemaDefinitions)}' contains schema definitions that are not referenced");
+            }
+        }
+
+        /// <summary>
+        /// Check the provided schema definitions for not being <c>null</c>, empty or only containing white spaces.
+        /// </summary>
+        /// <param name="mainSchemaDefinition">A string representing the main schema definition.</param>
+        /// <param name="nestedSchemaDefinitions">A <see cref="IDictionary{TKey,TValue}"/> containing
+        /// one or more nested schema definitions.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="nestedSchemaDefinitions"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when:
+        /// <list type="bullet">
+        /// <item><paramref name="mainSchemaDefinition"/> is invalid.</item>
+        /// <item><paramref name="nestedSchemaDefinitions"/> contains invalid schema definition values.</item>
+        /// </list>
+        /// </exception>
+        private static void CheckSchemaDefinitions(string mainSchemaDefinition, IDictionary<string, string> nestedSchemaDefinitions)
+        {
+            if (string.IsNullOrWhiteSpace(mainSchemaDefinition))
+            {
+                throw new ArgumentException($"'{nameof(mainSchemaDefinition)}' null, empty or only containing white spaces.");
+            }
+
+            if (nestedSchemaDefinitions == null)
+            {
+                throw new ArgumentNullException(nameof(nestedSchemaDefinitions));
+            }
+
+            if (nestedSchemaDefinitions.Values.Any(string.IsNullOrWhiteSpace))
+            {
+                throw new ArgumentException($"'{nameof(nestedSchemaDefinitions)}' holds a nested schema definition value that equals null, is empty or only contains white spaces.");
+            }
+        }
+
+        /// <summary>
+        /// Compiles the <see cref="xmlSchemaSet"/> based on the provided main schema definition string.
+        /// </summary>
+        /// <param name="mainSchemaDefinition">A string representing the main schema definition.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="mainSchemaDefinition"/>,
+        /// all together with its referenced nested schema definitions, contains an invalid schema
+        /// definition.</exception>
+        private void CompileSchemaSet(string mainSchemaDefinition)
+        {
+            try
+            {
+                xmlSchemaSet.Add(XmlSchema.Read(new StringReader(mainSchemaDefinition), null));
+                xmlSchemaSet.Compile();
+            }
+            catch (Exception exception) when (exception is XmlException
+                                              || exception is XmlSchemaException)
+            {
+                throw new ArgumentException($"'{nameof(mainSchemaDefinition)}' containing invalid schema definition: {exception.Message}", exception);
+            }
+        }
+
+        /// <summary>
+        /// Resolver for nested schema definitions.
+        /// </summary>
+        private class NestedSchemaDefinitionsResolver : XmlResolver
+        {
+            private readonly IDictionary<string, string> nestedSchemaDefinitions;
+            private readonly IDictionary<string, bool> nestedSchemaDefinitionsUsage;
+
+            /// <summary>
+            /// Creates a new instance of <see cref="NestedSchemaDefinitionsResolver"/>.
+            /// </summary>
+            /// <param name="nestedSchemaDefinitions">A <see cref="IDictionary{TKey,TValue}"/> containing
+            /// one or more nested schema definitions.</param>
+            public NestedSchemaDefinitionsResolver(IDictionary<string, string> nestedSchemaDefinitions)
+            {
+                this.nestedSchemaDefinitions = nestedSchemaDefinitions;
+                nestedSchemaDefinitionsUsage = nestedSchemaDefinitions.Keys.ToDictionary(k => k, k => false);
+            }
+
+            public override ICredentials Credentials
+            {
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            /// <summary>
+            /// Gets whether or not all nested schema definitions are used.
+            /// </summary>
+            public bool AllNestedSchemaDefinitionsReferenced
+            {
+                get
+                {
+                    return nestedSchemaDefinitionsUsage.Values.All(v => v);
+                }
+            }
+
+            public override object GetEntity(Uri absoluteUri, string role, Type ofObjectToReturn)
+            {
+                string fileName = Path.GetFileName(absoluteUri.ToString());
+
+                if (nestedSchemaDefinitions.ContainsKey(fileName))
+                {
+                    nestedSchemaDefinitionsUsage[fileName] = true;
+                    return new MemoryStream(Encoding.UTF8.GetBytes(nestedSchemaDefinitions[fileName]));
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.IO.Test/Readers/CombinedXmlSchemaDefinitionTest.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Common.IO.Readers;
+
+namespace Ringtoets.Common.IO.Test.Readers
+{
+    [TestFixture]
+    public class CombinedXmlSchemaDefinitionTest
+    {
+        private readonly string validMainSchemaDefinition;
+
+        private readonly string testDirectoryPath = TestHelper.GetTestDataPath(TestDataPath.Ringtoets.Common.IO,
+                                                                               "CombinedXmlSchemaDefinition");
+
+        [Test]
+        [TestCase("")]
+        [TestCase("      ")]
+        [TestCase(null)]
+        public void Constructor_InvalidMainSchemaDefinition_ThrowArgumentException(string invalidMainSchemaString)
+        {
+            // Call
+            TestDelegate call = () => new CombinedXmlSchemaDefinition(invalidMainSchemaString, new Dictionary<string, string>());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentException>(call);
+            Assert.AreEqual("'mainSchemaDefinition' null, empty or only containing white spaces.", exception.Message);
+        }
+
+        [Test]
+        public void Constructor_NestedSchemaDefinitionsNull_ThrowArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new CombinedXmlSchemaDefinition(validMainSchemaDefinition, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("nestedSchemaDefinitions", exception.ParamName);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("      ")]
+        [TestCase(null)]
+        public void Constructor_InvalidNestedSchemaDefinitions_ThrowArgumentException(string invalidNestedSchemaString)
+        {
+            // Call
+            TestDelegate call = () => new CombinedXmlSchemaDefinition(validMainSchemaDefinition, new Dictionary<string, string>
+            {
+                {
+                    "Test", invalidNestedSchemaString
+                }
+            });
+
+            // Assert
+            var exception = Assert.Throws<ArgumentException>(call);
+            Assert.AreEqual("'nestedSchemaDefinitions' holds a nested schema definition value that equals null, is empty or only contains white spaces.", exception.Message);
+        }
+
+        public CombinedXmlSchemaDefinitionTest()
+        {
+            validMainSchemaDefinition = File.ReadAllText(Path.Combine(testDirectoryPath, "validConfigurationSchema.xsd"));
+        }
+    }
+}
