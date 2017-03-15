637f851f0b3e92ae9bc1d09f1f462332fc3eef73
Elmar Kleijn <elmar.kleijn@deltares.nl>
2017-03-15 15:46:21 +0000

    Split up GrassCoverErosionOutwardsWaveConditionsCalculationContextExportInfoTest Related to Issue [WTI-1157]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@8811 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 24fb832e65ac3e8e5577093f06708607949eb8b3
    Former-commit-id: b4ad01843ddb562ed43a3eb1740b454944ae59d7

---
+++ Ringtoets/GrassCoverErosionOutwards/test/Ringtoets.GrassCoverErosionOutwards.Plugin.Test/ExportInfos/GrassCoverErosionOutwardsWaveConditionsCalculationContextWaveConditionsExportInfoTest.cs
@@ -0,0 +1,144 @@
+﻿// Copyright (C) Stichting Deltares 2016. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Linq;
+using Core.Common.Base.IO;
+using Core.Common.Gui;
+using Core.Common.Gui.Plugin;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.GrassCoverErosionOutwards.Data;
+using Ringtoets.GrassCoverErosionOutwards.Forms.PresentationObjects;
+using Ringtoets.Revetment.IO.Exporters;
+using Ringtoets.Revetment.TestUtil;
+
+namespace Ringtoets.GrassCoverErosionOutwards.Plugin.Test.ExportInfos
+{
+    [TestFixture]
+    public class GrassCoverErosionOutwardsWaveConditionsCalculationContextWaveConditionsExportInfoTest
+    {
+        private ExportInfo exportInfo;
+
+        [SetUp]
+        public void Setup()
+        {
+            using (var plugin = new GrassCoverErosionOutwardsPlugin())
+            {
+                exportInfo = plugin.GetExportInfos()
+                                   .Single(ei => ei.DataType == typeof(GrassCoverErosionOutwardsWaveConditionsCalculationContext)
+                                                 && ei.Name.Equals("Berekende belastingen bij verschillende waterstanden (*.csv)."));
+            }
+        }
+
+        [Test]
+        public void Initialized_Always_ExpectedPropertiesSet()
+        {
+            // Assert
+            Assert.IsNotNull(exportInfo.CreateFileExporter);
+            Assert.IsNotNull(exportInfo.IsEnabled);
+            Assert.IsNull(exportInfo.Category);
+            Assert.IsNull(exportInfo.Image);
+            Assert.IsNotNull(exportInfo.FileFilterGenerator);
+        }
+
+        [Test]
+        public void CreateFileExporter_Always_ReturnFileExporter()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new GrassCoverErosionOutwardsFailureMechanism();
+
+            var context = new GrassCoverErosionOutwardsWaveConditionsCalculationContext(new GrassCoverErosionOutwardsWaveConditionsCalculation(),
+                                                                                        failureMechanism, assessmentSection);
+
+            // Call
+            IFileExporter fileExporter = exportInfo.CreateFileExporter(context, "test");
+
+            // Assert
+            Assert.IsInstanceOf<WaveConditionsExporterBase>(fileExporter);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void FileFilterGenerator_Always_ReturnFileFilter()
+        {
+            // Call
+            FileFilterGenerator fileFilterGenerator = exportInfo.FileFilterGenerator;
+
+            // Assert
+            Assert.AreEqual("Kommagescheiden bestand (*.csv)|*.csv", fileFilterGenerator.Filter);
+        }
+
+        [Test]
+        public void IsEnabled_GrassCoverErosionOutwardsWaveConditionsCalculationHasOutputFalse_ReturnFalse()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new GrassCoverErosionOutwardsFailureMechanism();
+
+            var context = new GrassCoverErosionOutwardsWaveConditionsCalculationContext(new GrassCoverErosionOutwardsWaveConditionsCalculation(),
+                                                                                        failureMechanism, assessmentSection);
+
+            // Call
+            bool isEnabled = exportInfo.IsEnabled(context);
+
+            // Assert
+            Assert.IsFalse(isEnabled);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void IsEnabled_GrassCoverErosionOutwardsWaveConditionsCalculationHasOutputTrue_ReturnTrue()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new GrassCoverErosionOutwardsFailureMechanism();
+            var output = new[]
+            {
+                new TestWaveConditionsOutput()
+            };
+
+            var context = new GrassCoverErosionOutwardsWaveConditionsCalculationContext(
+                new GrassCoverErosionOutwardsWaveConditionsCalculation
+                {
+                    Output = new GrassCoverErosionOutwardsWaveConditionsOutput(output)
+                },
+                failureMechanism, assessmentSection);
+
+            // Call
+            bool isEnabled = exportInfo.IsEnabled(context);
+
+            // Assert
+            Assert.IsTrue(isEnabled);
+            mocks.VerifyAll();
+        }
+    }
+}
