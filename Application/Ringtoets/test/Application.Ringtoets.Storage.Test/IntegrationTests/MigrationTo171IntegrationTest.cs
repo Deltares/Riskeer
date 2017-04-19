4b64e4ff0b3de19a9905981f3f82305009f1d8fc
Marten Janssen <marten.janssen@deltares.nl>
2017-04-19 10:55:58 +0000

    Update migration test to assert database file Related to Issue [WTI-1247]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@9345 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: ca86ce796a76a53101f04f8bc335e11259a01614
    Former-commit-id: 094884ec06306479d42177c44ba6b8232d7c6224

--- Application/Ringtoets/test/Application.Ringtoets.Storage.Test/IntegrationTests/MigrationTo171IntegrationTest.cs
+++ Application/Ringtoets/test/Application.Ringtoets.Storage.Test/IntegrationTests/MigrationTo171IntegrationTest.cs
@@ -19,22 +19,14 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
-using System.Collections.Generic;
-using System.Linq;
+using System;
+using System.Data;
+using System.Data.SQLite;
 using Application.Ringtoets.Migration.Core;
+using Core.Common.Base.IO;
+using Core.Common.IO.Readers;
 using Core.Common.TestUtil;
 using NUnit.Framework;
-using Ringtoets.ClosingStructures.Data;
-using Ringtoets.Common.Data.Hydraulics;
-using Ringtoets.Common.Data.Structures;
-using Ringtoets.GrassCoverErosionInwards.Data;
-using Ringtoets.GrassCoverErosionOutwards.Data;
-using Ringtoets.HeightStructures.Data;
-using Ringtoets.Integration.Data;
-using Ringtoets.Piping.Data;
-using Ringtoets.StabilityPointStructures.Data;
-using Ringtoets.StabilityStoneCover.Data;
-using Ringtoets.WaveImpactAsphaltCover.Data;
 
 namespace Application.Ringtoets.Storage.Test.IntegrationTests
 {
@@ -60,77 +52,120 @@ namespace Application.Ringtoets.Storage.Test.IntegrationTests
                 migrator.Migrate(fromVersionedFile, newVersion, targetFilePath);
 
                 // Then
-                var storageSqLite = new StorageSqLite();
-                var project = (RingtoetsProject) storageSqLite.LoadProject(targetFilePath);
+                using (var reader = new MigratedDatabaseReader(targetFilePath))
+                {
+                    AssertClosingStructuresFailureMechanism(reader);
+                    AssertGrassCoverErosionInwardsFailureMechanism(reader);
+                    AssertGrassCoverErosionOutwardsFailureMechanism(reader);
+                    AssertHeightStructuresFailureMechanism(reader);
+                    AssertPipingFailureMechanism(reader);
+                    AssertStabilityPointStructuresFailureMechanism(reader);
+                    AssertStabilityStoneCoverFailureMechanism(reader);
+                    AssertWaveImpactAsphaltCoverFailureMechanism(reader);
 
-                AssertClosingStructuresFailureMechanism(project.AssessmentSections);
-                AssertGrassCoverErosionInwardsFailureMechanism(project.AssessmentSections);
-                AssertGrassCoverErosionOutwardsFailureMechanism(project.AssessmentSections);
-                AssertHeightStructuresFailureMechanism(project.AssessmentSections);
-                AssertHydraulicBoundaryDatabase(project.AssessmentSections);
-                AssertPipingFailureMechanism(project.AssessmentSections);
-                AssertStabilityPointStructuresFailureMechanism(project.AssessmentSections);
-                AssertStabilityStoneCoverFailureMechanism(project.AssessmentSections);
-                AssertWaveImpactAsphaltCoverFailureMechanism(project.AssessmentSections);
+                    AssertHydraulicBoundaryLocations(reader);
+                }
             }
         }
 
-        private static void AssertClosingStructuresFailureMechanism(IList<AssessmentSection> assessmentSections)
+        private static void AssertClosingStructuresFailureMechanism(MigratedDatabaseReader reader)
         {
-            Assert.IsFalse(assessmentSections.All(a => a.ClosingStructures.Calculations.OfType<StructuresCalculation<ClosingStructuresInput>>().Any(pcs => pcs.HasOutput)));
+            const string getClosingStructuresCalculationOutput = "SELECT 'x' " +
+                                                                 "FROM ClosingStructuresOutputEntity";
+            reader.AssertData(getClosingStructuresCalculationOutput, r => Assert.IsFalse(r.Read()));
         }
 
-        private static void AssertGrassCoverErosionInwardsFailureMechanism(IList<AssessmentSection> assessmentSections)
+        private static void AssertGrassCoverErosionInwardsFailureMechanism(MigratedDatabaseReader reader)
         {
-            Assert.IsFalse(assessmentSections.All(a => a.GrassCoverErosionInwards.Calculations.OfType<GrassCoverErosionInwardsCalculation>().Any(pcs => pcs.HasOutput)));
+            const string getGrassCoverErosionInwardsCalculationOutput = "SELECT 'x' " +
+                                                                        "FROM GrassCoverErosionInwardsDikeHeightOutputEntity " +
+                                                                        "JOIN GrassCoverErosionInwardsOutputEntity";
+            reader.AssertData(getGrassCoverErosionInwardsCalculationOutput, r => Assert.IsFalse(r.Read()));
         }
 
-        private static void AssertGrassCoverErosionOutwardsFailureMechanism(IList<AssessmentSection> assessmentSections)
+        private static void AssertGrassCoverErosionOutwardsFailureMechanism(MigratedDatabaseReader reader)
         {
-            Assert.IsFalse(assessmentSections.All(a => a.GrassCoverErosionOutwards.Calculations.OfType<GrassCoverErosionOutwardsWaveConditionsCalculation>().Any(pcs => pcs.HasOutput)));
+            const string getGrassCoverErosionOutwardsCalculationOutput = "SELECT 'x' " +
+                                                                         "FROM GrassCoverErosionOutwardsHydraulicLocationOutputEntity " +
+                                                                         "JOIN GrassCoverErosionOutwardsWaveConditionsOutputEntity";
+            reader.AssertData(getGrassCoverErosionOutwardsCalculationOutput, r => Assert.IsFalse(r.Read()));
+        }
 
-            foreach (AssessmentSection assessmentSection in assessmentSections)
+        private static void AssertHeightStructuresFailureMechanism(MigratedDatabaseReader reader)
+        {
+            const string getHeightStructuresCalculationOutput = "SELECT 'x' " +
+                                                                "FROM HeightStructuresOutputEntity";
+            reader.AssertData(getHeightStructuresCalculationOutput, r => Assert.IsFalse(r.Read()));
+        }
+
+        private static void AssertHydraulicBoundaryLocations(MigratedDatabaseReader reader)
+        {
+            const string getHydraulicBoundaryLocationsOutput = "SELECT 'x' " +
+                                                               "FROM HydraulicLocationOutputEntity";
+            reader.AssertData(getHydraulicBoundaryLocationsOutput, r => Assert.IsFalse(r.Read()));
+        }
+
+        private static void AssertPipingFailureMechanism(MigratedDatabaseReader reader)
+        {
+            const string getPipingCalculationOutput = "SELECT 'x' " +
+                                                      "FROM PipingCalculationOutputEntity " +
+                                                      "JOIN PipingSemiProbabilisticOutputEntity";
+            reader.AssertData(getPipingCalculationOutput, r => Assert.IsFalse(r.Read()));
+        }
+
+        private static void AssertStabilityPointStructuresFailureMechanism(MigratedDatabaseReader reader)
+        {
+            const string getStabilityPointStructuresCalculationOutput = "SELECT 'x' " +
+                                                                        "FROM StabilityPointStructuresOutputEntity";
+            reader.AssertData(getStabilityPointStructuresCalculationOutput, r => Assert.IsFalse(r.Read()));
+        }
+
+        private static void AssertStabilityStoneCoverFailureMechanism(MigratedDatabaseReader reader)
+        {
+            const string getStabilityStoneCoverWaveConditionsCalculationOutput = "SELECT 'x' " +
+                                                                                 "FROM StabilityStoneCoverWaveConditionsOutputEntity";
+            reader.AssertData(getStabilityStoneCoverWaveConditionsCalculationOutput, r => Assert.IsFalse(r.Read()));
+        }
+
+        private static void AssertWaveImpactAsphaltCoverFailureMechanism(MigratedDatabaseReader reader)
+        {
+            const string getWaveImpactAsphaltCoverCalculationOutput = "SELECT 'x' " +
+                                                                      "FROM WaveImpactAsphaltCoverWaveConditionsOutputEntity";
+
+            reader.AssertData(getWaveImpactAsphaltCoverCalculationOutput, r => Assert.IsFalse(r.Read()));
+        }
+
+        /// <summary>
+        /// Database reader for migrated database.
+        /// </summary>
+        private class MigratedDatabaseReader : SqLiteDatabaseReaderBase
+        {
+            /// <summary>
+            /// Creates a new instance of <see cref="MigratedDatabaseReader"/>.
+            /// </summary>
+            /// <param name="databaseFilePath">The path of the database file to open.</param>
+            /// <exception cref="CriticalFileReadException">Thrown when:
+            /// <list type="bullet">
+            /// <item>The <paramref name="databaseFilePath"/> contains invalid characters.</item>
+            /// <item>No file could be found at <paramref name="databaseFilePath"/>.</item>
+            /// <item>Unable to open database file.</item>
+            /// </list>
+            /// </exception>
+            public MigratedDatabaseReader(string databaseFilePath) : base(databaseFilePath) { }
+
+            /// <summary>
+            /// Asserts the data from the <paramref name="queryString"/> result.
+            /// </summary>
+            /// <param name="queryString">The query to execute.</param>
+            /// <param name="assert">The assert to perform.</param>
+            /// <exception cref="SQLiteException">The execution of queryString failed.</exception>
+            public void AssertData(string queryString, Action<IDataReader> assert)
             {
-                AssertHydraulicBoundaryLocations(assessmentSection.GrassCoverErosionOutwards.HydraulicBoundaryLocations);
+                using (IDataReader dataReader = CreateDataReader(queryString))
+                {
+                    assert(dataReader);
+                }
             }
-        }
-
-        private static void AssertHeightStructuresFailureMechanism(IList<AssessmentSection> assessmentSections)
-        {
-            Assert.IsFalse(assessmentSections.All(a => a.ClosingStructures.Calculations.OfType<StructuresCalculation<HeightStructuresInput>>().Any(pcs => pcs.HasOutput)));
-        }
-
-        private static void AssertHydraulicBoundaryDatabase(IList<AssessmentSection> assessmentSections)
-        {
-            foreach (AssessmentSection assessmentSection in assessmentSections)
-            {
-                AssertHydraulicBoundaryLocations(assessmentSection.HydraulicBoundaryDatabase.Locations);
-            }
-        }
-
-        private static void AssertHydraulicBoundaryLocations(IList<HydraulicBoundaryLocation> hydraulicBoundaryLocations)
-        {
-            Assert.IsFalse(hydraulicBoundaryLocations.Any(l => l.DesignWaterLevelOutput != null || l.WaveHeightOutput != null));
-        }
-
-        private static void AssertPipingFailureMechanism(IList<AssessmentSection> assessmentSections)
-        {
-            Assert.IsFalse(assessmentSections.All(a => a.PipingFailureMechanism.Calculations.OfType<PipingCalculationScenario>().Any(pcs => pcs.HasOutput)));
-        }
-
-        private static void AssertStabilityPointStructuresFailureMechanism(IList<AssessmentSection> assessmentSections)
-        {
-            Assert.IsFalse(assessmentSections.All(a => a.StabilityPointStructures.Calculations.OfType<StructuresCalculation<StabilityPointStructuresInput>>().Any(pcs => pcs.HasOutput)));
-        }
-
-        private static void AssertStabilityStoneCoverFailureMechanism(IList<AssessmentSection> assessmentSections)
-        {
-            Assert.IsFalse(assessmentSections.All(a => a.StabilityStoneCover.Calculations.OfType<StabilityStoneCoverWaveConditionsCalculation>().Any(pcs => pcs.HasOutput)));
-        }
-
-        private static void AssertWaveImpactAsphaltCoverFailureMechanism(IList<AssessmentSection> assessmentSections)
-        {
-            Assert.IsFalse(assessmentSections.All(a => a.WaveImpactAsphaltCover.Calculations.OfType<WaveImpactAsphaltCoverWaveConditionsCalculation>().Any(pcs => pcs.HasOutput)));
         }
     }
 }
