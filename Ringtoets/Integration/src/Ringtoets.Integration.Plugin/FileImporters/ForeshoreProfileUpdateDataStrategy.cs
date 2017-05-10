ebde35e650a398aa9ce1b9c6950b91d2a47c007c
Dennis Tang <dennis.tang@deltares.nl>
2017-05-10 07:11:05 +0000

    Initial setup of the UpdateDataStrategy of the foreshore profiles: - Implemented basic tests for verifying behaviour when adding or removing elements of the target data collection - No update logic was implemented yet!
    Related to Issue [WTI-1113]
    
    
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@9703 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 75c559e03e4557486744e14b270bb3e8fe3b4d74
    Former-commit-id: 5cbd2e89f2e7cbc2f5c58b6253d6b168e2cf1f5d

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Plugin/FileImporters/ForeshoreProfileUpdateDataStrategy.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Core.Common.Base;
+using Ringtoets.Common.Data.DikeProfiles;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.UpdateDataStrategies;
+using Ringtoets.Common.IO.FileImporters;
+
+namespace Ringtoets.Integration.Plugin.FileImporters
+{
+    public class ForeshoreProfileUpdateDataStrategy : UpdateDataStrategyBase<ForeshoreProfile, IFailureMechanism>,
+                                                      IForeshoreProfileUpdateDataStrategy
+    {
+        public ForeshoreProfileUpdateDataStrategy(IFailureMechanism failureMechanism) : base(failureMechanism, new ForeshoreProfileEqualityComparer()) {}
+
+
+        protected override IEnumerable<IObservable> UpdateObjectAndDependentData(ForeshoreProfile objectToUpdate, ForeshoreProfile objectToUpdateFrom)
+        {
+            // TODO
+            return new IObservable[0];
+        }
+
+        protected override IEnumerable<IObservable> RemoveObjectAndDependentData(ForeshoreProfile removedObject)
+        {
+            // TODO
+            return new IObservable[0];
+        }
+
+        public IEnumerable<IObservable> UpdateForeshoreProfilesWithImportedData(ForeshoreProfileCollection targetDataCollection, IEnumerable<ForeshoreProfile> importedDataCollection, string sourceFilePath)
+        {
+            return UpdateTargetCollectionData(targetDataCollection, importedDataCollection, sourceFilePath);
+        }
+
+        /// <summary>
+        /// Class for comparing he <see cref="ForeshoreProfile"/> only by ID.
+        /// </summary>
+        private class ForeshoreProfileEqualityComparer : IEqualityComparer<ForeshoreProfile>
+        {
+            public bool Equals(ForeshoreProfile x, ForeshoreProfile y)
+            {
+                return x.Id.Equals(y.Id);
+            }
+
+            public int GetHashCode(ForeshoreProfile obj)
+            {
+                return obj.Id.GetHashCode();
+            }
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Plugin.Test/FileImporters/ForeshoreProfileUpdateDataStrategyTest.cs
@@ -0,0 +1,193 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base;
+using NUnit.Framework;
+using Ringtoets.Common.Data.DikeProfiles;
+using Ringtoets.Common.Data.Exceptions;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Common.Data.UpdateDataStrategies;
+using Ringtoets.Common.IO.FileImporters;
+using Ringtoets.Integration.Plugin.FileImporters;
+
+namespace Ringtoets.Integration.Plugin.Test.FileImporters
+{
+    [TestFixture]
+    public class ForeshoreProfileUpdateDataStrategyTest
+    {
+        private const string sourceFilePath = "some/path/to/foreshoreProfiles";
+
+        [Test]
+        public void Constructor_CreatesNewInstance()
+        {
+            // Call
+            var strategy = new ForeshoreProfileUpdateDataStrategy(new TestFailureMechanism());
+
+            // Assert
+            Assert.IsInstanceOf<IForeshoreProfileUpdateDataStrategy>(strategy);
+            Assert.IsInstanceOf<UpdateDataStrategyBase<ForeshoreProfile, IFailureMechanism>>(strategy);
+        }
+
+        [Test]
+        public void UpdateForeshoreProfilesWithImportedData_CurrentAndImportedCollectionEmpty_DoesNothing()
+        {
+            // Setup
+            var strategy = new ForeshoreProfileUpdateDataStrategy(new TestFailureMechanism());
+            var foreshoreProfiles = new ForeshoreProfileCollection();
+
+            // Call
+            IEnumerable<IObservable> affectedObjects =
+                strategy.UpdateForeshoreProfilesWithImportedData(foreshoreProfiles,
+                                                                 Enumerable.Empty<ForeshoreProfile>(),
+                                                                 sourceFilePath);
+
+            // Assert
+            CollectionAssert.IsEmpty(affectedObjects);
+            CollectionAssert.IsEmpty(foreshoreProfiles);
+        }
+
+        [Test]
+        public void UpdateForeshoreProfilesWithImportedData_CurrentCollectionNotEmptyImportedCollectionContainDuplicateIDs_ThrowUpdateException()
+        {
+            // Setup 
+            var strategy = new ForeshoreProfileUpdateDataStrategy(new TestFailureMechanism());
+
+            var foreshoreProfiles = new ForeshoreProfileCollection();
+            foreshoreProfiles.AddRange(new[]
+            {
+                new TestForeshoreProfile("name 1", "different ID")
+            }, sourceFilePath);
+
+            const string duplicateId = "duplicate ID";
+            var importedForeshoreProfiles = new[]
+            {
+                new TestForeshoreProfile("Name A", duplicateId),
+                new TestForeshoreProfile("Name B", duplicateId)
+            };
+
+            // Call
+            TestDelegate call = () => strategy.UpdateForeshoreProfilesWithImportedData(foreshoreProfiles,
+                                                                                       importedForeshoreProfiles,
+                                                                                       sourceFilePath);
+
+            // Assert
+            var exception = Assert.Throws<UpdateDataException>(call);
+            string expectedMessage = $"Voorlandprofielen moeten een unieke id hebben. Gevonden dubbele elementen: {duplicateId}.";
+            Assert.AreEqual(expectedMessage, exception.Message);
+
+            CollectionAssert.IsEmpty(foreshoreProfiles);
+        }
+
+        [Test]
+        public void UpdateForeshoreProfilesWithImportedData_CurrentCollectionEmptyAndImportedCollectionNotEmpty_NewProfilesAdded()
+        {
+            // Setup
+            var strategy = new ForeshoreProfileUpdateDataStrategy(new TestFailureMechanism());
+            var foreshoreProfiles = new ForeshoreProfileCollection();
+
+            var importedForeshoreProfiles = new[]
+            {
+                new TestForeshoreProfile("Name A", "ID A"),
+                new TestForeshoreProfile("Name B", "ID B")
+            };
+
+            // Call
+            IEnumerable<IObservable> affectedObjects =
+                strategy.UpdateForeshoreProfilesWithImportedData(foreshoreProfiles,
+                                                                 importedForeshoreProfiles,
+                                                                 sourceFilePath);
+
+            // Assert
+            CollectionAssert.AreEqual(new[]
+            {
+                foreshoreProfiles
+            }, affectedObjects);
+            CollectionAssert.AreEqual(importedForeshoreProfiles, foreshoreProfiles);
+        }
+
+        [Test]
+        public void UpdateForeshoreProfilesWithImportedData_CurrentCollectionNotEmptyAndImportedCollectionEmpty_RemovesProfiles()
+        {
+            // Setup
+            var strategy = new ForeshoreProfileUpdateDataStrategy(new TestFailureMechanism());
+            var foreshoreProfiles = new ForeshoreProfileCollection();
+            foreshoreProfiles.AddRange(new[]
+            {
+                new TestForeshoreProfile("Name A", "ID A"),
+                new TestForeshoreProfile("Name B", "ID B")
+            }, sourceFilePath);
+
+            // Call
+            IEnumerable<IObservable> affectedObjects =
+                strategy.UpdateForeshoreProfilesWithImportedData(foreshoreProfiles,
+                                                                 Enumerable.Empty<ForeshoreProfile>(),
+                                                                 sourceFilePath);
+
+            // Assert
+            CollectionAssert.AreEqual(new[]
+            {
+                foreshoreProfiles
+            }, affectedObjects);
+            CollectionAssert.IsEmpty(foreshoreProfiles);
+        }
+
+        [Test]
+        public void UpdateForeshoreProfilesWithImportedData_CurrentCollectionAndImportedCollectionHasNoOverlap_UpdatesTargetCollection()
+        {
+            // Setup 
+            const string currentForeshoreProfile = "Current ID";
+            var targetForeshoreProfile = new TestForeshoreProfile(string.Empty, currentForeshoreProfile);
+
+            const string readForeshoreProfileId = "Read ID";
+            var readForeshoreProfile = new TestForeshoreProfile(string.Empty, readForeshoreProfileId);
+            var readForeshoreProfiles = new[]
+            {
+                readForeshoreProfile
+            };
+
+            var foreshoreProfiles = new ForeshoreProfileCollection();
+            foreshoreProfiles.AddRange(new[]
+            {
+                targetForeshoreProfile
+            }, sourceFilePath);
+
+            var strategy = new ForeshoreProfileUpdateDataStrategy(new TestFailureMechanism());
+
+            // Call
+            IEnumerable<IObservable> affectedObjects =
+                strategy.UpdateForeshoreProfilesWithImportedData(foreshoreProfiles,
+                                                                 readForeshoreProfiles,
+                                                                 sourceFilePath);
+
+            // Assert
+            Assert.AreEqual(1, foreshoreProfiles.Count);
+            Assert.AreSame(readForeshoreProfile, foreshoreProfiles[0]);
+
+            CollectionAssert.AreEqual(new[]
+            {
+                foreshoreProfiles
+            }, affectedObjects);
+        }
+    }
+}
