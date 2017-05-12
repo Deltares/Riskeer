b30ce02fbe2a89d3f608437135808b77ed734787
Marten Janssen <marten.janssen@deltares.nl>
2017-05-12 10:57:25 +0000

    Fixed failing test Related to Issue [WTI-1114]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@9789 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: c19bd90e56963d4083a6c4c9ba41be4ebdc66e25
    Former-commit-id: 1f528dd120b901c6a52fbcc8fa3e0eb14dee2615

--- Ringtoets/StabilityStoneCover/test/Ringtoets.StabilityStoneCover.Plugin.Test/TreeNodeInfos/StabilityStoneCoverWaveConditionsCalculationContextTreeNodeInfoTest.cs
+++ Ringtoets/StabilityStoneCover/test/Ringtoets.StabilityStoneCover.Plugin.Test/TreeNodeInfos/StabilityStoneCoverWaveConditionsCalculationContextTreeNodeInfoTest.cs
@@ -497,7 +497,7 @@ namespace Ringtoets.StabilityStoneCover.Plugin.Test.TreeNodeInfos
 
                     TestHelper.AssertContextMenuStripContainsItem(menu, contextMenuUpdateForeshoreProfileIndex,
                                                                   "&Bijwerken voorlandprofiel...",
-                                                                  "Er moet een voorlandprofiel geselecteerd zijn.",
+                                                                  "Geselecteerd voorlandprofiel heeft geen wijzingingen om bij te werken.",
                                                                   RingtoetsCommonFormsResources.UpdateItemIcon,
                                                                   false);
 
