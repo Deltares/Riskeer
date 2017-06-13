18c1a79b0b66f21f9a0ebb055183e7fcccaac158
Joël van den Berg <joel.vandenberg@deltares.nl>
2017-06-13 10:59:13 +0000

    Added tests Related to Issue [WTI-1097]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@10270 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: bbedf43404ef5a1383a0b31fa10f917d5bfb0bd8
    Former-commit-id: 704c55772d077262fdcb0ec74865bd67f9c8c58b

---
+++ Ringtoets/Common/test/Ringtoets.Common.Service.TestUtil.Test/CalculationServiceTestHelperTest.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+
+namespace Ringtoets.Common.Service.TestUtil.Test
+{
+    [TestFixture]
+    public class CalculationServiceTestHelperTest
+    {
+        [Test]
+        [TestCase("aab")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertValidationStartMessage_MessagesEqual_DoesNotThrow(string calculationName)
+        {
+            // Call
+            TestDelegate test = () => 
+                CalculationServiceTestHelper.AssertValidationStartMessage(calculationName, $"Validatie van '{calculationName}' gestart.");
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+
+        [Test]
+        [TestCase("alidatie van '' gestart.")]
+        [TestCase("Validatie an '' gestart.")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertValidationStartMessage_MessagesNotEqual_ThrowsAssertionException(string incorrectMessage)
+        {
+            // Call
+            TestDelegate test = () => 
+                CalculationServiceTestHelper.AssertValidationStartMessage(string.Empty, incorrectMessage);
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+
+        [Test]
+        [TestCase("aab")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertValidationEndMessage_MessagesEqual_DoesNotThrow(string calculationName)
+        {
+            // Call
+            TestDelegate test = () => 
+                CalculationServiceTestHelper.AssertValidationEndMessage(calculationName, $"Validatie van '{calculationName}' beëindigd.");
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+
+        [Test]
+        [TestCase("alidatie van '' beëindigd.")]
+        [TestCase("Validatie an '' beëindigd.")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertValidationEndMessage_MessagesNotEqual_ThrowsAssertionException(string incorrectMessage)
+        {
+            // Call
+            TestDelegate test = () => 
+                CalculationServiceTestHelper.AssertValidationEndMessage(string.Empty, incorrectMessage);
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+        [Test]
+        [TestCase("aab")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertCalculationStartMessage_MessagesEqual_DoesNotThrow(string calculationName)
+        {
+            // Call
+            TestDelegate test = () => 
+                CalculationServiceTestHelper.AssertCalculationStartMessage(calculationName, $"Berekening van '{calculationName}' gestart.");
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+
+        [Test]
+        [TestCase("erekening van '' gestart.")]
+        [TestCase("Berekening an '' gestart.")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertCalculationStartMessage_MessagesNotEqual_ThrowsAssertionException(string incorrectMessage)
+        {
+            // Call
+            TestDelegate test = () => 
+                CalculationServiceTestHelper.AssertCalculationStartMessage(string.Empty, incorrectMessage);
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+        [Test]
+        [TestCase("aab")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertCalculationEndMessage_MessagesEqual_DoesNotThrow(string calculationName)
+        {
+            // Call
+            TestDelegate test = () => 
+                CalculationServiceTestHelper.AssertCalculationEndMessage(calculationName, $"Berekening van '{calculationName}' beëindigd.");
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+
+        [Test]
+        [TestCase("erekening van '' beëindigd.")]
+        [TestCase("Berekening an '' beëindigd.")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertCalculationEndMessage_MessagesNotEqual_ThrowsAssertionException(string incorrectMessage)
+        {
+            // Call
+            TestDelegate test = () => 
+                CalculationServiceTestHelper.AssertCalculationEndMessage(string.Empty, incorrectMessage);
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+    }
+}
---
+++ Ringtoets/GrassCoverErosionInwards/test/Ringtoets.GrassCoverErosionInwards.Service.TestUtil.Test/GrassCoverErosionInwardsCalculationServiceTestHelperTest.cs
@@ -0,0 +1,147 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+
+namespace Ringtoets.GrassCoverErosionInwards.Service.TestUtil.Test
+{
+    [TestFixture]
+    public class GrassCoverErosionInwardsCalculationServiceTestHelperTest
+    {
+        [Test]
+        public void Constants_Always_ReturnExpectedValues()
+        {
+            // Call
+            const string overtoppingDescription = GrassCoverErosionInwardsCalculationServiceTestHelper.OvertoppingCalculationDescription;
+            const string overtoppingRateDescription = GrassCoverErosionInwardsCalculationServiceTestHelper.OvertoppingRateCalculationDescription;
+            const string hbnDescription = GrassCoverErosionInwardsCalculationServiceTestHelper.HbnCalculationDescription;
+
+            // Assert
+            Assert.AreEqual("overloop en overslag", overtoppingDescription);
+            Assert.AreEqual("overslagdebiet", overtoppingRateDescription);
+            Assert.AreEqual("HBN", hbnDescription);
+        }
+
+        [Test]
+        [TestCase("description", "output")]
+        [TestCase("", null)]
+        [TestCase(null, "")]
+        public void AssertCalculationFinishedMessage_ForDifferentParametersWithEqualMessage_DoesNotThrow(
+            string description, string output)
+        {
+            // Call
+            TestDelegate test = () =>
+                GrassCoverErosionInwardsCalculationServiceTestHelper.AssertCalculationFinishedMessage(
+                    description,
+                    output,
+                    $"De {description} berekening is uitgevoerd op de tijdelijke locatie '{output}'. " +
+                    "Gedetailleerde invoer en uitvoer kan in de bestanden op deze locatie worden gevonden.");
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+
+        [Test]
+        [TestCase("De  is uitgevoerd op de tijdelijke locatie ''. Gedetailleerde invoer en uitvoer kan in de bestanden op deze locatie worden gevonden.")]
+        [TestCase("De  berekening is uitgevoerd op de tijdelijke locatie ''. edetailleerde invoer en uitvoer kan in de bestanden op deze locatie worden gevonden.")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertCalculationFinishedMessage_WithNotEqualMessage_ThrowsAssertionException(string message)
+        {
+            // Call
+            TestDelegate test = () =>
+                GrassCoverErosionInwardsCalculationServiceTestHelper.AssertCalculationFinishedMessage(string.Empty, string.Empty, message);
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+
+        [Test]
+        [TestCase("description", "name", "error")]
+        [TestCase("", null, "")]
+        [TestCase(null, "", "  ")]
+        public void AssertCalculationFinishedMessage_ForDifferentParametersWithErrorWithEqualMessage_DoesNotThrow(
+            string description, string name, string error)
+        {
+            // Call
+            TestDelegate test = () =>
+                GrassCoverErosionInwardsCalculationServiceTestHelper.AssertCalculationFailedMessage(
+                    description,
+                    name,
+                    error,
+                    $"De {description} berekening voor grasbekleding erosie kruin en binnentalud '{name}' is niet gelukt. " +
+                    $"Bekijk het foutrapport door op details te klikken.\r\n{error}");
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+
+        [Test]
+        [TestCase("description", "output")]
+        [TestCase("", null)]
+        [TestCase(null, "")]
+        public void AssertCalculationFinishedMessage_ForDifferentParametersWithoutErrorWithEqualMessage_DoesNotThrow(
+            string description, string name)
+        {
+            // Call
+            TestDelegate test = () =>
+                GrassCoverErosionInwardsCalculationServiceTestHelper.AssertCalculationFailedMessage(
+                    description,
+                    name,
+                    null,
+                    $"De {description} berekening voor grasbekleding erosie kruin en binnentalud '{name}' is niet gelukt. " +
+                    "Er is geen foutrapport beschikbaar.");
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+
+        [Test]
+        [TestCase("De  berekening voor grasbekleding erosie kruin en binnentalud '' is niet gelukt. Bekijk het foutrapport door op details te klikken.\n")]
+        [TestCase("De  berekening voor grasbekleding erosie kruin en binnentalud '' is niet gelukt. ekijk het foutrapport door op details te klikken.\r\n")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertCalculationFailedMessage_WithoutErrorWithNotEqualMessage_ThrowsAssertionException(string message)
+        {
+            // Call
+            TestDelegate test = () =>
+                GrassCoverErosionInwardsCalculationServiceTestHelper.AssertCalculationFailedMessage(string.Empty, string.Empty, null, message);
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+
+        [Test]
+        [TestCase("e  berekening is uitgevoerd op de tijdelijke locatie ''. Gedetailleerde invoer en uitvoer kan in de bestanden op deze locatie worden gevonden.")]
+        [TestCase("De  berekening is uitgevoerd op de tijdelijke locatie ''. edetailleerde invoer en uitvoer kan in de bestanden op deze locatie worden gevonden.")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void AssertCalculationFailedMessage_WithErrorWithNotEqualMessage_ThrowsAssertionException(string message)
+        {
+            // Call
+            TestDelegate test = () =>
+                GrassCoverErosionInwardsCalculationServiceTestHelper.AssertCalculationFailedMessage(string.Empty, string.Empty, string.Empty, message);
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+    }
+}
