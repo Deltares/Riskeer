30df7a3cabc5c48307afa542944fc17fd7a355e3
Marten Janssen <marten.janssen@deltares.nl>
2017-06-28 08:47:49 +0000

    Added extensions for read and create Stochast and RealizedStochast Related to Issue [WTI-1305]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@10491 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 79b7a3110e20a01b2e987f0e13b8e88f38c5d15e
    Former-commit-id: 69d3d78941b287c31f5c94f129e0e1ac022fa2a6

---
+++ Application/Ringtoets/src/Application.Ringtoets.Storage/Create/IllustrationPoints/RealizedStochastCreateExtensions.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Application.Ringtoets.Storage.DbContext;
+using Core.Common.Utils.Extensions;
+using Ringtoets.Common.Data.Hydraulics.IllustrationPoints;
+
+namespace Application.Ringtoets.Storage.Create.IllustrationPoints
+{
+    /// <summary>
+    /// Extension methods for <see cref="RealizedStochast"/> related to creating an instance of 
+    /// <see cref="HydraulicLocationRealizedStochastEntity"/>.
+    /// </summary>
+    internal static class RealizedStochastCreateExtensions
+    {
+        /// <summary>
+        /// Creates a <see cref="HydraulicLocationRealizedStochastEntity"/> based on the information 
+        /// of the <paramref name="realizedStochast"/>.
+        /// </summary>
+        /// <param name="realizedStochast">The stochast to create a database entity for.</param>
+        /// <param name="order">The index at which <paramref name="realizedStochast"/> resides within its parent.</param>
+        /// <returns>A new <see cref="HydraulicLocationRealizedStochastEntity"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="realizedStochast"/> 
+        /// is <c>null</c>.</exception>
+        public static HydraulicLocationRealizedStochastEntity CreateHydraulicLocationRealizedStochastEntity(
+            this RealizedStochast realizedStochast, int order)
+        {
+            if (realizedStochast == null)
+            {
+                throw new ArgumentNullException(nameof(realizedStochast));
+            }
+
+            var entity = new HydraulicLocationRealizedStochastEntity
+            {
+                Name = realizedStochast.Name.DeepClone(),
+                Alpha = realizedStochast.Alpha,
+                Duration = realizedStochast.Duration,
+                Realization = realizedStochast.Realization,
+                Order = order
+            };
+
+            return entity;
+        }
+
+        /// <summary>
+        /// Creates a <see cref="GrassCoverErosionOutwardsHydraulicLocationRealizedStochastEntity"/> based 
+        /// on the information of the <paramref name="realizedStochast"/>.
+        /// </summary>
+        /// <param name="realizedStochast">The stochast to create a database entity for.</param>
+        /// <param name="order">The index at which <paramref name="realizedStochast"/> resides within its parent.</param>
+        /// <returns>A new <see cref="GrassCoverErosionOutwardsHydraulicLocationRealizedStochastEntity"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="realizedStochast"/> 
+        /// is <c>null</c>.</exception>
+        public static GrassCoverErosionOutwardsHydraulicLocationRealizedStochastEntity CreateGrassCoverErosionOutwardsHydraulicLocationRealizedStochastEntity(
+            this RealizedStochast realizedStochast, int order)
+        {
+            if (realizedStochast == null)
+            {
+                throw new ArgumentNullException(nameof(realizedStochast));
+            }
+
+            var entity = new GrassCoverErosionOutwardsHydraulicLocationRealizedStochastEntity
+            {
+                Name = realizedStochast.Name.DeepClone(),
+                Alpha = realizedStochast.Alpha,
+                Duration = realizedStochast.Duration,
+                Realization = realizedStochast.Realization,
+                Order = order
+            };
+
+            return entity;
+        }
+    }
+}
---
+++ Application/Ringtoets/test/Application.Ringtoets.Storage.Test/Create/IllustrationPoints/StochastCreateExtensionsTest.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Application.Ringtoets.Storage.Create.IllustrationPoints;
+using Application.Ringtoets.Storage.DbContext;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Hydraulics.IllustrationPoints;
+using Ringtoets.Common.Data.TestUtil;
+
+namespace Application.Ringtoets.Storage.Test.Create.IllustrationPoints
+{
+    [TestFixture]
+    public class StochastCreateExtensionsTest
+    {
+        [Test]
+        public void CreateHydraulicLocationStochastEntity_StochastNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => ((Stochast) null).CreateHydraulicLocationStochastEntity(0);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("stochast", paramName);
+        }
+
+        [Test]
+        public void CreateHydraulicLocationStochastEntity_ValidStochast_ReturnEntity()
+        {
+            // Setup
+            var random = new Random(123);
+            var stochast = new Stochast("Some description",
+                                        random.NextDouble(),
+                                        random.NextDouble());
+            int order = random.Next();
+
+            // Call
+            HydraulicLocationStochastEntity entity = stochast.CreateHydraulicLocationStochastEntity(order);
+
+            // Assert
+            Assert.IsInstanceOf<IStochastEntity>(entity);
+            TestHelper.AssertAreEqualButNotSame(stochast.Name, entity.Name);
+            Assert.AreEqual(stochast.Alpha, entity.Alpha, stochast.Alpha.GetAccuracy());
+            Assert.AreEqual(stochast.Duration, entity.Duration, stochast.Duration.GetAccuracy());
+            Assert.AreEqual(order, entity.Order);
+        }
+
+        [Test]
+        public void CreateGrassCoverErosionOutwardsHydraulicLocationStochastEntity_StochastNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => ((Stochast) null).CreateGrassCoverErosionOutwardsHydraulicLocationStochastEntity(0);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("stochast", paramName);
+        }
+
+        [Test]
+        public void CreateGrassCoverErosionOutwardsHydraulicLocationStochastEntity_ValidStochast_ReturnEntity()
+        {
+            // Setup
+            var random = new Random(123);
+            var stochast = new Stochast("Some description",
+                                        random.NextDouble(),
+                                        random.NextDouble());
+            int order = random.Next();
+
+            // Call
+            GrassCoverErosionOutwardsHydraulicLocationStochastEntity entity =
+                stochast.CreateGrassCoverErosionOutwardsHydraulicLocationStochastEntity(order);
+
+            // Assert
+            Assert.IsInstanceOf<IStochastEntity>(entity);
+            TestHelper.AssertAreEqualButNotSame(stochast.Name, entity.Name);
+            Assert.AreEqual(stochast.Alpha, entity.Alpha, stochast.Alpha.GetAccuracy());
+            Assert.AreEqual(stochast.Duration, entity.Duration, stochast.Duration.GetAccuracy());
+            Assert.AreEqual(order, entity.Order);
+        }
+    }
+}
