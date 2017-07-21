2c46e0b74368f3dbf2f9c992f93f315bdda51bea
Elmar Kleijn <elmar.kleijn@deltares.nl>
2017-07-21 08:44:56 +0000

    Introduced first version of GeneralResultFaultTreeIllustrationPointView Related to Issue [WTI-1311]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@10809 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 49c277aeda2249396a6fb8686d9049854e9bdfd4
    Former-commit-id: 3f5ce1d98d5fdaa29de2beb7d51392f9634f258a

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/Views/GeneralResultFaultTreeIllustrationPointView.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using Ringtoets.Common.Data.IllustrationPoints;
+
+namespace Ringtoets.Common.Forms.Views
+{
+    /// <summary>
+    /// This class is a view for presenting <see cref="TopLevelFaultTreeIllustrationPoint"/> objects
+    /// (as part of a <see cref="GeneralResult{T}"/>).
+    /// </summary>
+    public partial class GeneralResultFaultTreeIllustrationPointView : UserControl, IView
+    {
+        private GeneralResult<TopLevelFaultTreeIllustrationPoint> data;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="GeneralResultFaultTreeIllustrationPointView"/>.
+        /// </summary>
+        public GeneralResultFaultTreeIllustrationPointView()
+        {
+            InitializeComponent();
+        }
+
+        public object Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value as GeneralResult<TopLevelFaultTreeIllustrationPoint>;
+            }
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.Test/Views/GeneralResultFaultTreeIllustrationPointViewTest.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using NUnit.Framework;
+using Ringtoets.Common.Data.TestUtil.IllustrationPoints;
+using Ringtoets.Common.Forms.Views;
+
+namespace Ringtoets.Common.Forms.Test.Views
+{
+    [TestFixture]
+    public class GeneralResultFaultTreeIllustrationPointViewTest
+    {
+        [Test]
+        public void DefaultConstructor_DefaultValues()
+        {
+            // Call
+            using (var view = new GeneralResultFaultTreeIllustrationPointView())
+            {
+                // Assert
+                Assert.IsInstanceOf<UserControl>(view);
+                Assert.IsInstanceOf<IView>(view);
+                Assert.IsNull(view.Data);
+            }
+        }
+
+        [Test]
+        public void Data_GeneralResultFaultTreeIllustrationPoint_DataSet()
+        {
+            // Setup
+            using (var view = new GeneralResultFaultTreeIllustrationPointView())
+            {
+                var generalResultFaultTreeIllustrationPoint = new TestGeneralResultFaultTreeIllustrationPoint();
+
+                // Call
+                view.Data = generalResultFaultTreeIllustrationPoint;
+
+                // Assert
+                Assert.AreSame(generalResultFaultTreeIllustrationPoint, view.Data);
+            }
+        }
+
+        [Test]
+        public void Data_OtherThanGeneralResultFaultTreeIllustrationPoint_NullSet()
+        {
+            // Setup
+            using (var view = new GeneralResultFaultTreeIllustrationPointView())
+            {
+                var generalResultSubMechanismIllustrationPoint = new TestGeneralResultSubMechanismIllustrationPoint();
+
+                // Call
+                view.Data = generalResultSubMechanismIllustrationPoint;
+
+                // Assert
+                Assert.IsNull(view.Data);
+            }
+        }
+
+        [Test]
+        public void Data_Null_NullSet()
+        {
+            // Setup
+            using (var view = new GeneralResultFaultTreeIllustrationPointView())
+            {
+                // Call
+                view.Data = null;
+
+                // Assert
+                Assert.IsNull(view.Data);
+            }
+        }
+    }
+}
