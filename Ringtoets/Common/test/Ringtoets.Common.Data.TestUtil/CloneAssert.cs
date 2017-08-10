5a04e92c56a507e440e86487d97fc731de005d88
Elmar Kleijn <elmar.kleijn@deltares.nl>
2017-08-10 09:50:09 +0000

    https://www.youtube.com/watch?v=VaFf75-8YI4 Related to Issue [WTI-1370]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11111 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: af9d88f0948cbf579f808e6e7141625054e688ad
    Former-commit-id: 23392e1fa8105bbb7b49a230ff064a61f07dc6c1

--- Ringtoets/Common/test/Ringtoets.Common.Data.TestUtil/CloneAssert.cs
+++ Ringtoets/Common/test/Ringtoets.Common.Data.TestUtil/CloneAssert.cs
@@ -28,7 +28,7 @@ namespace Ringtoets.Common.Data.TestUtil
     /// <summary>
     /// Class that defines methods for asserting whether two objects are clones.
     /// </summary>
-    public class CloneAssert
+    public static class CloneAssert
     {
         /// <summary>
         /// Method that asserts whether <paramref name="original"/> and <paramref name="clone"/>
