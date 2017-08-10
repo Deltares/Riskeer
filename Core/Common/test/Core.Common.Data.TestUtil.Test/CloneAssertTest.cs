0c4d52e7ae76886aaca7096aee8871ef9cd86b33
Elmar Kleijn <elmar.kleijn@deltares.nl>
2017-08-10 08:37:34 +0000

    Added CloneAssert Related to Issue [WTI-1370]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11102 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: e09a459425511e911e78eb8eec44b1170026cfd5
    Former-commit-id: 0654921be4eb9bdf70db22c749b5b80ddfb65669

---
+++ Core/Common/test/Core.Common.Data.TestUtil.Test/CloneAssertTest.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+
+namespace Core.Common.Data.TestUtil.Test
+{
+    [TestFixture]
+    public class CloneAssertTest
+    {
+        [Test]
+        public void AreClones_TypeSpecificAssertsNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate test = () => CloneAssert.AreClones(new object(), new object(), null);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(test);
+        }
+
+        [Test]
+        public void AreClones_OriginalAndCloneBothNull_DoesNotThrow()
+        {
+            // Call
+            TestDelegate test = () => CloneAssert.AreClones<object>(null, null, (original, clone) => {});
+
+            // Assert
+            Assert.DoesNotThrow(test);
+        }
+
+        [Test]
+        public void AreClones_OriginalNull_ThrowsAssertionException()
+        {
+            // Call
+            TestDelegate test = () => CloneAssert.AreClones<object>(null, new object(), (original, clone) => {});
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+
+        [Test]
+        public void AreClones_CloneNull_ThrowsAssertionException()
+        {
+            // Call
+            TestDelegate test = () => CloneAssert.AreClones(new object(), null, (original, clone) => {});
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+
+        [Test]
+        public void AreClones_CloneOfOtherDataType_ThrowsAssertionException()
+        {
+            // Call
+            TestDelegate test = () => CloneAssert.AreClones(1.0, 1, (original, clone) => {});
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+
+        [Test]
+        public void AreClones_OriginalAndCloneAreSame_ThrowsAssertionException()
+        {
+            // Setup
+            var o = new object();
+
+            // Call
+            TestDelegate test = () => CloneAssert.AreClones(o, o, (original, clone) => {});
+
+            // Assert
+            Assert.Throws<AssertionException>(test);
+        }
+
+        [Test]
+        public void AreClones_TypeSpecificAsserts_TypeSpecificAssertsCalled()
+        {
+            // Setup
+            var counter = 0;
+
+            // Call
+            CloneAssert.AreClones(new object(), new object(), (original, clone) => counter++);
+
+            // Assert
+            Assert.AreEqual(1, counter);
+        }
+    }
+}
---
+++ Core/Common/test/Core.Common.Data.TestUtil/CloneAssert.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+
+namespace Core.Common.Data.TestUtil
+{
+    /// <summary>
+    /// Class for asserting whether two objects are clones.
+    /// </summary>
+    public static class CloneAssert
+    {
+        /// <summary>
+        /// Method that asserts whether <paramref name="original"/> and <paramref name="clone"/>
+        /// are clones. Some general clone assertions are performed, followed by the type specific
+        /// assertions (provided via <paramref name="typeSpecificAsserts"/>).
+        /// </summary>
+        /// <typeparam name="T">The type of the objects to assert.</typeparam>
+        /// <param name="original">The original object.</param>
+        /// <param name="clone">The cloned object.</param>
+        /// <param name="typeSpecificAsserts">The action for performing the <typeparamref name="T"/>
+        /// specific assertions.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="typeSpecificAsserts"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="AssertionException">Thrown when <paramref name="original"/> and
+        /// <paramref name="clone"/> are not clones.</exception>
+        public static void AreClones<T>(T original, object clone, Action<T, T> typeSpecificAsserts)
+        {
+            if (typeSpecificAsserts == null)
+            {
+                throw new ArgumentNullException(nameof(typeSpecificAsserts));
+            }
+
+            if (original == null && clone == null)
+            {
+                return;
+            }
+
+            Assert.IsNotNull(original);
+            Assert.IsInstanceOf<T>(clone);
+            Assert.AreNotSame(original, clone);
+
+            typeSpecificAsserts(original, (T) clone);
+        }
+    }
+}
