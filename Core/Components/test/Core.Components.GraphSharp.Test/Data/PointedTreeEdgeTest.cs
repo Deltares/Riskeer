95433bcf41f452154959a6884129265c6de295b7
Robert Tangerman <robert.tangerman@deltares.nl>
2017-08-10 11:04:38 +0000

    Added GraphSharp data classes and GraphLayout. Related to Issue [WTI-1383]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11113 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 32caf3c366b5650ef9d2747bd315f88e5fe7071e
    Former-commit-id: 553415da6e6ae2b97836c8892f0c519fe10de88e

---
+++ Core/Components/test/Core.Components.GraphSharp.Test/Data/PointedTreeEdgeTest.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Components.GraphSharp.Data;
+using NUnit.Framework;
+using QuickGraph;
+
+namespace Core.Components.GraphSharp.Test.Data
+{
+    [TestFixture]
+    public class PointedTreeEdgeTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var source = new PointedTreeElementVertex();
+            var target = new PointedTreeElementVertex();
+
+            // Call
+            var edge = new PointedTreeEdge(source, target);
+
+            // Assert
+            Assert.IsInstanceOf<Edge<PointedTreeElementVertex>>(edge);
+            Assert.AreSame(source, edge.Source);
+            Assert.AreSame(target, edge.Target);
+        }
+    }
+}
