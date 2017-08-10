b11a871d609824c8b414cecf94977c3923c3d09a
Robert Tangerman <robert.tangerman@deltares.nl>
2017-08-10 09:01:13 +0000

    Introduced control for PointedTree. Related to Issue [WTI-1383]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11104 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 078115f1826135f798a13c9aaf1ae0b5d52ac852
    Former-commit-id: df3c8f0c0bb07b52309adca5c7ed5b631774912e

---
+++ Core/Components/src/Core.Components.GraphSharp.Forms/PointedTreeGraphControl.Designer.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Core.Components.GraphSharp.Forms
+{
+    partial class PointedTreeGraphControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wpfElementHost = new System.Windows.Forms.Integration.ElementHost();
+            this.SuspendLayout();
+            // 
+            // wpfElementHost
+            // 
+            this.wpfElementHost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wpfElementHost.Location = new System.Drawing.Point(0, 0);
+            this.wpfElementHost.Name = "wpfElementHost";
+            this.wpfElementHost.Size = new System.Drawing.Size(150, 150);
+            this.wpfElementHost.TabIndex = 0;
+            this.wpfElementHost.Text = "elementHost1";
+            this.wpfElementHost.Child = null;
+            // 
+            // PointedTreeGraphControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.wpfElementHost);
+            this.Name = "PointedTreeGraphControl";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost wpfElementHost;
+    }
+}
---
+++ Core/Components/test/Core.Components.GraphSharp.Forms.Test/PointedTreeGraphControlTest.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using Core.Components.PointedTree.Forms;
+using NUnit.Framework;
+
+namespace Core.Components.GraphSharp.Forms.Test
+{
+    [TestFixture]
+    public class PointedTreeGraphControlTest
+    {
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void Constructor_ExptedValues()
+        {
+            // Call
+            using (var graphControl = new PointedTreeGraphControl())
+            {
+                // Assert
+                Assert.IsInstanceOf<UserControl>(graphControl);
+                Assert.IsInstanceOf<IPointedTreeGraphControl>(graphControl);
+
+                Assert.IsNull(graphControl.Data);
+
+                Assert.AreEqual(1, graphControl.Controls.Count);
+                Assert.IsInstanceOf<ElementHost>(graphControl.Controls[0]);
+            }
+        }
+    }
+}
