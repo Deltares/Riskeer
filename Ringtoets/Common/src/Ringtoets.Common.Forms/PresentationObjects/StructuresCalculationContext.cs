9fe25ebfb1f70f00d66564ef2a89f6e22c27dce4
Elmar Kleijn <elmar.kleijn@deltares.nl>
2017-08-16 06:08:07 +0000

    Introduced StructuresCalculationContext Related to Issue [WTI-1367]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11269 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: d303927f05faf480cc8f9d57316586a31c969eb1
    Former-commit-id: 4773bda1617c0864108a6e8c2b06f856addd0975

--- Ringtoets/Common/src/Ringtoets.Common.Forms/PresentationObjects/StructuresCalculationContext.cs
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/PresentationObjects/StructuresCalculationContext.cs
@@ -22,30 +22,42 @@
 using System;
 using Ringtoets.Common.Data.AssessmentSection;
 using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Common.Data.FailureMechanism;
 using Ringtoets.Common.Data.Structures;
-using Ringtoets.Common.Forms.PresentationObjects;
-using Ringtoets.HeightStructures.Data;
 
-namespace Ringtoets.HeightStructures.Forms.PresentationObjects
+namespace Ringtoets.Common.Forms.PresentationObjects
 {
     /// <summary>
-    /// Presentation object for all data required to configure an instance of <see cref="StructuresCalculation{T}"/>
+    /// Presentation object for all data required to configure an instance of <see cref="StructuresCalculation{TInput}"/>
     /// in order to prepare it for performing a calculation.
     /// </summary>
-    public class HeightStructuresCalculationContext : StructuresCalculationContext<HeightStructuresInput, HeightStructuresFailureMechanism>
+    public class StructuresCalculationContext<TInput, TFailureMechanism> : FailureMechanismItemContextBase<StructuresCalculation<TInput>, TFailureMechanism>,
+                                                                           ICalculationContext<StructuresCalculation<TInput>, TFailureMechanism>
+        where TInput : IStructuresCalculationInput, new()
+        where TFailureMechanism : IFailureMechanism
     {
         /// <summary>
-        /// Creates a new instance of <see cref="HeightStructuresCalculationContext"/>.
+        /// Creates a new instance of <see cref="StructuresCalculationContext{TInput, TFailureMechanism}"/>.
         /// </summary>
-        /// <param name="calculation">The <see cref="StructuresCalculation{T}"/> instance wrapped by this context object.</param>
+        /// <param name="calculation">The <see cref="StructuresCalculation{TInput}"/> instance wrapped by this context object.</param>
         /// <param name="parent">The <see cref="CalculationGroup"/> that owns the wrapped calculation.</param>
         /// <param name="failureMechanism">The failure mechanism which the context belongs to.</param>
         /// <param name="assessmentSection">The assessment section which the calculation belongs to.</param>
         /// <exception cref="ArgumentNullException">Thrown when any input argument is <c>null</c>.</exception>
-        public HeightStructuresCalculationContext(StructuresCalculation<HeightStructuresInput> calculation,
-                                                  CalculationGroup parent,
-                                                  HeightStructuresFailureMechanism failureMechanism,
-                                                  IAssessmentSection assessmentSection)
-            : base(calculation, parent, failureMechanism, assessmentSection) {}
+        public StructuresCalculationContext(StructuresCalculation<TInput> calculation,
+                                            CalculationGroup parent,
+                                            TFailureMechanism failureMechanism,
+                                            IAssessmentSection assessmentSection)
+            : base(calculation, failureMechanism, assessmentSection)
+        {
+            if (parent == null)
+            {
+                throw new ArgumentNullException(nameof(parent));
+            }
+
+            Parent = parent;
+        }
+
+        public CalculationGroup Parent { get; }
     }
 }
