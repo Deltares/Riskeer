9ca04c726ce0c9ce7c4bcc65d3fd2ba62ac918b1
Robert Tangerman <robert.tangerman@deltares.nl>
2017-08-16 11:13:13 +0000

    Introduced MacroStabilityInwardsGrid. Related to Issue [WTI-1292]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11292 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: e8a099905f692867c6d7e768748a4d116c72eedb
    Former-commit-id: 894d80e0539d51168ebd68a612ca0fd829312af6

---
+++ Ringtoets/MacroStabilityInwards/src/Ringtoets.MacroStabilityInwards.Data/MacroStabilityInwardsGrid.cs
@@ -0,0 +1,121 @@
+// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Base.Data;
+
+namespace Ringtoets.MacroStabilityInwards.Data
+{
+    /// <summary>
+    /// A grid used in the <see cref="MacroStabilityInwardsInput"/>.
+    /// </summary>
+    public class MacroStabilityInwardsGrid
+    {
+        private RoundedDouble xLeft;
+        private RoundedDouble xRight;
+        private RoundedDouble zTop;
+        private RoundedDouble zBottom;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="MacroStabilityInwardsGrid"/>.
+        /// </summary>
+        public MacroStabilityInwardsGrid()
+        {
+            xLeft = new RoundedDouble(2);
+            xRight = new RoundedDouble(2);
+            zTop = new RoundedDouble(2);
+            zBottom = new RoundedDouble(2);
+        }
+
+        /// <summary>
+        /// Gets or sets the x left of the grid.
+        /// [m]
+        /// </summary>
+        public RoundedDouble XLeft
+        {
+            get
+            {
+                return xLeft;
+            }
+            set
+            {
+                xLeft = value.ToPrecision(xLeft.NumberOfDecimalPlaces);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the x right of the grid.
+        /// [m]
+        /// </summary>
+        public RoundedDouble XRight
+        {
+            get
+            {
+                return xRight;
+            }
+            set
+            {
+                xRight = value.ToPrecision(xRight.NumberOfDecimalPlaces);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the z top of the grid.
+        /// [m+NAP]
+        /// </summary>
+        public RoundedDouble ZTop
+        {
+            get
+            {
+                return zTop;
+            }
+            set
+            {
+                zTop = value.ToPrecision(ZTop.NumberOfDecimalPlaces);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the z bottom of the grid.
+        /// [m+NAP]
+        /// </summary>
+        public RoundedDouble ZBottom
+        {
+            get
+            {
+                return zBottom;
+            }
+            set
+            {
+                zBottom = value.ToPrecision(zBottom.NumberOfDecimalPlaces);
+            }
+        }
+
+        /// <summary>
+        /// Gets or set the number of horizontal points.
+        /// </summary>
+        public int NumberOfHorizontalPoints { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of vertical points.
+        /// </summary>
+        public int NumberOfVerticalpoints { get; set; }
+    }
+}
---
+++ Ringtoets/MacroStabilityInwards/test/Ringtoets.MacroStabilityInwards.Data.Test/MacroStabilityInwardsGridTest.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.Data;
+using NUnit.Framework;
+
+namespace Ringtoets.MacroStabilityInwards.Data.Test
+{
+    [TestFixture]
+    public class MacroStabilityInwardsGridTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var grid = new MacroStabilityInwardsGrid();
+
+            // Assert
+            Assert.AreEqual(0, grid.XLeft.Value);
+            Assert.AreEqual(2, grid.XLeft.NumberOfDecimalPlaces);
+
+            Assert.AreEqual(0, grid.XRight.Value);
+            Assert.AreEqual(2, grid.XRight.NumberOfDecimalPlaces);
+
+            Assert.AreEqual(0, grid.ZTop.Value);
+            Assert.AreEqual(2, grid.ZTop.NumberOfDecimalPlaces);
+
+            Assert.AreEqual(0, grid.ZBottom.Value);
+            Assert.AreEqual(2, grid.ZBottom.NumberOfDecimalPlaces);
+
+            Assert.AreEqual(0, grid.NumberOfHorizontalPoints);
+            Assert.AreEqual(0, grid.NumberOfVerticalpoints);
+        }
+
+        [Test]
+        public void Constructor_PropertiesSet_ExpectedValues()
+        {
+            // Setup
+            var random = new Random();
+            double xLeft = random.Next();
+            double xRight = random.Next();
+            double zTop = random.Next();
+            double zBottom = random.Next();
+
+            // call
+            var grid = new MacroStabilityInwardsGrid
+            {
+                XLeft = (RoundedDouble) xLeft,
+                XRight = (RoundedDouble) xRight,
+                ZTop = (RoundedDouble) zTop,
+                ZBottom = (RoundedDouble) zBottom
+            };
+
+            // Assert
+            Assert.AreEqual(new RoundedDouble(2, xLeft), grid.XLeft);
+            Assert.AreEqual(new RoundedDouble(2, xRight), grid.XRight);
+            Assert.AreEqual(new RoundedDouble(2, zTop), grid.ZTop);
+            Assert.AreEqual(new RoundedDouble(2, zBottom), grid.ZBottom);
+        }
+    }
+}
