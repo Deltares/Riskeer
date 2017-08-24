515adad18359253ba49f28454f2c2e9bb5cd2e92
Marten Janssen <marten.janssen@deltares.nl>
2017-08-24 12:18:42 +0000

    Created IllustrationPointsFaultTreeControl Related to Issue [WTI-1312]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11452 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: c7fc8100b21d2e31b9af1101ca280296e39e10de
    Former-commit-id: add73284fdb5f4acc04bbc7cbe81e936f6bfe2a9

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/Views/IllustrationPointsFaultTreeControl.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+
+namespace Ringtoets.Common.Forms.Views
+{
+    /// <summary>
+    /// Control to show fault tree illustration points.
+    /// </summary>
+    public partial class IllustrationPointsFaultTreeControl : UserControl, ISelectionProvider
+    {
+        public event EventHandler<EventArgs> SelectionChanged;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="IllustrationPointsFaultTreeControl"/>.
+        /// </summary>
+        public IllustrationPointsFaultTreeControl()
+        {
+            InitializeComponent();
+            InitializeEventHandlers();
+        }
+
+        /// <summary>
+        /// Gets or sets the data of the control.
+        /// </summary>
+        public object Data { get; set; }
+
+        public object Selection
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        private void InitializeEventHandlers()
+        {
+            pointedTreeGraphControl.SelectionChanged += FaultTreeIllustrationPointsControlOnSelectionChanged;
+        }
+
+        private void FaultTreeIllustrationPointsControlOnSelectionChanged(object sender, EventArgs e)
+        {
+            OnSelectionChanged(e);
+        }
+
+        private void OnSelectionChanged(EventArgs e)
+        {
+            SelectionChanged?.Invoke(this, e);
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.Test/Views/IllustrationPointsFaultTreeControlTest.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Threading;
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using Core.Components.GraphSharp.Forms;
+using NUnit.Framework;
+using Ringtoets.Common.Forms.Views;
+
+namespace Ringtoets.Common.Forms.Test.Views
+{
+    [TestFixture]
+    [Apartment(ApartmentState.STA)]
+    public class IllustrationPointsFaultTreeControlTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            using (var control = new IllustrationPointsFaultTreeControl())
+            {
+                // Assert
+                Assert.IsInstanceOf<UserControl>(control);
+                Assert.IsInstanceOf<ISelectionProvider>(control);
+                Assert.IsNull(control.Data);
+                Assert.IsNull(control.Selection);
+
+                Assert.AreEqual(1, control.Controls.Count);
+                var pointedTreeGraphControl = control.Controls[0] as PointedTreeGraphControl;
+                Assert.IsNotNull(pointedTreeGraphControl);
+            }
+        }
+    }
+}
