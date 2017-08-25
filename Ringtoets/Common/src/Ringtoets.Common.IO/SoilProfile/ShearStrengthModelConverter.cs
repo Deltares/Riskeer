887a554f8ed7f16c3d4ad14e89a91fe6a087fa76
Dennis Tang <dennis.tang@deltares.nl>
2017-08-25 13:30:04 +0000

    Introduced converter: - Converter converts double ShearStrengthModel value to a valid enum, to be used in the 1D and 2D reader of the SoilProfiles. Related to Issue [WTI-1340]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11472 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 34695c7d61683ef466740f33bcbb2eee62ece0f3
    Former-commit-id: a256948c47e0e2cb89c04e766fb46a5b4ac2da38

---
+++ Ringtoets/Common/src/Ringtoets.Common.IO/SoilProfile/ShearStrengthModelConverter.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Globalization;
+
+namespace Ringtoets.Common.IO.SoilProfile
+{
+    /// <summary>
+    /// This class provides helpers for converting values from the D-Soil Model database into
+    /// <see cref="ShearStrengthModel"/>.
+    /// </summary>
+    public static class ShearStrengthModelConverter
+    {
+        /// <summary>
+        /// Converts a nullable <see cref="double"/> into a <see cref="ShearStrengthModel"/>.
+        /// </summary>
+        /// <param name="shearStrengthValue">The value to convert.</param>
+        /// <returns>The <see cref="ShearStrengthModel"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="shearStrengthValue"/> 
+        /// could not be converted.</exception>
+        public static ShearStrengthModel Convert(double? shearStrengthValue)
+        {
+            if (shearStrengthValue == null)
+            {
+                return ShearStrengthModel.None;
+            }
+
+            if (shearStrengthValue.Value.Equals(0.0))
+            {
+                return ShearStrengthModel.None;
+            }
+            if (shearStrengthValue.Value.Equals(1.0))
+            {
+                return ShearStrengthModel.SuCalculated;
+            }
+            if (shearStrengthValue.Value.Equals(2.0))
+            {
+                return ShearStrengthModel.CPhi;
+            }
+            if (shearStrengthValue.Value.Equals(3.0))
+            {
+                return ShearStrengthModel.CPhiOrSuCalculated;
+            }
+
+            string message = $"Cannot convert a value of {shearStrengthValue.Value.ToString(CultureInfo.CurrentCulture)} to {typeof(ShearStrengthModel)}.";
+            throw new ArgumentException(message);
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.IO.Test/SoilProfile/ShearStrengthModelConverterTest.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Common.IO.SoilProfile;
+
+namespace Ringtoets.Common.IO.Test.SoilProfile
+{
+    [TestFixture]
+    public class ShearStrengthModelConverterTest
+    {
+        [Test]
+        public void Convert_Null_ReturnsDefaultValue()
+        {
+            // Call
+            ShearStrengthModel model = ShearStrengthModelConverter.Convert(null);
+
+            // Assert
+            Assert.AreEqual(ShearStrengthModel.None, model);
+        }
+
+        [Test]
+        [TestCase(0, ShearStrengthModel.None)]
+        [TestCase(1, ShearStrengthModel.SuCalculated)]
+        [TestCase(2, ShearStrengthModel.CPhi)]
+        [TestCase(3, ShearStrengthModel.CPhiOrSuCalculated)]
+        public void Convert_ValidValues_ReturnsExpectedValues(double value, ShearStrengthModel expectedShearStrengthModel)
+        {
+            // Call
+            ShearStrengthModel model = ShearStrengthModelConverter.Convert(value);
+
+            // Assert
+            Assert.AreEqual(expectedShearStrengthModel, model);
+        }
+
+        [Test]
+        [SetCulture("nl-NL")]
+        [TestCase(3.5)]
+        [TestCase(-0.5)]
+        [TestCase(double.NaN)]
+        [TestCase(double.NegativeInfinity)]
+        [TestCase(double.PositiveInfinity)]
+        public void Convert_InvalidNumericValues_ThrowsArgumentException(double value)
+        {
+            // Call
+            TestDelegate call = () => ShearStrengthModelConverter.Convert(value);
+
+            // Assert
+            string expectedErrorMessage = $"Cannot convert a value of {value} to {typeof(ShearStrengthModel)}.";
+            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<ArgumentException>(call, expectedErrorMessage);
+        }
+    }
+}
