02de9412f0e856e0ab90df7d1aeff0278b4d2f07
Marten Janssen <marten.janssen@deltares.nl>
2017-08-29 10:04:27 +0000

    Created context classes for the combination of IllustrationPointNode, windDirectionName, and closingSituation Related to Issue [WTI-1312]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11517 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: e9ecdf4a0371eef37952abb319ec6b6413d86527
    Former-commit-id: d08872c17ae1e555d14db848e75df0d810c497c3

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/PresentationObjects/IllustrationPointNodeSubMechanismContext.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Ringtoets.Common.Data.IllustrationPoints;
+
+namespace Ringtoets.Common.Forms.PresentationObjects
+{
+    /// <summary>
+    /// This class is a presentation object for an <see cref="Data.IllustrationPoints.IllustrationPointNode"/> which' 
+    /// <see cref="Data.IllustrationPoints.IllustrationPointNode.Data"/> is of type <see cref="SubMechanismIllustrationPoint"/>.
+    /// </summary>
+    public class IllustrationPointNodeSubMechanismContext
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="IllustrationPointNodeFaultTreeContext"/>.
+        /// </summary>
+        /// <param name="illustrationPointNode">The illustration point node.</param>
+        /// <param name="windDirectionName">The name of the wind direction.</param>
+        /// <param name="closingSituation">The closing situation of the illustration point.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any of the input parameters is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="illustrationPointNode"/> 
+        /// is not of type <see cref="SubMechanismIllustrationPoint"/>.</exception>
+        public IllustrationPointNodeSubMechanismContext(IllustrationPointNode illustrationPointNode,
+                                                     string windDirectionName,
+                                                     string closingSituation)
+        {
+            if (illustrationPointNode == null)
+            {
+                throw new ArgumentNullException(nameof(illustrationPointNode));
+            }
+            if (windDirectionName == null)
+            {
+                throw new ArgumentNullException(nameof(windDirectionName));
+            }
+            if (closingSituation == null)
+            {
+                throw new ArgumentNullException(nameof(closingSituation));
+            }
+
+            var illustrationPoint = illustrationPointNode.Data as SubMechanismIllustrationPoint;
+            if (illustrationPoint == null)
+            {
+                throw new ArgumentException($"{nameof(illustrationPointNode)} type has to be {nameof(SubMechanismIllustrationPoint)}");
+            }
+
+            IllustrationPointNode = illustrationPointNode;
+            WindDirectionName = windDirectionName;
+            ClosingSituation = closingSituation;
+        }
+
+        /// <summary>
+        /// Gets the illustration point node.
+        /// </summary>
+        public IllustrationPointNode IllustrationPointNode { get; }
+
+        /// <summary>
+        /// Gets the wind direction name.
+        /// </summary>
+        public string WindDirectionName { get; }
+
+        /// <summary>
+        /// Gets the closing situation.
+        /// </summary>
+        public string ClosingSituation { get; }
+    }
+}
