1a1d49f43fdeae932e025d9906dcf00a9539bb0d
Dennis Tang <dennis.tang@deltares.nl>
2017-09-08 12:35:55 +0000

    - Removed Preconsolidation stresses from the soil layer => should be defined on the profiles -- Fixed compiling error Related to Issue [WTI-1341]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11659 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 24e4246c11fdd79dee5f70a45bbaa2eda57fa66c
    Former-commit-id: 0341acb383af7d2f8d301d7e10ae59026cf208ee

--- Demo/Ringtoets/test/Demo.Ringtoets.Test/Commands/AddNewDemoAssessmentSectionCommandTest.cs
+++ Demo/Ringtoets/test/Demo.Ringtoets.Test/Commands/AddNewDemoAssessmentSectionCommandTest.cs
@@ -640,7 +640,6 @@ namespace Demo.Ringtoets.Test.Commands
                 })
             };
             CollectionAssert.AreEqual(holes, soilLayer2D.Holes);
-            CollectionAssert.IsEmpty(soilLayer2D.PreconsolidationStresses);
             Assert.AreEqual("2D Layer", soilLayer2D.Properties.MaterialName);
         }
 
