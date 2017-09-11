4abb9905e5c81445217060c7394966acf6a7c0d3
Marten Janssen <marten.janssen@deltares.nl>
2017-09-11 12:05:41 +0000

    - Created MacroStabilityInwardsSoilProfile2DCreateExtensions Related to Issue [WTI-1378]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11684 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: ce8f7dd715e4e407b961f696a2b60bd435abb98a
    Former-commit-id: b923ef798a75fb9f8c0a229781188424a9daf1d1

---
+++ Application/Ringtoets/src/Application.Ringtoets.Storage/Create/MacroStabilityInwards/MacroStabilityInwardsSoilProfile2DCreateExtensions.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Application.Ringtoets.Storage.DbContext;
+using Core.Common.Utils.Extensions;
+using Ringtoets.MacroStabilityInwards.Primitives;
+
+namespace Application.Ringtoets.Storage.Create.MacroStabilityInwards
+{
+    /// <summary>
+    /// Extension methods for <see cref="MacroStabilityInwardsSoilProfile2D"/> related to creating 
+    /// a <see cref="MacroStabilityInwardsSoilProfile2DEntity"/>.
+    /// </summary>
+    internal static class MacroStabilityInwardsSoilProfile2DCreateExtensions
+    {
+        /// <summary>
+        /// Creates a <see cref="MacroStabilityInwardsSoilProfile2DEntity"/> based on the information 
+        /// of the <see cref="MacroStabilityInwardsSoilProfile2D"/>.
+        /// </summary>
+        /// <param name="soilProfile">The soil profile to create a database entity for.</param>
+        /// <param name="registry">The object keeping track of create operations.</param>
+        /// <returns>A new <see cref="MacroStabilityInwardsSoilLayer2DEntity"/> or one from the 
+        /// <paramref name="registry"/> if it was created for the <see cref="soilProfile"/> earlier.</returns>
+        internal static MacroStabilityInwardsSoilProfile2DEntity Create(this MacroStabilityInwardsSoilProfile2D soilProfile,
+                                                                        PersistenceRegistry registry)
+        {
+            if (soilProfile == null)
+            {
+                throw new ArgumentNullException(nameof(soilProfile));
+            }
+            if (registry == null)
+            {
+                throw new ArgumentNullException(nameof(registry));
+            }
+
+            if (registry.Contains(soilProfile))
+            {
+                return registry.Get(soilProfile);
+            }
+
+            var entity = new MacroStabilityInwardsSoilProfile2DEntity
+            {
+                Name = soilProfile.Name.DeepClone()
+            };
+
+            AddEntitiesForSoilLayers(soilProfile.Layers, entity);
+
+            registry.Register(entity, soilProfile);
+            return entity;
+        }
+
+        private static void AddEntitiesForSoilLayers(IEnumerable<MacroStabilityInwardsSoilLayer2D> layers,
+                                                     MacroStabilityInwardsSoilProfile2DEntity entity)
+        {
+            var index = 0;
+            foreach (MacroStabilityInwardsSoilLayer2D layer in layers)
+            {
+                entity.MacroStabilityInwardsSoilLayer2DEntity.Add(layer.Create(index++));
+            }
+        }
+    }
+}
