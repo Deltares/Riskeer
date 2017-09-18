f7f2fb5812d9a3c71a95f13140e8dd22f85768fa
Robert Tangerman <robert.tangerman@deltares.nl>
2017-09-18 11:46:31 +0000

    Added slice with coordinates. Related to Issue [WTI-1407]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11762 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 56fe6b0af4e380b7b73aa650295f183522437976
    Former-commit-id: 08fc071f542cdc74346d9ef4cf1b69b1954fbbe7

---
+++ Ringtoets/MacroStabilityInwards/src/Ringtoets.MacroStabilityInwards.Primitives/Output/MacroStabilityInwardsSlice.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.Geometry;
+
+namespace Ringtoets.MacroStabilityInwards.Primitives.Output
+{
+    /// <summary>
+    /// The slice result of a macro stability calculation.
+    /// </summary>
+    public class MacroStabilityInwardsSlice
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="MacroStabilityInwardsSlice"/>.
+        /// </summary>
+        /// <param name="topLeftPoint">The top left point of the slice.</param>
+        /// <param name="topRightPoint">The top right point of the slice.</param>
+        /// <param name="bottomLeftPoint">The bottom left point of the slice.</param>
+        /// <param name="bottomRightPoint">The bottom right point of the slice.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public MacroStabilityInwardsSlice(Point2D topLeftPoint, Point2D topRightPoint,
+                                          Point2D bottomLeftPoint, Point2D bottomRightPoint)
+        {
+            if (topLeftPoint == null)
+            {
+                throw new ArgumentNullException(nameof(topLeftPoint));
+            }
+            if (topRightPoint == null)
+            {
+                throw new ArgumentNullException(nameof(topRightPoint));
+            }
+            if (bottomLeftPoint == null)
+            {
+                throw new ArgumentNullException(nameof(bottomLeftPoint));
+            }
+            if (bottomRightPoint == null)
+            {
+                throw new ArgumentNullException(nameof(bottomRightPoint));
+            }
+
+            TopLeftPoint = topLeftPoint;
+            TopRightPoint = topRightPoint;
+            BottomLeftPoint = bottomLeftPoint;
+            BottomRightPoint = bottomRightPoint;
+        }
+
+        /// <summary>
+        /// Gets the top left point of the slice.
+        /// </summary>
+        public Point2D TopLeftPoint { get; }
+
+        /// <summary>
+        /// Gets the top right point of the slice.
+        /// </summary>
+        public Point2D TopRightPoint { get; }
+
+        /// <summary>
+        /// Gets the bottom left point of the slice.
+        /// </summary>
+        public Point2D BottomLeftPoint { get; }
+
+        /// <summary>
+        /// Gets the bottom right point of the slice.
+        /// </summary>
+        public Point2D BottomRightPoint { get; }
+    }
+}
---
+++ Ringtoets/MacroStabilityInwards/test/Ringtoets.MacroStabilityInwards.Primitives.Test/Output/MacroStabilityInwardsSliceTest.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.Geometry;
+using NUnit.Framework;
+using Ringtoets.MacroStabilityInwards.Primitives.Output;
+
+namespace Ringtoets.MacroStabilityInwards.Primitives.Test.Output
+{
+    [TestFixture]
+    public class MacroStabilityInwardsSliceTest
+    {
+        [Test]
+        public void Constructor_TopLeftPointNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new MacroStabilityInwardsSlice(null, new Point2D(0, 0), new Point2D(0, 0), new Point2D(0, 0));
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("topLeftPoint", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_TopRightPointNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new MacroStabilityInwardsSlice(new Point2D(0, 0), null, new Point2D(0, 0), new Point2D(0, 0));
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("topRightPoint", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_BottomLeftPointNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new MacroStabilityInwardsSlice(new Point2D(0, 0), new Point2D(0, 0), null, new Point2D(0, 0));
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("bottomLeftPoint", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_BottomRightPointNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new MacroStabilityInwardsSlice(new Point2D(0, 0), new Point2D(0, 0), new Point2D(0, 0), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("bottomRightPoint", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_WithCoordinates_ExpectedValues()
+        {
+            // Setup
+            var topLeftPoint = new Point2D(0, 0);
+            var topRightPoint = new Point2D(1, 1);
+            var bottomLeftPoint = new Point2D(2, 2);
+            var bottomRightPoint = new Point2D(3, 3);
+
+            // Call
+            var slice = new MacroStabilityInwardsSlice(topLeftPoint, topRightPoint, bottomLeftPoint, bottomRightPoint);
+
+            // Assert
+            Assert.AreEqual(topLeftPoint, slice.TopLeftPoint);
+            Assert.AreEqual(topRightPoint, slice.TopRightPoint);
+            Assert.AreEqual(bottomLeftPoint, slice.BottomLeftPoint);
+            Assert.AreEqual(bottomRightPoint, slice.BottomRightPoint);
+        }
+    }
+}
