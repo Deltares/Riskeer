ec61461326a925d109c7a58600a8fe3ee4c2b31c
Robert Tangerman <robert.tangerman@deltares.nl>
2017-09-19 08:33:06 +0000

    Added object for the UpliftVanCalcutionGrid result. Related to Issue [WTI-1407]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11774 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 09434a2acb2cff30504e1aee0277c321a5975ee4
    Former-commit-id: bae18921f5d8499caea69fd26b41976b84e8615b

---
+++ Ringtoets/MacroStabilityInwards/test/Ringtoets.MacroStabilityInwards.KernelWrapper.TestUtil/Result/MacroStabilityInwardsGridResultTestFactory.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Ringtoets.MacroStabilityInwards.KernelWrapper.Result;
+
+namespace Ringtoets.MacroStabilityInwards.KernelWrapper.TestUtil.Result
+{
+    /// <summary>
+    /// Factory to create simple <see cref="MacroStabilityInwardsGridResult"/>
+    /// instances that can be used for testing.
+    /// </summary>
+    public static class MacroStabilityInwardsGridResultTestFactory
+    {
+        /// <summary>
+        /// Creates a new <see cref="MacroStabilityInwardsGridResult"/>.
+        /// </summary>
+        /// <returns>The created <see cref="MacroStabilityInwardsGridResult"/>.</returns>
+        public static MacroStabilityInwardsGridResult Create()
+        {
+            return new MacroStabilityInwardsGridResult(0.1, 0.2, 0.3, 0.4, 1, 2);
+        }
+    }
+}
