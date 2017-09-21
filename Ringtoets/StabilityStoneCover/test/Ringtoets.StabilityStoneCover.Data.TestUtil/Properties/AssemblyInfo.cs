27066d4ab45979dd355bd65610d94f91cc98dc77
Carlos Lubbers <carlos.lubbers@deltares.nl>
2017-09-21 11:56:24 +0000

    Fixed broken build Related to Issue [WTI-1371]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11797 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 7cb0f5112f5d5b7ee5e961212d64d04a7ae0147c
    Former-commit-id: bf569032229599a41b6d0c1e7b100d381f5e4774

--- Ringtoets/StabilityStoneCover/test/Ringtoets.StabilityStoneCover.Data.TestUtil/Properties/AssemblyInfo.cs
+++ Ringtoets/StabilityStoneCover/test/Ringtoets.StabilityStoneCover.Data.TestUtil/Properties/AssemblyInfo.cs
@@ -1,5 +1,27 @@
-﻿using System.Reflection;
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Reflection;
 using System.Runtime.InteropServices;
+
 [assembly: AssemblyTitle("Ringtoets.StabilityStoneCover.Data.TestUtil")]
 [assembly: AssemblyProduct("Ringtoets.StabilityStoneCover.Data.TestUtil")]
 [assembly: Guid("b727e93a-74b2-467a-ae46-9a4e39ed4b5f")]
