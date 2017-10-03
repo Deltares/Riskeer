5c9a69817e0e984a0dcd8c2be1e985af414b9eaf
Elmar Kleijn <elmar.kleijn@deltares.nl>
2017-10-03 06:19:30 +0000

    Introduced some additional calculator enum types Related to Issue [WTI-1405]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@11986 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: aaf09949387f9dfa5e6be2bd7c4bf93ffd34a0fe
    Former-commit-id: 6e40b5302382a8232cff1b0906b8d4b4594ef27d

---
+++ Ringtoets/MacroStabilityInwards/test/Ringtoets.MacroStabilityInwards.KernelWrapper.Test/Calculators/UpliftVan/Input/UpliftVanPlLineCreationMethodTest.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+using Ringtoets.MacroStabilityInwards.KernelWrapper.Calculators.UpliftVan.Input;
+
+namespace Ringtoets.MacroStabilityInwards.KernelWrapper.Test.Calculators.UpliftVan.Input
+{
+    [TestFixture]
+    public class UpliftVanPlLineCreationMethodTest
+    {
+        [Test]
+        public void Values_ExpectedValues()
+        {
+            // Assert
+            Assert.AreEqual(7, Enum.GetValues(typeof(UpliftVanPlLineCreationMethod)).Length);
+            Assert.AreEqual(1, (int) UpliftVanPlLineCreationMethod.ExpertKnowledgeRrd);
+            Assert.AreEqual(2, (int) UpliftVanPlLineCreationMethod.ExpertKnowledgeLinearInDike);
+            Assert.AreEqual(3, (int) UpliftVanPlLineCreationMethod.RingtoetsWti2017);
+            Assert.AreEqual(4, (int) UpliftVanPlLineCreationMethod.DupuitStatic);
+            Assert.AreEqual(5, (int) UpliftVanPlLineCreationMethod.DupuitDynamic);
+            Assert.AreEqual(6, (int) UpliftVanPlLineCreationMethod.Sensors);
+            Assert.AreEqual(7, (int) UpliftVanPlLineCreationMethod.None);
+        }
+    }
+}
