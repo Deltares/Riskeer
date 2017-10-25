e8ab3cb868ab1b4075c3d12f4caf3221a35ab4d4
Dennis Tang <dennis.tang@deltares.nl>
2017-10-25 15:45:46 +0000

    Added tests for creating the waternet zones: - Added test with multiple surface line intersections and waternet line intersection
    Related to Issue [WTI-1395]
    
    
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@12368 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 977ebb9969b8b0af65c5a9c95f5600cfa30ee041
    Former-commit-id: e0981b72eaed4d3ba67513872f4db5c82229ff30

--- Ringtoets/MacroStabilityInwards/test/Ringtoets.MacroStabilityInwards.Forms.Test/Factories/MacroStabilityInwardsChartDataPointsFactoryTest.cs
+++ Ringtoets/MacroStabilityInwards/test/Ringtoets.MacroStabilityInwards.Forms.Test/Factories/MacroStabilityInwardsChartDataPointsFactoryTest.cs
@@ -924,6 +924,67 @@ namespace Ringtoets.MacroStabilityInwards.Forms.Test.Factories
             }, zones[0]);
         }
 
+        [Test]
+        public void CreateWaternetZonePoints_PhreaticLineIntersectsSurfaceLineAtMultiplePointsAndIntersectsWaterNetLine_ReturnsPointArray()
+        {
+            // Setup
+            var waternetLineGeometry = new[]
+            {
+                new Point2D(0, -2),
+                new Point2D(15, -2)
+            };
+            var phreaticLineGeometry = new[]
+            {
+                new Point2D(0, 7),
+                new Point2D(10, 7),
+                new Point2D(15, -8)
+            };
+
+            var surfaceLine = new MacroStabilityInwardsSurfaceLine("Test");
+            surfaceLine.SetGeometry(new[]
+            {
+                new Point3D(0, 2, 2),
+                new Point3D(5, 10, 10),
+                new Point3D(10, 2, 2),
+                new Point3D(15, 2, 10)
+            });
+
+            MacroStabilityInwardsWaternetLine waternetLine = CreateWaterNetLine(phreaticLineGeometry, waternetLineGeometry);
+
+            // Call
+            Point2D[][] zones = MacroStabilityInwardsChartDataPointsFactory.CreateWaternetZonePoints(waternetLine, surfaceLine).ToArray();
+
+            // Assert
+            Assert.AreEqual(2, zones.Length);
+            CollectionAssert.AreEqual(new[]
+            {
+                new Point2D(0, 2),
+                new Point2D(3.125, 7),
+                new Point2D(5, 7),
+                new Point2D(6.875, 7),
+                new Point2D(10, 2),
+                new Point2D(11.09, 3.74),
+                new Point2D(13, -2),
+                new Point2D(13, -2),
+                new Point2D(11.09, -2),
+                new Point2D(10, -2),
+                new Point2D(6.875, -2),
+                new Point2D(5, -2),
+                new Point2D(3.125, -2),
+                new Point2D(0, -2),
+                new Point2D(0, 2)
+            }, zones[0], new Point2DComparerWithTolerance(1e-2));
+
+            CollectionAssert.AreEqual(new[]
+            {
+                new Point2D(13, -2),
+                new Point2D(15, -2),
+                new Point2D(15, -8),
+                new Point2D(13, -2),
+                new Point2D(13, -2)
+            }, zones[1], new Point2DComparerWithTolerance(1e-2));
+        }
+
         private static MacroStabilityInwardsWaternetLine CreateWaterNetLine(IEnumerable<Point2D> waternetLineGeometry,
                                                                             IEnumerable<Point2D> phreaticLineGeometry)
         {
