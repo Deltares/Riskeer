2fd427d95bfd8c000da089fedcf7b2fe2ca592e2
Marten Janssen <marten.janssen@deltares.nl>
2017-10-25 09:00:20 +0000

    Added extension method to find interpolation in Segment2DCollections Related to Issue [WTI-1400]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@12348 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: eb84ac511aff30a6c1a9ffc86fcde7c0c0dfd3b6
    Former-commit-id: 77a45d9d867dcb36ed017408e9448dc16027548b

---
+++ Core/Common/src/Core.Common.Base/Geometry/Segment2DCollectionExtensions.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Common.Base.Geometry
+{
+    /// <summary>
+    /// Extension methods for collections of <see cref="Segment2D"/>.
+    /// </summary>
+    public static class Segment2DCollectionExtensions
+    {
+        /// <summary>
+        /// Interpolates the segments on <paramref name="x"/>.
+        /// </summary>
+        /// <param name="segments">The segments to interpolate on.</param>
+        /// <param name="x">The x value to use for interpolation.</param>
+        /// <returns>The interpolated y value of the segment that is closest to <paramref name="x"/> 
+        /// or <see cref="double.NaN"/> when no interpolation was found.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="segments"/> is <c>null</c>.</exception>
+        public static double Interpolate(this IEnumerable<Segment2D> segments, double x)
+        {
+            if (segments == null)
+            {
+                throw new ArgumentNullException(nameof(segments));
+            }
+
+            Segment2D segment = segments.LastOrDefault(s => s.FirstPoint.X <= x) ?? segments.First();
+
+            return Interpolate(segment, x);
+        }
+
+        /// <summary>
+        /// Interpolates the segment on <paramref name="x"/>.
+        /// </summary>
+        /// <param name="segment">The segment to interpolate on.</param>
+        /// <param name="x">The x value to use for interpolation.</param>
+        /// <returns>The interpolated y value of the <paramref name="segment"/> or 
+        /// <see cref="double.NaN"/> when no interpolation was found.</returns>
+        private static double Interpolate(Segment2D segment, double x)
+        {
+            double differenceInX = segment.SecondPoint.X - segment.FirstPoint.X;
+            if (Math.Abs(differenceInX) < 1e-6)
+            {
+                return double.NaN;
+            }
+
+            double m = (segment.SecondPoint.Y - segment.FirstPoint.Y) / differenceInX;
+
+            double b = segment.FirstPoint.Y - (m * segment.FirstPoint.X);
+
+            return m * x + b;
+        }
+    }
+}
