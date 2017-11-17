b2b948b3cfebe14dbd1f46ae3a690682f47721a9
Elmar Kleijn <elmar.kleijn@deltares.nl>
2017-11-17 09:14:12 +0000

    Extended MapPluginTest Related to Issue [WTI-1485]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/branches/Release/17_2_Development@12878 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 4917b2dbd9b822dbd815a83c38ee24e26fffd282
    Former-commit-id: 2db2fd31bb1dce62e747ee0a2f4cd4d93ad7c478

--- Core/Plugins/test/Core.Plugins.Map.Test/MapPluginTest.cs
+++ Core/Plugins/test/Core.Plugins.Map.Test/MapPluginTest.cs
@@ -379,6 +379,54 @@ namespace Core.Plugins.Map.Test
             Dispatcher.CurrentDispatcher.InvokeShutdown();
         }
 
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void GivenConfiguredGui_WhenOtherMapViewRemoved_ThenComponentsNotUpdated()
+        {
+            // Given
+            var mocks = new MockRepository();
+            var projectStore = mocks.Stub<IStoreProject>();
+            var projectMigrator = mocks.Stub<IMigrateProject>();
+            var projectFactory = mocks.Stub<IProjectFactory>();
+            projectFactory.Stub(pf => pf.CreateNewProject()).Return(mocks.Stub<IProject>());
+            mocks.ReplayAll();
+
+            using (var gui = new GuiCore(new MainWindow(), projectStore, projectMigrator, projectFactory, new GuiCoreSettings()))
+            {
+                var plugin = new MapPlugin
+                {
+                    Gui = gui
+                };
+
+                gui.Plugins.Add(plugin);
+                gui.Run();
+
+                var view1 = new TestMapView();
+                var view2 = new TestMapView();
+                IViewHost guiViewHost = gui.ViewHost;
+                MapLegendView mapLegendView = guiViewHost.ToolViews.OfType<MapLegendView>().First();
+                var mapRibbon = (MapRibbon) plugin.RibbonCommandHandler;
+
+                guiViewHost.AddDocumentView(view1);
+                guiViewHost.AddDocumentView(view2);
+
+                // Precondition
+                Assert.AreSame(view2.Map, GetMapControl(mapLegendView));
+                Assert.AreSame(view2.Map, GetMapControl(mapRibbon));
+
+                // When
+                guiViewHost.Remove(view1);
+
+                // Then
+                Assert.AreSame(view2.Map, GetMapControl(mapLegendView));
+                Assert.AreSame(view2.Map, GetMapControl(mapRibbon));
+
+                mocks.VerifyAll();
+            }
+
+            Dispatcher.CurrentDispatcher.InvokeShutdown();
+        }
+
         private static IMapControl GetMapControl(MapRibbon mapRibbon)
         {
             return TypeUtils.GetProperty<IMapControl>(mapRibbon, "Map");
