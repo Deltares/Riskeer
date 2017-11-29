654acf7fdb1297cdd9d16f2d19600993a3f4a16f
Robert Tangerman <robert.tangerman@deltares.nl>
2017-11-29 10:32:53 +0000

    Remove unused using.
    Paired with kleijn
    Related to Issue [WTI-1476]
    
    
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/branches/Release/17_3@13062 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 6c6ec3f2c23492ae86fadca3b8ca615d6b4e0561
    Former-commit-id: bd727d169aedde252626784d3a3582933e96b791

--- Ringtoets/Common/src/Ringtoets.Common.IO/HydraRing/PreprocessorSettingsProvider.cs
+++ Ringtoets/Common/src/Ringtoets.Common.IO/HydraRing/PreprocessorSettingsProvider.cs
@@ -24,7 +24,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Core.Common.Base.IO;
 using Ringtoets.HydraRing.Calculation.Data.Settings;
-using Ringtoets.HydraRing.Calculation.Readers;
 
 namespace Ringtoets.Common.IO.HydraRing
 {
