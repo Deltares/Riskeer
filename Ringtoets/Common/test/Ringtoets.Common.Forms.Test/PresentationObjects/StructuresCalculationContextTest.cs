b8fc9e68ddf41b3a07a7ef7b74d5ed7e98a42794
Dennis Tang <dennis.tang@deltares.nl>
2017-11-30 09:50:00 +0000

    Introduced EqualsGuidelinesTestFixture for testing overrides of Equals() implementations - Replaced Equals-tests with the testfixture in StructuresCalculationContextTest Related to Issue [WTI-1390]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/trunk@13111 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: 033d121dd878acc69f78d7af54e410fc3dd342e4
    Former-commit-id: 06c322a06d6dcec5eaa7ce3cba99eb95fbc249d8

--- Ringtoets/Common/test/Ringtoets.Common.Forms.Test/PresentationObjects/StructuresCalculationContextTest.cs
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.Test/PresentationObjects/StructuresCalculationContextTest.cs
@@ -20,6 +20,8 @@
 // All rights reserved.
 
 using System;
+using System.Collections.Generic;
+using Core.Common.TestUtil;
 using NUnit.Framework;
 using Rhino.Mocks;
 using Ringtoets.Common.Data.AssessmentSection;
@@ -78,202 +80,88 @@ namespace Ringtoets.Common.Forms.Test.PresentationObjects
             mockRepository.VerifyAll();
         }
 
-        [Test]
-        public void Equals_ToNull_ReturnFalse()
+        [TestFixture]
+        private class StructuresCalculationContextEqualsTest : EqualsGuidelinesTestFixture<TestStructuresCalculationContext, DerivedTestStructuresCalculationContext>
         {
-            // Setup
-            var mocksRepository = new MockRepository();
-            var assessmentSection = mocksRepository.Stub<IAssessmentSection>();
-            mocksRepository.ReplayAll();
+            private MockRepository mocks;
 
-            var calculation = new TestStructuresCalculation();
-            var failureMechanism = new TestFailureMechanism();
-            var parent = new CalculationGroup();
-            var context = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
+            private IAssessmentSection assessmentSection;
+            private TestStructuresCalculation calculation;
+            private TestFailureMechanism failureMechanism;
+            private CalculationGroup parent;
 
-            // Call
-            bool isEqual = context.Equals(null);
+            [SetUp]
+            public void SetUp()
+            {
+                calculation = new TestStructuresCalculation();
+                failureMechanism = new TestFailureMechanism();
+                parent = new CalculationGroup();
 
-            // Assert
-            Assert.IsFalse(isEqual);
+                mocks = new MockRepository();
+                assessmentSection = mocks.Stub<IAssessmentSection>();
+                mocks.ReplayAll();
+            }
 
-            mocksRepository.VerifyAll();
-        }
+            [TearDown]
+            public void TearDown()
+            {
+                mocks.VerifyAll();
+            }
 
-        [Test]
-        public void Equals_ToItself_ReturnTrue()
-        {
-            // Setup
-            var mocksRepository = new MockRepository();
-            var assessmentSection = mocksRepository.Stub<IAssessmentSection>();
-            mocksRepository.ReplayAll();
+            [Test]
+            public void Equals_ToOtherWithDifferentWrappedData_ReturnFalse()
+            {
+                // Setup
+                var differentCalculation = new TestStructuresCalculation();
+                var context1 = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
+                var context2 = new TestStructuresCalculationContext(differentCalculation, parent, failureMechanism, assessmentSection);
 
-            var calculation = new TestStructuresCalculation();
-            var failureMechanism = new TestFailureMechanism();
-            var parent = new CalculationGroup();
-            var context = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
+                // Precondition:
+                Assert.IsFalse(calculation.Equals(differentCalculation));
 
-            // Call
-            bool isEqual = context.Equals(context);
+                // Call
+                bool isEqual1 = context1.Equals(context2);
+                bool isEqual2 = context2.Equals(context1);
 
-            // Assert
-            Assert.IsTrue(isEqual);
+                // Assert
+                Assert.IsFalse(isEqual1);
+                Assert.IsFalse(isEqual2);
+            }
 
-            mocksRepository.VerifyAll();
-        }
+            [Test]
+            public void Equals_ToOtherWithDifferentParent_ReturnFalse()
+            {
+                // Setup
+                var differentParent = new CalculationGroup();
+                var context1 = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
+                var context2 = new TestStructuresCalculationContext(calculation, differentParent, failureMechanism, assessmentSection);
 
-        [Test]
-        public void Equals_ToOtherWithDifferentType_ReturnFalse()
-        {
-            // Setup
-            var mocksRepository = new MockRepository();
-            var assessmentSection = mocksRepository.Stub<IAssessmentSection>();
-            mocksRepository.ReplayAll();
+                // Precondition:
+                Assert.IsFalse(parent.Equals(differentParent));
 
-            var calculation = new TestStructuresCalculation();
-            var failureMechanism = new TestFailureMechanism();
-            var parent = new CalculationGroup();
-            var context = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
+                // Call
+                bool isEqual1 = context1.Equals(context2);
+                bool isEqual2 = context2.Equals(context1);
 
-            // Call
-            bool isEqual = context.Equals(new object());
+                // Assert
+                Assert.IsFalse(isEqual1);
+                Assert.IsFalse(isEqual2);
+            }
 
-            // Assert
-            Assert.IsFalse(isEqual);
+            protected override TestStructuresCalculationContext CreateObject()
+            {
+                return new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
+            }
 
-            mocksRepository.VerifyAll();
-        }
+            protected override DerivedTestStructuresCalculationContext CreateDerivedObject()
+            {
+                return new DerivedTestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
+            }
 
-        [Test]
-        public void Equals_ToDerivedObject_ReturnsFalse()
-        {
-            // Setup  
-            var mocksRepository = new MockRepository();
-            var assessmentSection = mocksRepository.Stub<IAssessmentSection>();
-            mocksRepository.ReplayAll();
-
-            var calculation = new StructuresCalculation<TestStructuresInput>();
-            var failureMechanism = new TestFailureMechanism();
-            var parent = new CalculationGroup();
-            var context = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
-            var derivedContext = new DerivedTestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
-
-            // Call
-            bool isEqual = context.Equals(derivedContext);
-
-            // Assert
-            Assert.IsFalse(isEqual);
-            mocksRepository.VerifyAll();
-        }
-
-        [Test]
-        public void Equals_ToOtherWithDifferentWrappedData_ReturnFalse()
-        {
-            // Setup
-            var mocksRepository = new MockRepository();
-            var assessmentSection = mocksRepository.Stub<IAssessmentSection>();
-            mocksRepository.ReplayAll();
-
-            var calculation1 = new TestStructuresCalculation();
-            var calculation2 = new TestStructuresCalculation();
-            var failureMechanism = new TestFailureMechanism();
-            var parent = new CalculationGroup();
-            var context1 = new TestStructuresCalculationContext(calculation1, parent, failureMechanism, assessmentSection);
-            var context2 = new TestStructuresCalculationContext(calculation2, parent, failureMechanism, assessmentSection);
-
-            // Precondition:
-            Assert.IsFalse(calculation1.Equals(calculation2));
-
-            // Call
-            bool isEqual1 = context1.Equals(context2);
-            bool isEqual2 = context2.Equals(context1);
-
-            // Assert
-            Assert.IsFalse(isEqual1);
-            Assert.IsFalse(isEqual2);
-
-            mocksRepository.VerifyAll();
-        }
-
-        [Test]
-        public void Equals_ToOtherWithDifferentParent_ReturnFalse()
-        {
-            // Setup
-            var mocksRepository = new MockRepository();
-            var assessmentSection = mocksRepository.Stub<IAssessmentSection>();
-            mocksRepository.ReplayAll();
-
-            var calculation = new TestStructuresCalculation();
-            var failureMechanism = new TestFailureMechanism();
-            var parent1 = new CalculationGroup();
-            var parent2 = new CalculationGroup();
-            var context1 = new TestStructuresCalculationContext(calculation, parent1, failureMechanism, assessmentSection);
-            var context2 = new TestStructuresCalculationContext(calculation, parent2, failureMechanism, assessmentSection);
-
-            // Precondition:
-            Assert.IsFalse(parent1.Equals(parent2));
-
-            // Call
-            bool isEqual1 = context1.Equals(context2);
-            bool isEqual2 = context2.Equals(context1);
-
-            // Assert
-            Assert.IsFalse(isEqual1);
-            Assert.IsFalse(isEqual2);
-
-            mocksRepository.VerifyAll();
-        }
-
-        [Test]
-        public void Equals_ToOtherWithSameWrappedDataAndParent_ReturnTrue()
-        {
-            // Setup
-            var mocksRepository = new MockRepository();
-            var assessmentSection = mocksRepository.Stub<IAssessmentSection>();
-            mocksRepository.ReplayAll();
-
-            var calculation = new TestStructuresCalculation();
-            var failureMechanism = new TestFailureMechanism();
-            var parent = new CalculationGroup();
-            var context1 = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
-            var context2 = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
-
-            // Call
-            bool isEqual1 = context1.Equals(context2);
-            bool isEqual2 = context2.Equals(context1);
-
-            // Assert
-            Assert.IsTrue(isEqual1);
-            Assert.IsTrue(isEqual2);
-
-            mocksRepository.VerifyAll();
-        }
-
-        [Test]
-        public void GetHashCode_EqualObjects_ReturnSameHashCode()
-        {
-            // Setup
-            var mocksRepository = new MockRepository();
-            var assessmentSection = mocksRepository.Stub<IAssessmentSection>();
-            mocksRepository.ReplayAll();
-
-            var calculation = new TestStructuresCalculation();
-            var failureMechanism = new TestFailureMechanism();
-            var parent = new CalculationGroup();
-            var context1 = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
-            var context2 = new TestStructuresCalculationContext(calculation, parent, failureMechanism, assessmentSection);
-
-            // Precondition:
-            Assert.AreEqual(context1, context2);
-
-            // Call
-            int hashCode1 = context1.GetHashCode();
-            int hashCode2 = context2.GetHashCode();
-
-            // Assert
-            Assert.AreEqual(hashCode1, hashCode2);
-
-            mocksRepository.VerifyAll();
+            private static IEnumerable<TestCaseData> GetUnequalTestCases()
+            {
+                yield break;
+            }
         }
 
         private class TestStructuresCalculationContext : StructuresCalculationContext<TestStructuresInput, TestFailureMechanism>
