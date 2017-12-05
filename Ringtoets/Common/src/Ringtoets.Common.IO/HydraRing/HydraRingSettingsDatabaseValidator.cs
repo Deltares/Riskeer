e4c8b63e26051b6bd120260482a565a06635743e
Robert Tangerman <robert.tangerman@deltares.nl>
2017-12-05 11:09:15 +0000

    Introduced validator for Hydra-Ring settings database. Related to Issue [WTI-1476]
    git-svn-id: https://repos.deltares.nl/repos/WettelijkToetsInstrumentarium/branches/Release/17_3@13197 5a3db67b-de53-47b5-99c8-a1c30a6650e2
    
    Former-commit-id: b4fcc6d0aa5607e31a3757b14502792239834617
    Former-commit-id: a09321a603cf61a41589aee1437baf34858970f6

---
+++ Ringtoets/Common/src/Ringtoets.Common.IO/HydraRing/HydraRingSettingsDatabaseValidator.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Base.IO;
+using Core.Common.IO.Readers;
+
+namespace Ringtoets.Common.IO.HydraRing
+{
+    /// <summary>
+    /// Validator to validate the Hydra-Ring settings database.
+    /// </summary>
+    internal class HydraRingSettingsDatabaseValidator : SqLiteDatabaseReaderBase
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="HydraRingSettingsDatabaseValidator"/>.
+        /// </summary>
+        /// <param name="databaseFilePath">The full path to the database file to use when reading settings.</param>
+        /// <exception cref="CriticalFileReadException">Thrown when:
+        /// <list type="bullet">
+        /// <item>The <paramref name="databaseFilePath"/> contains invalid characters.</item>
+        /// <item>No file could be found at <paramref name="databaseFilePath"/>.</item>
+        /// <item>Unable to open database file.</item>
+        /// </list>
+        /// </exception>
+        public HydraRingSettingsDatabaseValidator(string databaseFilePath) : base(databaseFilePath) {}
+    }
+}
