b3bd3e50716cae539be438773246e2c8c32dafd8
Elmar Kleijn <elmar.kleijn@deltares.nl>
2018-01-15 11:57:32 +0100

    Improved DesignWaterLevelLocationsView. WTI-1559

--- Ringtoets/Integration/src/Ringtoets.Integration.Forms/Views/DesignWaterLevelLocationsView.cs
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Views/DesignWaterLevelLocationsView.cs
@@ -102,7 +102,7 @@ namespace Ringtoets.Integration.Forms.Views
 
         protected override HydraulicBoundaryLocationCalculation GetCalculation(HydraulicBoundaryLocation location)
         {
-            return location.DesignWaterLevelCalculation;
+            return getCalculationFunc(location);
         }
     }
 }
