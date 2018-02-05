a2916c19f07e0af3138fe5ae29c001ba3d293e56
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-02-05 16:34:03 +0100

    Add enums for the SimpleAssessment result types WTI-1544

---
+++ Ringtoets/Common/src/Ringtoets.Common.Data/FailureMechanism/SimpleAssessmentResultTypeA.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Ringtoets.Common.Data.FailureMechanism
+{
+    /// <summary>
+    /// This enum defines the possible statuses for a simple assessment 
+    /// for each failure mechanism section of type A.
+    /// </summary>
+    public enum SimpleAssessmentResultTypeA
+    {
+        /// <summary>
+        /// No option has been selected for this failure
+        /// mechanism section.
+        /// </summary>
+        NotSelected = 1,
+
+        /// <summary>
+        /// The failure mechanism section is not applicable.
+        /// </summary>
+        NotApplicable = 2,
+
+        /// <summary>
+        /// The probability of failure for the failure mechanism
+        /// section is negligible.
+        /// </summary>
+        ProbabilityNegligible = 3,
+        
+        /// <summary>
+        /// The failure mechanism section needs further assessment.
+        /// </summary>
+        AssessFurther = 4
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Data.Test/FailureMechanism/SimpleAssessmentResultTypeATest.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Common.Data.Test.FailureMechanism
+{
+    [TestFixture]
+    public class SimpleAssessmentResultTypeATest : EnumValuesTestFixture<SimpleAssessmentResultTypeA, byte>
+    {
+        protected override IDictionary<SimpleAssessmentResultTypeA, byte> ExpectedValueForEnumValues
+        {
+            get
+            {
+                return new Dictionary<SimpleAssessmentResultTypeA, byte>
+                {
+                    {
+                        SimpleAssessmentResultTypeA.NotSelected, 1
+                    },
+                    {
+                        SimpleAssessmentResultTypeA.NotApplicable, 2
+                    },
+                    {
+                        SimpleAssessmentResultTypeA.ProbabilityNegligible, 3
+                    },
+                    {
+                        SimpleAssessmentResultTypeA.AssessFurther, 4
+                    }
+                };
+            }
+        }
+    }
+}
