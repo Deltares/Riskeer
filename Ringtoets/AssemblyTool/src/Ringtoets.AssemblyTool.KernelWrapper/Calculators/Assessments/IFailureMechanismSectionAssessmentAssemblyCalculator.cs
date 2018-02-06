4d2792e36d49685ec7aa4de0b7e1e06270c99fe6
Robert Tangerman <robert.tangerman@deltares.nl>
2018-02-06 13:37:41 +0100

    Implemented interface for calculating failure mechanism section assessment assembly. WTI-1618

---
+++ Ringtoets/AssemblyTool/src/Ringtoets.AssemblyTool.KernelWrapper/Calculators/Assessments/IFailureMechanismSectionAssessmentAssemblyCalculator.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Ringtoets.Common.Data.AssemblyTool;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.AssemblyTool.KernelWrapper.Calculators.Assessments
+{
+    /// <summary>
+    /// Interface representing an failure mechanism section assembly assessment calculator.
+    /// </summary>
+    /// <remarks>
+    /// This interface is introduced for being able to test the conversion of:
+    /// <list type="bullet">
+    /// <item>Ringtoets failure mechanism section assembly assessment input into kernel input;</item>
+    /// <item>kernel output into Ringtoets failure mechanism section assembly assessment output.</item>
+    /// </list>
+    /// </remarks>
+    public interface IFailureMechanismSectionAssessmentAssemblyCalculator
+    {
+        /// <summary>
+        /// Assembles the simple assessment for the given <paramref name="input"/>.
+        /// </summary>
+        /// <param name="input">The <see cref="SimpleAssessmentResultType"/> to assemble for.</param>
+        /// <returns>A <see cref="FailureMechanismSectionAssessment"/>.</returns>
+        FailureMechanismSectionAssessment AssembleSimpleAssessment(SimpleAssessmentResultType input);
+
+        /// <summary>
+        /// Assembles the simple assessment for the given <paramref name="input"/>.
+        /// </summary>
+        /// <param name="input">The <see cref="SimpleAssessmentResultValidityOnlyType"/> to assemble for.</param>
+        /// <returns>A <see cref="FailureMechanismSectionAssessment"/>.</returns>
+        FailureMechanismSectionAssessment AssembleSimpleAssessment(SimpleAssessmentResultValidityOnlyType input);
+    }
+}
