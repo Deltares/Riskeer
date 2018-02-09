5bd35f84daa9dfe636d960a4ea0efa0b85cc6e30
Dennis Tang <dennis.tang@deltares.nl>
2018-02-09 10:40:31 +0100

    Set up a service for assembling Wave Impact Asphalt Cover failure mechanism section results
    WTI-1620

---
+++ Ringtoets/WaveImpactAsphaltCover/test/Ringtoets.WaveImpactAsphaltCover.Service.Test/WaveImpactAsphaltCoverAssemblyServiceTest.cs
@@ -0,0 +1,122 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.KernelWrapper.Calculators;
+using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators;
+using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators.Assembly;
+using Ringtoets.Common.Data.AssemblyTool;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.WaveImpactAsphaltCover.Data;
+
+namespace Ringtoets.WaveImpactAsphaltCover.Service.Test
+{
+    [TestFixture]
+    public class WaveImpactAsphaltCoverAssemblyServiceTest
+    {
+        [Test]
+        public void AssembleSimpleAssessment_FailureMechanismSectionResultNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => WaveImpactAsphaltCoverAssemblyService.AssembleSimpleAssessment(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanismSectionResult", exception.ParamName);
+        }
+
+        [Test]
+        public void AssembleSimpleAssessment_WithInput_SetsInputOnCalculator()
+        {
+            // Setup
+            var random = new Random(21);
+            FailureMechanismSection failureMechanismSection = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionResult = new WaveImpactAsphaltCoverFailureMechanismSectionResult(failureMechanismSection)
+            {
+                SimpleAssessmentInput = random.NextEnumValue<SimpleAssessmentResultType>()
+            };
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                var calculatorfactory = (TestAssemblyToolCalculatorFactory)AssemblyToolCalculatorFactory.Instance;
+                FailureMechanismSectionAssemblyCalculatorStub calculator = calculatorfactory.LastCreatedFailureMechanismSectionAssemblyCalculator;
+
+                // Call
+                WaveImpactAsphaltCoverAssemblyService.AssembleSimpleAssessment(sectionResult);
+
+                // Assert
+                Assert.AreEqual(sectionResult.SimpleAssessmentInput, calculator.SimpleAssessmentInput);
+            }
+        }
+
+        [Test]
+        public void AssembleSimpleAssessment_AssemblyRan_SetsOutput()
+        {
+            // Setup
+            FailureMechanismSection failureMechanismSection = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionResult = new WaveImpactAsphaltCoverFailureMechanismSectionResult(failureMechanismSection);
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                // Pre-condition
+                Assert.IsNull(sectionResult.SimpleAssemblyResult);
+
+                var calculatorfactory = (TestAssemblyToolCalculatorFactory)AssemblyToolCalculatorFactory.Instance;
+                FailureMechanismSectionAssemblyCalculatorStub calculator = calculatorfactory.LastCreatedFailureMechanismSectionAssemblyCalculator;
+
+                var random = new Random(21);
+                var calculatorOutput = new FailureMechanismSectionAssembly(random.NextDouble(), random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+                calculator.SimpleAssessmentAssemblyOutput = calculatorOutput;
+
+                // Call
+                WaveImpactAsphaltCoverAssemblyService.AssembleSimpleAssessment(sectionResult);
+
+                // Assert
+                Assert.AreSame(calculatorOutput, sectionResult.SimpleAssemblyResult);
+            }
+        }
+
+        [Test]
+        public void AssembleSimpleAssessment_CalculatorThrowsExceptions_DoesNothing()
+        {
+            // Setup
+            FailureMechanismSection failureMechanismSection = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionResult = new WaveImpactAsphaltCoverFailureMechanismSectionResult(failureMechanismSection);
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                var calculatorfactory = (TestAssemblyToolCalculatorFactory)AssemblyToolCalculatorFactory.Instance;
+                FailureMechanismSectionAssemblyCalculatorStub calculator = calculatorfactory.LastCreatedFailureMechanismSectionAssemblyCalculator;
+                calculator.ThrowExceptionOnCalculate = true;
+
+                // Call
+                TestDelegate call = () => WaveImpactAsphaltCoverAssemblyService.AssembleSimpleAssessment(sectionResult);
+
+                // Assert
+                Assert.DoesNotThrow(call);
+                Assert.IsNull(sectionResult.SimpleAssemblyResult);
+            }
+        }
+    }
+}
