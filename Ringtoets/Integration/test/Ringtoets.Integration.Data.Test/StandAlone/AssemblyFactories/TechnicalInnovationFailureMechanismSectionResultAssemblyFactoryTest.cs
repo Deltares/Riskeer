63e2c5282655255b39ae57323d2ff1a5d247686d
Dennis Tang <dennis.tang@deltares.nl>
2018-02-13 13:19:09 +0100

    Moved services from .Services to .Data for TechnicalInnovation: - Renamed service to factory - Updated XMLDocs
    WTI-1620

--- Ringtoets/Integration/test/Ringtoets.Integration.Data.Test/StandAlone/AssemblyFactories/TechnicalInnovationFailureMechanismSectionResultAssemblyFactoryTest.cs
+++ Ringtoets/Integration/test/Ringtoets.Integration.Data.Test/StandAlone/AssemblyFactories/TechnicalInnovationFailureMechanismSectionResultAssemblyFactoryTest.cs
@@ -23,9 +23,11 @@ using System;
 using Core.Common.TestUtil;
 using NUnit.Framework;
 using Ringtoets.AssemblyTool.KernelWrapper.Calculators;
+using Ringtoets.AssemblyTool.KernelWrapper.Calculators.Assembly;
 using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators;
 using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators.Assembly;
 using Ringtoets.Common.Data.AssemblyTool;
+using Ringtoets.Common.Data.Exceptions;
 using Ringtoets.Common.Data.FailureMechanism;
 using Ringtoets.Common.Data.TestUtil;
 using Ringtoets.Integration.Data.StandAlone.AssemblyFactories;
@@ -34,13 +36,13 @@ using Ringtoets.Integration.Data.StandAlone.SectionResults;
 namespace Ringtoets.Integration.Data.Test.StandAlone.AssemblyFactories
 {
     [TestFixture]
-    public class TechnicalInnovationAssemblyServiceTest
+    public class TechnicalInnovationFailureMechanismSectionResultAssemblyFactoryTest
     {
         [Test]
         public void AssembleSimpleAssessment_FailureMechanismSectionResultNull_ThrowsArgumentNullException()
         {
             // Call
-            TestDelegate call = () => TechnicalInnovationAssemblyService.AssembleSimpleAssessment(null);
+            TestDelegate call = () => TechnicalInnovationFailureMechanismSectionResultAssemblyFactory.AssembleSimpleAssessment(null);
 
             // Assert
             var exception = Assert.Throws<ArgumentNullException>(call);
@@ -64,7 +66,7 @@ namespace Ringtoets.Integration.Data.Test.StandAlone.AssemblyFactories
                 FailureMechanismSectionAssemblyCalculatorStub calculator = calculatorfactory.LastCreatedFailureMechanismSectionAssemblyCalculator;
 
                 // Call
-                TechnicalInnovationAssemblyService.AssembleSimpleAssessment(sectionResult);
+                TechnicalInnovationFailureMechanismSectionResultAssemblyFactory.AssembleSimpleAssessment(sectionResult);
 
                 // Assert
                 Assert.AreEqual(sectionResult.SimpleAssessmentInput, calculator.SimpleAssessmentInput);
@@ -72,7 +74,7 @@ namespace Ringtoets.Integration.Data.Test.StandAlone.AssemblyFactories
         }
 
         [Test]
-        public void AssembleSimpleAssessment_AssemblyRan_SetsOutput()
+        public void AssembleSimpleAssessment_AssemblyRan_ReturnsOutput()
         {
             // Setup
             FailureMechanismSection failureMechanismSection = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
@@ -87,16 +89,17 @@ namespace Ringtoets.Integration.Data.Test.StandAlone.AssemblyFactories
                 FailureMechanismSectionAssemblyCalculatorStub calculator = calculatorfactory.LastCreatedFailureMechanismSectionAssemblyCalculator;
 
                 // Call
-                TechnicalInnovationAssemblyService.AssembleSimpleAssessment(sectionResult);
+                FailureMechanismSectionAssembly actualOutput = 
+                    TechnicalInnovationFailureMechanismSectionResultAssemblyFactory.AssembleSimpleAssessment(sectionResult);
 
                 // Assert
                 FailureMechanismSectionAssembly calculatorOutput = calculator.SimpleAssessmentAssemblyOutput;
-                Assert.AreSame(calculatorOutput, sectionResult.SimpleAssemblyResult);
+                Assert.AreSame(calculatorOutput, actualOutput);
             }
         }
 
         [Test]
-        public void AssembleSimpleAssessment_CalculatorThrowsExceptions_DoesNothing()
+        public void AssembleSimpleAssessment_CalculatorThrowsExceptions_ThrowsAssemblyFactoryException()
         {
             // Setup
             FailureMechanismSection failureMechanismSection = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
@@ -109,11 +112,13 @@ namespace Ringtoets.Integration.Data.Test.StandAlone.AssemblyFactories
                 calculator.ThrowExceptionOnCalculate = true;
 
                 // Call
-                TestDelegate call = () => TechnicalInnovationAssemblyService.AssembleSimpleAssessment(sectionResult);
+                TestDelegate call = () => TechnicalInnovationFailureMechanismSectionResultAssemblyFactory.AssembleSimpleAssessment(sectionResult);
 
                 // Assert
-                Assert.DoesNotThrow(call);
-                Assert.IsNull(sectionResult.SimpleAssemblyResult);
+                var exception = Assert.Throws<AssemblyFactoryException>(call);
+                Exception innerException = exception.InnerException;
+                Assert.IsInstanceOf<FailureMechanismSectionAssemblyCalculatorException>(innerException);
+                Assert.AreEqual(innerException.Message, exception.Message);
             }
         }
     }
