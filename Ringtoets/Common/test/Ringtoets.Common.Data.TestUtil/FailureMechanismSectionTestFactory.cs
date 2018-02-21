f2c1d92559e378e4dab2a0c40592ef8f0f1801cc
Dennis Tang <dennis.tang@deltares.nl>
2018-02-21 16:26:39 +0100

    Incorporated comments review 2507: - Updated XMLDoc
    Paired with carloslubbers
    
    WTI-1617

--- Ringtoets/Common/test/Ringtoets.Common.Data.TestUtil/FailureMechanismSectionTestFactory.cs
+++ Ringtoets/Common/test/Ringtoets.Common.Data.TestUtil/FailureMechanismSectionTestFactory.cs
@@ -37,6 +37,8 @@ namespace Ringtoets.Common.Data.TestUtil
         /// </summary>
         /// <param name="name">The name of the section.</param>
         /// <returns>A valid <see cref="FailureMechanismSection"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="name"/>
+        /// is <c>null.</c></exception>
         public static FailureMechanismSection CreateFailureMechanismSection(string name = "test")
         {
             return CreateFailureMechanismSection(name, new[]
