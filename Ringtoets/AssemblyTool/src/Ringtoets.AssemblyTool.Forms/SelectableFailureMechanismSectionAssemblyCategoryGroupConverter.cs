342d73a689f0cd50cc9283824061554002aaa792
Robert Tangerman <robert.tangerman@deltares.nl>
2018-03-06 13:26:41 +0100

    Added selectable display enum converter. WTI-1627

---
+++ Ringtoets/AssemblyTool/src/Ringtoets.AssemblyTool.Forms/SelectableFailureMechanismSectionAssemblyCategoryGroupConverter.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using Ringtoets.AssemblyTool.Data;
+
+namespace Ringtoets.AssemblyTool.Forms
+{
+    /// <summary>
+    /// Converter to convert into and from <see cref="SelectableFailureMechanismSectionAssemblyCategoryGroup"/>.
+    /// </summary>
+    public static class SelectableFailureMechanismSectionAssemblyCategoryGroupConverter
+    {
+        /// <summary>
+        /// Converts <see cref="FailureMechanismSectionAssemblyCategoryGroup"/> into
+        /// <see cref="SelectableFailureMechanismSectionAssemblyCategoryGroup"/>.
+        /// </summary>
+        /// <param name="categoryGroup">The group to convert.</param>
+        /// <returns>The converted <see cref="SelectableFailureMechanismSectionAssemblyCategoryGroup"/>.</returns>
+        /// <exception cref="InvalidEnumArgumentException">Thrown when <see cref="FailureMechanismSectionAssemblyCategoryGroup"/>
+        /// is an invalid value.</exception>
+        /// <exception cref="NotSupportedException">Thrown when <see cref="FailureMechanismSectionAssemblyCategoryGroup"/>
+        /// is a valid value, but unsupported.</exception>
+        public static SelectableFailureMechanismSectionAssemblyCategoryGroup ConvertTo(FailureMechanismSectionAssemblyCategoryGroup categoryGroup)
+        {
+            if (!Enum.IsDefined(typeof(FailureMechanismSectionAssemblyCategoryGroup), categoryGroup))
+            {
+                throw new InvalidEnumArgumentException(nameof(categoryGroup),
+                                                       (int) categoryGroup,
+                                                       typeof(FailureMechanismSectionAssemblyCategoryGroup));
+            }
+
+            switch (categoryGroup)
+            {
+                case FailureMechanismSectionAssemblyCategoryGroup.None:
+                    return SelectableFailureMechanismSectionAssemblyCategoryGroup.None;
+                case FailureMechanismSectionAssemblyCategoryGroup.NotApplicable:
+                    return SelectableFailureMechanismSectionAssemblyCategoryGroup.NotApplicable;
+                case FailureMechanismSectionAssemblyCategoryGroup.Iv:
+                    return SelectableFailureMechanismSectionAssemblyCategoryGroup.Iv;
+                case FailureMechanismSectionAssemblyCategoryGroup.IIv:
+                    return SelectableFailureMechanismSectionAssemblyCategoryGroup.IIv;
+                case FailureMechanismSectionAssemblyCategoryGroup.IIIv:
+                    return SelectableFailureMechanismSectionAssemblyCategoryGroup.IIIv;
+                case FailureMechanismSectionAssemblyCategoryGroup.IVv:
+                    return SelectableFailureMechanismSectionAssemblyCategoryGroup.IVv;
+                case FailureMechanismSectionAssemblyCategoryGroup.Vv:
+                    return SelectableFailureMechanismSectionAssemblyCategoryGroup.Vv;
+                case FailureMechanismSectionAssemblyCategoryGroup.VIv:
+                    return SelectableFailureMechanismSectionAssemblyCategoryGroup.VIv;
+                case FailureMechanismSectionAssemblyCategoryGroup.VIIv:
+                    return SelectableFailureMechanismSectionAssemblyCategoryGroup.VIIv;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
---
+++ Ringtoets/AssemblyTool/test/Ringtoets.AssemblyTool.Forms.Test/SelectableFailureMechanismSectionAssemblyCategoryGroupConverterTest.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.ComponentModel;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+
+namespace Ringtoets.AssemblyTool.Forms.Test
+{
+    [TestFixture]
+    public class SelectableFailureMechanismSectionAssemblyCategoryGroupConverterTest
+    {
+        [Test]
+        public void ConvertTo_InvalidValue_ThrowsInvalidEnumArgumentException()
+        {
+            // Call
+            TestDelegate test = () => SelectableFailureMechanismSectionAssemblyCategoryGroupConverter.ConvertTo((FailureMechanismSectionAssemblyCategoryGroup) 99);
+
+            // Assert
+            const string expectedMessage = "The value of argument 'categoryGroup' (99) is invalid for Enum type 'FailureMechanismSectionAssemblyCategoryGroup'.";
+            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<InvalidEnumArgumentException>(test, expectedMessage);
+        }
+
+        [Test]
+        [TestCase(FailureMechanismSectionAssemblyCategoryGroup.None, SelectableFailureMechanismSectionAssemblyCategoryGroup.None)]
+        [TestCase(FailureMechanismSectionAssemblyCategoryGroup.NotApplicable, SelectableFailureMechanismSectionAssemblyCategoryGroup.NotApplicable)]
+        [TestCase(FailureMechanismSectionAssemblyCategoryGroup.Iv, SelectableFailureMechanismSectionAssemblyCategoryGroup.Iv)]
+        [TestCase(FailureMechanismSectionAssemblyCategoryGroup.IIv, SelectableFailureMechanismSectionAssemblyCategoryGroup.IIv)]
+        [TestCase(FailureMechanismSectionAssemblyCategoryGroup.IIIv, SelectableFailureMechanismSectionAssemblyCategoryGroup.IIIv)]
+        [TestCase(FailureMechanismSectionAssemblyCategoryGroup.IVv, SelectableFailureMechanismSectionAssemblyCategoryGroup.IVv)]
+        [TestCase(FailureMechanismSectionAssemblyCategoryGroup.Vv, SelectableFailureMechanismSectionAssemblyCategoryGroup.Vv)]
+        [TestCase(FailureMechanismSectionAssemblyCategoryGroup.VIv, SelectableFailureMechanismSectionAssemblyCategoryGroup.VIv)]
+        [TestCase(FailureMechanismSectionAssemblyCategoryGroup.VIIv, SelectableFailureMechanismSectionAssemblyCategoryGroup.VIIv)]
+        public void ConvertTo_ValidValue_ReturnsConvertedValue(FailureMechanismSectionAssemblyCategoryGroup categoryGroup,
+                                                               SelectableFailureMechanismSectionAssemblyCategoryGroup expectedDisplayCategoryGroup)
+        {
+            // Call
+            SelectableFailureMechanismSectionAssemblyCategoryGroup displayCategoryGroup = SelectableFailureMechanismSectionAssemblyCategoryGroupConverter.ConvertTo(
+                categoryGroup);
+
+            // Assert
+            Assert.AreEqual(expectedDisplayCategoryGroup, displayCategoryGroup);
+        }
+    }
+}
