3f68efc446aaae3370b32ba75825e7735f71e742
Robert Tangerman <robert.tangerman@deltares.nl>
2018-03-09 13:24:30 +0100

    Added WBI-0G-4 method to interface. WTI-1634

--- Ringtoets/AssemblyTool/src/Ringtoets.AssemblyTool.KernelWrapper/Calculators/Assembly/IFailureMechanismSectionAssemblyCalculator.cs
+++ Ringtoets/AssemblyTool/src/Ringtoets.AssemblyTool.KernelWrapper/Calculators/Assembly/IFailureMechanismSectionAssemblyCalculator.cs
@@ -83,6 +83,28 @@ namespace Ringtoets.AssemblyTool.KernelWrapper.Calculators.Assembly
                                                                    double n);
 
         /// <summary>
+        /// Assembles the detailed assessment based on the input parameters.
+        /// </summary>
+        /// <param name="detailedAssesmentResultForFactorizedSignalingNorm">The detailed assessment result
+        /// for category Iv - IIv.</param>
+        /// <param name="detailedAssesmentResultForSignalingNorm">The detailed assessment result for category
+        /// IIv - IIIv.</param>
+        /// <param name="detailedAssesmentResultForMechanismSpecificLowerLimitNorm">The detailed assessment
+        /// result  for category IIIv - IVv.</param>
+        /// <param name="detailedAssesmentResultForLowerLimitNorm">The detailed assessment result for category
+        /// IVv - Vv.</param>
+        /// <param name="detailedAssesmentResultForFactorizedLowerLimitNorm">The detailed assessment result
+        /// for category Vv - VIv.</param>
+        /// <returns>A <see cref="FailureMechanismSectionAssemblyCategoryGroup"/>.</returns>
+        /// <exception cref="FailureMechanismSectionAssemblyCalculatorException">Thrown when
+        /// an error occurs when performing the assembly.</exception>
+        FailureMechanismSectionAssemblyCategoryGroup AssembleDetailedAssessment(DetailedAssessmentResultType detailedAssesmentResultForFactorizedSignalingNorm,
+                                                                                DetailedAssessmentResultType detailedAssesmentResultForSignalingNorm,
+                                                                                DetailedAssessmentResultType detailedAssesmentResultForMechanismSpecificLowerLimitNorm,
+                                                                                DetailedAssessmentResultType detailedAssesmentResultForLowerLimitNorm,
+                                                                                DetailedAssessmentResultType detailedAssesmentResultForFactorizedLowerLimitNorm);
+
+        /// <summary>
         /// Assembles the tailor made assessment based on the input parameters.
         /// </summary>
         /// <param name="tailorMadeAssessmentResult">The <see cref="TailorMadeAssessmentProbabilityAndDetailedCalculationResultType"/>
