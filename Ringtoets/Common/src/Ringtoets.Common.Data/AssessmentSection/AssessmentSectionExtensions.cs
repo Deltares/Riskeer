83ad808e24b4781346cbf61955354127fd942a75
Elmar Kleijn <elmar.kleijn@deltares.nl>
2018-03-20 07:17:41 +0100

    Rewired GetNormativeAssessmentLevel of AssessmentSectionExtensions. WTI-1612

--- Ringtoets/Common/src/Ringtoets.Common.Data/AssessmentSection/AssessmentSectionExtensions.cs
+++ Ringtoets/Common/src/Ringtoets.Common.Data/AssessmentSection/AssessmentSectionExtensions.cs
@@ -21,6 +21,8 @@
 
 using System;
 using System.ComponentModel;
+using System.Linq;
+using Core.Common.Base;
 using Core.Common.Base.Data;
 using Ringtoets.Common.Data.Contribution;
 using Ringtoets.Common.Data.Hydraulics;
@@ -33,15 +35,17 @@ namespace Ringtoets.Common.Data.AssessmentSection
     public static class AssessmentSectionExtensions
     {
         /// <summary>
-        /// Gets the normative assessment level from a <see cref="HydraulicBoundaryLocation"/>.
+        /// Gets the normative assessment level for a <see cref="HydraulicBoundaryLocation"/>.
         /// </summary>
-        /// <param name="assessmentSection">The assessment section.</param>
+        /// <param name="assessmentSection">The assessment section to get the normative assessment
+        /// level from.</param>
         /// <param name="hydraulicBoundaryLocation">The hydraulic boundary location to get the normative
-        /// assessment level from.</param>
+        /// assessment level for.</param>
         /// <returns>The normative assessment level or <see cref="RoundedDouble.NaN"/> when:
         /// <list type="bullet">
         /// <item><paramref name="hydraulicBoundaryLocation"/> is <c>null</c>;</item>
-        /// <item><paramref name="hydraulicBoundaryLocation"/> contains no corresponding calculation output.</item>
+        /// <item><paramref name="hydraulicBoundaryLocation"/> is not part of <paramref name="assessmentSection"/>.</item>
+        /// <item><paramref name="assessmentSection"/> contains no corresponding calculation output.</item>
         /// </list>
         /// </returns>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="assessmentSection"/>
@@ -66,15 +70,23 @@ namespace Ringtoets.Common.Data.AssessmentSection
                                                        typeof(NormType));
             }
 
+            IObservableEnumerable<HydraulicBoundaryLocationCalculation> calculations;
+
             switch (normType)
             {
                 case NormType.Signaling:
-                    return hydraulicBoundaryLocation?.DesignWaterLevelCalculation2.Output?.Result ?? RoundedDouble.NaN;
+                    calculations = assessmentSection.WaterLevelCalculationsForSignalingNorm;
+                    break;
                 case NormType.LowerLimit:
-                    return hydraulicBoundaryLocation?.DesignWaterLevelCalculation3.Output?.Result ?? RoundedDouble.NaN;
+                    calculations = assessmentSection.WaterLevelCalculationsForLowerLimitNorm;
+                    break;
                 default:
                     throw new NotSupportedException();
             }
+
+            HydraulicBoundaryLocationCalculation calculation = calculations.FirstOrDefault(calc => ReferenceEquals(calc.HydraulicBoundaryLocation, hydraulicBoundaryLocation));
+
+            return calculation?.Output?.Result ?? RoundedDouble.NaN;
         }
     }
 }
