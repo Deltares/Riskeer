76d124728818abb926ed5adb281b264857a025c3
Robert Tangerman <robert.tangerman@deltares.nl>
2018-03-23 13:07:10 +0100

    Cleaned test of piping failure mechanism view. WTI-1612

--- Ringtoets/Piping/test/Ringtoets.Piping.Forms.Test/Views/PipingFailureMechanismViewTest.cs
+++ Ringtoets/Piping/test/Ringtoets.Piping.Forms.Test/Views/PipingFailureMechanismViewTest.cs
@@ -63,7 +63,7 @@ namespace Ringtoets.Piping.Forms.Test.Views
         private const int calculationsIndex = 7;
 
         [Test]
-        public void DefaultConstructor_DefaultValues()
+        public void Constructor_ExpectedValues()
         {
             // Call
             using (var view = new PipingFailureMechanismView())
@@ -71,22 +71,12 @@ namespace Ringtoets.Piping.Forms.Test.Views
                 // Assert
                 Assert.IsInstanceOf<UserControl>(view);
                 Assert.IsInstanceOf<IMapView>(view);
-                Assert.IsNotNull(view.Map);
                 Assert.IsNull(view.Data);
-            }
-        }
 
-        [Test]
-        public void DefaultConstructor_Always_AddEmptyMapControl()
-        {
-            // Call
-            using (var view = new PipingFailureMechanismView())
-            {
-                // Assert
                 Assert.AreEqual(1, view.Controls.Count);
                 Assert.IsInstanceOf<RingtoetsMapControl>(view.Controls[0]);
-                Assert.AreSame(view.Map, ((RingtoetsMapControl) view.Controls[0]).MapControl);
-                Assert.AreEqual(DockStyle.Fill, ((Control) view.Map).Dock);
+                Assert.AreSame(view.Map, ((RingtoetsMapControl)view.Controls[0]).MapControl);
+                Assert.AreEqual(DockStyle.Fill, ((Control)view.Map).Dock);
                 Assert.IsNull(view.Map.Data);
             }
         }
