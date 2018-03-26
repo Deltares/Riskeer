8a82df0e24f6ebb364ef926808ef15418613a381
Robert Tangerman <robert.tangerman@deltares.nl>
2018-03-26 13:15:51 +0200

    Cleaned grass cover erosion inwards view test. WTI-1612

--- Ringtoets/GrassCoverErosionInwards/test/Ringtoets.GrassCoverErosionInwards.Forms.Test/Views/GrassCoverErosionInwardsFailureMechanismViewTest.cs
+++ Ringtoets/GrassCoverErosionInwards/test/Ringtoets.GrassCoverErosionInwards.Forms.Test/Views/GrassCoverErosionInwardsFailureMechanismViewTest.cs
@@ -57,7 +57,7 @@ namespace Ringtoets.GrassCoverErosionInwards.Forms.Test.Views
         private const int calculationsIndex = 7;
 
         [Test]
-        public void DefaultConstructor_DefaultValues()
+        public void Constructor_ExpectedValues()
         {
             // Call
             using (var view = new GrassCoverErosionInwardsFailureMechanismView())
@@ -65,23 +65,13 @@ namespace Ringtoets.GrassCoverErosionInwards.Forms.Test.Views
                 // Assert
                 Assert.IsInstanceOf<UserControl>(view);
                 Assert.IsInstanceOf<IMapView>(view);
-                Assert.IsNotNull(view.Map);
                 Assert.IsNull(view.Data);
-            }
-        }
 
-        [Test]
-        public void DefaultConstructor_Always_AddEmptyMapControl()
-        {
-            // Call
-            using (var view = new GrassCoverErosionInwardsFailureMechanismView())
-            {
-                // Assert
                 Assert.AreEqual(1, view.Controls.Count);
                 Assert.IsInstanceOf<RingtoetsMapControl>(view.Controls[0]);
-                Assert.AreSame(view.Map, ((RingtoetsMapControl) view.Controls[0]).MapControl);
-                Assert.AreEqual(DockStyle.Fill, ((Control) view.Map).Dock);
-                Assert.IsNull(view.Map.Data);
+                Assert.AreSame(view.Map, ((RingtoetsMapControl)view.Controls[0]).MapControl);
+                Assert.AreEqual(DockStyle.Fill, ((Control)view.Map).Dock);
+                AssertEmptyMapData(view.Map.Data);
             }
         }
 
