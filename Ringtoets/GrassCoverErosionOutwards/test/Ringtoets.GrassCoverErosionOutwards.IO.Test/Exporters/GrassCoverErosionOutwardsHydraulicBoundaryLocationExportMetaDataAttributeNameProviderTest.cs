e613a7c9d1e9d2cc071aa9dbad42fbffeb4d2228
Robert Tangerman <robert.tangerman@deltares.nl>
2018-04-03 11:08:24 +0200

    Created name provider that can be used in exporter. WTI-1612

---
+++ Ringtoets/GrassCoverErosionOutwards/test/Ringtoets.GrassCoverErosionOutwards.IO.Test/Exporters/GrassCoverErosionOutwardsHydraulicBoundaryLocationExportMetaDataAttributeNameProviderTest.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+using Ringtoets.GrassCoverErosionOutwards.IO.Exporters;
+using Ringtoets.GrassCoverErosionOutwards.Util;
+
+namespace Ringtoets.GrassCoverErosionOutwards.IO.Test.Exporters
+{
+    [TestFixture]
+    public class GrassCoverErosionOutwardsHydraulicBoundaryLocationExportMetaDataAttributeNameProviderTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var nameProvider = new GrassCoverErosionOutwardsHydraulicBoundaryLocationExportMetaDataAttributeNameProvider();
+
+            // Assert
+            Assert.IsInstanceOf<IGrassCoverErosionOutwardsHydraulicBoundaryLocationMetaDataAttributeNameProvider>(nameProvider);
+            Assert.AreEqual("h I-II", nameProvider.WaterLevelCalculationForMechanismSpecificFactorizedSignalingNormAttributeName);
+            Assert.AreEqual("h II-III", nameProvider.WaterLevelCalculationForMechanismSpecificSignalingNormAttributeName);
+            Assert.AreEqual("h III-IV", nameProvider.WaterLevelCalculationForMechanismSpecificLowerLimitNormAttributeName);
+            Assert.AreEqual("h IV-V", nameProvider.WaterLevelCalculationForLowerLimitNormAttributeName);
+            Assert.AreEqual("h V-VI", nameProvider.WaterLevelCalculationForFactorizedLowerLimitNormAttributeName);
+            Assert.AreEqual("hs I-II", nameProvider.WaveHeightCalculationForMechanismSpecificFactorizedSignalingNormAttributeName);
+            Assert.AreEqual("hs II-III", nameProvider.WaveHeightCalculationForMechanismSpecificSignalingNormAttributeName);
+            Assert.AreEqual("hs III-IV", nameProvider.WaveHeightCalculationForMechanismSpecificLowerLimitNormAttributeName);
+            Assert.AreEqual("hs IV-V", nameProvider.WaveHeightCalculationForLowerLimitNormAttributeName);
+            Assert.AreEqual("hs V-VI", nameProvider.WaveHeightCalculationForFactorizedLowerLimitNormAttributeName);
+        }
+    }
+}
