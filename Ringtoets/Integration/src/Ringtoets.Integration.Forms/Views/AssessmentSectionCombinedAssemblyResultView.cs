2557299b40afd0095edc420a76cb9b7ea03c1f7f
Dennis Tang <dennis.tang@deltares.nl>
2018-04-23 16:05:56 +0200

    Added initial setup of the total assembly for the assessment section: - added view with a datagridview control and tests
    WTI-1679

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Views/AssessmentSectionCombinedAssemblyResultView.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using Ringtoets.Integration.Data;
+
+namespace Ringtoets.Integration.Forms.Views
+{
+    /// <summary>
+    /// The view for the combined assembly result for all failure mechanisms of 
+    /// the <see cref="AssessmentSection"/>. 
+    /// </summary>
+    public partial class AssessmentSectionCombinedAssemblyResultView : UserControl, IView
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="AssessmentSectionCombinedAssemblyResultView"/>.
+        /// </summary>
+        /// <param name="assessmentSection">The <see cref="AssessmentSection"/> to create the view for.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="assessmentSection"/>
+        /// is <c>null</c>.</exception>
+        public AssessmentSectionCombinedAssemblyResultView(AssessmentSection assessmentSection)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+
+        public object Data { get; set; }
+
+        private void InitializeDataGridView()
+        {
+            dataGridViewControl.AddTextBoxColumn(null, "Toetsspoor", true);
+            dataGridViewControl.AddTextBoxColumn(null, "Label", true);
+            dataGridViewControl.AddTextBoxColumn(null, "Groep", true);
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/Views/AssessmentSectionCombinedAssemblyResultViewTest.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using Core.Common.TestUtil;
+using NUnit.Extensions.Forms;
+using NUnit.Framework;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.Forms.Views;
+
+namespace Ringtoets.Integration.Forms.Test.Views
+{
+    [TestFixture]
+    public class AssessmentSectionCombinedAssemblyResultViewTest
+    {
+        private const int failureMechanismNameColumnIndex = 0;
+        private const int failureMechanismCodeColumnIndex = 1;
+        private const int failureMechanismGroupColumnIndex = 2;
+        private Form testForm;
+
+        [SetUp]
+        public void Setup()
+        {
+            testForm = new Form();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            testForm.Dispose();
+        }
+
+        [Test]
+        public void Constructor_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new AssessmentSectionCombinedAssemblyResultView(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
+        public void DefaultConstructor_DefaultValues()
+        {
+            // Setup
+            var random = new Random(21);
+            var assessmentSection = new AssessmentSection(random.NextEnumValue<AssessmentSectionComposition>());
+
+            // Call
+            using (var view = new AssessmentSectionCombinedAssemblyResultView(assessmentSection))
+            {
+                testForm.Controls.Add(view);
+                testForm.Show();
+
+                // Assert
+                Assert.AreEqual(1, view.Controls.Count);
+
+                Assert.IsInstanceOf<IView>(view);
+                Assert.IsInstanceOf<UserControl>(view);
+                Assert.IsNull(view.Data);
+            }
+        }
+
+        [Test]
+        public void GivenFormWithAssessmentSection_ThenExpectedColumnsAreVisible()
+        {
+            // Given
+            var random = new Random(21);
+            var assessmentSection = new AssessmentSection(random.NextEnumValue<AssessmentSectionComposition>());
+
+            // Then
+            using (var view = new AssessmentSectionCombinedAssemblyResultView(assessmentSection))
+            {
+                testForm.Controls.Add(view);
+                testForm.Show();
+
+                var dataGridView = (DataGridView) new ControlTester("dataGridView").TheObject;
+                Assert.AreEqual(3, dataGridView.ColumnCount);
+
+                DataGridViewColumnCollection dataGridViewColumns = dataGridView.Columns;
+
+                Assert.IsInstanceOf<DataGridViewTextBoxColumn>(dataGridViewColumns[failureMechanismNameColumnIndex]);
+                Assert.IsInstanceOf<DataGridViewTextBoxColumn>(dataGridViewColumns[failureMechanismCodeColumnIndex]);
+                Assert.IsInstanceOf<DataGridViewTextBoxColumn>(dataGridViewColumns[failureMechanismGroupColumnIndex]);
+
+                Assert.AreEqual("Toetsspoor", dataGridViewColumns[failureMechanismNameColumnIndex].HeaderText);
+                Assert.AreEqual("Label", dataGridViewColumns[failureMechanismCodeColumnIndex].HeaderText);
+                Assert.AreEqual("Groep", dataGridViewColumns[failureMechanismGroupColumnIndex].HeaderText);
+
+                Assert.IsTrue(dataGridViewColumns[failureMechanismNameColumnIndex].ReadOnly);
+                Assert.IsTrue(dataGridViewColumns[failureMechanismCodeColumnIndex].ReadOnly);
+                Assert.IsTrue(dataGridViewColumns[failureMechanismGroupColumnIndex].ReadOnly);
+            }
+        }
+    }
+}
