2fac8e6d2eb69793eba95e294250796849577aef
Elmar Kleijn <elmar.kleijn@deltares.nl>
2018-05-01 16:12:42 +0200

    Added TestWaveConditionsInput.

--- Ringtoets/Revetment/test/Ringtoets.Revetment.Forms.TestUtil.Test/TestWaveConditionsInputContextTest.cs
+++ Ringtoets/Revetment/test/Ringtoets.Revetment.Forms.TestUtil.Test/TestWaveConditionsInputContextTest.cs
@@ -23,7 +23,6 @@ using NUnit.Framework;
 using Rhino.Mocks;
 using Ringtoets.Common.Data.AssessmentSection;
 using Ringtoets.Common.Data.DikeProfiles;
-using Ringtoets.Common.Data.Hydraulics;
 using Ringtoets.Common.Data.TestUtil;
 using Ringtoets.Revetment.Data;
 using Ringtoets.Revetment.Data.TestUtil;
