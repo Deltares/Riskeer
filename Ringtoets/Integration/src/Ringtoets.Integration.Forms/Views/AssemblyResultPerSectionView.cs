d61d7132f071bfb24f72fba861f85b7a35814b5d
Robert Tangerman <robert.tangerman@deltares.nl>
2018-05-01 15:05:12 +0200

    Created view for Assembly result per section. WTI-1684

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Views/AssemblyResultPerSectionView.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using Ringtoets.Integration.Data;
+
+namespace Ringtoets.Integration.Forms.Views
+{
+    /// <summary>
+    /// The view for the assembly result per section for all failure mechanisms of 
+    /// the <see cref="AssessmentSection"/>. 
+    /// </summary>
+    public partial class AssemblyResultPerSectionView : UserControl, IView
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="AssemblyResultPerSectionView"/>.
+        /// </summary>
+        public AssemblyResultPerSectionView()
+        {
+            InitializeComponent();
+        }
+
+        public object Data { get; set; }
+    }
+}
