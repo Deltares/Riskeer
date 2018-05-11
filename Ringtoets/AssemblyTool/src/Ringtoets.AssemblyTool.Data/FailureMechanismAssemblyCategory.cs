a251293462e2a2aa75936cb721a1fc815645b17b
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-05-11 11:26:39 +0200

    Add failure mechanism assembly category object WTI-1594

---
+++ Ringtoets/AssemblyTool/src/Ringtoets.AssemblyTool.Data/FailureMechanismAssemblyCategory.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Ringtoets.AssemblyTool.Data
+{
+    /// <summary>
+    /// Assembly category for failure mechanism section.
+    /// </summary>
+    public class FailureMechanismAssemblyCategory : AssemblyCategory
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="FailureMechanismAssemblyCategory"/>.
+        /// </summary>
+        /// <param name="lowerBoundary">The lower boundary of the category.</param>
+        /// <param name="upperBoundary">The upper boundary of the category.</param>
+        /// <param name="group">The group of the category.</param>
+        public FailureMechanismAssemblyCategory(double lowerBoundary, double upperBoundary, FailureMechanismAssemblyCategoryGroup group)
+            : base(lowerBoundary, upperBoundary)
+        {
+            Group = group;
+        }
+
+        /// <summary>
+        /// Gets the group of the assembly category.
+        /// </summary>
+        public FailureMechanismAssemblyCategoryGroup Group { get; }
+    }
+}
