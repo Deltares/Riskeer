7c1d0fc0849778e055f4f4120dd2b6fcdfd673b3
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-05-16 11:35:09 +0200

    Create base failure mechanism result observer WTI-1687

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/Observers/FailureMechanismResultObserver.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Common.Forms.Observers
+{
+    /// <summary>
+    /// Class that observes all objects in an <see cref="IFailureMechanism"/> related to
+    /// its section results.
+    /// </summary>
+    /// <typeparam name="TFailureMechanism">The type of the failure mechanism to observe.</typeparam>
+    /// <typeparam name="TSectionResult">The type of the section results in the failure mechanism.</typeparam>
+    public class FailureMechanismResultObserver<TFailureMechanism, TSectionResult> : Observable, IDisposable
+    where TFailureMechanism : IFailureMechanism, IHasSectionResults<TSectionResult>
+    where TSectionResult : FailureMechanismSectionResult
+    {
+        private readonly Observer failureMechanismObserver;
+        private readonly Observer failureMechanismSectionResultObserver;
+        private readonly RecursiveObserver<IObservableEnumerable<TSectionResult>, TSectionResult> failureMechanismSectionResultsObserver;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="FailureMechanismResultObserver{TFailureMechanism,TSectionResult}"/>.
+        /// </summary>
+        /// <param name="failureMechanism">The <typeparamref name="TFailureMechanism"/> to observe.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="failureMechanism"/> is <c>null</c>.</exception>
+        public FailureMechanismResultObserver(TFailureMechanism failureMechanism)
+        {
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+
+            failureMechanismObserver = new Observer(NotifyObservers)
+            {
+                Observable = failureMechanism
+            };
+
+            failureMechanismSectionResultObserver = new Observer(NotifyObservers)
+            {
+                Observable = failureMechanism.SectionResults
+            };
+
+            failureMechanismSectionResultsObserver = new RecursiveObserver<IObservableEnumerable<TSectionResult>, TSectionResult>(
+                NotifyObservers,
+                sr => sr)
+            {
+                Observable = failureMechanism.SectionResults
+            };
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                failureMechanismObserver.Dispose();
+                failureMechanismSectionResultObserver.Dispose();
+                failureMechanismSectionResultsObserver.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.Test/Observers/FailureMechanismResultObserverTest.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using Core.Common.Base;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Common.Forms.Observers;
+
+namespace Ringtoets.Common.Forms.Test.Observers
+{
+    [TestFixture]
+    public class FailureMechanismResultObserverTest
+    {
+        [Test]
+        public void Constructor_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate test = () => new FailureMechanismResultObserver<TestFailureMechanism, FailureMechanismSectionResult>(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(test);
+            Assert.AreEqual("failureMechanism", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_WithFailureMechanism_ExpectedProperties()
+        {
+            // Call
+            using (var resultObserver = new FailureMechanismResultObserver<TestFailureMechanism, FailureMechanismSectionResult>(new TestFailureMechanism()))
+            {
+                // Assert
+                Assert.IsInstanceOf<Observable>(resultObserver);
+                Assert.IsInstanceOf<IDisposable>(resultObserver);
+            }
+        }
+
+        [Test]
+        public void GivenFailureMechanismResultObserverWithAttachedObserver_WhenFailureMechanismNotifiesObservers_ThenFailureMechanismResultObserverNotified()
+        {
+            // Given
+            var failureMechanism = new TestFailureMechanism();
+
+            using (var resultObserver = new FailureMechanismResultObserver<TestFailureMechanism, FailureMechanismSectionResult>(failureMechanism))
+            {
+                var mocks = new MockRepository();
+                var observer = mocks.StrictMock<IObserver>();
+                observer.Expect(o => o.UpdateObserver());
+                mocks.ReplayAll();
+
+                resultObserver.Attach(observer);
+                
+                // When
+                failureMechanism.NotifyObservers();
+
+                // Then
+                mocks.VerifyAll();
+            }
+        }
+
+        [Test]
+        public void GivenFailureMechanismResultObserverWithAttachedObserver_WhenFailureMechanismSectionResultCollectionNotifiesObservers_ThenFailureMechanismResultObserverNotified()
+        {
+            // Given
+            var failureMechanism = new TestFailureMechanism();
+
+            using (var resultObserver = new FailureMechanismResultObserver<TestFailureMechanism, FailureMechanismSectionResult>(failureMechanism))
+            {
+                var mocks = new MockRepository();
+                var observer = mocks.StrictMock<IObserver>();
+                observer.Expect(o => o.UpdateObserver());
+                mocks.ReplayAll();
+
+                resultObserver.Attach(observer);
+                
+                // When
+                failureMechanism.SectionResults.NotifyObservers();
+
+                // Then
+                mocks.VerifyAll();
+            }
+        }
+
+        [Test]
+        public void GivenFailureMechanismResultObserverWithAttachedObserver_WhenFailureMechanismSectionResultNotifiesObservers_ThenFailureMechanismResultObserverNotified()
+        {
+            // Given
+            var failureMechanism = new TestFailureMechanism();
+            failureMechanism.AddSection(FailureMechanismSectionTestFactory.CreateFailureMechanismSection());
+
+            using (var resultObserver = new FailureMechanismResultObserver<TestFailureMechanism, FailureMechanismSectionResult>(failureMechanism))
+            {
+                var mocks = new MockRepository();
+                var observer = mocks.StrictMock<IObserver>();
+                observer.Expect(o => o.UpdateObserver());
+                mocks.ReplayAll();
+
+                resultObserver.Attach(observer);
+                
+                // When
+                failureMechanism.SectionResults.Single().NotifyObservers();
+
+                // Then
+                mocks.VerifyAll();
+            }
+        }
+    }
+}
