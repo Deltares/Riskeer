f806fff6a5ffb1ae1267dda990d6c170b55083da
Robert Tangerman <robert.tangerman@deltares.nl>
2018-05-22 13:10:41 +0200

    Created factory for creating assembly result. WTI-1683

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Data/Assembly/CombinedFailureMechanismSectionAssemblyResultFactory.cs
@@ -0,0 +1,172 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Integration.Data.Assembly
+{
+    /// <summary>
+    /// Factory that creates <see cref="CombinedFailureMechanismSectionAssemblyResult"/> instances.
+    /// </summary>
+    internal static class CombinedFailureMechanismSectionAssemblyResultFactory
+    {
+        /// <summary>
+        /// Creates a collection of <see cref="CombinedFailureMechanismSectionAssemblyResult"/> based
+        /// on the given <paramref name="output"/> and <paramref name="failureMechanisms"/>.
+        /// </summary>
+        /// <param name="output">The output to create the results for.</param>
+        /// <param name="failureMechanisms">The failure mechanisms to create the results for.</param>
+        /// <param name="assessmentSection">The assessment section to use while creating the results.</param>
+        /// <returns>A collection of <see cref="CombinedFailureMechanismSectionAssemblyResult"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public static IEnumerable<CombinedFailureMechanismSectionAssemblyResult> Create(IEnumerable<CombinedFailureMechanismSectionAssembly> output,
+                                                                                        IDictionary<IFailureMechanism, int> failureMechanisms,
+                                                                                        AssessmentSection assessmentSection)
+        {
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            if (failureMechanisms == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanisms));
+            }
+
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            return output.Select(assembly => new CombinedFailureMechanismSectionAssemblyResult(
+                                     assembly.Section.SectionStart,
+                                     assembly.Section.SectionEnd,
+                                     assembly.Section.CategoryGroup,
+                                     CreateFailureMechanismResults(assembly.FailureMechanismResults,
+                                                                   failureMechanisms, assessmentSection)))
+                         .ToArray();
+        }
+
+        private static CombinedFailureMechanismSectionAssemblyResult.ConstructionProperties CreateFailureMechanismResults(
+            IEnumerable<FailureMechanismSectionAssemblyCategoryGroup> failureMechanismResults,
+            IDictionary<IFailureMechanism, int> failureMechanisms,
+            AssessmentSection assessmentSection)
+        {
+            var constructionProperties = new CombinedFailureMechanismSectionAssemblyResult.ConstructionProperties();
+
+            if (failureMechanisms.ContainsKey(assessmentSection.Piping))
+            {
+                constructionProperties.Piping = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.Piping]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.GrassCoverErosionInwards))
+            {
+                constructionProperties.GrassCoverErosionInwards = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.GrassCoverErosionInwards]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.MacroStabilityInwards))
+            {
+                constructionProperties.MacroStabilityInwards = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.MacroStabilityInwards]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.MacroStabilityOutwards))
+            {
+                constructionProperties.MacroStabilityOutwards = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.MacroStabilityOutwards]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.Microstability))
+            {
+                constructionProperties.Microstability = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.Microstability]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.StabilityStoneCover))
+            {
+                constructionProperties.StabilityStoneCover = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.StabilityStoneCover]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.WaveImpactAsphaltCover))
+            {
+                constructionProperties.WaveImpactAsphaltCover = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.WaveImpactAsphaltCover]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.WaterPressureAsphaltCover))
+            {
+                constructionProperties.WaterPressureAsphaltCover = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.WaterPressureAsphaltCover]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.GrassCoverErosionOutwards))
+            {
+                constructionProperties.GrassCoverErosionOutwards = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.GrassCoverErosionOutwards]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.GrassCoverSlipOffOutwards))
+            {
+                constructionProperties.GrassCoverSlipOffOutwards = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.GrassCoverSlipOffOutwards]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.GrassCoverSlipOffInwards))
+            {
+                constructionProperties.GrassCoverSlipOffInwards = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.GrassCoverSlipOffInwards]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.HeightStructures))
+            {
+                constructionProperties.HeightStructures = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.HeightStructures]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.ClosingStructures))
+            {
+                constructionProperties.ClosingStructures = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.ClosingStructures]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.PipingStructure))
+            {
+                constructionProperties.PipingStructure = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.PipingStructure]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.StabilityPointStructures))
+            {
+                constructionProperties.StabilityPointStructures = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.StabilityPointStructures]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.StrengthStabilityLengthwiseConstruction))
+            {
+                constructionProperties.StrengthStabilityLengthwise = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.StrengthStabilityLengthwiseConstruction]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.DuneErosion))
+            {
+                constructionProperties.DuneErosion = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.DuneErosion]);
+            }
+
+            if (failureMechanisms.ContainsKey(assessmentSection.TechnicalInnovation))
+            {
+                constructionProperties.TechnicalInnovation = failureMechanismResults.ElementAt(failureMechanisms[assessmentSection.TechnicalInnovation]);
+            }
+
+            return constructionProperties;
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Data.Test/Assembly/CombinedFailureMechanismSectionAssemblyResultFactoryTest.cs
@@ -0,0 +1,196 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Integration.Data.Assembly;
+
+namespace Ringtoets.Integration.Data.Test.Assembly
+{
+    public class CombinedFailureMechanismSectionAssemblyResultFactoryTest
+    {
+        [Test]
+        public void Create_OutputNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+
+            // Call
+            TestDelegate call = () => CombinedFailureMechanismSectionAssemblyResultFactory.Create(null, new Dictionary<IFailureMechanism, int>(),
+                                                                                                  new AssessmentSection(random.NextEnumValue<AssessmentSectionComposition>()));
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("output", exception.ParamName);
+        }
+
+        [Test]
+        public void Create_FailureMechanismsNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+
+            // Call
+            TestDelegate call = () => CombinedFailureMechanismSectionAssemblyResultFactory.Create(Enumerable.Empty<CombinedFailureMechanismSectionAssembly>(),
+                                                                                                  null, new AssessmentSection(random.NextEnumValue<AssessmentSectionComposition>()));
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanisms", exception.ParamName);
+        }
+
+        [Test]
+        public void Create_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => CombinedFailureMechanismSectionAssemblyResultFactory.Create(Enumerable.Empty<CombinedFailureMechanismSectionAssembly>(),
+                                                                                                  new Dictionary<IFailureMechanism, int>(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
+        public void Create_WithAllFailureMechanisms_ReturnsCombinedFailureMechanismSectionAssemblyResults()
+        {
+            // Setup
+            var random = new Random(21);
+            var assessmentSection = new AssessmentSection(random.NextEnumValue<AssessmentSectionComposition>());
+            Dictionary<IFailureMechanism, int> failureMechanisms = assessmentSection.GetFailureMechanisms()
+                                                                                           .Where(fm => fm.IsRelevant)
+                                                                                           .Select((fm, i) => new
+                                                                                           {
+                                                                                               FailureMechanism = fm,
+                                                                                               Index = i
+                                                                                           })
+                                                                                           .ToDictionary(x => x.FailureMechanism, x => x.Index);
+
+            var section1 = new CombinedAssemblyFailureMechanismSection(0, 5, random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+            var section2 = new CombinedAssemblyFailureMechanismSection(5, 11, random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+            var output = new[]
+            {
+                new CombinedFailureMechanismSectionAssembly(section1, GetFailureMechanismsOutput(failureMechanisms.Keys, random)),
+                new CombinedFailureMechanismSectionAssembly(section2, GetFailureMechanismsOutput(failureMechanisms.Keys, random))
+            };
+
+            // Call
+            CombinedFailureMechanismSectionAssemblyResult[] results = CombinedFailureMechanismSectionAssemblyResultFactory.Create(output, failureMechanisms, assessmentSection).ToArray();
+
+            // Assert
+            Assert.AreEqual(output.Length, results.Length);
+            for (var i = 0; i < output.Length; i++)
+            {
+                Assert.AreEqual(output[i].Section.SectionStart, results[i].SectionStart);
+                Assert.AreEqual(output[i].Section.SectionEnd, results[i].SectionEnd);
+                Assert.AreEqual(output[i].Section.CategoryGroup, results[i].TotalResult);
+
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.Piping]), results[i].Piping);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.GrassCoverErosionInwards]), results[i].GrassCoverErosionInwards);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.MacroStabilityInwards]), results[i].MacroStabilityInwards);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.MacroStabilityOutwards]), results[i].MacroStabilityOutwards);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.Microstability]), results[i].Microstability);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.StabilityStoneCover]), results[i].StabilityStoneCover);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.WaveImpactAsphaltCover]), results[i].WaveImpactAsphaltCover);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.WaterPressureAsphaltCover]), results[i].WaterPressureAsphaltCover);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.GrassCoverErosionOutwards]), results[i].GrassCoverErosionOutwards);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.GrassCoverSlipOffOutwards]), results[i].GrassCoverSlipOffOutwards);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.GrassCoverSlipOffInwards]), results[i].GrassCoverSlipOffInwards);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.HeightStructures]), results[i].HeightStructures);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.ClosingStructures]), results[i].ClosingStructures);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.PipingStructure]), results[i].PipingStructure);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.StabilityPointStructures]), results[i].StabilityPointStructures);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.StrengthStabilityLengthwiseConstruction]), results[i].StrengthStabilityLengthwise);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.DuneErosion]), results[i].DuneErosion);
+                Assert.AreEqual(output[i].FailureMechanismResults.ElementAt(failureMechanisms[assessmentSection.TechnicalInnovation]), results[i].TechnicalInnovation);
+            }
+        }
+
+        [Test]
+        public void Create_WithEmptyFailureMechanisms_ReturnsCombinedFailureMechanismSectionAssemblyResults()
+        {
+            // Setup
+            var random = new Random(21);
+            var assessmentSection = new AssessmentSection(random.NextEnumValue<AssessmentSectionComposition>());
+            Dictionary<IFailureMechanism, int> failureMechanisms = assessmentSection.GetFailureMechanisms()
+                                                                                           .Where(fm => fm.IsRelevant)
+                                                                                           .Select((fm, i) => new
+                                                                                           {
+                                                                                               FailureMechanism = fm,
+                                                                                               Index = i
+                                                                                           })
+                                                                                           .ToDictionary(x => x.FailureMechanism, x => x.Index);
+
+            var section1 = new CombinedAssemblyFailureMechanismSection(0, 5, random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+            var section2 = new CombinedAssemblyFailureMechanismSection(5, 11, random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+            var output = new[]
+            {
+                new CombinedFailureMechanismSectionAssembly(section1, GetFailureMechanismsOutput(failureMechanisms.Keys, random)),
+                new CombinedFailureMechanismSectionAssembly(section2, GetFailureMechanismsOutput(failureMechanisms.Keys, random))
+            };
+
+            // Call
+            CombinedFailureMechanismSectionAssemblyResult[] results = CombinedFailureMechanismSectionAssemblyResultFactory.Create(output,
+                                                                                                                                  new Dictionary<IFailureMechanism, int>(),
+                                                                                                                                  assessmentSection).ToArray();
+
+            // Assert
+            Assert.AreEqual(output.Length, results.Length);
+            for (var i = 0; i < output.Length; i++)
+            {
+                Assert.AreEqual(output[i].Section.SectionStart, results[i].SectionStart);
+                Assert.AreEqual(output[i].Section.SectionEnd, results[i].SectionEnd);
+                Assert.AreEqual(output[i].Section.CategoryGroup, results[i].TotalResult);
+
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].Piping);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].GrassCoverErosionInwards);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].MacroStabilityInwards);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].MacroStabilityOutwards);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].Microstability);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].StabilityStoneCover);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].WaveImpactAsphaltCover);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].WaterPressureAsphaltCover);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].GrassCoverErosionOutwards);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].GrassCoverSlipOffOutwards);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].GrassCoverSlipOffInwards);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].HeightStructures);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].ClosingStructures);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].PipingStructure);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].StabilityPointStructures);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].StrengthStabilityLengthwise);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].DuneErosion);
+                Assert.AreEqual(FailureMechanismSectionAssemblyCategoryGroup.None, results[i].TechnicalInnovation);
+            }
+        }
+
+        private static IEnumerable<FailureMechanismSectionAssemblyCategoryGroup> GetFailureMechanismsOutput(IEnumerable<IFailureMechanism> failureMechanisms,
+                                                                                                            Random random)
+        {
+            return failureMechanisms.Select(fm => random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+        }
+    }
+}
