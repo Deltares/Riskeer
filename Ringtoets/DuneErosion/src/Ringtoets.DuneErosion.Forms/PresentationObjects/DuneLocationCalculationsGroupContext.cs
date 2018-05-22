afad5a86d5555f63edc6ee6210ac48951db08c37
Dennis Tang <dennis.tang@deltares.nl>
2018-05-22 12:45:08 +0200

    Introduced DuneLocationsCalculationsGroupContext to represent a treenode folder to group the collections of DuneLocationCalculations in the FailureMechanism.
    WTI-1575

---
+++ Ringtoets/DuneErosion/src/Ringtoets.DuneErosion.Forms/PresentationObjects/DuneLocationCalculationsGroupContext.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base;
+using Core.Common.Controls.PresentationObjects;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.DuneErosion.Data;
+
+namespace Ringtoets.DuneErosion.Forms.PresentationObjects
+{
+    /// <summary>
+    /// Presentation object for all data required to configure multiple enumerations of <see cref="DuneLocation"/> 
+    /// with a dune location calculation result.
+    /// </summary>
+    public class DuneLocationCalculationsGroupContext : ObservableWrappedObjectContextBase<IObservableEnumerable<DuneLocation>>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="DuneLocationCalculationsGroupContext"/>.
+        /// </summary>
+        /// <param name="wrappedData">The locations the context belongs to.</param>
+        /// <param name="failureMechanism">The failure mechanism the context belongs to.</param>
+        /// <param name="assessmentSection">The <see cref="IAssessmentSection"/> the context belongs to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public DuneLocationCalculationsGroupContext(IObservableEnumerable<DuneLocation> wrappedData,
+                                                    DuneErosionFailureMechanism failureMechanism,
+                                                    IAssessmentSection assessmentSection)
+            : base(wrappedData)
+        {
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            FailureMechanism = failureMechanism;
+            AssessmentSection = assessmentSection;
+        }
+
+        /// <summary>
+        /// Gets the failure mechanism the context belongs to.
+        /// </summary>
+        public DuneErosionFailureMechanism FailureMechanism { get; }
+
+        /// <summary>
+        /// Gets the assessment section the context belongs to.
+        /// </summary>
+        public IAssessmentSection AssessmentSection { get; }
+    }
+}
---
+++ Ringtoets/DuneErosion/test/Ringtoets.DuneErosion.Forms.Test/PresentationObjects/DuneLocationCalculationsGroupContextTest.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base;
+using Core.Common.Controls.PresentationObjects;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.DuneErosion.Data;
+using Ringtoets.DuneErosion.Forms.PresentationObjects;
+
+namespace Ringtoets.DuneErosion.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class DuneLocationCalculationsGroupContextTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mockRepository = new MockRepository();
+            var assessmentSection = mockRepository.Stub<IAssessmentSection>();
+            mockRepository.ReplayAll();
+
+            var locations = new ObservableList<DuneLocation>();
+            var failureMechanism = new DuneErosionFailureMechanism();
+
+            // Call
+            var context = new DuneLocationCalculationsGroupContext(locations, failureMechanism, assessmentSection);
+
+            // Assert
+            Assert.IsInstanceOf<ObservableWrappedObjectContextBase<IObservableEnumerable<DuneLocation>>>(context);
+            Assert.AreSame(locations, context.WrappedData);
+            Assert.AreSame(failureMechanism, context.FailureMechanism);
+            Assert.AreSame(assessmentSection, context.AssessmentSection);
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void Constructor_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mockRepository = new MockRepository();
+            var assessmentSection = mockRepository.Stub<IAssessmentSection>();
+            mockRepository.ReplayAll();
+
+            // Call
+            TestDelegate call = () => new DuneLocationCalculationsGroupContext(new ObservableList<DuneLocation>(), null, assessmentSection);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanism", exception.ParamName);
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void Constructor_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new DuneLocationCalculationsGroupContext(new ObservableList<DuneLocation>(), new DuneErosionFailureMechanism(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+    }
+}
