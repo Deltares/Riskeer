9fdfceb490ba7e2aecd187374559dceaef0820bb
Robert Tangerman <robert.tangerman@deltares.nl>
2018-05-28 09:11:31 +0200

    Created base exporter for wave conditions calculation configuration. WTI-1705

---
+++ Ringtoets/Revetment/src/Ringtoets.Revetment.IO/Configurations/WaveConditionsCalculationConfigurationExporter.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Common.IO.Configurations.Export;
+using Ringtoets.Revetment.Data;
+
+namespace Ringtoets.Revetment.IO.Configurations
+{
+    /// <summary>
+    /// Base implementation of exporter to export a wave conditions calculation configuration and stores it as an XML file.
+    /// </summary>
+    /// <typeparam name="TWaveConditionsCalculationConfigurationWriter">The type of writer to use.</typeparam>
+    /// <typeparam name="TWaveConditionsCalculationConfiguration">The type of calculation configuration to export.</typeparam>
+    /// <typeparam name="TCalculation">The type of calculation to use.</typeparam>
+    public abstract class WaveConditionsCalculationConfigurationExporter<TWaveConditionsCalculationConfigurationWriter, TWaveConditionsCalculationConfiguration, TCalculation>
+        : CalculationConfigurationExporter<TWaveConditionsCalculationConfigurationWriter, TCalculation, TWaveConditionsCalculationConfiguration>
+        where TWaveConditionsCalculationConfigurationWriter : CalculationConfigurationWriter<TWaveConditionsCalculationConfiguration>
+        where TWaveConditionsCalculationConfiguration : WaveConditionsCalculationConfiguration
+        where TCalculation : class, ICalculation<WaveConditionsInput>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="WaveConditionsCalculationConfigurationExporter{TWaveConditionsCalculationConfigurationWriter,TWaveConditionsCalculationConfiguration,TCalculation}"/>.
+        /// </summary>
+        /// <param name="calculations">The hierarchy of calculations to export.</param>
+        /// <param name="filePath">The path of the XML file to export to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="calculations"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="filePath"/> is invalid.</exception>
+        protected WaveConditionsCalculationConfigurationExporter(IEnumerable<ICalculationBase> calculations, string filePath)
+            : base(calculations, filePath) {}
+
+        protected abstract override TWaveConditionsCalculationConfigurationWriter CreateWriter(string filePath);
+
+        protected abstract override TWaveConditionsCalculationConfiguration ToConfiguration(TCalculation calculation);
+    }
+}
---
+++ Ringtoets/Revetment/test/Ringtoets.Revetment.IO.Test/Configurations/WaveConditionsCalculationConfigurationExporterTest.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Common.IO.TestUtil;
+using Ringtoets.Revetment.Data;
+using Ringtoets.Revetment.Data.TestUtil;
+using Ringtoets.Revetment.IO.Configurations;
+
+namespace Ringtoets.Revetment.IO.Test.Configurations
+{
+    [TestFixture]
+    public class WaveConditionsCalculationConfigurationExporterTest : CustomCalculationConfigurationExporterDesignGuidelinesTestFixture<
+        WaveConditionsCalculationConfigurationExporter<WaveConditionsCalculationConfigurationWriter<WaveConditionsCalculationConfiguration>, WaveConditionsCalculationConfiguration, ICalculation<WaveConditionsInput>>,
+        WaveConditionsCalculationConfigurationWriter<WaveConditionsCalculationConfiguration>,
+        ICalculation<WaveConditionsInput>,
+        WaveConditionsCalculationConfiguration>
+    {
+        protected override ICalculation<WaveConditionsInput> CreateCalculation()
+        {
+            return new TestWaveConditionsCalculation<WaveConditionsInput>(new TestWaveConditionsInput());
+        }
+
+        protected override WaveConditionsCalculationConfigurationExporter<WaveConditionsCalculationConfigurationWriter<WaveConditionsCalculationConfiguration>, WaveConditionsCalculationConfiguration, ICalculation<WaveConditionsInput>> CallConfigurationFilePathConstructor(IEnumerable<ICalculationBase> calculations, string filePath)
+        {
+            return new TestWaveConditionsCalculationConfigurationExporter(calculations, filePath);
+        }
+
+        private class TestWaveConditionsCalculationConfigurationExporter : WaveConditionsCalculationConfigurationExporter<WaveConditionsCalculationConfigurationWriter<WaveConditionsCalculationConfiguration>, WaveConditionsCalculationConfiguration, ICalculation<WaveConditionsInput>>
+        {
+            public TestWaveConditionsCalculationConfigurationExporter(IEnumerable<ICalculationBase> calculations, string filePath) 
+                : base(calculations, filePath) {}
+
+            protected override WaveConditionsCalculationConfigurationWriter<WaveConditionsCalculationConfiguration> CreateWriter(string filePath)
+            {
+                return new WaveConditionsCalculationConfigurationWriter<WaveConditionsCalculationConfiguration>(filePath);
+            }
+
+            protected override WaveConditionsCalculationConfiguration ToConfiguration(ICalculation<WaveConditionsInput> calculation)
+            {
+                return new WaveConditionsCalculationConfiguration("test");
+            }
+        }
+    }
+}
