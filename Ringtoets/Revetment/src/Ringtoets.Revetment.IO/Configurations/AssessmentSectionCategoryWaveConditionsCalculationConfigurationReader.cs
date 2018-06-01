37595f31d0b37d1f6c93cf927f5eff067e992b33
Robert Tangerman <robert.tangerman@deltares.nl>
2018-06-01 09:11:05 +0200

    Introduced specific reader for assessment section category wave conditions calculation configurations. WTI-1705

---
+++ Ringtoets/Revetment/src/Ringtoets.Revetment.IO/Configurations/AssessmentSectionCategoryWaveConditionsCalculationConfigurationReader.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.IO;
+using Ringtoets.Common.IO.Configurations;
+
+namespace Ringtoets.Revetment.IO.Configurations
+{
+    /// <summary>
+    /// This class reads a wave conditions calculation configuration from XML and creates a collection of corresponding
+    /// <see cref="IConfigurationItem"/>, typically containing one or more <see cref="AssessmentSectionCategoryWaveConditionsCalculationConfiguration"/>.
+    /// </summary>
+    public class AssessmentSectionCategoryWaveConditionsCalculationConfigurationReader : WaveConditionsCalculationConfigurationReader
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="AssessmentSectionCategoryWaveConditionsCalculationConfigurationReader"/>.
+        /// </summary>
+        /// <param name="xmlFilePath">The file path to the XML file.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="xmlFilePath"/> is invalid.</exception>
+        /// <exception cref="CriticalFileReadException">Thrown when:
+        /// <list type="bullet">
+        /// <item><paramref name="xmlFilePath"/> points to a file that does not exist.</item>
+        /// <item><paramref name="xmlFilePath"/> points to a file that does not contain valid XML.</item>
+        /// <item><paramref name="xmlFilePath"/> points to a file that does not pass the schema validation.</item>
+        /// <item><paramref name="xmlFilePath"/> points to a file that does not contain configuration elements.</item>
+        /// </list>
+        /// </exception>
+        public AssessmentSectionCategoryWaveConditionsCalculationConfigurationReader(string xmlFilePath)
+            : base(xmlFilePath) {}
+    }
+};
