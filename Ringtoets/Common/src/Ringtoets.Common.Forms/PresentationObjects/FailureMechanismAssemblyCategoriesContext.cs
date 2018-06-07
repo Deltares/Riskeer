c12db10749246f8e848dd63293eb46311924c7c9
Dennis Tang <dennis.tang@deltares.nl>
2018-06-07 14:38:51 +0200

    Introduced context object for displaying assembly categories on failure mechanism level
    WTI-1540

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/PresentationObjects/FailureMechanismAssemblyCategoriesContext.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Controls.PresentationObjects;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Common.Forms.PresentationObjects
+{
+    /// <summary>
+    /// This class is a presentation object for failure mechanism category boundaries for a <see cref="IFailureMechanism"/> instance.
+    /// </summary>
+    public class FailureMechanismAssemblyCategoriesContext : ObservableWrappedObjectContextBase<IFailureMechanism>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="FailureMechanismAssemblyCategoriesContext"/>.
+        /// </summary>
+        /// <param name="wrappedData">The failure mechanism to wrap.</param>
+        /// <param name="assessmentSection">The assessment section the <paramref name="wrappedData"/> belongs to.</param>
+        /// <param name="getNFunc">The function to get the 'N' parameter used to factor in the 'length effect'.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any input parameter is <c>null</c>.</exception>
+        public FailureMechanismAssemblyCategoriesContext(IFailureMechanism wrappedData,
+                                                         IAssessmentSection assessmentSection,
+                                                         Func<double> getNFunc)
+            : base(wrappedData)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            if (getNFunc == null)
+            {
+                throw new ArgumentNullException(nameof(getNFunc));
+            }
+
+            AssessmentSection = assessmentSection;
+            GetNFunc = getNFunc;
+        }
+
+        /// <summary>
+        /// Gets the assessment section the <see cref="WrappedObjectContextBase{T}.WrappedData"/> belongs to.
+        /// </summary>
+        public IAssessmentSection AssessmentSection { get; }
+
+        /// <summary>
+        /// Gets the function to get the 'N' parameter used to factor in the 'length effect'.
+        /// </summary>
+        public Func<double> GetNFunc { get; }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.Test/PresentationObjects/FailureMechanismAssemblyCategoriesContextTest.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Controls.PresentationObjects;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Forms.PresentationObjects;
+
+namespace Ringtoets.Common.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class FailureMechanismAssemblyCategoriesContextTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            var failureMechanism = mocks.Stub<IFailureMechanism>();
+            mocks.ReplayAll();
+
+            var random = new Random(21);
+            Func<double> getNFunc = () => random.NextDouble();
+
+            // Call
+            var context = new FailureMechanismAssemblyCategoriesContext(failureMechanism,
+                                                                        assessmentSection,
+                                                                        getNFunc);
+
+            // Assert
+            Assert.IsInstanceOf<ObservableWrappedObjectContextBase<IFailureMechanism>>(context);
+            Assert.AreSame(failureMechanism, context.WrappedData);
+            Assert.AreSame(assessmentSection, context.AssessmentSection);
+            Assert.AreSame(getNFunc, context.GetNFunc);
+
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Constructor_AssessementSectionNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var failureMechanism = mocks.Stub<IFailureMechanism>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => new FailureMechanismAssemblyCategoriesContext(failureMechanism,
+                                                                                    null,
+                                                                                    () => 0);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Constructor_GetNFuncNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            var failureMechanism = mocks.Stub<IFailureMechanism>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => new FailureMechanismAssemblyCategoriesContext(failureMechanism,
+                                                                                    assessmentSection,
+                                                                                    null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("getNFunc", exception.ParamName);
+        }
+    }
+}
