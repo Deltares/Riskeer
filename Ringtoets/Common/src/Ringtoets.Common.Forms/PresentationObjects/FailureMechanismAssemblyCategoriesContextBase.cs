bed9a78467c584c41edfdeadd4dce27b9e23d1ae
Dennis Tang <dennis.tang@deltares.nl>
2018-06-11 13:57:32 +0200

    Incorporated comments review 2940 - introduce 3 context objects: - Added context base
    WTI-1540

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/PresentationObjects/FailureMechanismAssemblyCategoriesContextBase.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Core.Common.Controls.PresentationObjects;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Common.Data.AssemblyTool;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Data.Contribution;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Common.Forms.PresentationObjects
+{
+    /// <summary>
+    /// This class is a presentation object for failure mechanism category boundaries for a <see cref="IFailureMechanism"/> instance.
+    /// </summary>
+    public abstract class FailureMechanismAssemblyCategoriesContextBase : ObservableWrappedObjectContextBase<IFailureMechanism>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="FailureMechanismAssemblyCategoriesContext"/>.
+        /// </summary>
+        /// <param name="wrappedData">The failure mechanism to wrap.</param>
+        /// <param name="assessmentSection">The assessment section the <paramref name="wrappedData"/> belongs to.</param>
+        /// <param name="getNFunc">The function to get the 'N' parameter used to factor in the 'length effect'.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any input parameter is <c>null</c>.</exception>
+        protected FailureMechanismAssemblyCategoriesContextBase(IFailureMechanism wrappedData,
+                                                                IAssessmentSection assessmentSection,
+                                                                Func<double> getNFunc)
+            : base(wrappedData)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            if (getNFunc == null)
+            {
+                throw new ArgumentNullException(nameof(getNFunc));
+            }
+
+            AssessmentSection = assessmentSection;
+            FailureMechanismContribution failureMechanismContribution = AssessmentSection.FailureMechanismContribution;
+            GetFailureMechanismCategoriesFunc = () => AssemblyToolCategoriesFactory.CreateFailureMechanismAssemblyCategories(failureMechanismContribution.SignalingNorm,
+                                                                                                                             failureMechanismContribution.LowerLimitNorm,
+                                                                                                                             wrappedData.Contribution,
+                                                                                                                             getNFunc());
+        }
+
+        /// <summary>
+        /// Gets the assessment section that the context belongs to.
+        /// </summary>
+        public IAssessmentSection AssessmentSection { get; }
+
+        /// <summary>
+        /// Gets the function to retrieve a collection of <see cref="FailureMechanismAssemblyCategory"/>.
+        /// </summary>
+        public Func<IEnumerable<FailureMechanismAssemblyCategory>> GetFailureMechanismCategoriesFunc { get; }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.Test/PresentationObjects/FailureMechanismAssemblyCategoriesContextBaseTest.cs
@@ -0,0 +1,141 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Controls.PresentationObjects;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.AssemblyTool.KernelWrapper.Calculators;
+using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators;
+using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators.Categories;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Data.Contribution;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Common.Forms.PresentationObjects;
+
+namespace Ringtoets.Common.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class FailureMechanismAssemblyCategoriesContextBaseTest
+    {
+        private const double tolerance = 1e-5;
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            double n = random.NextDouble();
+
+            var mocks = new MockRepository();
+            var failureMechanism = mocks.Stub<IFailureMechanism>();
+            failureMechanism.Contribution = random.NextDouble();
+            mocks.ReplayAll();
+
+            var assessmentSection = new AssessmentSectionStub();
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                var calculatorFactory = (TestAssemblyToolCalculatorFactory) AssemblyToolCalculatorFactory.Instance;
+                AssemblyCategoriesCalculatorStub calculator = calculatorFactory.LastCreatedAssemblyCategoriesCalculator;
+
+                // Call
+                var context = new TestFailureMechanismAssemblyCategoriesContextBase(failureMechanism,
+                                                                                    assessmentSection,
+                                                                                    () => n);
+
+                // Assert
+                Assert.IsInstanceOf<ObservableWrappedObjectContextBase<IFailureMechanism>>(context);
+                Assert.AreSame(failureMechanism, context.WrappedData);
+                Assert.AreSame(assessmentSection, context.AssessmentSection);
+
+                IEnumerable<FailureMechanismAssemblyCategory> output = context.GetFailureMechanismCategoriesFunc();
+                IEnumerable<FailureMechanismAssemblyCategory> calculatorOutput = calculator.FailureMechanismCategoriesOutput;
+                Assert.AreEqual(calculatorOutput.Count(), output.Count());
+                CollectionAssert.AreEqual(calculatorOutput.Select(co => co.LowerBoundary), output.Select(o => o.LowerBoundary));
+                CollectionAssert.AreEqual(calculatorOutput.Select(co => co.UpperBoundary), output.Select(o => o.UpperBoundary));
+                CollectionAssert.AreEqual(calculatorOutput.Select(co => co.Group), output.Select(o => o.Group));
+
+                AssemblyCategoriesInput actualCalculatorInput = calculator.AssemblyCategoriesInput;
+                FailureMechanismContribution expectedContribution = assessmentSection.FailureMechanismContribution;
+                Assert.AreEqual(failureMechanism.Contribution / 100, actualCalculatorInput.FailureMechanismContribution, tolerance);
+                Assert.AreEqual(expectedContribution.LowerLimitNorm, actualCalculatorInput.LowerLimitNorm, tolerance);
+                Assert.AreEqual(expectedContribution.SignalingNorm, actualCalculatorInput.SignalingNorm, tolerance);
+                Assert.AreEqual(n, actualCalculatorInput.N);
+            }
+
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Constructor_AssessementSectionNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var failureMechanism = mocks.Stub<IFailureMechanism>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => new TestFailureMechanismAssemblyCategoriesContextBase(failureMechanism,
+                                                                                            null,
+                                                                                            () => 0);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Constructor_GetNFuncNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            var failureMechanism = mocks.Stub<IFailureMechanism>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => new FailureMechanismAssemblyCategoriesContext(failureMechanism,
+                                                                                    assessmentSection,
+                                                                                    null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("getNFunc", exception.ParamName);
+
+            mocks.VerifyAll();
+        }
+
+        private class TestFailureMechanismAssemblyCategoriesContextBase : FailureMechanismAssemblyCategoriesContextBase
+        {
+            public TestFailureMechanismAssemblyCategoriesContextBase(IFailureMechanism wrappedData,
+                                                                     IAssessmentSection assessmentSection,
+                                                                     Func<double> getNFunc)
+                : base(wrappedData, assessmentSection, getNFunc) {}
+        }
+    }
+}
