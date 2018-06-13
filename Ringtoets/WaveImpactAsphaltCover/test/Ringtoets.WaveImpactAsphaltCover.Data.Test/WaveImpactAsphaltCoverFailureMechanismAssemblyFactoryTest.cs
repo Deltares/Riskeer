6d8e05710b2c63e1f1f62ff3fd1b73346618286f
Dennis Tang <dennis.tang@deltares.nl>
2018-06-13 13:29:35 +0200

    Incorporated comments review 2946: - Adjusted remaining testnames for failuremechanism section results making use of SimpleAssessmentResultType
    WTI-1723

--- Ringtoets/WaveImpactAsphaltCover/test/Ringtoets.WaveImpactAsphaltCover.Data.Test/WaveImpactAsphaltCoverFailureMechanismAssemblyFactoryTest.cs
+++ Ringtoets/WaveImpactAsphaltCover/test/Ringtoets.WaveImpactAsphaltCover.Data.Test/WaveImpactAsphaltCoverFailureMechanismAssemblyFactoryTest.cs
@@ -316,7 +316,7 @@ namespace Ringtoets.WaveImpactAsphaltCover.Data.Test
         [Test]
         [TestCase(SimpleAssessmentResultType.None)]
         [TestCase(SimpleAssessmentResultType.AssessFurther)]
-        public void AssembleCombinedAssessment_WithInputSimpleAssessmentNoneOrAssessFurther_SetsInputOnCalculator(
+        public void AssembleCombinedAssessment_WithVariousSimpleAssessmentInputAssemblesWithAllInformation_SetsInputOnCalculator(
             SimpleAssessmentResultType simpleAssessmentResult)
         {
             // Setup
@@ -350,7 +350,7 @@ namespace Ringtoets.WaveImpactAsphaltCover.Data.Test
         [Test]
         [TestCase(SimpleAssessmentResultType.NotApplicable)]
         [TestCase(SimpleAssessmentResultType.ProbabilityNegligible)]
-        public void AssembleCombinedAssessment_WithInputSimpleAssessmentNotApplicableOrProbabilityNegligible_SetsInputOnCalculator(
+        public void AssembleCombinedAssessment_WithVariousSimpleAssessmentInputAssemblesWithOnlySimpleAssessmentInput_SetsInputOnCalculator(
             SimpleAssessmentResultType simpleAssessmentResult)
         {
             // Setup
