e00fdc8d1be50a612496b6005f690b64849b933b
Dennis Tang <dennis.tang@deltares.nl>
2018-06-29 10:54:08 +0200

    Moved Migration.Core to Ringtoets namespace. WTI-1760

---
+++ Ringtoets/Migration/src/Ringtoets.Migration.Core/RingtoetsDatabaseFile.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Data.SQLite;
+using Core.Common.IO;
+using Core.Common.Util;
+
+namespace Application.Ringtoets.Migration.Core
+{
+    /// <summary>
+    /// Class that provides methods for the migration of a Ringtoets database target file.
+    /// </summary>
+    public class RingtoetsDatabaseFile : IDisposable
+    {
+        private readonly string filePath;
+        private SQLiteConnection connection;
+        private bool disposed;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="RingtoetsDatabaseFile"/> class.
+        /// </summary>
+        /// <param name="path">The path to the target file.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="path"/>:
+        /// <list type="bullet">
+        /// <item>is not empty or <c>null</c>,</item>
+        /// <item>does not consist out of only whitespace characters,</item>
+        /// <item>does not contain an invalid character,</item>
+        /// <item>does not end with a directory or path separator (empty file name),</item>
+        /// <item>is not writable.</item>
+        /// </list></exception>
+        /// <remarks>Creates the file if it does not exist.</remarks>
+        public RingtoetsDatabaseFile(string path)
+        {
+            IOUtils.CreateFileIfNotExists(path);
+            filePath = path;
+        }
+
+        /// <summary>
+        /// Opens the connection to the Ringtoets database file.
+        /// </summary>
+        public void OpenDatabaseConnection()
+        {
+            connection = new SQLiteConnection(SqLiteConnectionStringBuilder.BuildSqLiteConnectionString(filePath, false));
+            connection.Open();
+        }
+
+        /// <summary>
+        /// Executes the <paramref name="query"/> on the Ringtoets database file.
+        /// </summary>
+        /// <param name="query">Create structure query to execute.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="query"/> is <c>null</c> 
+        /// or consist out of only whitespace characters.</exception>
+        /// <exception cref="SQLiteException">Thrown when executing <paramref name="query"/> failed.</exception>
+        public void ExecuteQuery(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                throw new ArgumentException(@"Parameter must be a valid database script.", nameof(query));
+            }
+            using (var command = new SQLiteCommand(connection)
+            {
+                CommandText = query
+            })
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                connection?.Dispose();
+                connection = null;
+            }
+            disposed = true;
+        }
+    }
+}
---
+++ Ringtoets/Migration/src/Ringtoets.Migration.Core/RingtoetsDatabaseSourceFile.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Data;
+using Application.Ringtoets.Migration.Core.Properties;
+using Core.Common.Base.IO;
+using Core.Common.Base.Storage;
+using Core.Common.IO.Readers;
+
+namespace Application.Ringtoets.Migration.Core
+{
+    /// <summary>
+    /// Class that provides methods for the migration database source file.
+    /// </summary>
+    public class RingtoetsDatabaseSourceFile : SqLiteDatabaseReaderBase
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="RingtoetsDatabaseSourceFile"/> class.
+        /// </summary>
+        /// <param name="databaseFilePath">The path to the source file.</param>
+        /// <exception cref="CriticalFileReadException">Thrown when:
+        /// <list type="bullet">
+        /// <item>The <paramref name="databaseFilePath"/> contains invalid characters.</item>
+        /// <item>No file could be found at <paramref name="databaseFilePath"/>.</item>
+        /// <item>Unable to open database file.</item>
+        /// </list>
+        /// </exception>
+        public RingtoetsDatabaseSourceFile(string databaseFilePath) : base(databaseFilePath) {}
+
+        /// <summary>
+        /// Gets the version of the Ringtoets database source file at location <see cref="SqLiteDatabaseReaderBase.Path"/>.
+        /// </summary>
+        /// <returns>The version of the file.</returns>
+        /// <exception cref="StorageValidationException">Thrown when the file is not a valid 
+        /// Ringtoets database source file.</exception>
+        public string GetVersion()
+        {
+            string versionQuery = RingtoetsDatabaseQueryBuilder.GetVersionQuery();
+            try
+            {
+                using (IDataReader dataReader = CreateDataReader(versionQuery, null))
+                {
+                    if (!dataReader.Read())
+                    {
+                        return string.Empty;
+                    }
+
+                    return dataReader.Read<string>(VersionTableDefinitions.Version);
+                }
+            }
+            catch (SystemException exception)
+            {
+                throw new StorageValidationException(string.Format(Resources.RingtoetsDatabaseSourceFile_Invalid_Ringtoets_File_Path_0,
+                                                                   Path), exception);
+            }
+        }
+    }
+}
