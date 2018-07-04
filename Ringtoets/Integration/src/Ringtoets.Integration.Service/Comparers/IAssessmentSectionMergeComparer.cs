66626f5c114e03e5aad4ec0e4e1400dd43608fb6
Dennis Tang <dennis.tang@deltares.nl>
2018-07-04 12:47:20 +0200

    Introduced interface for the AssessmentSectionMergeComparer.
    WTI-1744

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Service/Comparers/IAssessmentSectionMergeComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Ringtoets.Common.Data.AssessmentSection;
+
+namespace Ringtoets.Integration.Service.Comparers
+{
+    /// <summary>
+    /// Specifies the interface for defining classes that can be used to compare assessment sections
+    /// which can then be used for merging operations.
+    /// </summary>
+    public interface IAssessmentSectionMergeComparer
+    {
+        /// <summary>
+        /// Compares <see cref="IAssessmentSection"/> and determines whether they are equal and thus
+        /// suitable for merge operations.
+        /// </summary>
+        /// <param name="referenceAssessmentSection">The <see cref="IAssessmentSection"/> to compare against.</param>
+        /// <param name="assessmentSectionToCompare">The <see cref="IAssessmentSection"/> to compare.</param>
+        /// <returns><c>true</c> when <paramref name="assessmentSectionToCompare"/> is equal to
+        /// <paramref name="assessmentSectionToCompare"/> and suitable to merge, <c>false</c> if otherwise.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        bool Compare(IAssessmentSection referenceAssessmentSection, IAssessmentSection assessmentSectionToCompare);
+    }
+}
