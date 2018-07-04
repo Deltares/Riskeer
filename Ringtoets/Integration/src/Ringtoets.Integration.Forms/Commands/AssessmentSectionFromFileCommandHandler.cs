55bc38bfa86e54bbd5ee3c9abaa1117e4d918b03
Robert Tangerman <robert.tangerman@deltares.nl>
2018-07-04 13:15:20 +0200

    Moved dialogs to folder dialogs. WTI-1750

--- Ringtoets/Integration/src/Ringtoets.Integration.Forms/Commands/AssessmentSectionFromFileCommandHandler.cs
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Commands/AssessmentSectionFromFileCommandHandler.cs
@@ -37,6 +37,7 @@ using Ringtoets.Common.IO;
 using Ringtoets.Common.IO.Exceptions;
 using Ringtoets.Common.IO.ReferenceLines;
 using Ringtoets.Integration.Data;
+using Ringtoets.Integration.Forms.Dialogs;
 using Ringtoets.Integration.Forms.Properties;
 using IntegrationResources = Ringtoets.Integration.Data.Properties.Resources;
 using BaseResources = Core.Common.Base.Properties.Resources;
