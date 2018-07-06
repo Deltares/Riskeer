3c6f16091f1dfbf8c521fc0d1e49e2941f46fd49
Dennis Tang <dennis.tang@deltares.nl>
2018-07-06 14:01:09 +0200

    Introduced an AssessmentSectionsOwner to hold the collection of AssessmentSections when running an activity to retrieve them. WTI-1761

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Data/Merge/AssessmentSectionsOwner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Ringtoets.Integration.Data.Merge
+{
+    /// <summary>
+    /// Class that holds a collection of <see cref="AssessmentSection"/>. 
+    /// </summary>
+    public class AssessmentSectionsOwner
+    {
+        /// <summary>
+        /// Gets or sets the collection of <see cref="AssessmentSection"/>.
+        /// </summary>
+        public IEnumerable<AssessmentSection> AssessmentSection { get; set; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Data.Test/Merge/AssessmentSectionsOwnerTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Ringtoets.Integration.Data.Merge;
+
+namespace Ringtoets.Integration.Data.Test.Merge
+{
+    [TestFixture]
+    public class AssessmentSectionsOwnerTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var owner = new AssessmentSectionsOwner();
+
+            // Assert
+            Assert.IsNull(owner.AssessmentSection);
+        }
+    }
+}
