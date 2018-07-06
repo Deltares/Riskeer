a11b9ebc3155cc78af19e03b867d4d88c076d83b
Dennis Tang <dennis.tang@deltares.nl>
2018-07-06 13:06:02 +0200

    Added custom exception to wrap around the StorageException when it occurs in the provider service. WTI-1761

---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Service.Test/Exceptions/AssessmentSectionProviderExceptionTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Integration.Service.Exceptions;
+
+namespace Ringtoets.Integration.Service.Test.Exceptions
+{
+    [TestFixture]
+    public class AssessmentSectionProviderExceptionTest :
+        CustomExceptionDesignGuidelinesTestFixture<AssessmentSectionProviderException, Exception> {}
+}
