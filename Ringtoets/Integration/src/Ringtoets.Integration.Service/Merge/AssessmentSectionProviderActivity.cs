e43447e29761f3bb845f13fc3418d9fb856ab323
Dennis Tang <dennis.tang@deltares.nl>
2018-07-06 13:48:48 +0200

    Added initial setup for the provider activity. WTI-1761

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Service/Merge/AssessmentSectionProviderActivity.cs
@@ -0,0 +1,29 @@
+﻿using Core.Common.Base.Service;
+
+namespace Ringtoets.Integration.Service.Merge
+{
+    public class AssessmentSectionProviderActivity : Activity
+    {
+        private readonly IAssessmentSectionProvider assessmentSectionProvider;
+
+        public AssessmentSectionProviderActivity(IAssessmentSectionProvider assessmentSectionProvider)
+        {
+            this.assessmentSectionProvider = assessmentSectionProvider;
+        }
+
+        protected override void OnRun()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnCancel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnFinish()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Service.Test/Merge/AssessmentSectionProviderActivityTest.cs
@@ -0,0 +1,26 @@
+﻿using Core.Common.Base.Service;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Integration.Service.Merge;
+
+namespace Ringtoets.Integration.Service.Test.Merge
+{
+    [TestFixture]
+    public class AssessmentSectionProviderActivityTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var provider = mocks.Stub<IAssessmentSectionProvider>();
+            mocks.ReplayAll();
+
+            // Call
+            var activity = new AssessmentSectionProviderActivity(provider);
+
+            // Assert
+            Assert.IsInstanceOf<Activity>(activity);
+        }
+    }
+}
