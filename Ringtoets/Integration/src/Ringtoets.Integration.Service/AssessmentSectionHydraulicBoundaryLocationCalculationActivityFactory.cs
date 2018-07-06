5cb386767f4e8fcfee8737bcd44126664d203144
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-07-06 14:00:11 +0200

    Introduce factory for creating hbl calculations for all category boundaries WTI-1733

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Service/AssessmentSectionHydraulicBoundaryLocationCalculationActivityFactory.cs
@@ -0,0 +1,122 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Service;
+using RingtoetsCommonDataResources = Ringtoets.Common.Data.Properties.Resources;
+
+namespace Ringtoets.Integration.Service
+{
+    /// <summary>
+    /// This class defines factory methods that can be used to create instances of <see cref="CalculatableActivity"/> for
+    /// hydraulic boundary location calculations on assessment section level.
+    /// </summary>
+    public static class AssessmentSectionHydraulicBoundaryLocationCalculationActivityFactory
+    {
+        /// <summary>
+        /// Creates a collection of <see cref="CalculatableActivity"/> for wave height calculations
+        /// based on the given <see cref="IAssessmentSection"/>.
+        /// </summary>
+        /// <param name="assessmentSection">The assessment section to create the activities for.</param>
+        /// <returns>A collection of <see cref="CalculatableActivity"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="assessmentSection"/> is <c>null</c>.</exception>
+        public static IEnumerable<CalculatableActivity> CreateWaveHeightCalculationActivities(IAssessmentSection assessmentSection)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            var activities = new List<CalculatableActivity>();
+
+            activities.AddRange(HydraulicBoundaryLocationCalculationActivityFactory.CreateWaveHeightCalculationActivities(
+                                    assessmentSection,
+                                    assessmentSection.WaveHeightCalculationsForFactorizedSignalingNorm,
+                                    assessmentSection.GetNorm(AssessmentSectionCategoryType.FactorizedSignalingNorm),
+                                    RingtoetsCommonDataResources.AssessmentSectionCategoryType_FactorizedSignalingNorm_DisplayName));
+
+            activities.AddRange(HydraulicBoundaryLocationCalculationActivityFactory.CreateWaveHeightCalculationActivities(
+                                    assessmentSection,
+                                    assessmentSection.WaveHeightCalculationsForSignalingNorm,
+                                    assessmentSection.GetNorm(AssessmentSectionCategoryType.SignalingNorm),
+                                    RingtoetsCommonDataResources.AssessmentSectionCategoryType_SignalingNorm_DisplayName));
+
+            activities.AddRange(HydraulicBoundaryLocationCalculationActivityFactory.CreateWaveHeightCalculationActivities(
+                                    assessmentSection,
+                                    assessmentSection.WaveHeightCalculationsForLowerLimitNorm,
+                                    assessmentSection.GetNorm(AssessmentSectionCategoryType.LowerLimitNorm),
+                                    RingtoetsCommonDataResources.AssessmentSectionCategoryType_LowerLimitNorm_DisplayName));
+
+            activities.AddRange(HydraulicBoundaryLocationCalculationActivityFactory.CreateWaveHeightCalculationActivities(
+                                    assessmentSection,
+                                    assessmentSection.WaveHeightCalculationsForFactorizedLowerLimitNorm,
+                                    assessmentSection.GetNorm(AssessmentSectionCategoryType.FactorizedLowerLimitNorm),
+                                    RingtoetsCommonDataResources.AssessmentSectionCategoryType_FactorizedLowerLimitNorm_DisplayName));
+
+            return activities;
+        }
+
+        /// <summary>
+        /// Creates a collection of <see cref="CalculatableActivity"/> for wave height calculations
+        /// based on the given <see cref="IAssessmentSection"/>.
+        /// </summary>
+        /// <param name="assessmentSection">The assessment section to create the activities for.</param>
+        /// <returns>A collection of <see cref="CalculatableActivity"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="assessmentSection"/> is <c>null</c>.</exception>
+        public static IEnumerable<CalculatableActivity> CreateDesignWaterLevelCalculationActivities(IAssessmentSection assessmentSection)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            var activities = new List<CalculatableActivity>();
+
+            activities.AddRange(HydraulicBoundaryLocationCalculationActivityFactory.CreateDesignWaterLevelCalculationActivities(
+                                    assessmentSection,
+                                    assessmentSection.WaterLevelCalculationsForFactorizedSignalingNorm,
+                                    assessmentSection.GetNorm(AssessmentSectionCategoryType.FactorizedSignalingNorm),
+                                    RingtoetsCommonDataResources.AssessmentSectionCategoryType_FactorizedSignalingNorm_DisplayName));
+
+            activities.AddRange(HydraulicBoundaryLocationCalculationActivityFactory.CreateDesignWaterLevelCalculationActivities(
+                                    assessmentSection,
+                                    assessmentSection.WaterLevelCalculationsForSignalingNorm,
+                                    assessmentSection.GetNorm(AssessmentSectionCategoryType.SignalingNorm),
+                                    RingtoetsCommonDataResources.AssessmentSectionCategoryType_SignalingNorm_DisplayName));
+
+            activities.AddRange(HydraulicBoundaryLocationCalculationActivityFactory.CreateDesignWaterLevelCalculationActivities(
+                                    assessmentSection,
+                                    assessmentSection.WaterLevelCalculationsForLowerLimitNorm,
+                                    assessmentSection.GetNorm(AssessmentSectionCategoryType.LowerLimitNorm),
+                                    RingtoetsCommonDataResources.AssessmentSectionCategoryType_LowerLimitNorm_DisplayName));
+
+            activities.AddRange(HydraulicBoundaryLocationCalculationActivityFactory.CreateDesignWaterLevelCalculationActivities(
+                                    assessmentSection,
+                                    assessmentSection.WaterLevelCalculationsForFactorizedLowerLimitNorm,
+                                    assessmentSection.GetNorm(AssessmentSectionCategoryType.FactorizedLowerLimitNorm),
+                                    RingtoetsCommonDataResources.AssessmentSectionCategoryType_FactorizedLowerLimitNorm_DisplayName));
+
+            return activities;
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Service.Test/AssessmentSectionHydraulicBoundaryLocationCalculationActivityFactoryTest.cs
@@ -0,0 +1,227 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Core.Common.Base.Service;
+using Core.Common.TestUtil;
+using Core.Common.Util;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.Hydraulics;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Common.Service;
+using Ringtoets.HydraRing.Calculation.Calculator.Factory;
+using Ringtoets.HydraRing.Calculation.Data.Input.Hydraulics;
+using Ringtoets.HydraRing.Calculation.TestUtil.Calculator;
+
+namespace Ringtoets.Integration.Service.Test
+{
+    [TestFixture]
+    public class AssessmentSectionHydraulicBoundaryLocationCalculationActivityFactoryTest
+    {
+        private static readonly string testDataPath = TestHelper.GetTestDataPath(TestDataPath.Ringtoets.Integration.Service, "HydraRingCalculation");
+
+        [Test]
+        public void CreateDesignWaterLevelCalculationActivities_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate test = () => AssessmentSectionHydraulicBoundaryLocationCalculationActivityFactory.CreateDesignWaterLevelCalculationActivities(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(test);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateDesignWaterLevelCalculationActivities_WithValidData_ExpectedInputSetToActivities()
+        {
+            // Setup
+            var assessmentSection = new AssessmentSectionStub
+            {
+                HydraulicBoundaryDatabase =
+                {
+                    FilePath = Path.Combine(testDataPath, "HRD ijsselmeer.sqlite")
+                }
+            };
+
+            var hydraulicBoundaryLocation1 = new TestHydraulicBoundaryLocation("locationName 1");
+            var hydraulicBoundaryLocation2 = new TestHydraulicBoundaryLocation("locationName 2");
+            assessmentSection.SetHydraulicBoundaryLocationCalculations(new[]
+            {
+                hydraulicBoundaryLocation1,
+                hydraulicBoundaryLocation2
+            });
+
+            // Call
+            IEnumerable<CalculatableActivity> activities =
+                AssessmentSectionHydraulicBoundaryLocationCalculationActivityFactory.CreateDesignWaterLevelCalculationActivities(assessmentSection);
+
+            // Assert
+            double signalingNorm = assessmentSection.FailureMechanismContribution.SignalingNorm;
+            double factorizedSignalingNorm = signalingNorm / 30;
+            AssertDesignWaterLevelCalculationActivity(activities.First(),
+                                                      hydraulicBoundaryLocation1,
+                                                      factorizedSignalingNorm);
+            AssertDesignWaterLevelCalculationActivity(activities.ElementAt(1),
+                                                      hydraulicBoundaryLocation2,
+                                                      factorizedSignalingNorm);
+
+            AssertDesignWaterLevelCalculationActivity(activities.ElementAt(2),
+                                                      hydraulicBoundaryLocation1,
+                                                      signalingNorm);
+            AssertDesignWaterLevelCalculationActivity(activities.ElementAt(3),
+                                                      hydraulicBoundaryLocation2,
+                                                      signalingNorm);
+
+            double lowerLimitNorm = assessmentSection.FailureMechanismContribution.LowerLimitNorm;
+            AssertDesignWaterLevelCalculationActivity(activities.ElementAt(4),
+                                                      hydraulicBoundaryLocation1,
+                                                      lowerLimitNorm);
+            AssertDesignWaterLevelCalculationActivity(activities.ElementAt(5),
+                                                      hydraulicBoundaryLocation2,
+                                                      lowerLimitNorm);
+
+            double factorizedLowerLimitNorm = lowerLimitNorm * 30;
+            AssertDesignWaterLevelCalculationActivity(activities.ElementAt(6),
+                                                      hydraulicBoundaryLocation1,
+                                                      factorizedLowerLimitNorm);
+            AssertDesignWaterLevelCalculationActivity(activities.ElementAt(7),
+                                                      hydraulicBoundaryLocation2,
+                                                      factorizedLowerLimitNorm);
+        }
+
+        [Test]
+        public void CreateWaveHeightCalculationActivities_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate test = () => AssessmentSectionHydraulicBoundaryLocationCalculationActivityFactory.CreateWaveHeightCalculationActivities(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(test);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateWaveHeightCalculationActivities_WithValidData_ExpectedInputSetToActivities()
+        {
+            // Setup
+            var assessmentSection = new AssessmentSectionStub
+            {
+                HydraulicBoundaryDatabase =
+                {
+                    FilePath = Path.Combine(testDataPath, "HRD ijsselmeer.sqlite")
+                }
+            };
+
+            var hydraulicBoundaryLocation1 = new TestHydraulicBoundaryLocation("locationName 1");
+            var hydraulicBoundaryLocation2 = new TestHydraulicBoundaryLocation("locationName 2");
+            assessmentSection.SetHydraulicBoundaryLocationCalculations(new[]
+            {
+                hydraulicBoundaryLocation1,
+                hydraulicBoundaryLocation2
+            });
+
+            // Call
+            IEnumerable<CalculatableActivity> activities =
+                AssessmentSectionHydraulicBoundaryLocationCalculationActivityFactory.CreateWaveHeightCalculationActivities(assessmentSection);
+
+            // Assert
+            double signalingNorm = assessmentSection.FailureMechanismContribution.SignalingNorm;
+            double factorizedSignalingNorm = signalingNorm / 30;
+            AssertWaveHeightCalculationActivity(activities.First(),
+                                                hydraulicBoundaryLocation1,
+                                                factorizedSignalingNorm);
+            AssertWaveHeightCalculationActivity(activities.ElementAt(1),
+                                                hydraulicBoundaryLocation2,
+                                                factorizedSignalingNorm);
+
+            AssertWaveHeightCalculationActivity(activities.ElementAt(2),
+                                                hydraulicBoundaryLocation1,
+                                                signalingNorm);
+            AssertWaveHeightCalculationActivity(activities.ElementAt(3),
+                                                hydraulicBoundaryLocation2,
+                                                signalingNorm);
+
+            double lowerLimitNorm = assessmentSection.FailureMechanismContribution.LowerLimitNorm;
+            AssertWaveHeightCalculationActivity(activities.ElementAt(4),
+                                                hydraulicBoundaryLocation1,
+                                                lowerLimitNorm);
+            AssertWaveHeightCalculationActivity(activities.ElementAt(5),
+                                                hydraulicBoundaryLocation2,
+                                                lowerLimitNorm);
+
+            double factorizedLowerLimitNorm = lowerLimitNorm * 30;
+            AssertWaveHeightCalculationActivity(activities.ElementAt(6),
+                                                hydraulicBoundaryLocation1,
+                                                factorizedLowerLimitNorm);
+            AssertWaveHeightCalculationActivity(activities.ElementAt(7),
+                                                hydraulicBoundaryLocation2,
+                                                factorizedLowerLimitNorm);
+        }
+
+        private static void AssertDesignWaterLevelCalculationActivity(Activity activity,
+                                                                      HydraulicBoundaryLocation hydraulicBoundaryLocation,
+                                                                      double norm)
+        {
+            var mocks = new MockRepository();
+            var designWaterLevelCalculator = new TestDesignWaterLevelCalculator();
+            var calculatorFactory = mocks.Stub<IHydraRingCalculatorFactory>();
+            calculatorFactory.Expect(cf => cf.CreateDesignWaterLevelCalculator(testDataPath, string.Empty)).Return(designWaterLevelCalculator);
+            mocks.ReplayAll();
+
+            using (new HydraRingCalculatorFactoryConfig(calculatorFactory))
+            {
+                activity.Run();
+
+                AssessmentLevelCalculationInput actualCalculationInput = designWaterLevelCalculator.ReceivedInputs.Single();
+                Assert.AreEqual(hydraulicBoundaryLocation.Id, actualCalculationInput.HydraulicBoundaryLocationId);
+                Assert.AreEqual(StatisticsConverter.ProbabilityToReliability(norm), actualCalculationInput.Beta);
+            }
+
+            mocks.VerifyAll();
+        }
+
+        private static void AssertWaveHeightCalculationActivity(Activity activity,
+                                                                HydraulicBoundaryLocation hydraulicBoundaryLocation,
+                                                                double norm)
+        {
+            var mocks = new MockRepository();
+            var waveHeightCalculator = new TestWaveHeightCalculator();
+            var calculatorFactory = mocks.Stub<IHydraRingCalculatorFactory>();
+            calculatorFactory.Expect(cf => cf.CreateWaveHeightCalculator(testDataPath, string.Empty)).Return(waveHeightCalculator);
+            mocks.ReplayAll();
+
+            using (new HydraRingCalculatorFactoryConfig(calculatorFactory))
+            {
+                activity.Run();
+
+                WaveHeightCalculationInput actualCalculationInput = waveHeightCalculator.ReceivedInputs.Single();
+                Assert.AreEqual(hydraulicBoundaryLocation.Id, actualCalculationInput.HydraulicBoundaryLocationId);
+                Assert.AreEqual(StatisticsConverter.ProbabilityToReliability(norm), actualCalculationInput.Beta);
+            }
+
+            mocks.VerifyAll();
+        }
+    }
+}
