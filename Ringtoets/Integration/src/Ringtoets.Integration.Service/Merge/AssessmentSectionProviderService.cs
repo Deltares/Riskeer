03944888070e0a93a7a8af136e0e859ecc77e694
Dennis Tang <dennis.tang@deltares.nl>
2018-07-06 12:55:10 +0200

    Initial setup of a RingtoetsProjectServiceProvider. WTI-1761

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Service/Merge/AssessmentSectionProviderService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Common.Base.Storage;
+using log4net;
+using Ringtoets.Integration.Data;
+
+namespace Ringtoets.Integration.Service.Merge
+{
+    /// <summary>
+    /// Service which provides a <see cref="RingtoetsProject"/> from a file.
+    /// </summary>
+    public class AssessmentSectionProviderService : IAssessmentSectionProvider
+    {
+        private readonly ILog log = LogManager.GetLogger(typeof(AssessmentSectionProviderService));
+        private readonly IStoreProject storage;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="AssessmentSectionProviderService"/>
+        /// </summary>
+        /// <param name="projectStorage">Class responsible to storing and loading the application project.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="projectStorage"/> is <c>null</c>.</exception>
+        public AssessmentSectionProviderService(IStoreProject projectStorage)
+        {
+            if (projectStorage == null)
+            {
+                throw new ArgumentNullException(nameof(projectStorage));
+            }
+
+            storage = projectStorage;
+        }
+
+        public IEnumerable<AssessmentSection> GetAssessmentSections(string filePath)
+        {
+            RingtoetsProject openedProject = null;
+            try
+            {
+                openedProject = (RingtoetsProject)storage.LoadProject(filePath);
+            }
+            catch (StorageException e)
+            {
+                log.Error(e.Message, e.InnerException);
+            }
+
+            return openedProject?.AssessmentSections;
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Service.Test/Merge/AssessmentSectionProviderServiceTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Common.Base.Storage;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.Service.Merge;
+
+namespace Ringtoets.Integration.Service.Test.Merge
+{
+    [TestFixture]
+    public class AssessmentSectionProviderServiceTest
+    {
+        [Test]
+        public void Constructor_ProjectStorageNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new AssessmentSectionProviderService(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("projectStorage", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var storeProject = mocks.Stub<IStoreProject>();
+            mocks.ReplayAll();
+
+            // Call
+            var provider = new AssessmentSectionProviderService(storeProject);
+
+            // Assert
+            Assert.IsInstanceOf<IAssessmentSectionProvider>(provider);
+        }
+
+        [Test]
+        public void GetAssessmentSections_LoadingProjectSuccesful_ReturnsRingtoetsProject()
+        {
+            // Setup
+            const string filePath = "Some path";
+            var project = new RingtoetsProject();
+
+            var mocks = new MockRepository();
+            var storeProject = mocks.StrictMock<IStoreProject>();
+            storeProject.Expect(sp => sp.LoadProject(filePath)).Return(project);
+            mocks.ReplayAll();
+
+            var provider = new AssessmentSectionProviderService(storeProject);
+
+            // Call
+            IEnumerable<AssessmentSection> assessmentSections = provider.GetAssessmentSections(filePath);
+
+            // Assert
+            Assert.AreSame(project.AssessmentSections, assessmentSections);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void GetAssessmentSections_LoadedProjectNull_ReturnsNull()
+        {
+            // Setup
+            const string filePath = "Some path";
+
+            var mocks = new MockRepository();
+            var storeProject = mocks.StrictMock<IStoreProject>();
+            storeProject.Expect(sp => sp.LoadProject(filePath)).Return(null);
+            mocks.ReplayAll();
+
+            var provider = new AssessmentSectionProviderService(storeProject);
+
+            // Call
+            IEnumerable<AssessmentSection> assessmentSections = provider.GetAssessmentSections(filePath);
+
+            // Assert
+            Assert.IsNull(assessmentSections);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void GetAssessmentSections_LoadingProjectFailed_LogsErrorAndThrowsStorageException()
+        {
+            // Setup
+            const string filePath = "Some path";
+            const string exceptionMessage = "Storage exception";
+
+            var mocks = new MockRepository();
+            var storeProject = mocks.StrictMock<IStoreProject>();
+            storeProject.Expect(sp => sp.LoadProject(filePath)).Throw(new StorageException(exceptionMessage));
+            mocks.ReplayAll();
+
+            var provider = new AssessmentSectionProviderService(storeProject);
+
+            // Call
+            Action call = () => provider.GetAssessmentSections(filePath);
+
+            // Assert
+            var expectedLogMessage = new Tuple<string, LogLevelConstant>(exceptionMessage, LogLevelConstant.Error);
+            TestHelper.AssertLogMessageWithLevelIsGenerated(call, expectedLogMessage);
+            mocks.VerifyAll();
+        }
+    }
+}
