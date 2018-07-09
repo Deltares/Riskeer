e1d11884f6326f861b8e627396446a75a4b06123
Dennis Tang <dennis.tang@deltares.nl>
2018-07-09 10:01:43 +0200

    Introduced factory to create activity to load collections of AssessmentSections from a file
    - Done to maintain consistency within the Activity - Factory structure within the application
    
    WTI-1761

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Service/Merge/LoadAssessmentSectionsActivityFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Core.Common.Base.Service;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.Data.Merge;
+
+namespace Ringtoets.Integration.Service.Merge
+{
+    /// <summary>
+    /// This class defines factory methods that can be used to create instance of <see cref="Activity"/>
+    /// to load <see cref="AssessmentSection"/>.
+    /// </summary>
+    public static class LoadAssessmentSectionsActivityFactory
+    {
+        /// <summary>
+        /// Creates an activity to load collections of <see cref="AssessmentSection"/> from a file.
+        /// </summary>
+        /// <param name="owner">The owner to set the retrieved collection
+        /// of <see cref="AssessmentSection"/> on.</param>
+        /// <param name="assessmentSectionProvider">The provider defining how to
+        /// retrieve the collection of <see cref="AssessmentSection"/> from a file.</param>
+        /// <param name="filePath">The file path to retrieve the collection of
+        /// <see cref="AssessmentSection"/> from.</param>
+        /// <returns>The <see cref="Activity"/> to load <see cref="AssessmentSection"/> from a file.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is <c>null</c>.</exception>
+        public static Activity CreateLoadAssessmentSectionsActivity(AssessmentSectionsOwner owner,
+                                                                    IAssessmentSectionProvider assessmentSectionProvider,
+                                                                    string filePath)
+        {
+            if (owner == null)
+            {
+                throw new ArgumentNullException(nameof(owner));
+            }
+
+            if (assessmentSectionProvider == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSectionProvider));
+            }
+
+            if (filePath == null)
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            return new LoadAssessmentSectionsActivity(owner, assessmentSectionProvider, filePath);
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Service.Test/Merge/LoadAssessmentSectionsActivityFactoryTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base.Service;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.Data.Merge;
+using Ringtoets.Integration.Service.Merge;
+
+namespace Ringtoets.Integration.Service.Test.Merge
+{
+    [TestFixture]
+    public class LoadAssessmentSectionsActivityFactoryTest
+    {
+        [Test]
+        public void CreateLoadAssessmentSectionsActivity_AssessmentSectionsOwnerNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var provider = mocks.Stub<IAssessmentSectionProvider>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => LoadAssessmentSectionsActivityFactory.CreateLoadAssessmentSectionsActivity(null, provider, string.Empty);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("owner", exception.ParamName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CreateLoadAssessmentSectionsActivity_AssessmentSectionsProviderNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => LoadAssessmentSectionsActivityFactory.CreateLoadAssessmentSectionsActivity(new AssessmentSectionsOwner(),
+                                                                                                                 null,
+                                                                                                                 string.Empty);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSectionProvider", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateLoadAssessmentSectionsActivity_FilePathNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var provider = mocks.Stub<IAssessmentSectionProvider>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => LoadAssessmentSectionsActivityFactory.CreateLoadAssessmentSectionsActivity(new AssessmentSectionsOwner(),
+                                                                                                                 provider,
+                                                                                                                 null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("filePath", exception.ParamName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CreateLoadAssessmentSectionsActivity_WithArguments_ReturnsActivityWithParametersSet()
+        {
+            // Setup
+            const string filePath = "File\\Path";
+
+            var owner = new AssessmentSectionsOwner();
+            IEnumerable<AssessmentSection> expectedAssessmentSections = Enumerable.Empty<AssessmentSection>();
+
+            var mocks = new MockRepository();
+            var provider = mocks.Stub<IAssessmentSectionProvider>();
+            provider.Expect(pr => pr.GetAssessmentSections(filePath)).Return(expectedAssessmentSections);
+            mocks.ReplayAll();
+
+            // Call
+            Activity activity = LoadAssessmentSectionsActivityFactory.CreateLoadAssessmentSectionsActivity(owner, provider, filePath);
+
+            // Assert
+            Assert.IsInstanceOf<LoadAssessmentSectionsActivity>(activity);
+            AssertLoadAssessmentSectionsActivity(activity, owner, expectedAssessmentSections);
+            mocks.VerifyAll();
+        }
+
+        private static void AssertLoadAssessmentSectionsActivity(Activity activity,
+                                                                 AssessmentSectionsOwner owner,
+                                                                 IEnumerable<AssessmentSection> expectedAssessmentSections)
+        {
+            activity.Run();
+
+            Assert.AreSame(expectedAssessmentSections, owner.AssessmentSections);
+        }
+    }
+}
