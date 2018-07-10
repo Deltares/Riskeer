2f76dec1e282b3a6a8dc4fb593643248d36aac28
Dennis Tang <dennis.tang@deltares.nl>
2018-07-10 11:17:53 +0200

    Added data row to represent failure mechanisms in the datagridview control for the mergedata selection dialog. WTI-1745

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Merge/FailureMechanismMergeDataRow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Integration.Forms.Merge
+{
+    /// <summary>
+    /// Row representing the information of a <see cref="IFailureMechanism"/> to be
+    /// used for merging.
+    /// </summary>
+    internal class FailureMechanismMergeDataRow
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="FailureMechanismMergeDataRow"/>.
+        /// </summary>
+        /// <param name="failureMechanism">The <see cref="IFailureMechanism"/> it needs to wrap.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="failureMechanism"/>
+        /// is <c>null</c>.</exception>
+        public FailureMechanismMergeDataRow(IFailureMechanism failureMechanism)
+        {
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+
+            FailureMechanism = failureMechanism;
+        }
+
+        /// <summary>
+        /// Gets the wrapped failure mechanism of the row.
+        /// </summary>
+        public IFailureMechanism FailureMechanism { get; }
+
+        /// <summary>
+        /// Indicates whether the failure mechanism is selected to be merged.
+        /// </summary>
+        public bool IsSelected { get; set; }
+
+        /// <summary>
+        /// Gets the name of the failure mechanism.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return FailureMechanism.Name;
+            }
+        }
+
+        /// <summary>
+        /// Gets if the failure mechanism is relevant.
+        /// </summary>
+        public bool IsRelevant
+        {
+            get
+            {
+                return FailureMechanism.IsRelevant;
+            }
+        }
+
+        /// <summary>
+        /// Gets if the failure mechanism has sections.
+        /// </summary>
+        public bool HasSections
+        {
+            get
+            {
+                return FailureMechanism.Sections.Any();
+            }
+        }
+
+        /// <summary>
+        /// Gets the amount of calculations that are contained by the failure mechanism.
+        /// </summary>
+        public int NumberOfCalculations
+        {
+            get
+            {
+                return FailureMechanism.Calculations.Count();
+            }
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/Merge/FailureMechanismMergeDataRowTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Integration.Forms.Merge;
+
+namespace Ringtoets.Integration.Forms.Test.Merge
+{
+    [TestFixture]
+    public class FailureMechanismMergeDataRowTest
+    {
+        [Test]
+        public void Constructor_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new FailureMechanismMergeDataRow(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanism", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            const string failureMechanismName = "Just a name";
+
+            var random = new Random(21);
+            bool isRelevant = random.NextBoolean();
+            IEnumerable<TestCalculation> calculations = Enumerable.Repeat(new TestCalculation(), random.Next(0, 10));
+
+            var mocks = new MockRepository();
+            var failureMechanism = mocks.Stub<IFailureMechanism>();
+            failureMechanism.Stub(fm => fm.Name).Return(failureMechanismName);
+            failureMechanism.Stub(fm => fm.Calculations).Return(calculations);
+            failureMechanism.Stub(fm => fm.Sections).Return(Enumerable.Empty<FailureMechanismSection>());
+            mocks.ReplayAll();
+
+            failureMechanism.IsRelevant = isRelevant;
+
+            // Call
+            var row = new FailureMechanismMergeDataRow(failureMechanism);
+
+            // Assert
+            Assert.AreSame(failureMechanism, row.FailureMechanism);
+
+            Assert.IsFalse(row.IsSelected);
+            Assert.AreEqual(failureMechanism.Name, row.Name);
+            Assert.AreEqual(isRelevant, row.IsRelevant);
+            Assert.IsFalse(row.HasSections);
+            Assert.AreEqual(calculations.Count(), row.NumberOfCalculations);
+
+            mocks.ReplayAll();
+        }
+
+        [Test]
+        public void HasSections_FailureMechanismWithSections_ReturnsTrue()
+        {
+            // Setup
+            var random = new Random(21);
+            IEnumerable<FailureMechanismSection> sections = Enumerable.Repeat(FailureMechanismSectionTestFactory.CreateFailureMechanismSection(),
+                                                                              random.Next(1, 10));
+
+            var mocks = new MockRepository();
+            var failureMechanism = mocks.Stub<IFailureMechanism>();
+            failureMechanism.Stub(fm => fm.Sections).Return(sections);
+            mocks.ReplayAll();
+
+            // Call
+            var row = new FailureMechanismMergeDataRow(failureMechanism);
+
+            // Assert
+            Assert.IsTrue(row.HasSections);
+        }
+    }
+}
