1f81a21fc2d33ca0e96085fa4cd26df30cf45aa5
Robert Tangerman <robert.tangerman@deltares.nl>
2018-07-13 15:04:13 +0200

    Introduced implementation of the IAssessmentSectionMergeFilePathProvider. WTI-1750

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Merge/AssessmentSectionMergeFilePathProvider.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Gui;
+using RingtoetsStorageCoreResources = Ringtoets.Storage.Core.Properties.Resources;
+
+namespace Ringtoets.Integration.Forms.Merge
+{
+    /// <summary>
+    /// Class for providing a file path of a project for which the assessment section can be merged.
+    /// </summary>
+    public class AssessmentSectionMergeFilePathProvider : IAssessmentSectionMergeFilePathProvider
+    {
+        private readonly IInquiryHelper inquiryHelper;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="AssessmentSectionMergeFilePathProvider"/>.
+        /// </summary>
+        /// <param name="inquiryHelper">Object responsible for inquiring the required data.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="inquiryHelper"/>
+        /// is <c>null</c>.</exception>
+        public AssessmentSectionMergeFilePathProvider(IInquiryHelper inquiryHelper)
+        {
+            if (inquiryHelper == null)
+            {
+                throw new ArgumentNullException(nameof(inquiryHelper));
+            }
+
+            this.inquiryHelper = inquiryHelper;
+        }
+
+        public string GetFilePath()
+        {
+            return inquiryHelper.GetSourceFileLocation(RingtoetsStorageCoreResources.Ringtoets_project_file_filter);
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/Merge/AssessmentSectionMergeFilePathProviderTest.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Gui;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Integration.Forms.Merge;
+
+namespace Ringtoets.Integration.Forms.Test.Merge
+{
+    [TestFixture]
+    public class AssessmentSectionMergeFilePathProviderTest
+    {
+        [Test]
+        public void Constructor_InquiryHelperNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new AssessmentSectionMergeFilePathProvider(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("inquiryHelper", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.StrictMock<IInquiryHelper>();
+            mocks.ReplayAll();
+
+            // Call
+            var provider = new AssessmentSectionMergeFilePathProvider(inquiryHelper);
+
+            // Assert
+            Assert.IsInstanceOf<IAssessmentSectionMergeFilePathProvider>(provider);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("FilePath/Something")]
+        public void GetFilePath_Always_ReturnFilePathFromInquiryHelper(string expectedFilePath)
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.StrictMock<IInquiryHelper>();
+            inquiryHelper.Expect(ih => ih.GetSourceFileLocation("Ringtoetsproject (*.rtd)|*.rtd")).Return(expectedFilePath);
+            mocks.ReplayAll();
+
+            var provider = new AssessmentSectionMergeFilePathProvider(inquiryHelper);
+
+            // Call
+            string filePath = provider.GetFilePath();
+
+            // Assert
+            Assert.AreEqual(expectedFilePath, filePath);
+            mocks.VerifyAll();
+        }
+    }
+}
