93522ef41779575693bb5692c5be649017570b30
Robert Tangerman <robert.tangerman@deltares.nl>
2018-07-19 15:08:17 +0200

    Introduced merge handler. WTI-1746

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Plugin/Merge/AssessmentSectionMergeHandler.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Integration.Data;
+
+namespace Ringtoets.Integration.Plugin.Merge
+{
+    /// <summary>
+    /// Class responsible for handling the merge of <see cref="AssessmentSection"/> data.
+    /// </summary>
+    public class AssessmentSectionMergeHandler : IAssessmentSectionMergeHandler
+    {
+        public void PerformMerge(AssessmentSection targetAssessmentSection, AssessmentSection sourceAssessmentSection,
+                                 IEnumerable<IFailureMechanism> failureMechanismsToMerge) {}
+    }
+}
