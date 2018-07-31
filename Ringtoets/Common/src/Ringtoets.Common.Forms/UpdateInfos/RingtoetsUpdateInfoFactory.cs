0f065fd28bb73332728fbfccebe7ce647cd008b6
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-07-31 14:36:36 +0200

    Introduce updateinfofactory for creating updateinfo objects for failure mechanism section updating WTI-1752

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/UpdateInfos/RingtoetsUpdateInfoFactory.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Gui.Plugin;
+using Core.Common.Util;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Forms.PresentationObjects;
+using Ringtoets.Common.Forms.Properties;
+using Ringtoets.Common.IO.FileImporters;
+using RingtoetsCommonIOResources = Ringtoets.Common.IO.Properties.Resources;
+
+namespace Ringtoets.Common.Forms.UpdateInfos
+{
+    /// <summary>
+    /// Factory for creating standard <see cref="UpdateInfo"/> objects. 
+    /// </summary>
+    public static class RingtoetsUpdateInfoFactory
+    {
+        /// <summary>
+        /// Creates a <see cref="UpdateInfo"/> object for a <see cref="TSectionContext"/>.
+        /// </summary>
+        /// <typeparam name="TSectionContext">The type of the failure mechanism sections context
+        /// to create the <see cref="UpdateInfo"/> for.</typeparam>
+        /// <typeparam name="TFailureMechanism">The type of the failure mechanism to create
+        /// the <see cref="UpdateInfo"/> for.</typeparam>
+        /// <typeparam name="TSectionResult">The type of the failure mechanism section result
+        /// to create the <see cref="UpdateInfo"/> for.</typeparam>
+        /// <param name="sectionResultUpdateStrategy">The <see cref="IFailureMechanismSectionResultUpdateStrategy{T}"/>
+        /// to use for the created <see cref="UpdateInfo"/>.</param>
+        /// <returns>An <see cref="UpdateInfo"/> object.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="sectionResultUpdateStrategy"/>
+        /// is <c>null</c>.</exception>
+        public static UpdateInfo<TSectionContext> CreateFailureMechanismSectionsUpdateInfo<TSectionContext, TFailureMechanism, TSectionResult>(
+            IFailureMechanismSectionResultUpdateStrategy<TSectionResult> sectionResultUpdateStrategy)
+            where TSectionContext : FailureMechanismSectionsContext
+            where TFailureMechanism : IHasSectionResults<TSectionResult>
+            where TSectionResult : FailureMechanismSectionResult
+        {
+            if (sectionResultUpdateStrategy == null)
+            {
+                throw new ArgumentNullException(nameof(sectionResultUpdateStrategy));
+            }
+
+            return new UpdateInfo<TSectionContext>
+            {
+                Name = Resources.FailureMechanismSections_DisplayName,
+                Category = Resources.Ringtoets_Category,
+                Image = Resources.SectionsIcon,
+                FileFilterGenerator = new FileFilterGenerator(RingtoetsCommonIOResources.Shape_file_filter_Extension,
+                                                              RingtoetsCommonIOResources.Shape_file_filter_Description),
+                IsEnabled = context => context.WrappedData.FailureMechanismSectionSourcePath != null,
+                CurrentPath = context => context.WrappedData.FailureMechanismSectionSourcePath,
+                CreateFileImporter = (context, filePath) => new FailureMechanismSectionsImporter(
+                    context.WrappedData,
+                    context.AssessmentSection.ReferenceLine,
+                    filePath,
+                    new FailureMechanismSectionUpdateStrategy<TSectionResult>((TFailureMechanism) context.WrappedData,
+                                                                              sectionResultUpdateStrategy))
+            };
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.Test/UpdateInfos/RingtoetsUpdateInfoFactoryTest.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Gui.Plugin;
+using Core.Common.TestUtil;
+using Core.Common.Util;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Common.Forms.PresentationObjects;
+using Ringtoets.Common.Forms.Properties;
+using Ringtoets.Common.Forms.UpdateInfos;
+using Ringtoets.Common.IO.FileImporters;
+
+namespace Ringtoets.Common.Forms.Test.UpdateInfos
+{
+    [TestFixture]
+    public class RingtoetsUpdateInfoFactoryTest
+    {
+        [Test]
+        public void CreateFailureMechanismSectionsUpdateInfo_SectionResultUpdateStrategyNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => RingtoetsUpdateInfoFactory.CreateFailureMechanismSectionsUpdateInfo<
+                FailureMechanismSectionsContext, TestFailureMechanism, FailureMechanismSectionResult>(null);
+
+            // Assert
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("sectionResultUpdateStrategy", paramName);
+        }
+
+        [Test]
+        public void CreateFailureMechanismSectionsUpdateInfo_WithArguments_ExpectedPropertiesSet()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var sectionResultUpdateStrategy = mocks.Stub<IFailureMechanismSectionResultUpdateStrategy<FailureMechanismSectionResult>>();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            assessmentSection.ReferenceLine = new ReferenceLine();
+            mocks.ReplayAll();
+
+            // Call
+            UpdateInfo<FailureMechanismSectionsContext> updateInfo = RingtoetsUpdateInfoFactory.CreateFailureMechanismSectionsUpdateInfo<
+                FailureMechanismSectionsContext, TestFailureMechanism, FailureMechanismSectionResult>(sectionResultUpdateStrategy);
+
+            // Assert
+            var failureMechanismSectionsContext = new FailureMechanismSectionsContext(new TestFailureMechanism(), assessmentSection);
+            Assert.IsInstanceOf<FailureMechanismSectionsImporter>(updateInfo.CreateFileImporter(failureMechanismSectionsContext, ""));
+            Assert.AreEqual("Vakindeling", updateInfo.Name);
+            Assert.AreEqual("Algemeen", updateInfo.Category);
+
+            FileFilterGenerator fileFilterGenerator = updateInfo.FileFilterGenerator;
+            Assert.AreEqual("Shapebestand (*.shp)|*.shp", fileFilterGenerator.Filter);
+
+            TestHelper.AssertImagesAreEqual(Resources.SectionsIcon, updateInfo.Image);
+            Assert.IsFalse(updateInfo.IsEnabled(failureMechanismSectionsContext));
+
+            mocks.VerifyAll();
+        }
+    }
+}
