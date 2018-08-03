7cf7e1a258fbc088058debc57a3104749410ae39
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-08-03 12:59:45 +0200

    Introduce failure mechanism context objects for standalones WTI-1753

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/PresentationObjects/StandAlone/StrengthStabilityLengthwiseConstructionFailureMechanismContext.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Forms.PresentationObjects;
+using Ringtoets.Integration.Data.StandAlone;
+
+namespace Ringtoets.Integration.Forms.PresentationObjects.StandAlone
+{
+    /// <summary>
+    /// This class is a presentation object for an instance of <see cref="StrengthStabilityLengthwiseConstructionFailureMechanism"/>.
+    /// </summary>
+    public class StrengthStabilityLengthwiseConstructionFailureMechanismContext : FailureMechanismContext<StrengthStabilityLengthwiseConstructionFailureMechanism>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StrengthStabilityLengthwiseConstructionFailureMechanismContext"/> class.
+        /// </summary>
+        /// <param name="failureMechanism">The failure mechanism.</param>
+        /// <param name="assessmentSection">The parent of <paramref name="failureMechanism"/>.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any input argument is <c>null</c>.</exception>
+        public StrengthStabilityLengthwiseConstructionFailureMechanismContext(StrengthStabilityLengthwiseConstructionFailureMechanism failureMechanism,
+                                                                              IAssessmentSection assessmentSection)
+            : base(failureMechanism, assessmentSection) {}
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/PresentationObjects/StandAlone/MicrostabilityFailureMechanismContextTest.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Forms.PresentationObjects;
+using Ringtoets.Integration.Data.StandAlone;
+using Ringtoets.Integration.Forms.PresentationObjects.StandAlone;
+
+namespace Ringtoets.Integration.Forms.Test.PresentationObjects.StandAlone
+{
+    [TestFixture]
+    public class MicrostabilityFailureMechanismContextTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new MicrostabilityFailureMechanism();
+
+            // Call
+            var context = new MicrostabilityFailureMechanismContext(failureMechanism, assessmentSection);
+
+            // Assert
+            Assert.IsInstanceOf<FailureMechanismContext<MicrostabilityFailureMechanism>>(context);
+            Assert.AreSame(assessmentSection, context.Parent);
+            Assert.AreSame(failureMechanism, context.WrappedData);
+            mocks.VerifyAll();
+        }
+    }
+}
