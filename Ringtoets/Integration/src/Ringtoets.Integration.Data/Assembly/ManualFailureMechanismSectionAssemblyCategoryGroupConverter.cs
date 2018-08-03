bba475f0f6a350c23bc9088bb38b1b52d22aae25
Dennis Tang <dennis.tang@deltares.nl>
2018-08-03 10:34:54 +0200

    Introduced converter to convert from Manual to FailureMechanismSectionCategoryGroup
    - Required in order to feed the custom enum into the assembly calculators for Group 4 failure mechanisms.
    
    WTI-1764

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Data/Assembly/ManualFailureMechanismSectionAssemblyCategoryGroupConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using Ringtoets.AssemblyTool.Data;
+
+namespace Ringtoets.Integration.Data.Assembly
+{
+    /// <summary>
+    /// The cpnverter that <see cref="ManualFailureMechanismSectionAssemblyCategoryGroup"/>
+    /// to <see cref="FailureMechanismSectionAssemblyCategoryGroup"/>.
+    /// </summary>
+    public static class ManualFailureMechanismSectionAssemblyCategoryGroupConverter
+    {
+        /// <summary>
+        /// Converts a <see cref="ManualFailureMechanismSectionAssemblyCategoryGroup"/> into a
+        /// <see cref="FailureMechanismSectionAssemblyCategoryGroup"/>.
+        /// </summary>
+        /// <param name="categoryGroup">The <see cref="ManualFailureMechanismSectionAssemblyCategoryGroup"/> to convert.</param>
+        /// <returns>The <see cref="FailureMechanismSectionAssemblyCategoryGroup"/> based on <paramref name="categoryGroup"/>.</returns>
+        /// <exception cref="InvalidEnumArgumentException">Thrown when <paramref name="categoryGroup"/> is an invalid value.</exception>
+        /// <exception cref="NotSupportedException">Thrown when <paramref name="categoryGroup"/> is a valid value, but not supported.</exception>
+        public static FailureMechanismSectionAssemblyCategoryGroup Convert(ManualFailureMechanismSectionAssemblyCategoryGroup categoryGroup)
+        {
+            if (!Enum.IsDefined(typeof(ManualFailureMechanismSectionAssemblyCategoryGroup), categoryGroup))
+            {
+                throw new InvalidEnumArgumentException(nameof(categoryGroup),
+                                                       (int) categoryGroup,
+                                                       typeof(ManualFailureMechanismSectionAssemblyCategoryGroup));
+            }
+
+            switch (categoryGroup)
+            {
+                case ManualFailureMechanismSectionAssemblyCategoryGroup.None:
+                    return FailureMechanismSectionAssemblyCategoryGroup.None;
+                case ManualFailureMechanismSectionAssemblyCategoryGroup.NotApplicable:
+                    return FailureMechanismSectionAssemblyCategoryGroup.NotApplicable;
+                case ManualFailureMechanismSectionAssemblyCategoryGroup.Iv:
+                    return FailureMechanismSectionAssemblyCategoryGroup.Iv;
+                case ManualFailureMechanismSectionAssemblyCategoryGroup.IIv:
+                    return FailureMechanismSectionAssemblyCategoryGroup.IIv;
+                case ManualFailureMechanismSectionAssemblyCategoryGroup.Vv:
+                    return FailureMechanismSectionAssemblyCategoryGroup.Vv;
+                case ManualFailureMechanismSectionAssemblyCategoryGroup.VIIv:
+                    return FailureMechanismSectionAssemblyCategoryGroup.VIIv;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Data.Test/Assembly/ManualFailureMechanismSectionAssemblyCategoryGroupConverterTest.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Integration.Data.Assembly;
+
+namespace Ringtoets.Integration.Data.Test.Assembly
+{
+    [TestFixture]
+    public class ManualFailureMechanismSectionAssemblyCategoryGroupConverterTest
+    {
+        [Test]
+        [TestCase(ManualFailureMechanismSectionAssemblyCategoryGroup.None, FailureMechanismSectionAssemblyCategoryGroup.None)]
+        [TestCase(ManualFailureMechanismSectionAssemblyCategoryGroup.NotApplicable, FailureMechanismSectionAssemblyCategoryGroup.NotApplicable)]
+        [TestCase(ManualFailureMechanismSectionAssemblyCategoryGroup.Iv, FailureMechanismSectionAssemblyCategoryGroup.Iv)]
+        [TestCase(ManualFailureMechanismSectionAssemblyCategoryGroup.IIv, FailureMechanismSectionAssemblyCategoryGroup.IIv)]
+        [TestCase(ManualFailureMechanismSectionAssemblyCategoryGroup.Vv, FailureMechanismSectionAssemblyCategoryGroup.Vv)]
+        [TestCase(ManualFailureMechanismSectionAssemblyCategoryGroup.VIIv, FailureMechanismSectionAssemblyCategoryGroup.VIIv)]
+        public void Convert_WIthValidManualFailureMechanismSectionAssemblyCategoryGroup_ReturnsExpectedFailureMechanismSectionAssemblyCategoryGroup(
+            ManualFailureMechanismSectionAssemblyCategoryGroup originalCategoryGroup,
+            FailureMechanismSectionAssemblyCategoryGroup expectedCategoryGroup)
+        {
+            // Call
+            FailureMechanismSectionAssemblyCategoryGroup result =
+                ManualFailureMechanismSectionAssemblyCategoryGroupConverter.Convert(originalCategoryGroup);
+
+            // Assert
+            Assert.AreEqual(expectedCategoryGroup, result);
+        }
+
+        [Test]
+        public void Convert_WithInvalidManualFailureMechanismSectionAssemblyCategoryGroup_ThrowsInvalidEnumArgumentException()
+        {
+            // Setup
+            const ManualFailureMechanismSectionAssemblyCategoryGroup invalidCategoryGroup = (ManualFailureMechanismSectionAssemblyCategoryGroup) 99;
+
+            // Call
+            TestDelegate test = () => ManualFailureMechanismSectionAssemblyCategoryGroupConverter.Convert(invalidCategoryGroup);
+
+            // Assert
+            string expectedMessage = $"The value of argument 'categoryGroup' (99) is invalid for Enum type '{nameof(ManualFailureMechanismSectionAssemblyCategoryGroup)}'.";
+            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<InvalidEnumArgumentException>(test, expectedMessage);
+        }
+    }
+}
