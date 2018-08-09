245fc0e0843e3ac3694436d2a9cef6504c308559
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-08-09 15:37:47 +0200

    Introduce assembly methods WTI-1789

--- Ringtoets/AssemblyTool/test/Ringtoets.AssemblyTool.IO.Test/Model/Enums/AssemblyXmlEnumIdentifiersTest.cs
+++ Ringtoets/AssemblyTool/test/Ringtoets.AssemblyTool.IO.Test/Model/Enums/AssemblyXmlEnumIdentifiersTest.cs
@@ -38,6 +38,29 @@ namespace Ringtoets.AssemblyTool.IO.Test.Model.Enums
             Assert.AreEqual("V-traject", AssemblyXmlEnumIdentifiers.SerializableFailureMechanismCategoryGroupVt);
             Assert.AreEqual("VI-traject", AssemblyXmlEnumIdentifiers.SerializableFailureMechanismCategoryGroupVIt);
             Assert.AreEqual("VII-traject", AssemblyXmlEnumIdentifiers.SerializableFailureMechanismCategoryGroupVIIt);
+
+            Assert.AreEqual("WBI-0E-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0E1);
+            Assert.AreEqual("WBI-0E-3", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0E3);
+            Assert.AreEqual("WBI-0G-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0G1);
+            Assert.AreEqual("WBI-0G-3", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0G3);
+            Assert.AreEqual("WBI-0G-4", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0G4);
+            Assert.AreEqual("WBI-0G-5", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0G5);
+            Assert.AreEqual("WBI-0G-6", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0G6);
+            Assert.AreEqual("WBI-0T-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0T1);
+            Assert.AreEqual("WBI-0T-3", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0T3);
+            Assert.AreEqual("WBI-0T-4", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0T4);
+            Assert.AreEqual("WBI-0T-5", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0T5);
+            Assert.AreEqual("WBI-0T-6", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0T6);
+            Assert.AreEqual("WBI-0T-7", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0T7);
+            Assert.AreEqual("WBI-0A-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI0A1);
+            Assert.AreEqual("WBI-1A-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI1A1);
+            Assert.AreEqual("WBI-1B-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI1B1);
+            Assert.AreEqual("WBI-2A-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI2A1);
+            Assert.AreEqual("WBI-2B-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI2B1);
+            Assert.AreEqual("WBI-2C-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI2C1);
+            Assert.AreEqual("WBI-3A-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI3A1);
+            Assert.AreEqual("WBI-3B-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI3B1);
+            Assert.AreEqual("WBI-3C-1", AssemblyXmlEnumIdentifiers.AssemblyMethodWBI3C1);
         }
     }
 }
