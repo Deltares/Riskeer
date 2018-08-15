719f58258effac77f637df0dbb631cf5ac3b0753
Dennis Tang <dennis.tang@deltares.nl>
2018-08-15 16:18:11 +0200

    Introduced exportable failure mechanism section:
    - Ringtoets definition of the failure mechanism section is inadequate to transform the intermediate result to the serializable counterpart.
    
    WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableFailureMechanismSection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Common.Base.Geometry;
+
+namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Class which holds all the information to export a failure mechanism section.
+    /// </summary>
+    public class ExportableFailureMechanismSection
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExportableFailureMechanismSection"/>.
+        /// </summary>
+        /// <param name="geometry">The geometry of the failure mechanism section.</param>
+        /// <param name="startDistance">The start distance of the failure mechanism section between the section
+        /// and the start of the reference line.</param>
+        /// <param name="endDistance">The end distance of the failure mechanism section between the section
+        /// and the start of the reference line.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="geometry"/> is <c>null</c>.</exception>
+        public ExportableFailureMechanismSection(IEnumerable<Point2D> geometry, double startDistance, double endDistance)
+        {
+            if (geometry == null)
+            {
+                throw new ArgumentNullException(nameof(geometry));
+            }
+
+            Geometry = geometry;
+            StartDistance = startDistance;
+            EndDistance = endDistance;
+        }
+
+        /// <summary>
+        /// Gets the geometry of the failure mechanism section.
+        /// </summary>
+        public IEnumerable<Point2D> Geometry { get; }
+
+        /// <summary>
+        /// Gets the start distance between the failure mechanism section and the start of the reference line.
+        /// </summary>
+        public double StartDistance { get; }
+
+        /// <summary>
+        /// Gets the end distance between the failure mechanism section and the reference line.
+        /// </summary>
+        public double EndDistance { get; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableFailureMechanismSectionTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base.Geometry;
+using NUnit.Framework;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableFailureMechanismSectionTest
+    {
+        [Test]
+        public void Constructor_GeometryNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+
+            // Call
+            TestDelegate call = () => new ExportableFailureMechanismSection(null,
+                                                                            random.NextDouble(),
+                                                                            random.NextDouble());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("geometry", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            IEnumerable<Point2D> geometry = Enumerable.Empty<Point2D>();
+            double startDistance = random.NextDouble();
+            double endDistance = random.NextDouble();
+
+            // Call
+            var section = new ExportableFailureMechanismSection(geometry, startDistance, endDistance);
+
+            // Assert
+            Assert.AreSame(geometry, section.Geometry);
+            Assert.AreEqual(startDistance, section.StartDistance);
+            Assert.AreEqual(endDistance, section.EndDistance);
+        }
+    }
+}
