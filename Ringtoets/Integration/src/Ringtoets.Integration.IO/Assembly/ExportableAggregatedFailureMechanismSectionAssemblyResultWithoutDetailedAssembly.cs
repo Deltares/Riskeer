083dc60305e72a225bb92b247fbc03d428ac05f3
Dennis Tang <dennis.tang@deltares.nl>
2018-08-15 12:49:34 +0200

    Introduced object to hold all assembly information:
    - Introduced a derivative to represent failure mechanism section result that do not have a detailed assembly result
    
    WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableAggregatedFailureMechanismSectionAssemblyResultWithoutDetailedAssembly.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Class that holds all the information to export a failure mechanism section assembly result without a detailed assembly.
+    /// </summary>
+    public class ExportableAggregatedFailureMechanismSectionAssemblyResultWithoutDetailedAssembly :
+        ExportableAggregatedFailureMechanismSectionAssemblyResultBase<ExportableSectionAssemblyResult>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExportableAggregatedFailureMechanismSectionAssemblyResultWithProbability"/>.
+        /// </summary>
+        /// <param name="failureMechanismSection">The failure mechanism section.</param>
+        /// <param name="simpleAssembly">The simple assembly result of the failure mechanism section.</param>
+        /// <param name="tailorMadeAssembly">The tailor made assembly result of the failure mechanism section.</param>
+        /// <param name="combinedAssembly">The combined assembly result of the failure mechanism section.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null.</c></exception>
+        public ExportableAggregatedFailureMechanismSectionAssemblyResultWithoutDetailedAssembly(FailureMechanismSection failureMechanismSection,
+                                                                                                ExportableSectionAssemblyResult simpleAssembly,
+                                                                                                ExportableSectionAssemblyResult tailorMadeAssembly,
+                                                                                                ExportableSectionAssemblyResult combinedAssembly)
+            : base(failureMechanismSection, simpleAssembly, tailorMadeAssembly, combinedAssembly) {}
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableAggregatedFailureMechanismSectionAssemblyResultWithoutDetailedAssemblyTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableAggregatedFailureMechanismSectionAssemblyResultWithoutDetailedAssemblyTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            FailureMechanismSection failureMechanismSection = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            ExportableSectionAssemblyResult simpleAssembly = CreateSectionResult();
+            ExportableSectionAssemblyResult tailorMadeAssembly = CreateSectionResult();
+            ExportableSectionAssemblyResult combinedAssembly = CreateSectionResult();
+
+            // Call
+            var assemblyResult = new ExportableAggregatedFailureMechanismSectionAssemblyResultWithoutDetailedAssembly(failureMechanismSection,
+                                                                                                                      simpleAssembly,
+                                                                                                                      tailorMadeAssembly,
+                                                                                                                      combinedAssembly);
+
+            // Assert
+            Assert.IsInstanceOf<ExportableAggregatedFailureMechanismSectionAssemblyResultBase<ExportableSectionAssemblyResult>>(assemblyResult);
+
+            Assert.AreSame(failureMechanismSection, assemblyResult.FailureMechanismSection);
+            Assert.AreSame(simpleAssembly, assemblyResult.SimpleAssembly);
+            Assert.AreSame(tailorMadeAssembly, assemblyResult.TailorMadeAssembly);
+            Assert.AreSame(combinedAssembly, assemblyResult.CombinedAssembly);
+        }
+
+        private static ExportableSectionAssemblyResult CreateSectionResult()
+        {
+            var random = new Random(21);
+            return new ExportableSectionAssemblyResult(random.NextEnumValue<ExportableFailureMechanismSectionAssemblyMethod>(),
+                                                       random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+        }
+    }
+}
