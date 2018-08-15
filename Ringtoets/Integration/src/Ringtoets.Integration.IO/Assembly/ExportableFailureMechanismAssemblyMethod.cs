dd302c312d14317c1ac7a065f94696f53920517e
Dennis Tang <dennis.tang@deltares.nl>
2018-08-15 14:11:16 +0200

    Introduced enum to represent the method calls for a failure mechanism assembly. WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableFailureMechanismAssemblyMethod.cs
@@ -0,0 +1,23 @@
+﻿namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Enum defining the exportable assembly category methods for a failure mechanism.
+    /// </summary>
+    public enum ExportableFailureMechanismAssemblyMethod
+    {
+        /// <summary>
+        /// Represents the assembly method WBI-2A-1.
+        /// </summary>
+        WBI2A1 = 1,
+
+        /// <summary>
+        /// Represents the assembly method WBI-2B-1.
+        /// </summary>
+        WBI2B1 = 2,
+
+        /// <summary>
+        /// Represents the assembly method WBI-2C-1.
+        /// </summary>
+        WBI2C1 = 3
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableFailureMechanismAssemblyMethodTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableFailureMechanismAssemblyMethodTest : EnumValuesTestFixture<ExportableFailureMechanismAssemblyMethod, int>
+    {
+        protected override IDictionary<ExportableFailureMechanismAssemblyMethod, int> ExpectedValueForEnumValues
+        {
+            get
+            {
+                return new Dictionary<ExportableFailureMechanismAssemblyMethod, int>
+                {
+                    {
+                        ExportableFailureMechanismAssemblyMethod.WBI2A1, 1
+                    },
+                    {
+                        ExportableFailureMechanismAssemblyMethod.WBI2B1, 2
+                    },
+                    {
+                        ExportableFailureMechanismAssemblyMethod.WBI2C1, 3
+                    }
+                };
+            }
+        }
+    }
+}
