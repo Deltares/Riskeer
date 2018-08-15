79b40b5d932dcb0212ec26b0962fe1fa80d15805
Dennis Tang <dennis.tang@deltares.nl>
2018-08-15 10:11:44 +0200

    Implemented enum representing the assembly methods for failure mechanism sections. WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableFailureMechanismSectionAssemblyMethod.cs
@@ -0,0 +1,73 @@
+﻿namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Enum defining the exportable assembly category methods for a failure mechanism section.
+    /// </summary>
+    public enum ExportableFailureMechanismSectionAssemblyMethod
+    {
+        /// <summary>
+        /// Represents the assembly method WBI-0E-1.
+        /// </summary>
+        WBI0E1 = 1,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0E-3.
+        /// </summary>
+        WBI0E3 = 2,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0G-1.
+        /// </summary>
+        WBI0G1 = 3,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0G-3.
+        /// </summary>
+        WBI0G3 = 4,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0G-4.
+        /// </summary>
+        WBI0G4 = 5,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0G-5.
+        /// </summary>
+        WBI0G5 = 6,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0G-6.
+        /// </summary>
+        WBI0G6 = 7,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0T-1.
+        /// </summary>
+        WBI0T1 = 8,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0T-3.
+        /// </summary>
+        WBI0T3 = 9,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0T-4.
+        /// </summary>
+        WBI0T4 = 10,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0T-5.
+        /// </summary>
+        WBI0T5 = 11,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0T-6.
+        /// </summary>
+        WBI0T6 = 12,
+
+        /// <summary>
+        /// Represents the assembly method WBI-0T-7.
+        /// </summary>
+        WBI0T7 = 13,
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableFailureMechanismSectionAssemblyMethodTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableFailureMechanismSectionAssemblyMethodTest : EnumValuesTestFixture<ExportableFailureMechanismSectionAssemblyMethod, int>
+    {
+        protected override IDictionary<ExportableFailureMechanismSectionAssemblyMethod, int> ExpectedValueForEnumValues
+        {
+            get
+            {
+                return new Dictionary<ExportableFailureMechanismSectionAssemblyMethod, int>
+                {
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0E1, 1 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0E3, 2 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0G1, 3 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0G3, 4 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0G4, 5 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0G5, 6 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0G6, 7 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0T1, 8 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0T3, 9 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0T4, 10 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0T5, 11 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0T6, 12 },
+                    { ExportableFailureMechanismSectionAssemblyMethod.WBI0T7, 13 }
+                };
+            }
+        }
+    }
+}
