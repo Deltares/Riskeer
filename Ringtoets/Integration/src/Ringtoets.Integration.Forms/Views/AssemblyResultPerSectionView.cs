034ebb0408dfbf2a816c9042632a53b492143e1a
Robert Tangerman <robert.tangerman@deltares.nl>
2018-08-15 11:51:23 +0200

    Revert "Added separate observer for assessment section in assembly result per section view. WTI-1793"
    This reverts commit 407e8e07eed3bf16bac838855a7fdfdfd39f22ac.

--- Ringtoets/Integration/src/Ringtoets.Integration.Forms/Views/AssemblyResultPerSectionView.cs
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Views/AssemblyResultPerSectionView.cs
@@ -50,7 +50,6 @@ namespace Ringtoets.Integration.Forms.Views
     /// </summary>
     public partial class AssemblyResultPerSectionView : UserControl, IView
     {
-        private readonly Observer assessmentSectionObserver;
         private readonly Observer assessmentSectionResultObserver;
 
         /// <summary>
@@ -69,10 +68,6 @@ namespace Ringtoets.Integration.Forms.Views
             AssessmentSection = assessmentSection;
             InitializeComponent();
 
-            assessmentSectionObserver = new Observer(EnableRefreshButton)
-            {
-                Observable = assessmentSection
-            };
             assessmentSectionResultObserver = new Observer(EnableRefreshButton)
             {
                 Observable = new AssessmentSectionResultObserver(assessmentSection)
@@ -97,10 +92,11 @@ namespace Ringtoets.Integration.Forms.Views
 
         protected override void Dispose(bool disposing)
         {
+            dataGridViewControl.CellFormatting -= HandleCellStyling;
+
             if (disposing)
             {
                 components?.Dispose();
-                assessmentSectionObserver.Dispose();
                 assessmentSectionResultObserver.Dispose();
             }
 
