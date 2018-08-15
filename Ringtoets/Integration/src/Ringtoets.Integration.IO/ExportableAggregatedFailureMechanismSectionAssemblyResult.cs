317d9ca8e3c59fd481ec2d4183742b528241127d
Dennis Tang <dennis.tang@deltares.nl>
2018-08-15 11:33:28 +0200

    Introduced object to hold all assembly information:
    - Object represents a failure mechanism section result that has assembly results without a probability.
    
    WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/ExportableAggregatedFailureMechanismSectionAssemblyResult.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO
+{
+    /// <summary>
+    /// Class that holds all the information to export a failure mechanism section assembly result.
+    /// </summary>
+    public class ExportableAggregatedFailureMechanismSectionAssemblyResult
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExportableAggregatedFailureMechanismSectionAssemblyResult"/>.
+        /// </summary>
+        /// <param name="failureMechanismSection">The failure mechanism section.</param>
+        /// <param name="simpleAssembly">The simple assembly result of the failure mechanism section.</param>
+        /// <param name="detailedAssembly">The detailed assembly result of the failure mechanism section.</param>
+        /// <param name="tailorMadeAssembly">The tailor made assembly result of the failure mechanism section.</param>
+        /// <param name="combinedAssembly">The combined assembly result of the failure mechanism section.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null.</c></exception>
+        public ExportableAggregatedFailureMechanismSectionAssemblyResult(FailureMechanismSection failureMechanismSection, 
+                                                                         ExportableSectionAssemblyResult simpleAssembly, 
+                                                                         ExportableSectionAssemblyResult detailedAssembly, 
+                                                                         ExportableSectionAssemblyResult tailorMadeAssembly, 
+                                                                         ExportableSectionAssemblyResult combinedAssembly)
+        {
+            if (failureMechanismSection == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSection));
+            }
+
+            if (simpleAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(simpleAssembly));
+            }
+
+            if (detailedAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(detailedAssembly));
+            }
+
+            if (tailorMadeAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(tailorMadeAssembly));
+            }
+
+            if (combinedAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(combinedAssembly));
+            }
+
+            FailureMechanismSection = failureMechanismSection;
+            SimpleAssembly = simpleAssembly;
+            DetailedAssembly = detailedAssembly;
+            TailorMadeAssembly = tailorMadeAssembly;
+            CombinedAssembly = combinedAssembly;
+        }
+
+        /// <summary>
+        /// Gets the failure mechanism section.
+        /// </summary>
+        public FailureMechanismSection FailureMechanismSection { get; }
+
+        /// <summary>
+        /// Gets the simple assembly result.
+        /// </summary>
+        public ExportableSectionAssemblyResult SimpleAssembly { get; }
+
+        /// <summary>
+        /// Gets the detailed assembly result.
+        /// </summary>
+        public ExportableSectionAssemblyResult DetailedAssembly { get; }
+
+        /// <summary>
+        /// Gets the tailor made assembly result.
+        /// </summary>
+        public ExportableSectionAssemblyResult TailorMadeAssembly { get; }
+
+        /// <summary>
+        /// Gets the combined assembly result.
+        /// </summary>
+        public ExportableSectionAssemblyResult CombinedAssembly { get; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableAggregatedFailureMechanismSectionAssemblyResultTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableAggregatedFailureMechanismSectionAssemblyResultTest
+    {
+        [Test]
+        public void Constructor_FailureMechanismSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new ExportableAggregatedFailureMechanismSectionAssemblyResult(null,
+                                                                                                    CreateSectionResult(),
+                                                                                                    CreateSectionResult(),
+                                                                                                    CreateSectionResult(),
+                                                                                                    CreateSectionResult());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanismSection", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_SimpleAssemblyNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new ExportableAggregatedFailureMechanismSectionAssemblyResult(
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(),
+                null,
+                CreateSectionResult(),
+                CreateSectionResult(),
+                CreateSectionResult());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("simpleAssembly", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_DetailedAssemblyNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new ExportableAggregatedFailureMechanismSectionAssemblyResult(
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(),
+                CreateSectionResult(),
+                null,
+                CreateSectionResult(),
+                CreateSectionResult());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("detailedAssembly", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_TailorMadeAssemblyNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new ExportableAggregatedFailureMechanismSectionAssemblyResult(
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(),
+                CreateSectionResult(),
+                CreateSectionResult(),
+                null,
+                CreateSectionResult());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("tailorMadeAssembly", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_CombinedAssemblyNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new ExportableAggregatedFailureMechanismSectionAssemblyResult(
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(),
+                CreateSectionResult(),
+                CreateSectionResult(),
+                CreateSectionResult(),
+                null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("combinedAssembly", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            FailureMechanismSection failureMechanismSection = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            ExportableSectionAssemblyResult simpleAssembly = CreateSectionResult();
+            ExportableSectionAssemblyResult detailedAssembly = CreateSectionResult();
+            ExportableSectionAssemblyResult tailorMadeAssembly = CreateSectionResult();
+            ExportableSectionAssemblyResult combinedAssembly = CreateSectionResult();
+
+            // Call
+            var assemblyResult = new ExportableAggregatedFailureMechanismSectionAssemblyResult(failureMechanismSection,
+                                                                                               simpleAssembly,
+                                                                                               detailedAssembly,
+                                                                                               tailorMadeAssembly,
+                                                                                               combinedAssembly);
+
+            // Assert
+            Assert.AreSame(failureMechanismSection, assemblyResult.FailureMechanismSection);
+            Assert.AreSame(simpleAssembly, assemblyResult.SimpleAssembly);
+            Assert.AreSame(detailedAssembly, assemblyResult.DetailedAssembly);
+            Assert.AreSame(tailorMadeAssembly, assemblyResult.TailorMadeAssembly);
+            Assert.AreSame(combinedAssembly, assemblyResult.CombinedAssembly);
+        }
+
+        private static ExportableSectionAssemblyResult CreateSectionResult()
+        {
+            var random = new Random(21);
+            return new ExportableSectionAssemblyResult(random.NextEnumValue<ExportableFailureMechanismSectionAssemblyMethod>(),
+                                                       random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+        }
+    }
+}
