7de4e692b62a78191a0a0076f70c5b7ceae3ea31
Dennis Tang <dennis.tang@deltares.nl>
2018-08-15 14:35:52 +0200

    Introduced object to hold failure mechanism specific assembly information:
    - introduced an object that represent the assembly result with probability of a failure mechanism itself.
    
    WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableFailureMechanismAssemblyResultWithProbability.cs
@@ -0,0 +1,29 @@
+﻿using Ringtoets.AssemblyTool.Data;
+
+namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Class that holds the information to export the assembly result with probability of a failure mechanism.
+    /// </summary>
+    public class ExportableFailureMechanismAssemblyResultWithProbability : ExportableFailureMechanismAssemblyResult
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExportableFailureMechanismAssemblyResultWithProbability"/>.
+        /// </summary>
+        /// <param name="assemblyMethod">The method that was used to assemble this result.</param>
+        /// <param name="assemblyCategory">The assembly result.</param>
+        /// <param name="probability">The probability of the assembly result.</param>
+        public ExportableFailureMechanismAssemblyResultWithProbability(ExportableFailureMechanismAssemblyMethod assemblyMethod,
+                                                                       FailureMechanismAssemblyCategoryGroup assemblyCategory,
+                                                                       double probability)
+            : base(assemblyMethod, assemblyCategory)
+        {
+            Probability = probability;
+        }
+
+        /// <summary>
+        /// Gets the probability.
+        /// </summary>
+        public double Probability { get; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableFailureMechanismAssemblyResultWithProbablityTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableFailureMechanismAssemblyResultWithProbablityTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            var assemblyMethod = random.NextEnumValue<ExportableFailureMechanismAssemblyMethod>();
+            var assemblyCategory = random.NextEnumValue<FailureMechanismAssemblyCategoryGroup>();
+            double probability = random.NextDouble();
+
+            // Call
+            var assemblyResult = new ExportableFailureMechanismAssemblyResultWithProbability(assemblyMethod, assemblyCategory, probability);
+
+            // Assert
+            Assert.IsInstanceOf<ExportableFailureMechanismAssemblyResult>(assemblyResult);
+            Assert.AreEqual(assemblyMethod, assemblyResult.AssemblyMethod);
+            Assert.AreEqual(assemblyCategory, assemblyResult.AssemblyCategory);
+            Assert.AreEqual(probability, assemblyResult.Probability);
+        }
+    }
+}
