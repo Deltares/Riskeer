e26a1087fab8d758f2c451a6ed79879c780acb56
Dennis Tang <dennis.tang@deltares.nl>
2018-08-15 11:17:35 +0200

    Introduced object to hold all assembly information:
    - Object represents a failure mechanism section result that has assembly results with a probability.
    
    WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/ExportableAggregatedFailureMechanismSectionAssemblyResultWithProbability.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO
+{
+    /// <summary>
+    /// Class that holds all the information to export a failure mechanism section assembly result with probability.
+    /// </summary>
+    public class ExportableAggregatedFailureMechanismSectionAssemblyResultWithProbability
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExportableAggregatedFailureMechanismSectionAssemblyResultWithProbability"/>.
+        /// </summary>
+        /// <param name="failureMechanismSection">The failure mechanism section.</param>
+        /// <param name="simpleAssembly">The simple assembly result of the failure mechanism section.</param>
+        /// <param name="detailedAssembly">The detailed assembly result of the failure mechanism section.</param>
+        /// <param name="tailorMadeAssembly">The tailor made assembly result of the failure mechanism section.</param>
+        /// <param name="combinedAssembly">The combined assembly result of the failure mechanism section.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null.</c></exception>
+        public ExportableAggregatedFailureMechanismSectionAssemblyResultWithProbability(FailureMechanismSection failureMechanismSection,
+                                                                                        ExportableSectionAssemblyResultWithProbability simpleAssembly,
+                                                                                        ExportableSectionAssemblyResultWithProbability detailedAssembly,
+                                                                                        ExportableSectionAssemblyResultWithProbability tailorMadeAssembly,
+                                                                                        ExportableSectionAssemblyResultWithProbability combinedAssembly)
+        {
+            if (failureMechanismSection == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSection));
+            }
+
+            if (simpleAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(simpleAssembly));
+            }
+
+            if (detailedAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(detailedAssembly));
+            }
+
+            if (tailorMadeAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(tailorMadeAssembly));
+            }
+
+            if (combinedAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(combinedAssembly));
+            }
+
+            FailureMechanismSection = failureMechanismSection;
+            SimpleAssembly = simpleAssembly;
+            DetailedAssembly = detailedAssembly;
+            TailorMadeAssembly = tailorMadeAssembly;
+            CombinedAssembly = combinedAssembly;
+        }
+
+        /// <summary>
+        /// Gets the failure mechanism section.
+        /// </summary>
+        public FailureMechanismSection FailureMechanismSection { get; }
+
+        /// <summary>
+        /// Gets the simple assembly result.
+        /// </summary>
+        public ExportableSectionAssemblyResultWithProbability SimpleAssembly { get; }
+
+        /// <summary>
+        /// Gets the detailed assembly result.
+        /// </summary>
+        public ExportableSectionAssemblyResultWithProbability DetailedAssembly { get; }
+
+        /// <summary>
+        /// Gets the tailor made assembly result.
+        /// </summary>
+        public ExportableSectionAssemblyResultWithProbability TailorMadeAssembly { get; }
+
+        /// <summary>
+        /// Gets the combined assembly result.
+        /// </summary>
+        public ExportableSectionAssemblyResultWithProbability CombinedAssembly { get; }
+    }
+}
