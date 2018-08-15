83c252ab2d908ee1e1d88037c398d668ed8fdf67
Dennis Tang <dennis.tang@deltares.nl>
2018-08-15 11:50:21 +0200

    Introduced object to hold all assembly information:
    - Introduced base class.
    
    WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableAggregatedFailureMechanismSectionAssemblyResultBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Base implementation to hold all information for exporting assembly results for a failure mechanism section.
+    /// </summary>
+    /// <typeparam name="TSectionAssemblyResult">The type of <see cref="ExportableSectionAssemblyResult"/> the results hold.</typeparam>
+    public abstract class ExportableAggregatedFailureMechanismSectionAssemblyResultBase<TSectionAssemblyResult>
+        where TSectionAssemblyResult : ExportableSectionAssemblyResult
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExportableAggregatedFailureMechanismSectionAssemblyResultBase{T}"/>.
+        /// </summary>
+        /// <param name="failureMechanismSection">The failure mechanism section.</param>
+        /// <param name="simpleAssembly">The simple assembly result of the failure mechanism section.</param>
+        /// <param name="tailorMadeAssembly">The tailor made assembly result of the failure mechanism section.</param>
+        /// <param name="combinedAssembly">The combined assembly result of the failure mechanism section.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null.</c></exception>
+        protected ExportableAggregatedFailureMechanismSectionAssemblyResultBase(FailureMechanismSection failureMechanismSection,
+                                                                                TSectionAssemblyResult simpleAssembly,
+                                                                                TSectionAssemblyResult tailorMadeAssembly,
+                                                                                TSectionAssemblyResult combinedAssembly)
+        {
+            if (failureMechanismSection == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSection));
+            }
+
+            if (simpleAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(simpleAssembly));
+            }
+
+            if (tailorMadeAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(tailorMadeAssembly));
+            }
+
+            if (combinedAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(combinedAssembly));
+            }
+
+            FailureMechanismSection = failureMechanismSection;
+            SimpleAssembly = simpleAssembly;
+            TailorMadeAssembly = tailorMadeAssembly;
+            CombinedAssembly = combinedAssembly;
+        }
+
+        /// <summary>
+        /// Gets the failure mechanism section.
+        /// </summary>
+        public FailureMechanismSection FailureMechanismSection { get; }
+
+        /// <summary>
+        /// Gets the simple assembly result.
+        /// </summary>
+        public TSectionAssemblyResult SimpleAssembly { get; }
+
+        /// <summary>
+        /// Gets the tailor made assembly result.
+        /// </summary>
+        public TSectionAssemblyResult TailorMadeAssembly { get; }
+
+        /// <summary>
+        /// Gets the combined assembly result.
+        /// </summary>
+        public TSectionAssemblyResult CombinedAssembly { get; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableAggregatedFailureMechanismSectionAssemblyResultBaseTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableAggregatedFailureMechanismSectionAssemblyResultBaseTest
+    {
+        [Test]
+        public void Constructor_FailureMechanismSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new TestExportableAggregatedFailureMechanismSectionAssemblyResultBase(null,
+                                                                                                            CreateSectionResult(),
+                                                                                                            CreateSectionResult(),
+                                                                                                            CreateSectionResult());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanismSection", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_SimpleAssemblyNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new TestExportableAggregatedFailureMechanismSectionAssemblyResultBase(
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(),
+                null,
+                CreateSectionResult(),
+                CreateSectionResult());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("simpleAssembly", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_TailorMadeAssemblyNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new TestExportableAggregatedFailureMechanismSectionAssemblyResultBase(
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(),
+                CreateSectionResult(),
+                null,
+                CreateSectionResult());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("tailorMadeAssembly", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_CombinedAssemblyNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new TestExportableAggregatedFailureMechanismSectionAssemblyResultBase(
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(),
+                CreateSectionResult(),
+                CreateSectionResult(),
+                null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("combinedAssembly", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            FailureMechanismSection failureMechanismSection = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            ExportableSectionAssemblyResult simpleAssembly = CreateSectionResult();
+            ExportableSectionAssemblyResult tailorMadeAssembly = CreateSectionResult();
+            ExportableSectionAssemblyResult combinedAssembly = CreateSectionResult();
+
+            // Call
+            var assemblyResult = new TestExportableAggregatedFailureMechanismSectionAssemblyResultBase(failureMechanismSection,
+                                                                                                       simpleAssembly,
+                                                                                                       tailorMadeAssembly,
+                                                                                                       combinedAssembly);
+
+            // Assert
+            Assert.AreSame(failureMechanismSection, assemblyResult.FailureMechanismSection);
+            Assert.AreSame(simpleAssembly, assemblyResult.SimpleAssembly);
+            Assert.AreSame(tailorMadeAssembly, assemblyResult.TailorMadeAssembly);
+            Assert.AreSame(combinedAssembly, assemblyResult.CombinedAssembly);
+        }
+
+        private static ExportableSectionAssemblyResult CreateSectionResult()
+        {
+            var random = new Random(21);
+            return new ExportableSectionAssemblyResult(random.NextEnumValue<ExportableFailureMechanismSectionAssemblyMethod>(),
+                                                       random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+        }
+
+        private class TestExportableAggregatedFailureMechanismSectionAssemblyResultBase
+            : ExportableAggregatedFailureMechanismSectionAssemblyResultBase<ExportableSectionAssemblyResult>
+        {
+            public TestExportableAggregatedFailureMechanismSectionAssemblyResultBase(FailureMechanismSection failureMechanismSection,
+                                                                                     ExportableSectionAssemblyResult simpleAssembly,
+                                                                                     ExportableSectionAssemblyResult tailorMadeAssembly,
+                                                                                     ExportableSectionAssemblyResult combinedAssembly)
+                : base(failureMechanismSection, simpleAssembly, tailorMadeAssembly, combinedAssembly) {}
+        }
+    }
+}
