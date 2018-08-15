72791f54c0d59551e9468ca28acc573bfd001486
Dennis Tang <dennis.tang@deltares.nl>
2018-08-15 14:27:07 +0200

    Introduced object to hold failure mechanism specific assembly information:
    - Introduced object that represents the assembly result of a failure mechanism itself.
    
    WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableFailureMechanismAssemblyResult.cs
@@ -0,0 +1,32 @@
+﻿using Ringtoets.AssemblyTool.Data;
+
+namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Class that holds the information to export the assembly result of a failure mechanism.
+    /// </summary>
+    public class ExportableFailureMechanismAssemblyResult
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExportableFailureMechanismAssemblyResult"/>.
+        /// </summary>
+        /// <param name="assemblyMethod">The method that was used to assemble this result.</param>
+        /// <param name="assemblyCategory">The assembly result.</param>
+        public ExportableFailureMechanismAssemblyResult(ExportableFailureMechanismAssemblyMethod assemblyMethod,
+                                                        FailureMechanismAssemblyCategoryGroup assemblyCategory)
+        {
+            AssemblyMethod = assemblyMethod;
+            AssemblyCategory = assemblyCategory;
+        }
+        
+        /// <summary>
+        /// Gets the assembly method that was used to assembly the assembly result.
+        /// </summary>
+        public ExportableFailureMechanismAssemblyMethod AssemblyMethod { get; }
+
+        /// <summary>
+        /// Gets the assembly category.
+        /// </summary>
+        public FailureMechanismAssemblyCategoryGroup AssemblyCategory { get; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableFailureMechanismAssemblyResultTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableFailureMechanismAssemblyResultTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            var assemblyMethod = random.NextEnumValue<ExportableFailureMechanismAssemblyMethod>();
+            var assemblyCategory = random.NextEnumValue<FailureMechanismAssemblyCategoryGroup>();
+
+            // Call
+            var assemblyResult = new ExportableFailureMechanismAssemblyResult(assemblyMethod, assemblyCategory);
+
+            // Assert
+            Assert.AreEqual(assemblyMethod, assemblyResult.AssemblyMethod);
+            Assert.AreEqual(assemblyCategory, assemblyResult.AssemblyCategory);
+        }
+    }
+}
