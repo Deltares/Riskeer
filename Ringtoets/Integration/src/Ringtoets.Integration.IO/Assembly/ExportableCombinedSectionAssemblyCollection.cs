9377aace11d9280d2eefbe64b004517c1731a8c9
Dennis Tang <dennis.tang@deltares.nl>
2018-08-16 15:27:27 +0200

    Introduced exportable object to represent a collection of combined section assemblies.  WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableCombinedSectionAssemblyCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Class that holds all the information to export a collection
+    /// of <see cref="ExportableCombinedSectionAssembly"/>.
+    /// </summary>
+    public class ExportableCombinedSectionAssemblyCollection
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExportableCombinedSectionAssembly"/>.
+        /// </summary>
+        /// <param name="sections">The sections belonging to this collection of <see cref="ExportableCombinedSectionAssembly"/>.</param>
+        /// <param name="combinedSectionAssemblyResults">The collection of <see cref="ExportableCombinedSectionAssembly"/>.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public ExportableCombinedSectionAssemblyCollection(IEnumerable<ExportableCombinedFailureMechanismSection> sections,
+                                                  IEnumerable<ExportableCombinedSectionAssembly> combinedSectionAssemblyResults)
+        {
+            if (sections == null)
+            {
+                throw new ArgumentNullException(nameof(sections));
+            }
+
+            if (combinedSectionAssemblyResults == null)
+            {
+                throw new ArgumentNullException(nameof(combinedSectionAssemblyResults));
+            }
+
+            Sections = sections;
+            CombinedSectionAssemblyResults = combinedSectionAssemblyResults;
+        }
+
+        /// <summary>
+        /// Gets the sections belonging to this collection of <see cref="ExportableCombinedSectionAssembly"/>.
+        /// </summary>
+        public IEnumerable<ExportableCombinedFailureMechanismSection> Sections { get; }
+
+        /// <summary>
+        /// Gets the collection of <see cref="ExportableCombinedSectionAssembly"/>.
+        /// </summary>
+        public IEnumerable<ExportableCombinedSectionAssembly> CombinedSectionAssemblyResults { get; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableCombinedSectionAssemblyCollectionTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableCombinedSectionAssemblyCollectionTest
+    {
+        [Test]
+        public void Constructor_SectionsNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new ExportableCombinedSectionAssemblyCollection(null,
+                                                                                      Enumerable.Empty<ExportableCombinedSectionAssembly>());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("sections", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_CombinedSectionAssemblyResultsNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new ExportableCombinedSectionAssemblyCollection(Enumerable.Empty<ExportableCombinedFailureMechanismSection>(),
+                                                                                      null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("combinedSectionAssemblyResults", exception.ParamName);
+        }
+
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            IEnumerable<ExportableCombinedFailureMechanismSection> sections = Enumerable.Empty<ExportableCombinedFailureMechanismSection>();
+            IEnumerable<ExportableCombinedSectionAssembly> combinedSectionAssemblyResults = Enumerable.Empty<ExportableCombinedSectionAssembly>();
+
+            // Call
+            var assembly = new ExportableCombinedSectionAssemblyCollection(sections, combinedSectionAssemblyResults);
+
+            // Assert
+            Assert.AreSame(sections, assembly.Sections);
+            Assert.AreSame(combinedSectionAssemblyResults, assembly.CombinedSectionAssemblyResults);
+        }
+    }
+}
