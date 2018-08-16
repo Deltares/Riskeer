ab7637423891ead19190ff8be30751f23c7b31d4
Dennis Tang <dennis.tang@deltares.nl>
2018-08-16 10:38:50 +0200

    Introduced exportable assessment section assembly:
    - Introduced object to hold the assembly result of an assessment section.
    
    WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableAssessmentSectionAssemblyResult.cs
@@ -0,0 +1,33 @@
+﻿using Ringtoets.AssemblyTool.Data;
+
+namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Class that holds all the information to export the assembly result
+    /// of an assessment section.
+    /// </summary>
+    public class ExportableAssessmentSectionAssemblyResult
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="ExportableAssessmentSectionAssemblyResult"/>.
+        /// </summary>
+        /// <param name="assemblyMethod">The method that was used to assemble this result.</param>
+        /// <param name="assemblyCategory">The assembly result.</param>
+        public ExportableAssessmentSectionAssemblyResult(ExportableAssemblyMethod assemblyMethod,
+                                                         AssessmentSectionAssemblyCategoryGroup assemblyCategory)
+        {
+            AssemblyMethod = assemblyMethod;
+            AssemblyCategory = assemblyCategory;
+        }
+
+        /// <summary>
+        /// Gets the assembly method that was used to assembly the assembly result.
+        /// </summary>
+        public ExportableAssemblyMethod AssemblyMethod { get; }
+
+        /// <summary>
+        /// Gets the assembly category.
+        /// </summary>
+        public AssessmentSectionAssemblyCategoryGroup AssemblyCategory { get; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableAssessmentSectionAssemblyResultTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableAssessmentSectionAssemblyResultTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            var assemblyMethod = random.NextEnumValue<ExportableAssemblyMethod>();
+            var category = random.NextEnumValue<AssessmentSectionAssemblyCategoryGroup>();
+
+            // Call
+            var assembly = new ExportableAssessmentSectionAssemblyResult(assemblyMethod, category);
+
+            // Assert
+            Assert.AreEqual(assemblyMethod, assembly.AssemblyMethod);
+            Assert.AreEqual(category, assembly.AssemblyCategory);
+        }
+    }
+}
