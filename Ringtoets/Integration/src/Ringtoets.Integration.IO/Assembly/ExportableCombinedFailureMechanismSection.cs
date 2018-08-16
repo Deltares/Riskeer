813dd2297b7f9945a005da0e3191dac27eee0cb5
Dennis Tang <dennis.tang@deltares.nl>
2018-08-16 08:57:44 +0200

    Introduced exportable combined section assembly:
    - Introduced a derivative failure mechanism section to keep track how a section was generated in case of a combined assembly result.
    
    WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Assembly/ExportableCombinedFailureMechanismSection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Common.Base.Geometry;
+
+namespace Ringtoets.Integration.IO.Assembly
+{
+    /// <summary>
+    /// Class that holds all the information to export a failure mechanism section
+    /// which is the result of an combined section assembly.
+    /// </summary>
+    public class ExportableCombinedFailureMechanismSection : ExportableFailureMechanismSection
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ExportableCombinedFailureMechanismSection"/>.
+        /// </summary>
+        /// <param name="geometry">The geometry of the failure mechanism section.</param>
+        /// <param name="startDistance">The start distance of the failure mechanism section between the section
+        /// and the start of the reference line.</param>
+        /// <param name="endDistance">The end distance of the failure mechanism section between the section
+        /// and the start of the reference line.</param>
+        /// <param name="assemblyMethod">The assembly method which was used to get this section.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="geometry"/>
+        /// is <c>null</c>.</exception>
+        public ExportableCombinedFailureMechanismSection(IEnumerable<Point2D> geometry,
+                                                         double startDistance,
+                                                         double endDistance,
+                                                         ExportableAssemblyMethod assemblyMethod)
+            : base(geometry, startDistance, endDistance)
+        {
+            AssemblyMethod = assemblyMethod;
+        }
+
+        /// <summary>
+        /// Gets the method that was used to get this section.
+        /// </summary>
+        public ExportableAssemblyMethod AssemblyMethod { get; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableCombinedFailureMechanismSectionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base.Geometry;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableCombinedFailureMechanismSectionTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            IEnumerable<Point2D> geometry = Enumerable.Empty<Point2D>();
+            var assemblyMethod = random.NextEnumValue<ExportableAssemblyMethod>();
+            double startDistance = random.NextDouble();
+            double endDistance = random.NextDouble();
+
+            // Call
+            var section = new ExportableCombinedFailureMechanismSection(geometry, startDistance, endDistance, assemblyMethod);
+
+            // Assert
+            Assert.IsInstanceOf<ExportableFailureMechanismSection>(section);
+
+            Assert.AreSame(geometry, section.Geometry);
+            Assert.AreEqual(startDistance, section.StartDistance);
+            Assert.AreEqual(endDistance, section.EndDistance);
+            Assert.AreEqual(assemblyMethod, section.AssemblyMethod);
+        }
+    }
+}
