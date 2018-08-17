3626842af8fe45995839d4bffeb820251334bc77
Dennis Tang <dennis.tang@deltares.nl>
2018-08-17 09:02:57 +0200

    Introduced factory to create exportable AssessmentSection WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Factories/ExportableAssessmentSectionFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Factories
+{
+    /// <summary>
+    /// Factory to create instances of <see cref="ExportableAssessmentSection"/>
+    /// with assembly results.
+    /// </summary>
+    public static class ExportableAssessmentSectionFactory
+    {
+        /// <summary>
+        /// Creates an <see cref="ExportableAssessmentSection"/> with assembly results
+        /// based on <paramref name="assessmentSection"/>.
+        /// </summary>
+        /// <param name="assessmentSection">The <see cref="AssessmentSection"/> to create
+        /// a <see cref="ExportableAssessmentSection"/> for.</param>
+        /// <returns>A <see cref="ExportableAssessmentSection"/> with assembly results.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="assessmentSection"/>
+        /// is <c>null</c>.</exception>
+        public static ExportableAssessmentSection CreateExportableAssessmentSection(AssessmentSection assessmentSection)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            return new ExportableAssessmentSection(assessmentSection.Name,
+                                                   assessmentSection.ReferenceLine.Points,
+                                                   new ExportableAssessmentSectionAssemblyResult(ExportableAssemblyMethod.WBI0A1, AssessmentSectionAssemblyCategoryGroup.A),
+                                                   Enumerable.Empty<ExportableFailureMechanism<ExportableFailureMechanismAssemblyResultWithProbability>>(),
+                                                   Enumerable.Empty<ExportableFailureMechanism<ExportableFailureMechanismAssemblyResult>>(),
+                                                   new ExportableCombinedSectionAssemblyCollection(Enumerable.Empty<ExportableCombinedFailureMechanismSection>(),
+                                                                                                   Enumerable.Empty<ExportableCombinedSectionAssembly>()));
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Factories/ExportableAssessmentSectionFactoryTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Core.Common.Base.Geometry;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.IO.Assembly;
+using Ringtoets.Integration.IO.Factories;
+
+namespace Ringtoets.Integration.IO.Test.Factories
+{
+    [TestFixture]
+    public class ExportableAssessmentSectionFactoryTest
+    {
+        [Test]
+        public void CreateExportableAssessmentSection_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => ExportableAssessmentSectionFactory.CreateExportableAssessmentSection(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateExportableAssessmentSection_WithAssessmentSection_ReturnsExpectedValues()
+        {
+            // Setup
+            const string name = "assessmentSectionName";
+
+            var referenceLine = new ReferenceLine();
+            referenceLine.SetGeometry(new[]
+            {
+                new Point2D(1, 1),
+                new Point2D(2, 2)
+            });
+
+            var random = new Random(21);
+            var assessmentSection = new AssessmentSection(random.NextEnumValue<AssessmentSectionComposition>())
+            {
+                Name = name,
+                ReferenceLine = referenceLine
+            };
+
+            // Call
+            ExportableAssessmentSection exportableAssessmentSection = ExportableAssessmentSectionFactory.CreateExportableAssessmentSection(assessmentSection);
+
+            // Assert
+            Assert.AreEqual(name, exportableAssessmentSection.Name);
+            CollectionAssert.AreEqual(referenceLine.Points, exportableAssessmentSection.Geometry);
+            CollectionAssert.IsEmpty(exportableAssessmentSection.FailureMechanismsWithProbability);
+            CollectionAssert.IsEmpty(exportableAssessmentSection.FailureMechanismsWithoutProbability);
+            Assert.IsNotNull(exportableAssessmentSection.CombinedSectionAssemblyResults);
+            Assert.IsNotNull(exportableAssessmentSection.AssessmentSectionAssembly);
+        }
+    }
+}
