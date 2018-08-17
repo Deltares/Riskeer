85059e781f7f4982909f6acf6fca8bc6c618434c
Dennis Tang <dennis.tang@deltares.nl>
2018-08-17 15:58:24 +0200

    Introduced factory to create a ExportableSectionAssembly based on FailureMechanismAssembly. WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Factories/ExportableSectionAssemblyResultFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Factories
+{
+    /// <summary>
+    /// Factory to create instances of <see cref="ExportableSectionAssemblyResult"/>.
+    /// </summary>
+    public static class ExportableSectionAssemblyResultFactory
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="ExportableSectionAssemblyResultWithProbability"/>
+        /// based on the <paramref name="failureMechanismSectionAssembly"/>.
+        /// </summary>
+        /// <param name="failureMechanismSectionAssembly"></param>
+        /// <param name="assemblyMethod">The assembly method <see cref="ExportableAssemblyMethod"/>
+        /// which was used to generate the result.</param>
+        /// <returns>A <see cref="ExportableSectionAssemblyResultWithProbability"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="failureMechanismSectionAssembly"/>
+        /// is <c>null</c>.</exception>
+        public static ExportableSectionAssemblyResultWithProbability CreateExportableSectionAssemblyResultWithProbability(
+            FailureMechanismSectionAssembly failureMechanismSectionAssembly,
+            ExportableAssemblyMethod assemblyMethod)
+        {
+            if (failureMechanismSectionAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSectionAssembly));
+            }
+
+            return new ExportableSectionAssemblyResultWithProbability(assemblyMethod,
+                                                                      failureMechanismSectionAssembly.Group,
+                                                                      failureMechanismSectionAssembly.Probability);
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Factories/ExportableSectionAssemblyResultFactoryTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Integration.IO.Assembly;
+using Ringtoets.Integration.IO.Factories;
+
+namespace Ringtoets.Integration.IO.Test.Factories
+{
+    [TestFixture]
+    public class ExportableSectionAssemblyResultFactoryTest
+    {
+        [Test]
+        public void CreateExportableSectionAssemblyResultWithProbability_FailureMechanismSectionAssemblyNull_ThrowsArgumentNullException()
+        {
+            // Setiup
+            var random = new Random(21);
+            var assemblyMethod = random.NextEnumValue<ExportableAssemblyMethod>();
+
+            // Call
+            TestDelegate call = () => ExportableSectionAssemblyResultFactory.CreateExportableSectionAssemblyResultWithProbability(null, assemblyMethod);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanismSectionAssembly", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateExportableSectionAssemblyResultWithProbability_WithFailureMechanismAssembly_ReturnsExportableAssemblyResultWithProbability()
+        {
+            // Setup
+            var random = new Random(21);
+            var assembly = new FailureMechanismSectionAssembly(random.NextDouble(),
+                                                               random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+            var assemblyMethod = random.NextEnumValue<ExportableAssemblyMethod>();
+
+            // Call
+            ExportableSectionAssemblyResultWithProbability exportableAssemblyResult = 
+                ExportableSectionAssemblyResultFactory.CreateExportableSectionAssemblyResultWithProbability(assembly, assemblyMethod);
+
+            // Assert
+            Assert.AreEqual(assembly.Group, exportableAssemblyResult.AssemblyCategory);
+            Assert.AreEqual(assembly.Probability, exportableAssemblyResult.Probability);
+            Assert.AreEqual(assemblyMethod, exportableAssemblyResult.AssemblyMethod);
+        }
+    }
+}
