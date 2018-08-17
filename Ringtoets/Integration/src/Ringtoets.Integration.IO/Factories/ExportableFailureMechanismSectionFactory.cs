fe7f036a5bea0c75556c1f1f8d1d8c422acf5177
Dennis Tang <dennis.tang@deltares.nl>
2018-08-17 16:11:43 +0200

    Introduced a factory to create ExportableFailureMechanismSection. WTI-1790

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Factories/ExportableFailureMechanismSectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Factories
+{
+    /// <summary>
+    /// Factory to create instance of <see cref="ExportableAssessmentSection"/>.
+    /// </summary>
+    public static class ExportableFailureMechanismSectionFactory
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="ExportableFailureMechanismSection"/> based on
+        /// <paramref name="failureMechanismSection"/>.
+        /// </summary>
+        /// <param name="failureMechanismSection">The <see cref="FailureMechanismSection"/>
+        /// to create a <see cref="ExportableFailureMechanismSection"/> for.</param>
+        /// <returns>A <see cref="ExportableFailureMechanismSection"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="failureMechanismSection"/>
+        /// is <c>null</c>.</exception>
+        public static ExportableFailureMechanismSection CreateExportableFailureMechanismSection(FailureMechanismSection failureMechanismSection)
+        {
+            if (failureMechanismSection == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSection));
+            }
+
+            return new ExportableFailureMechanismSection(failureMechanismSection.Points, double.NaN, double.NaN);
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Factories/ExportableFailureMechanismSectionFactoryTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Core.Common.Base.Geometry;
+using NUnit.Framework;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Integration.IO.Assembly;
+using Ringtoets.Integration.IO.Factories;
+
+namespace Ringtoets.Integration.IO.Test.Factories
+{
+    [TestFixture]
+    public class ExportableFailureMechanismSectionFactoryTest
+    {
+        [Test]
+        public void CreateExportableFailureMechanismSection_FailureMechanismSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => ExportableFailureMechanismSectionFactory.CreateExportableFailureMechanismSection(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanismSection", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateExportableFailureMechanismSection_WithFailureMechanismSection_ReturnExportableFailureMechanismSection()
+        {
+            // Setup
+            FailureMechanismSection failureMechanismSection = FailureMechanismSectionTestFactory.CreateFailureMechanismSection(new[]
+            {
+                new Point2D(1, 1)
+            });
+
+            // Call
+            ExportableFailureMechanismSection exportableFailureMechanismSection =
+                ExportableFailureMechanismSectionFactory.CreateExportableFailureMechanismSection(failureMechanismSection);
+
+            // Assert
+            Assert.AreSame(failureMechanismSection.Points, exportableFailureMechanismSection.Geometry);
+            Assert.IsNaN(exportableFailureMechanismSection.StartDistance);
+            Assert.IsNaN(exportableFailureMechanismSection.EndDistance);
+        }
+    }
+}
