65b61354ce27f254a2f20c56969a4c340747354c
Dennis Tang <dennis.tang@deltares.nl>
2018-08-21 13:30:36 +0200

    Introduced factory method to create collections of exportable failure mechanism section results. WTI-1790

--- Ringtoets/Integration/src/Ringtoets.Integration.IO/Factories/ExportableFailureMechanismSectionFactory.cs
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Factories/ExportableFailureMechanismSectionFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Core.Common.Base.Geometry;
 using Ringtoets.Common.Data.FailureMechanism;
 using Ringtoets.Integration.IO.Assembly;
 
@@ -26,6 +28,36 @@ namespace Ringtoets.Integration.IO.Factories
             }
 
             return new ExportableFailureMechanismSection(failureMechanismSection.Points, double.NaN, double.NaN);
+        }
+
+        /// <summary>
+        /// Creates a collection of <see cref="ExportableFailureMechanismSection"/> based on <paramref name="failureMechanismSections"/>.
+        /// </summary>
+        /// <param name="failureMechanismSections">The collection of <see cref="FailureMechanismSection"/>
+        /// to create a collection of <see cref="ExportableFailureMechanismSection"/> for.</param>
+        /// <returns>A collection of <see cref="ExportableFailureMechanismSection"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="failureMechanismSections"/> is <c>null</c>.</exception>
+        public static IEnumerable<ExportableFailureMechanismSection> CreateExportableFailureMechanismSections(IEnumerable<FailureMechanismSection> failureMechanismSections)
+        {
+            if (failureMechanismSections == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSections));
+            }
+
+            var exportableFailureMechanismSections = new List<ExportableFailureMechanismSection>();
+
+            double startDistance = 0;
+            foreach (FailureMechanismSection section in failureMechanismSections)
+            {
+                double endDistance = startDistance + Math2D.Length(section.Points);
+
+                exportableFailureMechanismSections.Add(new ExportableFailureMechanismSection(section.Points,
+                                                                                             startDistance,
+                                                                                             endDistance));
+                startDistance = endDistance;
+            }
+
+            return exportableFailureMechanismSections;
         }
     }
 }
--- Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Factories/ExportableFailureMechanismSectionFactoryTest.cs
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Factories/ExportableFailureMechanismSectionFactoryTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Core.Common.Base.Geometry;
 using NUnit.Framework;
 using Ringtoets.Common.Data.FailureMechanism;
@@ -11,6 +13,74 @@ namespace Ringtoets.Integration.IO.Test.Factories
     [TestFixture]
     public class ExportableFailureMechanismSectionFactoryTest
     {
+        [Test]
+        public void CreateExportableFailureMechanismSections_FailureMechanismSectionsNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => ExportableFailureMechanismSectionFactory.CreateExportableFailureMechanismSections(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanismSections", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateExportableFailureMechanismSections_WithEmptyFailureMechanismSections_ReturnsEmptyCollection()
+        {
+            // Call
+            IEnumerable<ExportableFailureMechanismSection> exportableFailureMechanismSections =
+                ExportableFailureMechanismSectionFactory.CreateExportableFailureMechanismSections(Enumerable.Empty<FailureMechanismSection>());
+
+            // Assert
+            CollectionAssert.IsEmpty(exportableFailureMechanismSections);
+        }
+
+        [Test]
+        public void CreateExportableFailureMechanismSections_WithFailureMechanismSections_ReturnsExportableFailureMechanismSections()
+        {
+            // Setup
+            FailureMechanismSection[] failureMechanismSections =
+            {
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(new[]
+                {
+                    new Point2D(0, 0),
+                    new Point2D(0, 10)
+                }),
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(new[]
+                {
+                    new Point2D(0, 10),
+                    new Point2D(0, 20)
+                }),
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection(new[]
+                {
+                    new Point2D(0, 20),
+                    new Point2D(0, 40)
+                })
+            };
+
+            // Call
+            IEnumerable<ExportableFailureMechanismSection> exportableFailureMechanismSections =
+                ExportableFailureMechanismSectionFactory.CreateExportableFailureMechanismSections(failureMechanismSections);
+
+            // Assert
+            Assert.AreEqual(3, exportableFailureMechanismSections.Count());
+
+            ExportableFailureMechanismSection firstExportableSection = exportableFailureMechanismSections.First();
+            Assert.AreSame(failureMechanismSections[0].Points, firstExportableSection.Geometry);
+            Assert.AreEqual(0, firstExportableSection.StartDistance);
+            Assert.AreEqual(10, firstExportableSection.EndDistance);
+
+            ExportableFailureMechanismSection secondExportableSection = exportableFailureMechanismSections.ElementAt(1);
+            Assert.AreSame(failureMechanismSections[1].Points, secondExportableSection.Geometry);
+            Assert.AreEqual(10, secondExportableSection.StartDistance);
+            Assert.AreEqual(20, secondExportableSection.EndDistance);
+
+            ExportableFailureMechanismSection thirdExportableSection = exportableFailureMechanismSections.ElementAt(2);
+            Assert.AreSame(failureMechanismSections[2].Points, thirdExportableSection.Geometry);
+            Assert.AreEqual(20, thirdExportableSection.StartDistance);
+            Assert.AreEqual(40, thirdExportableSection.EndDistance);
+        }
+
         [Test]
         public void CreateExportableFailureMechanismSection_FailureMechanismSectionNull_ThrowsArgumentNullException()
         {
