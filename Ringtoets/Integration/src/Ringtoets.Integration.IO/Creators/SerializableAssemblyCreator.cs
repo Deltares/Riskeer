0a9f06e0327c4b0dafad250ab516d375701d231e
Dennis Tang <dennis.tang@deltares.nl>
2018-08-24 11:33:36 +0200

    Introduced creator to create the root node SerializableAssembly:
    - Initial commit, creator is filled with empty data
    
    WTI-1803

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Creators/SerializableAssemblyCreator.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base.Geometry;
+using Ringtoets.AssemblyTool.IO.Model;
+using Ringtoets.Integration.IO.Assembly;
+using Ringtoets.Integration.IO.Helpers;
+
+namespace Ringtoets.Integration.IO.Creators
+{
+    /// <summary>
+    /// Creator class which creates instances of <see cref="SerializableAssembly"/>.
+    /// </summary>
+    public static class SerializableAssemblyCreator
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="SerializableAssembly"/> based
+        /// on <paramref name="assessmentSection"/>.
+        /// </summary>
+        /// <param name="assessmentSection">The <see cref="ExportableAssessmentSection"/>
+        /// to create a <see cref="SerializableAssembly"/> for.</param>
+        /// <returns>A <see cref="SerializableAssembly"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="assessmentSection"/>
+        /// is <c>null</c>.</exception>
+        public static SerializableAssembly Create(ExportableAssessmentSection assessmentSection)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            var idGenerator = new UniqueIdentifierGenerator();
+            string serializableAssemblyId = idGenerator.GetNewId().ToString();
+
+            return new SerializableAssembly(serializableAssemblyId,
+                                            GetLowerCorner(assessmentSection.Geometry),
+                                            GetUpperCorner(assessmentSection.Geometry),
+                                            new SerializableAssessmentSection(),
+                                            new SerializableAssessmentProcess(),
+                                            new SerializableTotalAssemblyResult(),
+                                            Enumerable.Empty<SerializableFailureMechanism>(),
+                                            Enumerable.Empty<SerializableFailureMechanismSectionAssembly>(),
+                                            Enumerable.Empty<SerializableCombinedFailureMechanismSectionAssembly>(),
+                                            Enumerable.Empty<SerializableFailureMechanismSectionCollection>(),
+                                            Enumerable.Empty<SerializableFailureMechanismSection>());
+        }
+
+        private static Point2D GetLowerCorner(IEnumerable<Point2D> geometry)
+        {
+            return new Point2D(geometry.Select(p => p.X).Min(),
+                               geometry.Select(p => p.Y).Min());
+        }
+
+        private static Point2D GetUpperCorner(IEnumerable<Point2D> geometry)
+        {
+            return new Point2D(geometry.Select(p => p.X).Max(),
+                               geometry.Select(p => p.Y).Max());
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Creators/SerializableAssemblyCreatorTest.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base.Geometry;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.IO.Model;
+using Ringtoets.AssemblyTool.IO.Model.Gml;
+using Ringtoets.AssemblyTool.IO.Model.Helpers;
+using Ringtoets.Integration.IO.Assembly;
+using Ringtoets.Integration.IO.Creators;
+using Ringtoets.Integration.IO.TestUtil;
+
+namespace Ringtoets.Integration.IO.Test.Creators
+{
+    [TestFixture]
+    public class SerializableAssemblyCreatorTest
+    {
+        [Test]
+        public void Create_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => SerializableAssemblyCreator.Create(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateSerializableAssembly_WithValidArguments_ReturnsSerializableAssembly()
+        {
+            // Setup
+            const string assessmentSectionName = "assessmentSectionName";
+
+            IEnumerable<Point2D> geometry = CreateGeometry();
+            ExportableAssessmentSectionAssemblyResult assessmentSectionAssembly =
+                ExportableAssessmentSectionAssemblyResultTestFactory.CreateResult();
+            ExportableFailureMechanismAssemblyResultWithProbability failureMechanismAssemblyResultWithProbability =
+                ExportableFailureMechanismAssemblyResultTestFactory.CreateResultWithProbability();
+            ExportableFailureMechanismAssemblyResult failureMechanismAssemblyResultWithoutProbability =
+                ExportableFailureMechanismAssemblyResultTestFactory.CreateResultWithoutProbability();
+            IEnumerable<ExportableFailureMechanism<ExportableFailureMechanismAssemblyResultWithProbability>> failureMechanismsWithProbability =
+                Enumerable.Empty<ExportableFailureMechanism<ExportableFailureMechanismAssemblyResultWithProbability>>();
+            IEnumerable<ExportableFailureMechanism<ExportableFailureMechanismAssemblyResult>> failureMechanismsWithoutProbability =
+                Enumerable.Empty<ExportableFailureMechanism<ExportableFailureMechanismAssemblyResult>>();
+            ExportableCombinedSectionAssemblyCollection combinedSectionAssemblyResults = CreateCombinedSectionAssemblyCollection();
+
+            var exportableAssessmentSection = new ExportableAssessmentSection(assessmentSectionName,
+                                                                              geometry,
+                                                                              assessmentSectionAssembly,
+                                                                              failureMechanismAssemblyResultWithProbability,
+                                                                              failureMechanismAssemblyResultWithoutProbability,
+                                                                              failureMechanismsWithProbability,
+                                                                              failureMechanismsWithoutProbability,
+                                                                              combinedSectionAssemblyResults);
+
+            // Call
+            SerializableAssembly serializableAssembly = SerializableAssemblyCreator.Create(exportableAssessmentSection);
+
+            // Assert
+            Assert.AreEqual("0", serializableAssembly.Id);
+            AssertSerializableBoundary(exportableAssessmentSection.Geometry, serializableAssembly.Boundary);
+            Assert.AreEqual(3, serializableAssembly.FeatureMembers.Length);
+        }
+
+        private static IEnumerable<Point2D> CreateGeometry()
+        {
+            return new[]
+            {
+                new Point2D(1, 1),
+                new Point2D(4, 4),
+                new Point2D(5, -1)
+            };
+        }
+
+        private static ExportableCombinedSectionAssemblyCollection CreateCombinedSectionAssemblyCollection()
+        {
+            return new ExportableCombinedSectionAssemblyCollection(Enumerable.Empty<ExportableCombinedFailureMechanismSection>(),
+                                                                   Enumerable.Empty<ExportableCombinedSectionAssembly>());
+        }
+
+        private static void AssertSerializableBoundary(IEnumerable<Point2D> geometry,
+                                                       SerializableBoundary actualBoundary)
+        {
+            var expectedLowerCorner = new Point2D(geometry.Select(p => p.X).Min(),
+                                                  geometry.Select(p => p.Y).Min());
+
+            var expectedUpperCorner = new Point2D(geometry.Select(p => p.X).Max(),
+                                                  geometry.Select(p => p.Y).Max());
+
+            string expectedLowerCornerFormat = GeometrySerializationFormatter.Format(expectedLowerCorner);
+            string expectedUpperCornerFormat = GeometrySerializationFormatter.Format(expectedUpperCorner);
+
+            SerializableEnvelope envelope = actualBoundary.Envelope;
+            Assert.AreEqual(expectedLowerCornerFormat, envelope.LowerCorner);
+            Assert.AreEqual(expectedUpperCornerFormat, envelope.UpperCorner);
+        }
+    }
+}
