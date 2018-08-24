b1d9204ba99370e35846e831d8ea7cdffa25f836
Robert Tangerman <robert.tangerman@deltares.nl>
2018-08-24 11:02:26 +0200

    Improvements after review. WTI-1715

--- Ringtoets/GrassCoverErosionInwards/src/Ringtoets.GrassCoverErosionInwards.IO/Configurations/GrassCoverErosionInwardsCalculationConfigurationWriter.cs
+++ Ringtoets/GrassCoverErosionInwards/src/Ringtoets.GrassCoverErosionInwards.IO/Configurations/GrassCoverErosionInwardsCalculationConfigurationWriter.cs
@@ -53,7 +53,7 @@ namespace Ringtoets.GrassCoverErosionInwards.IO.Configurations
 
             WriteElementWhenContentAvailable(
                 writer,
-                ConfigurationSchemaIdentifiers.HydraulicBoundaryLocationElementOld,
+                ConfigurationSchemaIdentifiers.HydraulicBoundaryLocationElementNew,
                 configuration.HydraulicBoundaryLocationName);
 
             WriteElementWhenContentAvailable(
