2e1a9648c3c88c68c7cdf3826e18c3cfedc11af0
Dennis Tang <dennis.tang@deltares.nl>
2018-08-27 09:43:48 +0200

    Introduced creators to convert RT enums to SerializableEnums:
    - Introduced creator to convert the assembly methods.
    
    WTI-1803

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Creators/SerializableAssemblyMethodCreator.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using Ringtoets.AssemblyTool.IO.Model.Enums;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Creators
+{
+    /// <summary>
+    /// Creator to create <see cref="SerializableAssemblyMethod"/>.
+    /// </summary>
+    public static class SerializableAssemblyMethodCreator
+    {
+        /// <summary>
+        /// Creates a <see cref="SerializableAssemblyMethod"/> based on
+        /// <paramref name="assemblyMethod"/>.
+        /// </summary>
+        /// <param name="assemblyMethod">The <see cref="ExportableAssemblyMethod"/>
+        /// to create a <see cref="SerializableAssemblyMethod"/> for.</param>
+        /// <returns>A <see cref="SerializableAssemblyMethod"/>.</returns>
+        /// <exception cref="InvalidEnumArgumentException">Thrown when <paramref name="assemblyMethod"/>
+        /// is an invalid value.</exception>
+        /// <exception cref="NotSupportedException">Thrown when <paramref name="assemblyMethod"/>
+        /// is a valid value, but unsupported.</exception>
+        public static SerializableAssemblyMethod Create(ExportableAssemblyMethod assemblyMethod)
+        {
+            if (!Enum.IsDefined(typeof(ExportableAssemblyMethod), assemblyMethod))
+            {
+                throw new InvalidEnumArgumentException(nameof(assemblyMethod),
+                                                       (int) assemblyMethod,
+                                                       typeof(ExportableAssemblyMethod));
+            }
+
+            switch (assemblyMethod)
+            {
+                case ExportableAssemblyMethod.WBI0E1:
+                    return SerializableAssemblyMethod.WBI0E1;
+                case ExportableAssemblyMethod.WBI0E3:
+                    return SerializableAssemblyMethod.WBI0E3;
+                case ExportableAssemblyMethod.WBI0G1:
+                    return SerializableAssemblyMethod.WBI0G1;
+                case ExportableAssemblyMethod.WBI0G3:
+                    return SerializableAssemblyMethod.WBI0G3;
+                case ExportableAssemblyMethod.WBI0G4:
+                    return SerializableAssemblyMethod.WBI0G4;
+                case ExportableAssemblyMethod.WBI0G5:
+                    return SerializableAssemblyMethod.WBI0G5;
+                case ExportableAssemblyMethod.WBI0G6:
+                    return SerializableAssemblyMethod.WBI0G6;
+                case ExportableAssemblyMethod.WBI0T1:
+                    return SerializableAssemblyMethod.WBI0T1;
+                case ExportableAssemblyMethod.WBI0T3:
+                    return SerializableAssemblyMethod.WBI0T3;
+                case ExportableAssemblyMethod.WBI0T4:
+                    return SerializableAssemblyMethod.WBI0T4;
+                case ExportableAssemblyMethod.WBI0T5:
+                    return SerializableAssemblyMethod.WBI0T5;
+                case ExportableAssemblyMethod.WBI0T6:
+                    return SerializableAssemblyMethod.WBI0T6;
+                case ExportableAssemblyMethod.WBI0T7:
+                    return SerializableAssemblyMethod.WBI0T7;
+                case ExportableAssemblyMethod.WBI0A1:
+                    return SerializableAssemblyMethod.WBI0A1;
+                case ExportableAssemblyMethod.WBI1A1:
+                    return SerializableAssemblyMethod.WBI1A1;
+                case ExportableAssemblyMethod.WBI1B1:
+                    return SerializableAssemblyMethod.WBI1B1;
+                case ExportableAssemblyMethod.WBI2A1:
+                    return SerializableAssemblyMethod.WBI2A1;
+                case ExportableAssemblyMethod.WBI2B1:
+                    return SerializableAssemblyMethod.WBI2B1;
+                case ExportableAssemblyMethod.WBI2C1:
+                    return SerializableAssemblyMethod.WBI2C1;
+                case ExportableAssemblyMethod.WBI3A1:
+                    return SerializableAssemblyMethod.WBI3A1;
+                case ExportableAssemblyMethod.WBI3B1:
+                    return SerializableAssemblyMethod.WBI3B1;
+                case ExportableAssemblyMethod.WBI3C1:
+                    return SerializableAssemblyMethod.WBI3C1;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Creators/SerializableAssemblyMethodCreatorTest.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.ComponentModel;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.IO.Model.Enums;
+using Ringtoets.Integration.IO.Assembly;
+using Ringtoets.Integration.IO.Creators;
+
+namespace Ringtoets.Integration.IO.Test.Creators
+{
+    [TestFixture]
+    public class SerializableAssemblyMethodCreatorTest
+    {
+        [Test]
+        public void Create_InvalidAssemblyMethod_ThrowInvalidEnumArgumentException()
+        {
+            // Setup
+            const ExportableAssemblyMethod assemblyMethod = (ExportableAssemblyMethod) 999;
+
+            // Call
+            TestDelegate call = () => SerializableAssemblyMethodCreator.Create(assemblyMethod);
+
+            // Assert
+            string message = $"The value of argument 'assemblyMethod' ({(int) assemblyMethod}) is invalid for Enum type '{nameof(ExportableAssemblyMethod)}'.";
+            var exception = TestHelper.AssertThrowsArgumentExceptionAndTestMessage<InvalidEnumArgumentException>(call, message);
+            Assert.AreEqual("assemblyMethod", exception.ParamName);
+        }
+
+        [Test]
+        [TestCase(ExportableAssemblyMethod.WBI0E1, SerializableAssemblyMethod.WBI0E1)]
+        [TestCase(ExportableAssemblyMethod.WBI0E3, SerializableAssemblyMethod.WBI0E3)]
+        [TestCase(ExportableAssemblyMethod.WBI0G1, SerializableAssemblyMethod.WBI0G1)]
+        [TestCase(ExportableAssemblyMethod.WBI0G3, SerializableAssemblyMethod.WBI0G3)]
+        [TestCase(ExportableAssemblyMethod.WBI0G4, SerializableAssemblyMethod.WBI0G4)]
+        [TestCase(ExportableAssemblyMethod.WBI0G5, SerializableAssemblyMethod.WBI0G5)]
+        [TestCase(ExportableAssemblyMethod.WBI0G6, SerializableAssemblyMethod.WBI0G6)]
+        [TestCase(ExportableAssemblyMethod.WBI0T1, SerializableAssemblyMethod.WBI0T1)]
+        [TestCase(ExportableAssemblyMethod.WBI0T3, SerializableAssemblyMethod.WBI0T3)]
+        [TestCase(ExportableAssemblyMethod.WBI0T4, SerializableAssemblyMethod.WBI0T4)]
+        [TestCase(ExportableAssemblyMethod.WBI0T5, SerializableAssemblyMethod.WBI0T5)]
+        [TestCase(ExportableAssemblyMethod.WBI0T6, SerializableAssemblyMethod.WBI0T6)]
+        [TestCase(ExportableAssemblyMethod.WBI0T7, SerializableAssemblyMethod.WBI0T7)]
+        [TestCase(ExportableAssemblyMethod.WBI0A1, SerializableAssemblyMethod.WBI0A1)]
+        [TestCase(ExportableAssemblyMethod.WBI1A1, SerializableAssemblyMethod.WBI1A1)]
+        [TestCase(ExportableAssemblyMethod.WBI1B1, SerializableAssemblyMethod.WBI1B1)]
+        [TestCase(ExportableAssemblyMethod.WBI2A1, SerializableAssemblyMethod.WBI2A1)]
+        [TestCase(ExportableAssemblyMethod.WBI2B1, SerializableAssemblyMethod.WBI2B1)]
+        [TestCase(ExportableAssemblyMethod.WBI2C1, SerializableAssemblyMethod.WBI2C1)]
+        [TestCase(ExportableAssemblyMethod.WBI3A1, SerializableAssemblyMethod.WBI3A1)]
+        [TestCase(ExportableAssemblyMethod.WBI3B1, SerializableAssemblyMethod.WBI3B1)]
+        [TestCase(ExportableAssemblyMethod.WBI3C1, SerializableAssemblyMethod.WBI3C1)]
+        public void Create_WithAssemblyMethod_ReturnsExpectedValues(ExportableAssemblyMethod assemblyMethod,
+                                                                    SerializableAssemblyMethod expectedAssemblyMethod)
+        {
+            // Call
+            SerializableAssemblyMethod serializableAssemblyMethod = SerializableAssemblyMethodCreator.Create(assemblyMethod);
+
+            // Assert
+            Assert.AreEqual(expectedAssemblyMethod, serializableAssemblyMethod);
+        }
+    }
+}
