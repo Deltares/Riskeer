597dfc8dd0177c9739b61f0fc6a983306b0d3aff
Dennis Tang <dennis.tang@deltares.nl>
2018-08-28 15:28:07 +0200

    Introduced creator to create failure mechanism section results WTI-1803

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/Creators/SerializableFailureMechanismSectionAssemblyResultCreator.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Ringtoets.AssemblyTool.IO.Model.DataTypes;
+using Ringtoets.AssemblyTool.IO.Model.Enums;
+using Ringtoets.Integration.IO.Assembly;
+
+namespace Ringtoets.Integration.IO.Creators
+{
+    /// <summary>
+    /// Creator to create instances of <see cref="SerializableFailureMechanismSectionAssemblyResult"/>
+    /// </summary>
+    public static class SerializableFailureMechanismSectionAssemblyResultCreator
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="SerializableFailureMechanismSectionAssemblyResult"/>
+        /// based on a <see cref="ExportableSectionAssemblyResult"/>.
+        /// </summary>
+        /// <param name="assessmentType">The type of assessment the
+        /// <see cref="SerializableFailureMechanismSectionAssemblyResult"/> represents.</param>
+        /// <param name="sectionResult">The <see cref="ExportableSectionAssemblyResult"/> to create a
+        /// <see cref="SerializableFailureMechanismSectionAssemblyResult"/> for.</param>
+        /// <returns>A <see cref="SerializableFailureMechanismSectionAssemblyResult"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="sectionResult"/> is <c>null</c>.</exception>
+        public static SerializableFailureMechanismSectionAssemblyResult Create(SerializableAssessmentType assessmentType, ExportableSectionAssemblyResult sectionResult)
+        {
+            if (sectionResult == null)
+            {
+                throw new ArgumentNullException(nameof(sectionResult));
+            }
+
+            return new SerializableFailureMechanismSectionAssemblyResult(SerializableAssemblyMethodCreator.Create(sectionResult.AssemblyMethod),
+                                                                         assessmentType,
+                                                                         SerializableFailureMechanismSectionCategoryGroupCreator.Create(sectionResult.AssemblyCategory));
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="SerializableFailureMechanismSectionAssemblyResult"/>
+        /// based on a <see cref="ExportableSectionAssemblyResultWithProbability"/>.
+        /// </summary>
+        /// <param name="assessmentType">The type of assessment the
+        /// <see cref="SerializableFailureMechanismSectionAssemblyResult"/> represents.</param>
+        /// <param name="sectionResult">The <see cref="ExportableSectionAssemblyResultWithProbability"/> to create a
+        /// <see cref="SerializableFailureMechanismSectionAssemblyResult"/> for.</param>
+        /// <returns>A <see cref="SerializableFailureMechanismSectionAssemblyResult"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="sectionResult"/> is <c>null</c>.</exception>
+        public static SerializableFailureMechanismSectionAssemblyResult Create(SerializableAssessmentType assessmentType, ExportableSectionAssemblyResultWithProbability sectionResult)
+        {
+            if (sectionResult == null)
+            {
+                throw new ArgumentNullException(nameof(sectionResult));
+            }
+
+            return new SerializableFailureMechanismSectionAssemblyResult(SerializableAssemblyMethodCreator.Create(sectionResult.AssemblyMethod),
+                                                                         assessmentType,
+                                                                         SerializableFailureMechanismSectionCategoryGroupCreator.Create(sectionResult.AssemblyCategory),
+                                                                         sectionResult.Probability);
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Creators/SerializableFailureMechanismSectionAssemblyResultCreatorTest.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.IO.Model.DataTypes;
+using Ringtoets.AssemblyTool.IO.Model.Enums;
+using Ringtoets.Integration.IO.Assembly;
+using Ringtoets.Integration.IO.Creators;
+using Ringtoets.Integration.IO.TestUtil;
+
+namespace Ringtoets.Integration.IO.Test.Creators
+{
+    [TestFixture]
+    public class SerializableFailureMechanismSectionAssemblyResultCreatorTest
+    {
+        [Test]
+        public void CreateWithExportableSectionAssemblyResult_SectionResultNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var assessmentType = new Random(21).NextEnumValue<SerializableAssessmentType>();
+
+            // Call
+            TestDelegate call = () => SerializableFailureMechanismSectionAssemblyResultCreator.Create(assessmentType, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("sectionResult", exception.ParamName);
+        }
+
+        [Test]
+        public void Create_WithExportableSectionAssemblyResult_ReturnsSerializableFailureMechanismAssemblyResult()
+        {
+            // Setup
+            var assessmentType = new Random(21).NextEnumValue<SerializableAssessmentType>();
+            ExportableSectionAssemblyResult sectionResult = ExportableSectionAssemblyResultTestFactory.CreateSectionAssemblyResult();
+
+            // Call
+            SerializableFailureMechanismSectionAssemblyResult serializableResult =
+                SerializableFailureMechanismSectionAssemblyResultCreator.Create(assessmentType, sectionResult);
+
+            // Assert
+            Assert.AreEqual(assessmentType, serializableResult.AssessmentType);
+
+            Assert.AreEqual(SerializableAssemblyMethodCreator.Create(sectionResult.AssemblyMethod), serializableResult.AssemblyMethod);
+            Assert.AreEqual(SerializableFailureMechanismSectionCategoryGroupCreator.Create(sectionResult.AssemblyCategory), serializableResult.CategoryGroup);
+            Assert.IsNull(serializableResult.Probability);
+        }
+
+        [Test]
+        public void CreateWithExportableSectionAssemblyResultWithProbability_SectionResultNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var assessmentType = new Random(21).NextEnumValue<SerializableAssessmentType>();
+            ExportableSectionAssemblyResultWithProbability sectionResult = null;
+
+            // Call
+            TestDelegate call = () => SerializableFailureMechanismSectionAssemblyResultCreator.Create(assessmentType, sectionResult);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("sectionResult", exception.ParamName);
+        }
+
+        [Test]
+        public void Create_WithExportableSectionAssemblyResultWithProbability_ReturnsSerializableFailureMechanismAssemblyResult()
+        {
+            // Setup
+            var assessmentType = new Random(21).NextEnumValue<SerializableAssessmentType>();
+            ExportableSectionAssemblyResultWithProbability sectionResult = ExportableSectionAssemblyResultTestFactory.CreateSectionAssemblyResultWithProbability();
+
+            // Call
+            SerializableFailureMechanismSectionAssemblyResult serializableResult =
+                SerializableFailureMechanismSectionAssemblyResultCreator.Create(assessmentType, sectionResult);
+
+            // Assert
+            Assert.AreEqual(assessmentType, serializableResult.AssessmentType);
+
+            Assert.AreEqual(SerializableAssemblyMethodCreator.Create(sectionResult.AssemblyMethod), serializableResult.AssemblyMethod);
+            Assert.AreEqual(SerializableFailureMechanismSectionCategoryGroupCreator.Create(sectionResult.AssemblyCategory), serializableResult.CategoryGroup);
+            Assert.AreEqual(sectionResult.Probability, serializableResult.Probability);
+        }
+    }
+}
