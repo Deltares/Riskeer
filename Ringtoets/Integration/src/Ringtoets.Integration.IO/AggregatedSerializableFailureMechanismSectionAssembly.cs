31b55c89e9ef86da7fd3ae5b23de20479794b058
Dennis Tang <dennis.tang@deltares.nl>
2018-08-29 16:20:33 +0200

    Introduced data object to contain all references related to creating a serializable failure mechanism section assembly. WTI-1803

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/AggregatedSerializableFailureMechanismSectionAssembly.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Ringtoets.AssemblyTool.IO.Model;
+using Ringtoets.AssemblyTool.IO.Model.DataTypes;
+
+namespace Ringtoets.Integration.IO
+{
+    /// <summary>
+    /// Class that holds all the information related to creating a
+    /// <see cref="SerializableFailureMechanismSectionAssemblyResult"/>.
+    /// </summary>
+    public class AggregatedSerializableFailureMechanismSectionAssembly
+    {
+        /// <summary>
+        /// Instantiates a <see cref="AggregatedSerializableFailureMechanismSectionAssembly"/>.
+        /// </summary>
+        /// <param name="failureMechanismSection">The <see cref="SerializableFailureMechanismSection"/></param>
+        /// <param name="failureMechanismSectionAssemblyResult">The <see cref="SerializableFailureMechanismSectionAssemblyResult"/>
+        /// that is associated with <paramref name="failureMechanismSection"/>.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public AggregatedSerializableFailureMechanismSectionAssembly(SerializableFailureMechanismSection failureMechanismSection,
+                                                                     SerializableFailureMechanismSectionAssemblyResult failureMechanismSectionAssemblyResult)
+        {
+            if (failureMechanismSection == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSection));
+            }
+
+            if (failureMechanismSectionAssemblyResult == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSectionAssemblyResult));
+            }
+
+            FailureMechanismSection = failureMechanismSection;
+            FailureMechanismSectionAssemblyResult = failureMechanismSectionAssemblyResult;
+        }
+
+        /// <summary>
+        /// Gets the failure mechanism section.
+        /// </summary>
+        public SerializableFailureMechanismSection FailureMechanismSection { get; }
+
+        /// <summary>
+        /// Gets the failure mechanism section assembly result.
+        /// </summary>
+        public SerializableFailureMechanismSectionAssemblyResult FailureMechanismSectionAssemblyResult { get; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/AggregatedSerializableFailureMechanismSectionAssemblyTest.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.IO.Model;
+using Ringtoets.AssemblyTool.IO.Model.DataTypes;
+
+namespace Ringtoets.Integration.IO.Test
+{
+    [TestFixture]
+    public class AggregatedSerializableFailureMechanismSectionAssemblyTest
+    {
+        [Test]
+        public void Constructor_FailureMechanismSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () =>
+                new AggregatedSerializableFailureMechanismSectionAssembly(null, new SerializableFailureMechanismSectionAssemblyResult());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanismSection", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_FailureMechanismSectionAssemblyResultNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () =>
+                new AggregatedSerializableFailureMechanismSectionAssembly(new SerializableFailureMechanismSection(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanismSectionAssemblyResult", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_WithValidArguments_ExpectedValues()
+        {
+            // Setup
+            var section = new SerializableFailureMechanismSection();
+            var sectionAssemblyResult = new SerializableFailureMechanismSectionAssemblyResult();
+
+            // Call
+            var aggregatedResult =
+                new AggregatedSerializableFailureMechanismSectionAssembly(section, sectionAssemblyResult);
+
+            // Assert
+            Assert.AreSame(section, aggregatedResult.FailureMechanismSection);
+            Assert.AreSame(sectionAssemblyResult, aggregatedResult.FailureMechanismSectionAssemblyResult);
+        }
+    }
+}
