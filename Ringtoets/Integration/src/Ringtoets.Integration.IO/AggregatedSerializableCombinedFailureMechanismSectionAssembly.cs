d4657b4682a9dc5744969a848413db91b31cfc2f
Dennis Tang <dennis.tang@deltares.nl>
2018-08-30 13:58:34 +0200

    Introduced data object to contain all references related to creating a serializable combined failure mechanism section assembly. WTI-1803

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.IO/AggregatedSerializableCombinedFailureMechanismSectionAssembly.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Ringtoets.AssemblyTool.IO.Model;
+
+namespace Ringtoets.Integration.IO
+{
+    /// <summary>
+    /// Class that holds all the information related to creating a
+    /// <see cref="SerializableFailureMechanismSection"/>
+    /// </summary>
+    public class AggregatedSerializableCombinedFailureMechanismSectionAssembly
+    {
+        /// <summary>
+        /// Gets the failure mechanism section.
+        /// </summary>
+        public SerializableFailureMechanismSection FailureMechanismSection { get; }
+
+        /// <summary>
+        /// Gets the combined failure mechanism section assembly.
+        /// </summary>
+        public SerializableCombinedFailureMechanismSectionAssembly CombinedFailureMechanismSectionAssembly { get; }
+
+        /// <summary>
+        /// Instantiates a <see cref="AggregatedSerializableFailureMechanismSectionAssembly"/>.
+        /// </summary>
+        /// <param name="failureMechanismSection">The <see cref="SerializableFailureMechanismSection"/></param>
+        /// <param name="combinedFailureMechanismSectionAssembly">The <see cref="SerializableCombinedFailureMechanismSectionAssembly"/>
+        /// that is associated with <paramref name="failureMechanismSection"/>.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public AggregatedSerializableCombinedFailureMechanismSectionAssembly(SerializableFailureMechanismSection failureMechanismSection,
+                                                                             SerializableCombinedFailureMechanismSectionAssembly combinedFailureMechanismSectionAssembly)
+        {
+            if (failureMechanismSection == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSection));
+            }
+
+            if (combinedFailureMechanismSectionAssembly == null)
+            {
+                throw new ArgumentNullException(nameof(combinedFailureMechanismSectionAssembly));
+            }
+
+            FailureMechanismSection = failureMechanismSection;
+            CombinedFailureMechanismSectionAssembly = combinedFailureMechanismSectionAssembly;
+        }
+    }
+}
