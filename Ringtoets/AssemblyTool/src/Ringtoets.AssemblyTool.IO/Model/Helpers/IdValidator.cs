259f662afb80263cee647a25e6cadeeb654dc41d
Dennis Tang <dennis.tang@deltares.nl>
2018-09-03 10:41:48 +0200

    Introduced helper to verify the validity of an id in the serializable model. WTI-1789

---
+++ Ringtoets/AssemblyTool/src/Ringtoets.AssemblyTool.IO/Model/Helpers/IdValidator.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace Ringtoets.AssemblyTool.IO.Model.Helpers
+{
+    /// <summary>
+    /// Helper to validate if an id is suitable to use as an identifier.
+    /// </summary>
+    public static class IdValidator
+    {
+        /// <summary>
+        /// Validates if <paramref name="id"/> is a valid id to be used
+        /// as an identifier in an xml context.
+        /// </summary>
+        /// <param name="id">The identifier to validate.</param>
+        /// <returns><c>true</c> if <paramref name="id"/> is valid, <c>false</c> if otherwise.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="id"/> is <c>null</c>.</exception>
+        public static bool Validate(string id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            var regex = new Regex(@"^[A-Za-z\\_][A-Za-z\\_\d\-\.]+$");
+            return regex.IsMatch(id);
+        }
+    }
+}
