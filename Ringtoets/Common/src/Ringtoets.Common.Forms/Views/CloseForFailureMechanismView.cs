24a4dedceab58b48ea5ca0a463a0a7b614e26590
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-09-11 16:10:59 +0200

    Introduce base view that can close for a failure mechanism WTI-1831

---
+++ Ringtoets/Common/src/Ringtoets.Common.Forms/Views/CloseForFailureMechanismView.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Common.Forms.Views
+{
+    /// <summary>
+    /// Class defining a base view for assembly category views.
+    /// </summary>
+    public abstract class CloseForFailureMechanismView : UserControl, IView
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="CloseForFailureMechanismView"/>.
+        /// </summary>
+        /// <param name="failureMechanism">The failure mechanism belonging to the view.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="failureMechanism"/>
+        /// is <c>null</c>.</exception>
+        protected CloseForFailureMechanismView(IFailureMechanism failureMechanism)
+        {
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+             
+            FailureMechanism = failureMechanism;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="IFailureMechanism"/> the view belongs to.
+        /// </summary>
+        public IFailureMechanism FailureMechanism { get; }
+
+        public object Data { get; set; }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Forms.Test/Views/CloseForFailureMechanismViewTest.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using NUnit.Framework;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Common.Forms.Views;
+
+namespace Ringtoets.Common.Forms.Test.Views
+{
+    [TestFixture]
+    public class CloseForFailureMechanismViewTest
+    {
+        [Test]
+        public void Constructor_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new TestCloseForFailureMechanismView(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanism", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var failureMechanism = new TestFailureMechanism();
+
+            // Call
+            var view = new TestCloseForFailureMechanismView(failureMechanism);
+
+            // Assert
+            Assert.IsInstanceOf<UserControl>(view);
+            Assert.IsInstanceOf<IView>(view);
+            Assert.IsNull(view.Data);
+            Assert.AreSame(failureMechanism, view.FailureMechanism);
+            Assert.AreEqual(0, view.Controls.Count);
+        }
+
+        private class TestCloseForFailureMechanismView : CloseForFailureMechanismView
+        {
+            public TestCloseForFailureMechanismView(IFailureMechanism failureMechanism) : base(failureMechanism) {}
+        }
+    }
+}
