a90c6c5f4d638f396205a6a4801972ff0b656b92
Dennis Tang <dennis.tang@deltares.nl>
2018-09-18 16:02:59 +0200

    Introduced an exportable aggregate failure mechanism section with combined assembly only:
    - Required for the functional change to export non relevant failure mechanisms
    -- Introduced a version with probability
    
    WTI-1803

---
+++ Ringtoets/Integration/test/Ringtoets.Integration.IO.Test/Assembly/ExportableAggregatedFailureMechanismSectionAssemblyResultWithProbabilityAndCombinedResultOnlyTest.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+using Ringtoets.Integration.IO.Assembly;
+using Ringtoets.Integration.IO.TestUtil;
+
+namespace Ringtoets.Integration.IO.Test.Assembly
+{
+    [TestFixture]
+    public class ExportableAggregatedFailureMechanismSectionAssemblyResultWithProbabilityAndCombinedResultOnlyTest
+    {
+        [Test]
+        public void Constructor_CombinedAssemblyNull_ThrowsArgumentNullException()
+        {
+            // Section
+            ExportableFailureMechanismSection section = ExportableFailureMechanismSectionTestFactory.CreateExportableFailureMechanismSection();
+
+            // Call
+            TestDelegate call = () => new ExportableAggregatedFailureMechanismSectionAssemblyResultWithProbabilityAndCombinedResultOnly(section, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("combinedAssembly", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_WithValidArguments_ExpectedValues()
+        {
+            // Setup
+            ExportableFailureMechanismSection section = ExportableFailureMechanismSectionTestFactory.CreateExportableFailureMechanismSection();
+            ExportableSectionAssemblyResultWithProbability combinedAssemblyResult = ExportableSectionAssemblyResultTestFactory.CreateSectionAssemblyResultWithProbability();
+
+            // Call
+            var assemblyResult = new ExportableAggregatedFailureMechanismSectionAssemblyResultWithProbabilityAndCombinedResultOnly(section, combinedAssemblyResult);
+
+            // Assert
+            Assert.IsInstanceOf<ExportableAggregatedFailureMechanismSectionAssemblyResultBase>(assemblyResult);
+
+            Assert.AreSame(section, assemblyResult.FailureMechanismSection);
+            Assert.AreSame(combinedAssemblyResult, assemblyResult.CombinedAssembly);
+        }
+    }
+}
