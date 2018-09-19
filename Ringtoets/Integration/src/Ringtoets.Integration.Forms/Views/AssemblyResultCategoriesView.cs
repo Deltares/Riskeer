e165cd0a0f0be54799bd4657d83d255acb10c962
Robert Tangerman <robert.tangerman@deltares.nl>
2018-09-19 14:28:33 +0200

    Introduced assembly result categories view. WTI-1845

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Views/AssemblyResultCategoriesView.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.Integration.Data;
+
+namespace Ringtoets.Integration.Forms.Views
+{
+    /// <summary>
+    /// View to show a collection of <see cref="FailureMechanismAssemblyCategory"/>.
+    /// </summary>
+    public partial class AssemblyResultCategoriesView : UserControl, IView
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="AssemblyResultCategoriesView"/>.
+        /// </summary>
+        /// <param name="assessmentSection">The assessment section.</param>
+        /// <param name="getAssemblyCategoriesFunc">The func to get the assembly categories from.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public AssemblyResultCategoriesView(AssessmentSection assessmentSection,
+                                            Func<IEnumerable<FailureMechanismAssemblyCategory>> getAssemblyCategoriesFunc)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            if (getAssemblyCategoriesFunc == null)
+            {
+                throw new ArgumentNullException(nameof(getAssemblyCategoriesFunc));
+            }
+
+            InitializeComponent();
+        }
+
+        public object Data { get; set; }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/Views/AssemblyResultCategoriesViewTest.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Windows.Forms;
+using Core.Common.Controls.Views;
+using NUnit.Framework;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.Forms.Views;
+
+namespace Ringtoets.Integration.Forms.Test.Views
+{
+    [TestFixture]
+    public class AssemblyResultCategoriesViewTest
+    {
+        [Test]
+        public void Constructor_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new AssemblyResultCategoriesView(null, () => null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_GetAssemblyCategoriesFuncNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new AssemblyResultCategoriesView(new AssessmentSection(AssessmentSectionComposition.Dike), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("getAssemblyCategoriesFunc", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            using (var view = new AssemblyResultCategoriesView(new AssessmentSection(AssessmentSectionComposition.Dike),
+                                                               () => null))
+            {
+                // Assert
+                Assert.IsInstanceOf<UserControl>(view);
+                Assert.IsInstanceOf<IView>(view);
+                Assert.IsNull(view.Data);
+            }
+        }
+    }
+}
