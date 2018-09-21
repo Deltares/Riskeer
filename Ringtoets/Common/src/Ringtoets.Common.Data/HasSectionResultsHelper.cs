63c08e1b9de70f1880352de985f4ba89c5648386
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-09-21 10:03:48 +0200

    Introduce new helper WTI-1839

---
+++ Ringtoets/Common/src/Ringtoets.Common.Data/HasSectionResultsHelper.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using Ringtoets.Common.Data.FailureMechanism;
+
+namespace Ringtoets.Common.Data
+{
+    /// <summary>
+    /// Class containing helper methods for <see cref="IHasSectionResults{T}"/>.
+    /// </summary>
+    public static class HasSectionResultsHelper
+    {
+        /// <summary>
+        /// Determines whether the <see cref="IHasSectionResults{T}"/> has section results that are manually overwritten.
+        /// </summary>
+        /// <param name="failureMechanism">The <see cref="IHasSectionResults{T}"/>.</param>
+        /// <returns><c>true</c> if the <see cref="IHasSectionResults{T}"/> contains section results that are manually overwritten,
+        /// <c>false</c> otherwise.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="failureMechanism"/> is <c>null</c>.</exception>
+        public static bool HasManualAssemblyResults<T>(IHasSectionResults<T> failureMechanism)
+            where T : FailureMechanismSectionResult
+        {
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+
+            return failureMechanism.SectionResults.Any(sr => sr.UseManualAssembly);
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Data.Test/HasSectionResultsHelperTest.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using NUnit.Framework;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+
+namespace Ringtoets.Common.Data.Test
+{
+    [TestFixture]
+    public class HasSectionResultsHelperTest
+    {
+        [Test]
+        public void HasManualAssemblyResults_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => HasSectionResultsHelper.HasManualAssemblyResults((IHasSectionResults<FailureMechanismSectionResult>) null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("failureMechanism", exception.ParamName);
+        }
+
+        [Test]
+        public void HasManualAssemblyResults_FailureMechanismWithManualAssemblyResults_ReturnsTrue()
+        {
+            // Setup
+            var failureMechanism = new TestFailureMechanism();
+            FailureMechanismTestHelper.AddSections(failureMechanism, 2);
+            failureMechanism.SectionResults.First().UseManualAssembly = true;
+
+            // Call
+            bool hasManualAssemblyResults = HasSectionResultsHelper.HasManualAssemblyResults(failureMechanism);
+
+            // Assert
+            Assert.IsTrue(hasManualAssemblyResults);
+        }
+
+        [Test]
+        public void HasManualAssemblyResults_FailureMechanismWithoutManualAssemblyResults_ReturnsFalse()
+        {
+            // Setup
+            var failureMechanism = new TestFailureMechanism();
+            FailureMechanismTestHelper.AddSections(failureMechanism, 2);
+
+            // Call
+            bool hasManualAssemblyResults = HasSectionResultsHelper.HasManualAssemblyResults(failureMechanism);
+
+            // Assert
+            Assert.IsFalse(hasManualAssemblyResults);
+        }
+    }
+}
