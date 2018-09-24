7112111f042c275b774622464cb27537a6563c66
Dennis Tang <dennis.tang@deltares.nl>
2018-09-24 14:10:55 +0200

    Introduced test helper for groups 1 and 2:
    - Testhelper produces assessment section configurations and sets one failure mechanism up with manual assembly results.
    
    WTI-1842

---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Data.TestUtil.Test/AssessmentSectionAssemblyTestHelperTest.cs
@@ -0,0 +1,117 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Ringtoets.ClosingStructures.Data;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.GrassCoverErosionInwards.Data;
+using Ringtoets.HeightStructures.Data;
+using Ringtoets.MacroStabilityInwards.Data;
+using Ringtoets.Piping.Data;
+using Ringtoets.StabilityPointStructures.Data;
+
+namespace Ringtoets.Integration.Data.TestUtil.Test
+{
+    [TestFixture]
+    public class AssessmentSectionAssemblyTestHelperTest
+    {
+        [Test]
+        public void GetConfiguredAssessmentSectionWithGroup1And2FailureMechanisms_Always_ReturnsExpectedTestCases()
+        {
+            // Call
+            TestCaseData[] testCases = AssessmentSectionAssemblyTestHelper.GetConfiguredAssessmentSectionWithGroup1And2FailureMechanisms()
+                                                                          .ToArray();
+
+            // Assert
+            CollectionAssert.AreEqual(new[]
+            {
+                "Piping",
+                "MacroStabilityInwards",
+                "GrassCoverErosionInwards",
+                "ClosingStructures",
+                "HeightStructures",
+                "StabilityPointStructures"
+            }, testCases.Select(tc => tc.TestName));
+            Assert.IsTrue(testCases.All(tc => tc.Arguments.Length == 1));
+
+            var assessmentSectionWithPiping = (AssessmentSection) testCases[0].Arguments[0];
+            PipingFailureMechanism piping = assessmentSectionWithPiping.Piping;
+            AssertAssessmentSection(assessmentSectionWithPiping, piping);
+            PipingFailureMechanismSectionResult pipingSectionResult = piping.SectionResults.Single();
+            Assert.IsTrue(pipingSectionResult.UseManualAssembly);
+            Assert.AreEqual(0.5, pipingSectionResult.ManualAssemblyProbability);
+
+            var assessmentSectionWithMacroStabilityInwards = (AssessmentSection) testCases[1].Arguments[0];
+            MacroStabilityInwardsFailureMechanism macroStabilityInwards = assessmentSectionWithMacroStabilityInwards.MacroStabilityInwards;
+            AssertAssessmentSection(assessmentSectionWithMacroStabilityInwards, macroStabilityInwards);
+            MacroStabilityInwardsFailureMechanismSectionResult macroStabilityInwardsSectionResult = macroStabilityInwards.SectionResults.Single();
+            Assert.IsTrue(macroStabilityInwardsSectionResult.UseManualAssembly);
+            Assert.AreEqual(0.5, macroStabilityInwardsSectionResult.ManualAssemblyProbability);
+
+            var assessmentSectionWithGrassCoverErosionInwards = (AssessmentSection) testCases[2].Arguments[0];
+            GrassCoverErosionInwardsFailureMechanism grassCoverErosionInwards = assessmentSectionWithGrassCoverErosionInwards.GrassCoverErosionInwards;
+            AssertAssessmentSection(assessmentSectionWithGrassCoverErosionInwards, grassCoverErosionInwards);
+            GrassCoverErosionInwardsFailureMechanismSectionResult grassCoverErosionInwardsSectionResult = grassCoverErosionInwards.SectionResults.Single();
+            Assert.IsTrue(grassCoverErosionInwardsSectionResult.UseManualAssembly);
+            Assert.AreEqual(0.5, grassCoverErosionInwardsSectionResult.ManualAssemblyProbability);
+
+            var assessmentSectionWithClosingStructures = (AssessmentSection) testCases[3].Arguments[0];
+            ClosingStructuresFailureMechanism closingStructures = assessmentSectionWithClosingStructures.ClosingStructures;
+            AssertAssessmentSection(assessmentSectionWithClosingStructures, closingStructures);
+            ClosingStructuresFailureMechanismSectionResult closingStructuresSectionResult = closingStructures.SectionResults.Single();
+            Assert.IsTrue(closingStructuresSectionResult.UseManualAssembly);
+            Assert.AreEqual(0.5, closingStructuresSectionResult.ManualAssemblyProbability);
+
+            var assessmentSectionWithHeightStructures = (AssessmentSection) testCases[4].Arguments[0];
+            HeightStructuresFailureMechanism heightStructures = assessmentSectionWithHeightStructures.HeightStructures;
+            AssertAssessmentSection(assessmentSectionWithHeightStructures, heightStructures);
+            HeightStructuresFailureMechanismSectionResult heightStructuresSectionResult = heightStructures.SectionResults.Single();
+            Assert.IsTrue(heightStructuresSectionResult.UseManualAssembly);
+            Assert.AreEqual(0.5, heightStructuresSectionResult.ManualAssemblyProbability);
+
+            var assessmentSectionWithStabilityPointStructures = (AssessmentSection) testCases[5].Arguments[0];
+            StabilityPointStructuresFailureMechanism stabilityPointStructures = assessmentSectionWithStabilityPointStructures.StabilityPointStructures;
+            AssertAssessmentSection(assessmentSectionWithStabilityPointStructures, stabilityPointStructures);
+            StabilityPointStructuresFailureMechanismSectionResult stabilityPointStructuresSectionResult = stabilityPointStructures.SectionResults.Single();
+            Assert.IsTrue(stabilityPointStructuresSectionResult.UseManualAssembly);
+            Assert.AreEqual(0.5, stabilityPointStructuresSectionResult.ManualAssemblyProbability);
+        }
+
+        private static void AssertAssessmentSection(AssessmentSection assessmentSection,
+                                                    IFailureMechanism relevantFailureMechanism)
+        {
+            Assert.AreEqual(AssessmentSectionComposition.DikeAndDune, assessmentSection.Composition);
+
+            IEnumerable<IFailureMechanism> irrelevantFailureMechanisms = assessmentSection.GetFailureMechanisms()
+                                                                                          .Where(fm => !ReferenceEquals(relevantFailureMechanism, fm));
+            AssertIrrelevantFailureMechanisms(irrelevantFailureMechanisms);
+        }
+
+        private static void AssertIrrelevantFailureMechanisms(IEnumerable<IFailureMechanism> failureMechanisms)
+        {
+            Assert.IsTrue(failureMechanisms.All(fm => !fm.IsRelevant));
+            CollectionAssert.IsEmpty(failureMechanisms.SelectMany(fm => fm.Sections));
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Data.TestUtil/AssessmentSectionAssemblyTestHelper.cs
@@ -0,0 +1,155 @@
+﻿// Copyright (C) Stichting Deltares 2017. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Util.Extensions;
+using NUnit.Framework;
+using Ringtoets.ClosingStructures.Data;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.GrassCoverErosionInwards.Data;
+using Ringtoets.HeightStructures.Data;
+using Ringtoets.MacroStabilityInwards.Data;
+using Ringtoets.Piping.Data;
+using Ringtoets.StabilityPointStructures.Data;
+
+namespace Ringtoets.Integration.Data.TestUtil
+{
+    /// <summary>
+    /// Helper which creates configurations of <see cref="AssessmentSection"/>
+    /// which can be used for testing assembly results.
+    /// </summary>
+    public static class AssessmentSectionAssemblyTestHelper
+    {
+        /// <summary>
+        /// Gets a collection of <see cref="AssessmentSection"/> configurations of
+        /// failure mechanisms belonging in groups 1 and 2, such that:
+        /// <list type="bullet">
+        /// <item>All other failure mechanisms are marked as irrelevant, except one failure mechanism.</item>
+        /// <item>The aforementioned failure mechanism will have one section which contains manual assembly data.</item>
+        /// </list>
+        /// </summary>
+        /// <returns>A collection of <see cref="AssessmentSection"/> configurations.</returns>
+        public static IEnumerable<TestCaseData> GetConfiguredAssessmentSectionWithGroup1And2FailureMechanisms()
+        {
+            foreach (FailureMechanismConfiguration assessmentSectionConfiguration in GetGroup1And2FailureMechanismConfigurations())
+            {
+                AssessmentSection assessmentSection = CreateAssessmentSectionWithIrrelevantFailureMechanisms();
+                assessmentSectionConfiguration.ConfigureAssessmentSection(assessmentSection);
+
+                yield return new TestCaseData(assessmentSection).SetName(assessmentSectionConfiguration.FailureMechanismName);
+            }
+        }
+
+        private static AssessmentSection CreateAssessmentSectionWithIrrelevantFailureMechanisms()
+        {
+            var assessmentSection = new AssessmentSection(AssessmentSectionComposition.DikeAndDune);
+            assessmentSection.GetFailureMechanisms().ForEachElementDo(fm => fm.IsRelevant = false);
+            return assessmentSection;
+        }
+
+        private static IEnumerable<FailureMechanismConfiguration> GetGroup1And2FailureMechanismConfigurations()
+        {
+            const double manualAssemblyProbability = 0.5;
+
+            yield return new FailureMechanismConfiguration(section =>
+            {
+                PipingFailureMechanism failureMechanism = section.Piping;
+                failureMechanism.IsRelevant = true;
+                FailureMechanismTestHelper.AddSections(failureMechanism, 1);
+
+                PipingFailureMechanismSectionResult sectionResult = failureMechanism.SectionResults.Single();
+                sectionResult.UseManualAssembly = true;
+                sectionResult.ManualAssemblyProbability = manualAssemblyProbability;
+            }, "Piping");
+
+            yield return new FailureMechanismConfiguration(section =>
+            {
+                MacroStabilityInwardsFailureMechanism failureMechanism = section.MacroStabilityInwards;
+                failureMechanism.IsRelevant = true;
+                FailureMechanismTestHelper.AddSections(failureMechanism, 1);
+
+                MacroStabilityInwardsFailureMechanismSectionResult sectionResult = failureMechanism.SectionResults.Single();
+                sectionResult.UseManualAssembly = true;
+                sectionResult.ManualAssemblyProbability = manualAssemblyProbability;
+            }, "MacroStabilityInwards");
+
+            yield return new FailureMechanismConfiguration(section =>
+            {
+                GrassCoverErosionInwardsFailureMechanism failureMechanism = section.GrassCoverErosionInwards;
+                failureMechanism.IsRelevant = true;
+                FailureMechanismTestHelper.AddSections(failureMechanism, 1);
+
+                GrassCoverErosionInwardsFailureMechanismSectionResult sectionResult = failureMechanism.SectionResults.Single();
+                sectionResult.UseManualAssembly = true;
+                sectionResult.ManualAssemblyProbability = manualAssemblyProbability;
+            }, "GrassCoverErosionInwards");
+
+            yield return new FailureMechanismConfiguration(section =>
+            {
+                ClosingStructuresFailureMechanism failureMechanism = section.ClosingStructures;
+                failureMechanism.IsRelevant = true;
+                FailureMechanismTestHelper.AddSections(failureMechanism, 1);
+
+                ClosingStructuresFailureMechanismSectionResult sectionResult = failureMechanism.SectionResults.Single();
+                sectionResult.UseManualAssembly = true;
+                sectionResult.ManualAssemblyProbability = manualAssemblyProbability;
+            }, "ClosingStructures");
+
+            yield return new FailureMechanismConfiguration(section =>
+            {
+                HeightStructuresFailureMechanism failureMechanism = section.HeightStructures;
+                failureMechanism.IsRelevant = true;
+                FailureMechanismTestHelper.AddSections(failureMechanism, 1);
+
+                HeightStructuresFailureMechanismSectionResult sectionResult = failureMechanism.SectionResults.Single();
+                sectionResult.UseManualAssembly = true;
+                sectionResult.ManualAssemblyProbability = manualAssemblyProbability;
+            }, "HeightStructures");
+
+            yield return new FailureMechanismConfiguration(section =>
+            {
+                StabilityPointStructuresFailureMechanism failureMechanism = section.StabilityPointStructures;
+                failureMechanism.IsRelevant = true;
+                FailureMechanismTestHelper.AddSections(failureMechanism, 1);
+
+                StabilityPointStructuresFailureMechanismSectionResult sectionResult = failureMechanism.SectionResults.Single();
+                sectionResult.UseManualAssembly = true;
+                sectionResult.ManualAssemblyProbability = manualAssemblyProbability;
+            }, "StabilityPointStructures");
+        }
+
+        private class FailureMechanismConfiguration
+        {
+            public FailureMechanismConfiguration(Action<AssessmentSection> configureAssessmentSection,
+                                                 string failureMechanismName)
+            {
+                ConfigureAssessmentSection = configureAssessmentSection;
+                FailureMechanismName = failureMechanismName;
+            }
+
+            public Action<AssessmentSection> ConfigureAssessmentSection { get; }
+            public string FailureMechanismName { get; }
+        }
+    }
+}
