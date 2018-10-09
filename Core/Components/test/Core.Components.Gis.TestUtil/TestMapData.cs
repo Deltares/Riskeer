446a9faeda7106403740326d16186c281d8a6542
Robert Tangerman <robert.tangerman@deltares.nl>
2018-10-09 10:13:02 +0200

    Added TestMapData. WTI-1856

---
+++ Core/Components/test/Core.Components.Gis.TestUtil/TestMapData.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Components.Gis.Data;
+
+namespace Core.Components.Gis.TestUtil
+{
+    /// <summary>
+    /// A class representing a <see cref="MapData"/> implementation which is
+    /// not in the regular codebase.
+    /// </summary>
+    public class TestMapData : MapData
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="TestMapData"/>.
+        /// </summary>
+        public TestMapData() : base("test data") {}
+    }
+}
