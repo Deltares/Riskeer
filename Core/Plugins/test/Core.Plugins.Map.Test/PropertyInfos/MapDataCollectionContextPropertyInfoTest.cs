0ebf4dc4fe1e30897df1089305850d469d4397c8
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-10-19 09:13:22 +0200

    Add propertyinfo test WTI-1880

---
+++ Core/Plugins/test/Core.Plugins.Map.Test/PropertyInfos/MapDataCollectionContextPropertyInfoTest.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Linq;
+using Core.Common.Gui.Plugin;
+using Core.Common.Gui.PropertyBag;
+using Core.Components.Gis.Data;
+using Core.Plugins.Map.PresentationObjects;
+using Core.Plugins.Map.PropertyClasses;
+using NUnit.Framework;
+
+namespace Core.Plugins.Map.Test.PropertyInfos
+{
+    [TestFixture]
+    public class FailureMechanismSectionsPropertyInfoTest
+    {
+        private MapPlugin plugin;
+        private PropertyInfo info;
+
+        [SetUp]
+        public void SetUp()
+        {
+            plugin = new MapPlugin();
+            info = plugin.GetPropertyInfos().First(tni => tni.PropertyObjectType == typeof(MapDataCollectionProperties));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            plugin.Dispose();
+        }
+
+        [Test]
+        public void Initialized_Always_ExpectedPropertiesSet()
+        {
+            // Assert
+            Assert.AreEqual(typeof(MapDataCollectionContext), info.DataType);
+            Assert.AreEqual(typeof(MapDataCollectionProperties), info.PropertyObjectType);
+        }
+
+        [Test]
+        public void CreateInstance_WithValidArguments_NewPropertiesWithMapDataCollectionAsData()
+        {
+            // Setup
+            var mapDataCollection = new MapDataCollection("Test");
+            var context = new MapDataCollectionContext(mapDataCollection, null);
+
+            // Call
+            IObjectProperties objectProperties = info.CreateInstance(context);
+
+            // Assert
+            Assert.IsInstanceOf<MapDataCollectionProperties>(objectProperties);
+            Assert.AreSame(mapDataCollection, objectProperties.Data);
+        }
+    }
+}
