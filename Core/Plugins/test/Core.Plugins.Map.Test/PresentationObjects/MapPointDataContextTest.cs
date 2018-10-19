ee1c1d48049db88e611c3402de95ed5bee4190b3
Robert Tangerman <robert.tangerman@deltares.nl>
2018-10-19 14:47:04 +0200

    Introduced context for MapPointData. WTI-1885

---
+++ Core/Plugins/test/Core.Plugins.Map.Test/PresentationObjects/MapPointDataContextTest.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Components.Gis.Data;
+using Core.Plugins.Map.PresentationObjects;
+using NUnit.Framework;
+
+namespace Core.Plugins.Map.Test.PresentationObjects
+{
+    [TestFixture]
+    public class MapPointDataContextTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var data = new MapPointData("test");
+            var parent = new MapDataCollectionContext(new MapDataCollection("parent"), null);
+
+            // Call
+            var context = new MapPointDataContext(data, parent);
+
+            // Assert
+            Assert.IsInstanceOf<FeatureBasedMapDataContext>(context);
+            Assert.AreSame(data, context.WrappedData);
+            Assert.AreSame(parent, context.ParentMapData);
+        }
+    }
+}
