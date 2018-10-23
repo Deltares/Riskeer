32209b7cab25855e87f0976f0af5040568933563
Carlos Lubbers <carlos.lubbers@deltares.nl>
2018-10-23 14:04:48 +0200

    Introduce map features factory for height structures WTI-1871

---
+++ Ringtoets/HeightStructures/test/Ringtoets.HeightStructures.Forms.Test/Factories/HeightStructuresAssemblyMapDataFeaturesFactoryTest.cs
@@ -0,0 +1,240 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Core.Common.TestUtil;
+using Core.Components.Gis.Features;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.AssemblyTool.KernelWrapper.Calculators;
+using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators;
+using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators.Assembly;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.Common.Forms.TestUtil;
+using Ringtoets.HeightStructures.Data;
+using Ringtoets.HeightStructures.Forms.Factories;
+
+namespace Ringtoets.HeightStructures.Forms.Test.Factories
+{
+    [TestFixture]
+    public class HeightStructuresAssemblyMapDataFeaturesFactoryTest
+    {
+        [Test]
+        public void CreateSimpleAssemblyFeatures_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => HeightStructuresAssemblyMapDataFeaturesFactory.CreateSimpleAssemblyFeatures(null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("failureMechanism", paramName);
+        }
+
+        [Test]
+        public void CreateSimpleAssemblyFeatures_WithValidData_ReturnsFeaturesCollection()
+        {
+            // Setup
+            var random = new Random(39);
+            var failureMechanism = new HeightStructuresFailureMechanism();
+            FailureMechanismTestHelper.AddSections(failureMechanism, random.Next(0, 10));
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                var calculatorFactory = (TestAssemblyToolCalculatorFactory) AssemblyToolCalculatorFactory.Instance;
+                FailureMechanismSectionAssemblyCalculatorStub calculator = calculatorFactory.LastCreatedFailureMechanismSectionAssemblyCalculator;
+
+                calculator.SimpleAssessmentAssemblyOutput = new FailureMechanismSectionAssembly(random.NextDouble(),
+                                                                                                random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+
+                // Call
+                IEnumerable<MapFeature> features = HeightStructuresAssemblyMapDataFeaturesFactory.CreateSimpleAssemblyFeatures(failureMechanism);
+
+                // Assert
+                MapFeaturesTestHelper.AssertAssemblyMapFeatures(calculator.SimpleAssessmentAssemblyOutput, failureMechanism, features);
+            }
+        }
+
+        [Test]
+        public void CreateDetailedAssemblyFeatures_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => HeightStructuresAssemblyMapDataFeaturesFactory.CreateDetailedAssemblyFeatures(null, assessmentSection);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("failureMechanism", paramName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CreateDetailedAssemblyFeatures_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => HeightStructuresAssemblyMapDataFeaturesFactory.CreateDetailedAssemblyFeatures(new HeightStructuresFailureMechanism(), null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("assessmentSection", paramName);
+        }
+
+        [Test]
+        public void CreateDetailedAssemblyFeatures_WithValidData_ReturnsFeaturesCollection()
+        {
+            // Setup
+            var random = new Random(39);
+            var assessmentSection = new AssessmentSectionStub();
+            var failureMechanism = new HeightStructuresFailureMechanism();
+            FailureMechanismTestHelper.AddSections(failureMechanism, random.Next(0, 10));
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                var calculatorFactory = (TestAssemblyToolCalculatorFactory) AssemblyToolCalculatorFactory.Instance;
+                FailureMechanismSectionAssemblyCalculatorStub calculator = calculatorFactory.LastCreatedFailureMechanismSectionAssemblyCalculator;
+
+                calculator.DetailedAssessmentAssemblyOutput = new FailureMechanismSectionAssembly(random.NextDouble(),
+                                                                                                  random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+
+                // Call
+                IEnumerable<MapFeature> features = HeightStructuresAssemblyMapDataFeaturesFactory.CreateDetailedAssemblyFeatures(failureMechanism,
+                                                                                                                                 assessmentSection);
+
+                // Assert
+                MapFeaturesTestHelper.AssertAssemblyMapFeatures(calculator.DetailedAssessmentAssemblyOutput, failureMechanism, features);
+            }
+        }
+
+        [Test]
+        public void CreateTailorMadeAssemblyFeatures_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => HeightStructuresAssemblyMapDataFeaturesFactory.CreateTailorMadeAssemblyFeatures(null, assessmentSection);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("failureMechanism", paramName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CreateTailorMadeAssemblyFeatures_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => HeightStructuresAssemblyMapDataFeaturesFactory.CreateTailorMadeAssemblyFeatures(new HeightStructuresFailureMechanism(), null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("assessmentSection", paramName);
+        }
+
+        [Test]
+        public void CreateTailorMadeAssemblyFeatures_WithValidData_ReturnsFeaturesCollection()
+        {
+            // Setup
+            var random = new Random(39);
+            var assessmentSection = new AssessmentSectionStub();
+            var failureMechanism = new HeightStructuresFailureMechanism();
+            FailureMechanismTestHelper.AddSections(failureMechanism, random.Next(0, 10));
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                var calculatorFactory = (TestAssemblyToolCalculatorFactory) AssemblyToolCalculatorFactory.Instance;
+                FailureMechanismSectionAssemblyCalculatorStub calculator = calculatorFactory.LastCreatedFailureMechanismSectionAssemblyCalculator;
+
+                calculator.TailorMadeAssessmentAssemblyOutput = new FailureMechanismSectionAssembly(random.NextDouble(),
+                                                                                                    random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+
+                // Call
+                IEnumerable<MapFeature> features = HeightStructuresAssemblyMapDataFeaturesFactory.CreateTailorMadeAssemblyFeatures(failureMechanism,
+                                                                                                                                   assessmentSection);
+
+                // Assert
+                MapFeaturesTestHelper.AssertAssemblyMapFeatures(calculator.TailorMadeAssessmentAssemblyOutput, failureMechanism, features);
+            }
+        }
+
+        [Test]
+        public void CreateCombinedAssemblyFeatures_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => HeightStructuresAssemblyMapDataFeaturesFactory.CreateCombinedAssemblyFeatures(null, assessmentSection);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("failureMechanism", paramName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CreateCombinedAssemblyFeatures_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => HeightStructuresAssemblyMapDataFeaturesFactory.CreateCombinedAssemblyFeatures(new HeightStructuresFailureMechanism(), null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(call).ParamName;
+            Assert.AreEqual("assessmentSection", paramName);
+        }
+
+        [Test]
+        public void CreateCombinedAssemblyFeatures_WithValidData_ReturnsFeaturesCollection()
+        {
+            // Setup
+            var random = new Random(39);
+            var assessmentSection = new AssessmentSectionStub();
+            var failureMechanism = new HeightStructuresFailureMechanism();
+            FailureMechanismTestHelper.AddSections(failureMechanism, random.Next(0, 10));
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                var calculatorFactory = (TestAssemblyToolCalculatorFactory) AssemblyToolCalculatorFactory.Instance;
+                FailureMechanismSectionAssemblyCalculatorStub calculator = calculatorFactory.LastCreatedFailureMechanismSectionAssemblyCalculator;
+
+                calculator.CombinedAssemblyOutput = new FailureMechanismSectionAssembly(random.NextDouble(),
+                                                                                        random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>());
+
+                // Call
+                IEnumerable<MapFeature> features = HeightStructuresAssemblyMapDataFeaturesFactory.CreateCombinedAssemblyFeatures(failureMechanism,
+                                                                                                                                 assessmentSection);
+
+                // Assert
+                MapFeaturesTestHelper.AssertAssemblyMapFeatures(calculator.CombinedAssemblyOutput, failureMechanism, features);
+            }
+        }
+    }
+}
