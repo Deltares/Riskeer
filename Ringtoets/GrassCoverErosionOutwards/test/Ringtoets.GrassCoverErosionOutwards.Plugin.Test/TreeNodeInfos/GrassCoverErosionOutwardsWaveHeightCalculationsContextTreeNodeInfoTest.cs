2e12ba2136827703874abbf7ddc7f2bf49d3f91b
Robert Tangerman <robert.tangerman@deltares.nl>
2018-10-24 10:36:21 +0200

    Fixed compilation errors. WTI-1888

--- Ringtoets/GrassCoverErosionOutwards/test/Ringtoets.GrassCoverErosionOutwards.Plugin.Test/TreeNodeInfos/GrassCoverErosionOutwardsWaveHeightCalculationsContextTreeNodeInfoTest.cs
+++ Ringtoets/GrassCoverErosionOutwards/test/Ringtoets.GrassCoverErosionOutwards.Plugin.Test/TreeNodeInfos/GrassCoverErosionOutwardsWaveHeightCalculationsContextTreeNodeInfoTest.cs
@@ -82,7 +82,7 @@ namespace Ringtoets.GrassCoverErosionOutwards.Plugin.Test.TreeNodeInfos
                 Assert.IsNull(info.CanRemove);
                 Assert.IsNull(info.OnNodeRemoved);
                 Assert.IsNull(info.CanCheck);
-                Assert.IsNull(info.IsChecked);
+                Assert.IsNull(info.CheckedState);
                 Assert.IsNull(info.OnNodeChecked);
                 Assert.IsNull(info.CanDrag);
                 Assert.IsNull(info.CanDrop);
