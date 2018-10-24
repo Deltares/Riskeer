f39c636237b6926be208f51670352f4a9b870971
Robert Tangerman <robert.tangerman@deltares.nl>
2018-10-24 10:39:06 +0200

    Fixed compilation errors. WTI-1888

--- Ringtoets/StabilityPointStructures/test/Ringtoets.StabilityPointStructures.Plugin.Test/TreeNodeInfos/StabilityPointStructuresProbabilityFailureMechanismSectionResultContextTreeNodeInfoTest.cs
+++ Ringtoets/StabilityPointStructures/test/Ringtoets.StabilityPointStructures.Plugin.Test/TreeNodeInfos/StabilityPointStructuresProbabilityFailureMechanismSectionResultContextTreeNodeInfoTest.cs
@@ -68,7 +68,7 @@ namespace Ringtoets.StabilityPointStructures.Plugin.Test.TreeNodeInfos
             Assert.IsNull(info.CanRemove);
             Assert.IsNull(info.OnNodeRemoved);
             Assert.IsNull(info.CanCheck);
-            Assert.IsNull(info.IsChecked);
+            Assert.IsNull(info.CheckedState);
             Assert.IsNull(info.OnNodeChecked);
             Assert.IsNull(info.CanDrag);
             Assert.IsNull(info.CanDrop);
