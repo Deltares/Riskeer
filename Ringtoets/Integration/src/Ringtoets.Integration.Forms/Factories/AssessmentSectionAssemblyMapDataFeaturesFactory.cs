519620fbb4746cb9234a10ed46579fd26314dbc0
Dennis Tang <dennis.tang@deltares.nl>
2018-11-14 11:23:42 +0100

    Added map data features factory. WTI-1554

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Forms/Factories/AssessmentSectionAssemblyMapDataFeaturesFactory.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Core.Common.Base.Geometry;
+using Core.Common.Util;
+using Core.Components.Gis.Features;
+using Ringtoets.AssemblyTool.Forms;
+using Ringtoets.Common.Forms.Factories;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.Data.Assembly;
+using Ringtoets.Integration.Forms.Properties;
+using RingtoetsCommonFormsResources = Ringtoets.Common.Forms.Properties.Resources;
+
+namespace Ringtoets.Integration.Forms.Factories
+{
+    /// <summary>
+    /// Factory for creating collections of <see cref="MapFeature"/> for assembly results
+    /// in an <see cref="AssessmentSection"/>.
+    /// </summary>
+    public static class AssessmentSectionAssemblyMapDataFeaturesFactory
+    {
+        /// <summary>
+        /// Creates features for the combined failure mechanism section assembly.
+        /// </summary>
+        /// <param name="assessmentSection">The <see cref="AssessmentSection"/> to create the features for.</param>
+        /// <returns>A collection of <see cref="MapFeature"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="assessmentSection"/>
+        /// is <c>null</c>.</exception>
+        public static IEnumerable<MapFeature> CreateCombinedFailureMechanismSectionAssemblyFeatures(AssessmentSection assessmentSection)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            IEnumerable<CombinedFailureMechanismSectionAssemblyResult> assemblyResult =
+                AssessmentSectionAssemblyFactory.AssembleCombinedPerFailureMechanismSection(assessmentSection, true);
+            var mapFeatures = new List<MapFeature>();
+            foreach (CombinedFailureMechanismSectionAssemblyResult combinedFailureMechanismSectionAssemblyResult in assemblyResult)
+            {
+                MapFeature mapFeature = RingtoetsMapDataFeaturesFactory.CreateSingleLineMapFeature(new Point2D[0]);
+
+                mapFeature.MetaData[RingtoetsCommonFormsResources.AssemblyCategory_Group_DisplayName] =
+                    new EnumDisplayWrapper<DisplayFailureMechanismSectionAssemblyCategoryGroup>(
+                        DisplayFailureMechanismSectionAssemblyCategoryGroupConverter.Convert(combinedFailureMechanismSectionAssemblyResult.TotalResult)).DisplayName;
+                mapFeature.MetaData[Resources.SectionNumber_DisplayName] = combinedFailureMechanismSectionAssemblyResult.SectionNumber;
+
+                mapFeatures.Add(mapFeature);
+            }
+
+            return mapFeatures;
+        }
+    }
+}
---
+++ Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/Factories/AssessmentSectionAssemblyMapDataFeaturesFactoryTest.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base.Geometry;
+using Core.Common.TestUtil;
+using Core.Common.Util;
+using Core.Components.Gis.Features;
+using Core.Components.Gis.Geometries;
+using NUnit.Framework;
+using Ringtoets.AssemblyTool.Data;
+using Ringtoets.AssemblyTool.Forms;
+using Ringtoets.AssemblyTool.KernelWrapper.Calculators;
+using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators;
+using Ringtoets.AssemblyTool.KernelWrapper.TestUtil.Calculators.Assembly;
+using Ringtoets.Common.Data.AssessmentSection;
+using Ringtoets.Integration.Data;
+using Ringtoets.Integration.Data.Assembly;
+using Ringtoets.Integration.Forms.Factories;
+
+namespace Ringtoets.Integration.Forms.Test.Factories
+{
+    [TestFixture]
+    public class AssessmentSectionAssemblyMapDataFeaturesFactoryTest
+    {
+        [Test]
+        public void CreateCombinedFailureMechanismSectionAssemblyFeatures_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => AssessmentSectionAssemblyMapDataFeaturesFactory.CreateCombinedFailureMechanismSectionAssemblyFeatures(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateCombinedFailureMechanismSectionAssemblyFeatures_WithAssessmentSection_ReturnsFeatureCollection()
+        {
+            // Setup
+            var random = new Random(21);
+            var assessmentSection = new AssessmentSection(random.NextEnumValue<AssessmentSectionComposition>())
+            {
+                ReferenceLine = new ReferenceLine()
+            };
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                var calculatorFactory = (TestAssemblyToolCalculatorFactory) AssemblyToolCalculatorFactory.Instance;
+                AssessmentSectionAssemblyCalculatorStub calculator = calculatorFactory.LastCreatedAssessmentSectionAssemblyCalculator;
+                CombinedFailureMechanismSectionAssembly[] failureMechanismSectionAssembly =
+                {
+                    CreateCombinedFailureMechanismSectionAssembly(assessmentSection, 20),
+                    CreateCombinedFailureMechanismSectionAssembly(assessmentSection, 21)
+                };
+                calculator.CombinedFailureMechanismSectionAssemblyOutput = failureMechanismSectionAssembly;
+
+                // Call
+                IEnumerable<MapFeature> features = AssessmentSectionAssemblyMapDataFeaturesFactory.CreateCombinedFailureMechanismSectionAssemblyFeatures(assessmentSection);
+
+                // Assert
+                IEnumerable<CombinedFailureMechanismSectionAssemblyResult> expectedAssemblyResults =
+                    AssessmentSectionAssemblyFactory.AssembleCombinedPerFailureMechanismSection(assessmentSection, true);
+                int expectedNrOfResults = expectedAssemblyResults.Count();
+                Assert.AreEqual(expectedNrOfResults, features.Count());
+
+                for (var i = 0; i < expectedNrOfResults; i++)
+                {
+                    CombinedFailureMechanismSectionAssemblyResult expectedAssemblyResult = expectedAssemblyResults.ElementAt(i);
+                    MapFeature actualFeature = features.ElementAt(i);
+
+                    MapGeometry mapGeometry = actualFeature.MapGeometries.Single();
+                    Assert.AreEqual(0, mapGeometry.PointCollections.Single().Count());
+
+                    Assert.AreEqual(2, actualFeature.MetaData.Keys.Count);
+                    Assert.AreEqual(expectedAssemblyResult.SectionNumber, actualFeature.MetaData["Vaknummer"]);
+                    Assert.AreEqual(new EnumDisplayWrapper<DisplayFailureMechanismSectionAssemblyCategoryGroup>(
+                                        DisplayFailureMechanismSectionAssemblyCategoryGroupConverter.Convert(expectedAssemblyResult.TotalResult)).DisplayName,
+                                    features.ElementAt(i).MetaData["Categorie"]);
+                }
+            }
+        }
+
+        private static CombinedFailureMechanismSectionAssembly CreateCombinedFailureMechanismSectionAssembly(AssessmentSection assessmentSection, int seed)
+        {
+            var random = new Random(seed);
+            return new CombinedFailureMechanismSectionAssembly(new CombinedAssemblyFailureMechanismSection(random.NextDouble(),
+                                                                                                           random.NextDouble(),
+                                                                                                           random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>()),
+                                                               assessmentSection.GetFailureMechanisms()
+                                                                                .Where(fm => fm.IsRelevant)
+                                                                                .Select(fm => random.NextEnumValue<FailureMechanismSectionAssemblyCategoryGroup>()).ToArray());
+        }
+
+        private static void AssertEqualPointCollections(IEnumerable<Point2D> points, MapGeometry geometry)
+        {
+            CollectionAssert.AreEqual(points.Select(p => new Point2D(p)), geometry.PointCollections.First());
+        }
+    }
+}
