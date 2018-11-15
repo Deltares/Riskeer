0c45ab7d619015e9b177a38b37a01817dd4f2f25
Dennis Tang <dennis.tang@deltares.nl>
2018-11-15 13:14:12 +0100

    Added derived CategoryThemes. WTI-1887
    - Added derived themes to represent the individual styling of each possible data type.

---
+++ Core/Components/src/Core.Components.Gis/Theme/PointCategoryTheme.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Drawing;
+using Core.Components.Gis.Data;
+using Core.Components.Gis.Style;
+
+namespace Core.Components.Gis.Theme
+{
+    /// <summary>
+    /// Class to define categories for <see cref="MapPointData"/>.
+    /// </summary>
+    public class PointCategoryTheme : CategoryTheme
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="PointCategoryTheme"/>.
+        /// </summary>
+        /// <param name="criterion">The <see cref="ValueCriterion"/> belonging to the category.</param>
+        /// <param name="style">The <see cref="PointStyle"/> belonging to the category.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public PointCategoryTheme(ValueCriterion criterion, PointStyle style) : base(Color.AliceBlue, criterion)
+        {
+            if (style == null)
+            {
+                throw new ArgumentNullException(nameof(style));
+            }
+
+            Style = style;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="PointStyle"/> of the category.
+        /// </summary>
+        public PointStyle Style { get; }
+    }
+}
---
+++ Core/Components/test/Core.Components.Gis.Test/Theme/PointCategoryThemeTest.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Components.Gis.Style;
+using Core.Components.Gis.TestUtil;
+using Core.Components.Gis.Theme;
+using NUnit.Framework;
+
+namespace Core.Components.Gis.Test.Theme
+{
+    [TestFixture]
+    public class PointCategoryThemeTest
+    {
+        [Test]
+        public void Constructor_StyleNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new PointCategoryTheme(ValueCriterionTestFactory.CreateValueCriterion(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("style", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            ValueCriterion valueCriterion = ValueCriterionTestFactory.CreateValueCriterion();
+            var style = new PointStyle();
+
+            // Call
+            var category = new PointCategoryTheme(valueCriterion, style);
+
+            // Assert
+            Assert.IsInstanceOf<CategoryTheme>(category);
+            Assert.AreSame(valueCriterion, category.Criterion);
+            Assert.AreSame(style, category.Style);
+        }
+    }
+}
