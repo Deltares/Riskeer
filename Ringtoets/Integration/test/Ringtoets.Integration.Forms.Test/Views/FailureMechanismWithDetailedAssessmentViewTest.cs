3b1bfb86e0bdf64c0e04b1c9397f6eb77834b3f6
Robert Tangerman <robert.tangerman@deltares.nl>
2018-11-22 11:43:17 +0100

    Updated observers of stand alone views. WTI-977

--- Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/Views/FailureMechanismWithDetailedAssessmentViewTest.cs
+++ Ringtoets/Integration/test/Ringtoets.Integration.Forms.Test/Views/FailureMechanismWithDetailedAssessmentViewTest.cs
@@ -416,6 +416,47 @@ namespace Ringtoets.Integration.Forms.Test.Views
         }
 
         [Test]
+        public void UpdateObserver_AssessmentSectionUpdated_MapDataUpdated()
+        {
+            // Setup
+            var referenceLine = new ReferenceLine();
+            referenceLine.SetGeometry(new List<Point2D>
+            {
+                new Point2D(1.0, 2.0),
+                new Point2D(2.0, 1.0)
+            });
+
+            var assessmentSection = new AssessmentSectionStub
+            {
+                ReferenceLine = referenceLine
+            };
+
+            using (FailureMechanismWithDetailedAssessmentView<IHasSectionResults<FailureMechanismSectionResult>, FailureMechanismSectionResult> view =
+                CreateView(new TestFailureMechanism(), assessmentSection))
+            {
+                IMapControl map = ((RingtoetsMapControl) view.Controls[0]).MapControl;
+
+                var mocks = new MockRepository();
+                IObserver[] observers = AttachMapDataObservers(mocks, map.Data.Collection);
+                observers[referenceLineIndex].Expect(obs => obs.UpdateObserver());
+                mocks.ReplayAll();
+
+                MapData referenceLineMapData = map.Data.Collection.ElementAt(referenceLineIndex);
+
+                // Precondition
+                MapDataTestHelper.AssertReferenceLineMetaData(assessmentSection.ReferenceLine, assessmentSection, referenceLineMapData);
+
+                // Call
+                assessmentSection.Name = "New name";
+                assessmentSection.NotifyObservers();
+
+                // Assert
+                MapDataTestHelper.AssertReferenceLineMetaData(assessmentSection.ReferenceLine, assessmentSection, referenceLineMapData);
+                mocks.VerifyAll();
+            }
+        }
+
+        [Test]
         public void UpdateObserver_ReferenceLineUpdated_MapDataUpdated()
         {
             // Setup
@@ -447,12 +488,12 @@ namespace Ringtoets.Integration.Forms.Test.Views
                 MapDataTestHelper.AssertReferenceLineMapData(assessmentSection.ReferenceLine, referenceLineMapData);
 
                 // Call
-                assessmentSection.ReferenceLine.SetGeometry(new List<Point2D>
+                referenceLine.SetGeometry(new List<Point2D>
                 {
                     new Point2D(2.0, 5.0),
                     new Point2D(4.0, 3.0)
                 });
-                assessmentSection.NotifyObservers();
+                referenceLine.NotifyObservers();
 
                 // Assert
                 MapDataTestHelper.AssertReferenceLineMapData(assessmentSection.ReferenceLine, referenceLineMapData);
