d479aa88d66f9a5a61008baeb9d7e26a99fefa93
Dennis Tang <dennis.tang@deltares.nl>
2018-12-05 10:29:18 +0100

    Added HydraRing configuration class:  WTI-1923
    - Introduced to hold all general configuration information in order to perform a hydraring calculation.

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.Calculation/Data/Input/HydraRingCalculationSettings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Core.Common.Util;
+
+namespace Ringtoets.HydraRing.Calculation.Data.Input
+{
+    /// <summary>
+    /// Class which holds all the general information to run a Hydra-Ring calculation
+    /// </summary>
+    public class HydraRingCalculationSettings
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="HydraRingCalculationSettings"/>.
+        /// </summary>
+        /// <param name="hydraulicBoundaryDatabaseFilePath">The path which points to the hydraulic boundary
+        /// database file.</param>
+        /// <param name="hydraulicBoundaryLocationsConfigurationFilePath">The path which points to
+        /// the hydraulic boundary locations configuration file.</param>
+        /// <param name="preprocessorDirectory">The preprocessor directory to be used for the calculations.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="preprocessorDirectory"/>
+        /// is <c>null</c></exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="hydraulicBoundaryDatabaseFilePath"/>
+        /// or <paramref name="hydraulicBoundaryLocationsConfigurationFilePath"/>of the input parameters contains invalid
+        /// characters.</exception>
+        public HydraRingCalculationSettings(string hydraulicBoundaryDatabaseFilePath,
+                                            string hydraulicBoundaryLocationsConfigurationFilePath,
+                                            string preprocessorDirectory)
+        {
+            if (preprocessorDirectory == null)
+            {
+                throw new ArgumentNullException(nameof(preprocessorDirectory));
+            }
+
+            IOUtils.ValidateFilePath(hydraulicBoundaryDatabaseFilePath);
+            IOUtils.ValidateFilePath(hydraulicBoundaryLocationsConfigurationFilePath);
+
+            HydraulicBoundaryDatabaseFilePath = hydraulicBoundaryDatabaseFilePath;
+            HydraulicBoundaryLocationsConfigurationFilePath = hydraulicBoundaryLocationsConfigurationFilePath;
+            PreprocessorDirectory = preprocessorDirectory;
+        }
+
+        /// <summary>
+        /// Gets the hydraulic boundary database filepath.
+        /// </summary>
+        public string HydraulicBoundaryDatabaseFilePath { get; }
+
+        /// <summary>
+        /// Gets the hydraulic locations configurations filepath.
+        /// </summary>
+        public string HydraulicBoundaryLocationsConfigurationFilePath { get; }
+
+        /// <summary>
+        /// Gets the preprocessor director.
+        /// </summary>
+        public string PreprocessorDirectory { get; }
+    }
+}
---
+++ Ringtoets/HydraRing/test/Ringtoets.HydraRing.Calculation.Test/Data/Input/HydraRingCalculationSettingsTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.HydraRing.Calculation.Data.Input;
+
+namespace Ringtoets.HydraRing.Calculation.Test.Data.Input
+{
+    [TestFixture]
+    public class HydraRingCalculationSettingsTest
+    {
+        [Test]
+        [TestCaseSource(typeof(InvalidPathHelper), nameof(InvalidPathHelper.InvalidPaths))]
+        public void Constructor_WithInvalidHydraulicBoundaryDatabaseFilePath_ThrowsArgumentException(
+            string invalidHydraulicBoundaryDatabaseFilePath)
+        {
+            // Setup
+            string hlcdFilePath = TestHelper.GetScratchPadPath();
+            string preProcessorDirectory = TestHelper.GetScratchPadPath();
+
+            // Call
+            TestDelegate call = () => new HydraRingCalculationSettings(invalidHydraulicBoundaryDatabaseFilePath,
+                                                                       hlcdFilePath,
+                                                                       preProcessorDirectory);
+
+            // Assert
+            Assert.Throws<ArgumentException>(call);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(InvalidPathHelper), nameof(InvalidPathHelper.InvalidPaths))]
+        public void Constructor_WithInvalidHydraulicBoundaryLocationsConfigurationsFilePath_ThrowsArgumentException(
+            string invalidHlcdFilePath)
+        {
+            // Setup
+            string hydraulicBoundaryLocationsDatabaseFilePath = TestHelper.GetScratchPadPath();
+            string preProcessorDirectory = TestHelper.GetScratchPadPath();
+
+            // Call
+            TestDelegate call = () => new HydraRingCalculationSettings(hydraulicBoundaryLocationsDatabaseFilePath,
+                                                                       invalidHlcdFilePath,
+                                                                       preProcessorDirectory);
+
+            // Assert
+            Assert.Throws<ArgumentException>(call);
+        }
+
+        [Test]
+        public void Constructor_WithPreprocessorDirectoryNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            string hydraulicBoundaryLocationsDatabaseFilePath = TestHelper.GetScratchPadPath();
+            string hlcdFilePath = TestHelper.GetScratchPadPath();
+
+            // Call
+            TestDelegate call = () => new HydraRingCalculationSettings(hydraulicBoundaryLocationsDatabaseFilePath,
+                                                                       hlcdFilePath,
+                                                                       null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("preprocessorDirectory", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_WithArguments_ExpectedValues()
+        {
+            // Setup
+            string hydraulicBoundaryDatabaseFilePath = TestHelper.GetScratchPadPath();
+            string hlcdFilePath = TestHelper.GetScratchPadPath();
+            string preProcessorDirectory = TestHelper.GetScratchPadPath();
+
+            // Call
+            var settings = new HydraRingCalculationSettings(hydraulicBoundaryDatabaseFilePath,
+                                                            hlcdFilePath,
+                                                            preProcessorDirectory);
+
+            // Assert
+            Assert.AreEqual(hydraulicBoundaryDatabaseFilePath, settings.HydraulicBoundaryDatabaseFilePath);
+            Assert.AreEqual(hydraulicBoundaryDatabaseFilePath, settings.HydraulicBoundaryLocationsConfigurationFilePath);
+            Assert.AreEqual(preProcessorDirectory, settings.PreprocessorDirectory);
+        }
+    }
+}
