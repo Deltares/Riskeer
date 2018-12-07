71a73dccde6d14e69e1951a103e01a2d61e2e318
Robert Tangerman <robert.tangerman@deltares.nl>
2018-12-07 11:22:39 +0100

    Introduced new reader. WTI-1486

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.IO/HydraulicBoundaryDatabase/HydraulicBoundaryDatabaseReader.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Base.IO;
+using Core.Common.IO.Readers;
+
+namespace Ringtoets.HydraRing.IO.HydraulicBoundaryDatabase
+{
+    /// <summary>
+    /// This class reads a SqLite database file and constructs a <see cref="ReadHydraulicBoundaryDatabase"/>
+    /// instance from this database.
+    /// </summary>
+    public class HydraulicBoundaryDatabaseReader : SqLiteDatabaseReaderBase
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="HydraulicBoundaryDatabaseReader"/>,
+        /// which will use the <paramref name="databaseFilePath"/> as its source.
+        /// </summary>
+        /// <param name="databaseFilePath">The path of the database file to open.</param>
+        /// <exception cref="CriticalFileReadException">Thrown when:
+        /// <list type="bullet">
+        /// <item>The <paramref name="databaseFilePath"/> contains invalid characters.</item>
+        /// <item>No file could be found at <paramref name="databaseFilePath"/>.</item>
+        /// </list>
+        /// </exception>
+        public HydraulicBoundaryDatabaseReader(string databaseFilePath) : base(databaseFilePath) {}
+    }
+}
---
+++ Ringtoets/HydraRing/test/Ringtoets.HydraRing.IO.Test/HydraulicBoundaryDatabase/HydraulicBoundaryDatabaseReaderTest.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.IO;
+using Core.Common.Base.IO;
+using Core.Common.IO.Readers;
+using Core.Common.TestUtil;
+using Core.Common.Util.Builders;
+using NUnit.Framework;
+using Ringtoets.HydraRing.IO.HydraulicBoundaryDatabase;
+
+namespace Ringtoets.HydraRing.IO.Test.HydraulicBoundaryDatabase
+{
+    [TestFixture]
+    public class HydraulicBoundaryDatabaseReaderTest
+    {
+        private readonly string testDataPath = TestHelper.GetTestDataPath(TestDataPath.Ringtoets.HydraRing.IO, "HydraulicBoundaryDatabaseReader");
+
+        [Test]
+        public void Constructor_ValidFile_ExpectedValues()
+        {
+            // Setup
+            string hydraulicBoundaryDatabaseFile = Path.Combine(testDataPath, "complete.sqlite");
+
+            // Call
+            using (var hydraulicBoundaryDatabaseReader = new HydraulicBoundaryDatabaseReader(hydraulicBoundaryDatabaseFile))
+            {
+                // Assert
+                Assert.AreEqual(hydraulicBoundaryDatabaseFile, hydraulicBoundaryDatabaseReader.Path);
+                Assert.IsInstanceOf<SqLiteDatabaseReaderBase>(hydraulicBoundaryDatabaseReader);
+            }
+        }
+
+        [Test]
+        public void Constructor_NonExistingPath_ThrowsCriticalFileReadException()
+        {
+            // Setup
+            string hydraulicBoundaryDatabaseFile = Path.Combine(testDataPath, "doesNotExist.sqlite");
+
+            // Call
+            TestDelegate test = () =>
+            {
+                using (new HydraulicBoundaryDatabaseReader(hydraulicBoundaryDatabaseFile)) { }
+            };
+
+            // Assert
+            var exception = Assert.Throws<CriticalFileReadException>(test);
+            string expectedMessage = new FileReaderErrorMessageBuilder(hydraulicBoundaryDatabaseFile).Build("Het bestand bestaat niet.");
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public void Constructor_FileNullOrEmpty_ThrowsCriticalFileReadException(string hydraulicBoundaryDatabaseFile)
+        {
+            // Call
+            TestDelegate test = () =>
+            {
+                using (new HydraulicBoundaryDatabaseReader(hydraulicBoundaryDatabaseFile)) { }
+            };
+
+            // Assert
+            var exception = Assert.Throws<CriticalFileReadException>(test);
+            string expectedMessage = $"Fout bij het lezen van bestand '{hydraulicBoundaryDatabaseFile}': bestandspad mag niet leeg of ongedefinieerd zijn.";
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+    }
+}
