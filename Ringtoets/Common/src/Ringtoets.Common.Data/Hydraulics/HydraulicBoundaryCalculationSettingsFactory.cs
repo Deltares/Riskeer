1a75c0f703334eacfc82f695b38405ccb9313add
Dennis Tang <dennis.tang@deltares.nl>
2018-12-10 13:00:32 +0100

    Introduced factory to create the hydraulic calculation settings. WTI-1923
    - Object represents the Ringtoets counterparts of the HydraRing calculation settings.

---
+++ Ringtoets/Common/src/Ringtoets.Common.Data/Hydraulics/HydraulicBoundaryCalculationSettingsFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Ringtoets.Common.Data.Hydraulics
+{
+    /// <summary>
+    /// Factory to create instances of <see cref="HydraulicBoundaryCalculationSettings"/>.
+    /// </summary>
+    public static class HydraulicBoundaryCalculationSettingsFactory
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="HydraulicBoundaryCalculationSettings"/>
+        /// based on a <see cref="HydraulicBoundaryDatabase"/>.
+        /// </summary>
+        /// <param name="hydraulicBoundaryDatabase">The <see cref="HydraulicBoundaryDatabase"/>
+        /// to create a <see cref="HydraulicBoundaryCalculationSettings"/> for.</param>
+        /// <returns>The <see cref="HydraulicBoundaryCalculationSettings"/>.</returns>
+        public static HydraulicBoundaryCalculationSettings CreateSettings(HydraulicBoundaryDatabase hydraulicBoundaryDatabase)
+        {
+            if (hydraulicBoundaryDatabase == null)
+            {
+                throw new ArgumentNullException(nameof(hydraulicBoundaryDatabase));
+            }
+
+            string hydraulicBoundaryDatabaseFilePath = hydraulicBoundaryDatabase.FilePath;
+            string hydraulicBoundaryDatabaseDirectory = null;
+
+            if (!string.IsNullOrWhiteSpace(hydraulicBoundaryDatabaseFilePath))
+            {
+                string directory = Path.GetDirectoryName(hydraulicBoundaryDatabaseFilePath);
+                hydraulicBoundaryDatabaseDirectory = Path.Combine(directory, "HLCD.sqlite");
+            }
+
+            return new HydraulicBoundaryCalculationSettings(hydraulicBoundaryDatabaseFilePath,
+                                                            hydraulicBoundaryDatabaseDirectory,
+                                                            hydraulicBoundaryDatabase.EffectivePreprocessorDirectory());
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Data.Test/Hydraulics/HydraulicBoundaryCalculationSettingsFactoryTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Hydraulics;
+
+namespace Ringtoets.Common.Data.Test.Hydraulics
+{
+    [TestFixture]
+    public class HydraulicBoundaryCalculationSettingsFactoryTest
+    {
+        [Test]
+        public void CreateSettings_HydraulicBoundaryDatabaseNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => HydraulicBoundaryCalculationSettingsFactory.CreateSettings(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("hydraulicBoundaryDatabase", exception.ParamName);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void CreateSettings_WithHydraulicBoundaryDatabaseWithInvalidFilePath_ReturnsExpectedSettings(string filePath)
+        {
+            // Setup
+            var database = new HydraulicBoundaryDatabase
+            {
+                FilePath = filePath
+            };
+
+            // Call
+            HydraulicBoundaryCalculationSettings settings = HydraulicBoundaryCalculationSettingsFactory.CreateSettings(database);
+
+            // Assert
+            Assert.AreEqual(database.FilePath, settings.HydraulicBoundaryDatabaseFilePath);
+            Assert.IsNull(settings.HlcdFilePath);
+        }
+
+        [Test]
+        public void CreateSettings_WithHydraulicBoundaryDatabaseWithFilePath_ReturnsExpectedSettings()
+        {
+            // Setup
+            string testDataPath = TestHelper.GetTestDataPath(TestDataPath.Ringtoets.Integration.Service, "HydraRingCalculation");
+            string validFilePath = Path.Combine(testDataPath, "HRD dutch coast south.sqlite");
+            var database = new HydraulicBoundaryDatabase
+            {
+                FilePath = validFilePath
+            };
+
+            // Call
+            HydraulicBoundaryCalculationSettings settings = HydraulicBoundaryCalculationSettingsFactory.CreateSettings(database);
+
+            // Assert
+            Assert.AreEqual(validFilePath, settings.HydraulicBoundaryDatabaseFilePath);
+            Assert.AreEqual(Path.Combine(testDataPath, "HLCD.sqlite"), settings.HlcdFilePath);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetPreprocessorConfigurations))]
+        public void CreateSettings_WithHydraulicBoundaryDatabaseWithVariousPreprocessorConfigurations_ReturnsExpectedSettings(
+            HydraulicBoundaryDatabase database,
+            string expectedPreprocessorDirectory)
+        {
+            // Setup
+
+            // Call
+            HydraulicBoundaryCalculationSettings settings = HydraulicBoundaryCalculationSettingsFactory.CreateSettings(database);
+
+            // Assert
+            Assert.AreEqual(expectedPreprocessorDirectory, settings.PreprocessorDirectory);
+        }
+
+        private static IEnumerable<TestCaseData> GetPreprocessorConfigurations()
+        {
+            yield return new TestCaseData(new HydraulicBoundaryDatabase(),
+                                          string.Empty)
+                .SetName("UsePreprocessorFalse");
+
+            yield return new TestCaseData(new HydraulicBoundaryDatabase
+                {
+                    CanUsePreprocessor = true,
+                    PreprocessorDirectory = "Directory"
+                }, string.Empty)
+                .SetName("UsePreprocessorFalseWithPreprocessorDirectory");
+
+            const string preprocessorDirectory = "Directory";
+            yield return new TestCaseData(new HydraulicBoundaryDatabase
+                {
+                    CanUsePreprocessor = true,
+                    UsePreprocessor = true,
+                    PreprocessorDirectory = preprocessorDirectory
+                }, preprocessorDirectory)
+                .SetName("UsePreprocessorTrueWithPreprocessorDirectory");
+        }
+    }
+}
