59c26a72b1017bdb20e45a2301d8a109f5f16f5f
Dennis Tang <dennis.tang@deltares.nl>
2018-12-10 11:16:31 +0100

    Introduced Ringtoets object to hold HydraRing related calculation information. WTI-1923

---
+++ Ringtoets/Common/src/Ringtoets.Common.Data/Hydraulics/HydraulicBoundaryCalculationSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ringtoets.Common.Data.Hydraulics
+{
+    /// <summary>
+    /// Class which holds all hydraulic boundary calculations settings.
+    /// </summary>
+    public class HydraulicBoundaryCalculationSettings
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="HydraulicBoundaryCalculationSettings"/>.
+        /// </summary>
+        /// <param name="hydraulicBoundaryDatabaseFilePath">The file path of the hydraulic boundary database.</param>
+        /// <param name="hlcdFilePath">The file path to the hlcd.</param>
+        /// <param name="preprocessorDirectory">The preprocessor directory.</param>
+        public HydraulicBoundaryCalculationSettings(string hydraulicBoundaryDatabaseFilePath,
+                                                    string hlcdFilePath,
+                                                    string preprocessorDirectory)
+        {
+            HydraulicBoundaryDatabaseFilePath = hydraulicBoundaryDatabaseFilePath;
+            HlcdFilePath = hlcdFilePath;
+            PreprocessorDirectory = preprocessorDirectory;
+        }
+
+        /// <summary>
+        /// Gets the hydraulic boundary database filepath.
+        /// </summary>
+        public string HydraulicBoundaryDatabaseFilePath { get; }
+
+        /// <summary>
+        /// Gets the HLCD file path.
+        /// </summary>
+        public string HlcdFilePath { get; }
+
+        /// <summary>
+        ///  Gets the preprocessor directory.
+        /// </summary>
+        public string PreprocessorDirectory { get; }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Data.Test/Hydraulics/HydraulicBoundaryCalculationSettingsTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Hydraulics;
+
+namespace Ringtoets.Common.Data.Test.Hydraulics
+{
+    [TestFixture]
+    public class HydraulicBoundaryCalculationSettingsTest
+    {
+        [Test]
+        [TestCaseSource(nameof(GetTestCases))]
+        public void Constructor_WithArguments_ExpectedValues(string hydraulicBoundaryDatabaseFilePath,
+                                                             string hlcdFilePath,
+                                                             string preprocessorDirectory)
+        {
+            // Call
+            var settings = new HydraulicBoundaryCalculationSettings(hydraulicBoundaryDatabaseFilePath,
+                                                                    hlcdFilePath,
+                                                                    preprocessorDirectory);
+
+            // Assert
+            Assert.AreEqual(hydraulicBoundaryDatabaseFilePath, settings.HydraulicBoundaryDatabaseFilePath);
+            Assert.AreEqual(hlcdFilePath, settings.HlcdFilePath);
+            Assert.AreEqual(preprocessorDirectory, settings.PreprocessorDirectory);
+        }
+
+        private static IEnumerable<TestCaseData> GetTestCases()
+        {
+            yield return new TestCaseData("D:\\HydraulicBoundaryDatabase.sqlite",
+                                          "D:\\HLCD.sqlite",
+                                          "D:\\")
+                .SetName("All inputs with values");
+            yield return new TestCaseData("  ", "  ", "  ")
+                .SetName("All inputs whitespace");
+            yield return new TestCaseData(string.Empty, string.Empty, string.Empty)
+                .SetName("All inputs empty");
+            yield return new TestCaseData(null, null, null)
+                .SetName("All inputs null");
+        }
+    }
+}
