9135a5a4c5bab3c95b31ee7f5ba49c3e501a67c1
Robert Tangerman <robert.tangerman@deltares.nl>
2018-12-10 10:34:23 +0100

    Introduced read object for hydraulic location configuration database. WTI-1486

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.IO/HydraulicLocationConfigurationDatabase/ReadHydraulicLocationConfigurationDatabase.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+
+namespace Ringtoets.HydraRing.IO.HydraulicLocationConfigurationDatabase
+{
+    /// <summary>
+    /// Class for holding data that is read from a hydraulic location configuration database file.
+    /// </summary>
+    public class ReadHydraulicLocationConfigurationDatabase
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ReadHydraulicLocationConfigurationDatabase"/>.
+        /// </summary>
+        /// <param name="locationIds">The location ids of the read hydraulic location configuration database.</param>
+        internal ReadHydraulicLocationConfigurationDatabase(Dictionary<long, long> locationIds)
+        {
+            LocationIds = locationIds;
+        }
+
+        /// <summary>
+        /// Gets the location ids of the read hydraulic location configuration database.
+        /// </summary>
+        public Dictionary<long, long> LocationIds { get; }
+    }
+}
