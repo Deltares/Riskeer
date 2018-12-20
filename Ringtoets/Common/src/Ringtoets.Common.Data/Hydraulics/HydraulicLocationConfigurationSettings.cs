a09830acfa5617cc5ece25e8f3ffe8b234255710
Dennis Tang <dennis.tang@deltares.nl>
2018-12-20 14:07:06 +0100

    Added data object to hold HLCD relevant information. WTI-1938

---
+++ Ringtoets/Common/src/Ringtoets.Common.Data/Hydraulics/HydraulicLocationConfigurationSettings.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Ringtoets.Common.Data.Hydraulics {
+    /// <summary>
+    /// Class which holds information about the hydraulic
+    /// location configuration settings.
+    /// </summary>
+    public class HydraulicLocationConfigurationSettings
+    {
+        /// <summary>
+        /// Gets the scenario name.
+        /// </summary>
+        public string ScenarioName { get; private set; }
+
+        /// <summary>
+        /// Gets the year.
+        /// </summary>
+        public int Year { get; private set; }
+
+        /// <summary>
+        /// Gets the scope.
+        /// </summary>
+        public string Scope { get; private set; }
+
+        /// <summary>
+        /// Gets the sea level.
+        /// </summary>
+        public string SeaLevel { get; private set; }
+
+        /// <summary>
+        /// Gets the river discharge.
+        /// </summary>
+        public string RiverDischarge { get; private set; }
+
+        /// <summary>
+        /// Gets the lake level.
+        /// </summary>
+        public string LakeLevel { get; private set; }
+
+        /// <summary>
+        /// Gets the wind direction.
+        /// </summary>
+        public string WindDirection { get; private set; }
+
+        /// <summary>
+        /// Gets the wind speed.
+        /// </summary>
+        public string WindSpeed { get; private set; }
+
+        /// <summary>
+        /// Gets the comment.
+        /// </summary>
+        public string Comment { get; private set; }
+
+        /// <summary>
+        /// Sets values to the <see cref="HydraulicLocationConfigurationSettings"/>.
+        /// </summary>
+        /// <param name="scenarioName">The name of the scenario.</param>
+        /// <param name="year">The year.</param>
+        /// <param name="scope">The scope.</param>
+        /// <param name="seaLevel">The sea level.</param>
+        /// <param name="riverDischarge">The river discharge.</param>
+        /// <param name="lakeLevel">The lake level.</param>
+        /// <param name="windDirection">The wind direction.</param>
+        /// <param name="windSpeed">The wind speed.</param>
+        /// <param name="comment">The comment.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="scenarioName"/>
+        /// or <paramref name="scope"/> are <c>null</c>.</exception>
+        public void SetValues(string scenarioName, int year, string scope, 
+                              string seaLevel, string riverDischarge, string lakeLevel,
+                              string windDirection, string windSpeed, string comment)
+        {
+            if (scenarioName == null)
+            {
+                throw new ArgumentNullException(nameof(scenarioName));
+            }
+
+            if (scope == null)
+            {
+                throw new ArgumentNullException(nameof(scope));
+            }
+
+            ScenarioName = scenarioName;
+            Year = year;
+            Scope = scope;
+            SeaLevel = seaLevel;
+            RiverDischarge = riverDischarge;
+            LakeLevel = lakeLevel;
+            WindDirection = windDirection;
+            WindSpeed = windSpeed;
+            Comment = comment;
+        }
+    }
+}
---
+++ Ringtoets/Common/test/Ringtoets.Common.Data.Test/Hydraulics/HydraulicLocationConfigurationSettingsTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Hydraulics;
+
+namespace Ringtoets.Common.Data.Test.Hydraulics
+{
+    [TestFixture]
+    public class HydraulicLocationConfigurationSettingsTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var settings = new HydraulicLocationConfigurationSettings();
+
+            // Assert
+            Assert.IsNull(settings.ScenarioName);
+            Assert.Zero(settings.Year);
+            Assert.IsNull(settings.Scope);
+            Assert.IsNull(settings.SeaLevel);
+            Assert.IsNull(settings.RiverDischarge);
+            Assert.IsNull(settings.LakeLevel);
+            Assert.IsNull(settings.WindDirection);
+            Assert.IsNull(settings.WindSpeed);
+            Assert.IsNull(settings.Comment);
+        }
+
+        [Test]
+        public void SetValues_ScenarioNameNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+            int year = random.Next();
+            const string scope = "Scope";
+            const string seaLevel = "SeaLevel";
+            const string riverDischarge = "RiverDischarge";
+            const string lakeLevel = "LakeLevel";
+            const string windDirection = "WindDirection";
+            const string windSpeed = "WindSpeed";
+            const string comment = "Comment";
+
+            var settings = new HydraulicLocationConfigurationSettings();
+
+            // Call
+            TestDelegate call = () => settings.SetValues(null, year, scope,
+                                                         seaLevel, riverDischarge, lakeLevel,
+                                                         windDirection, windSpeed, comment);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("scenarioName", exception.ParamName);
+        }
+
+        [Test]
+        public void SetValues_ScopeNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+            const string scenarioName = "ScenarioName";
+            int year = random.Next();
+            const string seaLevel = "SeaLevel";
+            const string riverDischarge = "RiverDischarge";
+            const string lakeLevel = "LakeLevel";
+            const string windDirection = "WindDirection";
+            const string windSpeed = "WindSpeed";
+            const string comment = "Comment";
+
+            var settings = new HydraulicLocationConfigurationSettings();
+
+            // Call
+            TestDelegate call = () => settings.SetValues(scenarioName, year, null,
+                                                         seaLevel, riverDischarge, lakeLevel,
+                                                         windDirection, windSpeed, comment);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("scope", exception.ParamName);
+        }
+
+        [Test]
+        public void SetValues_WithArguments_SetsExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            const string scenarioName = "ScenarioName";
+            int year = random.Next();
+            const string scope = "Scope";
+            const string seaLevel = "SeaLevel";
+            const string riverDischarge = "RiverDischarge";
+            const string lakeLevel = "LakeLevel";
+            const string windDirection = "WindDirection";
+            const string windSpeed = "WindSpeed";
+            const string comment = "Comment";
+
+            var settings = new HydraulicLocationConfigurationSettings();
+
+            // Call
+            settings.SetValues(scenarioName, year, scope,
+                               seaLevel, riverDischarge, lakeLevel,
+                               windDirection, windSpeed, comment);
+
+            // Assert
+            Assert.AreEqual(scenarioName, settings.ScenarioName);
+            Assert.AreEqual(year, settings.Year);
+            Assert.AreEqual(scope, settings.Scope);
+            Assert.AreEqual(seaLevel, settings.SeaLevel);
+            Assert.AreEqual(riverDischarge, settings.RiverDischarge);
+            Assert.AreEqual(lakeLevel, settings.LakeLevel);
+            Assert.AreEqual(windDirection, settings.WindDirection);
+            Assert.AreEqual(windSpeed, settings.WindSpeed);
+            Assert.AreEqual(comment, settings.Comment);
+        }
+    }
+}
