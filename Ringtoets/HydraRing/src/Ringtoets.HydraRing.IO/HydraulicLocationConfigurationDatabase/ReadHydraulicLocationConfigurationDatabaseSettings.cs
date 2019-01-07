bfe1469d892b8690f3164a472069902e17d7e60c
Dennis Tang <dennis.tang@deltares.nl>
2019-01-07 14:04:07 +0100

    Introduced read object. WTI-1925

---
+++ Ringtoets/HydraRing/src/Ringtoets.HydraRing.IO/HydraulicLocationConfigurationDatabase/ReadHydraulicLocationConfigurationDatabaseSettings.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Ringtoets.HydraRing.IO.HydraulicLocationConfigurationDatabase
+{
+    /// <summary>
+    /// Class for holding configuration settings that are read from a hydraulic location
+    /// configuration database file.
+    /// </summary>
+    public class ReadHydraulicLocationConfigurationDatabaseSettings
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ReadHydraulicLocationConfigurationDatabaseSettings"/>.
+        /// </summary>
+        /// <param name="scenarioName">The name of the scenario.</param>
+        /// <param name="year">The year.</param>
+        /// <param name="scope">The scope.</param>
+        /// <param name="seaLevel">The sea level.</param>
+        /// <param name="riverDischarge">The river discharge.</param>
+        /// <param name="lakeLevel">The lake level.</param>
+        /// <param name="windDirection">The wind direction.</param>
+        /// <param name="windSpeed">The wind speed.</param>
+        /// <param name="comment">The comment.</param>
+        internal ReadHydraulicLocationConfigurationDatabaseSettings(string scenarioName, int year, string scope,
+                                                                    string seaLevel, string riverDischarge, string lakeLevel,
+                                                                    string windDirection, string windSpeed, string comment)
+        {
+            ScenarioName = scenarioName;
+            Year = year;
+            Scope = scope;
+            SeaLevel = seaLevel;
+            RiverDischarge = riverDischarge;
+            LakeLevel = lakeLevel;
+            WindDirection = windDirection;
+            WindSpeed = windSpeed;
+            Comment = comment;
+        }
+
+        /// <summary>
+        /// Gets the scenario name.
+        /// </summary>
+        public string ScenarioName { get; }
+
+        /// <summary>
+        /// Gets the year.
+        /// </summary>
+        public int Year { get; }
+
+        /// <summary>
+        /// Gets the scope.
+        /// </summary>
+        public string Scope { get; }
+
+        /// <summary>
+        /// Gets the sea level.
+        /// </summary>
+        public string SeaLevel { get; }
+
+        /// <summary>
+        /// Gets the river discharge.
+        /// </summary>
+        public string RiverDischarge { get; }
+
+        /// <summary>
+        /// Gets the lake level.
+        /// </summary>
+        public string LakeLevel { get; }
+
+        /// <summary>
+        /// Gets the wind direction.
+        /// </summary>
+        public string WindDirection { get; }
+
+        /// <summary>
+        /// Gets the wind speed.
+        /// </summary>
+        public string WindSpeed { get; }
+
+        /// <summary>
+        /// Gets the comment.
+        /// </summary>
+        public string Comment { get; }
+    }
+}
---
+++ Ringtoets/HydraRing/test/Ringtoets.HydraRing.IO.Test/HydraulicLocationConfigurationDatabase/ReadHydraulicLocationConfigurationDatabaseSettingsTest.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+using Ringtoets.HydraRing.IO.HydraulicLocationConfigurationDatabase;
+
+namespace Ringtoets.HydraRing.IO.Test.HydraulicLocationConfigurationDatabase
+{
+    [TestFixture]
+    public class ReadHydraulicLocationConfigurationDatabaseSettingsTest
+    {
+        [Test]
+        public void Constructor_WithArgumentsNull_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            int year = random.Next();
+
+            // Call
+            var settings = new ReadHydraulicLocationConfigurationDatabaseSettings(null, year, null, null, null,
+                                                                                  null, null, null, null);
+
+            // Assert
+            Assert.IsNull(settings.ScenarioName);
+            Assert.AreEqual(year, settings.Year);
+            Assert.IsNull(settings.Scope);
+            Assert.IsNull(settings.SeaLevel);
+            Assert.IsNull(settings.RiverDischarge);
+            Assert.IsNull(settings.LakeLevel);
+            Assert.IsNull(settings.WindDirection);
+            Assert.IsNull(settings.WindSpeed);
+            Assert.IsNull(settings.Comment);
+        }
+
+        [Test]
+        public void Constructor_WithArgumentValues_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            const string scenarioName = "ScenarioName";
+            int year = random.Next();
+            const string scope = "Scope";
+            const string seaLevel = "SeaLevel";
+            const string riverDischarge = "RiverDischarge";
+            const string lakeLevel = "LakeLevel";
+            const string windDirection = "WindDirection";
+            const string windSpeed = "WindSpeed";
+            const string comment = "Comment";
+
+            // Call
+            var settings = new ReadHydraulicLocationConfigurationDatabaseSettings(scenarioName, year, scope,
+                                                                                  seaLevel, riverDischarge, lakeLevel,
+                                                                                  windDirection, windSpeed, comment);
+
+            // Assert
+            Assert.AreEqual(scenarioName, settings.ScenarioName);
+            Assert.AreEqual(year, settings.Year);
+            Assert.AreEqual(scope, settings.Scope);
+            Assert.AreEqual(seaLevel, settings.SeaLevel);
+            Assert.AreEqual(riverDischarge, settings.RiverDischarge);
+            Assert.AreEqual(lakeLevel, settings.LakeLevel);
+            Assert.AreEqual(windDirection, settings.WindDirection);
+            Assert.AreEqual(windSpeed, settings.WindSpeed);
+            Assert.AreEqual(comment, settings.Comment);
+        }
+    }
+}
