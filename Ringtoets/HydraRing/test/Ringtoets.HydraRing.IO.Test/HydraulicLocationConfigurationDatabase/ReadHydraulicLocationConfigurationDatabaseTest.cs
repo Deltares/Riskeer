eabdcb34e6ceed7c9729de715cba30dcb946bc93
Dennis Tang <dennis.tang@deltares.nl>
2019-01-08 15:49:47 +0100

    Fixed compilation error after rebase. WTI-1925

--- Ringtoets/HydraRing/test/Ringtoets.HydraRing.IO.Test/HydraulicLocationConfigurationDatabase/ReadHydraulicLocationConfigurationDatabaseTest.cs
+++ Ringtoets/HydraRing/test/Ringtoets.HydraRing.IO.Test/HydraulicLocationConfigurationDatabase/ReadHydraulicLocationConfigurationDatabaseTest.cs
@@ -19,6 +19,7 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Core.Common.TestUtil;
