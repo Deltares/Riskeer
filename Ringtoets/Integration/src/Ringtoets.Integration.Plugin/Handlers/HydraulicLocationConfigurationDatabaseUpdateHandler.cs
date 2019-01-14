c14610150a92e51bb9c1df0b7468fca4109ae7cc
Robert Tangerman <robert.tangerman@deltares.nl>
2019-01-14 11:56:53 +0100

    Introduced update handler. WTI-1927

---
+++ Ringtoets/Integration/src/Ringtoets.Integration.Plugin/Handlers/HydraulicLocationConfigurationDatabaseUpdateHandler.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+using Core.Common.Base;
+using Ringtoets.Common.Data.Hydraulics;
+using Ringtoets.HydraRing.IO.HydraulicLocationConfigurationDatabase;
+using Ringtoets.Integration.IO.Handlers;
+
+namespace Ringtoets.Integration.Plugin.Handlers
+{
+    /// <summary>
+    /// Class that can properly update a <see cref="HydraulicLocationConfigurationSettings"/>.
+    /// </summary>
+    public class HydraulicLocationConfigurationDatabaseUpdateHandler : IHydraulicLocationConfigurationDatabaseUpdateHandler
+    {
+        public bool InquireConfirmation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IObservable> Update(HydraulicLocationConfigurationSettings hydraulicLocationConfigurationSettings,
+                                               ReadHydraulicLocationConfigurationDatabaseSettings readHydraulicLocationConfigurationDatabaseSettings,
+                                               string hlcdFilePath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
