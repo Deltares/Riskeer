2d850ad8de01dd4896ada4999d18f787165ce644
Dennis Tang <dennis.tang@deltares.nl>
2019-01-23 11:00:49 +0100

    Renamed Ringtoets --> Riskeer in Storage: WTI-1931
    - Renamed directory

---
+++ Ringtoets/Storage/test/Riskeer.Storage.Core.Test/Create/FailureMechanismCreateExtensionsTest.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Common.Data.FailureMechanism;
+using Ringtoets.Common.Data.TestUtil;
+using Riskeer.Storage.Core.Create;
+using Riskeer.Storage.Core.DbContext;
+
+namespace Riskeer.Storage.Core.Test.Create
+{
+    [TestFixture]
+    public class FailureMechanismCreateExtensionsTest
+    {
+        [Test]
+        public void AddEntitiesForFailureMechanismSections_WithoutCollector_ThrowsArgumentNullException()
+        {
+            // Setup
+            var failureMechanism = new TestFailureMechanism();
+
+            // Call
+            TestDelegate test = () => failureMechanism.AddEntitiesForFailureMechanismSections(null, new FailureMechanismEntity());
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(test).ParamName;
+            Assert.AreEqual("registry", paramName);
+        }
+
+        [Test]
+        public void AddEntitiesForFailureMechanismSections_WithoutEntity_ThrowsArgumentNullException()
+        {
+            // Setup
+            var failureMechanism = new TestFailureMechanism();
+
+            // Call
+            TestDelegate test = () => failureMechanism.AddEntitiesForFailureMechanismSections(new PersistenceRegistry(), null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(test).ParamName;
+            Assert.AreEqual("entity", paramName);
+        }
+
+        [Test]
+        public void AddEntitiesForFailureMechanismSections_WithoutSections_EmptyFailureMechanismSectionEntities()
+        {
+            // Setup
+            var failureMechanism = new TestFailureMechanism();
+            var failureMechanismEntity = new FailureMechanismEntity();
+
+            // Call
+            failureMechanism.AddEntitiesForFailureMechanismSections(new PersistenceRegistry(), failureMechanismEntity);
+
+            // Assert
+            CollectionAssert.IsEmpty(failureMechanismEntity.FailureMechanismSectionEntities);
+        }
+
+        [Test]
+        public void AddEntitiesForFailureMechanismSections_WithSections_FailureMechanismSectionEntitiesCreated()
+        {
+            // Setup
+            var failureMechanism = new TestFailureMechanism();
+            FailureMechanismTestHelper.SetSections(failureMechanism, new[]
+            {
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection()
+            });
+            var failureMechanismEntity = new FailureMechanismEntity();
+
+            // Call
+            failureMechanism.AddEntitiesForFailureMechanismSections(new PersistenceRegistry(), failureMechanismEntity);
+
+            // Assert
+            Assert.AreEqual(1, failureMechanismEntity.FailureMechanismSectionEntities.Count);
+        }
+
+        [Test]
+        public void Create_StringPropertiesDoNotShareReference()
+        {
+            // Setup
+            const string originalInput = "Some input text";
+            const string originalOutput = "Some output text";
+            const string originalNotRelevantText = "Really not relevant";
+            const string failureMechanismSectionsSourcePath = "File\\Path";
+            IFailureMechanism failureMechanism = new TestFailureMechanism("a", "cool");
+            failureMechanism.InputComments.Body = originalInput;
+            failureMechanism.OutputComments.Body = originalOutput;
+            failureMechanism.NotRelevantComments.Body = originalNotRelevantText;
+            failureMechanism.SetSections(new[]
+            {
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection()
+            }, failureMechanismSectionsSourcePath);
+
+            var registry = new PersistenceRegistry();
+
+            // Call
+            FailureMechanismEntity entity = failureMechanism.Create(FailureMechanismType.DuneErosion, registry);
+
+            // Assert
+            TestHelper.AssertAreEqualButNotSame(failureMechanism.InputComments.Body, entity.InputComments);
+            TestHelper.AssertAreEqualButNotSame(failureMechanism.OutputComments.Body, entity.OutputComments);
+            TestHelper.AssertAreEqualButNotSame(failureMechanism.NotRelevantComments.Body, entity.NotRelevantComments);
+            TestHelper.AssertAreEqualButNotSame(failureMechanism.FailureMechanismSectionSourcePath, entity.FailureMechanismSectionCollectionSourcePath);
+        }
+    }
+}
---
+++ Ringtoets/Storage/test/Riskeer.Storage.Core.Test/Create/WaveImpactAsphaltCover/WaveImpactAsphaltCoverFailureMechanismCreateExtensionsTest.cs
@@ -0,0 +1,237 @@
+﻿// Copyright (C) Stichting Deltares 2018. All rights reserved.
+//
+// This file is part of Ringtoets.
+//
+// Ringtoets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Ringtoets.Common.Data.Calculation;
+using Ringtoets.Common.Data.TestUtil;
+using Ringtoets.WaveImpactAsphaltCover.Data;
+using Riskeer.Storage.Core.Create;
+using Riskeer.Storage.Core.Create.WaveImpactAsphaltCover;
+using Riskeer.Storage.Core.DbContext;
+
+namespace Riskeer.Storage.Core.Test.Create.WaveImpactAsphaltCover
+{
+    [TestFixture]
+    public class WaveImpactAsphaltCoverFailureMechanismCreateExtensionsTest
+    {
+        [Test]
+        public void Create_PersistenceRegistryNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var failureMechanism = new WaveImpactAsphaltCoverFailureMechanism();
+
+            // Call
+            TestDelegate test = () => failureMechanism.Create(null);
+
+            // Assert
+            string paramName = Assert.Throws<ArgumentNullException>(test).ParamName;
+            Assert.AreEqual("registry", paramName);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void Create_WithCollectorAndPropertiesSet_ReturnsFailureMechanismEntityWithPropertiesSet(bool isRelevant)
+        {
+            // Setup
+            var failureMechanism = new WaveImpactAsphaltCoverFailureMechanism
+            {
+                IsRelevant = isRelevant,
+                InputComments =
+                {
+                    Body = "Some input text"
+                },
+                OutputComments =
+                {
+                    Body = "Some output text"
+                },
+                NotRelevantComments =
+                {
+                    Body = "Really not relevant"
+                },
+                GeneralWaveImpactAsphaltCoverInput =
+                {
+                    DeltaL = new Random(39).NextRoundedDouble(0.1, 2000.0)
+                }
+            };
+            var registry = new PersistenceRegistry();
+
+            // Call
+            FailureMechanismEntity entity = failureMechanism.Create(registry);
+
+            // Assert
+            Assert.IsNotNull(entity);
+            Assert.AreEqual((short) FailureMechanismType.WaveImpactOnAsphaltRevetment, entity.FailureMechanismType);
+            Assert.AreEqual(Convert.ToByte(isRelevant), entity.IsRelevant);
+            Assert.AreEqual(failureMechanism.InputComments.Body, entity.InputComments);
+            Assert.AreEqual(failureMechanism.OutputComments.Body, entity.OutputComments);
+            Assert.AreEqual(failureMechanism.NotRelevantComments.Body, entity.NotRelevantComments);
+            Assert.AreEqual(failureMechanism.GeneralWaveImpactAsphaltCoverInput.DeltaL,
+                            entity.WaveImpactAsphaltCoverFailureMechanismMetaEntities.Single().DeltaL);
+        }
+
+        [Test]
+        public void Create_StringPropertiesDoNotShareReference()
+        {
+            // Setup
+            const string originalInput = "Some input text";
+            const string originalOutput = "Some output text";
+            const string originalNotRelevantText = "Really not relevant";
+            var failureMechanism = new WaveImpactAsphaltCoverFailureMechanism
+            {
+                InputComments =
+                {
+                    Body = originalInput
+                },
+                OutputComments =
+                {
+                    Body = originalOutput
+                },
+                NotRelevantComments =
+                {
+                    Body = originalNotRelevantText
+                }
+            };
+            var registry = new PersistenceRegistry();
+
+            // Call
+            FailureMechanismEntity entity = failureMechanism.Create(registry);
+
+            // Assert
+            TestHelper.AssertAreEqualButNotSame(failureMechanism.InputComments.Body, entity.InputComments);
+            TestHelper.AssertAreEqualButNotSame(failureMechanism.OutputComments.Body, entity.OutputComments);
+            TestHelper.AssertAreEqualButNotSame(failureMechanism.NotRelevantComments.Body, entity.NotRelevantComments);
+        }
+
+        [Test]
+        public void Create_WithoutSections_EmptyFailureMechanismSectionEntities()
+        {
+            // Setup
+            var failureMechanism = new WaveImpactAsphaltCoverFailureMechanism();
+
+            // Call
+            FailureMechanismEntity entity = failureMechanism.Create(new PersistenceRegistry());
+
+            // Assert
+            CollectionAssert.IsEmpty(entity.FailureMechanismSectionEntities);
+            Assert.IsNull(entity.FailureMechanismSectionCollectionSourcePath);
+        }
+
+        [Test]
+        public void Create_WithSections_FailureMechanismSectionEntitiesCreated()
+        {
+            // Setup
+            const string filePath = "failureMechanismSections/File/Path";
+            var failureMechanism = new WaveImpactAsphaltCoverFailureMechanism();
+            failureMechanism.SetSections(new[]
+            {
+                FailureMechanismSectionTestFactory.CreateFailureMechanismSection()
+            }, filePath);
+
+            // Call
+            FailureMechanismEntity entity = failureMechanism.Create(new PersistenceRegistry());
+
+            // Assert
+            Assert.AreEqual(1, entity.FailureMechanismSectionEntities.Count);
+            Assert.AreEqual(1, entity.FailureMechanismSectionEntities.SelectMany(fms => fms.WaveImpactAsphaltCoverSectionResultEntities).Count());
+            TestHelper.AssertAreEqualButNotSame(filePath, entity.FailureMechanismSectionCollectionSourcePath);
+        }
+
+        [Test]
+        public void Create_WithoutForeshoreProfiles_EmptyForeshoreProfilesEntities()
+        {
+            // Setup
+            var failureMechanism = new WaveImpactAsphaltCoverFailureMechanism();
+
+            // Call
+            FailureMechanismEntity entity = failureMechanism.Create(new PersistenceRegistry());
+
+            // Assert
+            CollectionAssert.IsEmpty(entity.ForeshoreProfileEntities);
+
+            WaveImpactAsphaltCoverFailureMechanismMetaEntity metaEntity =
+                entity.WaveImpactAsphaltCoverFailureMechanismMetaEntities.Single();
+            Assert.IsNull(metaEntity.ForeshoreProfileCollectionSourcePath);
+        }
+
+        [Test]
+        public void Create_WithForeshoreProfiles_ForeshoreProfilesEntitiesCreated()
+        {
+            // Setup
+            var failureMechanism = new WaveImpactAsphaltCoverFailureMechanism();
+            const string filePath = "some/path/to/foreshoreProfiles";
+            failureMechanism.ForeshoreProfiles.AddRange(new[]
+            {
+                new TestForeshoreProfile()
+            }, filePath);
+
+            // Call
+            FailureMechanismEntity entity = failureMechanism.Create(new PersistenceRegistry());
+
+            // Assert
+            Assert.AreEqual(1, entity.ForeshoreProfileEntities.Count);
+
+            WaveImpactAsphaltCoverFailureMechanismMetaEntity metaEntity =
+                entity.WaveImpactAsphaltCoverFailureMechanismMetaEntities.Single();
+            string metaEntityForeshoreProfileCollectionSourcePath = metaEntity.ForeshoreProfileCollectionSourcePath;
+            TestHelper.AssertAreEqualButNotSame(filePath, metaEntityForeshoreProfileCollectionSourcePath);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void Create_WithCalculationGroup_ReturnFailureMechanismEntityWithCalculationGroupEntities(bool isRelevant)
+        {
+            // Setup
+            var failureMechanism = new WaveImpactAsphaltCoverFailureMechanism();
+            failureMechanism.WaveConditionsCalculationGroup.Children.Add(new CalculationGroup
+            {
+                Name = "A"
+            });
+            failureMechanism.WaveConditionsCalculationGroup.Children.Add(new CalculationGroup
+            {
+                Name = "B"
+            });
+
+            var registry = new PersistenceRegistry();
+
+            // Call
+            FailureMechanismEntity entity = failureMechanism.Create(registry);
+
+            // Assert
+            Assert.IsNotNull(entity);
+            Assert.AreEqual(failureMechanism.WaveConditionsCalculationGroup.Name, entity.CalculationGroupEntity.Name);
+            Assert.AreEqual(0, entity.CalculationGroupEntity.Order);
+
+            CalculationGroupEntity[] childGroupEntities = entity.CalculationGroupEntity.CalculationGroupEntity1
+                                                                .OrderBy(cge => cge.Order)
+                                                                .ToArray();
+            Assert.AreEqual(2, childGroupEntities.Length);
+            Assert.AreEqual("A", childGroupEntities[0].Name);
+            Assert.AreEqual(0, childGroupEntities[0].Order);
+            Assert.AreEqual("B", childGroupEntities[1].Name);
+            Assert.AreEqual(1, childGroupEntities[1].Order);
+        }
+    }
+}
