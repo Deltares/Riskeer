e67b6d5cf05451bc3355b4fd83ba813f84cb3295
Dennis Tang <dennis.tang@deltares.nl>
2019-01-25 09:06:28 +0100

    Renamed Ringtoets --> Riskeer in test/Ringtoets.AssemblyTool : WTI-1931
    - Renamed IO.TestUtil project
    - Fixed default namespace and assembly information
    -- Note: the namespaces of the sourcefiles were NOT fixed as part of this commit

--- Ringtoets/AssemblyTool/test/Ringtoets.AssemblyTool.IO.TestUtil/Properties/AssemblyInfo.cs
+++ Ringtoets/AssemblyTool/test/Ringtoets.AssemblyTool.IO.TestUtil/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyTitle("Ringtoets.AssemblyTool.IO.TestUtil")]
-[assembly: AssemblyProduct("Ringtoets.AssemblyTool.IO.TestUtil")]
+[assembly: AssemblyTitle("Riskeer.AssemblyTool.IO.TestUtil")]
+[assembly: AssemblyProduct("Riskeer.AssemblyTool.IO.TestUtil")]
