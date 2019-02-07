0fcb2757dba40aee48c8f924908c0bf2a522a672
Robert Tangerman <robert.tangerman@deltares.nl>
2019-02-07 13:47:28 +0100

    Introduced enum for stability stone cover calculation type. WTI-1801

--- Riskeer/StabilityStoneCover/src/Riskeer.StabilityStoneCover.Data/Properties/Resources.Designer.cs
+++ Riskeer/StabilityStoneCover/src/Riskeer.StabilityStoneCover.Data/Properties/Resources.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+// Copyright (C) Stichting Deltares 2019. All rights reserved.
 //
 // This file is part of Riskeer.
 //
@@ -40,7 +40,7 @@ namespace Riskeer.StabilityStoneCover.Data.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -96,6 +96,33 @@ namespace Riskeer.StabilityStoneCover.Data.Properties {
         public static string StabilityStoneCoverFailureMechanism_DisplayName {
             get {
                 return ResourceManager.GetString("StabilityStoneCoverFailureMechanism_DisplayName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Steen (blokken).
+        /// </summary>
+        public static string StabilityStoneCoverWaveConditionsCalculationType_Blocks_DisplayName {
+            get {
+                return ResourceManager.GetString("StabilityStoneCoverWaveConditionsCalculationType_Blocks_DisplayName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Steen (blokken en zuilen).
+        /// </summary>
+        public static string StabilityStoneCoverWaveConditionsCalculationType_Both_DisplayName {
+            get {
+                return ResourceManager.GetString("StabilityStoneCoverWaveConditionsCalculationType_Both_DisplayName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Steen (zuilen).
+        /// </summary>
+        public static string StabilityStoneCoverWaveConditionsCalculationType_Columns_DisplayName {
+            get {
+                return ResourceManager.GetString("StabilityStoneCoverWaveConditionsCalculationType_Columns_DisplayName", resourceCulture);
             }
         }
     }
---
+++ Riskeer/StabilityStoneCover/src/Riskeer.StabilityStoneCover.Data/StabilityStoneCoverWaveConditionsCalculationType.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Util.Attributes;
+using Riskeer.StabilityStoneCover.Data.Properties;
+
+namespace Riskeer.StabilityStoneCover.Data
+{
+    /// <summary>
+    /// Defines the various types of stability stone cover wave conditions calculations.
+    /// </summary>
+    public enum StabilityStoneCoverWaveConditionsCalculationType
+    {
+        /// <summary>
+        /// Calculate the columns.
+        /// </summary>
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.StabilityStoneCoverWaveConditionsCalculationType_Columns_DisplayName))]
+        Columns = 1,
+
+        /// <summary>
+        /// Calculate the blocks.
+        /// </summary>
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.StabilityStoneCoverWaveConditionsCalculationType_Blocks_DisplayName))]
+        Blocks = 2,
+
+        /// <summary>
+        /// Calculate both the blocks and columns.
+        /// </summary>
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.StabilityStoneCoverWaveConditionsCalculationType_Both_DisplayName))]
+        Both = 3
+    }
+}
