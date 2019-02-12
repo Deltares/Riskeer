cfbf1c5e0e7dafce27ae4d0205097963a973f78d
Robert Tangerman <robert.tangerman@deltares.nl>
2019-02-12 10:28:24 +0100

    Created factory for output. WTI-1805

---
+++ Riskeer/StabilityStoneCover/src/Riskeer.StabilityStoneCover.Data/StabilityStoneCoverWaveConditionsOutputFactory.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Riskeer.Revetment.Data;
+
+namespace Riskeer.StabilityStoneCover.Data
+{
+    /// <summary>
+    /// Factory for creating instances of <see cref="StabilityStoneCoverWaveConditionsOutput"/>.
+    /// </summary>
+    public static class StabilityStoneCoverWaveConditionsOutputFactory
+    {
+        /// <summary>
+        /// Creates <see cref="StabilityStoneCoverWaveConditionsOutput"/> with blocks set.
+        /// </summary>
+        /// <param name="blocks">The blocks output to set.</param>
+        /// <returns>The created <see cref="StabilityStoneCoverWaveConditionsOutput"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="blocks"/>
+        /// is <c>null</c>.</exception>
+        public static StabilityStoneCoverWaveConditionsOutput CreateOutputWithBlocks(IEnumerable<WaveConditionsOutput> blocks)
+        {
+            if (blocks == null)
+            {
+                throw new ArgumentNullException(nameof(blocks));
+            }
+
+            return new StabilityStoneCoverWaveConditionsOutput(null, blocks);
+        }
+
+        /// <summary>
+        /// Creates <see cref="StabilityStoneCoverWaveConditionsOutput"/> with columns set.
+        /// </summary>
+        /// <param name="columns">The columns output to set.</param>
+        /// <returns>The created <see cref="StabilityStoneCoverWaveConditionsOutput"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="columns"/>
+        /// is <c>null</c>.</exception>
+        public static StabilityStoneCoverWaveConditionsOutput CreateOutputWithColumns(IEnumerable<WaveConditionsOutput> columns)
+        {
+            if (columns == null)
+            {
+                throw new ArgumentNullException(nameof(columns));
+            }
+
+            return new StabilityStoneCoverWaveConditionsOutput(columns, null);
+        }
+
+        /// <summary>
+        /// Creates <see cref="StabilityStoneCoverWaveConditionsOutput"/> with columns and blocks set.
+        /// </summary>
+        /// <param name="columns">The columns output to set.</param>
+        /// <param name="blocks">The blocks output to set.</param>
+        /// <returns>The created <see cref="StabilityStoneCoverWaveConditionsOutput"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public static StabilityStoneCoverWaveConditionsOutput CreateOutputWithColumnsAndBlocks(
+            IEnumerable<WaveConditionsOutput> columns, IEnumerable<WaveConditionsOutput> blocks)
+        {
+            if (columns == null)
+            {
+                throw new ArgumentNullException(nameof(columns));
+            }
+
+            if (blocks == null)
+            {
+                throw new ArgumentNullException(nameof(blocks));
+            }
+
+            return new StabilityStoneCoverWaveConditionsOutput(columns, blocks);
+        }
+    }
+}
---
+++ Riskeer/StabilityStoneCover/test/Riskeer.StabilityStoneCover.Data.Test/StabilityStoneCoverWaveConditionsOutputFactoryTest.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Riskeer.Revetment.Data;
+
+namespace Riskeer.StabilityStoneCover.Data.Test
+{
+    [TestFixture]
+    public class StabilityStoneCoverWaveConditionsOutputFactoryTest
+    {
+        [Test]
+        public void CreateOutputWithBlocks_OutputNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => StabilityStoneCoverWaveConditionsOutputFactory.CreateOutputWithBlocks(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("blocks", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateOutputWithBlocks_WithBlocksOutput_ReturnsOutput()
+        {
+            // Setup
+            IEnumerable<WaveConditionsOutput> blocksOutput = Enumerable.Empty<WaveConditionsOutput>();
+
+            // Call
+            StabilityStoneCoverWaveConditionsOutput output = StabilityStoneCoverWaveConditionsOutputFactory.CreateOutputWithBlocks(blocksOutput);
+
+            // Assert
+            Assert.AreSame(blocksOutput, output.BlocksOutput);
+            Assert.IsNull(output.ColumnsOutput);
+        }
+
+        [Test]
+        public void CreateOutputWithColumns_OutputNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => StabilityStoneCoverWaveConditionsOutputFactory.CreateOutputWithColumns(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("columns", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateOutputWithColumns_WithColumnsOutput_ReturnsOutput()
+        {
+            // Setup
+            IEnumerable<WaveConditionsOutput> columnsOutput = Enumerable.Empty<WaveConditionsOutput>();
+
+            // Call
+            StabilityStoneCoverWaveConditionsOutput output = StabilityStoneCoverWaveConditionsOutputFactory.CreateOutputWithColumns(columnsOutput);
+
+            // Assert
+            Assert.AreSame(columnsOutput, output.ColumnsOutput);
+            Assert.IsNull(output.BlocksOutput);
+        }
+
+        [Test]
+        public void CreateOutputWithColumnsAndBlocks_ColumnsNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => StabilityStoneCoverWaveConditionsOutputFactory.CreateOutputWithColumnsAndBlocks(null, Enumerable.Empty<WaveConditionsOutput>());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("columns", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateOutputWithColumnsAndBlocks_BlocksNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => StabilityStoneCoverWaveConditionsOutputFactory.CreateOutputWithColumnsAndBlocks(Enumerable.Empty<WaveConditionsOutput>(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("blocks", exception.ParamName);
+        }
+
+        [Test]
+        public void CreateOutputWithColumnsAndBlocks_WithOutput_ReturnsOutput()
+        {
+            // Setup
+            IEnumerable<WaveConditionsOutput> columnsOutput = Enumerable.Empty<WaveConditionsOutput>();
+            IEnumerable<WaveConditionsOutput> blocksOutput = Enumerable.Empty<WaveConditionsOutput>();
+
+            // Call
+            StabilityStoneCoverWaveConditionsOutput output = StabilityStoneCoverWaveConditionsOutputFactory.CreateOutputWithColumnsAndBlocks(columnsOutput, blocksOutput);
+
+            // Assert
+            Assert.AreSame(columnsOutput, output.ColumnsOutput);
+            Assert.AreSame(blocksOutput, output.BlocksOutput);
+        }
+    }
+}
