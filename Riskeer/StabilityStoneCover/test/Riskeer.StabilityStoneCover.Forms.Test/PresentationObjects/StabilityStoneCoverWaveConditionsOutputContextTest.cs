ece82936a14f07e492c7eb51fdd0ab3a6368e3a2
Robert Tangerman <robert.tangerman@deltares.nl>
2019-02-12 08:42:10 +0100

    Added context for output. WTI-1805

---
+++ Riskeer/StabilityStoneCover/test/Riskeer.StabilityStoneCover.Forms.Test/PresentationObjects/StabilityStoneCoverWaveConditionsOutputContextTest.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using Core.Common.Controls.PresentationObjects;
+using NUnit.Framework;
+using Riskeer.Revetment.Data;
+using Riskeer.StabilityStoneCover.Data;
+using Riskeer.StabilityStoneCover.Forms.PresentationObjects;
+
+namespace Riskeer.StabilityStoneCover.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class StabilityStoneCoverWaveConditionsOutputContextTest
+    {
+        [Test]
+        public void Constructor_InputNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var output = new StabilityStoneCoverWaveConditionsOutput(Enumerable.Empty<WaveConditionsOutput>(), Enumerable.Empty<WaveConditionsOutput>());
+
+            // Call
+            TestDelegate call = () => new StabilityStoneCoverWaveConditionsOutputContext(output, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("input", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var output = new StabilityStoneCoverWaveConditionsOutput(Enumerable.Empty<WaveConditionsOutput>(), Enumerable.Empty<WaveConditionsOutput>());
+            var input = new StabilityStoneCoverWaveConditionsInput();
+
+            // Call
+            var context = new StabilityStoneCoverWaveConditionsOutputContext(output, input);
+
+            // Assert
+            Assert.IsInstanceOf<ObservableWrappedObjectContextBase<StabilityStoneCoverWaveConditionsOutput>>(context);
+            Assert.AreSame(output, context.WrappedData);
+            Assert.AreSame(input, context.Input);
+        }
+    }
+}
