2f2ea24b89d47a1495f83a461b622480c8a46f68
Robert Tangerman <robert.tangerman@deltares.nl>
2019-02-12 10:44:38 +0100

    Introduced test factory. WTI-1805

---
+++ Riskeer/StabilityStoneCover/test/Riskeer.StabilityStoneCover.Data.TestUtil/StabilityStoneCoverWaveConditionsOutputTestFactory.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Linq;
+using Riskeer.Revetment.Data;
+
+namespace Riskeer.StabilityStoneCover.Data.TestUtil
+{
+    /// <summary>
+    /// Factory that creates simple instances of <see cref="StabilityStoneCoverWaveConditionsOutput"/>
+    /// that can be used in tests.
+    /// </summary>
+    public class StabilityStoneCoverWaveConditionsOutputTestFactory
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="StabilityStoneCoverWaveConditionsOutput"/>
+        /// with default output.
+        /// </summary>
+        /// <returns>The created <see cref="StabilityStoneCoverWaveConditionsOutput"/>.</returns>
+        public StabilityStoneCoverWaveConditionsOutput Create()
+        {
+            return new StabilityStoneCoverWaveConditionsOutput(Enumerable.Empty<WaveConditionsOutput>(), Enumerable.Empty<WaveConditionsOutput>());
+        }
+    }
+}
