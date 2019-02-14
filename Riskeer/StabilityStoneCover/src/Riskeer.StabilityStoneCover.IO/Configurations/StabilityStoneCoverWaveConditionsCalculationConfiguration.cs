ae79690c2041fe9e9b8ac2410e19c43e69d36bc5
Robert Tangerman <robert.tangerman@deltares.nl>
2019-02-14 12:00:26 +0100

    Implemented configuration for stability stone cover. WTI-1810

---
+++ Riskeer/StabilityStoneCover/src/Riskeer.StabilityStoneCover.IO/Configurations/StabilityStoneCoverWaveConditionsCalculationConfiguration.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Riskeer.Revetment.IO.Configurations;
+
+namespace Riskeer.StabilityStoneCover.IO.Configurations
+{
+    /// <summary>
+    /// Class that represents a wave conditions calculation configuration
+    /// containing <see cref="ConfigurationStabilityStoneCoverCalculationType"/>.
+    /// </summary>
+    public class StabilityStoneCoverWaveConditionsCalculationConfiguration : AssessmentSectionCategoryWaveConditionsCalculationConfiguration
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="StabilityStoneCoverWaveConditionsCalculationConfiguration"/>.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="StabilityStoneCoverWaveConditionsCalculationConfiguration"/>.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="name"/> is <c>null</c>.</exception>
+        public StabilityStoneCoverWaveConditionsCalculationConfiguration(string name) 
+            : base(name) {}
+
+        /// <summary>
+        /// Gets or sets the calculation type.
+        /// </summary>
+        public ConfigurationStabilityStoneCoverCalculationType? CalculationType { get; set; }
+    }
+}
---
+++ Riskeer/StabilityStoneCover/test/Riskeer.StabilityStoneCover.IO.Test/Configurations/StabilityStoneCoverWaveConditionsCalculationConfigurationTest.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.Revetment.IO.Configurations;
+using Riskeer.StabilityStoneCover.IO.Configurations;
+
+namespace Riskeer.StabilityStoneCover.IO.Test.Configurations
+{
+    [TestFixture]
+    public class StabilityStoneCoverWaveConditionsCalculationConfigurationTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            const string name = "some name";
+
+            // Call
+            var configuration = new StabilityStoneCoverWaveConditionsCalculationConfiguration(name);
+
+            // Assert
+            Assert.IsInstanceOf<AssessmentSectionCategoryWaveConditionsCalculationConfiguration>(configuration);
+            Assert.IsNull(configuration.CalculationType);
+        }
+
+        [Test]
+        public void SimpleProperties_SetNewValues_NewValuesSet()
+        {
+            // Setup
+            const string calculationName = "Name of the calculation";
+            var calculationType = new Random(21).NextEnumValue<ConfigurationStabilityStoneCoverCalculationType>();
+
+            // Call
+            var configuration = new StabilityStoneCoverWaveConditionsCalculationConfiguration(calculationName)
+            {
+                CalculationType = calculationType
+            };
+
+            // Assert
+            Assert.AreEqual(calculationType, configuration.CalculationType);
+        }
+    }
+}
