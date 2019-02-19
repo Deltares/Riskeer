5f8942629994f0211830bc037b68778b11074470
Dennis Tang <dennis.tang@deltares.nl>
2019-02-19 16:35:03 +0100

    Added output test factory: WTI-1815
    - Introduced as preparation to make the output ctor internal

---
+++ Riskeer/GrassCoverErosionOutwards/test/Riskeer.GrassCoverErosionOutwards.Data.TestUtil/GrassCoverErosionOutwardsWaveConditionsOutputTestFactory.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Linq;
+using Riskeer.Revetment.Data;
+
+namespace Riskeer.GrassCoverErosionOutwards.Data.TestUtil
+{
+    /// <summary>
+    /// Factory that creates simple instances of <see cref="GrassCoverErosionOutwardsWaveConditionsOutput"/>
+    /// that can be used in tests.
+    /// </summary>
+    public static class GrassCoverErosionOutwardsWaveConditionsOutputTestFactory
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="GrassCoverErosionOutwardsWaveConditionsOutput"/>
+        /// with default output.
+        /// </summary>
+        /// <returns>The created <see cref="GrassCoverErosionOutwardsWaveConditionsOutput"/>.</returns>
+        public static GrassCoverErosionOutwardsWaveConditionsOutput Create()
+        {
+            return new GrassCoverErosionOutwardsWaveConditionsOutput(Enumerable.Empty<WaveConditionsOutput>(),
+                                                                     Enumerable.Empty<WaveConditionsOutput>());
+        }
+    }
+}
