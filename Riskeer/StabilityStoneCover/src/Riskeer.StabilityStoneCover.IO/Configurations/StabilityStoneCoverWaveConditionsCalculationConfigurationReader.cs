044b0bee4da1078f6ac23d143a4bb6d778b50a92
Robert Tangerman <robert.tangerman@deltares.nl>
2019-02-19 11:23:33 +0100

    Added reader. WTI-1810

---
+++ Riskeer/StabilityStoneCover/src/Riskeer.StabilityStoneCover.IO/Configurations/StabilityStoneCoverWaveConditionsCalculationConfigurationReader.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Xml.Linq;
+using Core.Common.Base.IO;
+using Riskeer.Common.IO.Configurations;
+using Riskeer.Revetment.IO.Configurations;
+using Riskeer.StabilityStoneCover.IO.Properties;
+
+namespace Riskeer.StabilityStoneCover.IO.Configurations
+{
+    /// <summary>
+    /// This class reads a stability stone cover wave conditions calculation configuration from XML and creates a collection of corresponding
+    /// <see cref="IConfigurationItem"/>, typically containing one or more <see cref="StabilityStoneCoverWaveConditionsCalculationConfiguration"/>.
+    /// </summary>
+    public class StabilityStoneCoverWaveConditionsCalculationConfigurationReader : WaveConditionsCalculationConfigurationReader<StabilityStoneCoverWaveConditionsCalculationConfiguration>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="StabilityStoneCoverWaveConditionsCalculationConfigurationReader"/>.
+        /// </summary>
+        /// <param name="xmlFilePath">The file path to the XML file.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="xmlFilePath"/> is invalid.</exception>
+        /// <exception cref="CriticalFileReadException">Thrown when:
+        /// <list type="bullet">
+        /// <item><paramref name="xmlFilePath"/> points to a file that does not exist.</item>
+        /// <item><paramref name="xmlFilePath"/> points to a file that does not contain valid XML.</item>
+        /// <item><paramref name="xmlFilePath"/> points to a file that does not pass the schema validation.</item>
+        /// <item><paramref name="xmlFilePath"/> points to a file that does not contain configuration elements.</item>
+        /// </list>
+        /// </exception>
+        public StabilityStoneCoverWaveConditionsCalculationConfigurationReader(string xmlFilePath)
+            : base(xmlFilePath, Resources.StabiliteitSteenzettingConfiguratieSchema) {}
+
+        protected override StabilityStoneCoverWaveConditionsCalculationConfiguration ParseCalculationElement(XElement calculationElement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Riskeer/StabilityStoneCover/test/Riskeer.StabilityStoneCover.IO.Test/Configurations/StabilityStoneCoverWaveConditionsCalculationConfigurationReaderTest.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Schema;
+using Core.Common.Base.IO;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.Revetment.IO.Configurations;
+using Riskeer.StabilityStoneCover.IO.Configurations;
+
+namespace Riskeer.StabilityStoneCover.IO.Test.Configurations
+{
+    [TestFixture]
+    public class StabilityStoneCoverWaveConditionsCalculationConfigurationReaderTest
+    {
+        private readonly string testDirectoryPath = TestHelper.GetTestDataPath(TestDataPath.Riskeer.StabilityStoneCover.IO,
+                                                                               nameof(StabilityStoneCoverWaveConditionsCalculationConfigurationReader));
+
+        private static IEnumerable<TestCaseData> InvalidConfigurations
+        {
+            get
+            {
+                yield return new TestCaseData("invalidCalculationMultipleCategoryType.xml",
+                                              "Element 'categoriegrens' cannot appear more than once if content model type is \"all\".")
+                    .SetName("invalidCalculationMultipleCategoryType");
+                yield return new TestCaseData("invalidCategoryTypeUnknownValue.xml",
+                                              "The 'categoriegrens' element is invalid - The value 'F' is invalid according to its datatype 'categoriegrensType' - The Enumeration constraint failed.")
+                    .SetName("invalidCategoryTypeUnknownValue");
+
+                yield return new TestCaseData("invalidCalculationMultipleRevetmentType.xml",
+                                              "Element 'typebekleding' cannot appear more than once if content model type is \"all\".")
+                    .SetName("invalidCalculationMultipleRevetmentType");
+                yield return new TestCaseData("invalidRevetmentTypeUnknownValue.xml",
+                                              "The 'typebekleding' element is invalid - The value 'Steen' is invalid according to its datatype 'String' - The Enumeration constraint failed.")
+                    .SetName("invalidRevetmentTypeUnknownValue");
+                yield return new TestCaseData("invalidBoundaryType.xml",
+                                              "The element 'berekening' has invalid child element 'typebelasting'.")
+                    .SetName("invalidBoundaryType");
+            }
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            string filePath = Path.Combine(testDirectoryPath, "validConfigurationEmptyCalculation.xml");
+
+            // Call
+            var reader = new StabilityStoneCoverWaveConditionsCalculationConfigurationReader(filePath);
+
+            // Assert
+            Assert.IsInstanceOf<WaveConditionsCalculationConfigurationReader<StabilityStoneCoverWaveConditionsCalculationConfiguration>>(reader);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(InvalidConfigurations))]
+        public void Constructor_FileInvalidBasedOnSchemaDefinition_ThrowCriticalFileReadException(string fileName, string expectedParsingMessage)
+        {
+            // Setup
+            string filePath = Path.Combine(testDirectoryPath, fileName);
+
+            // Call
+            TestDelegate call = () => new StabilityStoneCoverWaveConditionsCalculationConfigurationReader(filePath);
+
+            // Assert
+            var exception = Assert.Throws<CriticalFileReadException>(call);
+            Assert.IsInstanceOf<XmlSchemaValidationException>(exception.InnerException);
+            StringAssert.Contains(expectedParsingMessage, exception.InnerException?.Message);
+        }
+    }
+}
