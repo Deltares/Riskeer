a2d9ef31db17c701ea3ea4082aeffd2a0d9cc9b1
Dennis Tang <dennis.tang@deltares.nl>
2019-03-05 11:13:40 +0100

    Introduced change handler for clearing illustration point results of structure calculations. WTI-1798

---
+++ Riskeer/Common/src/Riskeer.Common.Forms/ChangeHandlers/ClearStructureCalculationsIllustrationPointsChangeHandler.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Core.Common.Base;
+using Core.Common.Gui;
+using Riskeer.Common.Data;
+using Riskeer.Common.Data.Structures;
+using Riskeer.Common.Service;
+
+namespace Riskeer.Common.Forms.ChangeHandlers
+{
+    /// <summary>
+    /// Class for handling structure calculations when its illustration points need to be cleared.
+    /// </summary>
+    /// <typeparam name="TStructureInput">Object type of the structure calculation input.</typeparam>
+    /// <typeparam name="TStructure">Object type of the structure property of <typeparamref name="TStructureInput"/>.</typeparam>
+    public class ClearStructureCalculationsIllustrationPointsChangeHandler<TStructureInput, TStructure> : ClearIllustrationPointsChangeHandlerBase
+        where TStructureInput : StructuresInputBase<TStructure>, new()
+        where TStructure : StructureBase
+    {
+        private readonly IEnumerable<StructuresCalculation<TStructureInput>> calculations;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="ClearStructureCalculationsIllustrationPointsChangeHandler{TStructuresInput,TStructure}"/>.
+        /// </summary>
+        /// <param name="inquiryHelper">Object responsible for inquiring the required data.</param>
+        /// <param name="inquiry">The description of the collection in which the illustration point results belong to.</param>
+        /// <param name="calculations">The calculations for which the illustration points should be cleared for.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public ClearStructureCalculationsIllustrationPointsChangeHandler(IInquiryHelper inquiryHelper,
+                                                                         string inquiry,
+                                                                         IEnumerable<StructuresCalculation<TStructureInput>> calculations)
+            : base(inquiryHelper, inquiry)
+        {
+            if (calculations == null)
+            {
+                throw new ArgumentNullException(nameof(calculations));
+            }
+
+            this.calculations = calculations;
+        }
+
+        public override IEnumerable<IObservable> ClearIllustrationPoints()
+        {
+            return RiskeerCommonDataSynchronizationService.ClearStructuresCalculationIllustrationPoints<TStructureInput, TStructure>(calculations);
+        }
+    }
+}
---
+++ Riskeer/Common/test/Riskeer.Common.Forms.Test/ChangeHandlers/ClearStructureCalculationsIllustrationPointsChangeHandlerTest.cs
@@ -0,0 +1,150 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base;
+using Core.Common.Gui;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.TestUtil;
+using Riskeer.Common.Data.TestUtil.IllustrationPoints;
+using Riskeer.Common.Forms.ChangeHandlers;
+
+namespace Riskeer.Common.Forms.Test.ChangeHandlers
+{
+    [TestFixture]
+    public class ClearStructureCalculationsIllustrationPointsChangeHandlerTest
+    {
+        [Test]
+        public void Constructor_CalculationsNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.Stub<IInquiryHelper>();
+            mocks.ReplayAll();
+
+            // Call
+            TestDelegate call = () => new ClearStructureCalculationsIllustrationPointsChangeHandler<TestStructuresInput, TestStructure>(
+                inquiryHelper, string.Empty, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("calculations", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_WithArguments_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.Stub<IInquiryHelper>();
+            mocks.ReplayAll();
+
+            // Call
+            var handler = new ClearStructureCalculationsIllustrationPointsChangeHandler<TestStructuresInput, TestStructure>(
+                inquiryHelper, string.Empty, Enumerable.Empty<TestStructuresCalculation>());
+
+            // Assert
+            Assert.IsInstanceOf<ClearIllustrationPointsChangeHandlerBase>(handler);
+        }
+
+        [Test]
+        public void InquireConfirmation_Always_ReturnsInquiry()
+        {
+            // Setup
+            const string inquiry = "An inquiry";
+
+            var random = new Random(21);
+            bool expectedConfirmation = random.NextBoolean();
+
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.StrictMock<IInquiryHelper>();
+            inquiryHelper.Expect(h => h.InquireContinuation(inquiry)).Return(expectedConfirmation);
+            mocks.ReplayAll();
+
+            var handler = new ClearStructureCalculationsIllustrationPointsChangeHandler<TestStructuresInput, TestStructure>(
+                inquiryHelper, inquiry, Enumerable.Empty<TestStructuresCalculation>());
+
+            // Call
+            bool confirmation = handler.InquireConfirmation();
+
+            // Assert
+            Assert.AreEqual(expectedConfirmation, confirmation);
+            mocks.ReplayAll();
+        }
+
+        [Test]
+        public void ClearIllustrationPoints_Always_ReturnsAffectedCalculations()
+        {
+            // Setup
+            var calculationWithIllustrationPoints = new TestStructuresCalculation
+            {
+                Output = new TestStructuresOutput(new TestGeneralResultFaultTreeIllustrationPoint())
+            };
+
+            var calculationWithOutput = new TestStructuresCalculation
+            {
+                Output = new TestStructuresOutput()
+            };
+
+            TestStructuresCalculation[] calculations =
+            {
+                new TestStructuresCalculation(),
+                calculationWithOutput,
+                calculationWithIllustrationPoints
+            };
+
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.StrictMock<IInquiryHelper>();
+
+            var calculationWithIllustrationPointsObserver = mocks.StrictMock<IObserver>();
+            calculationWithIllustrationPoints.Attach(calculationWithIllustrationPointsObserver);
+
+            var calculationWithoutIllustrationPointsObserver = mocks.StrictMock<IObserver>();
+            calculationWithOutput.Attach(calculationWithoutIllustrationPointsObserver);
+            mocks.ReplayAll();
+
+            var handler = new ClearStructureCalculationsIllustrationPointsChangeHandler<TestStructuresInput, TestStructure>(inquiryHelper, string.Empty, calculations);
+
+            // Call
+            IEnumerable<IObservable> affectedObjects = handler.ClearIllustrationPoints();
+
+            // Assert
+            CollectionAssert.AreEqual(new[]
+            {
+                calculationWithIllustrationPoints
+            }, affectedObjects);
+
+            TestStructuresCalculation[] calculationsWithOutput =
+            {
+                calculationWithOutput,
+                calculationWithIllustrationPoints
+            };
+            Assert.IsTrue(calculationsWithOutput.All(calc => calc.HasOutput));
+            Assert.IsTrue(calculationsWithOutput.All(calc => !calc.Output.HasGeneralResult));
+
+            mocks.VerifyAll();
+        }
+    }
+}
