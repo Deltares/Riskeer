31ecd613c35ed0c82e4fadf6e752b469299149ef
Dennis Tang <dennis.tang@deltares.nl>
2019-03-20 15:42:36 +0100

    Updated major version number. WTI-1970

--- Riskeer/Common/src/Riskeer.Common.Util/ProjectVersionHelper.cs
+++ Riskeer/Common/src/Riskeer.Common.Util/ProjectVersionHelper.cs
@@ -30,7 +30,7 @@ namespace Riskeer.Common.Util
     public static class ProjectVersionHelper
     {
         private const string validDatabaseVersion = "5";
-        private const string currentDatabaseVersion = "19.1";
+        private const string currentDatabaseVersion = "19.2";
 
         /// <summary>
         /// Gets the current database version.
--- Riskeer/Common/test/Riskeer.Common.Util.Test/ProjectVersionHelperTest.cs
+++ Riskeer/Common/test/Riskeer.Common.Util.Test/ProjectVersionHelperTest.cs
@@ -35,12 +35,12 @@ namespace Riskeer.Common.Util.Test
             string currentVersion = ProjectVersionHelper.GetCurrentDatabaseVersion();
 
             // Assert
-            Assert.AreEqual("19.1", currentVersion);
+            Assert.AreEqual("19.2", currentVersion);
         }
 
         [Test]
         [TestCase("9000.0")]
-        [TestCase("19.2")]
+        [TestCase("19.3")]
         public void IsNewerThanCurrentString_NewerVersion_ReturnsTrue(string newerVersion)
         {
             // Call
@@ -140,7 +140,8 @@ namespace Riskeer.Common.Util.Test
                 new TestCaseData("17.2"),
                 new TestCaseData("17.3"),
                 new TestCaseData("18.1"),
-                new TestCaseData("19.1")
+                new TestCaseData("19.1"),
+                new TestCaseData("19.2")
             };
         }
 
