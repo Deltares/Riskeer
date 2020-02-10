56f43c026e0a30b3000d96fb496924f7d25efda8
Robert Tangerman <robert.tangerman@deltares.nl>
2020-02-10 09:35:48 +0100

    Added importer.

---
+++ Riskeer/Integration/src/Riskeer.Integration.IO/Importers/AssemblyImporter.cs
@@ -0,0 +1,51 @@
+﻿using Core.Common.Base.IO;
+using Core.Common.IO.Readers;
+using Riskeer.AssemblyTool.IO;
+using Riskeer.AssemblyTool.IO.Model;
+using Riskeer.Integration.IO.Assembly;
+
+namespace Riskeer.Integration.IO.Importers
+{
+    public class AssemblyImporter : FileImporterBase<ExportableAssessmentSection>
+    {
+        public AssemblyImporter(ExportableAssessmentSection importTarget, string filePath) 
+            : base(filePath, importTarget) {}
+
+        protected override void LogImportCanceledMessage()
+        {
+        }
+
+        protected override bool OnImport()
+        {
+            ReadResult<SerializableAssembly> readSerializableAssembly = ReadAssembly();
+            if (readSerializableAssembly.CriticalErrorOccurred)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private ReadResult<SerializableAssembly> ReadAssembly()
+        {
+            try
+            {
+                using (var reader = new SerializableAssemblyReader(FilePath))
+                {
+                    return new ReadResult<SerializableAssembly>(false)
+                    {
+                        Items = new[]
+                        {
+                            reader.Read()
+                        }
+                    };
+                }
+            }
+            catch (CriticalFileReadException e)
+            {
+                Log.Error(e.Message);
+                return new ReadResult<SerializableAssembly>(true);
+            }
+        }
+    }
+}
---
+++ Riskeer/Integration/test/Riskeer.Integration.IO.Test/Importers/AssemblyImporterTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Core.Common.Base.Geometry;
+using Core.Common.Base.IO;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.AssemblyTool.Data;
+using Riskeer.Integration.IO.Assembly;
+using Riskeer.Integration.IO.Importers;
+
+namespace Riskeer.Integration.IO.Test.Importers
+{
+    [TestFixture]
+    public class AssemblyImporterTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var importer = new AssemblyImporter(CreateExportableAssessmentSection(), "");
+
+            // Assert
+            Assert.IsInstanceOf<FileImporterBase<ExportableAssessmentSection>>(importer);
+        }
+
+        [Test]
+        public void Import_FilePathIsDirectory_CancelImportWithErrorMessage()
+        {
+            // Setup
+            string path = TestHelper.GetTestDataPath(TestDataPath.Riskeer.Integration.IO, Path.DirectorySeparatorChar.ToString());
+
+            var importer = new AssemblyImporter(CreateExportableAssessmentSection(), path);
+
+            // Call
+            var importSuccessful = true;
+            Action call = () => importSuccessful = importer.Import();
+
+            // Assert
+            string expectedMessage = $"Fout bij het lezen van bestand '{path}': bestandspad mag niet verwijzen naar een lege bestandsnaam.";
+            TestHelper.AssertLogMessageWithLevelIsGenerated(call, new Tuple<string, LogLevelConstant>(expectedMessage, LogLevelConstant.Error), 1);
+            Assert.IsFalse(importSuccessful);
+        }
+
+        private static ExportableAssessmentSection CreateExportableAssessmentSection()
+        {
+            var random = new Random(21);
+            return new ExportableAssessmentSection("", "", new Point2D[0],
+                                                   new ExportableAssessmentSectionAssemblyResult(random.NextEnumValue<ExportableAssemblyMethod>(), random.NextEnumValue<AssessmentSectionAssemblyCategoryGroup>()),
+                                                   new ExportableFailureMechanismAssemblyResultWithProbability(random.NextEnumValue<ExportableAssemblyMethod>(), random.NextEnumValue<FailureMechanismAssemblyCategoryGroup>(),
+                                                                                                               random.NextDouble()),
+                                                   new ExportableFailureMechanismAssemblyResult(random.NextEnumValue<ExportableAssemblyMethod>(), random.NextEnumValue<FailureMechanismAssemblyCategoryGroup>()),
+                                                   new ExportableFailureMechanism<ExportableFailureMechanismAssemblyResultWithProbability>[0], new ExportableFailureMechanism<ExportableFailureMechanismAssemblyResult>[0],
+                                                   new ExportableCombinedSectionAssembly[0]);
+        }
+    }
+}
