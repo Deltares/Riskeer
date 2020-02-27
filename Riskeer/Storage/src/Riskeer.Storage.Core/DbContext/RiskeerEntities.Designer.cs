e2cc871857a0eb0ec459529dcb4b61fc8e29bcc8
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-02-27 15:06:48 +0100

    Updated database diagram. WTI-2008

--- Riskeer/Storage/src/Riskeer.Storage.Core/DbContext/RiskeerEntities.Designer.cs
+++ Riskeer/Storage/src/Riskeer.Storage.Core/DbContext/RiskeerEntities.Designer.cs
@@ -19,7 +19,7 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
-// T4 code generation is enabled for model 'C:\Repos\Riskeer\Riskeer\Storage\src\Riskeer.Storage.Core\DbContext\RiskeerEntities.edmx'. 
+// T4 code generation is enabled for model 'D:\Repositories\Riskeer\Riskeer\Storage\src\Riskeer.Storage.Core\DbContext\RiskeerEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
