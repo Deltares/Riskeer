1742a934260056f525c94f9cfc443346dd2dfb70
Robert Tangerman <robert.tangerman@deltares.nl>
2020-03-19 11:12:16 +0100

    Implemented creating the exporter. WTI-2015

--- Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Exporters/MacroStabilityInwardsCalculationGroupExporter.cs
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Exporters/MacroStabilityInwardsCalculationGroupExporter.cs
@@ -1,4 +1,6 @@
+﻿using System;
 using Core.Common.Base.IO;
+using Riskeer.Common.Data.Calculation;
 
 namespace Riskeer.MacroStabilityInwards.IO.Exporters
 {
@@ -7,9 +9,34 @@ namespace Riskeer.MacroStabilityInwards.IO.Exporters
     /// </summary>
     public class MacroStabilityInwardsCalculationGroupExporter : IFileExporter
     {
+        private readonly CalculationGroup calculationGroup;
+        private readonly string folderPath;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="MacroStabilityInwardsCalculationGroupExporter"/>.
+        /// </summary>
+        /// <param name="calculationGroup">The calculation group to export.</param>
+        /// <param name="folderPath">The folder path to export to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public MacroStabilityInwardsCalculationGroupExporter(CalculationGroup calculationGroup, string folderPath)
+        {
+            if (calculationGroup == null)
+            {
+                throw new ArgumentNullException(nameof(calculationGroup));
+            }
+
+            if (folderPath == null)
+            {
+                throw new ArgumentNullException(nameof(folderPath));
+            }
+
+            this.calculationGroup = calculationGroup;
+            this.folderPath = folderPath;
+        }
+
         public bool Export()
         {
-            throw new System.NotImplementedException();
+            return false;
         }
     }
 }
--- Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.Test/MacroStabilityInwardsCalculationGroupExporterTest.cs
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.Test/MacroStabilityInwardsCalculationGroupExporterTest.cs
@@ -1,5 +1,7 @@
+﻿using System;
 using Core.Common.Base.IO;
 using NUnit.Framework;
+using Riskeer.Common.Data.Calculation;
 using Riskeer.MacroStabilityInwards.IO.Exporters;
 
 namespace Riskeer.MacroStabilityInwards.IO.Test
@@ -11,10 +13,45 @@ namespace Riskeer.MacroStabilityInwards.IO.Test
         public void Constructor_ExpectedValues()
         {
             // Call
-            var exporter = new MacroStabilityInwardsCalculationGroupExporter();
+            var exporter = new MacroStabilityInwardsCalculationGroupExporter(new CalculationGroup(), string.Empty);
 
             // Assert
             Assert.IsInstanceOf<IFileExporter>(exporter);
+        }
+
+        [Test]
+        public void Constructor_CalculationGroupNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new MacroStabilityInwardsCalculationGroupExporter(null, string.Empty);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("calculationGroup", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_FolderPathNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new MacroStabilityInwardsCalculationGroupExporter(new CalculationGroup(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("folderPath", exception.ParamName);
+        }
+
+        [Test]
+        public void Export_Always_ReturnsFalse()
+        {
+            // Setup
+            var exporter = new MacroStabilityInwardsCalculationGroupExporter(new CalculationGroup(), string.Empty);
+
+            // Call
+            bool exportResult = exporter.Export();
+
+            // Assert
+            Assert.IsFalse(exportResult);
         }
     }
 }
