11b1d7ae81f23ba9b8efb90b4189d095903c0c52
Robert Tangerman <robert.tangerman@deltares.nl>
2020-03-19 11:27:09 +0100

    Created calculation exporter. WTI-2015

---
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Exporters/MacroStabilityInwardsCalculationExporter.cs
@@ -0,0 +1,15 @@
+﻿using Core.Common.Base.IO;
+
+namespace Riskeer.MacroStabilityInwards.IO.Exporters
+{
+    /// <summary>
+    /// Exports a macro stability inwards calculation stores it separate stix file.
+    /// </summary>
+    public class MacroStabilityInwardsCalculationExporter : IFileExporter
+    {
+        public bool Export()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.Test/Exporters/MacroStabilityInwardsCalculationExporterTest.cs
@@ -0,0 +1,20 @@
+﻿using Core.Common.Base.IO;
+using NUnit.Framework;
+using Riskeer.MacroStabilityInwards.IO.Exporters;
+
+namespace Riskeer.MacroStabilityInwards.IO.Test.Exporters
+{
+    [TestFixture]
+    public class MacroStabilityInwardsCalculationExporterTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var exporter = new MacroStabilityInwardsCalculationExporter();
+
+            // Assert
+            Assert.IsInstanceOf<IFileExporter>(exporter);
+        }
+    }
+}
