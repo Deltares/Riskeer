24c096785554f461c4a121ecab315c84c8cf754b
Robert Tangerman <robert.tangerman@deltares.nl>
2020-03-19 13:30:32 +0100

    Implemented creating the exporter. WTI-2015

--- Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Exporters/MacroStabilityInwardsCalculationExporter.cs
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Exporters/MacroStabilityInwardsCalculationExporter.cs
@@ -1,4 +1,7 @@
-﻿using Core.Common.Base.IO;
+﻿using System;
+using Core.Common.Base.IO;
+using Core.Common.Util;
+using Riskeer.MacroStabilityInwards.Data;
 
 namespace Riskeer.MacroStabilityInwards.IO.Exporters
 {
@@ -7,9 +10,37 @@ namespace Riskeer.MacroStabilityInwards.IO.Exporters
     /// </summary>
     public class MacroStabilityInwardsCalculationExporter : IFileExporter
     {
+        private readonly string filePath;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="MacroStabilityInwardsCalculationExporter"/>.
+        /// </summary>
+        /// <param name="calculation">The calculation to export.</param>
+        /// <param name="filePath">The file path to export to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="calculation"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="filePath"/> is invalid.</exception>
+        /// <remarks>A valid path:
+        /// <list type="bullet">
+        /// <item>is not empty or <c>null</c>,</item>
+        /// <item>does not consist out of only whitespace characters,</item>
+        /// <item>does not contain an invalid character,</item>
+        /// <item>does not end with a directory or path separator (empty file name).</item>
+        /// </list></remarks>
+        public MacroStabilityInwardsCalculationExporter(MacroStabilityInwardsCalculation calculation, string filePath)
+        {
+            if (calculation == null)
+            {
+                throw new ArgumentNullException(nameof(calculation));
+            }
+
+            IOUtils.ValidateFilePath(filePath);
+            
+            this.filePath = filePath;
+        }
+
         public bool Export()
         {
-            throw new System.NotImplementedException();
+            return false;
         }
     }
 }
--- Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.Test/Exporters/MacroStabilityInwardsCalculationExporterTest.cs
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.Test/Exporters/MacroStabilityInwardsCalculationExporterTest.cs
@@ -1,5 +1,8 @@
-﻿using Core.Common.Base.IO;
+﻿using System;
+using Core.Common.Base.IO;
+using Core.Common.TestUtil;
 using NUnit.Framework;
+using Riskeer.MacroStabilityInwards.Data;
 using Riskeer.MacroStabilityInwards.IO.Exporters;
 
 namespace Riskeer.MacroStabilityInwards.IO.Test.Exporters
@@ -11,10 +14,45 @@ namespace Riskeer.MacroStabilityInwards.IO.Test.Exporters
         public void Constructor_ExpectedValues()
         {
             // Call
-            var exporter = new MacroStabilityInwardsCalculationExporter();
+            var exporter = new MacroStabilityInwardsCalculationExporter(new MacroStabilityInwardsCalculation(), "ValidFilePath");
 
             // Assert
             Assert.IsInstanceOf<IFileExporter>(exporter);
+        }
+
+        [Test]
+        public void Constructor_CalculationNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new MacroStabilityInwardsCalculationExporter(null, string.Empty);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("calculation", exception.ParamName);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(InvalidPathHelper), nameof(InvalidPathHelper.InvalidPaths))]
+        public void Constructor_FilePathInvalid_ThrowsArgumentException(string filePath)
+        {
+            // Call
+            void Call() => new MacroStabilityInwardsCalculationExporter(new MacroStabilityInwardsCalculation(), filePath);
+
+            // Assert
+            Assert.Throws<ArgumentException>(Call);
+        }
+
+        [Test]
+        public void Export_Always_ReturnsFalse()
+        {
+            // Setup
+            var exporter = new MacroStabilityInwardsCalculationExporter(new MacroStabilityInwardsCalculation(), "ValidFilePath");
+
+            // Call
+            bool exportResult = exporter.Export();
+
+            // Assert
+            Assert.IsFalse(exportResult);
         }
     }
 }
