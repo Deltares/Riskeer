dbe36eff2b631d29bac3c1d2a360d58a236cbfc3
Robert Tangerman <robert.tangerman@deltares.nl>
2020-03-22 17:47:17 +0100

    Implemented getting the FilePath from the ExportHelper. WTI-2015

--- Core/Common/src/Core.Common.Gui/Helpers/ExportHelper.cs
+++ Core/Common/src/Core.Common.Gui/Helpers/ExportHelper.cs
@@ -1,9 +1,34 @@
-﻿namespace Core.Common.Gui.Helpers
+﻿using System;
+using Core.Common.Util;
+
+namespace Core.Common.Gui.Helpers
 {
     /// <summary>
     /// Class with helper methods that can be used during export.
     /// </summary>
     public static class ExportHelper
     {
+        /// <summary>
+        /// Gets the file path to export to.
+        /// </summary>
+        /// <param name="inquiryHelper">Helper responsible for performing information inquiries.</param>
+        /// <param name="fileFilterGenerator">The file filter generator to use.</param>
+        /// <returns>A path to a file, which may or may not exist yet, or <c>null</c> if no location
+        /// was chosen.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public static string GetFilePath(IInquiryHelper inquiryHelper, FileFilterGenerator fileFilterGenerator)
+        {
+            if (inquiryHelper == null)
+            {
+                throw new ArgumentNullException(nameof(inquiryHelper));
+            }
+
+            if (fileFilterGenerator == null)
+            {
+                throw new ArgumentNullException(nameof(fileFilterGenerator));
+            }
+
+            return inquiryHelper.GetTargetFileLocation(fileFilterGenerator.Filter, null);
+        }
     }
 }
