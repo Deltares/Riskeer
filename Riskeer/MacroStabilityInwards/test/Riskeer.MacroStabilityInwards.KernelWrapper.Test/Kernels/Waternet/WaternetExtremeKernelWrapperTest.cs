523b235878696c87f3cb265e02d0b07496ceab11
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-03-31 13:28:08 +0200

    Added WaternetKernelWrapperTest. WTI-2017

---
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.KernelWrapper.Test/Kernels/Waternet/WaternetExtremeKernelWrapperTest.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+using Riskeer.MacroStabilityInwards.KernelWrapper.Kernels.Waternet;
+
+namespace Riskeer.MacroStabilityInwards.KernelWrapper.Test.Kernels.Waternet
+{
+    [TestFixture]
+    public class WaternetExtremeKernelWrapperTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var kernel = new WaternetExtremeKernelWrapper();
+
+            // Assert
+            Assert.IsInstanceOf<WaternetKernelWrapper>(kernel);
+        }
+
+        [Test]
+        public void Constructor_Always_SetsCorrectWaternetName()
+        {
+            // Call
+            var kernel = new WaternetExtremeKernelWrapper();
+
+            Assert.AreEqual("WaternetExtreme", kernel.Waternet.Name);
+        }
+
+        [Test]
+        public void Calculate_ValidationErrorInCalculation_ThrowsWaternetKernelWrapperException()
+        {
+            // Setup
+            var kernel = new WaternetExtremeKernelWrapper();
+
+            // Call
+            TestDelegate test = () => kernel.Calculate();
+
+            // Assert
+            var exception = Assert.Throws<WaternetKernelWrapperException>(test);
+            Assert.IsNotNull(exception.InnerException);
+            Assert.AreEqual(exception.InnerException.Message, exception.Message);
+        }
+    }
+}
