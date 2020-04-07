0adc6ff0766cb9c6e8c635cf6f3e712daf875082
Robert Tangerman <robert.tangerman@deltares.nl>
2020-04-07 15:09:47 +0200

    Added test factory. WTI-2016

---
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.TestUtil.Test/MacroStabilityInwardsTestPersistenceFactoryTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Components.Persistence.Stability;
+using Components.Persistence.Stability.Data;
+using NUnit.Framework;
+using Shared.Components.Persistence;
+
+namespace Riskeer.MacroStabilityInwards.IO.TestUtil.Test
+{
+    [TestFixture]
+    public class MacroStabilityInwardsTestPersistenceFactoryTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var persistenceFactory = new MacroStabilityInwardsTestPersistenceFactory();
+
+            // Assert
+            Assert.IsInstanceOf<IPersistenceFactory>(persistenceFactory);
+            Assert.IsNull(persistenceFactory.PersistableDataModel);
+            Assert.IsNull(persistenceFactory.FilePath);
+        }
+
+        [Test]
+        public void CreateArchivePersister_Always_SetsPropertiesAndReturnsNull()
+        {
+            // Setup
+            var persistableDataModel = new PersistableDataModel();
+            const string filePath = "FilePath";
+
+            var persistenceFactory = new MacroStabilityInwardsTestPersistenceFactory();
+
+            // Call
+            Persister persister = persistenceFactory.CreateArchivePersister(filePath, persistableDataModel);
+
+            // Assert
+            Assert.AreSame(persistableDataModel, persistenceFactory.PersistableDataModel);
+            Assert.AreEqual(filePath, persistenceFactory.FilePath);
+            Assert.IsNull(persister);
+        }
+
+        [Test]
+        public void CreateArchiveReader_Always_ThrowsNotImplementedException()
+        {
+            // Setup
+            var persistenceFactory = new MacroStabilityInwardsTestPersistenceFactory();
+
+            // Call
+            void Call() => persistenceFactory.CreateArchiveReader(null);
+
+            // Assert
+            Assert.Throws<NotImplementedException>(Call);
+        }
+    }
+}
---
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.TestUtil/MacroStabilityInwardsTestPersistenceFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Components.Persistence.Stability;
+using Components.Persistence.Stability.Data;
+using Shared.Components.Persistence;
+
+namespace Riskeer.MacroStabilityInwards.IO.TestUtil
+{
+    /// <summary>
+    /// Persistence factory that can be used in tests.
+    /// </summary>
+    public class MacroStabilityInwardsTestPersistenceFactory : IPersistenceFactory
+    {
+        /// <summary>
+        /// Gets the <see cref="PersistableDataModel"/>.
+        /// </summary>
+        public PersistableDataModel PersistableDataModel { get; private set; }
+
+        /// <summary>
+        /// Gets the FilePath.
+        /// </summary>
+        public string FilePath { get; private set; }
+
+        public Persister CreateArchivePersister(string path, PersistableDataModel dataModel)
+        {
+            FilePath = path;
+            PersistableDataModel = dataModel;
+
+            return null;
+        }
+
+        public Reader<PersistableDataModel> CreateArchiveReader(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
