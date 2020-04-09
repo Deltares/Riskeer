2823c34be05b4b8dfebd54547e067de409fcd8ab
Robert Tangerman <robert.tangerman@deltares.nl>
2020-04-09 11:44:52 +0200

    Added persistence registry to track all items. WTI-2016

---
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Factories/MacroStabilityInwardsPersistenceRegistry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Components.Persistence.Stability.Data;
+
+namespace Riskeer.MacroStabilityInwards.IO.Factories
+{
+    /// <summary>
+    /// Registry used to keep track of items when exporting data.
+    /// </summary>
+    public class MacroStabilityInwardsPersistenceRegistry
+    {
+        private readonly Dictionary<PersistableCalculationSettings, string> settings;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="MacroStabilityInwardsPersistenceRegistry"/>.
+        /// </summary>
+        public MacroStabilityInwardsPersistenceRegistry()
+        {
+            settings = new Dictionary<PersistableCalculationSettings, string>();
+        }
+
+        /// <summary>
+        /// Gets the created <see cref="PersistableCalculationSettings"/> and their unique identifiers.
+        /// </summary>
+        public IReadOnlyDictionary<PersistableCalculationSettings, string> Settings => settings;
+
+        /// <summary>
+        /// Adds a created <see cref="PersistableCalculationSettings"/> to the registry.
+        /// </summary>
+        /// <param name="createdSettings">The settings to register.</param>
+        /// <param name="id">The id of the settings.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="createdSettings"/>
+        /// is <c>null</c>.</exception>
+        public void Add(PersistableCalculationSettings createdSettings, string id)
+        {
+            if (createdSettings == null)
+            {
+                throw new ArgumentNullException(nameof(createdSettings));
+            }
+
+            settings.Add(createdSettings, id);
+        }
+    }
+}
---
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.Test/Factories/MacroStabilityInwardsPersistenceRegistryTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Components.Persistence.Stability.Data;
+using NUnit.Framework;
+using Riskeer.MacroStabilityInwards.IO.Factories;
+
+namespace Riskeer.MacroStabilityInwards.IO.Test.Factories
+{
+    [TestFixture]
+    public class MacroStabilityInwardsPersistenceRegistryTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var registry = new MacroStabilityInwardsPersistenceRegistry();
+
+            // Assert
+            Assert.IsEmpty(registry.Settings);
+        }
+
+        [Test]
+        public void AddSettings_SettingsNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var registry = new MacroStabilityInwardsPersistenceRegistry();
+
+            // Call
+            void Call() => registry.Add(null, "1");
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("createdSettings", exception.ParamName);
+        }
+
+        [Test]
+        public void AddSettings_WithSettings_AddsSettings()
+        {
+            // Setup
+            var registry = new MacroStabilityInwardsPersistenceRegistry();
+            var settings = new PersistableCalculationSettings();
+            const string id = "1";
+
+            // Call
+            registry.Add(settings, id);
+
+            // Assert
+            Assert.AreEqual(1, registry.Settings.Count);
+            KeyValuePair<PersistableCalculationSettings, string> storedSettings = registry.Settings.Single();
+            Assert.AreSame(settings, storedSettings.Key);
+            Assert.AreEqual(id, storedSettings.Value);
+        }
+    }
+}
