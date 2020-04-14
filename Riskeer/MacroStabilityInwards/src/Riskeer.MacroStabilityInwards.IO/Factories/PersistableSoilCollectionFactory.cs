b85257272935adf000daa88cd3233ba55d0961cc
Robert Tangerman <robert.tangerman@deltares.nl>
2020-04-14 10:05:36 +0200

    Added factory for PersistableSoilCollection. WTI-2016

---
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Factories/PersistableSoilCollectionFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Components.Persistence.Stability.Data;
+using Riskeer.MacroStabilityInwards.Data.SoilProfile;
+using Riskeer.MacroStabilityInwards.Primitives;
+
+namespace Riskeer.MacroStabilityInwards.IO.Factories
+{
+    /// <summary>
+    /// Factory for creating instances of <see cref="PersistableSoilCollection"/>.
+    /// </summary>
+    internal static class PersistableSoilCollectionFactory
+    {
+        public static PersistableSoilCollection Create(IMacroStabilityInwardsSoilProfile<IMacroStabilityInwardsSoilLayer> soilProfile, IdFactory idFactory, MacroStabilityInwardsExportRegistry registry)
+        {
+            if (soilProfile == null)
+            {
+                throw new ArgumentNullException(nameof(soilProfile));
+            }
+
+            if (idFactory == null)
+            {
+                throw new ArgumentNullException(nameof(idFactory));
+            }
+
+            if (registry == null)
+            {
+                throw new ArgumentNullException(nameof(registry));
+            }
+
+            return new PersistableSoilCollection();
+        }
+    }
+}
---
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.Test/Factories/PersistableSoilCollectionFactoryTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Components.Persistence.Stability.Data;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.MacroStabilityInwards.Data.SoilProfile;
+using Riskeer.MacroStabilityInwards.IO.Factories;
+using Riskeer.MacroStabilityInwards.Primitives;
+
+namespace Riskeer.MacroStabilityInwards.IO.Test.Factories
+{
+    [TestFixture]
+    public class PersistableSoilCollectionFactoryTest
+    {
+        [Test]
+        public void Create_SoilProfileNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => PersistableSoilCollectionFactory.Create(null, new IdFactory(), new MacroStabilityInwardsExportRegistry());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("soilProfile", exception.ParamName);
+        }
+
+        [Test]
+        public void Create_IdFactoryNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var soilProfile = mocks.Stub<IMacroStabilityInwardsSoilProfile<IMacroStabilityInwardsSoilLayer>>();
+            mocks.ReplayAll();
+
+            // Call
+            void Call() => PersistableSoilCollectionFactory.Create(soilProfile,
+                                                                   null, new MacroStabilityInwardsExportRegistry());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("idFactory", exception.ParamName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Create_RegistryNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var soilProfile = mocks.Stub<IMacroStabilityInwardsSoilProfile<IMacroStabilityInwardsSoilLayer>>();
+            mocks.ReplayAll();
+
+            // Call
+            void Call() => PersistableSoilCollectionFactory.Create(soilProfile,
+                                                                   new IdFactory(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("registry", exception.ParamName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Create_WithValidData_ReturnsPersistableSoilCollection()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var soilProfile = mocks.Stub<IMacroStabilityInwardsSoilProfile<IMacroStabilityInwardsSoilLayer>>();
+            mocks.ReplayAll();
+
+            // Call
+            PersistableSoilCollection soilCollection = PersistableSoilCollectionFactory.Create(soilProfile, new IdFactory(), new MacroStabilityInwardsExportRegistry());
+
+            // Assert
+            Assert.IsNotNull(soilCollection);
+            Assert.IsNull(soilCollection.Soils);
+            mocks.VerifyAll();
+        }
+    }
+}
