6a1cfa3365cacbca09e263e64565b80887af60b9
Robert Tangerman <robert.tangerman@deltares.nl>
2020-04-15 07:49:43 +0200

    Added factory for PersistableStochasticParameter. WTI-2016

---
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Factories/PersistableStochasticParameterFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Components.Persistence.Stability.Data;
+using Riskeer.Common.Data.Probabilistics;
+
+namespace Riskeer.MacroStabilityInwards.IO.Factories
+{
+    /// <summary>
+    /// Factory for creating instances of <see cref="PersistableStochasticParameterFactory"/>.
+    /// </summary>
+    internal static class PersistableStochasticParameterFactory
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="PersistableStochasticParameter"/>.
+        /// </summary>
+        /// <param name="distribution">The distribution to create the
+        /// <see cref="PersistableStochasticParameter"/> for.</param>
+        /// <returns>The created <see cref="PersistableStochasticParameter"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="distribution"/>
+        /// is <c>null</c>.</exception>
+        public static PersistableStochasticParameter Create(IVariationCoefficientDistribution distribution)
+        {
+            if (distribution == null)
+            {
+                throw new ArgumentNullException(nameof(distribution));
+            }
+
+            return new PersistableStochasticParameter
+            {
+                IsProbabilistic = true,
+                Mean = distribution.Mean,
+                StandardDeviation = GetStandardDeviation(distribution)
+            };
+        }
+
+        private static double GetStandardDeviation(IVariationCoefficientDistribution distribution)
+        {
+            return distribution.Mean * distribution.CoefficientOfVariation;
+        }
+    }
+}
---
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.IO.Test/Factories/PersistableStochasticParameterFactoryTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Components.Persistence.Stability.Data;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.Probabilistics;
+using Riskeer.MacroStabilityInwards.IO.Factories;
+
+namespace Riskeer.MacroStabilityInwards.IO.Test.Factories
+{
+    [TestFixture]
+    public class PersistableStochasticParameterFactoryTest
+    {
+        [Test]
+        public void Create_DistributionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => PersistableStochasticParameterFactory.Create(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("distribution", exception.ParamName);
+        }
+
+        [Test]
+        public void Create_WithData_ReturnsPersistableStochasticParameter()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var distribution = mocks.Stub<IVariationCoefficientDistribution>();
+            mocks.ReplayAll();
+
+            var random = new Random(21);
+            distribution.Mean = random.NextRoundedDouble();
+            distribution.CoefficientOfVariation = random.NextRoundedDouble();
+
+            // Call
+            PersistableStochasticParameter stochasticParameter = PersistableStochasticParameterFactory.Create(distribution);
+
+            // Assert
+            Assert.AreEqual(distribution.Mean.Value, stochasticParameter.Mean);
+            Assert.AreEqual(distribution.Mean * distribution.CoefficientOfVariation, stochasticParameter.StandardDeviation);
+            Assert.IsTrue(stochasticParameter.IsProbabilistic);
+            mocks.VerifyAll();
+        }
+    }
+}
