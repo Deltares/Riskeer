04d512bd68c6179161ff563762e43758db42e7c1
Robert Tangerman <robert.tangerman@deltares.nl>
2020-04-16 07:54:06 +0200

    Added Stage Type. WTI-2016

---
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Factories/MacroStabilityInwardsExportStageType.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Riskeer.MacroStabilityInwards.IO.Factories
+{
+    /// <summary>
+    /// Defines the various types of construction stages.
+    /// </summary>
+    public enum MacroStabilityInwardsExportStageType
+    {
+        /// <summary>
+        /// The daily construction stage.
+        /// </summary>
+        Daily = 1,
+
+        /// <summary>
+        /// The extreme construction stage.
+        /// </summary>
+        Extreme = 2
+    }
+}
