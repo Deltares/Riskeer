47e804a6d0ea1747385f7060a01ec262afd969b8
Elmar Kleijn <elmar.kleijn@deltares.nl>
2020-05-11 10:06:25 +0200

    Fixed compilation errors. WTI-2009

--- Riskeer/StabilityPointStructures/src/Riskeer.StabilityPointStructures.Service/StabilityPointStructuresCalculationActivity.cs
+++ Riskeer/StabilityPointStructures/src/Riskeer.StabilityPointStructures.Service/StabilityPointStructuresCalculationActivity.cs
@@ -81,7 +81,7 @@ namespace Riskeer.StabilityPointStructures.Service
 
             calculationService.Calculate(calculation,
                                          failureMechanism.GeneralInput,
-                                         HydraulicBoundaryCalculationSettingsFactory.CreateSettings(assessmentSection.HydraulicBoundaryDatabase));
+                                         HydraulicBoundaryCalculationSettingsFactory.CreateSettings(assessmentSection, calculation.InputParameters.HydraulicBoundaryLocation));
         }
 
         protected override void OnCancel()
