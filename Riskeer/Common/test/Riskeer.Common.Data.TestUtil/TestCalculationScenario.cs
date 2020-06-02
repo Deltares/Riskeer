4dd79a65c94cebff532f5c9432af564314b74f13
Robert Tangerman <robert.tangerman@deltares.nl>
2020-06-02 13:23:21 +0200

    Added calculation scenario test object. WTI-2027

---
+++ Riskeer/Common/test/Riskeer.Common.Data.TestUtil/TestCalculationScenario.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Base.Data;
+using Riskeer.Common.Data.Calculation;
+
+namespace Riskeer.Common.Data.TestUtil
+{
+    /// <summary>
+    /// Simple calculation scenario that can be used in tests.
+    /// </summary>
+    public class TestCalculationScenario : TestCalculation, ICalculationScenario
+    {
+        private RoundedDouble contribution;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="TestCalculationScenario"/>.
+        /// </summary>
+        public TestCalculationScenario()
+        {
+            IsRelevant = true;
+            contribution = new RoundedDouble(4, 1.0);
+        }
+
+        public bool IsRelevant { get; set; }
+
+        public RoundedDouble Contribution
+        {
+            get => contribution;
+            set => contribution = value.ToPrecision(contribution.NumberOfDecimalPlaces);
+        }
+    }
+}
