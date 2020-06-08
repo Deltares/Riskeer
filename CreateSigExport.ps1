1109eaabdb5e0b4466229cccdb9b1ad80f6263ed
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-06-08 10:40:10 +0200

    Possible fix for Sig upload. WTI-2011

--- CreateSigExport.ps1
+++ CreateSigExport.ps1
@@ -2,7 +2,7 @@ Remove-Item .\Riskeer_source_*.zip
 Remove-Item .\repo-output*.zip
 if ( Test-Path '.\Riskeer_source' -PathType Container ) { Remove-Item -recurse -path .\Riskeer_source}
 md Riskeer_source
-git archive --output ./repo-output.zip HEAD: Application Components Domain Gui
+git archive --output ./repo-output.zip HEAD: Application Core Demo Migration Riskeer
 git submodule foreach --recursive 'git archive --verbose --format zip master --prefix=Shared/ --output ../repo-output-sub-$sha1.zip'
 dir ./ | where { $_.Extension -eq ".zip" } | foreach { expand-archive $_.FullName Riskeer_source/ }
 compress-archive -Path Riskeer_source/ -DestinationPath ('Riskeer_source_' + (get-date -Format yyyyMMdd) + '.zip')
