0657c49e2404bce5e1442b41eaa87d4f78bc5295
Robert Tangerman <robert.tangerman@deltares.nl>
2020-06-08 14:56:16 +0200

    Edited Sig export script. WTI-2011

--- build/CreateSigExport.ps1
+++ build/CreateSigExport.ps1
@@ -3,7 +3,5 @@ Remove-Item .\repo-output*.zip
 if ( Test-Path '.\Riskeer_src' -PathType Container ) { Remove-Item -recurse -path .\Riskeer_src}
 md Riskeer_src
 git archive --output ./repo-output.zip HEAD: Application build Core Demo Migration Riskeer
-git submodule foreach --recursive 'git archive --verbose --format zip master --prefix=Shared/ --output ../repo-output-sub-$sha1.zip'
-dir ./ | where { $_.Extension -eq ".zip" } | foreach { expand-archive $_.FullName Riskeer_src/ }
 compress-archive -Path Riskeer_src/ -DestinationPath ('Riskeer_src_' + (get-date -Format yyyyMMdd) + '.zip')
 Remove-Item .\repo-output*.zip
