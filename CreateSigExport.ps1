ec0bff664a863f4b0a93638e4ff94196aaad0e64
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-06-08 12:01:13 +0200

    Renamed Riskeer_source to Riskeer_src in sig export file. WTI-2011

--- CreateSigExport.ps1
+++ CreateSigExport.ps1
@@ -1,9 +1,9 @@
-Remove-Item .\Riskeer_source_*.zip
+Remove-Item .\Riskeer_src_*.zip
 Remove-Item .\repo-output*.zip
-if ( Test-Path '.\Riskeer_source' -PathType Container ) { Remove-Item -recurse -path .\Riskeer_source}
-md Riskeer_source
+if ( Test-Path '.\Riskeer_src' -PathType Container ) { Remove-Item -recurse -path .\Riskeer_src}
+md Riskeer_src
 git archive --output ./repo-output.zip HEAD: Application Core Demo Migration Riskeer
 git submodule foreach --recursive 'git archive --verbose --format zip master --prefix=Shared/ --output ../repo-output-sub-$sha1.zip'
-dir ./ | where { $_.Extension -eq ".zip" } | foreach { expand-archive $_.FullName Riskeer_source/ }
-compress-archive -Path Riskeer_source/ -DestinationPath ('Riskeer_source_' + (get-date -Format yyyyMMdd) + '.zip')
+dir ./ | where { $_.Extension -eq ".zip" } | foreach { expand-archive $_.FullName Riskeer_src/ }
+compress-archive -Path Riskeer_src/ -DestinationPath ('Riskeer_src_' + (get-date -Format yyyyMMdd) + '.zip')
 Remove-Item .\repo-output*.zip
