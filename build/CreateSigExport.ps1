6c9f2f3b75e3c12e1abc3c964183f45ea3da2ade
Robert Tangerman <robert.tangerman@deltares.nl>
2020-06-09 07:52:56 +0200

    Updated SIG export script. WTI-2011

--- build/CreateSigExport.ps1
+++ build/CreateSigExport.ps1
@@ -1,7 +1,8 @@
 Remove-Item .\Riskeer_src_*.zip
 Remove-Item .\repo-output*.zip
 if ( Test-Path '.\Riskeer_src' -PathType Container ) { Remove-Item -recurse -path .\Riskeer_src}
-md Riskeer_src
-git archive --output ./repo-output.zip HEAD: Application build Core Demo Migration Riskeer
+mkdir Riskeer_src
+git archive --output ./repo-output.zip HEAD: Application build/DatabaseStructure.sql Core Demo Migration Riskeer
+Get-ChildItem ./ | Where-Object { $_.Extension -eq ".zip" } | ForEach-Object { expand-archive $_.FullName Riskeer_src/ }
 compress-archive -Path Riskeer_src/ -DestinationPath ('Riskeer_src_' + (get-date -Format yyyyMMdd) + '.zip')
 Remove-Item .\repo-output*.zip
