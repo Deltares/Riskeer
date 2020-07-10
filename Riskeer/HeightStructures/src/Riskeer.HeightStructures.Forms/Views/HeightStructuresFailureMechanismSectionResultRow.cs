289960cdf6a56b45aac35607377bdee0cc98abeb
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-07-10 15:49:27 +0200

    Cleanup. WTI-2031

--- Riskeer/HeightStructures/src/Riskeer.HeightStructures.Forms/Views/HeightStructuresFailureMechanismSectionResultRow.cs
+++ Riskeer/HeightStructures/src/Riskeer.HeightStructures.Forms/Views/HeightStructuresFailureMechanismSectionResultRow.cs
@@ -387,7 +387,7 @@ namespace Riskeer.HeightStructures.Forms.Views
 
             ColumnStateHelper.SetColumnState(ColumnStateDefinitions[simpleAssessmentResultIndex], UseManualAssembly);
             ColumnStateHelper.SetColumnState(ColumnStateDefinitions[detailedAssessmentResultIndex],
-                                                                  simpleAssessmentSufficient || UseManualAssembly);
+                                             simpleAssessmentSufficient || UseManualAssembly);
             if (simpleAssessmentSufficient
                 || !FailureMechanismSectionResultRowHelper.DetailedAssessmentResultIsProbability(DetailedAssessmentResult)
                 || UseManualAssembly)
@@ -400,11 +400,11 @@ namespace Riskeer.HeightStructures.Forms.Views
             }
 
             ColumnStateHelper.SetColumnState(ColumnStateDefinitions[tailorMadeAssessmentResultIndex],
-                                                                  simpleAssessmentSufficient || UseManualAssembly);
+                                             simpleAssessmentSufficient || UseManualAssembly);
             ColumnStateHelper.SetColumnState(ColumnStateDefinitions[tailorMadeAssessmentProbabilityIndex],
-                                                                  simpleAssessmentSufficient
-                                                                  || !FailureMechanismSectionResultRowHelper.TailorMadeAssessmentResultIsProbability(TailorMadeAssessmentResult)
-                                                                  || UseManualAssembly);
+                                             simpleAssessmentSufficient
+                                             || !FailureMechanismSectionResultRowHelper.TailorMadeAssessmentResultIsProbability(TailorMadeAssessmentResult)
+                                             || UseManualAssembly);
 
             if (UseManualAssembly)
             {
