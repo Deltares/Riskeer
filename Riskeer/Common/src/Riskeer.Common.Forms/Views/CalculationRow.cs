05bcd95770091678cbf97930bf9aede593ffb749
Robert Tangerman <robert.tangerman@deltares.nl>
2020-07-23 11:50:23 +0200

    Added generic row for CalculationsView. WTI-2058

---
+++ Riskeer/Common/src/Riskeer.Common.Forms/Views/CalculationRow.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base.Geometry;
+using Core.Common.Controls.DataGrid;
+using Riskeer.Common.Data.Calculation;
+using Riskeer.Common.Data.Hydraulics;
+using Riskeer.Common.Forms.ChangeHandlers;
+using Riskeer.Common.Forms.PresentationObjects;
+using Riskeer.Common.Forms.PropertyClasses;
+
+namespace Riskeer.Common.Forms.Views
+{
+    /// <summary>
+    /// This class represents a row of <see cref="TCalculation"/> in the <see cref="CalculationsView"/>.
+    /// </summary>
+    /// <typeparam name="TCalculation">The type of the calculation.</typeparam>
+    public abstract class CalculationRow<TCalculation>
+        where TCalculation : class, ICalculation
+    {
+        private readonly IObservablePropertyChangeHandler propertyChangeHandler;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="CalculationRow{TCalculation}"/>.
+        /// </summary>
+        /// <param name="calculation">The <see cref="ICalculation"/> this row contains.</param>
+        /// <param name="propertyChangeHandler">The handler responsible for handling effects of a property change.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        protected CalculationRow(TCalculation calculation, IObservablePropertyChangeHandler propertyChangeHandler)
+        {
+            if (calculation == null)
+            {
+                throw new ArgumentNullException(nameof(calculation));
+            }
+
+            if (propertyChangeHandler == null)
+            {
+                throw new ArgumentNullException(nameof(propertyChangeHandler));
+            }
+
+            Calculation = calculation;
+            this.propertyChangeHandler = propertyChangeHandler;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ICalculation"/> this row contains.
+        /// </summary>
+        public TCalculation Calculation { get; }
+
+        /// <summary>
+        /// Gets or sets the name of the <see cref="Calculation"/>.
+        /// </summary>
+        public string Name
+        {
+            get => Calculation.Name;
+            set
+            {
+                Calculation.Name = value;
+                Calculation.NotifyObservers();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the hydraulic boundary location of the <see cref="Calculation"/>.
+        /// </summary>
+        public DataGridViewComboBoxItemWrapper<SelectableHydraulicBoundaryLocation> SelectableHydraulicBoundaryLocation
+        {
+            get
+            {
+                if (HydraulicBoundaryLocation == null)
+                {
+                    return new DataGridViewComboBoxItemWrapper<SelectableHydraulicBoundaryLocation>(null);
+                }
+
+                return new DataGridViewComboBoxItemWrapper<SelectableHydraulicBoundaryLocation>(
+                    new SelectableHydraulicBoundaryLocation(HydraulicBoundaryLocation,
+                                                            GetCalculationLocation()));
+            }
+            set
+            {
+                HydraulicBoundaryLocation valueToSet = value?.WrappedObject?.HydraulicBoundaryLocation;
+                if (!ReferenceEquals(HydraulicBoundaryLocation, valueToSet))
+                {
+                    PropertyChangeHelper.ChangePropertyAndNotify(() => HydraulicBoundaryLocation = valueToSet, propertyChangeHandler);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the location of the <see cref="Calculation"/>.
+        /// </summary>
+        /// <returns>The location of the <see cref="Calculation"/>.</returns>
+        public abstract Point2D GetCalculationLocation();
+
+        /// <summary>
+        /// Gets or sets the <see cref="HydraulicBoundaryLocation"/> of the <see cref="Calculation"/>.
+        /// </summary>
+        protected abstract HydraulicBoundaryLocation HydraulicBoundaryLocation { get; set; }
+    }
+}
---
+++ Riskeer/Common/test/Riskeer.Common.Forms.Test/Views/CalculationRowTest.cs
@@ -0,0 +1,273 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base;
+using Core.Common.Base.Geometry;
+using Core.Common.Controls.DataGrid;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.Calculation;
+using Riskeer.Common.Data.Hydraulics;
+using Riskeer.Common.Data.TestUtil;
+using Riskeer.Common.Forms.PresentationObjects;
+using Riskeer.Common.Forms.PropertyClasses;
+using Riskeer.Common.Forms.TestUtil;
+using Riskeer.Common.Forms.Views;
+
+namespace Riskeer.Common.Forms.Test.Views
+{
+    [TestFixture]
+    public class CalculationRowTest
+    {
+        [Test]
+        public void Constructor_CalculationNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var handler = mocks.Stub<IObservablePropertyChangeHandler>();
+            mocks.ReplayAll();
+
+            // Call
+            void Call() => new TestCalculationRow(null, handler);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("calculation", exception.ParamName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Constructor_PropertyChangeHandlerNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new TestCalculationRow(new TestCalculationWithHydraulicBoundaryLocation(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("propertyChangeHandler", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var handler = mocks.Stub<IObservablePropertyChangeHandler>();
+            mocks.ReplayAll();
+
+            var calculation = new TestCalculationWithHydraulicBoundaryLocation
+            {
+                HydraulicBoundaryLocation = new TestHydraulicBoundaryLocation()
+            };
+
+            // Call
+            var row = new TestCalculationRow(calculation, handler);
+
+            // Assert
+            Assert.AreSame(calculation, row.Calculation);
+            Assert.AreEqual(calculation.Name, row.Name);
+            Assert.AreSame(calculation.HydraulicBoundaryLocation, row.SelectableHydraulicBoundaryLocation.WrappedObject.HydraulicBoundaryLocation);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Name_AlwaysOnChange_NotifyObserverAndCalculationPropertyChanged()
+        {
+            // Setup
+            var mockRepository = new MockRepository();
+            var observer = mockRepository.StrictMock<IObserver>();
+            observer.Expect(o => o.UpdateObserver());
+            var handler = mockRepository.Stub<IObservablePropertyChangeHandler>();
+            mockRepository.ReplayAll();
+
+            const string newValue = "Test new name";
+
+            var calculation = new TestCalculationWithHydraulicBoundaryLocation();
+            var row = new TestCalculationRow(calculation, handler);
+
+            calculation.Attach(observer);
+
+            // Call
+            row.Name = newValue;
+
+            // Assert
+            Assert.AreEqual(newValue, calculation.Name);
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void SelectableHydraulicBoundaryLocation_AlwaysOnChange_NotifyObserverAndCalculationPropertyChanged()
+        {
+            // Setup
+            var newLocation = new TestHydraulicBoundaryLocation();
+            var selectableHydraulicBoundaryLocation = new SelectableHydraulicBoundaryLocation(newLocation, new Point2D(0, 0));
+            var newValue = new DataGridViewComboBoxItemWrapper<SelectableHydraulicBoundaryLocation>(selectableHydraulicBoundaryLocation);
+
+            var calculation = new TestCalculationWithHydraulicBoundaryLocation();
+
+            // Call & Assert
+            SetPropertyAndVerifyNotificationsAndOutputForCalculation(row => row.SelectableHydraulicBoundaryLocation = newValue, calculation);
+        }
+
+        [Test]
+        public void SelectableHydraulicBoundaryLocation_ChangeToEqualValue_NoNotificationsOutputNotCleared()
+        {
+            // Setup
+            DataGridViewComboBoxItemWrapper<SelectableHydraulicBoundaryLocation> oldValue = null;
+
+            // Call
+            AssertPropertyNotChanged(
+                row =>
+                {
+                    oldValue = row.SelectableHydraulicBoundaryLocation;
+                    row.SelectableHydraulicBoundaryLocation = row.SelectableHydraulicBoundaryLocation;
+                },
+                calculation =>
+                {
+                    // Assert
+                    Assert.NotNull(oldValue);
+                    Assert.AreEqual(oldValue.WrappedObject.HydraulicBoundaryLocation, calculation.HydraulicBoundaryLocation);
+                });
+        }
+
+        private static void SetPropertyAndVerifyNotificationsAndOutputForCalculation(
+            Action<CalculationRow<TestCalculationWithHydraulicBoundaryLocation>> setProperty,
+            TestCalculationWithHydraulicBoundaryLocation calculation)
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var observable = mocks.StrictMock<IObservable>();
+            observable.Expect(o => o.NotifyObservers());
+            mocks.ReplayAll();
+
+            var handler = new SetPropertyValueAfterConfirmationParameterTester(
+                new[]
+                {
+                    observable
+                });
+
+            var row = new TestCalculationRow(calculation, handler);
+
+            // Call
+            setProperty(row);
+
+            // Assert
+            Assert.IsTrue(handler.Called);
+            mocks.VerifyAll();
+        }
+
+        /// <summary>
+        /// Asserts that the output of a <see cref="ICalculation"/> remains
+        /// unaffected (and therefore no change notification occurring) when the input for
+        /// that calculation has been changed using an instance of <see cref="CalculationRow{TCalculation}"/>.
+        /// </summary>
+        /// <param name="setProperty">The function that changes a property of the <see cref="CalculationRow{TCalculation}"/>
+        /// instance. This function should not throw exceptions.</param>
+        /// <param name="assertions">The additional assertions to be performed on the <see cref="ICalculation"/>
+        /// whose input has been changed.</param>
+        private static void AssertPropertyNotChanged(
+            Action<CalculationRow<TestCalculationWithHydraulicBoundaryLocation>> setProperty,
+            Action<TestCalculationWithHydraulicBoundaryLocation> assertions)
+        {
+            AssertPropertyChangeWithOrWithoutCalculationOutput(setProperty, assertions, true, false);
+            AssertPropertyChangeWithOrWithoutCalculationOutput(setProperty, assertions, false, false);
+        }
+
+        private static void AssertPropertyChangeWithOrWithoutCalculationOutput(
+            Action<CalculationRow<TestCalculationWithHydraulicBoundaryLocation>> setProperty,
+            Action<TestCalculationWithHydraulicBoundaryLocation> assertions,
+            bool hasOutput,
+            bool expectUpdates)
+        {
+            // Setup
+            var mockRepository = new MockRepository();
+            var inputObserver = mockRepository.StrictMock<IObserver>();
+            if (expectUpdates)
+            {
+                inputObserver.Expect(o => o.UpdateObserver());
+            }
+
+            var calculationObserver = mockRepository.StrictMock<IObserver>();
+            if (expectUpdates && hasOutput)
+            {
+                calculationObserver.Expect(o => o.UpdateObserver());
+            }
+
+            var handler = mockRepository.Stub<IObservablePropertyChangeHandler>();
+            mockRepository.ReplayAll();
+
+            object assignedOutput = null;
+
+            var calculation = new TestCalculationWithHydraulicBoundaryLocation
+            {
+                HydraulicBoundaryLocation = new TestHydraulicBoundaryLocation()
+            };
+
+            if (hasOutput)
+            {
+                assignedOutput = new object();
+            }
+
+            calculation.Output = assignedOutput;
+
+            var row = new TestCalculationRow(calculation, handler);
+            calculation.Attach(calculationObserver);
+            calculation.InputParameters.Attach(inputObserver);
+
+            // Call
+            setProperty(row);
+
+            // Assert
+            assertions(calculation);
+            if (expectUpdates)
+            {
+                Assert.IsNull(calculation.Output);
+            }
+            else
+            {
+                Assert.AreSame(assignedOutput, calculation.Output);
+            }
+        }
+
+        private class TestCalculationRow : CalculationRow<TestCalculationWithHydraulicBoundaryLocation>
+        {
+            public TestCalculationRow(TestCalculationWithHydraulicBoundaryLocation calculation, IObservablePropertyChangeHandler propertyChangeHandler)
+                : base(calculation, propertyChangeHandler) {}
+
+            public override Point2D GetCalculationLocation()
+            {
+                return new Point2D(0, 0);
+            }
+
+            protected override HydraulicBoundaryLocation HydraulicBoundaryLocation
+            {
+                get => Calculation.HydraulicBoundaryLocation;
+                set => Calculation.HydraulicBoundaryLocation = value;
+            }
+        }
+
+        private class TestCalculationWithHydraulicBoundaryLocation : TestCalculation
+        {
+            public HydraulicBoundaryLocation HydraulicBoundaryLocation { get; set; }
+        }
+    }
+}
