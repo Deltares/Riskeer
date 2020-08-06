f4dff954b848d755423f4b5ed2451882cddb1286
Robert Tangerman <robert.tangerman@deltares.nl>
2020-08-06 14:53:30 +0200

    Created migration script for macro. WTI-2061

--- Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Properties/Resources.Designer.cs
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.IO/Properties/Resources.Designer.cs
@@ -138,6 +138,27 @@ namespace Riskeer.MacroStabilityInwards.IO.Properties {
         ///but WITHOUT ANY WARRANTY; without even the implied warranty of
         ///ME [rest of string was truncated]&quot;;.
         /// </summary>
+        public static string MacroStabiliteitBinnenwaartsConfiguratieSchema0To1 {
+            get {
+                return ResourceManager.GetString("MacroStabiliteitBinnenwaartsConfiguratieSchema0To1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;&lt;!--
+        ///Copyright (C) Stichting Deltares 2019. All rights reserved.
+        ///
+        ///This file is part of Riskeer.
+        ///
+        ///Riskeer is free software: you can redistribute it and/or modify
+        ///it under the terms of the GNU General Public License as published by
+        ///the Free Software Foundation, either version 3 of the License, or
+        ///(at your option) any later version.
+        ///
+        ///This program is distributed in the hope that it will be useful,
+        ///but WITHOUT ANY WARRANTY; without even the implied warranty of
+        ///ME [rest of string was truncated]&quot;;.
+        /// </summary>
         public static string MacroStabiliteitBinnenwaartsGridsSchema {
             get {
                 return ResourceManager.GetString("MacroStabiliteitBinnenwaartsGridsSchema", resourceCulture);
