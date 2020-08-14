8b0586675f2855b447b7b801c2b701df8a7af9a2
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-08-14 16:55:37 +0200

    Corrected column state definitions. WTI-2032

--- Riskeer/ClosingStructures/src/Riskeer.ClosingStructures.Forms/Views/ClosingStructuresCalculationRow.cs
+++ Riskeer/ClosingStructures/src/Riskeer.ClosingStructures.Forms/Views/ClosingStructuresCalculationRow.cs
@@ -258,10 +258,14 @@ namespace Riskeer.ClosingStructures.Forms.Views
             if (foreShoreProfileGeometry == null)
             {
                 ColumnStateHelper.DisableColumn(ColumnStateDefinitions[useBreakWaterColumnIndex]);
+                ColumnStateHelper.DisableColumn(ColumnStateDefinitions[breakWaterTypeColumnIndex]);
+                ColumnStateHelper.DisableColumn(ColumnStateDefinitions[breakWaterHeightColumnIndex]);
             }
             else
             {
                 ColumnStateHelper.EnableColumn(ColumnStateDefinitions[useBreakWaterColumnIndex]);
+                ColumnStateHelper.DisableColumn(ColumnStateDefinitions[breakWaterTypeColumnIndex]);
+                ColumnStateHelper.DisableColumn(ColumnStateDefinitions[breakWaterHeightColumnIndex]);
             }
         }
     }
