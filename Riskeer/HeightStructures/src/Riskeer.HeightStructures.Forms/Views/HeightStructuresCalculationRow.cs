f57d1f73b93678fe4661dd0ef71d6edec587e72a
Elmar Kleijn <elmar.kleijn@deltares.nl>
2020-08-18 14:42:25 +0200

    Improved readonly implementation of breakwater related cells. WTI-2033

--- Riskeer/HeightStructures/src/Riskeer.HeightStructures.Forms/Views/HeightStructuresCalculationRow.cs
+++ Riskeer/HeightStructures/src/Riskeer.HeightStructures.Forms/Views/HeightStructuresCalculationRow.cs
@@ -244,15 +244,13 @@ namespace Riskeer.HeightStructures.Forms.Views
             if (foreShoreProfileGeometry == null)
             {
                 ColumnStateHelper.DisableColumn(ColumnStateDefinitions[useBreakWaterColumnIndex]);
-                ColumnStateHelper.DisableColumn(ColumnStateDefinitions[breakWaterTypeColumnIndex]);
-                ColumnStateHelper.DisableColumn(ColumnStateDefinitions[breakWaterHeightColumnIndex]);
             }
             else
             {
                 ColumnStateHelper.EnableColumn(ColumnStateDefinitions[useBreakWaterColumnIndex]);
-                ColumnStateHelper.EnableColumn(ColumnStateDefinitions[breakWaterTypeColumnIndex]);
-                ColumnStateHelper.EnableColumn(ColumnStateDefinitions[breakWaterHeightColumnIndex]);
             }
+
+            UpdateBreakWaterTypeAndHeightColumnStateDefinitions();
         }
     }
 }
