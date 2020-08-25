9de91931f525360e17e6db9d712fb0d97de0476e
Elmar Kleijn <elmar.kleijn@deltares.nl>
2020-08-25 10:58:52 +0200

    Removed redundant tests from ClosingStructuresCalculationsViewTest. WTI-2032

--- Riskeer/ClosingStructures/test/Riskeer.ClosingStructures.Forms.Test/Views/ClosingStructuresCalculationsViewTest.cs
+++ Riskeer/ClosingStructures/test/Riskeer.ClosingStructures.Forms.Test/Views/ClosingStructuresCalculationsViewTest.cs
@@ -362,7 +362,7 @@ namespace Riskeer.ClosingStructures.Forms.Test.Views
         [TestCase(0, allowedLevelIncreaseStorageColumnIndex)]
         [TestCase(-123.45, allowedLevelIncreaseStorageColumnIndex)]
         [TestCase(1e-5, allowedLevelIncreaseStorageColumnIndex)]
-        public void CalculationsView_InvalidOvertoppingAndLevelIncreseStorage_ShowsErrorTooltip(double newValue, int index)
+        public void CalculationsView_InvalidOvertoppingAndLevelIncreaseStorage_ShowsErrorTooltip(double newValue, int index)
         {
             // Setup
             var mocks = new MockRepository();
@@ -491,37 +491,6 @@ namespace Riskeer.ClosingStructures.Forms.Test.Views
             Assert.AreEqual("Profiel 3", foreshoreProfileItems[3].ToString());
             Assert.AreEqual("Profiel 4", foreshoreProfileItems[4].ToString());
 
-            mocks.VerifyAll();
-        }
-
-        [Test]
-        [TestCase(true, false)]
-        [TestCase(false, true)]
-        public void CalculationsView_UseBreakWaterState_HasCorrespondingColumnState(bool newValue, bool expectedState)
-        {
-            // Setup
-            var mocks = new MockRepository();
-            var assessmentSection = mocks.Stub<IAssessmentSection>();
-            ConfigureHydraulicBoundaryDatabase(assessmentSection);
-            mocks.ReplayAll();
-
-            ClosingStructuresFailureMechanism failureMechanism = ConfigureFailureMechanism();
-            CalculationGroup calculationGroup = ConfigureCalculationGroup(failureMechanism, assessmentSection);
-
-            ShowCalculationsView(calculationGroup, failureMechanism, assessmentSection);
-
-            // This step is necessary because setting the same value would not change the view state.
-            var calculation = (StructuresCalculationScenario<ClosingStructuresInput>) calculationGroup.GetCalculations().First();
-            calculation.InputParameters.UseBreakWater = !newValue;
-
-            var dataGridView = (DataGridView) new ControlTester("dataGridView").TheObject;
-
-            // Call
-            dataGridView.Rows[0].Cells[useBreakWaterColumnIndex].Value = newValue;
-
-            // Assert
-            Assert.AreEqual(expectedState, dataGridView.Rows[0].Cells[breakWaterTypeColumnIndex].ReadOnly);
-            Assert.AreEqual(expectedState, dataGridView.Rows[0].Cells[breakWaterHeightColumnIndex].ReadOnly);
             mocks.VerifyAll();
         }
 
@@ -693,40 +662,6 @@ namespace Riskeer.ClosingStructures.Forms.Test.Views
             // Then
             CollectionAssert.IsEmpty(failureMechanism.Calculations);
             mocks.VerifyAll(); // No observer notified
-        }
-
-        [Test]
-        public void GivenForeshoreProfile_WhenSelectedForeshoreProfileNull_ThenCorrectColumnStates()
-        {
-            // Given
-            var mocks = new MockRepository();
-            var assessmentSection = mocks.Stub<IAssessmentSection>();
-            ConfigureHydraulicBoundaryDatabase(assessmentSection);
-            mocks.ReplayAll();
-
-            ClosingStructuresFailureMechanism failureMechanism = ConfigureFailureMechanism();
-            CalculationGroup calculationGroup = ConfigureCalculationGroup(failureMechanism, assessmentSection);
-
-            ShowCalculationsView(calculationGroup, failureMechanism, assessmentSection);
-
-            var dataGridView = (DataGridView) new ControlTester("dataGridView").TheObject;
-            var foreshoreProfileComboBox = (DataGridViewComboBoxColumn) dataGridView.Columns[foreshoreProfileColumnIndex];
-
-            // Precondition
-            Assert.AreEqual(3, foreshoreProfileComboBox.Items.Count);
-            Assert.AreEqual(false, dataGridView.Rows[0].Cells[useBreakWaterColumnIndex].ReadOnly);
-            Assert.AreEqual(true, dataGridView.Rows[0].Cells[breakWaterTypeColumnIndex].ReadOnly);
-            Assert.AreEqual(true, dataGridView.Rows[0].Cells[breakWaterHeightColumnIndex].ReadOnly);
-
-            // When
-            dataGridView.Rows[0].Cells[foreshoreProfileColumnIndex].Value = new DataGridViewComboBoxItemWrapper<ForeshoreProfile>(null);
-
-            // Then
-            Assert.AreEqual(true, dataGridView.Rows[0].Cells[useBreakWaterColumnIndex].ReadOnly);
-            Assert.AreEqual(true, dataGridView.Rows[0].Cells[breakWaterTypeColumnIndex].ReadOnly);
-            Assert.AreEqual(true, dataGridView.Rows[0].Cells[breakWaterHeightColumnIndex].ReadOnly);
-
-            mocks.VerifyAll();
         }
 
         public override void Setup()
