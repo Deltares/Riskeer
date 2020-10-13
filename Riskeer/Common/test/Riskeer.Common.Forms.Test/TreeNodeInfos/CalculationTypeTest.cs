5a07ef07b6ace5f9fd87bbcccda2b95683009fcf
Robert Tangerman <robert.tangerman@deltares.nl>
2020-10-13 15:26:39 +0200

    Introduced calculation type. WTI-2088

---
+++ Riskeer/Common/test/Riskeer.Common.Forms.Test/TreeNodeInfos/CalculationTypeTest.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.Common.Forms.TreeNodeInfos;
+
+namespace Riskeer.Common.Forms.Test.TreeNodeInfos
+{
+    [TestFixture]
+    public class CalculationTypeTest : EnumValuesTestFixture<CalculationType, int>
+    {
+        protected override IDictionary<CalculationType, int> ExpectedValueForEnumValues =>
+            new Dictionary<CalculationType, int>
+            {
+                {
+                    CalculationType.SemiProbabilistic, 1
+                },
+                {
+                    CalculationType.Probabilistic, 2
+                }
+            };
+    }
+}
