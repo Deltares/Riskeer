a3329d981b6720653b1f1e9436700b09f6ae9e5b
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-10-13 10:58:58 +0200

    Expanded ProbabilisticPipingInputContextProperties. WTI-2072

--- Riskeer/Piping/src/Riskeer.Piping.Forms/PropertyClasses/ProbabilisticPipingInputContextProperties.cs
+++ Riskeer/Piping/src/Riskeer.Piping.Forms/PropertyClasses/ProbabilisticPipingInputContextProperties.cs
@@ -19,11 +19,63 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
+using System;
+using System.Collections.Generic;
+using Core.Common.Base.Data;
+using Core.Common.Gui.PropertyBag;
+using Riskeer.Common.Forms.PresentationObjects;
+using Riskeer.Common.Forms.PropertyClasses;
+using Riskeer.Common.Forms.UITypeEditors;
+using Riskeer.Piping.Forms.PresentationObjects;
+
 namespace Riskeer.Piping.Forms.PropertyClasses
 {
-    public class ProbabilisticPipingInputContextProperties
+    /// <summary>
+    /// ViewModel of <see cref="PipingInputContext"/> for properties panel.
+    /// </summary>
+    public class ProbabilisticPipingInputContextProperties : ObjectProperties<ProbabilisticPipingInputContext>,
+                                                             IHasHydraulicBoundaryLocationProperty
     {
+        private readonly Func<RoundedDouble> getNormativeAssessmentLevelFunc;
+        private readonly IObservablePropertyChangeHandler propertyChangeHandler;
 
+        /// <summary>
+        /// Creates a new instance of <see cref="ProbabilisticPipingInputContextProperties"/>.
+        /// </summary>
+        /// <param name="data">The instance to show the properties for.</param>
+        /// <param name="getNormativeAssessmentLevelFunc"><see cref="Func{TResult}"/> for obtaining the normative assessment level.</param>
+        /// <param name="propertyChangeHandler">The handler responsible for handling effects of a property change.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public ProbabilisticPipingInputContextProperties(ProbabilisticPipingInputContext data,
+                                                         Func<RoundedDouble> getNormativeAssessmentLevelFunc,
+                                                         IObservablePropertyChangeHandler propertyChangeHandler)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
 
+            if (getNormativeAssessmentLevelFunc == null)
+            {
+                throw new ArgumentNullException(nameof(getNormativeAssessmentLevelFunc));
+            }
+
+            if (propertyChangeHandler == null)
+            {
+                throw new ArgumentNullException(nameof(propertyChangeHandler));
+            }
+
+            Data = data;
+
+            this.getNormativeAssessmentLevelFunc = getNormativeAssessmentLevelFunc;
+            this.propertyChangeHandler = propertyChangeHandler;
+        }
+
+        public SelectableHydraulicBoundaryLocation SelectedHydraulicBoundaryLocation { get; }
+
+        public IEnumerable<SelectableHydraulicBoundaryLocation> GetSelectableHydraulicBoundaryLocations()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
