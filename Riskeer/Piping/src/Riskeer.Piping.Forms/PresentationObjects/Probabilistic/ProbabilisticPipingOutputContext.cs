399615e9134f349b27bc430199c03c0294c015df
Robert Tangerman <robert.tangerman@deltares.nl>
2020-10-16 08:45:27 +0200

    Introduced context for probabilistic output. WTI-2088

---
+++ Riskeer/Piping/src/Riskeer.Piping.Forms/PresentationObjects/Probabilistic/ProbabilisticPipingOutputContext.cs
@@ -0,0 +1,40 @@
+// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Controls.PresentationObjects;
+using Riskeer.Piping.Data.Probabilistic;
+
+namespace Riskeer.Piping.Forms.PresentationObjects.Probabilistic
+{
+    /// <summary>
+    /// A presentation layer object which wraps a <see cref="ProbabilisticPipingOutput"/>.
+    /// </summary>
+    public class ProbabilisticPipingOutputContext : ObservableWrappedObjectContextBase<ProbabilisticPipingOutput>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="ProbabilisticPipingOutputContext"/>.
+        /// </summary>
+        /// <param name="pipingOutput">The <see cref="ProbabilisticPipingOutput"/> object to wrap.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="pipingOutput"/> is <c>null</c>.</exception>
+        public ProbabilisticPipingOutputContext(ProbabilisticPipingOutput pipingOutput) : base(pipingOutput) {}
+    }
+}
