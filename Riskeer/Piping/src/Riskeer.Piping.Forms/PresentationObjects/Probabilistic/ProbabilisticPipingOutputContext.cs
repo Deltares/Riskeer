82d2f4973f22ecd4d62d35b396463fcb3eedd0b6
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-10-27 10:27:23 +0100

    Adsjuted ProbabilisticPipingOutputContext with failure mechanism and assessment section. WTI-2092

--- Riskeer/Piping/src/Riskeer.Piping.Forms/PresentationObjects/Probabilistic/ProbabilisticPipingOutputContext.cs
+++ Riskeer/Piping/src/Riskeer.Piping.Forms/PresentationObjects/Probabilistic/ProbabilisticPipingOutputContext.cs
@@ -21,6 +21,8 @@
 
 using System;
 using Core.Common.Controls.PresentationObjects;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Piping.Data;
 using Riskeer.Piping.Data.Probabilistic;
 
 namespace Riskeer.Piping.Forms.PresentationObjects.Probabilistic
@@ -34,7 +36,33 @@ namespace Riskeer.Piping.Forms.PresentationObjects.Probabilistic
         /// Creates a new instance of <see cref="ProbabilisticPipingOutputContext"/>.
         /// </summary>
         /// <param name="calculation">The <see cref="ProbabilisticPipingCalculationScenario"/> object to wrap.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="calculation"/> is <c>null</c>.</exception>
-        public ProbabilisticPipingOutputContext(ProbabilisticPipingCalculationScenario calculation) : base(calculation) {}
+        /// <param name="failureMechanism">The failure mechanism the output belongs to.</param>
+        /// <param name="assessmentSection">The assessment section the output belongs to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public ProbabilisticPipingOutputContext(ProbabilisticPipingCalculationScenario calculation, PipingFailureMechanism failureMechanism, IAssessmentSection assessmentSection) : base(calculation)
+        {
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            FailureMechanism = failureMechanism;
+            AssessmentSection = assessmentSection;
+        }
+
+        /// <summary>
+        /// Gets the failure mechanism which the context belongs to.
+        /// </summary>
+        public PipingFailureMechanism FailureMechanism { get; }
+
+        /// <summary>
+        /// Gets the assessment section which the context belongs to.
+        /// </summary>
+        public IAssessmentSection AssessmentSection { get; }
     }
 }
--- Riskeer/Piping/test/Riskeer.Piping.Forms.Test/PresentationObjects/Probabilistic/ProbabilisticPipingOutputContextTest.cs
+++ Riskeer/Piping/test/Riskeer.Piping.Forms.Test/PresentationObjects/Probabilistic/ProbabilisticPipingOutputContextTest.cs
@@ -19,10 +19,18 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
+using System;
 using Core.Common.Controls.PresentationObjects;
 using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Data.TestUtil;
+using Riskeer.Piping.Data;
 using Riskeer.Piping.Data.Probabilistic;
+using Riskeer.Piping.Data.SoilProfile;
+using Riskeer.Piping.Data.TestUtil;
 using Riskeer.Piping.Forms.PresentationObjects.Probabilistic;
+using Riskeer.Piping.Primitives;
 
 namespace Riskeer.Piping.Forms.Test.PresentationObjects.Probabilistic
 {
@@ -30,13 +38,46 @@ namespace Riskeer.Piping.Forms.Test.PresentationObjects.Probabilistic
     public class ProbabilisticPipingOutputContextTest
     {
         [Test]
+        public void Constructor_CalculationNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new ProbabilisticPipingOutputContext(null, new TestPipingFailureMechanism(), new AssessmentSectionStub());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("calculation", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new ProbabilisticPipingOutputContext(new ProbabilisticPipingCalculationScenario(), null, new AssessmentSectionStub());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("failureMechanism", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_AssessmentSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new ProbabilisticPipingOutputContext(new ProbabilisticPipingCalculationScenario(), new TestPipingFailureMechanism() , null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("assessmentSection", exception.ParamName);
+        }
+
+        [Test]
         public void Constructor_ExpectedValues()
         {
             // Setup
             var calculation = new ProbabilisticPipingCalculationScenario();
 
             // Call
-            var context = new ProbabilisticPipingOutputContext(calculation);
+            var context = new ProbabilisticPipingOutputContext(calculation, new TestPipingFailureMechanism(), new AssessmentSectionStub());
 
             // Assert
             Assert.IsInstanceOf<ObservableWrappedObjectContextBase<ProbabilisticPipingCalculationScenario>>(context);
