cd5502d2101ef91a4b99bd9117a13e45a691a26d
Robert Tangerman <robert.tangerman@deltares.nl>
2020-10-30 13:12:39 +0100

    Fixed SIG export script.

--- build/CreateSigExport.ps1
+++ build/CreateSigExport.ps1
@@ -2,7 +2,7 @@ Remove-Item .\Riskeer_src_*.zip
 Remove-Item .\repo-output*.zip
 if ( Test-Path '.\Riskeer_src' -PathType Container ) { Remove-Item -recurse -path .\Riskeer_src}
 mkdir Riskeer_src
-git archive --output ./repo-output.zip HEAD: Application build/DatabaseStructure.sql Core Demo Migration Riskeer
+git archive --output ./repo-output.zip HEAD: Application build/DatabaseStructure.sql Core Migration Riskeer
 Get-ChildItem ./ | Where-Object { $_.Extension -eq ".zip" } | ForEach-Object { expand-archive $_.FullName Riskeer_src/ }
 compress-archive -Path Riskeer_src/ -DestinationPath ('Riskeer_src_' + (get-date -Format yyyyMMdd) + '.zip')
 Remove-Item .\repo-output*.zip
