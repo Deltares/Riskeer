c5c6386cb477f337a753cdbfdf37abba010d4e5b
Robert Tangerman <robert.tangerman@deltares.nl>
2020-11-03 10:48:20 +0100

    Updated HydraRing to temporary version. WTI-2075

--- Riskeer/HydraRing/src/Riskeer.HydraRing.Calculation/HydraRingFileConstants.cs
+++ Riskeer/HydraRing/src/Riskeer.HydraRing.Calculation/HydraRingFileConstants.cs
@@ -29,7 +29,7 @@ namespace Riskeer.HydraRing.Calculation
         /// <summary>
         /// The version of Hydra-Ring.
         /// </summary>
-        internal const string HydraRingVersionNumber = "19.1.1.9292";
+        internal const string HydraRingVersionNumber = "20.1.0.10064";
         
         /// <summary>
         /// The file name of the file containing the output of a calculation.
