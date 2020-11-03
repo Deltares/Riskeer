0bc94448c37f12a24a7a3a0e212cf4c8b92b5cad
Carlos Lubbers <carlos.lubbers@deltares.nl>
2020-11-03 11:29:05 +0100

    Improve manual assessment selection WTI-2108

--- System tests/AutomatedSystemTests/AutomatedSystemTests/Modules/Set_Assign/Assembly/SetManualAssessmentCategory.UserCode.cs
+++ System tests/AutomatedSystemTests/AutomatedSystemTests/Modules/Set_Assign/Assembly/SetManualAssessmentCategory.UserCode.cs
@@ -33,5 +33,27 @@ namespace AutomatedSystemTests.Modules.Set_Assign.Assembly
             // Your recording specific initialization code goes here.
         }
 
+        public void ScrollRight()
+        {
+        	if(repo.RiskeerMainWindow.DocumentViewContainer.FailureMechanismResultView.TableDataGridView.ColumnRightInfo.Exists(1000)) {
+	            Report.Log(ReportLevel.Info, "Mouse", "Mouse Right Click item 'RiskeerMainWindow.DocumentViewContainer.FailureMechanismResultView.TableDataGridView.ColumnRight' at 10;8.", repo.RiskeerMainWindow.DocumentViewContainer.FailureMechanismResultView.TableDataGridView.ColumnRightInfo, new RecordItemIndex(0));
+	            repo.RiskeerMainWindow.DocumentViewContainer.FailureMechanismResultView.TableDataGridView.ColumnRight.Click(System.Windows.Forms.MouseButtons.Right, "10;8");
+	            
+	            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ContextMenu.PageRight' at 49;7.", repo.ContextMenu.PageRightInfo, new RecordItemIndex(1));
+	            repo.ContextMenu.PageRight.Click("49;7");
+        	}
+        }
+
+        public void ScrollLeft()
+        {
+        	if(repo.RiskeerMainWindow.DocumentViewContainer.FailureMechanismResultView.TableDataGridView.ColumnLeftInfo.Exists(1000)) {
+	            Report.Log(ReportLevel.Info, "Mouse", "Mouse Right Click item 'RiskeerMainWindow.DocumentViewContainer.FailureMechanismResultView.TableDataGridView.ColumnLeft' at 10;7.", repo.RiskeerMainWindow.DocumentViewContainer.FailureMechanismResultView.TableDataGridView.ColumnLeftInfo, new RecordItemIndex(4));
+	            repo.RiskeerMainWindow.DocumentViewContainer.FailureMechanismResultView.TableDataGridView.ColumnLeft.Click(System.Windows.Forms.MouseButtons.Right, "10;7");
+	            
+	            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ContextMenu.PageLeft' at 30;7.", repo.ContextMenu.PageLeftInfo, new RecordItemIndex(5));
+	            repo.ContextMenu.PageLeft.Click("30;7");
+        	}
+        }
+
     }
 }
