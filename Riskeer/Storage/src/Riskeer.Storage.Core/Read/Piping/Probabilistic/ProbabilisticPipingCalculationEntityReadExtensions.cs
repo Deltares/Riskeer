dbd6ae8ddb2206eee0482b6a8ae689d0b76b0f20
Robert Tangerman <robert.tangerman@deltares.nl>
2020-11-05 11:39:14 +0100

    Implemented calculation read extensions. WTI-2157

--- Riskeer/Storage/src/Riskeer.Storage.Core/Read/Piping/Probabilistic/ProbabilisticPipingCalculationEntityReadExtensions.cs
+++ Riskeer/Storage/src/Riskeer.Storage.Core/Read/Piping/Probabilistic/ProbabilisticPipingCalculationEntityReadExtensions.cs
@@ -19,6 +19,8 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
+using System;
+using Core.Common.Base.Data;
 using Riskeer.Piping.Data.Probabilistic;
 using Riskeer.Storage.Core.DbContext;
 
@@ -30,6 +32,66 @@ namespace Riskeer.Storage.Core.Read.Piping.Probabilistic
     /// </summary>
     internal static class ProbabilisticPipingCalculationEntityReadExtensions
     {
-        
+        /// <summary>
+        /// Read the <see cref="ProbabilisticPipingCalculationEntity"/> and use the information to
+        /// construct a <see cref="ProbabilisticPipingCalculationScenario"/>.
+        /// </summary>
+        /// <param name="entity">The <see cref="ProbabilisticPipingCalculationEntity"/> to create
+        /// <see cref="ProbabilisticPipingCalculationScenario"/> for.</param>
+        /// <param name="collector">The object keeping track of read operations.</param>
+        /// <returns>A new <see cref="ProbabilisticPipingCalculationScenario"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="collector"/> is <c>null</c>.</exception>
+        public static ProbabilisticPipingCalculationScenario Read(this ProbabilisticPipingCalculationEntity entity,
+                                                                  ReadConversionCollector collector)
+        {
+            if (collector == null)
+            {
+                throw new ArgumentNullException(nameof(collector));
+            }
+
+            var calculation = new ProbabilisticPipingCalculationScenario
+            {
+                IsRelevant = Convert.ToBoolean(entity.RelevantForScenario),
+                Contribution = (RoundedDouble) entity.ScenarioContribution.ToNullAsNaN(),
+                Name = entity.Name,
+                Comments =
+                {
+                    Body = entity.Comments
+                }
+            };
+            ReadInputParameters(calculation.InputParameters, entity, collector);
+
+            return calculation;
+        }
+
+        private static void ReadInputParameters(ProbabilisticPipingInput inputParameters, ProbabilisticPipingCalculationEntity entity,
+                                                ReadConversionCollector collector)
+        {
+            if (entity.SurfaceLineEntity != null)
+            {
+                inputParameters.SurfaceLine = entity.SurfaceLineEntity.ReadAsPipingSurfaceLine(collector);
+            }
+
+            if (entity.HydraulicLocationEntity != null)
+            {
+                inputParameters.HydraulicBoundaryLocation = entity.HydraulicLocationEntity.Read(collector);
+            }
+
+            if (entity.PipingStochasticSoilProfileEntity != null)
+            {
+                inputParameters.StochasticSoilModel = entity.PipingStochasticSoilProfileEntity.StochasticSoilModelEntity.ReadAsPipingStochasticSoilModel(collector);
+                inputParameters.StochasticSoilProfile = entity.PipingStochasticSoilProfileEntity.Read(collector);
+            }
+
+            inputParameters.EntryPointL = (RoundedDouble) entity.EntryPointL.ToNullAsNaN();
+            inputParameters.ExitPointL = (RoundedDouble) entity.ExitPointL.ToNullAsNaN();
+            inputParameters.PhreaticLevelExit.Mean = (RoundedDouble) entity.PhreaticLevelExitMean.ToNullAsNaN();
+            inputParameters.PhreaticLevelExit.StandardDeviation = (RoundedDouble) entity.PhreaticLevelExitStandardDeviation.ToNullAsNaN();
+            inputParameters.DampingFactorExit.Mean = (RoundedDouble) entity.DampingFactorExitMean.ToNullAsNaN();
+            inputParameters.DampingFactorExit.StandardDeviation = (RoundedDouble) entity.DampingFactorExitStandardDeviation.ToNullAsNaN();
+
+            inputParameters.ShouldProfileSpecificIllustrationPointsBeCalculated = Convert.ToBoolean(entity.ShouldProfileSpecificIllustrationPointsBeCalculated);
+            inputParameters.ShouldSectionSpecificIllustrationPointsBeCalculated = Convert.ToBoolean(entity.ShouldSectionSpecificIllustrationPointsBeCalculated);
+        }
     }
 }
