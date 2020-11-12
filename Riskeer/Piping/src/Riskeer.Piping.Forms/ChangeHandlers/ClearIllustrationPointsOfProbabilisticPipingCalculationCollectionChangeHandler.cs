397e2a00a6da3e3bee9b96820c83e92d117d0162
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-11-12 15:30:47 +0100

    Added license headers. WTI-2079

--- Riskeer/Piping/src/Riskeer.Piping.Forms/ChangeHandlers/ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandler.cs
+++ Riskeer/Piping/src/Riskeer.Piping.Forms/ChangeHandlers/ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandler.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
 using System.Collections.Generic;
 using Core.Common.Base;
 using Core.Common.Gui.Helpers;
@@ -9,6 +30,9 @@ using RiskeerCommonFormsResources = Riskeer.Common.Forms.Properties.Resources;
 
 namespace Riskeer.Piping.Forms.ChangeHandlers
 {
+    /// <summary>
+    /// Class for handling clearing illustration points from a collection of probabilistic piping calculation scenarios.
+    /// </summary>
     public class ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandler : ClearIllustrationPointsOfCalculationCollectionChangeHandlerBase
     {
         private readonly IEnumerable<ProbabilisticPipingCalculationScenario> calculations;
@@ -34,7 +58,7 @@ namespace Riskeer.Piping.Forms.ChangeHandlers
         public override IEnumerable<IObservable> ClearIllustrationPoints()
         {
             var affectedObjects = new List<IObservable>();
-            foreach (var calculation in calculations)
+            foreach (ProbabilisticPipingCalculationScenario calculation in calculations)
             {
                 if (ProbabilisticPipingIllustrationPointsHelper.HasIllustrationPoints(calculation))
                 {
