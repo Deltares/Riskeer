4a758eb597440957a48b40c9b3033ca7dd24bc9c
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-11-12 15:05:57 +0100

    Added tests for newly added classes. WTI-2079

--- Riskeer/Piping/test/Riskeer.Piping.Forms.Test/ChangeHandlers/ClearIllustrationPointsOfProbabilisticPipingCalculationChangeHandlerTest.cs
+++ Riskeer/Piping/test/Riskeer.Piping.Forms.Test/ChangeHandlers/ClearIllustrationPointsOfProbabilisticPipingCalculationChangeHandlerTest.cs
@@ -1,7 +1,57 @@
-﻿namespace Riskeer.Piping.Forms.Test.ChangeHandlers
+﻿using Core.Common.Gui.Helpers;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Forms.ChangeHandlers;
+using Riskeer.Piping.Data.Probabilistic;
+using Riskeer.Piping.Data.TestUtil;
+using Riskeer.Piping.Forms.ChangeHandlers;
+
+namespace Riskeer.Piping.Forms.Test.ChangeHandlers
 {
     public class ClearIllustrationPointsOfProbabilisticPipingCalculationChangeHandlerTest
     {
-        
+        [Test]
+        public void Constructor_WithArguments_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.Stub<IInquiryHelper>();
+            mocks.ReplayAll();
+
+            var calculation = new ProbabilisticPipingCalculationScenario();
+
+            // Call
+            var handler = new ClearIllustrationPointsOfProbabilisticPipingCalculationChangeHandler(inquiryHelper, calculation);
+
+            // Assert
+            Assert.IsInstanceOf<ClearIllustrationPointsOfCalculationChangeHandlerBase<ProbabilisticPipingCalculation>>(handler);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void ClearIllustrationPoints_WithCalculationWithIllustrationPoints_ClearsIllustrationPointsAndReturnsExpectedResult()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.StrictMock<IInquiryHelper>();
+            mocks.ReplayAll();
+
+            var calculation = new ProbabilisticPipingCalculationScenario
+            {
+                Output = PipingTestDataGenerator.GetRandomProbabilisticPipingOutputWithIllustrationPoints()
+            };
+
+            var handler = new ClearIllustrationPointsOfProbabilisticPipingCalculationChangeHandler(inquiryHelper, calculation);
+
+            // Call
+            bool result = handler.ClearIllustrationPoints();
+
+            // Assert
+            Assert.IsTrue(result);
+
+            Assert.IsNull(calculation.Output?.ProfileSpecificOutput.GeneralResult);
+            Assert.IsNull(calculation.Output?.SectionSpecificOutput.GeneralResult);
+            mocks.VerifyAll();
+        }
     }
 }
--- Riskeer/Piping/test/Riskeer.Piping.Forms.Test/ChangeHandlers/ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandlerTest.cs
+++ Riskeer/Piping/test/Riskeer.Piping.Forms.Test/ChangeHandlers/ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandlerTest.cs
@@ -1,7 +1,131 @@
-﻿namespace Riskeer.Piping.Forms.Test.ChangeHandlers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base;
+using Core.Common.Gui.Helpers;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Forms.ChangeHandlers;
+using Riskeer.Piping.Data.Probabilistic;
+using Riskeer.Piping.Data.TestUtil;
+using Riskeer.Piping.Forms.ChangeHandlers;
+
+namespace Riskeer.Piping.Forms.Test.ChangeHandlers
 {
     public class ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandlerTest
     {
-        
+        [Test]
+        public void Constructor_CalculationsNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.Stub<IInquiryHelper>();
+            mocks.ReplayAll();
+
+            // Call
+            void Call() => new ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandler(inquiryHelper, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("calculations", exception.ParamName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Constructor_WithArguments_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.Stub<IInquiryHelper>();
+            mocks.ReplayAll();
+
+            // Call
+            var handler = new ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandler(
+                inquiryHelper, Enumerable.Empty<ProbabilisticPipingCalculationScenario>());
+
+            // Assert
+            Assert.IsInstanceOf<ClearIllustrationPointsOfCalculationCollectionChangeHandlerBase>(handler);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void InquireConfirmation_Always_ReturnsInquiry()
+        {
+            // Setup
+            var random = new Random(21);
+            bool expectedConfirmation = random.NextBoolean();
+
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.StrictMock<IInquiryHelper>();
+            inquiryHelper.Expect(h => h.InquireContinuation("Weet u zeker dat u alle illustratiepunten wilt wissen?")).Return(expectedConfirmation);
+            mocks.ReplayAll();
+
+            var handler = new ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandler(
+                inquiryHelper, Enumerable.Empty<ProbabilisticPipingCalculationScenario>());
+
+            // Call
+            bool confirmation = handler.InquireConfirmation();
+
+            // Assert
+            Assert.AreEqual(expectedConfirmation, confirmation);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void ClearIllustrationPoints_Always_ReturnsAffectedCalculations()
+        {
+            // Setup
+            var outputWithNoIllustrationPoints = PipingTestDataGenerator.GetRandomProbabilisticPipingOutputWithoutIllustrationPoints();
+            var calculationWitNoIllustrationPoints = new ProbabilisticPipingCalculationScenario
+            {
+                Output = outputWithNoIllustrationPoints
+            };
+
+            var outputWithIllustrationPoints = PipingTestDataGenerator.GetRandomProbabilisticPipingOutputWithIllustrationPoints();
+            var calculationWithIllustrationPoints = new ProbabilisticPipingCalculationScenario
+            {
+                Output = outputWithIllustrationPoints
+            };
+
+            ProbabilisticPipingCalculationScenario[] calculations =
+            {
+                calculationWitNoIllustrationPoints,
+                calculationWithIllustrationPoints,
+                new ProbabilisticPipingCalculationScenario()
+            };
+
+            var mocks = new MockRepository();
+            var inquiryHelper = mocks.StrictMock<IInquiryHelper>();
+            mocks.ReplayAll();
+
+            var handler = new ClearIllustrationPointsOfProbabilisticPipingCalculationCollectionChangeHandler(
+                inquiryHelper, calculations);
+
+            // Call
+            IEnumerable<IObservable> affectedObjects = handler.ClearIllustrationPoints();
+
+            // Assert
+            CollectionAssert.AreEquivalent(new[]
+            {
+                calculationWithIllustrationPoints
+            }, affectedObjects);
+
+            ProbabilisticPipingCalculationScenario[] calculationsWithOutput =
+            {
+                calculationWitNoIllustrationPoints,
+                calculationWithIllustrationPoints
+            };
+            Assert.IsTrue(calculationsWithOutput.All(calc => calc.HasOutput));
+
+            Assert.IsTrue(calculationsWithOutput.All(calc =>
+            {
+                ProbabilisticPipingOutput output = calc.Output;
+
+                return !output.ProfileSpecificOutput.HasGeneralResult
+                       && output.SectionSpecificOutput?.GeneralResult == null;
+            }));
+            mocks.VerifyAll();
+        }
     }
 }
