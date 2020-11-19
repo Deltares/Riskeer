44a7e7f4f55b43c704020d597cc573201afa18a7
David Rodriguez Aguilera <David.RodriguezAguilera@deltares.nl>
2020-11-19 18:35:28 +0100

    WTI-2163 Finished script 29. Removed screenshot of document view from CloseViewCentralPanelWithFocus

--- System tests/AutomatedSystemTests/AutomatedSystemTests/Modules/ActionsDocumentView/CloseViewCentralPanelWithFocus.cs
+++ System tests/AutomatedSystemTests/AutomatedSystemTests/Modules/ActionsDocumentView/CloseViewCentralPanelWithFocus.cs
@@ -82,9 +82,7 @@ namespace AutomatedSystemTests.Modules.ActionsDocumentView
             Report.Log(ReportLevel.Info, "Delay", "Waiting for 100ms.", new RecordItemIndex(0));
             Delay.Duration(100, false);
             
-            Report.Screenshot(ReportLevel.Info, "User", "", repo.RiskeerMainWindow.DocumentViewContainer.Self, false, new RecordItemIndex(1));
-            
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'RiskeerMainWindow.HeaderOpenViews.ViewCloseButton' at Center.", repo.RiskeerMainWindow.HeaderOpenViews.ViewCloseButtonInfo, new RecordItemIndex(2));
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'RiskeerMainWindow.HeaderOpenViews.ViewCloseButton' at Center.", repo.RiskeerMainWindow.HeaderOpenViews.ViewCloseButtonInfo, new RecordItemIndex(1));
             repo.RiskeerMainWindow.HeaderOpenViews.ViewCloseButton.Click();
             
         }
