d8149d5721c89b0ca55c9cf678c7d6a5085d40e0
David Rodriguez Aguilera <David.RodriguezAguilera@deltares.nl>
2020-12-18 09:28:28 +0100

    WTI-2180 New climate hlcd file. Continue iteration if failure (instead of go to sibling). Added (prepend) script output folder to CSV file (so they can be downloaded as artifacts).

---
+++ System tests/AutomatedSystemTests/AutomatedSystemTests/Modules/Calculation/PrependString.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Created by Ranorex
+ * User: rodriqu_dd
+ * Date: 18/12/2020
+ * Time: 09:22
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace AutomatedSystemTests.Modules.Calculation
+{
+    /// <summary>
+    /// Description of PrependString.
+    /// </summary>
+    [TestModule("1F888A85-095F-425F-8F17-E3D525506ECC", ModuleType.UserCode, 1)]
+    public class PrependString : ITestModule
+    {
+        
+        
+        string _mainString = "";
+        [TestVariable("4b2558be-df78-48e5-b43d-0fd066ceebe9")]
+        public string mainString
+        {
+            get { return _mainString; }
+            set { _mainString = value; }
+        }
+        
+        
+        string _stringToPrependToMainString = "";
+        [TestVariable("9f891cef-5938-4926-8560-0ba9fa86a15f")]
+        public string stringToPrependToMainString
+        {
+            get { return _stringToPrependToMainString; }
+            set { _stringToPrependToMainString = value; }
+        }
+        
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public PrependString()
+        {
+            // Do not delete - a parameterless constructor is required!
+        }
+
+        /// <summary>
+        /// Performs the playback of actions in this module.
+        /// </summary>
+        /// <remarks>You should not call this method directly, instead pass the module
+        /// instance to the <see cref="TestModuleRunner.Run(ITestModule)"/> method
+        /// that will in turn invoke this method.</remarks>
+        void ITestModule.Run()
+        {
+            Report.Info("Prepending \"" + stringToPrependToMainString + "\" to \"" + mainString + "\".");
+            mainString = stringToPrependToMainString + mainString;
+            Report.Info("Resulting string = " + mainString);
+        }
+    }
+}
