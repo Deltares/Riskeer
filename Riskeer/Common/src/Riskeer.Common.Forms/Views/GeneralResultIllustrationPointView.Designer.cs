8fd08bb471aaab486b80133cfec34c6db490f9ce
Robert Tangerman <robert.tangerman@deltares.nl>
2021-01-14 14:26:07 +0100

    Introduced new generic illustration points view. WTI-2204

---
+++ Riskeer/Common/src/Riskeer.Common.Forms/Views/GeneralResultIllustrationPointView.Designer.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Riskeer.Common.Forms.Views
+{
+    partial class GeneralResultIllustrationPointView<TTopLevelIllustrationPoint, TIllustrationPoint>
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        protected IllustrationPointsFaultTreeControl IllustrationPointsFaultTreeControl => illustrationPointsFaultTreeControl;
+
+        protected IllustrationPointsControl IllustrationPointsControl => illustrationPointsControl;
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.illustrationPointsControl = new Riskeer.Common.Forms.Views.IllustrationPointsControl();
+            this.illustrationPointsFaultTreeControl = new Riskeer.Common.Forms.Views.IllustrationPointsFaultTreeControl();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.IllustrationPointsControl);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.IllustrationPointsFaultTreeControl);
+            this.splitContainer.Size = new System.Drawing.Size(487, 357);
+            this.splitContainer.SplitterDistance = 261;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // illustrationPointsControl
+            // 
+            this.IllustrationPointsControl.Data = null;
+            this.IllustrationPointsControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.IllustrationPointsControl.Location = new System.Drawing.Point(0, 0);
+            this.IllustrationPointsControl.Name = "illustrationPointsControl";
+            this.IllustrationPointsControl.Size = new System.Drawing.Size(261, 357);
+            this.IllustrationPointsControl.TabIndex = 0;
+            // 
+            // illustrationPointsFaultTreeControl
+            // 
+            this.IllustrationPointsFaultTreeControl.Data = null;
+            this.IllustrationPointsFaultTreeControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.IllustrationPointsFaultTreeControl.Location = new System.Drawing.Point(0, 0);
+            this.IllustrationPointsFaultTreeControl.Name = "illustrationPointsFaultTreeControl";
+            this.IllustrationPointsFaultTreeControl.Size = new System.Drawing.Size(222, 357);
+            this.IllustrationPointsFaultTreeControl.TabIndex = 0;
+            // 
+            // GeneralResultFaultTreeIllustrationPointView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer);
+            this.Name = "GeneralResultIllustrationPointView";
+            this.Size = new System.Drawing.Size(487, 357);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private IllustrationPointsControl illustrationPointsControl;
+        private IllustrationPointsFaultTreeControl illustrationPointsFaultTreeControl;
+    }
+}
---
+++ Riskeer/Common/src/Riskeer.Common.Forms/Views/GeneralResultIllustrationPointView.cs
@@ -0,0 +1,155 @@
+﻿// Copyright (C) Stichting Deltares 2019. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Core.Common.Base;
+using Core.Common.Controls.Views;
+using Riskeer.Common.Data.Calculation;
+using Riskeer.Common.Data.IllustrationPoints;
+
+namespace Riskeer.Common.Forms.Views
+{
+    /// <summary>
+    /// This class is a view for presenting <see cref="TopLevelIllustrationPointBase"/> objects
+    /// (as part of the <see cref="GeneralResult{T}"/> of a <see cref="ICalculation"/>).
+    /// </summary>
+    /// <typeparam name="TTopLevelIllustrationPoint">The type of the top level illustration point.</typeparam>
+    /// <typeparam name="TIllustrationPoint">The type of the illustration point.</typeparam>
+    public abstract partial class GeneralResultIllustrationPointView<TTopLevelIllustrationPoint, TIllustrationPoint> : UserControl, IView, ISelectionProvider
+        where TTopLevelIllustrationPoint : TopLevelIllustrationPointBase
+        where TIllustrationPoint : IllustrationPointBase
+    {
+        protected readonly Func<GeneralResult<TTopLevelIllustrationPoint>> GetGeneralResultFunc;
+        private readonly Observer calculationObserver;
+        private ICalculation calculation;
+
+        private bool suspendIllustrationPointsControlEvents;
+
+        public event EventHandler<EventArgs> SelectionChanged;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="GeneralResultIllustrationPointView"/>.
+        /// </summary>
+        /// <param name="calculation">The calculation to show the illustration points for.</param>
+        /// <param name="getGeneralResultFunc">A <see cref="Func{TResult}"/> for obtaining the
+        /// illustration point that must be presented.</param>
+        /// <exception cref="NullReferenceException">Thrown when any parameter is <c>null</c>.</exception>
+        protected GeneralResultIllustrationPointView(ICalculation calculation, Func<GeneralResult<TTopLevelIllustrationPoint>> getGeneralResultFunc)
+        {
+            if (calculation == null)
+            {
+                throw new ArgumentNullException(nameof(calculation));
+            }
+
+            if (getGeneralResultFunc == null)
+            {
+                throw new ArgumentNullException(nameof(getGeneralResultFunc));
+            }
+
+            InitializeComponent();
+
+            this.calculation = calculation;
+            GetGeneralResultFunc = getGeneralResultFunc;
+
+            calculationObserver = new Observer(() =>
+            {
+                UpdateControls();
+                ProvideIllustrationPointSelection();
+            })
+            {
+                Observable = calculation
+            };
+
+            IllustrationPointsControl.SelectionChanged += IllustrationPointsControlOnSelectionChanged;
+            Name = "GeneralResultIllustrationPointView";
+
+            UpdateControls();
+            ProvideIllustrationPointSelection();
+        }
+
+        public object Selection { get; private set; }
+
+        public object Data
+        {
+            get => calculation;
+            set => calculation = value as ICalculation;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            calculationObserver.Dispose();
+
+            if (disposing)
+            {
+                components?.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+        
+        protected void UpdateControls()
+        {
+            suspendIllustrationPointsControlEvents = true;
+            UpdateIllustrationPointsControl();
+            suspendIllustrationPointsControlEvents = false;
+        }
+
+        protected abstract IEnumerable<IllustrationPointControlItem> GetIllustrationPointControlItems();
+
+        protected abstract IEnumerable<Stochast> GetStochasts(TIllustrationPoint illustrationPoint);
+
+        protected abstract void UpdateSpecificIllustrationPointsControl();
+
+        protected abstract object GetSelectedTopLevelIllustrationPoint(IllustrationPointControlItem selection);
+
+        private void OnSelectionChanged()
+        {
+            SelectionChanged?.Invoke(this, new EventArgs());
+        }
+
+        private void UpdateIllustrationPointsControl()
+        {
+            IllustrationPointsControl.Data = GetIllustrationPointControlItems();
+        }
+
+        private void IllustrationPointsControlOnSelectionChanged(object sender, EventArgs e)
+        {
+            if (suspendIllustrationPointsControlEvents)
+            {
+                return;
+            }
+
+            UpdateSpecificIllustrationPointsControl();
+
+            ProvideIllustrationPointSelection();
+        }
+
+        private void ProvideIllustrationPointSelection()
+        {
+            Selection = IllustrationPointsControl.Selection is IllustrationPointControlItem selection
+                            ? GetSelectedTopLevelIllustrationPoint(selection)
+                            : null;
+            OnSelectionChanged();
+        }
+    }
+}
