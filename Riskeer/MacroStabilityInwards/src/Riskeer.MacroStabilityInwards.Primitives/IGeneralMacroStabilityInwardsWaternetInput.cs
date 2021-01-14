9582efaccf755a2cb64d674c9f45b79c059c49e3
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-01-14 07:37:19 +0100

    Cleanup. WTI-2117

--- Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.Primitives/IGeneralMacroStabilityInwardsWaternetInput.cs
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.Primitives/IGeneralMacroStabilityInwardsWaternetInput.cs
@@ -19,8 +19,6 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
-using Core.Common.Base.Data;
-
 namespace Riskeer.MacroStabilityInwards.Primitives
 {
     /// <summary>
