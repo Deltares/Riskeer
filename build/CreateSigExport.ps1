436f3e4f92673ab436959f3fba3dc1b34825a153
Robert Tangerman <robert.tangerman@deltares.nl>
2021-02-04 13:24:52 +0100

    Possible SIG fix. WTI-2217

--- build/CreateSigExport.ps1
+++ build/CreateSigExport.ps1
@@ -1,8 +1,2 @@
 Remove-Item .\Riskeer_src_*.zip
-Remove-Item .\repo-output*.zip
-if ( Test-Path '.\Riskeer_src' -PathType Container ) { Remove-Item -recurse -path .\Riskeer_src}
-mkdir Riskeer_src
-git archive --output ./repo-output.zip HEAD: Application build/DatabaseStructure.sql Core Migration Riskeer
-Get-ChildItem ./ | Where-Object { $_.Extension -eq ".zip" } | ForEach-Object { expand-archive $_.FullName Riskeer_src/ }
-compress-archive -Path Riskeer_src/ -DestinationPath ('Riskeer_src_' + (get-date -Format yyyyMMdd) + '.zip')
-Remove-Item .\repo-output*.zip
+git archive --output ('.\Riskeer_src_' + (get-date -Format yyyyMMdd) + '.zip') --prefix=Riskeer_src/ HEAD: Application build/DatabaseStructure.sql Core Migration Riskeer
