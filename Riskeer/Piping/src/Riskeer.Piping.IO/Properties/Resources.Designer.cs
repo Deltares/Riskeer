c37eebd9171a53987f5929013edbe3d82e618ed0
Robert Tangerman <robert.tangerman@deltares.nl>
2021-02-11 10:18:06 +0100

    Adapted copyright year. WTI-2195

--- Riskeer/Piping/src/Riskeer.Piping.IO/Properties/Resources.Designer.cs
+++ Riskeer/Piping/src/Riskeer.Piping.IO/Properties/Resources.Designer.cs
@@ -1,4 +1,4 @@
-// Copyright (C) Stichting Deltares 2019. All rights reserved.
+// Copyright (C) Stichting Deltares 2021. All rights reserved.
 //
 // This file is part of Riskeer.
 //
@@ -180,7 +180,7 @@ namespace Riskeer.Piping.IO.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;&lt;!--
-        ///Copyright (C) Stichting Deltares 2019. All rights reserved.
+        ///Copyright (C) Stichting Deltares 2021. All rights reserved.
         ///
         ///This file is part of Riskeer.
         ///
@@ -201,7 +201,7 @@ namespace Riskeer.Piping.IO.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;&lt;!--
-        ///Copyright (C) Stichting Deltares 2019. All rights reserved.
+        ///Copyright (C) Stichting Deltares 2021. All rights reserved.
         ///
         ///This file is part of Riskeer.
         ///
@@ -222,7 +222,7 @@ namespace Riskeer.Piping.IO.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;&lt;!--
-        ///Copyright (C) Stichting Deltares 2019. All rights reserved.
+        ///Copyright (C) Stichting Deltares 2021. All rights reserved.
         ///
         ///This file is part of Riskeer.
         ///
