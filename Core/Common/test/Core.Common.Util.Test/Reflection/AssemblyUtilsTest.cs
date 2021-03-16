12fd01b5709b7ac14d605276b872520b2ab17d34
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-03-16 07:08:41 +0100

    Updated version number. WTI-2221

--- Core/Common/test/Core.Common.Util.Test/Reflection/AssemblyUtilsTest.cs
+++ Core/Common/test/Core.Common.Util.Test/Reflection/AssemblyUtilsTest.cs
@@ -73,7 +73,7 @@ namespace Core.Common.Util.Test.Reflection
             Assert.AreEqual("Copyright © Deltares 2021", assemblyInfo.Copyright);
             Assert.AreEqual("Core.Common.Util.Test", assemblyInfo.Product);
             Assert.AreEqual("Core.Common.Util.Test", assemblyInfo.Title);
-            StringAssert.StartsWith("21.1.1.2", assemblyInfo.Version);
+            StringAssert.StartsWith("21.1.2.1", assemblyInfo.Version);
         }
 
         [Test]
