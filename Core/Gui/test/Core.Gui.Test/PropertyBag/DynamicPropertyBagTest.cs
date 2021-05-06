8f152e50907e2f2549f8fb25f0bcdde7ddb75938
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-05-06 10:07:27 +0200

    Introducing different application states. WTI-2238

--- Core/Gui/test/Core.Gui.Test/PropertyBag/DynamicPropertyBagTest.cs
+++ Core/Gui/test/Core.Gui.Test/PropertyBag/DynamicPropertyBagTest.cs
@@ -23,7 +23,6 @@ using System;
 using System.ComponentModel;
 using System.Drawing.Design;
 using System.Linq;
-using Core.Common.TestUtil;
 using Core.Gui.Attributes;
 using Core.Gui.PropertyBag;
 using Core.Gui.TestUtil;
