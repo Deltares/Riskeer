257047abad4867e54560a0261b18d2cfecf68317
Robert Tangerman <robert.tangerman@deltares.nl>
2021-05-21 13:46:15 +0200

    Added context for failure paths. WTI-2254

---
+++ Riskeer/Integration/test/Riskeer.Integration.Forms.Test/PresentationObjects/FailurePathsContextTest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Controls.PresentationObjects;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Integration.Forms.PresentationObjects;
+
+namespace Riskeer.Integration.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class FailurePathsContextTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            // Call
+            var context = new FailurePathsContext(assessmentSection);
+
+            // Assert
+            Assert.IsInstanceOf<ObservableWrappedObjectContextBase<IAssessmentSection>>(context);
+            Assert.AreSame(assessmentSection, context.WrappedData);
+            mocks.VerifyAll();
+        }
+    }
+}
