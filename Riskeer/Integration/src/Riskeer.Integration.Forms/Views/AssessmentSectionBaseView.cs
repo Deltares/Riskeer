51c955b9d1fd44b643c32b670aa048245b76a8de
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-05-31 11:12:25 +0200

    Added AssessmentSectionBaseView. WTI-2268

---
+++ Riskeer/Integration/src/Riskeer.Integration.Forms/Views/AssessmentSectionBaseView.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Windows.Forms;
+using Core.Common.Base;
+using Core.Components.Gis.Data;
+using Core.Components.Gis.Forms;
+using Riskeer.Common.Forms.Factories;
+using Riskeer.Integration.Data;
+using Riskeer.Integration.Forms.Properties;
+
+namespace Riskeer.Integration.Forms.Views
+{
+    /// <summary>
+    /// This class is a view showing map data for an assessment section.
+    /// </summary>
+    public partial class AssessmentSectionBaseView : UserControl, IMapView
+    {
+        private readonly AssessmentSection assessmentSection;
+
+        private readonly MapLineData referenceLineMapData;
+
+        private Observer assessmentSectionObserver;
+        private Observer referenceLineObserver;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="AssessmentSectionBaseView"/>.
+        /// </summary>
+        /// <param name="assessmentSection">The assessment section to show the data for.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="assessmentSection"/>
+        /// is <c>null</c>.</exception>
+        public AssessmentSectionBaseView(AssessmentSection assessmentSection)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            InitializeComponent();
+
+            this.assessmentSection = assessmentSection;
+
+            CreateObservers();
+
+            var mapDataCollection = new MapDataCollection(Resources.AssessmentSectionMap_DisplayName);
+            referenceLineMapData = RiskeerMapDataFactory.CreateReferenceLineMapData();
+
+            mapDataCollection.Add(referenceLineMapData);
+
+            SetAllMapDataFeatures();
+            riskeerMapControl.SetAllData(mapDataCollection, assessmentSection.BackgroundData);
+        }
+
+        public object Data { get; set; }
+
+        public IMapControl Map
+        {
+            get
+            {
+                return riskeerMapControl.MapControl;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            assessmentSectionObserver.Dispose();
+            referenceLineObserver.Dispose();
+
+            if (disposing)
+            {
+                components?.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void CreateObservers()
+        {
+            assessmentSectionObserver = new Observer(UpdateReferenceLineMapData)
+            {
+                Observable = assessmentSection
+            };
+
+            referenceLineObserver = new Observer(UpdateReferenceLineMapData)
+            {
+                Observable = assessmentSection.ReferenceLine
+            };
+        }
+
+        private void SetAllMapDataFeatures()
+        {
+            SetReferenceLineMapData();
+        }
+
+        #region ReferenceLine MapData
+
+        private void UpdateReferenceLineMapData()
+        {
+            SetReferenceLineMapData();
+            referenceLineMapData.NotifyObservers();
+        }
+
+        private void SetReferenceLineMapData()
+        {
+            referenceLineMapData.Features = RiskeerMapDataFeaturesFactory.CreateReferenceLineFeatures(assessmentSection.ReferenceLine, assessmentSection.Id, assessmentSection.Name);
+        }
+
+        #endregion
+    }
+}
