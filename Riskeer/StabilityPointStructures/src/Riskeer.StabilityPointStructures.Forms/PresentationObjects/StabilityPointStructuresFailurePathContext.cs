03268913f8c45b9100dacd0a8ac37eaed1e4b654
Robert Tangerman <robert.tangerman@deltares.nl>
2021-06-04 11:30:03 +0200

    Added context. WTI-2303

---
+++ Riskeer/StabilityPointStructures/src/Riskeer.StabilityPointStructures.Forms/PresentationObjects/StabilityPointStructuresFailurePathContext.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Forms.PresentationObjects;
+using Riskeer.StabilityPointStructures.Data;
+
+namespace Riskeer.StabilityPointStructures.Forms.PresentationObjects
+{
+    /// <summary>
+    /// Presentation object for the failure path of <see cref="StabilityPointStructuresFailureMechanism"/>.
+    /// </summary>
+    public class StabilityPointStructuresFailurePathContext : FailureMechanismContext<StabilityPointStructuresFailureMechanism>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="StabilityPointStructuresFailurePathContext"/>.
+        /// </summary>
+        /// <param name="failureMechanism">The <see cref="StabilityPointStructuresFailureMechanism"/>
+        /// instance wrapped by this context object.</param>
+        /// <param name="assessmentSection">The assessment section which the failure mechanism belongs to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public StabilityPointStructuresFailurePathContext(StabilityPointStructuresFailureMechanism failureMechanism,
+                                                          IAssessmentSection assessmentSection)
+            : base(failureMechanism, assessmentSection) {}
+    }
+}
