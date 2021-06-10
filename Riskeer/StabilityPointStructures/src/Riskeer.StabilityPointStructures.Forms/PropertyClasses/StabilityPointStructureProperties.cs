d1969c0ecefca21b9e7cce3ad27a1c6621dd54ed
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-06-10 13:13:08 +0200

    Fixed description. WTI-2281

--- Riskeer/StabilityPointStructures/src/Riskeer.StabilityPointStructures.Forms/PropertyClasses/StabilityPointStructureProperties.cs
+++ Riskeer/StabilityPointStructures/src/Riskeer.StabilityPointStructures.Forms/PropertyClasses/StabilityPointStructureProperties.cs
@@ -35,7 +35,7 @@ using RiskeerCommonFormsResources = Riskeer.Common.Forms.Properties.Resources;
 namespace Riskeer.StabilityPointStructures.Forms.PropertyClasses
 {
     /// <summary>
-    /// ViewModel of <see cref="StabilityPointStructure"/> for properties panel.
+    /// Calculation related ViewModel of <see cref="StabilityPointStructuresFailureMechanism"/> for properties panel.
     /// </summary>
     public class StabilityPointStructureProperties : ObjectProperties<StabilityPointStructure>
     {
