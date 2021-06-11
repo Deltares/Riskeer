5ad3abfef71e17bd6a0230e5d91ccd7ac27307f7
Robert Tangerman <robert.tangerman@deltares.nl>
2021-06-11 09:12:28 +0200

    Cleanup. WTI-2245

--- Core/Common/src/Core.Common.Base/Data/IProjectFactory.cs
+++ Core/Common/src/Core.Common.Base/Data/IProjectFactory.cs
@@ -19,8 +19,6 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
-using System;
-
 namespace Core.Common.Base.Data
 {
     /// <summary>
