d556996689570131fbc938bf71542b81fef8e9ea
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-06-30 16:55:19 +0200

    Added failure path related propertyinfo test class. WTI-2307

---
+++ Riskeer/GrassCoverErosionInwards/test/Riskeer.GrassCoverErosionInwards.Plugin.Test/PropertyInfos/GrassCoverErosionInwardsFailurePathPropertyInfoTest.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Linq;
+using Core.Gui.Plugin;
+using Core.Gui.PropertyBag;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.GrassCoverErosionInwards.Data;
+using Riskeer.GrassCoverErosionInwards.Forms.PresentationObjects;
+using Riskeer.GrassCoverErosionInwards.Forms.PropertyClasses;
+
+namespace Riskeer.GrassCoverErosionInwards.Plugin.Test.PropertyInfos
+{
+    [TestFixture]
+    public class GrassCoverErosionInwardsFailurePathPropertyInfoTest
+    {
+        private GrassCoverErosionInwardsPlugin plugin;
+        private PropertyInfo info;
+
+        [SetUp]
+        public void SetUp()
+        {
+            plugin = new GrassCoverErosionInwardsPlugin();
+            info = plugin.GetPropertyInfos().First(tni => tni.PropertyObjectType == typeof(GrassCoverErosionInwardsFailurePathProperties));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            plugin.Dispose();
+        }
+
+        [Test]
+        public void Initialized_Always_ExpectedPropertiesSet()
+        {
+            // Assert
+            Assert.AreEqual(typeof(GrassCoverErosionInwardsFailurePathContext), info.DataType);
+            Assert.AreEqual(typeof(GrassCoverErosionInwardsFailurePathProperties), info.PropertyObjectType);
+        }
+
+        [Test]
+        public void CreateInstance_WithContext_NewPropertiesWithFailureMechanismAsData()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new GrassCoverErosionInwardsFailureMechanism();
+            var context = new GrassCoverErosionInwardsFailurePathContext(failureMechanism, assessmentSection);
+
+            // Call
+            IObjectProperties objectProperties = info.CreateInstance(context);
+
+            // Assert
+            Assert.IsInstanceOf<GrassCoverErosionInwardsFailurePathProperties>(objectProperties);
+            Assert.AreSame(failureMechanism, objectProperties.Data);
+            mocks.VerifyAll();
+        }
+    }
+}
