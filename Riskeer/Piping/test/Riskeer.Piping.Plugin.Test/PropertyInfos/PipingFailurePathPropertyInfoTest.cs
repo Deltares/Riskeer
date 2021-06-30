a64a75d165d5848b401f17458b65ed108d7a2e21
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-06-30 16:55:07 +0200

    Added failure path related propertyinfo test class. WTI-2306

---
+++ Riskeer/Piping/test/Riskeer.Piping.Plugin.Test/PropertyInfos/PipingFailurePathPropertyInfoTest.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Linq;
+using Core.Gui.Plugin;
+using Core.Gui.PropertyBag;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Piping.Data;
+using Riskeer.Piping.Forms.PresentationObjects;
+using Riskeer.Piping.Forms.PropertyClasses;
+
+namespace Riskeer.Piping.Plugin.Test.PropertyInfos
+{
+    [TestFixture]
+    public class PipingFailurePathPropertyInfoTest
+    {
+        private PipingPlugin plugin;
+        private PropertyInfo info;
+
+        [SetUp]
+        public void SetUp()
+        {
+            plugin = new PipingPlugin();
+            info = plugin.GetPropertyInfos().First(tni => tni.PropertyObjectType == typeof(PipingFailurePathProperties));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            plugin.Dispose();
+        }
+
+        [Test]
+        public void Initialized_Always_ExpectedPropertiesSet()
+        {
+            // Assert
+            Assert.AreEqual(typeof(PipingFailurePathContext), info.DataType);
+            Assert.AreEqual(typeof(PipingFailurePathProperties), info.PropertyObjectType);
+        }
+
+        [Test]
+        public void CreateInstance_WithContext_NewPropertiesWithFailureMechanismAsData()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new PipingFailureMechanism();
+            var context = new PipingFailurePathContext(failureMechanism, assessmentSection);
+
+            // Call
+            IObjectProperties objectProperties = info.CreateInstance(context);
+
+            // Assert
+            Assert.IsInstanceOf<PipingFailurePathProperties>(objectProperties);
+            Assert.AreSame(failureMechanism, objectProperties.Data);
+
+            mocks.VerifyAll();
+        }
+    }
+}
