191f112e94eb39eb3110b0305fba7724e5a9cd5b
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-06-30 16:57:07 +0200

    Added failure path related propertyinfo test class. WTI-2313

---
+++ Riskeer/ClosingStructures/test/Riskeer.ClosingStructures.Plugin.Test/PropertyInfos/ClosingStructuresFailurePathPropertyInfoTest.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Linq;
+using Core.Gui.Plugin;
+using Core.Gui.PropertyBag;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.ClosingStructures.Data;
+using Riskeer.ClosingStructures.Forms.PresentationObjects;
+using Riskeer.ClosingStructures.Forms.PropertyClasses;
+using Riskeer.Common.Data.AssessmentSection;
+
+namespace Riskeer.ClosingStructures.Plugin.Test.PropertyInfos
+{
+    [TestFixture]
+    public class ClosingStructuresFailurePathPropertyInfoTest
+    {
+        private ClosingStructuresPlugin plugin;
+        private PropertyInfo info;
+
+        [SetUp]
+        public void SetUp()
+        {
+            plugin = new ClosingStructuresPlugin();
+            info = plugin.GetPropertyInfos().First(tni => tni.PropertyObjectType == typeof(ClosingStructuresFailurePathProperties));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            plugin.Dispose();
+        }
+
+        [Test]
+        public void Initialized_Always_ExpectedPropertiesSet()
+        {
+            // Assert
+            Assert.AreEqual(typeof(ClosingStructuresFailurePathContext), info.DataType);
+            Assert.AreEqual(typeof(ClosingStructuresFailurePathProperties), info.PropertyObjectType);
+        }
+
+        [Test]
+        public void CreateInstance_WithContext_NewPropertiesWithFailureMechanismAsData()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new ClosingStructuresFailureMechanism();
+            var context = new ClosingStructuresFailurePathContext(failureMechanism, assessmentSection);
+
+            // Call
+            IObjectProperties objectProperties = info.CreateInstance(context);
+
+            // Assert
+            Assert.IsInstanceOf<ClosingStructuresFailurePathProperties>(objectProperties);
+            Assert.AreSame(failureMechanism, objectProperties.Data);
+
+            mocks.VerifyAll();
+        }
+    }
+}
