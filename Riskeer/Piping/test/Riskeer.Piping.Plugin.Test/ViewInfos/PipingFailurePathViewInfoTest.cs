34c32a7be8ff5bc718b45f340cc1c21119abf885
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-07-05 16:31:03 +0200

    Added ViewInfo tests. WTI-2317

---
+++ Riskeer/Piping/test/Riskeer.Piping.Plugin.Test/ViewInfos/PipingFailurePathViewInfoTest.cs
@@ -0,0 +1,209 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System.Drawing;
+using System.Linq;
+using Core.Common.Controls.Views;
+using Core.Common.TestUtil;
+using Core.Gui.Plugin;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Data.TestUtil;
+using Riskeer.Piping.Data;
+using Riskeer.Piping.Forms.PresentationObjects;
+using Riskeer.Piping.Forms.Views;
+using RiskeerCommonFormsResources = Riskeer.Common.Forms.Properties.Resources;
+
+namespace Riskeer.Piping.Plugin.Test.ViewInfos
+{
+    [TestFixture]
+    public class PipingFailurePathViewInfoTest
+    {
+        private MockRepository mocks;
+        private PipingPlugin plugin;
+        private ViewInfo info;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mocks = new MockRepository();
+            plugin = new PipingPlugin();
+            info = plugin.GetViewInfos().First(tni => tni.ViewType == typeof(PipingFailurePathView));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            plugin.Dispose();
+        }
+
+        [Test]
+        public void Initialized_Always_ExpectedPropertiesSet()
+        {
+            // Assert
+            Assert.AreEqual(typeof(PipingFailurePathContext), info.DataType);
+            Assert.AreEqual(typeof(PipingFailurePathContext), info.ViewDataType);
+        }
+
+        [Test]
+        public void GetViewName_WithPipingFailureMechanismContext_ReturnsNameOfFailureMechanism()
+        {
+            // Setup
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new PipingFailureMechanism();
+            var context = new PipingFailurePathContext(failureMechanism, assessmentSection);
+
+            // Call
+            string viewName = info.GetViewName(null, context);
+
+            // Assert
+            Assert.AreEqual(failureMechanism.Name, viewName);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void Image_Always_ReturnsGenericInputOutputIcon()
+        {
+            // Call
+            Image image = info.Image;
+
+            // Assert
+            TestHelper.AssertImagesAreEqual(RiskeerCommonFormsResources.FailureMechanismIcon, image);
+        }
+
+        [Test]
+        public void CloseForData_ViewNotCorrespondingToRemovedAssessmentSection_ReturnsFalse()
+        {
+            // Setup
+            var otherAssessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var assessmentSection = new AssessmentSectionStub();
+            var failureMechanism = new PipingFailureMechanism();
+
+            var view = new PipingFailurePathView(failureMechanism, assessmentSection);
+
+            // Call
+            bool closeForData = info.CloseForData(view, otherAssessmentSection);
+
+            // Assert
+            Assert.IsFalse(closeForData);
+
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CloseForData_ViewCorrespondingToRemovedAssessmentSection_ReturnsTrue()
+        {
+            // Setup
+            var assessmentSection = new AssessmentSectionStub();
+            var failureMechanism = new PipingFailureMechanism();
+
+            var view = new PipingFailurePathView(failureMechanism, assessmentSection);
+
+            // Call
+            bool closeForData = info.CloseForData(view, assessmentSection);
+
+            // Assert
+            Assert.IsTrue(closeForData);
+        }
+
+        [Test]
+        public void CloseForData_ViewNotCorrespondingToRemovedFailureMechanism_ReturnsFalse()
+        {
+            // Setup
+            var assessmentSection = new AssessmentSectionStub();
+            var failureMechanism = new PipingFailureMechanism();
+            var otherPipingFailureMechanism = new PipingFailureMechanism();
+
+            var view = new PipingFailurePathView(failureMechanism, assessmentSection);
+
+            // Call
+            bool closeForData = info.CloseForData(view, otherPipingFailureMechanism);
+
+            // Assert
+            Assert.IsFalse(closeForData);
+        }
+
+        [Test]
+        public void CloseForData_ViewCorrespondingToRemovedFailureMechanism_ReturnsTrue()
+        {
+            // Setup
+            var assessmentSection = new AssessmentSectionStub();
+            var failureMechanism = new PipingFailureMechanism();
+
+            var view = new PipingFailurePathView(failureMechanism, assessmentSection);
+
+            // Call
+            bool closeForData = info.CloseForData(view, failureMechanism);
+
+            // Assert
+            Assert.IsTrue(closeForData);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void AdditionalDataCheck_Always_ReturnTrueOnlyIfFailureMechanismRelevant(bool isRelevant)
+        {
+            // Setup
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new PipingFailureMechanism
+            {
+                IsRelevant = isRelevant
+            };
+
+            var context = new PipingFailurePathContext(failureMechanism, assessmentSection);
+
+            // Call
+            bool result = info.AdditionalDataCheck(context);
+
+            // Assert
+            Assert.AreEqual(isRelevant, result);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CreateInstance_WithContext_ReturnPipingFailurePathView()
+        {
+            // Setup
+            var assessmentSection = new AssessmentSectionStub();
+            var failureMechanism = new PipingFailureMechanism();
+
+            var context = new PipingFailurePathContext(failureMechanism, assessmentSection);
+
+            // Call
+            IView view = info.CreateInstance(context);
+
+            // Assert
+            Assert.IsInstanceOf<PipingFailurePathView>(view);
+
+            var failureMechanismView = (PipingFailurePathView) view;
+            Assert.AreSame(failureMechanism, failureMechanismView.FailureMechanism);
+            Assert.AreSame(assessmentSection, failureMechanismView.AssessmentSection);
+        }
+    }
+}
