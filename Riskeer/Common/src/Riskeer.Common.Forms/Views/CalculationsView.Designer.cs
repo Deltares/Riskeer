ee3358f8e00c94006764cc444c5adc7a507a897b
Robert Tangerman <robert.tangerman@deltares.nl>
2021-07-07 11:22:41 +0200

    Removed sections from view. WTI-2355

--- Riskeer/Common/src/Riskeer.Common.Forms/Views/CalculationsView.Designer.cs
+++ Riskeer/Common/src/Riskeer.Common.Forms/Views/CalculationsView.Designer.cs
@@ -36,59 +36,14 @@ namespace Riskeer.Common.Forms.Views
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer = new System.Windows.Forms.SplitContainer();
-            this.listBox = new System.Windows.Forms.ListBox();
-            this.sectionsLabel = new System.Windows.Forms.Label();
             this.dataGridTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.dataGridViewControl = new Core.Common.Controls.DataGrid.DataGridViewControl();
             this.calculationsLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.generateButton = new Core.Common.Controls.Forms.EnhancedButton();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
-            this.splitContainer.Panel1.SuspendLayout();
-            this.splitContainer.Panel2.SuspendLayout();
-            this.splitContainer.SuspendLayout();
             this.dataGridTableLayoutPanel.SuspendLayout();
             this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // splitContainer
-            // 
-            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer.Location = new System.Drawing.Point(3, 3);
-            this.splitContainer.Name = "splitContainer";
-            // 
-            // splitContainer.Panel1
-            // 
-            this.splitContainer.Panel1.Controls.Add(this.listBox);
-            this.splitContainer.Panel1.Controls.Add(this.sectionsLabel);
-            // 
-            // splitContainer.Panel2
-            // 
-            this.splitContainer.Panel2.Controls.Add(this.dataGridTableLayoutPanel);
-            this.splitContainer.Panel2.Controls.Add(this.calculationsLabel);
-            this.splitContainer.Size = new System.Drawing.Size(1343, 598);
-            this.splitContainer.SplitterDistance = 250;
-            this.splitContainer.TabIndex = 0;
-            // 
-            // listBox
-            // 
-            this.listBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox.FormattingEnabled = true;
-            this.listBox.Location = new System.Drawing.Point(0, 13);
-            this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(250, 585);
-            this.listBox.TabIndex = 1;
-            // 
-            // sectionsLabel
-            // 
-            this.sectionsLabel.AutoSize = true;
-            this.sectionsLabel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.sectionsLabel.Location = new System.Drawing.Point(0, 0);
-            this.sectionsLabel.Name = "sectionsLabel";
-            this.sectionsLabel.Size = new System.Drawing.Size(26, 13);
-            this.sectionsLabel.TabIndex = 0;
-            this.sectionsLabel.Text = "Vak";
             // 
             // dataGridTableLayoutPanel
             // 
@@ -96,21 +51,22 @@ namespace Riskeer.Common.Forms.Views
             this.dataGridTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.dataGridTableLayoutPanel.Controls.Add(this.dataGridViewControl, 0, 0);
             this.dataGridTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridTableLayoutPanel.Location = new System.Drawing.Point(0, 13);
+            this.dataGridTableLayoutPanel.Location = new System.Drawing.Point(3, 3);
             this.dataGridTableLayoutPanel.Name = "dataGridTableLayoutPanel";
             this.dataGridTableLayoutPanel.RowCount = 1;
             this.dataGridTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.dataGridTableLayoutPanel.Size = new System.Drawing.Size(1089, 585);
+            this.dataGridTableLayoutPanel.Size = new System.Drawing.Size(1343, 582);
             this.dataGridTableLayoutPanel.TabIndex = 1;
             // 
             // dataGridViewControl
             // 
             this.dataGridViewControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridViewControl.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewControl.Location = new System.Drawing.Point(4, 4);
+            this.dataGridViewControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridViewControl.MultiSelect = true;
             this.dataGridViewControl.Name = "dataGridViewControl";
             this.dataGridViewControl.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.RowHeaderSelect;
-            this.dataGridViewControl.Size = new System.Drawing.Size(1083, 579);
+            this.dataGridViewControl.Size = new System.Drawing.Size(1335, 574);
             this.dataGridViewControl.TabIndex = 0;
             // 
             // calculationsLabel
@@ -119,7 +75,7 @@ namespace Riskeer.Common.Forms.Views
             this.calculationsLabel.Dock = System.Windows.Forms.DockStyle.Top;
             this.calculationsLabel.Location = new System.Drawing.Point(0, 0);
             this.calculationsLabel.Name = "calculationsLabel";
-            this.calculationsLabel.Size = new System.Drawing.Size(182, 13);
+            this.calculationsLabel.Size = new System.Drawing.Size(230, 16);
             this.calculationsLabel.TabIndex = 0;
             this.calculationsLabel.Text = "Berekeningen voor geselecteerd vak";
             // 
@@ -127,50 +83,42 @@ namespace Riskeer.Common.Forms.Views
             // 
             this.tableLayoutPanel.ColumnCount = 1;
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel.Controls.Add(this.splitContainer, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.dataGridTableLayoutPanel, 0, 0);
             this.tableLayoutPanel.Controls.Add(this.generateButton, 0, 1);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 16);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 2;
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(1349, 644);
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(1349, 628);
             this.tableLayoutPanel.TabIndex = 0;
             // 
             // generateButton
             // 
-            this.generateButton.Location = new System.Drawing.Point(3, 607);
+            this.generateButton.Location = new System.Drawing.Point(3, 591);
             this.generateButton.Name = "generateButton";
             this.generateButton.Padding = new System.Windows.Forms.Padding(2);
-            this.generateButton.Size = new System.Drawing.Size(170, 34);
+            this.generateButton.Size = new System.Drawing.Size(170, 14);
             this.generateButton.TabIndex = 1;
-            this.generateButton.Text = global::Riskeer.Common.Forms.Properties.Resources.CalculationGroup_Generate_calculations;
             this.generateButton.UseVisualStyleBackColor = true;
             this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
             // 
             // CalculationsView
             // 
             this.Controls.Add(this.tableLayoutPanel);
+            this.Controls.Add(this.calculationsLabel);
             this.Name = "CalculationsView";
             this.Size = new System.Drawing.Size(1349, 644);
-            this.splitContainer.Panel1.ResumeLayout(false);
-            this.splitContainer.Panel1.PerformLayout();
-            this.splitContainer.Panel2.ResumeLayout(false);
-            this.splitContainer.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
-            this.splitContainer.ResumeLayout(false);
             this.dataGridTableLayoutPanel.ResumeLayout(false);
             this.tableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer;
-        private System.Windows.Forms.ListBox listBox;
-        private System.Windows.Forms.Label sectionsLabel;
         private System.Windows.Forms.Label calculationsLabel;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
         private System.Windows.Forms.TableLayoutPanel dataGridTableLayoutPanel;
