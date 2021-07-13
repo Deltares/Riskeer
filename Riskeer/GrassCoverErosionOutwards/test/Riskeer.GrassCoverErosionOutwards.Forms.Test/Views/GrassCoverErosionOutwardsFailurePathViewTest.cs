58853f79fbb7ba02053c88cf02f9123f0493c8bc
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-07-13 08:13:05 +0200

    Fixed tests. WTI-2322

--- Riskeer/GrassCoverErosionOutwards/test/Riskeer.GrassCoverErosionOutwards.Forms.Test/Views/GrassCoverErosionOutwardsFailurePathViewTest.cs
+++ Riskeer/GrassCoverErosionOutwards/test/Riskeer.GrassCoverErosionOutwards.Forms.Test/Views/GrassCoverErosionOutwardsFailurePathViewTest.cs
@@ -808,18 +808,6 @@ namespace Riskeer.GrassCoverErosionOutwards.Forms.Test.Views
         {
             MapData[] mapDataArray = mapData.ToArray();
 
-            var referenceLineMapDataObserver = mocks.StrictMock<IObserver>();
-            mapDataArray[referenceLineIndex].Attach(referenceLineMapDataObserver);
-
-            var hydraulicBoundaryLocationsMapDataObserver = mocks.StrictMock<IObserver>();
-            mapDataArray[hydraulicBoundaryLocationsIndex].Attach(hydraulicBoundaryLocationsMapDataObserver);
-
-            var foreshoreProfilesMapDataObserver = mocks.StrictMock<IObserver>();
-            mapDataArray[foreshoreProfilesIndex].Attach(foreshoreProfilesMapDataObserver);
-
-            var calculationsMapDataObserver = mocks.StrictMock<IObserver>();
-            mapDataArray[calculationsIndex].Attach(calculationsMapDataObserver);
-
             MapData[] sectionsCollection = ((MapDataCollection) mapDataArray[sectionsCollectionIndex]).Collection.ToArray();
             var sectionsMapDataObserver = mocks.StrictMock<IObserver>();
             sectionsCollection[sectionsIndex].Attach(sectionsMapDataObserver);
