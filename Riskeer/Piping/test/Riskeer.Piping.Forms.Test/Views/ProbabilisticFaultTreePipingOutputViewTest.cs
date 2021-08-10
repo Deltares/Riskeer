0713398dd811db873d18f57230ed8945e8dda1b4
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-08-10 11:49:50 +0200

    Review related changes. WTI-2374

--- Riskeer/Piping/test/Riskeer.Piping.Forms.Test/Views/ProbabilisticFaultTreePipingOutputViewTest.cs
+++ Riskeer/Piping/test/Riskeer.Piping.Forms.Test/Views/ProbabilisticFaultTreePipingOutputViewTest.cs
@@ -19,6 +19,7 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
+using System.Threading;
 using NUnit.Framework;
 using Riskeer.Common.Data.TestUtil.IllustrationPoints;
 using Riskeer.Common.Forms.Views;
@@ -28,6 +29,7 @@ using Riskeer.Piping.Forms.Views;
 namespace Riskeer.Piping.Forms.Test.Views
 {
     [TestFixture]
+    [Apartment(ApartmentState.STA)]
     public class ProbabilisticFaultTreePipingOutputViewTest
     {
         [Test]
