215c594bc67d2ab8b9ab7e797686eb37625564be
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-08-10 16:09:59 +0200

    Extended IViewHost so that titles can be set dynamically. WTI-2448

--- Core/Common/src/Core.Common.Controls/Views/IView.cs
+++ Core/Common/src/Core.Common.Controls/Views/IView.cs
@@ -32,10 +32,5 @@ namespace Core.Common.Controls.Views
         /// Gets or sets the data shown by the <see cref="IView"/>.
         /// </summary>
         object Data { get; set; }
-
-        /// <summary>
-        /// Gets or sets the caption/title of the <see cref="IView"/>.
-        /// </summary>
-        string Text { get; set; }
     }
 }
