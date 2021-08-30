6f3dc4c0604a47896eb338dd94fcf30080a90924
Robert Tangerman <robert.tangerman@deltares.nl>
2021-08-30 16:07:12 +0200

    Created new exporter. WTI-2462

---
+++ Riskeer/Integration/test/Riskeer.Integration.IO.Test/Exporters/HydraulicBoundaryLocationCalculationsExporterTest.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.IO;
+using System.Linq;
+using Core.Common.Base.IO;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.Common.Data.Hydraulics;
+using Riskeer.Integration.IO.Exporters;
+
+namespace Riskeer.Integration.IO.Test.Exporters
+{
+    [TestFixture]
+    public class HydraulicBoundaryLocationCalculationsExporterTest
+    {
+        [Test]
+        public void Constructor_CalculationsNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new HydraulicBoundaryLocationCalculationsExporter(null, string.Empty);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("calculations", exception.ParamName);
+        }
+        
+        [Test]
+        public void Constructor_FilePathNull_ThrowsArgumentException()
+        {
+            // Call
+            void Call() => new HydraulicBoundaryLocationCalculationsExporter(
+                Enumerable.Empty<HydraulicBoundaryLocationCalculation>(), null);
+
+            // Assert
+            Assert.Throws<ArgumentException>(Call);
+        }
+        
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            string filePath = TestHelper.GetScratchPadPath(Path.Combine("export", "test.shp"));
+
+            // Call
+            var exporter = new HydraulicBoundaryLocationCalculationsExporter(
+                Enumerable.Empty<HydraulicBoundaryLocationCalculation>(), filePath);
+
+            // Assert
+            Assert.IsInstanceOf<IFileExporter>(exporter);
+        }
+    }
+}
