97e6bc5e4e1d9c278905a6fb01fa6d7ec0d232c5
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-09-07 12:13:22 +0200

    Revert "Review related changes. WTI-2471"
    This reverts commit 1462b4c63bb7fccce9663df851d87b2bb74f8694.

--- Riskeer/Integration/test/Riskeer.Integration.Plugin.Test/PropertyInfos/WaterLevelCalculationsForNormTargetProbabilityContextPropertyInfoTest.cs
+++ Riskeer/Integration/test/Riskeer.Integration.Plugin.Test/PropertyInfos/WaterLevelCalculationsForNormTargetProbabilityContextPropertyInfoTest.cs
@@ -56,9 +56,9 @@ namespace Riskeer.Integration.Plugin.Test.PropertyInfos
             var assessmentSection = mockRepository.Stub<IAssessmentSection>();
             mockRepository.ReplayAll();
 
-            var calculationsForTargetProbability = new HydraulicBoundaryLocationCalculationsForTargetProbability(0.01);
+            var hydraulicBoundaryLocationCalculations = new HydraulicBoundaryLocationCalculationsForTargetProbability(0.01);
 
-            var context = new WaterLevelCalculationsForNormTargetProbabilityContext(calculationsForTargetProbability,
+            var context = new WaterLevelCalculationsForNormTargetProbabilityContext(hydraulicBoundaryLocationCalculations,
                                                                                     assessmentSection);
 
             using (var plugin = new RiskeerPlugin())
@@ -70,7 +70,7 @@ namespace Riskeer.Integration.Plugin.Test.PropertyInfos
 
                 // Assert
                 Assert.IsInstanceOf<WaterLevelCalculationsForNormTargetProbabilityProperties>(objectProperties);
-                Assert.AreSame(calculationsForTargetProbability, objectProperties.Data);
+                Assert.AreSame(hydraulicBoundaryLocationCalculations, objectProperties.Data);
             }
 
             mockRepository.VerifyAll();
