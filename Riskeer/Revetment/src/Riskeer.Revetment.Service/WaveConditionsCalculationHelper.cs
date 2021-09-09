dfb55ce2b1bff8a95caab34982608beef88ac8a3
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-09-09 10:05:58 +0200

    Introduced WaveConditionsCalculationHelper. WTI-2470

---
+++ Riskeer/Revetment/src/Riskeer.Revetment.Service/WaveConditionsCalculationHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Revetment.Data;
+
+namespace Riskeer.Revetment.Service
+{
+    /// <summary>
+    /// Helper class containing methods related to wave conditions calculations.
+    /// </summary>
+    public static class WaveConditionsCalculationHelper
+    {
+        /// <summary>
+        /// Gets the target probability to use during wave conditions calculations based on the provided <paramref name="input"/>.
+        /// </summary>
+        /// <param name="input">The wave conditions input to get the target probability for.</param>
+        /// <param name="assessmentSection">The assessment section the wave conditions input belongs to.</param>
+        /// <returns>A target probability.</returns>
+        /// <exception cref="NullReferenceException">Thrown when any input parameter is <c>null</c>.</exception>
+        /// <exception cref="InvalidEnumArgumentException">Thrown when <see cref="WaveConditionsInput.WaterLevelType"/>
+        /// is an invalid value.</exception>
+        /// <exception cref="NotSupportedException">Thrown when <see cref="WaveConditionsInput.WaterLevelType"/>
+        /// is a valid value, but unsupported.</exception>
+        public static double GetTargetProbability(WaveConditionsInput input, IAssessmentSection assessmentSection)
+        {
+            if (!Enum.IsDefined(typeof(WaveConditionsInputWaterLevelType), input.WaterLevelType))
+            {
+                throw new InvalidEnumArgumentException(nameof(input.WaterLevelType),
+                                                       (int) input.WaterLevelType,
+                                                       typeof(WaveConditionsInputWaterLevelType));
+            }
+
+            switch (input.WaterLevelType)
+            {
+                case WaveConditionsInputWaterLevelType.None:
+                    return double.NaN;
+                case WaveConditionsInputWaterLevelType.Signaling:
+                    return assessmentSection.FailureMechanismContribution.SignalingNorm;
+                case WaveConditionsInputWaterLevelType.LowerLimit:
+                    return assessmentSection.FailureMechanismContribution.LowerLimitNorm;
+                case WaveConditionsInputWaterLevelType.UserDefinedTargetProbability:
+                    return input.CalculationsTargetProbability.TargetProbability;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
