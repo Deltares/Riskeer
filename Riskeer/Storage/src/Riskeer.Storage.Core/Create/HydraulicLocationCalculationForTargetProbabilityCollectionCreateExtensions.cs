ab025c9a24225f2277abf79132e7e5c8d42af931
Dennis Tang <dennis.tang@deltares.nl>
2021-09-15 13:00:09 +0200

    WTI-2452 Added create and read extensions for the HydraulicLocationCalculationForTargetProbabilityCollection entities

---
+++ Riskeer/Storage/src/Riskeer.Storage.Core/Create/HydraulicLocationCalculationForTargetProbabilityCollectionCreateExtensions.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Riskeer.Common.Data.Hydraulics;
+using Riskeer.Storage.Core.DbContext;
+
+namespace Riskeer.Storage.Core.Create
+{
+    /// <summary>
+    /// Extension methods for collections of <see cref="HydraulicBoundaryLocationCalculationsForTargetProbability"/> related to creating a 
+    /// <see cref="HydraulicLocationCalculationForTargetProbabilityCollectionEntity"/>.
+    /// </summary>
+    internal static class HydraulicLocationCalculationForTargetProbabilityCollectionCreateExtensions
+    {
+        /// <summary>
+        /// Creates a <see cref="HydraulicLocationCalculationForTargetProbabilityCollectionEntity"/> based on the information
+        /// of the <paramref name="calculations"/>.
+        /// </summary>
+        /// <param name="calculations">The collection of <see cref="HydraulicBoundaryLocationCalculationsForTargetProbability"/>
+        /// to create a database entity for.</param>
+        /// <param name="calculationType">The type of calculation the entity should be created for.</param>
+        /// <param name="registry">The object keeping track of create operations.</param>
+        /// <returns>A new <see cref="HydraulicLocationCalculationForTargetProbabilityCollectionEntity"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        internal static HydraulicLocationCalculationForTargetProbabilityCollectionEntity Create(this HydraulicBoundaryLocationCalculationsForTargetProbability calculations,
+                                                                                                HydraulicBoundaryLocationCalculationType calculationType,
+                                                                                                PersistenceRegistry registry)
+        {
+            if (calculations == null)
+            {
+                throw new ArgumentNullException(nameof(calculations));
+            }
+
+            if (registry == null)
+            {
+                throw new ArgumentNullException(nameof(registry));
+            }
+
+            var collectionEntity = new HydraulicLocationCalculationForTargetProbabilityCollectionEntity
+            {
+                HydraulicBoundaryLocationCalculationType = Convert.ToByte(calculationType),
+                TargetProbability = calculations.TargetProbability
+            };
+            foreach (HydraulicBoundaryLocationCalculation calculation in calculations.HydraulicBoundaryLocationCalculations)
+            {
+                collectionEntity.HydraulicLocationCalculationEntities.Add(calculation.Create(registry));
+            }
+
+            return collectionEntity;
+        }
+    }
+}
---
+++ Riskeer/Storage/test/Riskeer.Storage.Core.Test/Create/HydraulicLocationCalculationForTargetProbabilityCollectionCreateExtensionTest.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.Common.Data.Hydraulics;
+using Riskeer.Common.Data.TestUtil;
+using Riskeer.Storage.Core.Create;
+using Riskeer.Storage.Core.DbContext;
+
+namespace Riskeer.Storage.Core.Test.Create
+{
+    [TestFixture]
+    public class HydraulicLocationCalculationForTargetProbabilityCollectionCreateExtensionTest
+    {
+        [Test]
+        public void Create_CalculationsNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+            var calculationType = random.NextEnumValue<HydraulicBoundaryLocationCalculationType>();
+
+            
+            // Call
+            TestDelegate call = () =>
+                ((HydraulicBoundaryLocationCalculationsForTargetProbability) null).Create(calculationType, 
+                                                                                          new PersistenceRegistry());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("calculations", exception.ParamName);
+        }
+
+        [Test]
+        public void Create_PersistenceRegistryNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+            var calculationType = random.NextEnumValue<HydraulicBoundaryLocationCalculationType>();
+            var calculations = new HydraulicBoundaryLocationCalculationsForTargetProbability(random.NextDouble(0, 0.1));
+
+            // Call
+            TestDelegate call = () => calculations.Create(calculationType, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("registry", exception.ParamName);
+        }
+
+        [Test]
+        public void Create_WithValidCollection_ReturnsEntityWithExpectedResults()
+        {
+            // Setup
+            var random = new Random(21);
+            var calculationType = random.NextEnumValue<HydraulicBoundaryLocationCalculationType>();
+
+            var hydraulicBoundaryLocation = new TestHydraulicBoundaryLocation();
+            var calculation = new HydraulicBoundaryLocationCalculation(hydraulicBoundaryLocation)
+            {
+                InputParameters =
+                {
+                    ShouldIllustrationPointsBeCalculated = random.NextBoolean()
+                }
+            };
+
+            var calculations = new HydraulicBoundaryLocationCalculationsForTargetProbability(random.NextDouble(0, 0.1))
+            {
+                HydraulicBoundaryLocationCalculations =
+                {
+                    calculation
+                }
+            };
+
+            var hydraulicLocationEntity = new HydraulicLocationEntity();
+            var registry = new PersistenceRegistry();
+            registry.Register(hydraulicLocationEntity, hydraulicBoundaryLocation);
+
+            // Call
+            HydraulicLocationCalculationForTargetProbabilityCollectionEntity entity = calculations.Create(calculationType,
+                                                                                                          registry);
+
+            // Assert
+            Assert.IsNotNull(entity);
+            Assert.AreEqual(calculations.TargetProbability, entity.TargetProbability);
+            Assert.AreEqual(Convert.ToByte(calculationType), entity.HydraulicBoundaryLocationCalculationType);
+
+            HydraulicLocationCalculationEntity hydraulicLocationCalculationEntity = entity.HydraulicLocationCalculationEntities.Single();
+            Assert.AreSame(hydraulicLocationEntity, hydraulicLocationCalculationEntity.HydraulicLocationEntity);
+            Assert.AreEqual(Convert.ToByte(calculation.InputParameters.ShouldIllustrationPointsBeCalculated),
+                            hydraulicLocationCalculationEntity.ShouldIllustrationPointsBeCalculated);
+            CollectionAssert.IsEmpty(hydraulicLocationCalculationEntity.HydraulicLocationOutputEntities);
+        }
+    }
+}
