ad6ef70196dda85e337e8c29fa114dcf5470732d
Robert Tangerman <robert.tangerman@deltares.nl>
2021-09-27 12:31:14 +0200

    Introduced map layer for dune locations. WTI-2490

---
+++ Riskeer/DuneErosion/src/Riskeer.DuneErosion.Forms/Views/DuneErosionLocationsMapLayer.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Components.Gis.Data;
+using Riskeer.Common.Forms.Factories;
+using Riskeer.DuneErosion.Data;
+using Riskeer.DuneErosion.Forms.Factories;
+
+namespace Riskeer.DuneErosion.Forms.Views
+{
+    /// <summary>
+    /// Map layer to show dune erosion locations.
+    /// </summary>
+    public class DuneErosionLocationsMapLayer : IDisposable
+    {
+        private readonly DuneErosionFailureMechanism failureMechanism;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="DuneErosionLocationsMapLayer"/>.
+        /// </summary>
+        /// <param name="failureMechanism">The <see cref="DuneErosionFailureMechanism"/>
+        /// to get the data from.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="failureMechanism"/>
+        /// is <c>null</c>.</exception>
+        public DuneErosionLocationsMapLayer(DuneErosionFailureMechanism failureMechanism)
+        {
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+
+            this.failureMechanism = failureMechanism;
+
+            MapData = RiskeerMapDataFactory.CreateHydraulicBoundaryLocationsMapData();
+            SetFeatures();
+        }
+
+        /// <summary>
+        /// Gets the dune erosion locations map data.
+        /// </summary>
+        public MapPointData MapData { get; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing) {}
+        }
+
+        private void SetFeatures()
+        {
+            MapData.Features = DuneErosionMapDataFeaturesFactory.CreateDuneLocationFeatures(failureMechanism);
+        }
+    }
+}
---
+++ Riskeer/DuneErosion/test/Riskeer.DuneErosion.Forms.Test/Views/DuneErosionLocationsMapLayerTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Core.Components.Gis.Data;
+using NUnit.Framework;
+using Riskeer.DuneErosion.Data;
+using Riskeer.DuneErosion.Data.TestUtil;
+using Riskeer.DuneErosion.Forms.TestUtil;
+using Riskeer.DuneErosion.Forms.Views;
+
+namespace Riskeer.DuneErosion.Forms.Test.Views
+{
+    [TestFixture]
+    public class DuneErosionLocationsMapLayerTest
+    {
+        [Test]
+        public void Constructor_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new DuneErosionLocationsMapLayer(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("failureMechanism", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            var failureMechanism = new DuneErosionFailureMechanism();
+            failureMechanism.SetDuneLocations(new[]
+            {
+                new TestDuneLocation("test")
+            });
+
+            // Call
+            var mapLayer = new DuneErosionLocationsMapLayer(failureMechanism);
+
+            // Assert
+            Assert.IsInstanceOf<IDisposable>(mapLayer);
+            AssertDuneLocationsMapData(failureMechanism, mapLayer.MapData);
+        }
+
+        private static void AssertDuneLocationsMapData(DuneErosionFailureMechanism failureMechanism,
+                                                       MapData mapData)
+        {
+            Assert.IsInstanceOf<MapPointData>(mapData);
+            Assert.AreEqual("Hydraulische belastingen", mapData.Name);
+
+            var duneLocationsMapData = (MapPointData) mapData;
+            DuneErosionMapFeaturesTestHelper.AssertDuneLocationFeaturesData(failureMechanism, duneLocationsMapData.Features);
+        }
+    }
+}
