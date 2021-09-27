82467c07fff7af740cbc6bc1245471e522cd7f24
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-09-27 14:58:59 +0200

    Introducing HydraulicBoundaryLocationCalculationsNamingHelper. WTI-2503

---
+++ Riskeer/Common/src/Riskeer.Common.Forms/Helpers/HydraulicBoundaryLocationCalculationsNamingHelper.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Data.Hydraulics;
+using Riskeer.Common.Forms.TypeConverters;
+
+namespace Riskeer.Common.Forms.Helpers
+{
+    /// <summary>
+    /// This class helps in uniquely naming the different hydraulic boundary location calculations within
+    /// an <see cref="IAssessmentSection"/>.
+    /// </summary>
+    public static class HydraulicBoundaryLocationCalculationsNamingHelper
+    {
+        private static readonly NoProbabilityValueDoubleConverter noProbabilityValueDoubleConverter = new NoProbabilityValueDoubleConverter();
+
+        public string GetUniqueNameForWaterLevelCalculations(IAssessmentSection assessmentSection, IEnumerable<HydraulicBoundaryLocationCalculation> calculations)
+        {
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            if (calculations == null)
+            {
+                throw new ArgumentNullException(nameof(calculations));
+            }
+
+            var waterLevelCalculationsNameLookup = new Dictionary<object, string>
+            {
+                {
+                    assessmentSection.WaterLevelCalculationsForLowerLimitNorm,
+                    noProbabilityValueDoubleConverter.ConvertToString(assessmentSection.FailureMechanismContribution.LowerLimitNorm)
+                },
+                {
+                    assessmentSection.WaterLevelCalculationsForSignalingNorm,
+                    noProbabilityValueDoubleConverter.ConvertToString(assessmentSection.FailureMechanismContribution.SignalingNorm)
+                }
+            };
+
+            foreach (HydraulicBoundaryLocationCalculationsForTargetProbability calculationsForTargetProbability in assessmentSection.WaterLevelCalculationsForUserDefinedTargetProbabilities)
+            {
+                waterLevelCalculationsNameLookup.Add(calculationsForTargetProbability.HydraulicBoundaryLocationCalculations,
+                                                     noProbabilityValueDoubleConverter.ConvertToString(calculationsForTargetProbability.TargetProbability));
+            }
+
+            if (!waterLevelCalculationsNameLookup.ContainsKey(calculations))
+            {
+                throw new InvalidOperationException("The provided calculations object is not part of the water level calculations within the assessment section.");
+            }
+
+            return GetUniqueNameLookup(waterLevelCalculationsNameLookup)[calculations];
+        }
+
+        private static Dictionary<object, string> GetUniqueNameLookup(Dictionary<object, string> waterLevelCalculationsNameLookup)
+        {
+            var waterLevelCalculationsUniqueNameLookup = new Dictionary<object, string>();
+
+            while (waterLevelCalculationsNameLookup.Any())
+            {
+                KeyValuePair<object, string> firstElement = waterLevelCalculationsNameLookup.First();
+
+                IEnumerable<KeyValuePair<object, string>> elementsWithNameOfFirstElement =
+                    waterLevelCalculationsNameLookup.Where(e => e.Value.Equals(firstElement.Value));
+
+                int count = elementsWithNameOfFirstElement.Count();
+
+                if (count == 1)
+                {
+                    waterLevelCalculationsUniqueNameLookup.Add(firstElement.Key, firstElement.Value);
+                    waterLevelCalculationsNameLookup.Remove(firstElement.Key);
+                }
+                else
+                {
+                    for (var i = 0; i < count; i++)
+                    {
+                        KeyValuePair<object, string> element = elementsWithNameOfFirstElement.ElementAt(i);
+
+                        waterLevelCalculationsUniqueNameLookup.Add(element.Key, element.Value + $"({i + 1})");
+                        waterLevelCalculationsNameLookup.Remove(element.Key);
+                    }
+                }
+            }
+
+            return waterLevelCalculationsUniqueNameLookup;
+        }
+    }
+}
