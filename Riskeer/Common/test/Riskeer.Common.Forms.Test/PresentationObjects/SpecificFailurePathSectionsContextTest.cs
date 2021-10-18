e66dcb4b62fb6cf0fc4bddcb45eb2ac2194c1232
Dennis Tang <dennis.tang@deltares.nl>
2021-10-18 11:01:22 +0200

    WTI-2507: Added sections context for the SpecificFailurePath

---
+++ Riskeer/Common/test/Riskeer.Common.Forms.Test/PresentationObjects/SpecificFailurePathSectionsContextTest.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Data.FailurePath;
+using Riskeer.Common.Forms.PresentationObjects;
+
+namespace Riskeer.Common.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class SpecificFailurePathSectionsContextTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failurePath = new SpecificFailurePath();
+
+            // Call
+            var context = new SpecificFailurePathSectionsContext(failurePath, assessmentSection);
+
+            // Assert
+            Assert.IsInstanceOf<FailureMechanismSectionsContext>(context);
+            Assert.AreSame(failurePath, context.WrappedData);
+            Assert.AreSame(assessmentSection, context.AssessmentSection);
+            mocks.VerifyAll();
+        }
+    }
+}
