74e67fe2510a464c3485e11e21c8fb7f525206eb
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-10-25 08:06:54 +0200

    Fixed bug that wave impact asphalt cover failure path view would open even when not relevant. WTI-2491

--- Riskeer/WaveImpactAsphaltCover/test/Riskeer.WaveImpactAsphaltCover.Plugin.Test/ViewInfos/WaveImpactAsphaltCoverFailurePathViewInfoTest.cs
+++ Riskeer/WaveImpactAsphaltCover/test/Riskeer.WaveImpactAsphaltCover.Plugin.Test/ViewInfos/WaveImpactAsphaltCoverFailurePathViewInfoTest.cs
@@ -93,6 +93,30 @@ namespace Riskeer.WaveImpactAsphaltCover.Plugin.Test.ViewInfos
         }
 
         [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void AdditionalDataCheck_Always_ReturnTrueOnlyIfFailureMechanismRelevant(bool isRelevant)
+        {
+            // Setup
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var failureMechanism = new WaveImpactAsphaltCoverFailureMechanism
+            {
+                IsRelevant = isRelevant
+            };
+
+            var context = new WaveImpactAsphaltCoverFailurePathContext(failureMechanism, assessmentSection);
+
+            // Call
+            bool result = info.AdditionalDataCheck(context);
+
+            // Assert
+            Assert.AreEqual(isRelevant, result);
+            mocks.VerifyAll();
+        }
+
+        [Test]
         [Apartment(ApartmentState.STA)]
         public void CreateInstance_WithContext_ReturnWaveImpactAsphaltCoverFailurePathView()
         {
