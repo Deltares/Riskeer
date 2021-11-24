b00f90b641c3c2975346118800c76fd95b738703
Dennis Tang <dennis.tang@deltares.nl>
2021-11-24 14:32:10 +0100

    WTI-2590: Implemented a new interface of the IAssemblyToolKernelFactory to create instances based on the new kernel

---
+++ Riskeer/AssemblyTool/src/Riskeer.AssemblyTool.KernelWrapper/Kernels/IAssemblyToolKernelFactory.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Assembly.Kernel.Interfaces;
+
+namespace Riskeer.AssemblyTool.KernelWrapper.Kernels
+{
+    /// <summary>
+    /// Factory responsible for creating kernels that can be used for performing an assembly tool calculation.
+    /// </summary>
+    public interface IAssemblyToolKernelFactory
+    {
+        /// <summary>
+        /// Creates an assembly categories kernel.
+        /// </summary>
+        /// <returns>A new <see cref="ICategoryLimitsCalculator"/>.</returns>
+        ICategoryLimitsCalculator CreateAssemblyCategoriesKernel();
+
+        /// <summary>
+        /// Creates a failure mechanism section assembly kernel.
+        /// </summary>
+        /// <returns>A new <see cref="IAssessmentResultsTranslator"/>.</returns>
+        IAssessmentResultsTranslator CreateFailureMechanismSectionAssemblyKernel();
+
+        /// <summary>
+        /// Creates a failure path assembly kernel.
+        /// </summary>
+        /// <returns>A new <see cref="IAssessmentResultsTranslator"/>.</returns>
+        IFailurePathResultAssembler CreateFailurePathAssemblyKernel();
+    }
+}
---
+++ Riskeer/AssemblyTool/test/Riskeer.AssemblyTool.KernelWrapper.Test/Kernels/AssemblyToolKernelFactoryTest.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Assembly.Kernel.Implementations;
+using Assembly.Kernel.Interfaces;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.AssemblyTool.KernelWrapper.Kernels;
+
+namespace Riskeer.AssemblyTool.KernelWrapper.Test.Kernels
+{
+    [TestFixture]
+    public class AssemblyToolKernelFactoryTest
+    {
+        [Test]
+        public void Instance_Always_ReturnsAnInstance()
+        {
+            // Call
+            IAssemblyToolKernelFactory factory = AssemblyToolKernelFactory.Instance;
+
+            // Assert
+            Assert.IsInstanceOf<AssemblyToolKernelFactory>(factory);
+        }
+
+        [Test]
+        public void Instance_WhenSetToNull_ReturnsNewInstance()
+        {
+            // Setup
+            IAssemblyToolKernelFactory firstFactory = AssemblyToolKernelFactory.Instance;
+            AssemblyToolKernelFactory.Instance = null;
+
+            // Call
+            IAssemblyToolKernelFactory secondFactory = AssemblyToolKernelFactory.Instance;
+
+            // Assert
+            Assert.AreNotSame(firstFactory, secondFactory);
+        }
+
+        [Test]
+        public void Instance_WhenSetToInstance_ReturnsThatInstance()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var firstFactory = mocks.Stub<IAssemblyToolKernelFactory>();
+            mocks.ReplayAll();
+
+            AssemblyToolKernelFactory.Instance = firstFactory;
+
+            // Call
+            IAssemblyToolKernelFactory secondFactory = AssemblyToolKernelFactory.Instance;
+
+            // Assert
+            Assert.AreSame(firstFactory, secondFactory);
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void CreateAssemblyCategoriesKernel_Always_ReturnsKernelCategoryLimitsCalculator()
+        {
+            // Setup
+            IAssemblyToolKernelFactory factory = AssemblyToolKernelFactory.Instance;
+
+            // Call
+            ICategoryLimitsCalculator assemblyCategoriesKernel = factory.CreateAssemblyCategoriesKernel();
+
+            // Assert
+            Assert.IsInstanceOf<CategoryLimitsCalculator>(assemblyCategoriesKernel);
+        }
+
+        [Test]
+        public void CreateFailureMechanismSectionAssemblyKernel_Always_ReturnsKernelAssessmentResultsTranslator()
+        {
+            // Setup
+            IAssemblyToolKernelFactory factory = AssemblyToolKernelFactory.Instance;
+
+            // Call
+            IAssessmentResultsTranslator kernel = factory.CreateFailureMechanismSectionAssemblyKernel();
+
+            // Assert
+            Assert.IsInstanceOf<AssessmentResultsTranslator>(kernel);
+        }
+
+        [Test]
+        public void CreateFailurePathAssemblyKernel_Always_ReturnsKernelFailureMechanismResultAssembler()
+        {
+            // Setup
+            IAssemblyToolKernelFactory factory = AssemblyToolKernelFactory.Instance;
+
+            // Call
+            IFailurePathResultAssembler kernel = factory.CreateFailurePathAssemblyKernel();
+
+            // Assert
+            Assert.IsInstanceOf<FailurePathResultAssembler>(kernel);
+        }
+    }
+}
