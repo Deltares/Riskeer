f64df150b0bc7d389a61e2f143ee5216baeb6197
Dennis Tang <dennis.tang@deltares.nl>
2021-11-25 13:18:34 +0100

    WTI-2590: Implemented input class to perform the failure mechanism section assembly with.

---
+++ Riskeer/AssemblyTool/src/Riskeer.AssemblyTool.Data/FailureMechanismSectionAssemblyInput.cs
@@ -0,0 +1,60 @@
+﻿namespace Riskeer.AssemblyTool.Data
+{
+    /// <summary>
+    /// Class that contains the data that is necessary to determine the failure mechanism section assembly. 
+    /// </summary>
+    public class FailureMechanismSectionAssemblyInput
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="FailureMechanismSectionAssemblyInput"/>.
+        /// </summary>
+        /// <param name="isRelevant">The indicator whether the section is relevant.</param>
+        /// <param name="profileProbability">The probability for the profile.</param>
+        /// <param name="sectionProbability">The probability for the section.</param>
+        /// <param name="needsRefinement">The indicator whether the section needs refinement.</param>
+        /// <param name="refinedProfileProbability">The refined probability for the profile.</param>
+        /// <param name="refinedSectionProbability">The refined probability for the section.</param>
+        public FailureMechanismSectionAssemblyInput(bool isRelevant,
+                                                    double profileProbability, double sectionProbability,
+                                                    bool needsRefinement,
+                                                    double refinedProfileProbability, double refinedSectionProbability)
+        {
+            IsRelevant = isRelevant;
+            ProfileProbability = profileProbability;
+            SectionProbability = sectionProbability;
+            NeedsRefinement = needsRefinement;
+            RefinedProfileProbability = refinedProfileProbability;
+            RefinedSectionProbability = refinedSectionProbability;
+        }
+
+        /// <summary>
+        /// Gets the indicator whether the section is relevant.
+        /// </summary>
+        public bool IsRelevant { get; }
+
+        /// <summary>
+        /// Gets the probability for the profile.
+        /// </summary>
+        public double ProfileProbability { get; }
+
+        /// <summary>
+        /// Gets the probability for the section.
+        /// </summary>
+        public double SectionProbability { get; }
+
+        /// <summary>
+        /// Gets the indicator whether the section needs refinement.
+        /// </summary>
+        public bool NeedsRefinement { get; }
+
+        /// <summary>
+        /// Gets the refined probability for the profile.
+        /// </summary>
+        public double RefinedProfileProbability { get; }
+
+        /// <summary>
+        /// Gets the refined probability for the section.
+        /// </summary>
+        public double RefinedSectionProbability { get; }
+    }
+}
---
+++ Riskeer/AssemblyTool/test/Riskeer.AssemblyTool.Data.Test/FailureMechanismSectionAssemblyInputTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+
+namespace Riskeer.AssemblyTool.Data.Test
+{
+    [TestFixture]
+    public class FailureMechanismSectionAssemblyInputTest
+    {
+        [Test]
+        public void Constructor_WithArguments_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            bool isRelevant = random.NextBoolean();
+            double profileProbability = random.NextDouble();
+            double sectionProbability = random.NextDouble();
+            bool needsRefinement = random.NextBoolean();
+            double refinedProfileProbability = random.NextDouble();
+            double refinedSectionProbability = random.NextDouble();
+
+            // Call
+            var input = new FailureMechanismSectionAssemblyInput(isRelevant,
+                                                                 profileProbability, sectionProbability,
+                                                                 needsRefinement,
+                                                                 refinedProfileProbability, refinedSectionProbability);
+
+            // Assert
+            Assert.AreEqual(isRelevant, input.IsRelevant);
+            Assert.AreEqual(profileProbability, input.ProfileProbability);
+            Assert.AreEqual(sectionProbability, input.SectionProbability);
+            Assert.AreEqual(needsRefinement, input.NeedsRefinement);
+            Assert.AreEqual(refinedProfileProbability, input.RefinedProfileProbability);
+            Assert.AreEqual(refinedSectionProbability, input.RefinedSectionProbability);
+        }
+    }
+}
