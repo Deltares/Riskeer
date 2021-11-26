368312895bc9b673831dd308d041c8ed6a9340c7
Dennis Tang <dennis.tang@deltares.nl>
2021-11-26 13:33:48 +0100

    WTI-2590: Added creator to create kernel inputs when performing a failure path assembly.

---
+++ Riskeer/AssemblyTool/src/Riskeer.AssemblyTool.KernelWrapper/Creators/FailurePathAssemblyCalculatorInputCreator.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using Assembly.Kernel.Model;
+using Assembly.Kernel.Model.Categories;
+using Assembly.Kernel.Model.FailurePathSections;
+using Riskeer.AssemblyTool.Data;
+
+namespace Riskeer.AssemblyTool.KernelWrapper.Creators
+{
+    /// <summary>
+    /// Creates input instances that can be used in the failure path assembly calculator.
+    /// </summary>
+    internal static class FailurePathAssemblyCalculatorInputCreator
+    {
+        /// <summary>
+        /// Creates a <see cref="FailurePathSectionAssemblyResult"/> based on <paramref name="result"/>.
+        /// </summary>
+        /// <param name="result">The <see cref="FailureMechanismSectionAssemblyResult"/> to create the
+        /// <see cref="FailurePathSectionAssemblyResult"/> with.</param>
+        /// <returns>A <see cref="FailurePathSectionAssemblyResult"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="result"/> is <c>null</c>.</exception>
+        /// <exception cref="InvalidEnumArgumentException">Thrown when <see cref="FailureMechanismSectionAssemblyGroup"/>
+        /// is an invalid value.</exception>
+        /// <exception cref="NotSupportedException">Thrown when <see cref="FailureMechanismSectionAssemblyGroup"/>
+        /// is a valid value, but unsupported.</exception>
+        public static FailurePathSectionAssemblyResult CreateFailurePathSectionAssemblyResult(FailureMechanismSectionAssemblyResult result)
+        {
+            if (result == null)
+            {
+                throw new ArgumentNullException(nameof(result));
+            }
+
+            return new FailurePathSectionAssemblyResult(new Probability(result.ProfileProbability),
+                                                        new Probability(result.SectionProbability),
+                                                        CreateInterpretationCategory(result.AssemblyGroup));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="EInterpretationCategory"/> into a <see cref="FailureMechanismSectionAssemblyGroup"/>.
+        /// </summary>
+        /// <param name="assemblyGroup">The <see cref="FailureMechanismSectionAssemblyGroup"/> to convert.</param>
+        /// <returns>A <see cref="EInterpretationCategory"/> based on <paramref name="assemblyGroup"/>.</returns>
+        /// <exception cref="InvalidEnumArgumentException">Thrown when <paramref name="assemblyGroup"/>
+        /// is an invalid value.</exception>
+        /// <exception cref="NotSupportedException">Thrown when <paramref name="assemblyGroup"/>
+        /// is a valid value, but unsupported.</exception>
+        private static EInterpretationCategory CreateInterpretationCategory(FailureMechanismSectionAssemblyGroup assemblyGroup)
+        {
+            if (!Enum.IsDefined(typeof(FailureMechanismSectionAssemblyGroup), assemblyGroup))
+            {
+                throw new InvalidEnumArgumentException(nameof(assemblyGroup),
+                                                       (int) assemblyGroup,
+                                                       typeof(FailureMechanismSectionAssemblyGroup));
+            }
+
+            switch (assemblyGroup)
+            {
+                case FailureMechanismSectionAssemblyGroup.ND:
+                    return EInterpretationCategory.ND;
+                case FailureMechanismSectionAssemblyGroup.III:
+                    return EInterpretationCategory.III;
+                case FailureMechanismSectionAssemblyGroup.II:
+                    return EInterpretationCategory.II;
+                case FailureMechanismSectionAssemblyGroup.I:
+                    return EInterpretationCategory.I;
+                case FailureMechanismSectionAssemblyGroup.ZeroPlus:
+                    return EInterpretationCategory.ZeroPlus;
+                case FailureMechanismSectionAssemblyGroup.Zero:
+                    return EInterpretationCategory.Zero;
+                case FailureMechanismSectionAssemblyGroup.IMin:
+                    return EInterpretationCategory.IMin;
+                case FailureMechanismSectionAssemblyGroup.IIMin:
+                    return EInterpretationCategory.IIMin;
+                case FailureMechanismSectionAssemblyGroup.IIIMin:
+                    return EInterpretationCategory.IIIMin;
+                case FailureMechanismSectionAssemblyGroup.D:
+                    return EInterpretationCategory.D;
+                case FailureMechanismSectionAssemblyGroup.Gr:
+                    return EInterpretationCategory.Gr;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
---
+++ Riskeer/AssemblyTool/test/Riskeer.AssemblyTool.KernelWrapper.Test/Creators/FailurePathAssemblyCalculatorInputCreatorTest.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using Assembly.Kernel.Model.Categories;
+using Assembly.Kernel.Model.FailurePathSections;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.AssemblyTool.Data;
+using Riskeer.AssemblyTool.KernelWrapper.Creators;
+using Riskeer.AssemblyTool.KernelWrapper.TestUtil;
+
+namespace Riskeer.AssemblyTool.KernelWrapper.Test.Creators
+{
+    [TestFixture]
+    public class FailurePathAssemblyCalculatorInputCreatorTest
+    {
+        [Test]
+        public void CreateFailurePathSectionAssemblyResult_ResultNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => FailurePathAssemblyCalculatorInputCreator.CreateFailurePathSectionAssemblyResult(null);
+
+            // Assert
+            Assert.That(Call, Throws.TypeOf<ArgumentNullException>()
+                                    .With.Property(nameof(ArgumentNullException.ParamName))
+                                    .EqualTo("result"));
+        }
+
+        [Test]
+        [TestCase(FailureMechanismSectionAssemblyGroup.ND, EInterpretationCategory.ND)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.III, EInterpretationCategory.III)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.II, EInterpretationCategory.II)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.I, EInterpretationCategory.I)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.ZeroPlus, EInterpretationCategory.ZeroPlus)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.Zero, EInterpretationCategory.Zero)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.IMin, EInterpretationCategory.IMin)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.IIMin, EInterpretationCategory.IIMin)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.IIIMin, EInterpretationCategory.IIIMin)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.D, EInterpretationCategory.D)]
+        [TestCase(FailureMechanismSectionAssemblyGroup.Gr, EInterpretationCategory.Gr)]
+        public void CreateFailurePathSectionAssemblyResult_WithValidResult_ReturnsExpectedFailurePathSectionAssemblyResult(
+            FailureMechanismSectionAssemblyGroup assemblyGroup, EInterpretationCategory expectedCategory)
+        {
+            // Setup
+            var random = new Random(21);
+            double profileProbability = random.NextDouble();
+            double sectionProbability = profileProbability + 0.001;
+
+            var result = new FailureMechanismSectionAssemblyResult(profileProbability, sectionProbability,
+                                                                   random.NextDouble(),
+                                                                   assemblyGroup);
+            // Call
+            FailurePathSectionAssemblyResult createdResult = FailurePathAssemblyCalculatorInputCreator.CreateFailurePathSectionAssemblyResult(result);
+
+            // Assert
+            ProbabilityAssert.AreEqual(profileProbability, createdResult.ProbabilityProfile);
+            ProbabilityAssert.AreEqual(sectionProbability, createdResult.ProbabilitySection);
+            Assert.AreEqual(expectedCategory, createdResult.InterpretationCategory);
+        }
+
+        [Test]
+        public void CreateFailurePathSectionAssemblyResult_InvalidAssemblyGroup_ThrowsInvalidEnumArgumentException()
+        {
+            // Setup
+            var random = new Random(21);
+            double probability = random.NextDouble();
+            var result = new FailureMechanismSectionAssemblyResult(probability, probability,
+                                                                   random.NextDouble(),
+                                                                   (FailureMechanismSectionAssemblyGroup) 99);
+
+            // Call
+            void Call() => FailurePathAssemblyCalculatorInputCreator.CreateFailurePathSectionAssemblyResult(result);
+
+            // Assert
+            var expectedMessage = $"The value of argument 'assemblyGroup' (99) is invalid for Enum type '{nameof(FailureMechanismSectionAssemblyGroup)}'.";
+            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<InvalidEnumArgumentException>(Call, expectedMessage);
+        }
+    }
+}
