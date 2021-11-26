68f93b2729ea817ed986e779bde5599990eb2138
Robert Tangerman <robert.tangerman@deltares.nl>
2021-11-26 06:39:14 +0100

    Added view model for sections in scenarios view. WTI-2567

---
+++ Riskeer/Piping/src/Riskeer.Piping.Forms/PresentationObjects/PipingScenariosViewFailureMechanismSectionViewModel.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Piping.Data;
+using Riskeer.Piping.Forms.Views;
+
+namespace Riskeer.Piping.Forms.PresentationObjects
+{
+    /// <summary>
+    /// ViewModel for a <see cref="FailureMechanismSection"/> as shown in the <see cref="PipingScenariosView"/>.
+    /// </summary>
+    public class PipingScenariosViewFailureMechanismSectionViewModel
+    {
+        private readonly PipingFailureMechanism failureMechanism;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="PipingScenariosViewFailureMechanismSectionViewModel"/>.
+        /// </summary>
+        /// <param name="failureMechanismSection">The wrapped <see cref="FailureMechanismSection"/>.</param>
+        /// <param name="failureMechanism">The failure mechanism the section belongs to.</param>
+        /// <param name="scenarioConfigurationPerSection">The scenario configuration that belongs to the section.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public PipingScenariosViewFailureMechanismSectionViewModel(FailureMechanismSection failureMechanismSection,
+                                                                   PipingFailureMechanism failureMechanism,
+                                                                   PipingScenarioConfigurationPerFailureMechanismSection scenarioConfigurationPerSection)
+        {
+            if (failureMechanismSection == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSection));
+            }
+
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+
+            if (scenarioConfigurationPerSection == null)
+            {
+                throw new ArgumentNullException(nameof(scenarioConfigurationPerSection));
+            }
+
+            Section = failureMechanismSection;
+            this.failureMechanism = failureMechanism;
+            ScenarioConfigurationPerSection = scenarioConfigurationPerSection;
+        }
+
+        /// <summary>
+        /// Gets the wrapped <see cref="FailureMechanismSection"/>.
+        /// </summary>
+        public FailureMechanismSection Section { get; }
+
+        /// <summary>
+        /// Gets the <see cref="PipingScenarioConfigurationPerFailureMechanismSection"/> that belongs to the section.
+        /// </summary>
+        public PipingScenarioConfigurationPerFailureMechanismSection ScenarioConfigurationPerSection { get; }
+
+        /// <summary>
+        /// Gets the display name.
+        /// </summary>
+        public string DisplayName
+        {
+            get
+            {
+                return Section.Name;
+            }
+        }
+    }
+}
---
+++ Riskeer/Piping/test/Riskeer.Piping.Forms.Test/PresentationObjects/PipingScenariosViewFailureMechanismSectionViewModelTest.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using NUnit.Framework;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Data.TestUtil;
+using Riskeer.Piping.Data;
+using Riskeer.Piping.Forms.PresentationObjects;
+
+namespace Riskeer.Piping.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class PipingScenariosViewFailureMechanismSectionViewModelTest
+    {
+        [Test]
+        public void Constructor_FailureMechanismSectionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new PipingScenariosViewFailureMechanismSectionViewModel(
+                null, new PipingFailureMechanism(), new PipingScenarioConfigurationPerFailureMechanismSection(
+                    FailureMechanismSectionTestFactory.CreateFailureMechanismSection()));
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("failureMechanismSection", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_FailureMechanismNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+
+            // Call
+            void Call() => new PipingScenariosViewFailureMechanismSectionViewModel(section, null, new PipingScenarioConfigurationPerFailureMechanismSection(section));
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("failureMechanism", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ScenarioConfigurationPerSectionNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+
+            // Call
+            void Call() => new PipingScenariosViewFailureMechanismSectionViewModel(section, new PipingFailureMechanism(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("scenarioConfigurationPerSection", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var scenarioConfiguration = new PipingScenarioConfigurationPerFailureMechanismSection(section);
+
+            // Call
+            var viewModel = new PipingScenariosViewFailureMechanismSectionViewModel(section, new PipingFailureMechanism(), scenarioConfiguration);
+
+            // Assert
+            Assert.AreSame(section, viewModel.Section);
+            Assert.AreSame(scenarioConfiguration, viewModel.ScenarioConfigurationPerSection);
+        }
+
+        [Test]
+        public void DisplayName_Always_ReturnsExpectedName()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var scenarioConfiguration = new PipingScenarioConfigurationPerFailureMechanismSection(section);
+            var viewModel = new PipingScenariosViewFailureMechanismSectionViewModel(section, new PipingFailureMechanism(), scenarioConfiguration);
+
+            // Call
+            string displayName = viewModel.DisplayName;
+
+            // Assert
+            Assert.AreEqual(section.Name, displayName);
+        }
+    }
+}
