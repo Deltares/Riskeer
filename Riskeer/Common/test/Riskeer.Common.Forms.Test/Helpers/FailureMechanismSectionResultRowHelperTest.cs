05e4083fd925194daf36dea89901f7be69318cf9
Robert Tangerman <robert.tangerman@deltares.nl>
2021-12-08 13:20:39 +0100

    Introduced helper to get colors. WTI-2579

---
+++ Riskeer/Common/test/Riskeer.Common.Forms.Test/Helpers/FailureMechanismSectionResultRowHelperTest.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using Core.Common.Controls.DataGrid;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.AssemblyTool.Data;
+using Riskeer.Common.Forms.Helpers;
+using Riskeer.Common.Forms.TestUtil;
+
+namespace Riskeer.Common.Forms.Test.Helpers
+{
+    [TestFixture]
+    public class FailureMechanismSectionResultRowHelperTest
+    {
+        [Test]
+        public void SetAssemblyGroupStyle_DataGridViewColumnStateDefinitionNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => FailureMechanismSectionResultRowHelper.SetAssemblyGroupStyle(
+                null, new Random(39).NextEnumValue<FailureMechanismSectionAssemblyGroup>());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("columnStateDefinition", exception.ParamName);
+        }
+
+        [Test]
+        public void SetAssemblyGroupStyle_InvalidFailureMechanismSectionAssemblyGroup_ThrowsInvalidEnumArgumentException()
+        {
+            // Setup
+            var columnStateDefinition = new DataGridViewColumnStateDefinition
+            {
+                Style = new CellStyle(Color.FromKnownColor(KnownColor.Transparent), Color.FromKnownColor(KnownColor.Transparent))
+            };
+            const FailureMechanismSectionAssemblyGroup assemblyGroup = (FailureMechanismSectionAssemblyGroup) 99;
+
+            // Call
+            void Call() => FailureMechanismSectionResultRowHelper.SetAssemblyGroupStyle(columnStateDefinition, assemblyGroup);
+
+            // Assert
+            var expectedMessage = $"The value of argument 'assemblyGroup' ({assemblyGroup}) is invalid for Enum type '{nameof(FailureMechanismSectionAssemblyGroup)}'.";
+            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<InvalidEnumArgumentException>(Call, expectedMessage);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(AssemblyGroupColorTestHelper), nameof(AssemblyGroupColorTestHelper.FailureMechanismSectionAssemblyGroupColorCases))]
+        public void SetAssemblyGroupStyle_ValidFailureMechanismSectionAssemblyGroup_UpdatesColumnStyle(
+            FailureMechanismSectionAssemblyGroup assemblyCategoryGroup, Color expectedColor)
+        {
+            // Setup
+            var columnStateDefinition = new DataGridViewColumnStateDefinition();
+
+            // Call
+            FailureMechanismSectionResultRowHelper.SetAssemblyGroupStyle(columnStateDefinition, assemblyCategoryGroup);
+
+            // Assert
+            Assert.AreEqual(expectedColor, columnStateDefinition.Style.BackgroundColor);
+            Assert.AreEqual(Color.FromKnownColor(KnownColor.ControlText), columnStateDefinition.Style.TextColor);
+        }
+    }
+}
