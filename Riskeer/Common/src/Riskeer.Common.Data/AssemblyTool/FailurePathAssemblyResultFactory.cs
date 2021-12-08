4fde577b816d24a871b9bbd47543a310c378335e
Dennis Tang <dennis.tang@deltares.nl>
2021-12-08 11:30:51 +0100

    WTI-2695: Introduced factory to assemble an entire failure path

---
+++ Riskeer/Common/src/Riskeer.Common.Data/AssemblyTool/FailurePathAssemblyResultFactory.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Riskeer.AssemblyTool.Data;
+using Riskeer.AssemblyTool.KernelWrapper.Calculators;
+using Riskeer.AssemblyTool.KernelWrapper.Calculators.Assembly;
+using Riskeer.AssemblyTool.KernelWrapper.Kernels;
+using Riskeer.Common.Data.Exceptions;
+
+namespace Riskeer.Common.Data.AssemblyTool
+{
+    /// <summary>
+    /// Factory for creating assembly results of a failure path.
+    /// </summary>
+    public static class FailurePathAssemblyResultFactory
+    {
+        /// <summary>
+        /// Assembles the failure path based on its input arguments.
+        /// </summary>
+        /// <param name="failurePathN">The length effect factor 'N' of the failure path.</param>
+        /// <param name="failureMechanismSectionAssemblyResults">A collection of <see cref="FailureMechanismSectionAssemblyResult"/>.</param>
+        /// <returns>A failure probability of the failure path.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="failureMechanismSectionAssemblyResults"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="AssemblyException">Thrown when the failure path could not be successfully assembled.</exception>
+        public static double AssemblyFailurePath(double failurePathN,
+                                                 IEnumerable<FailureMechanismSectionAssemblyResult> failureMechanismSectionAssemblyResults)
+        {
+            if (failureMechanismSectionAssemblyResults == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanismSectionAssemblyResults));
+            }
+
+            try
+            {
+                IFailurePathAssemblyCalculator calculator =
+                    AssemblyToolCalculatorFactory.Instance.CreateFailurePathAssemblyCalculator(AssemblyToolKernelFactory.Instance);
+
+                return calculator.Assemble(failurePathN, failureMechanismSectionAssemblyResults);
+            }
+            catch (FailurePathAssemblyCalculatorException e)
+            {
+                throw new AssemblyException(e.Message, e);
+            }
+        }
+    }
+}
---
+++ Riskeer/Common/test/Riskeer.Common.Data.Test/AssemblyTool/FailurePathAssemblyResultFactoryTest.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Riskeer.AssemblyTool.Data;
+using Riskeer.AssemblyTool.KernelWrapper.Calculators;
+using Riskeer.AssemblyTool.KernelWrapper.Calculators.Assembly;
+using Riskeer.AssemblyTool.KernelWrapper.TestUtil.Calculators;
+using Riskeer.AssemblyTool.KernelWrapper.TestUtil.Calculators.Assembly;
+using Riskeer.Common.Data.AssemblyTool;
+using Riskeer.Common.Data.Exceptions;
+
+namespace Riskeer.Common.Data.Test.AssemblyTool
+{
+    [TestFixture]
+    public class FailurePathAssemblyResultFactoryTest
+    {
+        [Test]
+        public void AssembleFailurePath_FailureMechanismSectionAssemblyResultsNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => FailurePathAssemblyResultFactory.AssemblyFailurePath(0, null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("failureMechanismSectionAssemblyResults", exception.ParamName);
+        }
+
+        [Test]
+        public void AssembleFailurePath_WithInput_SetsInputOnCalculator()
+        {
+            // Setup
+            var random = new Random(21);
+            double n = random.NextDouble();
+            IEnumerable<FailureMechanismSectionAssemblyResult> sectionResults = Enumerable.Empty<FailureMechanismSectionAssemblyResult>();
+
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                // Call
+                FailurePathAssemblyResultFactory.AssemblyFailurePath(n, sectionResults);
+
+                // Assert
+                var calculatorFactory = (TestAssemblyToolCalculatorFactory) AssemblyToolCalculatorFactory.Instance;
+                FailurePathAssemblyCalculatorStub calculator = calculatorFactory.LastCreatedFailurePathAssemblyCalculator;
+
+                Assert.AreEqual(n, calculator.FailurePathN);
+                Assert.AreSame(sectionResults, calculator.SectionAssemblyResultsInput);
+            }
+        }
+
+        [Test]
+        public void AssembleFailurePath_CalculatorRan_ReturnsOutput()
+        {
+            // Setup
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                // Call
+                double assemblyResult = FailurePathAssemblyResultFactory.AssemblyFailurePath(
+                    0, Enumerable.Empty<FailureMechanismSectionAssemblyResult>());
+
+                // Assert
+                var calculatorFactory = (TestAssemblyToolCalculatorFactory) AssemblyToolCalculatorFactory.Instance;
+                FailurePathAssemblyCalculatorStub calculator = calculatorFactory.LastCreatedFailurePathAssemblyCalculator;
+
+                Assert.AreEqual(calculator.AssemblyResult, assemblyResult);
+            }
+        }
+
+        [Test]
+        public void AssembleFailurePath_CalculatorThrowsException_ThrowsAssemblyException()
+        {
+            // Setup
+            using (new AssemblyToolCalculatorFactoryConfig())
+            {
+                var calculatorFactory = (TestAssemblyToolCalculatorFactory) AssemblyToolCalculatorFactory.Instance;
+                FailurePathAssemblyCalculatorStub calculator = calculatorFactory.LastCreatedFailurePathAssemblyCalculator;
+                calculator.ThrowExceptionOnCalculate = true;
+
+                // Call
+                void Call() => FailurePathAssemblyResultFactory.AssemblyFailurePath(
+                    0, Enumerable.Empty<FailureMechanismSectionAssemblyResult>());
+
+                // Assert
+                var exception = Assert.Throws<AssemblyException>(Call);
+                Exception innerException = exception.InnerException;
+                Assert.IsInstanceOf<FailurePathAssemblyCalculatorException>(innerException);
+                Assert.AreEqual(innerException.Message, exception.Message);
+            }
+        }
+    }
+}
