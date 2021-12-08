9b885a240afd6b172428ddbfb16ad5b3b2924c72
Robert Tangerman <robert.tangerman@deltares.nl>
2021-12-08 07:27:34 +0100

    Added generic failure mechanism section result extensions. WTI-2579

---
+++ Riskeer/Common/src/Riskeer.Common.Data/FailureMechanism/FailureMechanismSectionResultExtensions.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.Base.Geometry;
+using Riskeer.Common.Data.Calculation;
+
+namespace Riskeer.Common.Data.FailureMechanism
+{
+    /// <summary>
+    /// Extension methods for <see cref="FailureMechanismSectionResult"/>.
+    /// </summary>
+    public static class FailureMechanismSectionResultExtensions
+    {
+        /// <summary>
+        /// Gets a collection of the relevant <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="sectionResult">The section result to get the relevant scenarios for.</param>
+        /// <param name="calculationScenarios">The calculation scenarios to get the relevant scenarios from.</param>
+        /// <param name="intersectionFunc">The func to determine whether a scenario is belonging to the given <paramref name="sectionResult"/>.</param>
+        /// <typeparam name="T">The type of the calculation scenarios.</typeparam>
+        /// <returns>A collection of relevant calculation scenarios.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public static IEnumerable<T> GetCalculationScenarios<T>(this FailureMechanismSectionResult sectionResult,
+                                                                IEnumerable<ICalculationScenario> calculationScenarios,
+                                                                Func<T, IEnumerable<Segment2D>, bool> intersectionFunc)
+            where T : ICalculationScenario
+        {
+            if (sectionResult == null)
+            {
+                throw new ArgumentNullException(nameof(sectionResult));
+            }
+
+            if (calculationScenarios == null)
+            {
+                throw new ArgumentNullException(nameof(calculationScenarios));
+            }
+
+            if (intersectionFunc == null)
+            {
+                throw new ArgumentNullException(nameof(intersectionFunc));
+            }
+
+            IEnumerable<Segment2D> lineSegments = Math2D.ConvertPointsToLineSegments(sectionResult.Section.Points);
+
+            return calculationScenarios.OfType<T>().Where(pc => pc.IsRelevant && intersectionFunc(pc, lineSegments));
+        }
+    }
+}
---
+++ Riskeer/Common/test/Riskeer.Common.Data.Test/FailureMechanismSectionResultExtensionsTest.cs
@@ -0,0 +1,153 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.Calculation;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Data.TestUtil;
+
+namespace Riskeer.Common.Data.Test
+{
+    [TestFixture]
+    public class FailureMechanismSectionResultExtensionsTest
+    {
+        [Test]
+        public void GetCalculationScenarios_SectionResultNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => FailureMechanismSectionResultExtensions.GetCalculationScenarios<ICalculationScenario>(
+                null, Enumerable.Empty<ICalculationScenario>(), (scenario, segments) => false);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("sectionResult", exception.ParamName);
+        }
+
+        [Test]
+        public void GetCalculationScenarios_CalculationScenariosNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionResult = new TestFailureMechanismSectionResult(section);
+
+            // Call
+            void Call() => sectionResult.GetCalculationScenarios<ICalculationScenario>(null, (scenario, segments) => false);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("calculationScenarios", exception.ParamName);
+        }
+
+        [Test]
+        public void GetCalculationScenarios_IntersectionFuncNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionResult = new TestFailureMechanismSectionResult(section);
+
+            // Call
+            void Call() => sectionResult.GetCalculationScenarios<ICalculationScenario>(Enumerable.Empty<ICalculationScenario>(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("intersectionFunc", exception.ParamName);
+        }
+
+        [Test]
+        public void GetCalculationScenarios_WithRelevantAndIrrelevantScenariosOfDifferentTypes_ReturnsRelevantCalculationScenarios()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var calculationScenarioStub1 = mocks.Stub<ICalculationScenario>();
+            var calculationScenarioStub2 = mocks.Stub<ICalculationScenario>();
+            mocks.ReplayAll();
+
+            calculationScenarioStub1.IsRelevant = true;
+            calculationScenarioStub2.IsRelevant = false;
+
+            var calculationScenario1 = new TestCalculationScenario();
+            var calculationScenario2 = new TestCalculationScenario
+            {
+                IsRelevant = false
+            };
+
+            ICalculationScenario[] calculationScenarios =
+            {
+                calculationScenarioStub1,
+                calculationScenarioStub2,
+                calculationScenario1,
+                calculationScenario2
+            };
+
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionResult = new TestFailureMechanismSectionResult(section);
+
+            // Call
+            IEnumerable<ICalculationScenario> relevantScenarios = sectionResult.GetCalculationScenarios<TestCalculationScenario>(
+                calculationScenarios, (scenario, segments) => true);
+
+            // Assert
+            Assert.AreEqual(calculationScenario1, relevantScenarios.Single());
+            mocks.VerifyAll();
+        }
+
+        [Test]
+        public void GetCalculationScenarios_WithoutScenarioIntersectingSection_ReturnsNoCalculationScenarios()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var calculationScenarioStub1 = mocks.Stub<ICalculationScenario>();
+            var calculationScenarioStub2 = mocks.Stub<ICalculationScenario>();
+            mocks.ReplayAll();
+
+            calculationScenarioStub1.IsRelevant = true;
+            calculationScenarioStub2.IsRelevant = false;
+
+            var calculationScenario1 = new TestCalculationScenario();
+            var calculationScenario2 = new TestCalculationScenario
+            {
+                IsRelevant = false
+            };
+
+            ICalculationScenario[] calculationScenarios =
+            {
+                calculationScenarioStub1,
+                calculationScenarioStub2,
+                calculationScenario1,
+                calculationScenario2
+            };
+
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionResult = new TestFailureMechanismSectionResult(section);
+
+            // Call
+            IEnumerable<ICalculationScenario> relevantScenarios = sectionResult.GetCalculationScenarios<TestCalculationScenario>(
+                calculationScenarios, (scenario, segments) => false);
+
+            // Assert
+            CollectionAssert.IsEmpty(relevantScenarios);
+            mocks.VerifyAll();
+        }
+    }
+}
