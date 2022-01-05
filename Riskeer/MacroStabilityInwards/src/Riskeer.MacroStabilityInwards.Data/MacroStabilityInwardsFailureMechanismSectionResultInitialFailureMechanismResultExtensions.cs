a5252da293740f57b696b5333dfba7aff6ab8199
Robert Tangerman <robert.tangerman@deltares.nl>
2022-01-05 10:58:01 +0100

    Added new extension methods for calculation. WTI-2584

---
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.Data/MacroStabilityInwardsFailureMechanismSectionResultInitialFailureMechanismResultExtensions.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Data.Helpers;
+
+namespace Riskeer.MacroStabilityInwards.Data
+{
+    /// <summary>
+    /// Extension methods for obtaining initial failure mechanism result probabilities
+    /// from output for an assessment of the macro stability inwards failure mechanism.
+    /// </summary>
+    public static class MacroStabilityInwardsFailureMechanismSectionResultInitialFailureMechanismResultExtensions
+    {
+        /// <summary>
+        /// Gets the value for the initial failure mechanism result of safety per failure mechanism section as a probability.
+        /// </summary>
+        /// <param name="sectionResult">The section result to get the initial failure mechanism result probability for.</param>
+        /// <param name="calculationScenarios">All probabilistic calculation scenarios in the failure mechanism.</param>
+        /// <param name="modelFactor">The model factor used to calculate a reliability from a stability factor.</param>
+        /// <returns>The calculated initial failure mechanism result probability; or <see cref="double.NaN"/> when there
+        /// are no relevant calculations, when not all relevant calculations are performed or when the
+        /// contribution of the relevant calculations don't add up to 1.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public static double GetInitialFailureMechanismResultProbability(this MacroStabilityInwardsFailureMechanismSectionResult sectionResult,
+                                                                         IEnumerable<MacroStabilityInwardsCalculationScenario> calculationScenarios,
+                                                                         double modelFactor)
+        {
+            if (sectionResult == null)
+            {
+                throw new ArgumentNullException(nameof(sectionResult));
+            }
+
+            if (calculationScenarios == null)
+            {
+                throw new ArgumentNullException(nameof(calculationScenarios));
+            }
+
+            MacroStabilityInwardsCalculationScenario[] relevantScenarios = sectionResult.GetRelevantCalculationScenarios<MacroStabilityInwardsCalculationScenario>(
+                                                                                            calculationScenarios,
+                                                                                            (scenario, lineSegments) => scenario.IsSurfaceLineIntersectionWithReferenceLineInSection(lineSegments))
+                                                                                        .ToArray();
+
+            if (!CalculationScenarioHelper.ScenariosAreValid(relevantScenarios))
+            {
+                return double.NaN;
+            }
+
+            double totalInitialFailureMechanismResult = 0;
+            foreach (MacroStabilityInwardsCalculationScenario scenario in relevantScenarios)
+            {
+                DerivedMacroStabilityInwardsOutput derivedOutput = DerivedMacroStabilityInwardsOutputFactory.Create(scenario.Output, modelFactor);
+                totalInitialFailureMechanismResult += derivedOutput.MacroStabilityInwardsProbability * (double) scenario.Contribution;
+            }
+
+            return totalInitialFailureMechanismResult;
+        }
+    }
+}
---
+++ Riskeer/MacroStabilityInwards/test/Riskeer.MacroStabilityInwards.Data.Test/MacroStabilityInwardsFailureMechanismSectionResultInitialFailureMechanismResultExtensionsTest.cs
@@ -0,0 +1,216 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using Core.Common.Base.Data;
+using NUnit.Framework;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Data.TestUtil;
+using Riskeer.MacroStabilityInwards.Data.TestUtil;
+
+namespace Riskeer.MacroStabilityInwards.Data.Test
+{
+    [TestFixture]
+    public class MacroStabilityInwardsFailureMechanismSectionResultInitialFailureMechanismResultExtensionsTest
+    {
+        [Test]
+        public void SemiProbabilisticGetInitialFailureMechanismResultProbability_SectionResultNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => MacroStabilityInwardsFailureMechanismSectionResultInitialFailureMechanismResultExtensions.GetInitialFailureMechanismResultProbability(
+                null, Enumerable.Empty<MacroStabilityInwardsCalculationScenario>(), 0.1);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("sectionResult", exception.ParamName);
+        }
+
+        [Test]
+        public void SemiProbabilisticGetInitialFailureMechanismResultProbability_CalculationScenariosNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new MacroStabilityInwardsFailureMechanismSectionResult(section);
+
+            // Call
+            void Call() => failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(null, 0.1);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("calculationScenarios", exception.ParamName);
+        }
+
+        [Test]
+        public void SemiProbabilisticGetInitialFailureMechanismResultProbability_MultipleScenarios_ReturnsValueBasedOnRelevantScenarios()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new MacroStabilityInwardsFailureMechanismSectionResult(section);
+
+            const double factorOfStability1 = 1.0 / 10.0;
+            const double factorOfStability2 = 1.0 / 20.0;
+
+            MacroStabilityInwardsCalculationScenario macroStabilityInwardsCalculationScenario1 =
+                MacroStabilityInwardsCalculationScenarioTestFactory.CreateMacroStabilityInwardsCalculationScenario(factorOfStability1, section);
+            MacroStabilityInwardsCalculationScenario macroStabilityInwardsCalculationScenario2 =
+                MacroStabilityInwardsCalculationScenarioTestFactory.CreateMacroStabilityInwardsCalculationScenario(factorOfStability2, section);
+            MacroStabilityInwardsCalculationScenario macroStabilityInwardsCalculationScenario3 =
+                MacroStabilityInwardsCalculationScenarioTestFactory.CreateIrrelevantMacroStabilityInwardsCalculationScenario(section);
+
+            macroStabilityInwardsCalculationScenario1.Contribution = (RoundedDouble) 0.2111;
+            macroStabilityInwardsCalculationScenario2.Contribution = (RoundedDouble) 0.7889;
+
+            MacroStabilityInwardsCalculationScenario[] calculations =
+            {
+                macroStabilityInwardsCalculationScenario1,
+                macroStabilityInwardsCalculationScenario2,
+                macroStabilityInwardsCalculationScenario3
+            };
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(calculations, 1.1);
+
+            // Assert
+            Assert.AreEqual(0.99052414832077185, initialFailureMechanismResultProbability, 1e-8);
+        }
+
+        [Test]
+        public void SemiProbabilisticGetInitialFailureMechanismResultProbability_NoScenarios_ReturnsNaN()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new MacroStabilityInwardsFailureMechanismSectionResult(section);
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(
+                Enumerable.Empty<MacroStabilityInwardsCalculationScenario>(), 0.1);
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+
+        [Test]
+        public void SemiProbabilisticGetInitialFailureMechanismResultProbability_NoRelevantScenarios_ReturnsNaN()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new MacroStabilityInwardsFailureMechanismSectionResult(section);
+
+            MacroStabilityInwardsCalculationScenario calculationScenario =
+                MacroStabilityInwardsCalculationScenarioTestFactory.CreateIrrelevantMacroStabilityInwardsCalculationScenario(section);
+
+            MacroStabilityInwardsCalculationScenario[] calculationScenarios =
+            {
+                calculationScenario
+            };
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(calculationScenarios, 0.1);
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+
+        [Test]
+        public void SemiProbabilisticGetInitialFailureMechanismResultProbability_ScenarioNotCalculated_ReturnsNaN()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new MacroStabilityInwardsFailureMechanismSectionResult(section);
+
+            MacroStabilityInwardsCalculationScenario macroStabilityInwardsCalculationScenario =
+                MacroStabilityInwardsCalculationScenarioTestFactory.CreateNotCalculatedMacroStabilityInwardsCalculationScenario(section);
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(new[]
+            {
+                macroStabilityInwardsCalculationScenario
+            }, 0.1);
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+
+        [Test]
+        public void SemiProbabilisticGetInitialFailureMechanismResultProbability_ScenarioWithNaNResults_ReturnsNaN()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new MacroStabilityInwardsFailureMechanismSectionResult(section);
+
+            const double contribution1 = 0.2;
+            const double contribution2 = 0.8;
+
+            MacroStabilityInwardsCalculationScenario macroStabilityInwardsCalculationScenario1 =
+                MacroStabilityInwardsCalculationScenarioTestFactory.CreateMacroStabilityInwardsCalculationScenarioWithNaNOutput(section);
+            MacroStabilityInwardsCalculationScenario macroStabilityInwardsCalculationScenario2 =
+                MacroStabilityInwardsCalculationScenarioTestFactory.CreateMacroStabilityInwardsCalculationScenario(section);
+
+            macroStabilityInwardsCalculationScenario1.IsRelevant = true;
+            macroStabilityInwardsCalculationScenario1.Contribution = (RoundedDouble) contribution1;
+
+            macroStabilityInwardsCalculationScenario2.IsRelevant = true;
+            macroStabilityInwardsCalculationScenario2.Contribution = (RoundedDouble) contribution2;
+
+            MacroStabilityInwardsCalculationScenario[] calculations =
+            {
+                macroStabilityInwardsCalculationScenario1,
+                macroStabilityInwardsCalculationScenario2
+            };
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(calculations, 0.1);
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+
+        [Test]
+        [TestCase(0.0, 0.0)]
+        [TestCase(0.0, 0.5)]
+        [TestCase(0.3, 0.7 + 1e-4)]
+        public void SemiProbabilisticGetInitialFailureMechanismResultProbability_RelevantScenarioContributionsDoNotAddUpTo1_ReturnNaN(double contributionA, double contributionB)
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            MacroStabilityInwardsCalculationScenario macroStabilityInwardsCalculationScenarioA =
+                MacroStabilityInwardsCalculationScenarioTestFactory.CreateMacroStabilityInwardsCalculationScenario(section);
+            MacroStabilityInwardsCalculationScenario macroStabilityInwardsCalculationScenarioB =
+                MacroStabilityInwardsCalculationScenarioTestFactory.CreateMacroStabilityInwardsCalculationScenario(section);
+
+            macroStabilityInwardsCalculationScenarioA.Contribution = (RoundedDouble) contributionA;
+            macroStabilityInwardsCalculationScenarioB.Contribution = (RoundedDouble) contributionB;
+
+            var result = new MacroStabilityInwardsFailureMechanismSectionResult(section);
+
+            // Call
+            double initialFailureMechanismResultProbability = result.GetInitialFailureMechanismResultProbability(new[]
+            {
+                macroStabilityInwardsCalculationScenarioA,
+                macroStabilityInwardsCalculationScenarioB
+            }, 0.1);
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+    }
+}
