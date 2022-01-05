74d2c13d32f629e5055a9f490970ad1e38bc68d8
Robert Tangerman <robert.tangerman@deltares.nl>
2022-01-05 09:59:17 +0100

    Added row for view. WTI-2584

---
+++ Riskeer/MacroStabilityInwards/src/Riskeer.MacroStabilityInwards.Forms/Views/MacroStabilityInwardsFailureMechanismSectionResultRow.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Riskeer.Common.Forms.Views;
+using Riskeer.MacroStabilityInwards.Data;
+
+namespace Riskeer.MacroStabilityInwards.Forms.Views
+{
+    /// <summary>
+    /// This class represents a row of <see cref="MacroStabilityInwardsFailureMechanismSectionResult"/>.
+    /// </summary>
+    public class MacroStabilityInwardsFailureMechanismSectionResultRow : FailureMechanismSectionResultRow<MacroStabilityInwardsFailureMechanismSectionResult>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="MacroStabilityInwardsFailureMechanismSectionResultRow"/>.
+        /// </summary>
+        /// <param name="sectionResult">The <see cref="MacroStabilityInwardsFailureMechanismSectionResult"/> that is 
+        /// the source of this row.</param>
+        /// <exception cref="ArgumentNullException">Throw when any parameter is <c>null</c>.</exception>
+        public MacroStabilityInwardsFailureMechanismSectionResultRow(MacroStabilityInwardsFailureMechanismSectionResult sectionResult) 
+            : base(sectionResult) {}
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
