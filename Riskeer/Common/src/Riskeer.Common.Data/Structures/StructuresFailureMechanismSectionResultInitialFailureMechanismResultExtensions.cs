c0692634367b7ba26603ab98a3a7bcea1cf5ff51
Robert Tangerman <robert.tangerman@deltares.nl>
2022-01-11 07:44:15 +0100

    Added failure mechanism result extensions for structures. WTI-2589

---
+++ Riskeer/Common/src/Riskeer.Common.Data/Structures/StructuresFailureMechanismSectionResultInitialFailureMechanismResultExtensions.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Data.Helpers;
+using Riskeer.Common.Data.Probability;
+
+namespace Riskeer.Common.Data.Structures
+{
+    /// <summary>
+    /// Extension methods for obtaining initial failure mechanism result probabilities
+    /// from output for an assessment of a structures failure mechanism.
+    /// </summary>
+    public static class StructuresFailureMechanismSectionResultInitialFailureMechanismResultExtensions
+    {
+        /// <summary>
+        /// Gets the value for the initial failure mechanism result of safety per failure mechanism section as a probability.
+        /// </summary>
+        /// <param name="sectionResult">The section result to get the initial failure mechanism result probability for.</param>
+        /// <param name="calculationScenarios">All probabilistic calculation scenarios in the failure mechanism.</param>
+        /// <typeparam name="T">The type of the structure which can be assigned to the calculation.</typeparam>
+        /// <returns>The calculated initial failure mechanism result probability; or <see cref="double.NaN"/> when there
+        /// are no relevant calculations, when not all relevant calculations are performed or when the
+        /// contribution of the relevant calculations don't add up to 1.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public static double GetInitialFailureMechanismResultProbability<T>(this FailureMechanismSectionResult sectionResult,
+                                                                            IEnumerable<StructuresCalculationScenario<T>> calculationScenarios)
+            where T : IStructuresCalculationInput<StructureBase>, new()
+        {
+            if (sectionResult == null)
+            {
+                throw new ArgumentNullException(nameof(sectionResult));
+            }
+
+            if (calculationScenarios == null)
+            {
+                throw new ArgumentNullException(nameof(calculationScenarios));
+            }
+
+            StructuresCalculationScenario<T>[] relevantScenarios = sectionResult.GetRelevantCalculationScenarios<StructuresCalculationScenario<T>>(
+                                                                                    calculationScenarios,
+                                                                                    (scenario, lineSegments) => scenario.IsStructureIntersectionWithReferenceLineInSection(lineSegments))
+                                                                                .ToArray();
+
+            if (!CalculationScenarioHelper.ScenariosAreValid(relevantScenarios))
+            {
+                return double.NaN;
+            }
+
+            double totalInitialFailureMechanismResult = 0;
+            foreach (StructuresCalculationScenario<T> scenario in relevantScenarios)
+            {
+                ProbabilityAssessmentOutput derivedOutput = ProbabilityAssessmentOutputFactory.Create(scenario.Output.Reliability);
+                totalInitialFailureMechanismResult += derivedOutput.Probability * (double) scenario.Contribution;
+            }
+
+            return totalInitialFailureMechanismResult;
+        }
+    }
+}
---
+++ Riskeer/Common/test/Riskeer.Common.Data.Test/Structures/StructuresFailureMechanismSectionResultInitialFailureMechanismResultExtensionsTest.cs
@@ -0,0 +1,258 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Linq;
+using Core.Common.Base.Data;
+using NUnit.Framework;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Data.Structures;
+using Riskeer.Common.Data.TestUtil;
+
+namespace Riskeer.Common.Data.Test.Structures
+{
+    [TestFixture]
+    public class StructuresFailureMechanismSectionResultInitialFailureMechanismResultExtensionsTest
+    {
+        [Test]
+        public void GetInitialFailureMechanismResultProbability_SectionResultNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => StructuresFailureMechanismSectionResultInitialFailureMechanismResultExtensions.GetInitialFailureMechanismResultProbability(
+                null, Enumerable.Empty<TestStructuresCalculationScenario>());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("sectionResult", exception.ParamName);
+        }
+
+        [Test]
+        public void GetInitialFailureMechanismResultProbability_CalculationScenariosNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new FailureMechanismSectionResult(section);
+
+            // Call
+            void Call() => failureMechanismSectionResult.GetInitialFailureMechanismResultProbability<TestStructuresInput>(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("calculationScenarios", exception.ParamName);
+        }
+
+        [Test]
+        public void GetInitialFailureMechanismResultProbability_MultipleScenarios_ReturnsValueBasedOnRelevantScenarios()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new FailureMechanismSectionResult(section);
+
+            var calculationScenario1 = new TestStructuresCalculationScenario
+            {
+                InputParameters =
+                {
+                    Structure = new TestStructure(section.StartPoint)
+                },
+                Contribution = (RoundedDouble) 0.2111,
+                Output = new TestStructuresOutput(1.1)
+            };
+            var calculationScenario2 = new TestStructuresCalculationScenario
+            {
+                InputParameters =
+                {
+                    Structure = new TestStructure(section.StartPoint)
+                },
+                Contribution = (RoundedDouble) 0.7889,
+                Output = new TestStructuresOutput(2.2)
+            };
+            var calculationScenario3 = new TestStructuresCalculationScenario
+            {
+                InputParameters =
+                {
+                    Structure = new TestStructure(section.StartPoint)
+                },
+                IsRelevant = false
+            };
+
+            TestStructuresCalculationScenario[] calculations =
+            {
+                calculationScenario1,
+                calculationScenario2,
+                calculationScenario3
+            };
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(calculations);
+
+            // Assert
+            Assert.AreEqual(0.039607535209180436, initialFailureMechanismResultProbability, 1e-8);
+        }
+
+        [Test]
+        public void GetInitialFailureMechanismResultProbability_NoScenarios_ReturnsNaN()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new FailureMechanismSectionResult(section);
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(
+                Enumerable.Empty<TestStructuresCalculationScenario>());
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+
+        [Test]
+        public void GetInitialFailureMechanismResultProbability_NoRelevantScenarios_ReturnsNaN()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new FailureMechanismSectionResult(section);
+
+            var calculationScenario = new TestStructuresCalculationScenario
+            {
+                InputParameters =
+                {
+                    Structure = new TestStructure(section.StartPoint)
+                },
+                IsRelevant = false,
+                Output = new TestStructuresOutput()
+            };
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(new[]
+            {
+                calculationScenario
+            });
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+
+        [Test]
+        public void GetInitialFailureMechanismResultProbability_ScenarioNotCalculated_ReturnsNaN()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new FailureMechanismSectionResult(section);
+
+            var calculationScenario = new TestStructuresCalculationScenario
+            {
+                InputParameters =
+                {
+                    Structure = new TestStructure(section.StartPoint)
+                }
+            };
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(new[]
+            {
+                calculationScenario
+            });
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+
+        [Test]
+        public void GetInitialFailureMechanismResultProbability_ScenarioWithNaNResults_ReturnsNaN()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var failureMechanismSectionResult = new FailureMechanismSectionResult(section);
+
+            var calculationScenario1 = new TestStructuresCalculationScenario
+            {
+                InputParameters =
+                {
+                    Structure = new TestStructure(section.StartPoint)
+                },
+                IsRelevant = true,
+                Contribution = (RoundedDouble) 0.2,
+                Output = new TestStructuresOutput(double.NaN)
+            };
+            var calculationScenario2 = new TestStructuresCalculationScenario
+            {
+                InputParameters =
+                {
+                    Structure = new TestStructure(section.StartPoint)
+                },
+                IsRelevant = true,
+                Contribution = (RoundedDouble) 0.8,
+                Output = new TestStructuresOutput(0.1)
+            };
+
+            TestStructuresCalculationScenario[] calculations =
+            {
+                calculationScenario1,
+                calculationScenario2
+            };
+
+            // Call
+            double initialFailureMechanismResultProbability = failureMechanismSectionResult.GetInitialFailureMechanismResultProbability(calculations);
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+
+        [Test]
+        [TestCase(0.0, 0.0)]
+        [TestCase(0.0, 0.5)]
+        [TestCase(0.3, 0.7 + 1e-4)]
+        public void GetInitialFailureMechanismResultProbability_RelevantScenarioContributionsDoNotAddUpTo1_ReturnNaN(double contributionA, double contributionB)
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var result = new FailureMechanismSectionResult(section);
+
+            var calculationScenarioA = new TestStructuresCalculationScenario
+            {
+                InputParameters =
+                {
+                    Structure = new TestStructure(section.StartPoint)
+                },
+                Contribution = (RoundedDouble) contributionA,
+                Output = new TestStructuresOutput(0.1)
+            };
+            var calculationScenarioB = new TestStructuresCalculationScenario
+            {
+                InputParameters =
+                {
+                    Structure = new TestStructure(section.StartPoint)
+                },
+                Contribution = (RoundedDouble) contributionB,
+                Output = new TestStructuresOutput(0.2)
+            };
+
+            // Call
+            double initialFailureMechanismResultProbability = result.GetInitialFailureMechanismResultProbability(new[]
+            {
+                calculationScenarioA,
+                calculationScenarioB
+            });
+
+            // Assert
+            Assert.IsNaN(initialFailureMechanismResultProbability);
+        }
+    }
+}
