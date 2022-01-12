588f659a65b9150bec96ff4fd3a2d5e8b357409b
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2022-01-12 14:25:16 +0100

    Review related changes. Paired with tang_ds. WTI-2596

--- Riskeer/ClosingStructures/src/Riskeer.ClosingStructures.Plugin/FileImporters/ClosingStructuresFailureMechanismSectionResultUpdateStrategy.cs
+++ Riskeer/ClosingStructures/src/Riskeer.ClosingStructures.Plugin/FileImporters/ClosingStructuresFailureMechanismSectionResultUpdateStrategy.cs
@@ -29,7 +29,7 @@ namespace Riskeer.ClosingStructures.Plugin.FileImporters
     /// <summary>
     /// An update strategy that can be used to update either a <see cref="ClosingStructuresFailureMechanismSectionResultOld"/>
     /// or an <see cref="AdoptableFailureMechanismSectionResult"/> instance with data from an old instance;
-    /// /// </summary>
+    /// </summary>
     public class ClosingStructuresFailureMechanismSectionResultUpdateStrategy
         : IFailureMechanismSectionResultUpdateStrategy<ClosingStructuresFailureMechanismSectionResultOld, AdoptableFailureMechanismSectionResult>
     {
