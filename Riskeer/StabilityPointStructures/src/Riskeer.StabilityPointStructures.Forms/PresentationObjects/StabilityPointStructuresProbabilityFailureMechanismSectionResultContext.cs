0554906685bfbb559fc5d30b87f8d5ac0e161411
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2022-01-17 09:41:27 +0100

    Added new context. WTI-2608

---
+++ Riskeer/StabilityPointStructures/src/Riskeer.StabilityPointStructures.Forms/PresentationObjects/StabilityPointStructuresProbabilityFailureMechanismSectionResultContext.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.Base;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Forms.PresentationObjects;
+using Riskeer.StabilityPointStructures.Data;
+
+namespace Riskeer.StabilityPointStructures.Forms.PresentationObjects
+{
+    /// <summary>
+    /// This class is a presentation object for a collection of <see cref="AdoptableFailureMechanismSectionResult"/>
+    /// for the <see cref="StabilityPointStructuresFailureMechanism"/>.
+    /// </summary>
+    public class StabilityPointStructuresProbabilityFailureMechanismSectionResultContext : ProbabilityFailureMechanismSectionResultContext<AdoptableFailureMechanismSectionResult>
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="StabilityPointStructuresProbabilityFailureMechanismSectionResultContext"/>.
+        /// </summary>
+        /// <param name="wrappedSectionResults">The <see cref="IObservableEnumerable{T}"/>
+        /// of <see cref="AdoptableFailureMechanismSectionResult"/> to wrap.</param>
+        /// <param name="failureMechanism">The <see cref="StabilityPointStructuresFailureMechanism"/>
+        /// the <paramref name="wrappedSectionResults"/> belongs to.</param>
+        /// <param name="assessmentSection">The assessment section the section results belongs to.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public StabilityPointStructuresProbabilityFailureMechanismSectionResultContext(IObservableEnumerable<AdoptableFailureMechanismSectionResult> wrappedSectionResults,
+                                                                                       StabilityPointStructuresFailureMechanism failureMechanism, IAssessmentSection assessmentSection)
+            : base(wrappedSectionResults, failureMechanism, assessmentSection) {}
+    }
+}
