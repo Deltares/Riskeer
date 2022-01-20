0893ebdc89f4fa2cdf45ad2dfbbc1e2445de6ff8
Dennis Tang <dennis.tang@deltares.nl>
2022-01-20 15:39:42 +0100

    WTI-2636: Wired the SectionResults to the Clear() and Add() logic

--- Riskeer/StabilityStoneCover/test/Riskeer.StabilityStoneCover.Data.Test/StabilityStoneCoverFailureMechanismTest.cs
+++ Riskeer/StabilityStoneCover/test/Riskeer.StabilityStoneCover.Data.Test/StabilityStoneCoverFailureMechanismTest.cs
@@ -74,6 +74,9 @@ namespace Riskeer.StabilityStoneCover.Data.Test
             Assert.AreEqual(1, failureMechanism.Sections.Count());
             Assert.AreEqual(1, failureMechanism.SectionResultsOld.Count());
             Assert.AreSame(section, failureMechanism.SectionResultsOld.First().Section);
+
+            Assert.AreEqual(1, failureMechanism.SectionResults.Count());
+            Assert.AreSame(section, failureMechanism.SectionResults.First().Section);
         }
 
         [Test]
@@ -96,12 +99,14 @@ namespace Riskeer.StabilityStoneCover.Data.Test
 
             // Precondition
             Assert.AreEqual(2, failureMechanism.SectionResultsOld.Count());
+            Assert.AreEqual(2, failureMechanism.SectionResults.Count());
 
             // Call
             failureMechanism.ClearAllSections();
 
             // Assert
             CollectionAssert.IsEmpty(failureMechanism.SectionResultsOld);
+            CollectionAssert.IsEmpty(failureMechanism.SectionResults);
         }
 
         [Test]
