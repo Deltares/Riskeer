4b21a1267883d333d905785f2810d1a92fd2dcfe
Dennis Tang <dennis.tang@deltares.nl>
2022-02-10 09:18:35 +0100

    WTI-2541: Added assembly factory for Piping.

---
+++ Riskeer/Piping/src/Riskeer.Piping.Data/PipingFailureMechanismAssemblyFactory.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Riskeer.AssemblyTool.Data;
+using Riskeer.Common.Data.AssemblyTool;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Data.Exceptions;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Data.Probability;
+
+namespace Riskeer.Piping.Data
+{
+    /// <summary>
+    /// Factory for assembling assembly results for a piping failure mechanism.
+    /// </summary>
+    public static class PipingFailureMechanismAssemblyFactory
+    {
+        /// <summary>
+        /// Assembles the section based on the input arguments.
+        /// </summary>
+        /// <param name="sectionResult"></param>
+        /// <param name="failureMechanism"></param>
+        /// <param name="assessmentSection"></param>
+        /// <returns>A <see cref="FailureMechanismSectionAssemblyResult"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any argument is <c>null</c>.</exception>
+        /// <exception cref="AssemblyException">Thrown when the section could not be assembled.</exception>
+        public static FailureMechanismSectionAssemblyResult AssembleSection(
+            AdoptableWithProfileProbabilityFailureMechanismSectionResult sectionResult,
+            PipingFailureMechanism failureMechanism,
+            IAssessmentSection assessmentSection)
+        {
+            if (sectionResult == null)
+            {
+                throw new ArgumentNullException(nameof(sectionResult));
+            }
+
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
+            PipingScenarioConfigurationPerFailureMechanismSection scenarioConfigurationForSection =
+                failureMechanism.GetScenarioConfigurationForSection(sectionResult);
+
+            IFailureMechanismSectionResultCalculateProbabilityStrategy calculateProbabilityStrategy =
+                PipingFailureMechanismSectionResultCalculateProbabilityStrategyFactory.CreateCalculateStrategy(
+                    failureMechanism, sectionResult, scenarioConfigurationForSection, assessmentSection);
+
+            return FailureMechanismSectionResultAssemblyFactory.AssembleSection(
+                sectionResult, assessmentSection, calculateProbabilityStrategy,
+                failureMechanism.GeneralInput.ApplyLengthEffectInSection,
+                failureMechanism.PipingProbabilityAssessmentInput.GetN(sectionResult.Section.Length));
+        }
+    }
+}
