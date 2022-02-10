92ce27ee1854f01e9cc648b1ae4553a7ea0f3845
Dennis Tang <dennis.tang@deltares.nl>
2022-02-10 13:50:56 +0100

    WTI-2541: Added assembly factory for StandAlone FailureMechanisms.

---
+++ Riskeer/Integration/src/Riskeer.Integration.Data/StandAlone/AssemblyFactories/StandAloneFailureMechanismAssemblyFactory.cs
@@ -0,0 +1,17 @@
+﻿using Riskeer.AssemblyTool.Data;
+using Riskeer.Common.Data.AssemblyTool;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Data.FailureMechanism;
+
+namespace Riskeer.Integration.Data.StandAlone.AssemblyFactories
+{
+    public static class StandAloneFailureMechanismAssemblyFactory
+    {
+        public static FailureMechanismSectionAssemblyResult AssembleSection(NonAdoptableWithProfileProbabilityFailureMechanismSectionResult sectionResult,
+                                                                            IHasGeneralInput failureMechanism,
+                                                                            IAssessmentSection assessmentSection)
+        {
+            return FailureMechanismSectionResultAssemblyFactory.AssembleSection(sectionResult, assessmentSection, failureMechanism.GeneralInput.ApplyLengthEffectInSection);
+        }
+    }
+}
