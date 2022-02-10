34e5804e022f2ea502abfe4bf1eb8d14063fb602
Dennis Tang <dennis.tang@deltares.nl>
2022-02-10 13:47:42 +0100

    WTI-2541: Added assembly factory for Revetment items.

---
+++ Riskeer/WaveImpactAsphaltCover/src/Riskeer.WaveImpactAsphaltCover.Data/WaveImpactAsphaltCoverFailureMechanismAssemblyFactory.cs
@@ -0,0 +1,17 @@
+﻿using Riskeer.AssemblyTool.Data;
+using Riskeer.Common.Data.AssemblyTool;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Data.FailureMechanism;
+
+namespace Riskeer.WaveImpactAsphaltCover.Data
+{
+    public static class WaveImpactAsphaltCoverFailureMechanismAssemblyFactory
+    {
+        public static FailureMechanismSectionAssemblyResult AssembleSection(NonAdoptableWithProfileProbabilityFailureMechanismSectionResult sectionResult,
+                                                                            WaveImpactAsphaltCoverFailureMechanism failureMechanism,
+                                                                            IAssessmentSection assessmentSection)
+        {
+            return FailureMechanismSectionResultAssemblyFactory.AssembleSection(sectionResult, assessmentSection, failureMechanism.GeneralWaveImpactAsphaltCoverInput.ApplyLengthEffectInSection);
+        }
+    }
+}
