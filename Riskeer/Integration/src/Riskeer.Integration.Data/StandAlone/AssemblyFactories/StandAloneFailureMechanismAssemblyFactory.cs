252c1d1ac3a5a32b2f78bc0c577f3cf62d60b278
Dennis Tang <dennis.tang@deltares.nl>
2022-02-14 10:31:06 +0100

    WTI-2541: Added tests for the StandAlone Assembly Factory

--- Riskeer/Integration/src/Riskeer.Integration.Data/StandAlone/AssemblyFactories/StandAloneFailureMechanismAssemblyFactory.cs
+++ Riskeer/Integration/src/Riskeer.Integration.Data/StandAlone/AssemblyFactories/StandAloneFailureMechanismAssemblyFactory.cs
@@ -1,16 +1,44 @@
-﻿using Riskeer.AssemblyTool.Data;
+﻿using System;
+using Riskeer.AssemblyTool.Data;
 using Riskeer.Common.Data.AssemblyTool;
 using Riskeer.Common.Data.AssessmentSection;
 using Riskeer.Common.Data.FailureMechanism;
 
 namespace Riskeer.Integration.Data.StandAlone.AssemblyFactories
 {
+    /// <summary>
+    /// Factory for assembling assembly results for a stand alone failure mechanism.
+    /// </summary>
     public static class StandAloneFailureMechanismAssemblyFactory
     {
+        /// <summary>
+        /// Assembles the section based on the input arguments.
+        /// </summary>
+        /// <param name="sectionResult">The section result to assemble.</param>
+        /// <param name="failureMechanism">The failure mechanism the section result belongs to.</param>
+        /// <param name="assessmentSection">The <see cref="IAssessmentSection"/> the section belongs to.</param>
+        /// <returns>A <see cref="FailureMechanismSectionAssemblyResult"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any argument is <c>null</c>.</exception>
+        /// <exception cref="AssemblyException">Thrown when the section could not be assembled.</exception>
         public static FailureMechanismSectionAssemblyResult AssembleSection(NonAdoptableWithProfileProbabilityFailureMechanismSectionResult sectionResult,
                                                                             IHasGeneralInput failureMechanism,
                                                                             IAssessmentSection assessmentSection)
         {
+            if (sectionResult == null)
+            {
+                throw new ArgumentNullException(nameof(sectionResult));
+            }
+
+            if (failureMechanism == null)
+            {
+                throw new ArgumentNullException(nameof(failureMechanism));
+            }
+
+            if (assessmentSection == null)
+            {
+                throw new ArgumentNullException(nameof(assessmentSection));
+            }
+
             return FailureMechanismSectionAssemblyResultFactory.AssembleSection(sectionResult, assessmentSection, failureMechanism.GeneralInput.ApplyLengthEffectInSection);
         }
     }
