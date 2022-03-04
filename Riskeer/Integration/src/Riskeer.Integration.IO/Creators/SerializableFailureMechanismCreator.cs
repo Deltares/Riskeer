8058778f527e6a5084b7a0980cd4fd73f39ee02f
Robert Tangerman <robert.tangerman@deltares.nl>
2022-03-04 13:02:29 +0100

    Adapted failure mechanism assembly. WTI-2681

--- Riskeer/Integration/src/Riskeer.Integration.IO/Creators/SerializableFailureMechanismCreator.cs
+++ Riskeer/Integration/src/Riskeer.Integration.IO/Creators/SerializableFailureMechanismCreator.cs
@@ -69,42 +69,5 @@ namespace Riskeer.Integration.IO.Creators
                                                     SerializableFailureMechanismTypeCreator.Create(failureMechanism.Code),
                                                     SerializableFailureMechanismResultCreator.Create(failureMechanism.FailureMechanismAssembly));
         }
-
-        /// <summary>
-        /// Creates an instance of <see cref="SerializableFailureMechanism"/> based on its input parameters.
-        /// </summary>
-        /// <param name="idGenerator">The generator to generate an id for the <see cref="SerializableFailureMechanism"/>.</param>
-        /// <param name="serializableTotalAssembly">The <see cref="SerializableTotalAssemblyResult"/>
-        /// the <see cref="SerializableFailureMechanism"/> belongs to.</param>
-        /// <param name="failureMechanism">The <see cref="ExportableFailureMechanism{TFailureMechanismAssemblyResult}"/>
-        /// with a probability assembly result to create a <see cref="SerializableFailureMechanism"/> for.</param>
-        /// <returns>A <see cref="SerializableFailureMechanism"/>.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
-        /// <exception cref="AssemblyCreatorException">Thrown when <paramref name="failureMechanism"/> is invalid to
-        /// create a serializable counterpart for.</exception>
-        public static SerializableFailureMechanism Create(IdentifierGenerator idGenerator,
-                                                          SerializableTotalAssemblyResult serializableTotalAssembly,
-                                                          ExportableFailureMechanism<ExportableFailureMechanismAssemblyResultWithProbability> failureMechanism)
-        {
-            if (idGenerator == null)
-            {
-                throw new ArgumentNullException(nameof(idGenerator));
-            }
-
-            if (serializableTotalAssembly == null)
-            {
-                throw new ArgumentNullException(nameof(serializableTotalAssembly));
-            }
-
-            if (failureMechanism == null)
-            {
-                throw new ArgumentNullException(nameof(failureMechanism));
-            }
-
-            return new SerializableFailureMechanism(idGenerator.GetNewId(Resources.SerializableFailureMechanismCreator_IdPrefix),
-                                                    serializableTotalAssembly,
-                                                    SerializableFailureMechanismTypeCreator.Create(failureMechanism.Code),
-                                                    SerializableFailureMechanismResultCreator.Create(failureMechanism.FailureMechanismAssembly));
-        }
     }
 }
--- Riskeer/Integration/test/Riskeer.Integration.IO.Test/Creators/SerializableFailureMechanismCreatorTest.cs
+++ Riskeer/Integration/test/Riskeer.Integration.IO.Test/Creators/SerializableFailureMechanismCreatorTest.cs
@@ -36,41 +36,35 @@ namespace Riskeer.Integration.IO.Test.Creators
     public class SerializableFailureMechanismCreatorTest
     {
         [Test]
-        public void CreateWithFailureMechanismAssemblyResult_IdGeneratorNull_ThrowsArgumentNullException()
+        public void Create_IdGeneratorNull_ThrowsArgumentNullException()
         {
             // Call
-            TestDelegate call = () => SerializableFailureMechanismCreator.Create(null,
-                                                                                 CreateSerializableTotalAssembly("id"),
-                                                                                 CreateExportableFailureMechanismWithoutProbability());
+            void Call() => SerializableFailureMechanismCreator.Create(null, CreateSerializableTotalAssembly("id"), CreateExportableFailureMechanismWithoutProbability());
 
             // Assert
-            var exception = Assert.Throws<ArgumentNullException>(call);
+            var exception = Assert.Throws<ArgumentNullException>(Call);
             Assert.AreEqual("idGenerator", exception.ParamName);
         }
 
         [Test]
-        public void CreateWithFailureMechanismAssemblyResult_SerializableTotalAssemblyNull_ThrowsArgumentNullException()
+        public void Create_SerializableTotalAssemblyNull_ThrowsArgumentNullException()
         {
             // Call
-            TestDelegate call = () => SerializableFailureMechanismCreator.Create(new IdentifierGenerator(),
-                                                                                 null,
-                                                                                 CreateExportableFailureMechanismWithoutProbability());
+            void Call() => SerializableFailureMechanismCreator.Create(new IdentifierGenerator(), null, CreateExportableFailureMechanismWithoutProbability());
 
             // Assert
-            var exception = Assert.Throws<ArgumentNullException>(call);
+            var exception = Assert.Throws<ArgumentNullException>(Call);
             Assert.AreEqual("serializableTotalAssembly", exception.ParamName);
         }
 
         [Test]
-        public void CreateWithFailureMechanismAssemblyResult_FailureMechanismNull_ThrowsArgumentNullException()
+        public void Create_FailureMechanismNull_ThrowsArgumentNullException()
         {
             // Call
-            TestDelegate call = () => SerializableFailureMechanismCreator.Create(new IdentifierGenerator(),
-                                                                                 CreateSerializableTotalAssembly("id"),
-                                                                                 (ExportableFailureMechanism<ExportableFailureMechanismAssemblyResult>) null);
+            void Call() => SerializableFailureMechanismCreator.Create(new IdentifierGenerator(), CreateSerializableTotalAssembly("id"), null);
 
             // Assert
-            var exception = Assert.Throws<ArgumentNullException>(call);
+            var exception = Assert.Throws<ArgumentNullException>(Call);
             Assert.AreEqual("failureMechanism", exception.ParamName);
         }
 
@@ -98,83 +92,11 @@ namespace Riskeer.Integration.IO.Test.Creators
                                                                                                                   serializableFailureMechanism.FailureMechanismAssemblyResult);
         }
 
-        [Test]
-        public void CreateWithFailureMechanismAssemblyResultWithProbability_IdGeneratorNull_ThrowsArgumentNullException()
-        {
-            // Call
-            TestDelegate call = () => SerializableFailureMechanismCreator.Create(null,
-                                                                                 CreateSerializableTotalAssembly("id"),
-                                                                                 CreateExportableFailureMechanismWithProbability());
-
-            // Assert
-            var exception = Assert.Throws<ArgumentNullException>(call);
-            Assert.AreEqual("idGenerator", exception.ParamName);
-        }
-
-        [Test]
-        public void CreateWithFailureMechanismAssemblyResultWithProbability_SerializableTotalAssemblyNull_ThrowsArgumentNullException()
-        {
-            // Call
-            TestDelegate call = () => SerializableFailureMechanismCreator.Create(new IdentifierGenerator(),
-                                                                                 null,
-                                                                                 CreateExportableFailureMechanismWithProbability());
-
-            // Assert
-            var exception = Assert.Throws<ArgumentNullException>(call);
-            Assert.AreEqual("serializableTotalAssembly", exception.ParamName);
-        }
-
-        [Test]
-        public void CreateWithFailureMechanismAssemblyResultWithProbability_FailureMechanismNull_ThrowsArgumentNullException()
-        {
-            // Call
-            TestDelegate call = () => SerializableFailureMechanismCreator.Create(new IdentifierGenerator(),
-                                                                                 CreateSerializableTotalAssembly("id"),
-                                                                                 (ExportableFailureMechanism<ExportableFailureMechanismAssemblyResultWithProbability>) null);
-
-            // Assert
-            var exception = Assert.Throws<ArgumentNullException>(call);
-            Assert.AreEqual("failureMechanism", exception.ParamName);
-        }
-
-        [Test]
-        public void Create_WithFailureMechanismAssemblyResultWithProbability_ReturnsSerializableFailureMechanism()
-        {
-            // Setup
-            var idGenerator = new IdentifierGenerator();
-            ExportableFailureMechanism<ExportableFailureMechanismAssemblyResultWithProbability> failureMechanism = CreateExportableFailureMechanismWithProbability();
-
-            const string totalAssemblyId = "totalAssemblyId";
-            SerializableTotalAssemblyResult serializableTotalAssembly = CreateSerializableTotalAssembly(totalAssemblyId);
-
-            // Call
-            SerializableFailureMechanism serializableFailureMechanism = SerializableFailureMechanismCreator.Create(idGenerator,
-                                                                                                                   serializableTotalAssembly,
-                                                                                                                   failureMechanism);
-
-            // Assert
-            Assert.AreEqual("Ts.0", serializableFailureMechanism.Id);
-            Assert.AreEqual(serializableTotalAssembly.Id, serializableFailureMechanism.TotalAssemblyResultId);
-            Assert.AreEqual(SerializableFailureMechanismTypeCreator.Create(failureMechanism.Code), serializableFailureMechanism.FailureMechanismType);
-
-            SerializableFailureMechanismAssemblyResultTestHelper.AssertSerializableFailureMechanismAssemblyResult(failureMechanism.FailureMechanismAssembly,
-                                                                                                                  serializableFailureMechanism.FailureMechanismAssemblyResult);
-        }
-
         private static ExportableFailureMechanism<ExportableFailureMechanismAssemblyResult> CreateExportableFailureMechanismWithoutProbability()
         {
             var random = new Random(21);
             return new ExportableFailureMechanism<ExportableFailureMechanismAssemblyResult>(
                 ExportableFailureMechanismAssemblyResultTestFactory.CreateResultWithoutProbability(),
-                Enumerable.Empty<ExportableAggregatedFailureMechanismSectionAssemblyResultBase>(),
-                random.NextEnumValue<ExportableFailureMechanismType>());
-        }
-
-        private static ExportableFailureMechanism<ExportableFailureMechanismAssemblyResultWithProbability> CreateExportableFailureMechanismWithProbability()
-        {
-            var random = new Random(21);
-            return new ExportableFailureMechanism<ExportableFailureMechanismAssemblyResultWithProbability>(
-                ExportableFailureMechanismAssemblyResultTestFactory.CreateResultWithProbability(),
                 Enumerable.Empty<ExportableAggregatedFailureMechanismSectionAssemblyResultBase>(),
                 random.NextEnumValue<ExportableFailureMechanismType>());
         }
