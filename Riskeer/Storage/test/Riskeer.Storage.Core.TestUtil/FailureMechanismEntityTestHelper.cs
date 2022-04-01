b24490440b3e2bbcdf65ca3caad31a2a35f7452d
Dennis Tang <dennis.tang@deltares.nl>
2022-04-01 10:22:21 +0200

    WTI-2737: Incorporated comments review.
    Paired with ~tangerma

--- Riskeer/Storage/test/Riskeer.Storage.Core.TestUtil/FailureMechanismEntityTestHelper.cs
+++ Riskeer/Storage/test/Riskeer.Storage.Core.TestUtil/FailureMechanismEntityTestHelper.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
 using NUnit.Framework;
 using Riskeer.Common.Data.FailureMechanism;
 using Riskeer.Storage.Core.DbContext;
@@ -11,7 +32,7 @@ namespace Riskeer.Storage.Core.TestUtil
     public static class FailureMechanismEntityTestHelper
     {
         /// <summary>
-        /// Asserts the <see cref="IFailureMechanism{T}"/> against an <see cref="IFailureMechanismEntity"/>.
+        /// Asserts the <see cref="IFailureMechanism"/> against an <see cref="IFailureMechanismEntity"/>.
         /// </summary>
         /// <param name="entity">The <see cref="IFailureMechanismEntity"/> to assert with.</param>
         /// <param name="failureMechanism">The <see cref="IFailureMechanism{T}"/> to assert.</param>
