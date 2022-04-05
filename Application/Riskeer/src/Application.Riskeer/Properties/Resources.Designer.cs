74fec193a187ff8f53b71acd7e20c4c6475936fb
Elmar Kleijn <elmar.kleijn@deltares.nl>
2022-04-05 09:51:35 +0200

    Improved about page. WTI-2708

--- Application/Riskeer/src/Application.Riskeer/Properties/Resources.Designer.cs
+++ Application/Riskeer/src/Application.Riskeer/Properties/Resources.Designer.cs
@@ -40,7 +40,7 @@ namespace Application.Riskeer.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -96,6 +96,16 @@ namespace Application.Riskeer.Properties {
         internal static string App_Unhandled_exception {
             get {
                 return ResourceManager.GetString("App_Unhandled_exception", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap MadeBy {
+            get {
+                object obj = ResourceManager.GetObject("MadeBy", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
--- Core/Common/src/Core.Common.Controls/Properties/Resources.Designer.cs
+++ Core/Common/src/Core.Common.Controls/Properties/Resources.Designer.cs
@@ -40,7 +40,7 @@ namespace Core.Common.Controls.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -87,6 +87,16 @@ namespace Core.Common.Controls.Properties {
         public static string DisplayName_None {
             get {
                 return ResourceManager.GetString("DisplayName_None", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap MadeBy {
+            get {
+                object obj = ResourceManager.GetObject("MadeBy", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
