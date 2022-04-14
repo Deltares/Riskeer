a5945d5638c112a9211cc959df70800013f02372
Robert Tangerman <robert.tangerman@deltares.nl>
2022-04-14 11:29:37 +0200

    Created enum defining the assembly methods. WTI-2769

---
+++ Riskeer/AssemblyTool/src/Riskeer.AssemblyTool.Data/AssemblyMethod.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (C) Stichting Deltares 2021. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Riskeer.AssemblyTool.Data
+{
+    /// <summary>
+    /// Enum defining the assembly methods.
+    /// </summary>
+    public enum AssemblyMethod
+    {
+        /// <summary>
+        /// Represents the assembly method BOI-0A-1.
+        /// </summary>
+        BOI0A1 = 1,
+        
+        /// <summary>
+        /// Represents the assembly method BOI-0A-12
+        /// </summary>
+        BOI0A2 = 2,
+        
+        /// <summary>
+        /// Represents the assembly method BOI-0B-1.
+        /// </summary>
+        BOI0B1 = 3,
+        
+        /// <summary>
+        /// Represents the assembly method BOI-0C-1.
+        /// </summary>
+        BOI0C1 = 4,
+        
+        /// <summary>
+        /// Represents the assembly method BOI-0C-2.
+        /// </summary>
+        BOI0C2 = 5
+    }
+}
