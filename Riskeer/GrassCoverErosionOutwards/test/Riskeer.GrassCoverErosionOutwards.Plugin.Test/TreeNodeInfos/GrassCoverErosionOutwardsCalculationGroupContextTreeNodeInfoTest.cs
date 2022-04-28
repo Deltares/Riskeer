bf76e318677fcc507b885d6c3eb245ad3685ae6a
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2022-04-28 13:22:19 +0200

    Review related changes. WTI-2423

--- Riskeer/GrassCoverErosionOutwards/test/Riskeer.GrassCoverErosionOutwards.Plugin.Test/TreeNodeInfos/GrassCoverErosionOutwardsCalculationGroupContextTreeNodeInfoTest.cs
+++ Riskeer/GrassCoverErosionOutwards/test/Riskeer.GrassCoverErosionOutwards.Plugin.Test/TreeNodeInfos/GrassCoverErosionOutwardsCalculationGroupContextTreeNodeInfoTest.cs
@@ -696,8 +696,8 @@ namespace Riskeer.GrassCoverErosionOutwards.Plugin.Test.TreeNodeInfos
                     ToolStripItem calculateItem = contextMenu.Items[contextMenuCalculateAllIndexNestedGroup];
                     Assert.IsFalse(validateItem.Enabled);
                     Assert.IsFalse(calculateItem.Enabled);
-                    Assert.AreEqual(RiskeerCommonFormsResources.Calculate_All_No_calculations_to_run, calculateItem.ToolTipText);
-                    Assert.AreEqual(RiskeerCommonFormsResources.FailureMechanism_CreateValidateAllItem_No_calculations_to_validate, validateItem.ToolTipText);
+                    Assert.AreEqual("Er zijn geen berekeningen om uit te voeren.", calculateItem.ToolTipText);
+                    Assert.AreEqual("Er zijn geen berekeningen om te valideren.", validateItem.ToolTipText);
                 }
             }
         }
