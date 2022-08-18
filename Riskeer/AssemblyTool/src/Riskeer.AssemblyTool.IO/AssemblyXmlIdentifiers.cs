a1ef2f4e99fcee434b24ad7203be6a3f449c3ee6
Dennis Tang <dennis.tang@deltares.nl>
2022-08-18 08:27:05 +0200

    WTI-2765: Added initial AssemblyXmlIdentifiers

---
+++ Riskeer/AssemblyTool/src/Riskeer.AssemblyTool.IO/AssemblyXmlIdentifiers.cs
@@ -0,0 +1,166 @@
+﻿// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+namespace Riskeer.AssemblyTool.IO
+{
+    /// <summary>
+    /// Class containing definitions for XML identifiers and namespaces for
+    /// the serializable assembly model.
+    /// </summary>
+    public static class AssemblyXmlIdentifiers
+    {
+        /// <summary>
+        /// Identifier for an assembly collection element.
+        /// </summary>
+        public const string AssemblyCollection = "Assembleren-collectie";
+
+        /// <summary>
+        /// Identifier for an assessment section element.
+        /// </summary>
+        public const string AssessmentSection = "Waterkeringstelsel";
+
+        /// <summary>
+        /// Identifier for an assessment section type element.
+        /// </summary>
+        public const string AssessmentSectionType = "typeWaterkeringstelsel";
+
+        /// <summary>
+        /// Identifier for an assessment process element.
+        /// </summary>
+        public const string AssessmentProcess = "Beoordelingsproces";
+
+        /// <summary>
+        /// Identifier for a start year element.
+        /// </summary>
+        public const string StartYear = "beginJaarBeoordelingsronde";
+
+        /// <summary>
+        /// Identifier for an end year element.
+        /// </summary>
+        public const string EndYear = "eindJaarBeoordelingsronde";
+
+        #region Link identifiers
+
+        /// <summary>
+        /// Identifier for a link attribute.
+        /// </summary>
+        public const string Link = "href";
+
+        /// <summary>
+        /// Identifier for an assesses element.
+        /// </summary>
+        public const string Assesses = "beoordeelt";
+
+        #endregion
+
+        #region Namespaces
+
+        /// <summary>
+        /// The XML namespace identifier for XLink objects.
+        /// </summary>
+        public const string XLinkNamespaceIdentifier = "xlink";
+
+        /// <summary>
+        /// The XML namespace for XLink objects.
+        /// </summary>
+        public const string XLinkNamespace = "http://www.w3.org/1999/xlink";
+
+        /// <summary>
+        /// The XML namespace identifier for GML objects.
+        /// </summary>
+        public const string GmlNamespaceIdentifier = "gml";
+
+        /// <summary>
+        /// The XML namespace for GML objects.
+        /// </summary>
+        public const string GmlNamespace = "http://www.opengis.net/gml/3.2";
+
+        /// <summary>
+        /// The XML namespace identifier for IMWAP objects.
+        /// </summary>
+        public const string ImwapNamespaceIdentifier = "imwap";
+
+        /// <summary>
+        /// The XML namespace for IMWAP objects.
+        /// </summary>
+        public const string ImwapNamespace = "http://www.aquo.nl/BOI2023/imwaproxies/v20210113";
+
+        /// <summary>
+        /// The XML namespace identifier for UBOI objects.
+        /// </summary>
+        public const string UboiNamespaceIdentifier = "uboi";
+
+        /// <summary>
+        /// The XML namespace for UBOI objects.
+        /// </summary>
+        public const string UboiNamespace = "http://www.aquo.nl/BOI2023/uitwisselmodel/v20210113";
+
+        #endregion
+
+        #region Generic identifiers
+
+        /// <summary>
+        /// Identifier for an ID attribute.
+        /// </summary>
+        public const string Id = "id";
+
+        /// <summary>
+        /// Identifier for a name attribute.
+        /// </summary>
+        public const string Name = "naam";
+
+        /// <summary>
+        /// Identifier for a feature member element.
+        /// </summary>
+        public const string FeatureMember = "featureMember";
+
+        #endregion
+
+        #region Geometry
+
+        /// <summary>
+        /// Identifier for a line string element.
+        /// </summary>
+        public const string LineString = "LineString";
+
+        /// <summary>
+        /// Identifier for a coordinate system type attribute.
+        /// </summary>
+        public const string CoordinateSystem = "srsName";
+
+        /// <summary>
+        /// Identifier for a GML geometry element.
+        /// </summary>
+        public const string Geometry = "posList";
+
+        /// <summary>
+        /// Identifier for a 2D geometry element.
+        /// </summary>
+        public const string Geometry2D = "geometrie2D";
+
+        /// <summary>
+        /// Identifier for a length attribute.
+        /// </summary>
+        public const string Length = "lengte";
+
+        #endregion
+    }
+}
---
+++ Riskeer/AssemblyTool/test/Riskeer.AssemblyTool.IO.Test/AssemblyXmlIdentifiersTest.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using NUnit.Framework;
+
+namespace Riskeer.AssemblyTool.IO.Test
+{
+    [TestFixture]
+    public class AssemblyXmlIdentifiersTest
+    {
+        [Test]
+        public void AssemblyXmlIdentifiers_ExpectedValues()
+        {
+            Assert.AreEqual("Assembleren-collectie", AssemblyXmlIdentifiers.AssemblyCollection);
+            Assert.AreEqual("Waterkeringstelsel", AssemblyXmlIdentifiers.AssessmentSection);
+            Assert.AreEqual("typeWaterkeringstelsel", AssemblyXmlIdentifiers.AssessmentSectionType);
+            Assert.AreEqual("Beoordelingsproces", AssemblyXmlIdentifiers.AssessmentProcess);
+            Assert.AreEqual("beginJaarBeoordelingsronde", AssemblyXmlIdentifiers.StartYear);
+            Assert.AreEqual("eindJaarBeoordelingsronde", AssemblyXmlIdentifiers.EndYear);
+
+            Assert.AreEqual("href", AssemblyXmlIdentifiers.Link);
+            Assert.AreEqual("beoordeelt", AssemblyXmlIdentifiers.Assesses);
+
+            Assert.AreEqual("xlink", AssemblyXmlIdentifiers.XLinkNamespaceIdentifier);
+            Assert.AreEqual("http://www.w3.org/1999/xlink", AssemblyXmlIdentifiers.XLinkNamespace);
+            Assert.AreEqual("gml", AssemblyXmlIdentifiers.GmlNamespaceIdentifier);
+            Assert.AreEqual("http://www.opengis.net/gml/3.2", AssemblyXmlIdentifiers.GmlNamespace);
+            Assert.AreEqual("imwap", AssemblyXmlIdentifiers.ImwapNamespaceIdentifier);
+            Assert.AreEqual("http://www.aquo.nl/BOI2023/imwaproxies/v20210113", AssemblyXmlIdentifiers.ImwapNamespace);
+            Assert.AreEqual("uboi", AssemblyXmlIdentifiers.UboiNamespaceIdentifier);
+            Assert.AreEqual("http://www.aquo.nl/BOI2023/uitwisselmodel/v20210113", AssemblyXmlIdentifiers.UboiNamespace);
+
+            Assert.AreEqual("id", AssemblyXmlIdentifiers.Id);
+            Assert.AreEqual("naam", AssemblyXmlIdentifiers.Name);
+            Assert.AreEqual("featureMember", AssemblyXmlIdentifiers.FeatureMember);
+
+            Assert.AreEqual("lengte", AssemblyXmlIdentifiers.Length);
+            Assert.AreEqual("LineString", AssemblyXmlIdentifiers.LineString);
+            Assert.AreEqual("srsName", AssemblyXmlIdentifiers.CoordinateSystem);
+            Assert.AreEqual("posList", AssemblyXmlIdentifiers.Geometry);
+            Assert.AreEqual("geometrie2D", AssemblyXmlIdentifiers.Geometry2D);
+        }
+    }
+}
