1330d35a3db147d690b6fa660d3b1a1c990f2063
Dennis Tang <dennis.tang@deltares.nl>
2022-08-19 13:48:42 +0200

    WTI-2765: Added ExportableFailureMechanismSectionCollection

---
+++ Riskeer/AssemblyTool/test/Riskeer.AssemblyTool.IO.Test/Model/ExportableFailureMechanismSectionCollectionTest.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.AssemblyTool.IO.Model;
+using Riskeer.AssemblyTool.IO.TestUtil;
+
+namespace Riskeer.AssemblyTool.IO.Test.Model
+{
+    [TestFixture]
+    public class ExportableFailureMechanismSectionCollectionTest
+    {
+        [Test]
+        [TestCaseSource(typeof(InvalidIdTestHelper), nameof(InvalidIdTestHelper.InvalidIdCases))]
+        public void Constructor_InvalidId_ThrowsArgumentException(string invalidId)
+        {
+            // Call
+            void Call() => new ExportableFailureMechanismSectionCollection(invalidId, Enumerable.Empty<ExportableFailureMechanismSection>());
+
+            // Assert
+            const string expectedMessage = "'id' must have a value and consist only of alphanumerical characters, '-', '_' or '.'.";
+            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<ArgumentException>(Call, expectedMessage);
+        }
+
+        [Test]
+        public void Constructor_SectionsNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new ExportableFailureMechanismSectionCollection("id", null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("sections", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            const string id = "id";
+            IEnumerable<ExportableFailureMechanismSection> sections = Enumerable.Empty<ExportableFailureMechanismSection>();
+
+            // Call
+            var collection = new ExportableFailureMechanismSectionCollection(id, sections);
+
+            // Assert
+            Assert.AreEqual(id, collection.Id);
+            Assert.AreSame(sections, collection.Sections);
+        }
+    }
+}
