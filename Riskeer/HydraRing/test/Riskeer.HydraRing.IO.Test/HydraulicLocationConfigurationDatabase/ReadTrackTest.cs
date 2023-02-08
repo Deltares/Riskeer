1214fcf8a5e7fab9bdb5621c81929b4c3960c85c
Elmar Kleijn <elmar.kleijn@deltares.nl>
2023-02-08 08:21:01 +0100

    Introducing logic for reading all tracks.

---
+++ Riskeer/HydraRing/test/Riskeer.HydraRing.IO.Test/HydraulicLocationConfigurationDatabase/ReadTrackTest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.HydraRing.IO.HydraulicLocationConfigurationDatabase;
+
+namespace Riskeer.HydraRing.IO.Test.HydraulicLocationConfigurationDatabase
+{
+    [TestFixture]
+    public class ReadTrackTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            long trackId = random.Next();
+            var hrdFileName = random.Next().ToString();
+            bool usePreprocessorClosure = random.NextBoolean();
+
+            // Call
+            var readTrack = new ReadTrack(trackId, hrdFileName, usePreprocessorClosure);
+
+            // Assert
+            Assert.AreEqual(trackId, readTrack.TrackId);
+            Assert.AreEqual(hrdFileName, readTrack.HrdFileName);
+            Assert.AreEqual(trackId, readTrack.UsePreprocessorClosure);
+        }
+    }
+}
