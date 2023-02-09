4cf48eada01387bf7694a2cbd624f854800d7f7b
Elmar Kleijn <elmar.kleijn@deltares.nl>
2023-02-09 17:12:05 +0100

    Adding HrdFile.

--- Riskeer/Common/test/Riskeer.Common.Data.Test/Hydraulics/HrdFileTest.cs
+++ Riskeer/Common/test/Riskeer.Common.Data.Test/Hydraulics/HrdFileTest.cs
@@ -19,6 +19,8 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
+using System;
+using Core.Common.TestUtil;
 using NUnit.Framework;
 using Riskeer.Common.Data.Hydraulics;
 
@@ -36,6 +38,130 @@ namespace Riskeer.Common.Data.Test.Hydraulics
             // Assert
             Assert.IsNull(hrdFile.FilePath);
             Assert.IsNull(hrdFile.Version);
+            Assert.IsFalse(hrdFile.UsePreprocessorClosure);
+            Assert.IsFalse(hrdFile.CanUsePreprocessor);
+            Assert.IsFalse(hrdFile.UsePreprocessor);
+            Assert.IsNull(hrdFile.PreprocessorDirectory);
+        }
+
+        [Test]
+        public void UsePreprocessor_SetValueWithCanUsePreprocessorTrue_ExpectedValueSet()
+        {
+            // Setup
+            bool usePreprocessor = new Random(11).NextBoolean();
+            var hrdFile = new HrdFile
+            {
+                CanUsePreprocessor = true
+            };
+
+            // Call
+            hrdFile.UsePreprocessor = usePreprocessor;
+
+            // Assert
+            Assert.AreEqual(usePreprocessor, hrdFile.UsePreprocessor);
+        }
+
+        [Test]
+        public void UsePreprocessor_SetValueWithCanUsePreprocessorFalse_ThrowsInvalidOperationException()
+        {
+            // Setup
+            bool usePreprocessor = new Random(11).NextBoolean();
+            var hrdFile = new HrdFile();
+
+            // Call
+            void Call() => hrdFile.UsePreprocessor = usePreprocessor;
+
+            // Assert
+            string message = Assert.Throws<InvalidOperationException>(Call).Message;
+            Assert.AreEqual($"{nameof(HrdFile.CanUsePreprocessor)} is false.", message);
+        }
+
+        [Test]
+        public void PreprocessorDirectory_SetValidValueWithCanUsePreprocessorTrue_ExpectedValueSet()
+        {
+            // Setup
+            const string preprocessorDirectory = "OtherPreprocessor";
+            var hrdFile = new HrdFile
+            {
+                CanUsePreprocessor = true
+            };
+
+            // Call
+            hrdFile.PreprocessorDirectory = preprocessorDirectory;
+
+            // Assert
+            Assert.AreEqual(preprocessorDirectory, hrdFile.PreprocessorDirectory);
+        }
+
+        [Test]
+        public void PreprocessorDirectory_SetValidValueWithCanUsePreprocessorFalse_ThrowsInvalidOperationException()
+        {
+            // Setup
+            var hrdFile = new HrdFile();
+
+            // Call
+            void Call() => hrdFile.PreprocessorDirectory = "Preprocessor";
+
+            // Assert
+            string message = Assert.Throws<InvalidOperationException>(Call).Message;
+            Assert.AreEqual($"{nameof(HrdFile.CanUsePreprocessor)} is false.", message);
+        }
+
+        [Test]
+        public void GivenHrdFileWithPreprocessorValues_WhenSettingCanUsePreprocessorFalse_ThenPreprocessorValuesReset()
+        {
+            // Given
+            var hrdFile = new HrdFile
+            {
+                CanUsePreprocessor = true,
+                UsePreprocessor = true,
+                PreprocessorDirectory = "PreprocessorDirectory"
+            };
+
+            // When
+            hrdFile.CanUsePreprocessor = false;
+
+            // Then
+            Assert.IsFalse(hrdFile.CanUsePreprocessor);
+            Assert.IsFalse(hrdFile.UsePreprocessor);
+            Assert.IsNull(hrdFile.PreprocessorDirectory);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void CanUsePreprocessor_Always_ExpectedValuesSet(bool canUsePreprocessor)
+        {
+            // Setup
+            var hrdFile = new HrdFile();
+
+            // Call
+            hrdFile.CanUsePreprocessor = canUsePreprocessor;
+
+            // Assert
+            Assert.AreEqual(canUsePreprocessor, hrdFile.CanUsePreprocessor);
+            Assert.IsFalse(hrdFile.UsePreprocessor);
+            Assert.IsNull(hrdFile.PreprocessorDirectory);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void PreprocessorDirectory_SetInvalidValueWithCanUsePreprocessorTrue_ThrowsArgumentException(string preprocessorDirectory)
+        {
+            // Setup
+            var hrdFile = new HrdFile
+            {
+                CanUsePreprocessor = true
+            };
+
+            // Call
+            void Call() => hrdFile.PreprocessorDirectory = preprocessorDirectory;
+
+            // Assert
+            string message = Assert.Throws<ArgumentException>(Call).Message;
+            Assert.AreEqual("De bestandsmap waar de preprocessor bestanden opslaat moet een waarde hebben.", message);
         }
     }
 }
