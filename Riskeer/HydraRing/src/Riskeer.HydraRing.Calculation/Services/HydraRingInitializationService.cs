fa495a1e7d5257d59512c01e08251537d5bc43a8
Elmar Kleijn <elmar.kleijn@deltares.nl>
2023-02-21 13:44:37 +0100

    Removing outdated preprocessor logic.

--- Riskeer/HydraRing/src/Riskeer.HydraRing.Calculation/Services/HydraRingInitializationService.cs
+++ Riskeer/HydraRing/src/Riskeer.HydraRing.Calculation/Services/HydraRingInitializationService.cs
@@ -55,7 +55,6 @@ namespace Riskeer.HydraRing.Calculation.Services
         private readonly string hlcdFilePath;
         private readonly string hydraRingDirectory;
         private readonly string configurationDatabaseFilePath;
-        private readonly string preprocessorDirectory;
         private readonly bool usePreprocessorClosure;
 
         /// <summary>
@@ -86,7 +85,6 @@ namespace Riskeer.HydraRing.Calculation.Services
             hydraRingDirectory = Path.Combine(applicationFolder, "Standalone", "Deltares", $"{hydraRingBinariesSubDirectory}-{HydraRingFileConstants.HydraRingVersionNumber}");
 
             configurationDatabaseFilePath = Path.Combine(hydraRingDirectory, HydraRingFileConstants.ConfigurationDatabaseFileName);
-            preprocessorDirectory = settings.PreprocessorDirectory;
             usePreprocessorClosure = settings.UsePreprocessorClosure;
         }
 
@@ -151,11 +149,6 @@ namespace Riskeer.HydraRing.Calculation.Services
                                                            "configdbfilename        = " + configurationDatabaseFilePath,
                                                            "hydraulicdbfilename     = " + hlcdFilePath,
                                                            "designpointOutput       = sqlite");
-
-            if (preprocessorDirectory != string.Empty)
-            {
-                initializationFileContent += Environment.NewLine + "preprocessordbdirectory = " + preprocessorDirectory;
-            }
 
             if (usePreprocessorClosure)
             {
