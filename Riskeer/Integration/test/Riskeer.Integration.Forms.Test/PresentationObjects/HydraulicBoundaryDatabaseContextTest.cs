6630ee068d7ed26b2bc5767e8184c9cd3f45c9c1
Elmar Kleijn <elmar.kleijn@deltares.nl>
2023-02-23 09:31:53 +0100

    Introducing HydraulicBoundaryDatabase.

---
+++ Riskeer/Integration/test/Riskeer.Integration.Forms.Test/PresentationObjects/HydraulicBoundaryDatabaseContextTest.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Controls.PresentationObjects;
+using NUnit.Framework;
+using Riskeer.Common.Data.Hydraulics;
+using Riskeer.Integration.Forms.PresentationObjects;
+
+namespace Riskeer.Integration.Forms.Test.PresentationObjects
+{
+    [TestFixture]
+    public class HydraulicBoundaryDatabaseContextTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var hydraulicBoundaryDatabase = new HydraulicBoundaryDatabase();
+
+            // Call
+            var context = new HydraulicBoundaryDatabaseContext(hydraulicBoundaryDatabase);
+
+            // Assert
+            Assert.IsInstanceOf<ObservableWrappedObjectContextBase<HydraulicBoundaryDatabase>>(context);
+            Assert.AreSame(hydraulicBoundaryDatabase, context.WrappedData);
+        }
+    }
+}
