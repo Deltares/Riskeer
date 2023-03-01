7507b615d015c5731bc63b5eb4e205f97f1e7517
Elmar Kleijn <elmar.kleijn@deltares.nl>
2023-03-01 13:16:46 +0100

    Adapting HydraulicLocationConfigurationSettings.

--- Riskeer/Common/src/Riskeer.Common.Data/Hydraulics/HydraulicLocationConfigurationSettings.cs
+++ Riskeer/Common/src/Riskeer.Common.Data/Hydraulics/HydraulicLocationConfigurationSettings.cs
@@ -19,118 +19,66 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
-using System;
-
 namespace Riskeer.Common.Data.Hydraulics
 {
     /// <summary>
-    /// Class which holds information about the hydraulic location configuration settings.
+    /// Class which holds information about a hydraulic location configuration database.
     /// </summary>
     public class HydraulicLocationConfigurationSettings
     {
         /// <summary>
-        /// Gets the file path.
+        /// Gets or sets the file path.
         /// </summary>
-        public string FilePath { get; private set; }
+        public string FilePath { get; set; }
 
         /// <summary>
-        /// Gets the scenario name.
+        /// Gets or sets the scenario name.
         /// </summary>
-        public string ScenarioName { get; private set; }
+        public string ScenarioName { get; set; }
 
         /// <summary>
-        /// Gets the year.
+        /// Gets or sets the year.
         /// </summary>
-        public int Year { get; private set; }
+        public int Year { get; set; }
 
         /// <summary>
-        /// Gets the scope.
+        /// Gets or sets the scope.
         /// </summary>
-        public string Scope { get; private set; }
+        public string Scope { get; set; }
 
         /// <summary>
-        /// Gets the sea level.
+        /// Gets or sets the sea level.
         /// </summary>
-        public string SeaLevel { get; private set; }
+        public string SeaLevel { get; set; }
 
         /// <summary>
-        /// Gets the river discharge.
+        /// Gets or sets the river discharge.
         /// </summary>
-        public string RiverDischarge { get; private set; }
+        public string RiverDischarge { get; set; }
 
         /// <summary>
-        /// Gets the lake level.
+        /// Gets or sets the lake level.
         /// </summary>
-        public string LakeLevel { get; private set; }
+        public string LakeLevel { get; set; }
 
         /// <summary>
-        /// Gets the wind direction.
+        /// Gets or sets the wind direction.
         /// </summary>
-        public string WindDirection { get; private set; }
+        public string WindDirection { get; set; }
 
         /// <summary>
-        /// Gets the wind speed.
+        /// Gets or sets the wind speed.
         /// </summary>
-        public string WindSpeed { get; private set; }
+        public string WindSpeed { get; set; }
 
         /// <summary>
-        /// Gets the comment.
+        /// Gets or sets the comment.
         /// </summary>
-        public string Comment { get; private set; }
+        public string Comment { get; set; }
 
         /// <summary>
-        /// Gets the indicator whether to use the preprocessor closure.
+        /// Gets or sets the indicator whether to use the preprocessor closure.
         /// </summary>
-        public bool UsePreprocessorClosure { get; private set; }
-
-        /// <summary>
-        /// Sets values to the <see cref="HydraulicLocationConfigurationSettings"/>.
-        /// </summary>
-        /// <param name="filePath">The file path.</param>
-        /// <param name="scenarioName">The name of the scenario.</param>
-        /// <param name="year">The year.</param>
-        /// <param name="scope">The scope.</param>
-        /// <param name="usePreprocessorClosure">The use preprocessor closure indicator.</param>
-        /// <param name="seaLevel">The sea level.</param>
-        /// <param name="riverDischarge">The river discharge.</param>
-        /// <param name="lakeLevel">The lake level.</param>
-        /// <param name="windDirection">The wind direction.</param>
-        /// <param name="windSpeed">The wind speed.</param>
-        /// <param name="comment">The comment.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="scenarioName"/>
-        /// or <paramref name="scope"/> is <c>null</c>.</exception>
-        /// <exception cref="ArgumentException">Thrown when <paramref name="filePath"/>
-        /// is <c>null</c>, empty or consists of whitespace.</exception>
-        public void SetValues(string filePath, string scenarioName, int year, string scope,
-                              bool usePreprocessorClosure, string seaLevel, string riverDischarge,
-                              string lakeLevel, string windDirection, string windSpeed, string comment)
-        {
-            if (string.IsNullOrWhiteSpace(filePath))
-            {
-                throw new ArgumentException($@"'{nameof(filePath)}' is null, empty or consists of whitespace.");
-            }
-
-            if (scenarioName == null)
-            {
-                throw new ArgumentNullException(nameof(scenarioName));
-            }
-
-            if (scope == null)
-            {
-                throw new ArgumentNullException(nameof(scope));
-            }
-
-            FilePath = filePath;
-            ScenarioName = scenarioName;
-            Year = year;
-            Scope = scope;
-            UsePreprocessorClosure = usePreprocessorClosure;
-            SeaLevel = seaLevel;
-            RiverDischarge = riverDischarge;
-            LakeLevel = lakeLevel;
-            WindDirection = windDirection;
-            WindSpeed = windSpeed;
-            Comment = comment;
-        }
+        public bool UsePreprocessorClosure { get; set; }
     }
 }
