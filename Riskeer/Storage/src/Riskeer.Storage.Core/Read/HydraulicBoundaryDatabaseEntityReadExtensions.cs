1f44ff31cfcc30a7f79806d6eac552d1fa4a2eb2
Robert Tangerman <robert.tangerman@deltares.nl>
2023-03-09 14:22:38 +0100

    Adapted read extensions.

---
+++ Riskeer/Storage/src/Riskeer.Storage.Core/Read/HydraulicBoundaryDatabaseEntityReadExtensions.cs
@@ -0,0 +1,58 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Riskeer.Common.Data.Hydraulics;
+using Riskeer.Storage.Core.DbContext;
+
+namespace Riskeer.Storage.Core.Read
+{
+    /// <summary>
+    /// This class defines extension methods for read operations for a <see cref="HydraulicBoundaryDatabaseEntity"/>.
+    /// </summary>
+    internal static class HydraulicBoundaryDatabaseEntityReadExtensions
+    {
+        /// <summary>
+        /// Reads the <see cref="HydraulicBoundaryDatabaseEntity"/> and uses the information to update a
+        /// <see cref="HydraulicBoundaryDatabase"/> instance.
+        /// </summary>
+        /// <param name="entity">The <see cref="HydraulicBoundaryDatabaseEntity"/> to update the
+        /// <see cref="HydraulicBoundaryDatabase"/>.</param>
+        /// <param name="hydraulicBoundaryDatabase">The <see cref="HydraulicBoundaryDatabase"/> to update.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is <c>null</c>.</exception>
+        public static void Read(this HydraulicBoundaryDatabaseEntity entity, HydraulicBoundaryDatabase hydraulicBoundaryDatabase)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            if (hydraulicBoundaryDatabase == null)
+            {
+                throw new ArgumentNullException(nameof(hydraulicBoundaryDatabase));
+            }
+
+            hydraulicBoundaryDatabase.FilePath = entity.FilePath;
+            hydraulicBoundaryDatabase.Version = entity.Version;
+            hydraulicBoundaryDatabase.UsePreprocessorClosure = Convert.ToBoolean(entity.UsePreprocessorClosure);
+        }
+    }
+}
---
+++ Riskeer/Storage/test/Riskeer.Storage.Core.Test/Read/HydraulicBoundaryDatabaseEntityReadExtensionsTest.cs
@@ -0,0 +1,83 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using Riskeer.Common.Data.Hydraulics;
+using Riskeer.Storage.Core.DbContext;
+using Riskeer.Storage.Core.Read;
+
+namespace Riskeer.Storage.Core.Test.Read
+{
+    [TestFixture]
+    public class HydraulicBoundaryDatabaseEntityReadExtensionsTest
+    {
+        [Test]
+        public void Read_EntityNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => ((HydraulicBoundaryDatabaseEntity) null).Read(new HydraulicBoundaryDatabase());
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("entity", exception.ParamName);
+        }
+
+        [Test]
+        public void Read_HydraulicBoundaryDatabaseNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var entity = new HydraulicBoundaryDatabaseEntity();
+
+            // Call
+            void Call() => entity.Read(null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("hydraulicBoundaryData", exception.ParamName);
+        }
+
+        [Test]
+        public void Read_WithValidEntity_UpdatesHydraulicBoundaryData()
+        {
+            // Setup
+            var random = new Random(21);
+            bool usePreprocessorClosure = random.NextBoolean();
+            var entity = new HydraulicBoundaryDatabaseEntity
+            {
+                FilePath = "hrdFilePath",
+                Version = "1.0",
+                UsePreprocessorClosure = Convert.ToByte(usePreprocessorClosure)
+            };
+
+            var hydraulicBoundaryDatabase = new HydraulicBoundaryDatabase();
+
+            // Call
+            entity.Read(hydraulicBoundaryDatabase);
+
+            // Assert
+            Assert.AreEqual(entity.FilePath, hydraulicBoundaryDatabase.FilePath);
+            Assert.AreEqual(entity.Version, hydraulicBoundaryDatabase.Version);
+            Assert.AreEqual(usePreprocessorClosure, hydraulicBoundaryDatabase.UsePreprocessorClosure);
+        }
+    }
+}
