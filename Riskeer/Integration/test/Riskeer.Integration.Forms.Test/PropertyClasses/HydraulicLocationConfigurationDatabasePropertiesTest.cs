4df18494d98eb03078205c2fc03e46e509d422ec
Elmar Kleijn <elmar.kleijn@deltares.nl>
2023-03-27 16:28:33 +0200

    Adapted visualization.

--- Riskeer/Integration/test/Riskeer.Integration.Forms.Test/PropertyClasses/HydraulicLocationConfigurationDatabasePropertiesTest.cs
+++ Riskeer/Integration/test/Riskeer.Integration.Forms.Test/PropertyClasses/HydraulicLocationConfigurationDatabasePropertiesTest.cs
@@ -80,7 +80,7 @@ namespace Riskeer.Integration.Forms.Test.PropertyClasses
             // Assert
             Assert.IsEmpty(properties.FilePath);
             Assert.IsEmpty(properties.ScenarioName);
-            Assert.AreEqual(0, properties.Year);
+            Assert.AreEqual("0", properties.Year);
             Assert.IsEmpty(properties.Scope);
             Assert.IsEmpty(properties.SeaLevel);
             Assert.IsEmpty(properties.RiverDischarge);
