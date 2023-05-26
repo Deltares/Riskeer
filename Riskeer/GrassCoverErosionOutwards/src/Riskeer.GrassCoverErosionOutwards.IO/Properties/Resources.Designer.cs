2c8a3abae63494fce39939b239344dc2a41132c9
Robert Tangerman <robert.tangerman@deltares.nl>
2023-05-26 11:16:17 +0200

    Updated license headers. WTI-2820

--- Riskeer/GrassCoverErosionOutwards/src/Riskeer.GrassCoverErosionOutwards.IO/Properties/Resources.Designer.cs
+++ Riskeer/GrassCoverErosionOutwards/src/Riskeer.GrassCoverErosionOutwards.IO/Properties/Resources.Designer.cs
@@ -1,4 +1,4 @@
-// Copyright (C) Stichting Deltares 2022. All rights reserved.
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
 //
 // This file is part of Riskeer.
 //
@@ -40,7 +40,7 @@ namespace Riskeer.GrassCoverErosionOutwards.IO.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -83,7 +83,7 @@ namespace Riskeer.GrassCoverErosionOutwards.IO.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;&lt;!--
-        ///Copyright (C) Stichting Deltares 2022. All rights reserved.
+        ///Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
         ///
         ///This file is part of Riskeer.
         ///
@@ -93,8 +93,7 @@ namespace Riskeer.GrassCoverErosionOutwards.IO.Properties {
         ///(at your option) any later version.
         ///
         ///This program is distributed in the hope that it will be useful,
-        ///but WITHOUT ANY WARRANTY; without even the implied warranty of
-        ///ME [rest of string was truncated]&quot;;.
+        ///but WITHOUT ANY WARRANTY; without eve [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string GEBUConfiguratieSchema {
             get {
@@ -104,7 +103,7 @@ namespace Riskeer.GrassCoverErosionOutwards.IO.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;&lt;!--
-        ///Copyright (C) Stichting Deltares 2022. All rights reserved.
+        ///Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
         ///
         ///This file is part of Riskeer.
         ///
@@ -114,8 +113,7 @@ namespace Riskeer.GrassCoverErosionOutwards.IO.Properties {
         ///(at your option) any later version.
         ///
         ///This program is distributed in the hope that it will be useful,
-        ///but WITHOUT ANY WARRANTY; without even the implied warranty of
-        ///ME [rest of string was truncated]&quot;;.
+        ///but WITHOUT ANY WARRANTY; without eve [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string GEBUConfiguratieSchema_0 {
             get {
@@ -125,7 +123,7 @@ namespace Riskeer.GrassCoverErosionOutwards.IO.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;&lt;!--
-        ///Copyright (C) Stichting Deltares 2022. All rights reserved.
+        ///Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
         ///
         ///This file is part of Riskeer.
         ///
@@ -135,8 +133,7 @@ namespace Riskeer.GrassCoverErosionOutwards.IO.Properties {
         ///(at your option) any later version.
         ///
         ///This program is distributed in the hope that it will be useful,
-        ///but WITHOUT ANY WARRANTY; without even the implied warranty of
-        ///ME [rest of string was truncated]&quot;;.
+        ///but WITHOUT ANY WARRANTY; without eve [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string GEBUConfiguratieSchema_1 {
             get {
