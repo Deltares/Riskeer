937cad85a137a7c77b84dec605916b0b45415169
Robert Tangerman <robert.tangerman@deltares.nl>
2023-05-26 11:18:15 +0200

    Updated license headers. WTI-2820

--- Riskeer/HeightStructures/src/Riskeer.HeightStructures.IO/Properties/Resources.Designer.cs
+++ Riskeer/HeightStructures/src/Riskeer.HeightStructures.IO/Properties/Resources.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) Stichting Deltares 2022. All rights reserved.
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
 //
 // This file is part of Riskeer.
 //
@@ -40,7 +40,7 @@ namespace Riskeer.HeightStructures.IO.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -105,7 +105,7 @@ namespace Riskeer.HeightStructures.IO.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;&lt;!--
-        ///Copyright (C) Stichting Deltares 2022. All rights reserved.
+        ///Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
         ///
         ///This file is part of Riskeer.
         ///
@@ -115,8 +115,7 @@ namespace Riskeer.HeightStructures.IO.Properties {
         ///(at your option) any later version.
         ///
         ///This program is distributed in the hope that it will be useful,
-        ///but WITHOUT ANY WARRANTY; without even the implied warranty of
-        ///ME [rest of string was truncated]&quot;;.
+        ///but WITHOUT ANY WARRANTY; without eve [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string KunstwerkenHoogteSchema {
             get {
@@ -126,7 +125,7 @@ namespace Riskeer.HeightStructures.IO.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;&lt;!--
-        ///Copyright (C) Stichting Deltares 2022. All rights reserved.
+        ///Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
         ///
         ///This file is part of Riskeer.
         ///
@@ -136,8 +135,7 @@ namespace Riskeer.HeightStructures.IO.Properties {
         ///(at your option) any later version.
         ///
         ///This program is distributed in the hope that it will be useful,
-        ///but WITHOUT ANY WARRANTY; without even the implied warranty of
-        ///ME [rest of string was truncated]&quot;;.
+        ///but WITHOUT ANY WARRANTY; without eve [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string KunstwerkenHoogteSchema_0 {
             get {
