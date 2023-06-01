6fbdeb6efeb347e81bd8165d30b871ad8df88cb7
Ralf Kok <ralf.kok@deltares.nl>
2023-06-01 13:10:54 +0200

    WTI-2824: bumped up version number. Paired with tang_ds

--- Application/Riskeer/test/Application.Riskeer.Integration.Test/StorageMigrationIntegrationTest.cs
+++ Application/Riskeer/test/Application.Riskeer.Integration.Test/StorageMigrationIntegrationTest.cs
@@ -178,6 +178,7 @@ namespace Application.Riskeer.Integration.Test
             yield return new TestCaseData(Path.Combine(fileDirectory, "MigrationTestProjectSingleAssessmentSection191.risk"));
             yield return new TestCaseData(Path.Combine(fileDirectory, "MigrationTestProjectSingleAssessmentSection211.risk"));
             yield return new TestCaseData(Path.Combine(fileDirectory, "MigrationTestProjectSingleAssessmentSection221.risk"));
+            yield return new TestCaseData(Path.Combine(fileDirectory, "MigrationTestProjectSingleAssessmentSection231.risk"));
         }
 
         private static void MigrateFile(string sourceFilePath, string targetFilePath)
