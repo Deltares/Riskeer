0e2891bceef055dfaeb702e4599b72c7fae0bbe6
Dennis Tang <dennis.tang@deltares.nl>
2024-03-12 13:52:25 +0100

    WTI-2865: Added context object to wrap the calculation with the assessmentsection:
    - Needed in order to determine which HBD is associated with the calculation

---
+++ Riskeer/DuneErosion/test/Riskeer.DuneErosion.Forms.Test/PresentationObjects/HydraulicLoadsState/DuneLocationCalculationContextTest.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using Core.Common.Controls.PresentationObjects;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.AssessmentSection;
+using Riskeer.Common.Data.TestUtil;
+using Riskeer.DuneErosion.Data;
+using Riskeer.DuneErosion.Forms.PresentationObjects.RegistrationState;
+
+namespace Riskeer.DuneErosion.Forms.Test.PresentationObjects.HydraulicLoadsState
+{
+    [TestFixture]
+    public class DuneLocationCalculationContextTest
+    {
+        
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var mocks = new MockRepository();
+            var assessmentSection = mocks.Stub<IAssessmentSection>();
+            mocks.ReplayAll();
+
+            var duneLocation = new DuneLocation("0", new TestHydraulicBoundaryLocation(), new DuneLocation.ConstructionProperties());
+            var calculation = new DuneLocationCalculation(duneLocation);
+
+            // Call
+            var context = new DuneLocationCalculationContext(calculation, assessmentSection);
+
+            // Assert
+            Assert.IsInstanceOf<ObservableWrappedObjectContextBase<DuneLocationCalculation>>(context);
+            Assert.AreSame(calculation, context.WrappedData);
+            Assert.AreSame(assessmentSection, context.AssessmentSection);
+            mocks.VerifyAll();
+        }
+    }
+}
