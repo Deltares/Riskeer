882e8c78d31e9100773e459100f3f791d32e7b45
Dennis Tang <dennis.tang@deltares.nl>
2024-04-22 14:36:49 +0200

    WTI-2868 Updated GrassCoverErosionInwards:
    - Removed properties from property panel

--- Riskeer/GrassCoverErosionInwards/test/Riskeer.GrassCoverErosionInwards.Forms.Test/PropertyClasses/RegistrationState/GrassCoverErosionInwardsFailureMechanismPropertiesTest.cs
+++ Riskeer/GrassCoverErosionInwards/test/Riskeer.GrassCoverErosionInwards.Forms.Test/PropertyClasses/RegistrationState/GrassCoverErosionInwardsFailureMechanismPropertiesTest.cs
@@ -21,13 +21,9 @@
 
 using System;
 using System.ComponentModel;
-using Core.Common.Base;
-using Core.Common.Base.Data;
 using Core.Common.TestUtil;
 using Core.Gui.TestUtil;
 using NUnit.Framework;
-using Rhino.Mocks;
-using Riskeer.Common.Data.TestUtil;
 using Riskeer.GrassCoverErosionInwards.Data;
 using Riskeer.GrassCoverErosionInwards.Forms.PropertyClasses;
 using Riskeer.GrassCoverErosionInwards.Forms.PropertyClasses.RegistrationState;
@@ -40,8 +36,6 @@ namespace Riskeer.GrassCoverErosionInwards.Forms.Test.PropertyClasses.Registrati
         private const int namePropertyIndex = 0;
         private const int codePropertyIndex = 1;
         private const int inAssemblyPropertyIndex = 2;
-        private const int nPropertyIndex = 3;
-        private const int applyLengthEffectInSectionPropertyIndex = 4;
 
         [Test]
         public void Constructor_ExpectedValues()
@@ -50,11 +44,7 @@ namespace Riskeer.GrassCoverErosionInwards.Forms.Test.PropertyClasses.Registrati
             var random = new Random(21);
             var failureMechanism = new GrassCoverErosionInwardsFailureMechanism
             {
-                InAssembly = random.NextBoolean(),
-                GeneralInput =
-                {
-                    ApplyLengthEffectInSection = random.NextBoolean()
-                }
+                InAssembly = random.NextBoolean()
             };
 
             // Call
@@ -65,75 +55,15 @@ namespace Riskeer.GrassCoverErosionInwards.Forms.Test.PropertyClasses.Registrati
             Assert.AreEqual(failureMechanism.Name, properties.Name);
             Assert.AreEqual(failureMechanism.Code, properties.Code);
             Assert.AreEqual(failureMechanism.InAssembly, properties.InAssembly);
-
-            GeneralGrassCoverErosionInwardsInput generalInput = failureMechanism.GeneralInput;
-            Assert.AreEqual(2, properties.N.NumberOfDecimalPlaces);
-            Assert.AreEqual(generalInput.N,
-                            properties.N,
-                            properties.N.GetAccuracy());
-            Assert.AreEqual(generalInput.ApplyLengthEffectInSection, properties.ApplyLengthEffectInSection);
         }
 
         [Test]
-        public void Constructor_InAssemblyTrue_PropertiesHaveExpectedAttributesValues()
+        public void Constructor_PropertiesHaveExpectedAttributesValues()
         {
             // Setup
             var failureMechanism = new GrassCoverErosionInwardsFailureMechanism
             {
                 InAssembly = true
-            };
-
-            // Call
-            var properties = new GrassCoverErosionInwardsFailureMechanismProperties(failureMechanism);
-
-            // Assert
-            PropertyDescriptorCollection dynamicProperties = PropertiesTestHelper.GetAllVisiblePropertyDescriptors(properties);
-            Assert.AreEqual(5, dynamicProperties.Count);
-
-            const string generalCategory = "Algemeen";
-            const string lengthEffectCategory = "Lengte-effect";
-
-            PropertyDescriptor nameProperty = dynamicProperties[namePropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(nameProperty,
-                                                                            generalCategory,
-                                                                            "Naam",
-                                                                            "De naam van het faalmechanisme.",
-                                                                            true);
-
-            PropertyDescriptor labelProperty = dynamicProperties[codePropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(labelProperty,
-                                                                            generalCategory,
-                                                                            "Label",
-                                                                            "Het label van het faalmechanisme.",
-                                                                            true);
-
-            PropertyDescriptor inAssemblyProperty = dynamicProperties[inAssemblyPropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(inAssemblyProperty,
-                                                                            generalCategory,
-                                                                            "In assemblage",
-                                                                            "Geeft aan of dit faalmechanisme wordt meegenomen in de assemblage.",
-                                                                            true);
-
-            PropertyDescriptor nProperty = dynamicProperties[nPropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(nProperty,
-                                                                            lengthEffectCategory,
-                                                                            "N [-]",
-                                                                            "De parameter 'N' die gebruikt wordt om het lengte-effect mee te nemen in de beoordeling.");
-
-            PropertyDescriptor applySectionLengthInSectionProperty = dynamicProperties[applyLengthEffectInSectionPropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(applySectionLengthInSectionProperty,
-                                                                            lengthEffectCategory,
-                                                                            "Toepassen lengte-effect binnen vak",
-                                                                            "Geeft aan of het lengte-effect binnen een vak toegepast wordt.");
-        }
-
-        [Test]
-        public void Constructor_InAssemblyFalse_PropertiesHaveExpectedAttributesValues()
-        {
-            // Setup
-            var failureMechanism = new GrassCoverErosionInwardsFailureMechanism
-            {
-                InAssembly = false
             };
 
             // Call
@@ -165,99 +95,6 @@ namespace Riskeer.GrassCoverErosionInwards.Forms.Test.PropertyClasses.Registrati
                                                                             "In assemblage",
                                                                             "Geeft aan of dit faalmechanisme wordt meegenomen in de assemblage.",
                                                                             true);
-        }
-
-        [Test]
-        [SetCulture("nl-NL")]
-        [TestCase(0.0)]
-        [TestCase(-1.0)]
-        [TestCase(-20.0)]
-        public void N_SetInvalidValue_ThrowsArgumentOutOfRangeExceptionNoNotifications(double value)
-        {
-            // Setup
-            var mocks = new MockRepository();
-            var observer = mocks.StrictMock<IObserver>();
-            mocks.ReplayAll();
-
-            var failureMechanism = new GrassCoverErosionInwardsFailureMechanism();
-            failureMechanism.Attach(observer);
-            var properties = new GrassCoverErosionInwardsFailureMechanismProperties(failureMechanism);
-
-            // Call
-            void Call() => properties.N = (RoundedDouble) value;
-
-            // Assert
-            const string expectedMessage = "De waarde voor 'N' moet in het bereik [1,00, 20,00] liggen.";
-            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<ArgumentOutOfRangeException>(Call, expectedMessage);
-            mocks.VerifyAll();
-        }
-
-        [Test]
-        [TestCase(1.0)]
-        [TestCase(10.0)]
-        [TestCase(20.0)]
-        public void N_SetValidValue_SetsValueAndUpdatesObserver(double value)
-        {
-            // Setup
-            var mocks = new MockRepository();
-            var observer = mocks.StrictMock<IObserver>();
-            observer.Expect(o => o.UpdateObserver());
-            mocks.ReplayAll();
-
-            var failureMechanism = new GrassCoverErosionInwardsFailureMechanism();
-            failureMechanism.Attach(observer);
-            var properties = new GrassCoverErosionInwardsFailureMechanismProperties(failureMechanism);
-
-            // Call
-            properties.N = (RoundedDouble) value;
-
-            // Assert
-            Assert.AreEqual(value, failureMechanism.GeneralInput.N, failureMechanism.GeneralInput.N.GetAccuracy());
-            mocks.VerifyAll();
-        }
-
-        [Test]
-        public void ApplyLengthEffectInSection_SetNewValue_NotifyObservers()
-        {
-            // Setup
-            var mocks = new MockRepository();
-            var observer = mocks.StrictMock<IObserver>();
-            observer.Expect(o => o.UpdateObserver());
-            mocks.ReplayAll();
-
-            var failureMechanism = new GrassCoverErosionInwardsFailureMechanism();
-
-            failureMechanism.Attach(observer);
-
-            var properties = new GrassCoverErosionInwardsFailureMechanismProperties(failureMechanism);
-
-            // Call
-            properties.ApplyLengthEffectInSection = true;
-
-            // Assert
-            Assert.IsTrue(failureMechanism.GeneralInput.ApplyLengthEffectInSection);
-            mocks.VerifyAll();
-        }
-
-        [Test]
-        [TestCase(true)]
-        [TestCase(false)]
-        public void DynamicVisibleValidationMethod_DependingOnInAssembly_ReturnExpectedVisibility(bool inAssembly)
-        {
-            // Setup
-            var failureMechanism = new GrassCoverErosionInwardsFailureMechanism
-            {
-                InAssembly = inAssembly
-            };
-            var properties = new GrassCoverErosionInwardsFailureMechanismProperties(failureMechanism);
-
-            // Assert
-            Assert.IsTrue(properties.DynamicVisibleValidationMethod(nameof(properties.Name)));
-            Assert.IsTrue(properties.DynamicVisibleValidationMethod(nameof(properties.Code)));
-            Assert.IsTrue(properties.DynamicVisibleValidationMethod(nameof(properties.InAssembly)));
-
-            Assert.AreEqual(inAssembly, properties.DynamicVisibleValidationMethod(nameof(properties.N)));
-            Assert.AreEqual(inAssembly, properties.DynamicVisibleValidationMethod(nameof(properties.ApplyLengthEffectInSection)));
         }
     }
 }
