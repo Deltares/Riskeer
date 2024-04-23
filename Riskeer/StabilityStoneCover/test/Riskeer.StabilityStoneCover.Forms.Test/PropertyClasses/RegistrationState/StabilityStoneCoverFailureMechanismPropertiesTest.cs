578b74cc0c820182ee2be9f0e821dabb2e6be46b
Dennis Tang <dennis.tang@deltares.nl>
2024-04-23 20:30:55 +0200

    WTI-2868 Updated StabilityStoneCover:
    - Updated properties

--- Riskeer/StabilityStoneCover/test/Riskeer.StabilityStoneCover.Forms.Test/PropertyClasses/RegistrationState/StabilityStoneCoverFailureMechanismPropertiesTest.cs
+++ Riskeer/StabilityStoneCover/test/Riskeer.StabilityStoneCover.Forms.Test/PropertyClasses/RegistrationState/StabilityStoneCoverFailureMechanismPropertiesTest.cs
@@ -21,13 +21,9 @@
 
 using System;
 using System.ComponentModel;
-using Core.Common.Base;
-using Core.Common.Base.Data;
 using Core.Common.TestUtil;
 using Core.Gui.TestUtil;
 using NUnit.Framework;
-using Rhino.Mocks;
-using Riskeer.Common.Data.TestUtil;
 using Riskeer.StabilityStoneCover.Data;
 using Riskeer.StabilityStoneCover.Forms.PropertyClasses;
 using Riskeer.StabilityStoneCover.Forms.PropertyClasses.RegistrationState;
@@ -39,8 +35,6 @@ namespace Riskeer.StabilityStoneCover.Forms.Test.PropertyClasses.RegistrationSta
         private const int namePropertyIndex = 0;
         private const int codePropertyIndex = 1;
         private const int inAssemblyPropertyIndex = 2;
-        private const int nPropertyIndex = 3;
-        private const int applyLengthEffectInSectionPropertyIndex = 4;
 
         [Test]
         public void Constructor_ExpectedValues()
@@ -60,73 +54,13 @@ namespace Riskeer.StabilityStoneCover.Forms.Test.PropertyClasses.RegistrationSta
             Assert.AreEqual(failureMechanism.Name, properties.Name);
             Assert.AreEqual(failureMechanism.Code, properties.Code);
             Assert.AreEqual(failureMechanism.InAssembly, properties.InAssembly);
-
-            GeneralStabilityStoneCoverWaveConditionsInput generalInput = failureMechanism.GeneralInput;
-            Assert.AreEqual(2, properties.N.NumberOfDecimalPlaces);
-            Assert.AreEqual(generalInput.N,
-                            properties.N,
-                            properties.N.GetAccuracy());
-            Assert.AreEqual(generalInput.ApplyLengthEffectInSection, properties.ApplyLengthEffectInSection);
         }
 
         [Test]
-        public void Constructor_InAssemblyTrue_PropertiesHaveExpectedAttributesValues()
+        public void Constructor_PropertiesHaveExpectedAttributesValues()
         {
             // Setup
             var failureMechanism = new StabilityStoneCoverFailureMechanism();
-
-            // Call
-            var properties = new StabilityStoneCoverFailureMechanismProperties(failureMechanism);
-
-            // Assert
-            PropertyDescriptorCollection dynamicProperties = PropertiesTestHelper.GetAllVisiblePropertyDescriptors(properties);
-            Assert.AreEqual(5, dynamicProperties.Count);
-
-            const string generalCategory = "Algemeen";
-            const string lengthEffectCategory = "Lengte-effect";
-
-            PropertyDescriptor nameProperty = dynamicProperties[namePropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(nameProperty,
-                                                                            generalCategory,
-                                                                            "Naam",
-                                                                            "De naam van het faalmechanisme.",
-                                                                            true);
-
-            PropertyDescriptor labelProperty = dynamicProperties[codePropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(labelProperty,
-                                                                            generalCategory,
-                                                                            "Label",
-                                                                            "Het label van het faalmechanisme.",
-                                                                            true);
-
-            PropertyDescriptor inAssemblyProperty = dynamicProperties[inAssemblyPropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(inAssemblyProperty,
-                                                                            generalCategory,
-                                                                            "In assemblage",
-                                                                            "Geeft aan of dit faalmechanisme wordt meegenomen in de assemblage.",
-                                                                            true);
-
-            PropertyDescriptor nProperty = dynamicProperties[nPropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(nProperty,
-                                                                            lengthEffectCategory,
-                                                                            "N [-]",
-                                                                            "De parameter 'N' die gebruikt wordt om het lengte-effect mee te nemen in de beoordeling.");
-
-            PropertyDescriptor applySectionLengthInSectionProperty = dynamicProperties[applyLengthEffectInSectionPropertyIndex];
-            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(applySectionLengthInSectionProperty,
-                                                                            lengthEffectCategory,
-                                                                            "Toepassen lengte-effect binnen vak",
-                                                                            "Geeft aan of het lengte-effect binnen een vak toegepast wordt.");
-        }
-
-        [Test]
-        public void Constructor_InAssemblyFalse_PropertiesHaveExpectedAttributesValues()
-        {
-            // Setup
-            var failureMechanism = new StabilityStoneCoverFailureMechanism
-            {
-                InAssembly = false
-            };
 
             // Call
             var properties = new StabilityStoneCoverFailureMechanismProperties(failureMechanism);
@@ -157,105 +91,6 @@ namespace Riskeer.StabilityStoneCover.Forms.Test.PropertyClasses.RegistrationSta
                                                                             "In assemblage",
                                                                             "Geeft aan of dit faalmechanisme wordt meegenomen in de assemblage.",
                                                                             true);
-        }
-
-        [Test]
-        [SetCulture("nl-NL")]
-        [TestCase(0.0)]
-        [TestCase(-1.0)]
-        [TestCase(-20.0)]
-        public void N_SetInvalidValue_ThrowsArgumentOutOfRangeExceptionNoNotifications(double newN)
-        {
-            // Setup
-            var mocks = new MockRepository();
-            var observer = mocks.StrictMock<IObserver>();
-            mocks.ReplayAll();
-
-            var failureMechanism = new StabilityStoneCoverFailureMechanism();
-            failureMechanism.Attach(observer);
-
-            var properties = new StabilityStoneCoverFailureMechanismProperties(failureMechanism);
-
-            // Call
-            void Call() => properties.N = (RoundedDouble) newN;
-
-            // Assert
-            const string expectedMessage = "De waarde voor 'N' moet in het bereik [1,00, 20,00] liggen.";
-            TestHelper.AssertThrowsArgumentExceptionAndTestMessage<ArgumentOutOfRangeException>(Call, expectedMessage);
-
-            mocks.VerifyAll();
-        }
-
-        [Test]
-        [TestCase(1.0)]
-        [TestCase(10.0)]
-        [TestCase(20.0)]
-        public void N_SetValidValue_UpdateDataAndNotifyObservers(double newN)
-        {
-            // Setup
-            var mocks = new MockRepository();
-            var observer = mocks.StrictMock<IObserver>();
-            observer.Expect(o => o.UpdateObserver());
-            mocks.ReplayAll();
-
-            var failureMechanism = new StabilityStoneCoverFailureMechanism();
-            failureMechanism.Attach(observer);
-
-            var properties = new StabilityStoneCoverFailureMechanismProperties(failureMechanism);
-
-            // Call
-            properties.N = (RoundedDouble) newN;
-
-            // Assert
-            Assert.AreEqual(newN, failureMechanism.GeneralInput.N, failureMechanism.GeneralInput.N.GetAccuracy());
-
-            mocks.VerifyAll();
-        }
-
-        [Test]
-        public void ApplyLengthEffectInSection_SetNewValue_NotifyObservers()
-        {
-            // Setup
-            var mocks = new MockRepository();
-            var observer = mocks.StrictMock<IObserver>();
-            observer.Expect(o => o.UpdateObserver());
-            mocks.ReplayAll();
-
-            var failureMechanism = new StabilityStoneCoverFailureMechanism();
-
-            failureMechanism.Attach(observer);
-
-            var properties = new StabilityStoneCoverFailureMechanismProperties(failureMechanism);
-
-            // Call
-            properties.ApplyLengthEffectInSection = true;
-
-            // Assert
-            Assert.IsTrue(failureMechanism.GeneralInput.ApplyLengthEffectInSection);
-            mocks.VerifyAll();
-        }
-
-        [Test]
-        [TestCase(true)]
-        [TestCase(false)]
-        public void DynamicVisibleValidationMethod_DependingOnInAssembly_ReturnExpectedVisibility(bool inAssembly)
-        {
-            // Setup
-            var failureMechanism = new StabilityStoneCoverFailureMechanism
-            {
-                InAssembly = inAssembly
-            };
-            var properties = new StabilityStoneCoverFailureMechanismProperties(failureMechanism);
-
-            // Call & Assert
-            Assert.IsTrue(properties.DynamicVisibleValidationMethod(nameof(properties.Name)));
-            Assert.IsTrue(properties.DynamicVisibleValidationMethod(nameof(properties.Code)));
-            Assert.IsTrue(properties.DynamicVisibleValidationMethod(nameof(properties.InAssembly)));
-
-            Assert.AreEqual(inAssembly, properties.DynamicVisibleValidationMethod(nameof(properties.N)));
-            Assert.AreEqual(inAssembly, properties.DynamicVisibleValidationMethod(nameof(properties.ApplyLengthEffectInSection)));
-
-            Assert.IsTrue(properties.DynamicVisibleValidationMethod(null));
         }
     }
 }
