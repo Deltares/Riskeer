e672e8b8deef853062885af2f5a917899400e9b5
Dennis Tang <dennis.tang@deltares.nl>
2024-06-26 11:08:29 +0200

    WTI-2876: Fixed comments test:
    - Added modified property panel for a failure mechanism section configuration

---
+++ Riskeer/Common/src/Riskeer.Common.Forms/PropertyClasses/FailureMechanismSectionConfigurationProperties.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using Core.Common.Base.Data;
+using Core.Common.Util.Attributes;
+using Core.Gui.Attributes;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Data.Probability;
+using Riskeer.Common.Forms.Properties;
+
+namespace Riskeer.Common.Forms.PropertyClasses
+{
+    /// <summary>
+    /// ViewModel of <see cref="FailureMechanismSectionConfiguration"/> for properties panel.
+    /// </summary>
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class FailureMechanismSectionConfigurationProperties : FailureMechanismSectionProperties
+    {
+        private readonly FailureMechanismSectionConfiguration sectionConfiguration;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="FailureMechanismSectionConfiguration"/>.
+        /// </summary>
+        /// <param name="sectionConfiguration">The section configuration to show the properties for.</param>
+        /// <param name="sectionStart">The start of the section from the beginning
+        /// of the reference line in meters.</param>
+        /// <param name="sectionEnd">The end of the section from the beginning of
+        /// the reference line in meters.</param>
+        /// <param name="b">The 'b' parameter representing the equivalent independent length to factor in the
+        /// 'length effect'.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="sectionConfiguration"/>
+        /// is <c>null</c>.</exception>
+        public FailureMechanismSectionConfigurationProperties(FailureMechanismSectionConfiguration sectionConfiguration, double sectionStart, double sectionEnd, double b)
+            : base(sectionConfiguration?.Section ?? throw new ArgumentNullException(nameof(sectionConfiguration)), sectionStart, sectionEnd)
+        {
+            if (sectionConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(sectionConfiguration));
+            }
+
+            this.sectionConfiguration = sectionConfiguration;
+
+            LengthEffectNRounded = new RoundedDouble(2, this.sectionConfiguration.GetN(b));
+        }
+
+        [PropertyOrder(8)]
+        [ResourcesCategory(typeof(Resources), nameof(Resources.Categories_General))]
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.FailureMechanismSectionConfigurationsView_Parameter_A_DisplayName))]
+        [ResourcesDescription(typeof(Resources), nameof(Resources.Parameter_A_Description))]
+        public RoundedDouble ParameterA
+        {
+            get
+            {
+                return sectionConfiguration.A;
+            }
+            set
+            {
+                sectionConfiguration.A = value;
+                sectionConfiguration.NotifyObservers();
+            }
+        }
+
+        [PropertyOrder(9)]
+        [ResourcesCategory(typeof(Resources), nameof(Resources.Categories_General))]
+        [ResourcesDisplayName(typeof(Resources), nameof(Resources.FailureMechanismSectionConfigurationsView_LengthEffectNRounded_DisplayName))]
+        [ResourcesDescription(typeof(Resources), nameof(Resources.LengthEffectNRounded_Description))]
+        public RoundedDouble LengthEffectNRounded { get; }
+    }
+}
---
+++ Riskeer/Common/test/Riskeer.Common.Forms.Test/PropertyClasses/FailureMechanismSectionConfigurationPropertiesTest.cs
@@ -0,0 +1,178 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of Riskeer.
+//
+// Riskeer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+using System;
+using System.ComponentModel;
+using Core.Common.Base;
+using Core.Common.Base.Data;
+using Core.Common.TestUtil;
+using Core.Gui.TestUtil;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Riskeer.Common.Data.FailureMechanism;
+using Riskeer.Common.Data.Probability;
+using Riskeer.Common.Data.TestUtil;
+using Riskeer.Common.Forms.PropertyClasses;
+
+namespace Riskeer.Common.Forms.Test.PropertyClasses
+{
+    [TestFixture]
+    public class FailureMechanismSectionConfigurationPropertiesTest
+    {
+        [Test]
+        public void Constructor_SectionConfigurationNull_ThrowsArgumentNullException()
+        {
+            // Call
+            void Call() => new FailureMechanismSectionConfigurationProperties(null, double.NaN, double.NaN, double.NaN);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(Call);
+            Assert.AreEqual("sectionConfiguration", exception.ParamName);
+        }
+
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            double sectionStart = random.NextDouble();
+            double sectionEnd = random.NextDouble();
+            double b = random.NextDouble();
+
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionConfiguration = new FailureMechanismSectionConfiguration(section);
+
+            // Call
+            var properties = new FailureMechanismSectionConfigurationProperties(sectionConfiguration, sectionStart, sectionEnd, b);
+
+            // Assert
+            Assert.IsInstanceOf<FailureMechanismSectionProperties>(properties);
+            Assert.AreSame(section, properties.Data);
+            TestHelper.AssertTypeConverter<FailureMechanismSectionConfigurationProperties, ExpandableObjectConverter>();
+
+            Assert.AreEqual(section.Name, properties.Name);
+            Assert.AreEqual(2, properties.Length.NumberOfDecimalPlaces);
+            Assert.AreEqual(section.Length, properties.Length, properties.Length.GetAccuracy());
+            Assert.AreEqual(section.StartPoint, properties.StartPoint);
+            Assert.AreEqual(section.EndPoint, properties.EndPoint);
+
+            Assert.AreEqual(2, properties.SectionStart.NumberOfDecimalPlaces);
+            Assert.AreEqual(sectionStart, properties.SectionStart, properties.SectionStart.GetAccuracy());
+            Assert.AreEqual(2, properties.SectionEnd.NumberOfDecimalPlaces);
+            Assert.AreEqual(sectionEnd, properties.SectionEnd, properties.SectionEnd.GetAccuracy());
+
+            Assert.AreEqual(sectionConfiguration.A, properties.ParameterA);
+            Assert.AreEqual(2, properties.LengthEffectNRounded.NumberOfDecimalPlaces);
+            Assert.AreEqual(sectionConfiguration.GetN(b), properties.LengthEffectNRounded, properties.LengthEffectNRounded.GetAccuracy());
+        }
+
+        [Test]
+        public void Constructor_Always_PropertiesHaveExpectedAttributesValues()
+        {
+            // Setup
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionConfiguration = new FailureMechanismSectionConfiguration(section);
+
+            // Call
+            var properties = new FailureMechanismSectionConfigurationProperties(sectionConfiguration, double.NaN, double.NaN, double.NaN);
+
+            // Assert
+            PropertyDescriptorCollection dynamicProperties = PropertiesTestHelper.GetAllVisiblePropertyDescriptors(properties);
+            Assert.AreEqual(8, dynamicProperties.Count);
+
+            PropertyDescriptor nameProperty = dynamicProperties[0];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(nameProperty,
+                                                                            "Algemeen",
+                                                                            "Vaknaam",
+                                                                            "De naam van het vak.",
+                                                                            true);
+            PropertyDescriptor sectionStartDistanceProperty = dynamicProperties[1];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(sectionStartDistanceProperty,
+                                                                            "Algemeen",
+                                                                            "Metrering van* [m]",
+                                                                            "De afstand tussen het beginpunt van het vak en het begin van het traject, gemeten langs het traject in meters (afgerond).",
+                                                                            true);
+            PropertyDescriptor sectionEndDistanceProperty = dynamicProperties[2];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(sectionEndDistanceProperty,
+                                                                            "Algemeen",
+                                                                            "Metrering tot* [m]",
+                                                                            "De afstand tussen het eindpunt van het vak en het begin van het traject, gemeten langs het traject in meters (afgerond).",
+                                                                            true);
+            PropertyDescriptor lengthProperty = dynamicProperties[3];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(lengthProperty,
+                                                                            "Algemeen",
+                                                                            "Lengte* [m]",
+                                                                            "De totale lengte van het vak in meters (afgerond).",
+                                                                            true);
+            PropertyDescriptor startPointProperty = dynamicProperties[4];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(startPointProperty,
+                                                                            "Algemeen",
+                                                                            "Beginpunt",
+                                                                            "Beginpunt van het vak (X-coördinaat, Y-coördinaat).",
+                                                                            true);
+            PropertyDescriptor endPointProperty = dynamicProperties[5];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(endPointProperty,
+                                                                            "Algemeen",
+                                                                            "Eindpunt",
+                                                                            "Eindpunt van het vak (X-coördinaat, Y-coördinaat).",
+                                                                            true);
+
+            PropertyDescriptor parameterAProperty = dynamicProperties[6];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(parameterAProperty,
+                                                                            "Algemeen",
+                                                                            "a [-]",
+                                                                            "Mechanismegevoelige fractie van het dijkvak.");
+
+            PropertyDescriptor lengthEffectNRoundedProperty = dynamicProperties[7];
+            PropertiesTestHelper.AssertRequiredPropertyDescriptorProperties(lengthEffectNRoundedProperty,
+                                                                            "Algemeen",
+                                                                            "Nvak* [-]",
+                                                                            "De parameter 'Nvak*' die het lengte-effect beschrijft in de berekening van de faalkans per vak in de semi-probabilistische toets.",
+                                                                            true);
+        }
+
+        [Test]
+        public void ParameterA_SetNewValue_NotifyObservers()
+        {
+            // Setup
+            var random = new Random(21);
+            RoundedDouble newParameterAValue = random.NextRoundedDouble();
+
+            var mocks = new MockRepository();
+            var observer = mocks.StrictMock<IObserver>();
+            observer.Expect(o => o.UpdateObserver());
+            mocks.ReplayAll();
+
+            FailureMechanismSection section = FailureMechanismSectionTestFactory.CreateFailureMechanismSection();
+            var sectionConfiguration = new FailureMechanismSectionConfiguration(section);
+            sectionConfiguration.Attach(observer);
+
+            var properties = new FailureMechanismSectionConfigurationProperties(sectionConfiguration, double.NaN, double.NaN, double.NaN);
+
+            // Call
+            properties.ParameterA = newParameterAValue;
+
+            // Assert
+            Assert.AreEqual(newParameterAValue, sectionConfiguration.A, sectionConfiguration.A.GetAccuracy());
+            mocks.VerifyAll();
+        }
+    }
+}
