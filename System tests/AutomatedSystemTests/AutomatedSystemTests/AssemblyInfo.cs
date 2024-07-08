af86edc54ab9a3e7f83ad47c4e9bb4b758deda8d
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-07-08 11:47:39 +0200

    Updated license headers. WTI-2882

--- System tests/AutomatedSystemTests/AutomatedSystemTests/AssemblyInfo.cs
+++ System tests/AutomatedSystemTests/AutomatedSystemTests/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stichting Deltares and State of the Netherlands")]
 [assembly: AssemblyProduct("AutomatedSystemTests Riskeer")]
-[assembly: AssemblyCopyright("Copyright © Stichting Deltares and State of the Netherlands 2023")]
+[assembly: AssemblyCopyright("Copyright © Stichting Deltares and State of the Netherlands 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
