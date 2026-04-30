734003f9d6145ad23519c3d75b8b47e05656027b
Edwin Spee <edwin.spee@deltares.nl>
2026-04-30 11:34:20 +0200

    fix tests and application startup after upgrading log4net

--- Core/Gui/test/Core.Gui.Test/Forms/Log/MessageWindowLogAppenderTest.cs
+++ Core/Gui/test/Core.Gui.Test/Forms/Log/MessageWindowLogAppenderTest.cs
@@ -6,7 +6,7 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -59,8 +59,8 @@ namespace Core.Gui.Test.Forms.Log
             Assert.IsInstanceOf<OnlyOnceErrorHandler>(appender.ErrorHandler);
             Assert.AreEqual(null, appender.FilterHead);
             Assert.AreEqual(null, appender.Layout);
-            Assert.AreEqual(null, appender.Name);
-            Assert.AreEqual(null, appender.Threshold);
+            Assert.AreEqual(string.Empty, appender.Name);
+            Assert.AreEqual(Level.All, appender.Threshold);
             Assert.AreSame(appender, MessageWindowLogAppender.Instance);
         }
 
