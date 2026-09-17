84b9f70eb1e161868d15dac7c0511a661219b7da
kleijn <elmar.kleijn@deltares.nl>
2026-09-17 13:05:50 +0200

    Removed unreachable code from DoubleParsingHelper.
    OverflowException is .NET Framework and .NET Core 2.2 and earlier versions only.

--- Core/Common/src/Core.Common.Base/Helpers/DoubleParsingHelper.cs
+++ Core/Common/src/Core.Common.Base/Helpers/DoubleParsingHelper.cs
@@ -73,10 +73,6 @@ namespace Core.Common.Base.Helpers
 
                 throw new DoubleParsingException(Resources.DoubleParsingHelper_Parse_String_must_represent_number, exception);
             }
-            catch (OverflowException exception)
-            {
-                throw new DoubleParsingException(Resources.DoubleParsingHelper_Parse_String_too_small_or_too_big_to_represent_as_double, exception);
-            }
         }
     }
 }
