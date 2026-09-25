42bb7a2cd9436c6d4dafc31f4e313669adb0b0a7
kleijn <elmar.kleijn@deltares.nl>
2026-09-25 13:08:28 +0200

    Removed redundant formatting logic.

--- Riskeer/DuneErosion/src/Riskeer.DuneErosion.IO/DuneLocationCalculationsWriter.cs
+++ Riskeer/DuneErosion/src/Riskeer.DuneErosion.IO/DuneLocationCalculationsWriter.cs
@@ -64,7 +64,7 @@ namespace Riskeer.DuneErosion.IO
                 || Path.GetFileName(filePath).IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
             {
                 throw new CriticalFileWriteException(string.Format(CoreCommonUtilResources.Error_General_output_error_0, filePath),
-                                                     new ArgumentException(string.Format(CoreCommonUtilResources.Error_Path_cannot_contain_invalid_characters, filePath)));
+                                                     new ArgumentException(CoreCommonUtilResources.Error_Path_cannot_contain_invalid_characters));
             }
             
             var stringBuilder = new StringBuilder(Resources.DuneLocationCalculationsWriter_WriteDuneLocationCalculations_HeaderLine + Environment.NewLine);
--- Riskeer/Revetment/src/Riskeer.Revetment.IO/WaveConditions/WaveConditionsWriter.cs
+++ Riskeer/Revetment/src/Riskeer.Revetment.IO/WaveConditions/WaveConditionsWriter.cs
@@ -62,7 +62,7 @@ namespace Riskeer.Revetment.IO.WaveConditions
                 || Path.GetFileName(filePath).IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
             {
                 throw new CriticalFileWriteException(string.Format(CoreCommonUtilResources.Error_General_output_error_0, filePath),
-                                                     new ArgumentException(string.Format(CoreCommonUtilResources.Error_Path_cannot_contain_invalid_characters, filePath)));
+                                                     new ArgumentException(CoreCommonUtilResources.Error_Path_cannot_contain_invalid_characters));
             }
             
             var stringBuilder = new StringBuilder(Resources.WaveConditionsWriter_HeaderLine + Environment.NewLine);
